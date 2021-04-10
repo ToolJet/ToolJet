c19246606384dd99f843adc6f7566f730983fb06
navaneeth <navaneethpk@outlook.com>
2021-04-10 22:25:15 +0530

    [60] Drag actual-sized container while repositioning

--- frontend/src/Editor/BoxDragPreview.jsx
+++ frontend/src/Editor/BoxDragPreview.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState, memo } from 'react';
-import { Box } from './Box';
-const styles = {
-    display: 'inline-block',
-    width: 160,
-    height: 80,
-    background: '#a1bdf6',
-};
-export const BoxDragPreview = memo(function BoxDragPreview({ component }) {
+
+export const BoxDragPreview = memo(function BoxDragPreview({ item }) {
+    
     const [tickTock, setTickTock] = useState(false);
+
     useEffect(function subscribeToIntervalTick() {
         const interval = setInterval(() => setTickTock(!tickTock), 500);
         return () => clearInterval(interval);
     }, [tickTock]);
-    return (<div>
-				<img src="/public/images/logo.png" width="110" height="32" alt="StackEgg" class="navbar-brand-image"/>
-			</div>);
+
+    return (
+        <div style={{ background: '#438fd7', opacity: '0.7', height: item.height, width: item.width }}>
+		</div>
+    );
 });
