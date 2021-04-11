2bf4a87cf0851b5c3855cba630428ca2ed99361a
navaneeth <navaneethpk@outlook.com>
2021-04-11 12:08:06 +0530

    Feature: Container component

---
+++ frontend/src/Editor/Components/Container.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { resolve } from '@/_helpers/utils';
+
+export const Container = function Container({ id, width, height, component, onComponentClick, currentState }) {
+
+    const backgroundColor = component.definition.styles.backgroundColor.value;
+
+    const computedStyles = { 
+        backgroundColor,
+        width,
+        height
+    }
+
+    return (
+        <div className="jet-container" onClick={() => onComponentClick(id, component) } style={computedStyles} >
+            
+        </div>
+    );
+};
