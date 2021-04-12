d6aec598ac98652fceb331dd9d994931611adee2
navaneeth <navaneethpk@outlook.com>
2021-04-12 19:45:30 +0530

    Component: Date range picker

---
+++ frontend/src/Editor/Components/DateRangePicker.jsx
@@ -0,0 +1,55 @@
+import React, { useMemo, useState, useEffect } from "react";
+import Datetime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
+import {
+    START_DATE,
+    END_DATE,
+} from 'react-dates/constants';
+
+export const DaterangePicker = function DaterangePicker({ id, width, height, component, onComponentClick, currentState, onComponentOptionChanged }) {
+
+    console.log('currentState', currentState);
+
+    const startDateProp = component.definition.properties.startDate;
+    const endDateProp = component.definition.properties.endDate;
+    const formatProp = component.definition.properties.format;
+
+    const [focusedInput, setFocusedInput] = useState(null);
+    const [startDate, setStartDate] = useState(startDateProp ? startDateProp.value : null);
+    const [endDate, setEendDate] = useState(endDateProp ? endDateProp.value : null);
+
+    function onDateChange({ startDate, endDate }) {
+        
+        if(startDate){
+            onComponentOptionChanged(component, 'startDate', startDate.format(formatProp.value));
+        }
+
+        if(endDate){
+            onComponentOptionChanged(component, 'endDate', endDate.format(formatProp.value));
+        }
+
+        setStartDate(startDate);
+        setEendDate(endDate);
+    }
+
+    function focusChanged(focus){
+        setFocusedInput(focus);
+    }
+
+    return (
+        <div onClick={() => onComponentClick(id, component) }>
+            <DateRangePicker
+                startDate={startDate}
+                startDateId="startDate" 
+                endDate={endDate}
+                endDateId="endDate"
+                onDatesChange={({ startDate, endDate }) => onDateChange({ startDate, endDate })} 
+                onFocusChange={focus => focusChanged(focus)}
+                focusedInput={focusedInput}
+            />
+        </div>
+    );
+}
