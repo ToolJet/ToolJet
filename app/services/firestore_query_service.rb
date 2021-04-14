848a9eb9cd6ff2af188b7dfa5e41e1cbe86d4c69
navaneeth <navaneethpk@outlook.com>
2021-04-14 11:16:06 +0530

    Firestore: get all records of a collection

--- app/services/firestore_query_service.rb
+++ app/services/firestore_query_service.rb
@@ -36,6 +36,15 @@ class FirestoreQueryService
                 doc_ref.set body
             end
 
+            if operation == 'query_collection'
+                path = options["path"]
+                doc_ref = firestore.col path
+                data = []
+                doc_ref.get do |doc|
+                    data << { data: doc.data, document_id: doc.document_id}
+                end
+            end
+
         rescue => e
             puts e
             error = e.message
