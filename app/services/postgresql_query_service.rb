e448840b8a20066a81a5042d3e74471df82f948b
navaneeth <navaneethpk@outlook.com>
2021-04-15 13:17:24 +0530

    Query services should use encrypted credentials

--- app/services/postgresql_query_service.rb
+++ app/services/postgresql_query_service.rb
@@ -1,35 +1,26 @@
 class PostgresqlQueryService
 
-    attr_accessor :data_query, :data_source, :options
+    attr_accessor :data_query, :data_source, :options, :source_options
 
-    def initialize(data_query, options)
+    def initialize(data_query, options, source_options)
         @data_query = data_query
         @data_source = data_query.data_source
         @options = options
+        @source_options = source_options
     end
 
     def process
         conn = PG.connect( 
-            dbname: data_source.options["database"], 
-            user: data_source.options["username"],
-            password:data_source.options["password"],
-            host: data_source.options["host"],
-            port: data_source.options["port"]
+            dbname: source_options["database"], 
+            user: source_options["username"],
+            password:source_options["password"],
+            host: source_options["host"],
+            port: source_options["port"]
         )
 
         query_text = options["query"]
 
         result = conn.exec( query_text )
         { status: 'success', data: result.to_a }
-    end
-    
-    def self.connection options
-        PG.connect( 
-            dbname: options["database"], 
-            user: options["username"],
-            password:options["password"],
-            host: options["host"],
-            port: options["port"]
-        )
     end
 end
