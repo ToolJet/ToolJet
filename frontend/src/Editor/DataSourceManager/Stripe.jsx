0f407002fd066aa04e0068a196f81517a214a233
navaneeth <navaneethpk@outlook.com>
2021-04-16 22:22:29 +0530

    "Encrypted" tag for encrypted fields of data sources

--- frontend/src/Editor/DataSourceManager/Stripe.jsx
+++ frontend/src/Editor/DataSourceManager/Stripe.jsx
@@ -7,7 +7,13 @@ export const Stripe = ({ optionchanged, createDataSource, options  }) => {
         <div>
             <div className="row">
                 <div className="col-md-12">
-                    <label class="form-label">API key</label>
+                    <label class="form-label">
+                        API key
+                        <small className="text-green mx-2">
+                            <img className="mx-2" src="https://www.svgrepo.com/show/12694/padlock.svg" width="12" height="12"/>
+                            Encrypted
+                        </small>
+                    </label>
                     <input type="text" class="form-control" onChange={(e) => optionchanged('api_key', e.target.value)} value={options.api_key} />
                     <small className="text-muted">For creating API keys, visit: <a href="https://dashboard.stripe.com/account/apikeys" target="_blank">Stripe Developers</a></small>
                 </div>
