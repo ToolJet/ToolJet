a5d0cfdf7b732db7b8a520de1ad34bfe7e9b3f59
navaneeth <navaneethpk@outlook.com>
2021-04-16 12:00:09 +0530

    Use secret_key_base from ENV

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
     class << self
       def encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
-        JWT.encode(payload, Rails.application.credentials.secret_key_base)
+        JWT.encode(payload, ENV.fetch("SECRET_KEY_BASE"))
       end
    
       def decode(token)
-        body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+        body = JWT.decode(token, ENV.fetch("SECRET_KEY_BASE"))[0]
         HashWithIndifferentAccess.new body
       rescue
         nil
