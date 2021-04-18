55a4a1d96abcedab84e651e94b7e7a70af702d14
navaneeth <navaneethpk@outlook.com>
2021-04-18 22:29:17 +0530

    Toggle time picker for datepicker component

--- frontend/src/Editor/Components/Datepicker.jsx
+++ frontend/src/Editor/Components/Datepicker.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Datetime from 'react-datetime';
 import "react-datetime/css/react-datetime.css";
-
+import { resolve_references } from '@/_helpers/utils';
 
 export const Datepicker = function Datepicker({ id, width, height, component, onComponentClick, currentState, onComponentOptionChanged }) {
 
     console.log('currentState', currentState);
 
     const formatProp = component.definition.properties.format;
+    const enableTimeProp = component.definition.properties.enableTime;
+
+    const enabletime = resolve_references(enableTimeProp.value, currentState, false);
 
     function onDateChange(event) {
         onComponentOptionChanged(component, 'value', event.format(formatProp.value))
@@ -17,6 +20,7 @@ export const Datepicker = function Datepicker({ id, width, height, component, on
         <div style={{width, height}} onClick={() => onComponentClick(id, component) }>
             <Datetime 
                 onChange={onDateChange}
+                timeFormat={enabletime}
             />
         </div>
 );
