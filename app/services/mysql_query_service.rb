cef92cf5a1c3493b863532253eefccb36c8d673a
navaneeth <navaneethpk@outlook.com>
2021-04-20 10:26:17 +0530

    Store connections in global vars for pg, redis & mysql

--- app/services/mysql_query_service.rb
+++ app/services/mysql_query_service.rb
@@ -11,17 +11,23 @@ class MysqlQueryService
     end
 
     def process
-        client = Mysql2::Client.new(
-            host: source_options["host"], 
-            username: source_options["username"],
-            password:source_options["password"],
-            port: source_options["port"],
-            database: source_options["database"], 
-        )
+        if $connections.include? data_source.id
+            connection = $connections[data_source.id][:connection]
+        else
+            connection = client = Mysql2::Client.new(
+                host: source_options["host"], 
+                username: source_options["username"],
+                password:source_options["password"],
+                port: source_options["port"],
+                database: source_options["database"], 
+            )
+
+            $connections[data_source.id] = { connection: connection }
+        end
 
         query_text = options["query"]
 
-        results = client.query(query_text)
+        results = connection.query(query_text)
 
         { status: 'success', data: results.to_a }
     end
