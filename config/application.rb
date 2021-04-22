53b72a6dac69d7e6170155bdce29412c1fbe59f5
navaneeth <navaneethpk@outlook.com>
2021-04-22 09:42:01 +0530

    Setup dotenv

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module Stackegg
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
