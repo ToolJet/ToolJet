4434fd7edb8a705ccc30abe87391e99817ab13a0
navaneeth <navaneethpk@outlook.com>
2021-04-23 14:59:59 +0530

    BugFix: Dotenv should not be initialized for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+Dotenv::Railtie.load
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
