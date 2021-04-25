e5ef227ff219b9af88c9044b157dd0cf298bf713
navaneeth <navaneethpk@outlook.com>
2021-04-25 20:07:16 +0530

    Display component variables as json, policy fixes

---
+++ app/policies/organization_user_policy.rb
@@ -0,0 +1,17 @@
+class OrganizationUserPolicy < ApplicationPolicy
+    attr_reader :user, :organization_user
+
+    def initialize(user, organization_user)
+        @user = user
+        @organization_user = organization_user
+    end
+
+
+    def create?
+        user.org_admin?
+    end
+
+    def change_role?
+        (user.organization_id === organization_user.organization_id) && user.org_admin?
+    end
+end
