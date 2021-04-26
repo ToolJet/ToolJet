abd24dd356648950889599856f4fbac826bfb1df
navaneeth <navaneethpk@outlook.com>
2021-04-26 13:21:04 +0530

    Show/hide editor panels

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -42,7 +42,7 @@ class App extends React.Component {
                     }
 
                     <ToastContainer/>
-                    <ReactTooltip type="dark" effect="solid"/>
+                    <ReactTooltip type="dark" effect="solid" delayShow={1500} />
                     
                     <PrivateRoute exact path="/" component={HomePage} />
                     <Route path="/login" component={LoginPage} />
