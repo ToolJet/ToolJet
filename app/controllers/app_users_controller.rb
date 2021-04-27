def707f0e18f64c1af27089973c05faa12b18458
navaneeth <navaneethpk@outlook.com>
2021-04-27 12:36:45 +0530

    Feature: App-level users & permissions

---
+++ app/controllers/app_users_controller.rb
@@ -0,0 +1,24 @@
+class AppUsersController < ApplicationController
+
+    def create
+
+        org_user_id = params[:org_user_id]
+        app_id = params[:app_id]
+        role = params[:role]
+
+        org_user = OrganizationUser.find org_user_id
+        app = App.find app_id
+
+        app_user = AppUser.new(
+            role: role, 
+            app: app,
+            user_id: org_user.user_id,
+        )
+
+        if app_user.save
+            render json: { success: true }
+        else
+            render json: { message: 'Could not create user' }, status: 500
+        end
+    end
+end
--- app/models/app_user.rb
+++ app/models/app_user.rb
@@ -1,4 +1,5 @@
 class AppUser < ApplicationRecord
   belongs_to :app
   belongs_to :user
+  validates_uniqueness_of :app_id, :scope => [:user_id]
 end
