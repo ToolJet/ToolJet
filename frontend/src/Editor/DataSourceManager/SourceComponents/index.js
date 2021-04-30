b7ff6a0eac45c3fa162554f4de70a499b4d7a87c
navaneeth <navaneethpk@outlook.com>
2021-04-30 14:18:29 +0530

    Refactor & optimise data source manager

---
+++ frontend/src/Editor/DataSourceManager/SourceComponents/index.js
@@ -0,0 +1,19 @@
+import { Elasticsearch } from './Elasticsearch';
+import { Redis } from './Redis';
+import { Postgresql } from './Postgresql';
+import { Mysql } from './Mysql';
+import { Stripe } from './Stripe';
+import { Firestore } from './Firestore';
+import { RestApi } from './RestApi';
+import { Googlesheets } from './Googlesheets';
+
+export const SourceComponents = {
+    Elasticsearch,
+    Redis,
+    Postgresql,
+    Mysql,
+    Stripe,
+    Firestore,
+    RestApi,
+    Googlesheets
+};
