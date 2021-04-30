e1c9d6cbffde657e4e10dd88aab2ec8d3ff1893e
navaneeth <navaneethpk@outlook.com>
2021-04-30 11:01:47 +0530

    Feature: Connection testing for MySQL, ES & Firestore

--- app/services/mysql_query_service.rb
+++ app/services/mysql_query_service.rb
@@ -8,12 +8,22 @@ class MysqlQueryService
     @source_options = source_options
     @current_user = current_user
   end
+  
+  def self.connection options
+    connection = Mysql2::Client.new(
+        database: options.dig('database', 'value'),
+        user: options.dig('username', 'value'),
+        password: options.dig('password', 'value'),
+        host: options.dig('host', 'value'),
+        port: options.dig('port', 'value'),
+    )
+  end
 
   def process
     if $connections.include? data_source.id
       connection = $connections[data_source.id][:connection]
     else
-      connection = client = Mysql2::Client.new(
+      connection = Mysql2::Client.new(
         host: source_options['host'],
         username: source_options['username'],
         password: source_options['password'],
