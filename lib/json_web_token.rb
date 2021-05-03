ccbb848ab786999a4d7316d6414a27b9cdf3d6c0
navaneeth <navaneethpk@outlook.com>
2021-05-03 18:21:10 +0530

    Change JWT expiry to 30 days

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   class << self
-    def encode(payload, exp = 24.hours.from_now)
+    def encode(payload, exp = 30.days.from_now)
       payload[:exp] = exp.to_i
       JWT.encode(payload, ENV.fetch('SECRET_KEY_BASE'))
     end
