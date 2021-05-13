919c1ce87521ba347281fc74d5ff6e14548a9c64
navaneeth <navaneethpk@outlook.com>
2021-05-13 10:29:37 +0530

    [Google sheets] Refresh access token if expired

--- app/services/google_oauth_service.rb
+++ app/services/google_oauth_service.rb
@@ -31,4 +31,30 @@ class GoogleOauthService
 
     [['access_token', access_token], ['refresh_token', refresh_token]]
   end
+
+  def self.refresh_access_token(refresh_token, data_source)
+    access_token_url = 'https://oauth2.googleapis.com/token'
+    client_id = ENV.fetch('GOOGLE_CLIENT_ID')
+    client_secret = ENV.fetch('GOOGLE_CLIENT_SECRET')
+    grant_type = 'refresh_token'
+
+    response = HTTParty.post(access_token_url,
+                             body: { refresh_token: refresh_token,
+                                     client_id: client_id,
+                                     client_secret: client_secret,
+                                     grant_type: grant_type,
+                                     redirect_uri: "#{ENV.fetch('TOOLJET_HOST')}/oauth2/authorize"
+                                    }.to_json,
+                             headers: { 'Content-Type' => 'application/json' })
+
+    result = JSON.parse(response.body)
+
+    access_token = result['access_token']
+
+    credential_id = data_source.options["access_token"]["credential_id"]
+    credential = Credential.find(credential_id)
+    credential.update(value: access_token)
+
+    access_token
+  end
 end
