203a823a5f076ecd82e619f4d10d5d33573cb542
navaneeth <navaneethpk@outlook.com>
2021-05-14 12:39:38 +0530

    Feature: Purge cached connections on data source update

---
+++ app/services/concerns/datasource_utils.rb
@@ -0,0 +1,22 @@
+module DatasourceUtils
+  extend ActiveSupport::Concern
+
+  def get_cached_connection(data_source)
+
+    connection = nil
+    if $connections.include? data_source.id
+      data = $connections[data_source.id]
+      if data[:updated_at] === data_source.updated_at
+        connection = $connections[data_source.id][:connection]
+      end
+    end
+
+    connection            
+  end
+
+  def cache_connection(data_source, connection)
+    $connections[data_source.id] = { connection: connection, updated_at: data_source.updated_at }
+  end
+
+end
+  
