2c707231282623a6ba0fa347b945a768af7e6a39
navaneeth <navaneethpk@outlook.com>
2021-05-17 14:49:26 +0530

    Minor UI bug fixes

--- frontend/src/Editor/ConfigHandle.jsx
+++ frontend/src/Editor/ConfigHandle.jsx
@@ -7,9 +7,9 @@ export const ConfigHandle = function ConfigHandle({
     dragRef
 }) {
 
-    return <div className="config-handle" ref={dragRef}>
-         <span onClick={(e) => { e.preventDefault(); configHandleClicked(id, component) }} className="badge badge bg-azure-lt" role="button">
-            <img src="https://www.svgrepo.com/show/83981/menu.svg" width="8" height="8" style={{marginRight: '5px'}}/>
+return <div className="config-handle" ref={dragRef}>
+         <span  style={{cursor: 'move'}} onClick={(e) => { e.preventDefault(); configHandleClicked(id, component) }} className="badge badge bg-azure-lt" role="button">
+            <img style={{cursor: 'pointer'}} src="https://www.svgrepo.com/show/83981/menu.svg" width="8" height="8" style={{marginRight: '5px'}}/>
             {component.name}
          </span>
     </div>
