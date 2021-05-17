89f452387606dc2e1242acb1bf20b7493925e1e3
navaneeth <navaneethpk@outlook.com>
2021-05-17 09:42:52 +0530

    Avoiding unwanted resize initiation

--- frontend/src/Editor/ConfigHandle.jsx
+++ frontend/src/Editor/ConfigHandle.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 export const ConfigHandle = function ConfigHandle({
     id,
     component,
-    configHandleClicked
+    configHandleClicked,
+    dragRef
 }) {
 
-    return <div className="config-handle">
+    return <div className="config-handle" ref={dragRef}>
          <span onClick={(e) => { e.preventDefault(); configHandleClicked(id, component) }} className="badge badge bg-azure-lt" role="button">
-            <img src="https://www.svgrepo.com/show/83981/menu.svg" width="8" height="8" style={{marginRight: '5px'}} />
+            <img src="https://www.svgrepo.com/show/83981/menu.svg" width="8" height="8" style={{marginRight: '5px'}}/>
             {component.name}
          </span>
     </div>
