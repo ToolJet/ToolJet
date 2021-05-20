c40dd34b32ea8d7650c3ea4b2d41ca832dc665f3
navaneeth <navaneethpk@outlook.com>
2021-05-20 10:21:36 +0530

    Changed assets directory to work with firebase

--- frontend/src/_components/Header.jsx
+++ frontend/src/_components/Header.jsx
@@ -24,7 +24,7 @@ export const Header = function Header({
       </button>
       <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
         <Link to={'/'}>
-          <img src="/images/logo.svg" width="110" height="32" className="navbar-brand-image" />
+          <img src="/assets/images/logo.svg" width="110" height="32" className="navbar-brand-image" />
         </Link>
       </h1>
       <ul className="navbar-nav">
