6494752b19b503d62a697d9998f0a3aec3cabc76
navaneeth <navaneethpk@outlook.com>
2021-05-25 18:03:12 +0530

    Fixd broken image on login page

--- frontend/src/LoginPage/LoginPage.jsx
+++ frontend/src/LoginPage/LoginPage.jsx
@@ -49,7 +49,7 @@ class LoginPage extends React.Component {
         <div className="container-tight py-2">
           <div className="text-center mb-4">
             <a href=".">
-              <img src="/images/logo-text.svg" height="30" alt="" />
+              <img src="/assets/images/logo-text.svg" height="30" alt="" />
             </a>
           </div>
           <form className="card card-md" action="." method="get" autoComplete="off">
