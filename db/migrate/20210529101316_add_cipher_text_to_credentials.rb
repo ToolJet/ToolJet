370dfe99b2c5ff6f429f455f92f610e2298401ce
navaneeth <navaneethpk@outlook.com>
2021-05-29 20:45:41 +0530

    Changes for lockbox

--- db/migrate/20210529101316_add_cipher_text_to_credentials.rb
+++ db/migrate/20210529101316_add_cipher_text_to_credentials.rb
@@ -1,5 +1,15 @@
 class AddCipherTextToCredentials < ActiveRecord::Migration[6.1]
   def change
     add_column :credentials, :value_ciphertext, :text
+
+    Credential.all.each do |credential|
+      begin
+
+        if credential.value.is_a? String
+          credential.update(value: credential.value.as_json)
+        end
+      rescue 
+      end  
+    end
   end
 end
--- db/migrate/20210529140113_add_cipher_text_to_data_source_user_oauth2.rb
+++ db/migrate/20210529140113_add_cipher_text_to_data_source_user_oauth2.rb
@@ -1,5 +1,14 @@
 class AddCipherTextToDataSourceUserOauth2 < ActiveRecord::Migration[6.1]
   def change
     add_column :data_source_user_oauth2s, :options_ciphertext, :text
+
+    DataSourceUserOauth2.all.each do |credential|
+      begin
+        if credential.options.is_a? String
+          credential.update(options: credential.options)
+        end
+      rescue 
+      end  
+    end
   end
 end
