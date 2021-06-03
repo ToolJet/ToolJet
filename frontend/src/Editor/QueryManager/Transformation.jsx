fedef087e6bcb9e0205f44961bf793fd27eb7548
navaneeth <navaneethpk@outlook.com>
2021-06-03 11:29:13 +0530

    Use codehinter for transformations

--- frontend/src/Editor/QueryManager/Transformation.jsx
+++ frontend/src/Editor/QueryManager/Transformation.jsx
@@ -6,6 +6,7 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/hint/show-hint';
 import 'codemirror/addon/search/match-highlighter';
 import 'codemirror/addon/hint/show-hint.css';
+import { CodeHinter } from '../CodeBuilder/CodeHinter';
 
 export const Transformation = ({ changeOption, options, currentState }) => {
   const defaultValue = options.transformation
@@ -13,11 +14,11 @@ export const Transformation = ({ changeOption, options, currentState }) => {
 // return value will be set as data and the original data will be available as rawData
 return data.filter(row => row.amount > 1000);`;
 
-  // const [value, setValue] = useState(defaultValue);
+  const [value, setValue] = useState(defaultValue);
 
-  // useEffect(() => {
-  //   setValue(defaultValue);
-  // }, [defaultValue]);
+  useEffect(() => {
+    setValue(defaultValue);
+  }, [defaultValue]);
 
   function codeChanged(value) {
     changeOption('transformation', value);
@@ -43,20 +44,15 @@ return data.filter(row => row.amount > 1000);`;
       )}
       <br></br>
       {options.enableTransformation && (
-        <div>
-          <CodeMirror
-            height="220px"
-            fontSize="1"
-            onChange={(editor) => handleChange(editor, codeChanged, suggestions)}
-            onBeforeChange={(editor, change) => onBeforeChange(editor, change)}
-            value={defaultValue}
-            options={{
-              theme: 'base16-light',
-              mode: 'javascript',
-              lineWrapping: true,
-              scrollbarStyle: null,
-              highlightSelectionMatches: true,
-            }}
+        <div style={{height: '240px'}}>
+          <CodeHinter
+              currentState={currentState}
+              initialValue={value}
+              mode="javascript"
+              theme="base16-light"
+              lineNumbers={true}
+              className="query-hinter"
+              onChange={(value) => changeOption('transformation', value)}
           />
         </div>
 
