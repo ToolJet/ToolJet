6e081e977d3c19a1d52ebfeb93052b0903a1099c
navaneeth <navaneethpk@outlook.com>
2021-06-04 00:23:51 +0530

    Remove menu button from viewer

--- frontend/src/Editor/Viewer.jsx
+++ frontend/src/Editor/Viewer.jsx
@@ -118,9 +118,6 @@ class Viewer extends React.Component {
           <div className="header">
             <header className="navbar navbar-expand-md navbar-light d-print-none">
               <div className="container-xl header-container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
                 <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                   <a href="/">
                     <img src="/assets/images/logo.svg" width="110" height="32" className="navbar-brand-image" />
