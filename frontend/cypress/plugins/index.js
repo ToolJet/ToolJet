4931063afa9cee17dd2894018cadd683538988b1
navaneeth <navaneethpk@outlook.com>
2021-06-05 09:25:28 +0530

    Configured cypress to use webpack-dev-server

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -1,7 +1,16 @@
+const webpackPreprocessor = require('@cypress/webpack-preprocessor')
+
 module.exports = (on, config) => {
-    if (config.testingType === 'component') {
-      require('@cypress/react/plugins/react-scripts')(on, config)
-    }
-  
-    return config
+  if (config.testingType === 'component') {
+    const { startDevServer } = require('@cypress/webpack-dev-server')
+
+    // Your project's Webpack configuration
+    const webpackConfig = require('../../webpack.config.js')
+
+    on('dev-server:start', (options) =>
+      startDevServer({ options, webpackConfig })
+    )
+  }
+
+  return config
 }
