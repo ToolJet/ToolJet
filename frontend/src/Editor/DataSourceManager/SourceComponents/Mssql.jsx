134e2f51ca3919fcefd741389104704007446994
Akshay <akshaysasidharan93@gmail.com>
2021-06-14 07:33:13 +0530

    Feature: SQL Server integration  🎉  (#244)
    * MS SQL server integration
    
    * enable mssql service to test, create and process connection
    
    * return 400 when query fails
    
    * [Docker] Install freetds on buster
    
    Co-authored-by: navaneeth <navaneethpk@outlook.com>

---
+++ frontend/src/Editor/DataSourceManager/SourceComponents/Mssql.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export const Mssql = ({
+  optionchanged, options
+}) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-9">
+          <label className="form-label">Host</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => optionchanged('host', e.target.value)}
+            value={options.host.value}
+          />
+        </div>
+        <div className="col-md-3">
+          <label className="form-label">Port</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => optionchanged('port', e.target.value)}
+            value={options.port.value}
+          />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-md-4">
+          <label className="form-label">Database Name</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => optionchanged('database', e.target.value)}
+            value={options.database.value}
+          />
+        </div>
+        <div className="col-md-4">
+          <label className="form-label">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => optionchanged('username', e.target.value)}
+            value={options.username.value}
+          />
+        </div>
+        <div className="col-md-4">
+          <label className="form-label">
+            Password
+            <small className="text-green mx-2">
+              <img className="mx-2 encrypted-icon encrypted-icon" src="/assets/images/icons/padlock.svg" width="12" height="12" />
+              <span className="pt-2">Encrypted</span> 
+            </small>
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={(e) => optionchanged('password', e.target.value)}
+            value={options.password.value}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
