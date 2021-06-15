5f53f6dcd89cb3dfc9f572c30dc3d55b7291640e
Prasad Walvekar <prasadwalvekar@users.noreply.github.com>
2021-06-15 23:41:48 +0530

    Reset selected component state to null when a component is deleted (#258)

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -230,6 +230,16 @@ class Editor extends React.Component {
     this.computeComponentState(newDefinition.components);
   };
 
+  handleInspectorView = (component) => {
+    if (this.state.selectedComponent.hasOwnProperty('component')) {
+      const { id: selectedComponentId } = this.state.selectedComponent;
+      if (selectedComponentId === component.id) {
+        this.setState({selectedComponent: null})
+        this.switchSidebarTab(2);
+      }
+    }
+  }
+
   removeComponent = (component) => {
     let newDefinition = this.state.appDefinition;
 
@@ -241,7 +251,7 @@ class Editor extends React.Component {
 
     delete newDefinition.components[component.id];
     this.appDefinitionChanged(newDefinition);
-    this.switchSidebarTab(2);
+    this.handleInspectorView(component);
   };
 
   componentDefinitionChanged = (newDefinition) => {
