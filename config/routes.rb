f1f0b71eadf89b9bdc833eefa3c97c1cf92c319d
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-06-21 00:54:36 +0530

    remove unrelated changes from lint

--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,33 @@ Rails.application.routes.draw do
   resources :apps, only: %i[index create update] do
     resources :versions, only: %i[index create update]
 
-    get "/users", to: "apps#users"
+    get '/users', to: 'apps#users'
   end
   resources :apps, only: :show, param: :slug
 
   resources :data_sources, only: %i[create index update] do
-    post "/authorize_oauth2", to: "data_sources#authorize_oauth2"
+    post '/authorize_oauth2', to: 'data_sources#authorize_oauth2'
     collection do
-      post "/test_connection", to: "data_sources#test_connection"
-      post "/fetch_oauth2_base_url", to: "data_sources#fetch_oauth2_base_url"
+      post '/test_connection', to: 'data_sources#test_connection'
+      post '/fetch_oauth2_base_url', to: 'data_sources#fetch_oauth2_base_url'
     end
   end
 
   resources :data_queries, only: %i[create index update] do
-    post "/run", to: "data_queries#run"
+    post '/run', to: 'data_queries#run'
 
     collection do
-      post "/preview", to: "data_queries#preview"
+      post '/preview', to: 'data_queries#preview'
     end
   end
 
   resources :organizations, only: [] do
-    get "/users", to: "organizations#users"
+    get '/users', to: 'organizations#users'
   end
 
   resources :organization_users, only: [:create] do
-    post "/archive", to: "organization_users#archive"
-    post "/change_role", to: "organization_users#change_role"
+    post '/archive', to: 'organization_users#archive'
+    post '/change_role', to: 'organization_users#change_role'
   end
 
   resources :app_users, only: [:create]
@@ -37,21 +37,20 @@ Rails.application.routes.draw do
 
   resources :user do
     collection do
-      post "/set_password_from_token", to: "users#set_password_from_token"
+      post '/set_password_from_token', to: 'users#set_password_from_token'
     end
   end
 
-  post "authenticate", to: "authentication#authenticate"
-  post "signup", to: "authentication#signup"
+  post 'authenticate', to: 'authentication#authenticate'
+  post 'signup', to: 'authentication#signup'
 
   resources :metadata, only: [:index] do
     collection do
-      post "/skip_version", to: "metadata#skip_version"
-      post "/skip_onboarding", to: "metadata#skip_onboarding"
-      post "/finish_installation", to: "metadata#finish_installation"
+      post '/skip_version', to: 'metadata#skip_version'
+      post '/skip_onboarding', to: 'metadata#skip_onboarding'
+      post '/finish_installation', to: 'metadata#finish_installation'
     end
   end
 
-  get "/health", to: "probe#health_check"
-
+  get '/health', to: 'probe#health_check'
 end
