7b63cf441c677a113820314aba7ca460c8b9eeff
Prasad Walvekar <prasadwalvekar@users.noreply.github.com>
2021-06-22 23:22:37 +0530

    [Tests] Add cypress tests for login page (#291)

---
+++ cypress/support/commands.js
@@ -0,0 +1,10 @@
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('/login');
+  cy.get('[data-testid="emailField"]').type(email);
+  cy.get('[data-testid="passwordField"]').type(password);
+  cy.get('[data-testid="loginButton"').click();
+})
+
+Cypress.Commands.add('checkToastMessage', (toastId, message) => {
+  cy.get(`[id=${toastId}]`).should('contain', message);
+});
