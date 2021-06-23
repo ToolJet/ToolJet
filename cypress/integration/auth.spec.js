38a74c73a50ed3a0b898bd786480e4b42f95ae4c
Prasad Walvekar <prasadwalvekar@gmail.com>
2021-06-23 20:06:53 +0530

    Add e2e tests for forgot password flow

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -12,14 +12,14 @@ describe('User login', () => {
 
   it('should display invalid email or password error',  () => {
     cy.login('fake_email', 'abcdefg');
-    cy.checkToastMessage('toast-login-auth-error', 'Invalid email or password')
+    cy.checkToastMessage('toast-login-auth-error', 'Invalid email or password');
   });
 
   it('should take user to the forgot password page', () => {
     cy.visit('/forgot-password');
     cy.get('.card-title')
       .should('have.text', 'Forgot Password');
-  })
+  });
 
   it('should take user to the signup page', () => {
     cy.visit('/signup');
@@ -34,4 +34,22 @@ describe('User login', () => {
     cy.get('.page-title')
       .should('have.text', 'All applications');
   })
+
+  it('should display error if email is not found for "Forgot password"',  () => {
+    cy.visit('/forgot-password');
+    cy.get('[data-testid="emailField"]').type('abc@def.com');
+    cy.get('[data-testid="submitButton"').click();
+    cy.checkToastMessage('toast-forgot-password-email-error', 'Email address is not associated with a ToolJet cloud account.');
+  });
+
+  it('should send reset password confirmation code to email', () => {
+    cy.intercept('POST', '/password/forgot').as('forgotPasswordConfirmationCode');
+
+    cy.visit('/forgot-password');
+    cy.get('[data-testid="emailField"]').type('prasad@tooljet.io');
+    cy.get('[data-testid="submitButton"').click();
+
+    cy.wait('@forgotPasswordConfirmationCode').its('response.statusCode').should('eq', 200);
+    cy.checkToastMessage('toast-forgot-password-confirmation-code', 'We\'ve sent the confirmation code to your email address');
+  });
 })
