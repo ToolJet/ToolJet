8e7095f9feee09f56a23266637b61196e91f769a
navaneeth <navaneethpk@outlook.com>
2021-07-12 16:06:12 +0530

    Encryption service

---
+++ server/src/services/encryption.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+const crypto = require('crypto')
+
+@Injectable()
+export class EncryptionService {
+
+  async encryptColumnValue(table: string, column: string, text: string): Promise<string> {
+    const derivedKey = this.computeAttributeKey(table, column);
+    return this.encrypt(text, derivedKey);
+  }
+
+  async decryptColumnValue(table: string, column: string, cipherText: string): Promise<string> {
+    const derivedKey = this.computeAttributeKey(table, column);
+    return this.decrypt(cipherText, derivedKey);
+  }
+
+  encrypt(text: string, derivedKey: string): string {
+
+    const nonce = crypto.randomBytes(12);
+    const key = Buffer.from(derivedKey, 'hex')
+    const cipher = crypto.createCipheriv('aes-256-gcm', key, nonce);
+    const encrypted = Buffer.concat([cipher.update(text, 'utf8'), cipher.final()]);
+    const tag = cipher.getAuthTag();
+    const encryptedString = Buffer.concat([nonce, encrypted, tag]).toString('base64');
+
+    return encryptedString;
+  }
+
+  decrypt(cipherText: string, derivedKey: string): string {
+    const key = Buffer.from(derivedKey, 'hex');
+    let ciphertext = Buffer.from(cipherText, 'base64') ;
+
+    let nonce = ciphertext.slice(0, 12);
+    let auth_tag = ciphertext.slice(-16);
+    ciphertext = ciphertext.slice(12, -16);
+
+    let aesgcm = crypto.createDecipheriv('aes-256-gcm', key, nonce);
+    aesgcm.setAuthTag(auth_tag);
+    let plainText = aesgcm.update(ciphertext) + aesgcm.final();
+
+    return plainText;
+  }
+
+  // Generating separated keys for every column using the method used by Lockbox gem - https://github.com/ankane/lockbox
+  // This was needed when we migrated from Ruby on Rails to NestJS
+  computeAttributeKey(table: string, column: string): string {
+    const key = Buffer.from(process.env.LOCKBOX_MASTER_KEY, 'hex');
+    const salt = Buffer.alloc(32, '´', 'ascii');
+    const info = Buffer.concat([salt, Buffer.from(`${column}_ciphertext`)]);
+
+    const derivedKey = crypto.hkdfSync('sha384', key, table, info, 32);
+    const finalDerivedKey = Buffer.from(derivedKey).toString('hex');
+
+    return finalDerivedKey;
+  }
+
+}
