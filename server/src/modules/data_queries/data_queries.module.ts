52253f26adf0a0598167d7e536e95ddf28bfe11b
navaneeth <navaneethpk@outlook.com>
2021-07-16 20:33:20 +0530

    Feature: update data source connection options

--- server/src/modules/data_queries/data_queries.module.ts
+++ server/src/modules/data_queries/data_queries.module.ts
@@ -6,10 +6,12 @@ import { DataQueriesService } from '../../../src/services/data_queries.service';
 import { CredentialsService } from '../../../src/services/credentials.service';
 import { EncryptionService } from '../../../src/services/encryption.service';
 import { Credential } from '../../../src/entities/credential.entity';
+import { DataSourcesService } from '../../../src/services/data_sources.service';
+import { DataSource } from '../../../src/entities/data_source.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DataQuery, Credential])],
-  providers: [DataQueriesService, CredentialsService, EncryptionService],
+  imports: [TypeOrmModule.forFeature([DataQuery, Credential, DataSource])],
+  providers: [DataQueriesService, CredentialsService, EncryptionService, DataSourcesService],
   controllers: [DataQueriesController],
 })
 export class DataQueriesModule {}
