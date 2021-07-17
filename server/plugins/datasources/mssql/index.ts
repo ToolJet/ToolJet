34bcd2de79b1eeb02e5d307fb0b897e287821b39
navaneeth <navaneethpk@outlook.com>
2021-07-17 12:41:03 +0530

    MS SQL server as datasource

--- server/plugins/datasources/mssql/index.ts
+++ server/plugins/datasources/mssql/index.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { QueryResult } from 'src/modules/data_sources/query_result.type';
 import { QueryService } from 'src/modules/data_sources/query_service.interface';
-import { Knex, knex } from 'knex';
+import { Knex, knex } from 'knex'
+import { QueryError } from 'src/modules/data_sources/query.error';
 
 @Injectable()
-export default class MysqlQueryService implements QueryService {
+export default class MssqlQueryService implements QueryService {
 
   async run(sourceOptions: any, queryOptions: any, dataSourceId: string): Promise<QueryResult> {
 
     const config: Knex.Config = {
-      client: 'mysql',
+      client: 'mssql',
       connection: {
         host : sourceOptions.host,
         user : sourceOptions.username,
         password : sourceOptions.password,
         database : sourceOptions.database,
         port: sourceOptions.port,
+        options: {
+          encrypt: true
+        }
       }
     };
 
@@ -28,12 +32,12 @@ export default class MysqlQueryService implements QueryService {
     try {
       result = await knexInstance.raw(query);
     } catch (err) {
-      console.log(err);
+      throw new QueryError('Query could not be complated', err.message, {});
     }
 
     return {
       status: 'ok',
-      data: result[0]
+      data: result
     }
   }
 }
