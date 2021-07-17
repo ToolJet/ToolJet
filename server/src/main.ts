13e99fe24c9f0df298f874c79e2c642f6299b629
navaneeth <navaneethpk@outlook.com>
2021-07-17 23:28:55 +0530

    Setup dotnev for node server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config({ path: '../.env' })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
