a132f6addd9e0b4ecad8880a583e3a4507a0146a
navaneeth <navaneethpk@outlook.com>
2021-07-17 13:29:28 +0530

    Implement getConnection for query services

--- server/plugins/datasources/dynamodb/index.ts
+++ server/plugins/datasources/dynamodb/index.ts
@@ -9,35 +9,25 @@ export default class DynamodbQueryService implements QueryService {
 
   async run(sourceOptions: any, queryOptions: any, dataSourceId: string): Promise<QueryResult> {
 
-    const credentials = new AWS.Credentials(sourceOptions['access_key'], sourceOptions['secret_key']);
-    const region = sourceOptions['region'];
-
-    let client = {};
-
-    let result = { };
     const operation = queryOptions.operation;
+    const client = await this.getConnection(sourceOptions, { operation });
+    let result = { };
 
     try {
-      
       switch (operation) {
         case 'list_tables':
-          client = new AWS.DynamoDB({ region, credentials });
           result = await listTables(client);
           break;  
         case 'get_item':
-          client = new AWS.DynamoDB.DocumentClient({ region, credentials });
           result = await getItem(client, queryOptions.table, JSON.parse(queryOptions.key));
           break;
         case 'delete_item':
-          client = new AWS.DynamoDB.DocumentClient({ region, credentials });
           result = await deleteItem(client, queryOptions.table, JSON.parse(queryOptions.key));
           break;  
         case 'query_table':
-          client = new AWS.DynamoDB.DocumentClient({ region, credentials });
           result = await queryTable(client, JSON.parse(queryOptions['query_condition']));
           break; 
         case 'scan_table':
-          client = new AWS.DynamoDB.DocumentClient({ region, credentials });
           result = await scanTable(client, JSON.parse(queryOptions['scan_condition']));
           break;   
       }
@@ -48,6 +38,17 @@ export default class DynamodbQueryService implements QueryService {
     return {
       status: 'ok',
       data: result
+    }
+  }
+
+  async getConnection(sourceOptions: any, options?: object): Promise<any> {
+    const credentials = new AWS.Credentials(sourceOptions['access_key'], sourceOptions['secret_key']);
+    const region = sourceOptions['region'];
+
+    if(options['operation'] == 'list_tables') {
+      return new AWS.DynamoDB({ region, credentials });
+    } else {
+      return new AWS.DynamoDB.DocumentClient({ region, credentials })
     }
   }
 }
