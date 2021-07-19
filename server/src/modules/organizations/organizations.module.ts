8ac4508ed8812f996a66bd758e221b2d76acbc41
navaneeth <navaneethpk@outlook.com>
2021-07-19 13:30:42 +0530

    Endpoint to fetch organization users

--- server/src/modules/organizations/organizations.module.ts
+++ server/src/modules/organizations/organizations.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationUser } from '../../entities/organization_user.entity';
 import { Organization } from '../../entities/organization.entity';
 import { OrganizationsService } from 'src/services/organizations.service';
+import { OrganizationsController } from 'src/controllers/organizations.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Organization, OrganizationUser])],
   providers: [OrganizationsService],
-  controllers: [],
+  controllers: [OrganizationsController],
 })
 export class OrganizationsModule {}
