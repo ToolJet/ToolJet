1d9d1a833c4914e0d6887b801f54fef5ab65764f
navaneeth <navaneethpk@outlook.com>
2021-07-19 14:27:57 +0530

    Endpoint to archive org users

--- server/src/modules/organizations/organizations.module.ts
+++ server/src/modules/organizations/organizations.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationUser } from '../../entities/organization_user.entity';
 import { Organization } from '../../entities/organization.entity';
 import { OrganizationsService } from 'src/services/organizations.service';
+import { OrganizationUsersService } from 'src/services/organization_users.service';
 import { OrganizationsController } from 'src/controllers/organizations.controller';
+import { OrganizationUsersController } from 'src/controllers/organization_users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Organization, OrganizationUser])],
-  providers: [OrganizationsService],
-  controllers: [OrganizationsController],
+  providers: [OrganizationsService, OrganizationUsersService],
+  controllers: [OrganizationsController, OrganizationUsersController],
 })
 export class OrganizationsModule {}
