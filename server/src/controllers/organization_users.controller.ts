dd268a5cefa11ca6e1b66690d40035398df41064
navaneeth <navaneethpk@outlook.com>
2021-07-19 15:06:34 +0530

    Endpoint for creating new organization users

--- server/src/controllers/organization_users.controller.ts
+++ server/src/controllers/organization_users.controller.ts
@@ -9,11 +9,19 @@ export class OrganizationUsersController {
     private organizationUsersService: OrganizationUsersService
   ) { }
 
+  // Endpoint for inviting new organization users
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Request() req, @Param() params) {
+    const result = await this.organizationUsersService.inviteNewUser(req.user, req.body);
+    return decamelizeKeys({ users: result });
+  }
+
   @UseGuards(JwtAuthGuard)
   @Post(':id/archive')
-  async create(@Request() req, @Param() params) {
+  async archive(@Request() req, @Param() params) {
     const result = await this.organizationUsersService.archive(params.id);
-    return decamelizeKeys({ users: result });
+    return decamelizeKeys({ result });
   }
 
 }
