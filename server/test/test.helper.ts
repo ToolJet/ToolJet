3aac8f03fbc263c97cc96bf00d38680ea797bf0c
navaneeth <navaneethpk@outlook.com>
2021-07-22 12:36:12 +0530

    Helper for clearing DB before each test

--- server/test/test.helper.ts
+++ server/test/test.helper.ts
@@ -1,9 +1,19 @@
 import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from 'src/modules/auth/constants';
+import { ConfigService } from '@nestjs/config';
+import { getConnection, Repository } from 'typeorm';
 
 export function authHeaderForUser(user: any) {
-  const jwtService = new JwtService({secret: jwtConstants.secret});
+  const configService = new ConfigService();
+  const jwtService = new JwtService({secret: configService.get<string>('SECRET_KEY_BASE')});
   const authPayload = { username: user.id, sub: user.email };
   const authToken = jwtService.sign(authPayload);
   return `Bearer ${authToken}`;
+}
+
+export async function clearDB() {
+  const entities = getConnection().entityMetadatas;
+  for (const entity of entities) {
+    const repository = await getConnection().getRepository(entity.name);
+    await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+  }
 }
