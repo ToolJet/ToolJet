8afdb4917ae15a422ace0c6c572acd5fada88832
navaneeth <navaneethpk@outlook.com>
2021-07-26 18:15:10 +0530

    Feature: Switch folders on dashboard

--- server/src/modules/apps/apps.module.ts
+++ server/src/modules/apps/apps.module.ts
@@ -13,10 +13,13 @@ import { OrganizationUser } from 'src/entities/organization_user.entity';
 import { UsersService } from '@services/users.service';
 import { User } from 'src/entities/user.entity';
 import { Organization } from 'src/entities/organization.entity';
+import { FoldersService } from '@services/folders.service';
+import { Folder } from 'src/entities/folder.entity';
+import { FolderApp } from 'src/entities/folder_app.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([App, AppVersion, AppUser, DataQuery, OrganizationUser, User, Organization]), CaslModule],
-  providers: [AppsService, AppUsersService, UsersService],
+  imports: [TypeOrmModule.forFeature([App, AppVersion, AppUser, DataQuery, Folder, FolderApp, OrganizationUser, User, Organization]), CaslModule],
+  providers: [AppsService, AppUsersService, UsersService, FoldersService],
   controllers: [AppsController, AppUsersController],
 })
 export class AppsModule {}
