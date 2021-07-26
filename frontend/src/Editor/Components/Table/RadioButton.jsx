5e77728a21dc34f0e6d894a5d299e9fbaa3eba51
Viraj Bahulkar <bbahulkar@gmail.com>
2021-07-26 17:06:38 +0530

    Add table radio buttons from bootstrap to tabler

--- frontend/src/Editor/Components/Table/RadioButton.jsx
+++ frontend/src/Editor/Components/Table/RadioButton.jsx
@@ -27,13 +27,14 @@ export const RadioButton = ({options, onChange, cellIndex,  columntypeValues, co
   return (
     <div>
       {options.map((option, i) => 
-        <div className="form-check form-check-inline custom-radio" key={option.value}>
-          {checkboxValue == option.value && <div class="checked-circle" ></div>}
-          <input className="form-check-input" type="radio" name={`inlineRadioOptions${cellIndex}`}  
-          value={option.value} 
-          onChange={(e) => addValue(e.target.value)}   />
-          <label className="form-check-label" >{option.name}</label>
-        </div>
+        <label class="form-check form-check-inline " key={option.value}>
+          <input class="form-check-input" name={`inlineRadioOptions${cellIndex}`}
+          value={option.value}  
+          onChange={(e) => addValue(e.target.value)}
+          type="radio" 
+          checked={checkboxValue == option.value ? "checked" : "" } />
+          <span class="form-check-label">{option.name}</span>
+        </label>
       )}
     </div>
   );
