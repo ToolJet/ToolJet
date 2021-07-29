3fac9e10a7ac1e6b8175b98330288ccd8495cd48
xrimpy <=>
2021-07-29 14:17:17 +0530

    Switch DarkMode Styles according to localStorage.getItem('darkMode')
    Resource for Styling https://snazzymaps.com/

--- frontend/src/Editor/Components/Map.jsx
+++ frontend/src/Editor/Components/Map.jsx
@@ -44,6 +44,88 @@ export const Map = function Map({
     height
   };
 
+  const darkModeStyles = [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+  ]
+
+
   function handleMapClick(e) {
     if(!canAddNewMarkers) { return }
 
@@ -60,14 +142,14 @@ export const Map = function Map({
   function handleBoundsChange() {
     const mapBounds = gmap.getBounds();
 
-    const bounds = { 
+    const bounds = {
       northEast: mapBounds.getNorthEast().toJSON(),
       southWest: mapBounds.getSouthWest().toJSON(),
     }
 
     const newCenter = gmap.center.toJSON();
     setMapCenter(newCenter);
-    
+
     onComponentOptionsChanged(component, [
       ['bounds', bounds],
       ['center', newCenter]
@@ -110,6 +192,7 @@ export const Map = function Map({
           mapContainerStyle={containerStyle}
           zoom={12}
           options={{
+            styles: localStorage.getItem('darkMode') === 'true' ? darkModeStyles : '',
             streetViewControl: false,
             mapTypeControl: false,
             draggable: true
@@ -130,7 +213,7 @@ export const Map = function Map({
                 />
             </Autocomplete>
           }
-          {Array.isArray(markers) && 
+          {Array.isArray(markers) &&
             <>
               {markers.map((marker, index) =>
                 <Marker
