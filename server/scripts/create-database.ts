bee3a0f22471a7087eeead40d4d6e75bcd8ba117
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-07-30 20:28:36 +0530

    validate and pick db config from env if .env is not found

--- server/scripts/create-database.ts
+++ server/scripts/create-database.ts
@@ -2,28 +2,53 @@ import * as path from 'path';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import { exec } from 'child_process';
+import * as Joi from 'joi';
 
-const createDatabase = (envPath: string): void => {
+function validateDatabaseConfig(): Joi.ValidationResult {
+  const envVarsSchema = Joi.object()
+    .keys({
+      PG_HOST: Joi.string().required(),
+      PG_PORT: Joi.number().positive().default(5432),
+      PG_PASS: Joi.string().default(''),
+      PG_USER: Joi.string().required(),
+      PG_DB: Joi.string().required(),
+    })
+    .unknown();
+
+  return envVarsSchema.validate(process.env);
+}
+
+function createDatabaseFromFile(envPath: string): void {
   const result = dotenv.config({ path: envPath });
 
   if (result.error) {
     throw result.error;
   }
 
-  exec('command -v createdb', (err, stdout, stderr) => {
+  createDatabase();
+}
+
+function createDatabase(): void {
+  const { value: envVars, error } = validateDatabaseConfig();
+
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+  }
+
+  exec('command -v createdb', (err, _stdout, _stderr) => {
     if (err) {
       console.error(err);
       return;
     }
 
     const createdb =
-      `PGPASSWORD=${process.env.PG_PASS || ''} createdb ` +
-      `-h ${process.env.PG_HOST || ''} ` +
-      `-p ${process.env.PG_PORT || 5432} ` +
-      `-U ${process.env.PG_USER || ''} ` +
+      `PGPASSWORD=${envVars.PG_PASS} createdb ` +
+      `-h ${envVars.PG_HOST} ` +
+      `-p ${envVars.PG_PORT} ` +
+      `-U ${envVars.PG_USER} ` +
       process.env.PG_DB;
 
-    exec(createdb, (err, stdout, stderr) => {
+    exec(createdb, (err, _stdout, _stderr) => {
       if (err) {
         console.error(err);
         return;
@@ -32,7 +57,7 @@ const createDatabase = (envPath: string): void => {
       console.log(`Created database ${process.env.PG_DB}`);
     });
   });
-};
+}
 
 const nodeEnvPath = path.resolve(
   process.cwd(),
@@ -42,11 +67,14 @@ const nodeEnvPath = path.resolve(
 const fallbackPath = path.resolve(process.cwd(), '../.env');
 
 if (fs.existsSync(nodeEnvPath)) {
-  createDatabase(nodeEnvPath);
+  createDatabaseFromFile(nodeEnvPath);
 } else if (fs.existsSync(fallbackPath)) {
-  createDatabase(fallbackPath);
+  createDatabaseFromFile(fallbackPath);
 } else {
-  console.error(
-    `${nodeEnvPath} file not found to create database`,
+  console.log(
+    `${nodeEnvPath} file not found to create database` +
+      'Inferring config from the environment',
   );
+
+  createDatabase();
 }
