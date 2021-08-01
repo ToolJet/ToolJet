08eab4be65b72960a95a63aa111beb84c70bf3aa
navaneeth <navaneethpk@outlook.com>
2021-08-01 15:01:29 +0530

    Set up helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 const fs = require('fs');
 
 declare var TOOLJET_VERSION;
@@ -12,6 +13,7 @@ async function bootstrap() {
 
   await app.setGlobalPrefix('api');
   await app.enableCors();
+  app.use(helmet());
   
   const port = parseInt(process.env.PORT) || 3000;
   
