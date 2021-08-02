56fa8b9dc97c71724c77dfd4472b3cf3f3b82759
Viraj Bahulkar <bbahulkar@gmail.com>
2021-08-02 20:05:21 +0530

    Fix array dataUpdates storing duplicate values (#436)

--- frontend/src/Editor/Components/Table/Table.jsx
+++ frontend/src/Editor/Components/Table/Table.jsx
@@ -139,7 +139,10 @@ export function Table({
 
     obj = _.set(rowData, key, value);
 
-    let newDataUpdates = [...dataUpdates, { ...obj }];
+    let newDataUpdates = {
+      ...dataUpdates, 
+      [index]: { ...obj }
+    };
 
     onComponentOptionsChanged(component, [
       ['dataUpdates', newDataUpdates],
