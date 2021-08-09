22f6638d4c475b697ca4da1947a3f575dea56b19
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-08-09 09:34:46 +0530

    revert git clone dev branch

--- deploy/ec2/setup_machine.sh
+++ deploy/ec2/setup_machine.sh
@@ -45,7 +45,7 @@ sudo cp /tmp/nest.service /lib/systemd/system/nest.service
 
 # Setup app directory
 mkdir -p ~/app
-git clone -b docker-deploy-setup https://github.com/ToolJet/ToolJet.git ~/app && cd ~/app
+git clone -b main https://github.com/ToolJet/ToolJet.git ~/app && cd ~/app
 
 mv /tmp/.env ~/app/.env
 mv /tmp/setup_app ~/app/setup_app
