f0e6b380bbee42e178f035ed1483cdbf6fdb2bfb
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-08-12 15:44:39 +0530

    Add end-point to change password
    This commit also adds a Guard to ensure that the user reenters
    existing valid password.

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, Patch, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
+import { PasswordRevalidateGuard } from 'src/modules/auth/password-revalidate.guard';
 import { UsersService } from 'src/services/users.service';
 
 @Controller('users')
@@ -17,12 +18,19 @@ export class UsersController {
   @UseGuards(JwtAuthGuard)
   @Patch('update')
   async update(@Request() req, @Body() body) {
-    const {firstName, lastName, password } = body
-    const result = await this.usersService.update(req.user.id, { firstName, lastName, password });
+    const {firstName, lastName } = body
+    await this.usersService.update(req.user.id, { firstName, lastName });
     await req.user.reload()
     return {
       first_name: req.user.firstName,
       last_name: req.user.lastName
     };
+  }
+
+  @UseGuards(JwtAuthGuard, PasswordRevalidateGuard)
+  @Patch('change_password')
+  async changePassword(@Request() req, @Body() body) {
+    const { newPassword } = body
+    return await this.usersService.update(req.user.id, { password: newPassword });
   }
 }
---
+++ server/src/modules/auth/password-revalidate.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable, CanActivate, ExecutionContext, Inject, forwardRef } from '@nestjs/common';
+import { UsersService } from '@services/users.service'
+import { Observable } from 'rxjs';
+const bcrypt = require('bcrypt')
+
+@Injectable()
+export class PasswordRevalidateGuard implements CanActivate {
+
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<boolean> {
+    const user = await this.usersService.findByEmail(email);
+
+    if(!user) return null;
+
+    const isVerified = await bcrypt.compare(password, user.password);
+
+    return isVerified
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateUser(request.user.email, request.body.currentPassword)
+  }
+}
