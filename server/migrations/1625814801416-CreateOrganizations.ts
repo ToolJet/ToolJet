3206433b73e6be90705b729325a422fd78787d31
vimoxshah <vmoksh.shah179@gmail.com>
2021-08-13 16:51:21 +0530

    [ADD] enable pgcrypto extension

--- server/migrations/1625814801416-CreateOrganizations.ts
+++ server/migrations/1625814801416-CreateOrganizations.ts
@@ -3,6 +3,9 @@ import { MigrationInterface, QueryRunner, Table, TableIndex } from "typeorm";
 export class CreateOrganizations1625814801416 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;')
+
     await queryRunner.createTable(new Table({
       name: "organizations",
       columns: [
