03990dbfd0ebbdcc56ffc1eaa8564fe5becacad5
gandharv <gandharvkumargarg@gmail.com>
2021-08-16 12:20:01 +0530

    new: add svg spring motion dark mode icons

--- frontend/src/_components/DarkModeToggle.jsx
+++ frontend/src/_components/DarkModeToggle.jsx
@@ -1,22 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
-export const DarkModeToggle = function DarkModeToggle({
-  darkMode, switchDarkMode
-}) {
+export const DarkModeToggle = function DarkModeToggle({ darkMode = false, switchDarkMode }) {
+  const toggleDarkMode = () => {
+    switchDarkMode(!darkMode);
+  };
 
-  const [darkModeEnabled, setMode] = useState(darkMode);
+  const properties = {
+    sun: {
+      r: 9,
+      transform: 'rotate(40deg)',
+      cx: 12,
+      cy: 4,
+      opacity: 0,
+    },
+    moon: {
+      r: 5,
+      transform: 'rotate(90deg)',
+      cx: 30,
+      cy: 0,
+      opacity: 1,
+    },
+    springConfig: { mass: 4, tension: 250, friction: 35 },
+  };
 
-  const icon = darkModeEnabled ? 'night.svg' : 'day.svg';
+  const { r, transform, cx, cy, opacity } = properties[darkMode ? 'moon' : 'sun'];
 
-  return <div className='dark-mode'>
-    <label className="form-check form-switch">
-      <img src={`/assets/images/icons/${icon}`} width="16" height="16" />
-      <input
-        className="form-check-input"
-        type="checkbox"
-        onClick={() => { switchDarkMode(!darkModeEnabled); setMode(!darkModeEnabled); }}
-        checked={darkModeEnabled}
-      />
-    </label>
-  </div>
-}
+  const svgContainerProps = useSpring({
+    transform,
+    config: properties.springConfig,
+  });
+  const centerCircleProps = useSpring({ r, config: properties.springConfig });
+  const maskedCircleProps = useSpring({
+    cx,
+    cy,
+    config: properties.springConfig,
+  });
+  const linesProps = useSpring({ opacity, config: properties.springConfig });
+
+  return (
+    <OverlayTrigger
+      placement="right"
+      delay={{ show: 250, hide: 400 }}
+      overlay={<Tooltip id="button-tooltip">{darkMode ? 'Activate light mode' : 'Activate dark mode'}</Tooltip>}
+    >
+      <animated.svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        color={darkMode ? '#fff' : '#000'}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke="currentColor"
+        onClick={toggleDarkMode}
+        style={{
+          cursor: 'pointer',
+          ...svgContainerProps,
+        }}
+      >
+        <mask id="myMask2">
+          <rect x="0" y="0" width="100%" height="100%" fill="white" />
+          <animated.circle style={maskedCircleProps} r="9" fill="black" />
+        </mask>
+
+        <animated.circle cx="12" cy="12" style={centerCircleProps} fill="black" mask="url(#myMask2)" />
+        <animated.g stroke="currentColor" style={linesProps}>
+          <line x1="12" y1="1" x2="12" y2="3" />
+          <line x1="12" y1="21" x2="12" y2="23" />
+          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+          <line x1="1" y1="12" x2="3" y2="12" />
+          <line x1="21" y1="12" x2="23" y2="12" />
+          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+        </animated.g>
+      </animated.svg>
+    </OverlayTrigger>
+  );
+};
