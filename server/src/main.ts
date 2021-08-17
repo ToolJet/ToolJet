19a0088478baa9cb8c76023890a364e8d5dc0d6d
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-08-17 13:12:48 +0530

    Add blob: to content security policy

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     helmet.contentSecurityPolicy({
       useDefaults: true,
       directives: {
-        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", "blob:"],
       },
     }),
   );
