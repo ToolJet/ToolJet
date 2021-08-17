5bbf55c1af0028a57014293c35de9035838a6ffb
vimox-shah-genea <vshah@getgenea.com>
2021-08-17 11:08:17 +0530

    [REM] additional line breaks

--- server/migrations/1625814801415-MaybeCreateExtension.ts
+++ server/migrations/1625814801415-MaybeCreateExtension.ts
@@ -3,9 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class  MaybeCreateExtension1625814801415 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;')
-   
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;')   
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
