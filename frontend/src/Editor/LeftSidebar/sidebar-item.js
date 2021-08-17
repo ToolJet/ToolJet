6c8ff91dbb6361832730130017665aa2d9676268
“Apoorv <tiwari.apoorv1316@gmail.com>
2021-08-17 17:18:20 +0530

    Fix the bug

--- frontend/src/Editor/LeftSidebar/sidebar-item.js
+++ frontend/src/Editor/LeftSidebar/sidebar-item.js
@@ -3,8 +3,10 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
 export const LeftSidebarItem = ({ tip = '', className, icon, text, onClick, ...rest }) => {
+
   return (
     <OverlayTrigger
+      trigger={['click','hover', 'focus']}
       placement="bottom"
       delay={{ show: 250, hide: 400 }}
       overlay={<Tooltip id="button-tooltip">
@@ -13,7 +15,7 @@ export const LeftSidebarItem = ({ tip = '', className, icon, text, onClick, ...r
     >
       <div {...rest} className={className} onClick={onClick && onClick}>
       {icon && <img className="svg-icon" src={`/assets/images/icons/editor/left-sidebar/${icon}.svg`} width="20" height="20" />}
-      {text && text} 
+      {text && text}
     </div>
     </OverlayTrigger>
   )
