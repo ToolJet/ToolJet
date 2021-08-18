627706193877c15c75b6531e61bfbe22a776b1ab
Apoorv Tiwari <tiwari.apoorv1316@gmail.com>
2021-08-18 22:16:24 +0530

    Hide tooltip when the left sidebar icon is clicked (#531)
    * Fix the bug
    
    * dealy tooltip and postion it to right
    
    Co-authored-by: “Apoorv <“tiwari.apoorv1316@gmail.com”>

--- frontend/src/Editor/LeftSidebar/sidebar-item.js
+++ frontend/src/Editor/LeftSidebar/sidebar-item.js
@@ -3,17 +3,19 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
 export const LeftSidebarItem = ({ tip = '', className, icon, text, onClick, ...rest }) => {
+
   return (
     <OverlayTrigger
-      placement="bottom"
-      delay={{ show: 250, hide: 400 }}
+      trigger={['click','hover', 'focus']}
+      placement="right"
+      delay={{ show: 800, hide: 100 }}
       overlay={<Tooltip id="button-tooltip">
         {tip}
       </Tooltip>}
     >
       <div {...rest} className={className} onClick={onClick && onClick}>
       {icon && <img className="svg-icon" src={`/assets/images/icons/editor/left-sidebar/${icon}.svg`} width="20" height="20" />}
-      {text && text} 
+      {text && text}
     </div>
     </OverlayTrigger>
   )
