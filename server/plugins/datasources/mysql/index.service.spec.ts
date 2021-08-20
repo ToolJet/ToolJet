65d85a37f6e19253b41abf73ea3d870b13315668
Akshay <akshaysasidharan93@gmail.com>
2021-08-20 19:56:43 +0530

    handle multiple columns on bulk query (#562)

--- server/plugins/datasources/mysql/index.service.spec.ts
+++ server/plugins/datasources/mysql/index.service.spec.ts
@@ -32,7 +32,7 @@ describe('MysqlQueryService', () => {
      };
 
     const builtQuery = await service.buildBulkUpdateQuery(queryOptions);
-    const expectedQuery = "UPDATE customers SET name = 'sam',  email = 'sam@example.com',  WHERE id = 1; UPDATE customers SET name = 'jon',  email = 'jon@example.com',  WHERE id = 2;";
+    const expectedQuery = "UPDATE customers SET name = 'sam', email = 'sam@example.com' WHERE id = 1; UPDATE customers SET name = 'jon', email = 'jon@example.com' WHERE id = 2;";
 
     expect(builtQuery).toBe(expectedQuery);
   });
