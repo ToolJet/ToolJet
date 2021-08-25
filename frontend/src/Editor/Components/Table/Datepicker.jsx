47cf923931710f39be0da2c10b83c6c7e41a3564
arpitnath <arpitnath42@gmail.com>
2021-08-25 18:17:55 +0530

    dropdown fixed

--- frontend/src/Editor/Components/Table/Datepicker.jsx
+++ frontend/src/Editor/Components/Table/Datepicker.jsx
@@ -17,14 +17,14 @@ export const Datepicker = function Datepicker({value, onChange, readOnly, isTime
     }
 
     React.useEffect(() => {
-        const _date = isTimeChecked ? moment(value, `${dateFormat} hh:mm`) :  moment(value, dateFormat)
+        const _date = isTimeChecked ? moment(value, `${dateFormat} LT`) :  moment(value, dateFormat)
 
         if(!isTimeChecked) {
-            setDate(_date)
+            setDate(moment(value, 'DD-MM-YYYY').format(dateFormat))
         }
 
         if(isTimeChecked) {
-            setDate(_date)
+            setDate(moment(value, 'DD-MM-YYYY LT' ).format(`${dateFormat} LT`))
         }
     
     },[isTimeChecked, readOnly, dateFormat])
@@ -34,6 +34,38 @@ export const Datepicker = function Datepicker({value, onChange, readOnly, isTime
         // placeholder: 'N/A',
         disabled: !readOnly,
     };
+
+    const [isDatepickerOpen, setIsDatepickerOpen] = React.useState(false)
+
+    const onDatepickerClose = () => {
+       onChange(date)
+       setIsDatepickerOpen((prev) => !prev)
+    }
+
+
+    React.useEffect(() => {
+        const myElement = document.querySelector('.cell-type-datepicker')
+
+        myElement.parentNode.style.position = 'absolute'
+        myElement.style.position = 'relative'
+        myElement.style.marginTop = '2px'
+        myElement.style.left = '50%'
+        myElement.style.width = '250px'
+        myElement.style.transform = 'translate(-50%, -25%)'
+
+
+        return () => {
+            myElement.parentNode.style.position = ''
+            myElement.style.position = ''
+            myElement.style.marginTop = ''
+            myElement.style.left = ''
+            myElement.style.width = ''
+            myElement.style.transform = ''
+        }
+
+    },[isDatepickerOpen])
+
+
 
 
     return (
@@ -45,7 +77,7 @@ export const Datepicker = function Datepicker({value, onChange, readOnly, isTime
                 dateFormat={dateFormat} 
                 value={date} 
                 onChange={dateChange}
-                onClose={() => onChange(date)}
+                onClose={onDatepickerClose}
               />
             </>
     )
