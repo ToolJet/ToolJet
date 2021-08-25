10b484ce76ba82ae688adc7e422deedfa6c5f2cf
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-08-25 13:33:59 +0530

    List only those apps that has proper slugs for goto-app action

--- frontend/src/Editor/Inspector/EventManager.jsx
+++ frontend/src/Editor/Inspector/EventManager.jsx
@@ -45,7 +45,7 @@ export const EventManager = ({
 
   function getAllApps() {
     let appsOptionsList = [];
-    apps.map((item) => {
+    apps.filter(item => item.slug != undefined).map((item) => {
       appsOptionsList.push({
         name: item.name,
         value: item.slug
