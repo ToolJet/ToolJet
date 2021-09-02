51df271249a123450a120d0982a0952056d4cee6
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-09-03 03:14:13 +0530

    fixes server error when inviting new user

--- server/test/controllers/organization_users.e2e-spec.ts
+++ server/test/controllers/organization_users.e2e-spec.ts
@@ -1,6 +1,11 @@
 import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
-import { authHeaderForUser, clearDB, createUser, createNestAppInstance } from '../test.helper';
+import {
+  authHeaderForUser,
+  clearDB,
+  createUser,
+  createNestAppInstance,
+} from '../test.helper';
 
 describe('organization users controller', () => {
   let app: INestApplication;
@@ -13,109 +18,179 @@ describe('organization users controller', () => {
     app = await createNestAppInstance();
   });
 
+  it('should allow only admin to be able to invite new users', async () => {
+    // setup a pre existing user of different organization
+    await createUser(app, { email: 'someUser@tooljet.io', role: 'admin' });
+
+    // setup organization and user setup to test against
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
+
+    const organization = adminUserData.organization;
+
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+      organization,
+    });
+
+    const viewerUserData = await createUser(app, {
+      email: 'viewer@tooljet.io',
+      role: 'viewer',
+      organization,
+    });
+
+    await request(app.getHttpServer())
+      .post(`/organization_users`)
+      .set('Authorization', authHeaderForUser(adminUserData.user))
+      .send({ email: 'test@tooljet.io', role: 'Viewer' })
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .post(`/organization_users`)
+      .set('Authorization', authHeaderForUser(developerUserData.user))
+      .send({ email: 'test2@tooljet.io', role: 'Viewer' })
+      .expect(403);
+
+    await request(app.getHttpServer())
+      .post(`/organization_users`)
+      .set('Authorization', authHeaderForUser(viewerUserData.user))
+      .send({ email: 'test3@tooljet.io', role: 'Viewer' })
+      .expect(403);
+  });
+
   it('should allow only authenticated users to archive org users', async () => {
-    await request(app.getHttpServer()).post('/organization_users/random-id/archive').expect(401);
+    await request(app.getHttpServer())
+      .post('/organization_users/random-id/archive')
+      .expect(401);
   });
 
   it('should allow only admin users to archive org users', async () => {
-
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
     const organization = adminUserData.organization;
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer', organization });
-    const viewerUserData = await createUser(app, { email: 'viewer@tooljet.io', role: 'viewer', organization });
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+      organization,
+    });
+    const viewerUserData = await createUser(app, {
+      email: 'viewer@tooljet.io',
+      role: 'viewer',
+      organization,
+    });
 
     let response = await request(app.getHttpServer())
       .post(`/organization_users/${adminUserData.orgUser.id}/archive`)
-      .set('Authorization',authHeaderForUser(viewerUserData.user))
-      .expect(403)
+      .set('Authorization', authHeaderForUser(viewerUserData.user))
+      .expect(403);
 
     await adminUserData.orgUser.reload();
     expect(adminUserData.orgUser.status).toBe('invited');
 
     response = await request(app.getHttpServer())
       .post(`/organization_users/${adminUserData.orgUser.id}/archive`)
-      .set('Authorization',authHeaderForUser(developerUserData.user))
-      .expect(403) 
+      .set('Authorization', authHeaderForUser(developerUserData.user))
+      .expect(403);
 
     await adminUserData.orgUser.reload();
-    expect(adminUserData.orgUser.status).toBe('invited');  
-      
+    expect(adminUserData.orgUser.status).toBe('invited');
+
     response = await request(app.getHttpServer())
       .post(`/organization_users/${developerUserData.orgUser.id}/archive`)
-      .set('Authorization',authHeaderForUser(adminUserData.user))
-      .expect(201)   
+      .set('Authorization', authHeaderForUser(adminUserData.user))
+      .expect(201);
 
     await developerUserData.orgUser.reload();
-    expect(developerUserData.orgUser.status).toBe('archived');  
-
+    expect(developerUserData.orgUser.status).toBe('archived');
   });
 
   it('should allow only admin users to change role of org users', async () => {
-
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
     const organization = adminUserData.organization;
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer', organization });
-    const viewerUserData = await createUser(app, { email: 'viewer@tooljet.io', role: 'viewer', organization });
-
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+      organization,
+    });
+    const viewerUserData = await createUser(app, {
+      email: 'viewer@tooljet.io',
+      role: 'viewer',
+      organization,
+    });
 
     let response = await request(app.getHttpServer())
       .post(`/organization_users/${viewerUserData.orgUser.id}/change_role`)
-      .set('Authorization',authHeaderForUser(developerUserData.user))
+      .set('Authorization', authHeaderForUser(developerUserData.user))
       .send({ role: 'developer' })
-      .expect(403)
+      .expect(403);
 
     await viewerUserData.orgUser.reload();
     expect(viewerUserData.orgUser.role).toBe('viewer');
 
     response = await request(app.getHttpServer())
       .post(`/organization_users/${viewerUserData.orgUser.id}/change_role`)
-      .set('Authorization',authHeaderForUser(viewerUserData.user))
+      .set('Authorization', authHeaderForUser(viewerUserData.user))
       .send({ role: 'viewer' })
-      .expect(403)
+      .expect(403);
 
     await developerUserData.orgUser.reload();
     expect(developerUserData.orgUser.role).toBe('developer');
 
     response = await request(app.getHttpServer())
       .post(`/organization_users/${developerUserData.orgUser.id}/change_role`)
-      .set('Authorization',authHeaderForUser(adminUserData.user))
+      .set('Authorization', authHeaderForUser(adminUserData.user))
       .send({ role: 'viewer' })
-      .expect(201)
+      .expect(201);
 
     await developerUserData.orgUser.reload();
     expect(developerUserData.orgUser.role).toBe('viewer');
-
   });
 
   it('should allow only admin users to change role of org users', async () => {
-
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer' });
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+    });
 
     const response = await request(app.getHttpServer())
       .post(`/organization_users/${developerUserData.orgUser.id}/change_role`)
-      .set('Authorization',authHeaderForUser(adminUserData.user))
+      .set('Authorization', authHeaderForUser(adminUserData.user))
       .send({ role: 'viewer' })
-      .expect(403)
-    
-    await developerUserData.orgUser.reload();
-    expect(developerUserData.orgUser.role).toBe('developer');  
+      .expect(403);
 
+    await developerUserData.orgUser.reload();
+    expect(developerUserData.orgUser.role).toBe('developer');
   });
 
   it('should allow only admin users to archive org users', async () => {
-
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer' });
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+    });
 
     const response = await request(app.getHttpServer())
       .post(`/organization_users/${developerUserData.orgUser.id}/archive`)
-      .set('Authorization',authHeaderForUser(adminUserData.user))
-      .expect(403)
-    
-    await developerUserData.orgUser.reload();
-    expect(developerUserData.orgUser.status).toBe('invited');  
+      .set('Authorization', authHeaderForUser(adminUserData.user))
+      .expect(403);
 
+    await developerUserData.orgUser.reload();
+    expect(developerUserData.orgUser.status).toBe('invited');
   });
 
   afterAll(async () => {
