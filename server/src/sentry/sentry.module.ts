0fd96bcd6927b6b27c9734af614efa483d1c1113
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-09-07 14:01:31 +0530

    add module for sentry

---
+++ server/src/sentry/sentry.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SentryService } from './sentry.service';
+import { SentryInterceptor } from './sentry.interceptor';
+
+export const SENTRY_OPTIONS = 'SENTRY_OPTIONS';
+@Module({
+  providers: [SentryService],
+})
+export class SentryModule {
+  static forRoot(options: Sentry.NodeOptions) {
+    // initialization of Sentry, this is where Sentry will create a Hub
+    Sentry.init(options);
+
+    return {
+      module: SentryModule,
+      providers: [
+        {
+          provide: SENTRY_OPTIONS,
+          useValue: options,
+        },
+        SentryService,
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: SentryInterceptor,
+        },
+      ],
+      exports: [SentryService],
+    };
+  }
+}
