a4b3e52a7407766eeddad8060ed06e47ceedb0b3
gandharv <gandharvkumargarg@gmail.com>
2021-09-08 11:26:25 +0530

    feat: add tree shaking for lodash

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -37,10 +37,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader'
-      },
-      {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
       },
@@ -61,7 +57,16 @@ module.exports = {
           extensions: [".js", ".jsx"]
         },
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              [
+                'import',
+                { libraryName: 'lodash', libraryDirectory: "", camel2DashComponentName: false},
+                'lodash',
+              ],
+            ],
+          },
         }
       }
     ]
