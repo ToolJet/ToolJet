228379b2c6ea829cbd267c896fe9abd8bb8b0e80
gandharv <gandharvkumargarg@gmail.com>
2021-09-10 11:06:38 +0530

    remove unwanted class on toggle

--- frontend/src/_ui/Toggle/index.js
+++ frontend/src/_ui/Toggle/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default ({ defaultChecked, onChange }) => {
   return (
-    <label className="form-check form-switch mt-3">
+    <label className="form-switch mt-3">
       <input className="form-check-input" type="checkbox" defaultChecked={defaultChecked} onChange={onChange} />
     </label>
   );
