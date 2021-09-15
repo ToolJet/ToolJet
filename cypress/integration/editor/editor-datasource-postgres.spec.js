80145bd3150cbc42169535d46996b2b69767fae0
Mohini Dahiya <mohini@saeloun.com>
2021-09-15 08:21:56 +0530

    [Cypress] Fix existing tests on Editor (#728)
    * Test try git
    
    * Updated 'editor-navigation-bar.spec.js' tests
    
    Fixes #718
    1. Removes test for hiding left sidebar.
    2. Fixes test - resize canvas
    3. Fixes test - switch to dark theme
    
    * Updated index.js and cypress.json files
    
    1. Added viewport to cypress.json file to support macbook pro 16.
    2. Updated index.js file to do golobal import of cypress-drag-drop.
    3. Merged custom method from dev branch to command.js file.
    
    * Add cypress. reference type to index.js
    
    * Fixes 'editor-datasource-postgres.spec.js'
    
    1. Updated test with new UI changes.
    2. Updated 'addPostgresDataSource' method in 'command.js' file to mark SSL box unchecked.

--- cypress/integration/editor/editor-datasource-postgres.spec.js
+++ cypress/integration/editor/editor-datasource-postgres.spec.js
@@ -11,32 +11,32 @@ describe('Editor- Add "PostgreSQL" datasource', () => {
         cy.get('title').should('have.text', 'ToolJet - Dashboard');
     });
 
-    it('should add First data source successfully', () => {
+    it.only('should add First data source successfully', () => {
+        //test database icon
         cy.get('.left-sidebar')
-            .find('.datasources-container.w-100.mt-3')
-            .find('.p-1.text-muted')
-            .should('have.text', 'DATASOURCES')
-            .and('be.visible');
+            .find('.svg-icon[src="/assets/images/icons/editor/left-sidebar/database.svg"]')
+            .should('be.visible')
+            .click()
 
-        cy.get('.table-responsive').find('.p-2').should('include.text', "You haven't added data sources yet. ");
+        cy.get('.table-responsive')
+            .find('.p-2')
+            .should('have.text', "You haven't added any datasources yet. ")
 
-        cy.get('center[class="p-2 text-muted"]')
-            .find('button[class="btn btn-sm btn-outline-azure mt-3"]')
-            .should('have.text', 'add datasource')
+        cy.get('div[class="table-responsive"] button[class="btn btn-sm btn-outline-azure mt-3"]')
+            .should('have.text', 'Add datasource')
             .click();
 
+        //create database    
         cy.addPostgresDataSource();
-    });
 
-    it('should add data source from "Add new datasource button', () => {
+        //verify if you can see postgres database in the list now.
         cy.get('.left-sidebar')
-            .find('.datasources-container.w-100.mt-3')
-            .find('.p-1.text-muted')
-            .should('have.text', 'DATASOURCES')
-            .and('be.visible');
+            .find('.svg-icon[src="/assets/images/icons/editor/left-sidebar/database.svg"]')
+            .should('be.visible')
+            .click()
 
-        cy.get('[data-tip="Add new datasource"]').click();
-
-        cy.addPostgresDataSource();
+        cy.get('.table-responsive')
+            .find('tr td')
+            .contains('PostgreSQL')
     });
 });
