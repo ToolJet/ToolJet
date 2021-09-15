e451e910f240754f03c8646efdf143c4317491da
Gandharv <gandharvkumargarg@gmail.com>
2021-09-15 21:10:59 +0530

    Enable suggestion box for codehinter (#737)
    * enable suggestion box for codehinter
    
    * add heading as type of the preview
    
    * add preview only on focus
    
    * remove box shadow
    
    * add background to widget inspector documentation footer link
    
    * doc footer updats
    
    * add condition where boolean value was not getting printed
    
    * use index as key

--- frontend/src/Editor/CodeBuilder/CodeHinter.jsx
+++ frontend/src/Editor/CodeBuilder/CodeHinter.jsx
@@ -8,25 +8,25 @@ import 'codemirror/addon/display/placeholder';
 import 'codemirror/addon/search/match-highlighter';
 import 'codemirror/addon/hint/show-hint.css';
 import 'codemirror/theme/base16-light.css';
-import 'codemirror/theme/duotone-light.css'
+import 'codemirror/theme/duotone-light.css';
 import 'codemirror/theme/monokai.css';
 import { getSuggestionKeys, onBeforeChange, handleChange } from './utils';
 import { resolveReferences } from '@/_helpers/utils';
 
 export function CodeHinter({
-  initialValue, 
-  onChange, 
-  currentState, 
-  mode, 
-  theme, 
-  lineNumbers, 
-  className, 
-  placeholder, 
-  ignoreBraces, 
-  enablePreview, 
+  initialValue,
+  onChange,
+  currentState,
+  mode,
+  theme,
+  lineNumbers,
+  className,
+  placeholder,
+  ignoreBraces,
+  enablePreview,
   height,
   minHeight,
-  lineWrapping
+  lineWrapping,
 }) {
   const options = {
     lineNumbers: lineNumbers,
@@ -37,11 +37,12 @@ export function CodeHinter({
     theme: theme || 'default',
     readOnly: false,
     highlightSelectionMatches: true,
-    placeholder
+    placeholder,
   };
 
   const [realState, setRealState] = useState(currentState);
   const [currentValue, setCurrentValue] = useState(initialValue);
+  const [isFocused, setFocused] = useState(false);
 
   useEffect(() => {
     setRealState(currentState);
@@ -56,9 +57,35 @@ export function CodeHinter({
     setCurrentValue(editor.getValue());
   }
 
+  const getPreviewContent = (content, type) => {
+    switch (type) {
+      case 'object':
+        return JSON.stringify(content);
+      case 'boolean':
+        return content.toString()
+      default:
+        return content;
+    }
+  };
+
+  const getPreview = () => {
+    const preview = resolveReferences(currentValue, realState);
+    const previewType = typeof preview;
+    const content = getPreviewContent(preview, previewType);
+
+    return (
+      <div>
+        <div class="heading my-1">
+          <span>{previewType}</span>
+        </div>
+        {content}
+      </div>
+    );
+  };
+
   return (
-    <div 
-      className={`code-hinter ${className || 'codehinter-default-input'}`} 
+    <div
+      className={`code-hinter ${className || 'codehinter-default-input'}`}
       key={suggestions.length}
       style={{ height: height || 'auto', minHeight, maxHeight: '320px', overflow: 'auto' }}
     >
@@ -67,19 +94,17 @@ export function CodeHinter({
         realState={realState}
         scrollbarStyle={null}
         height={height}
-        onBlur={(editor) => { 
+        onFocus={() => setFocused(true)}
+        onBlur={(editor) => {
           const value = editor.getValue();
           onChange(value);
+          setFocused(false)
         }}
         onChange={(editor) => valueChanged(editor, onChange, suggestions, ignoreBraces)}
         onBeforeChange={(editor, change) => onBeforeChange(editor, change, ignoreBraces)}
         options={options}
       />
-      {enablePreview && 
-        <div className="dynamic-variable-preview bg-green-lt px-2 py-1">
-          {resolveReferences(currentValue, realState)}
-        </div>
-      }
+      {isFocused && enablePreview && <div className="dynamic-variable-preview bg-green-lt px-1 py-1">{getPreview()}</div>}
     </div>
   );
 }
