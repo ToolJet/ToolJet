8c5d521a434107c74e0fe103e0c62be9c49bd23a
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-09-16 01:34:52 +0530

    only update credentials when data source is updated

--- server/test/controllers/data_sources.e2e-spec.ts
+++ server/test/controllers/data_sources.e2e-spec.ts
@@ -14,35 +14,54 @@ describe('data sources controller', () => {
   });
 
   it('should be able to create data sources of an app only if admin/developer of same organization', async () => {
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+      organization: adminUserData.organization,
+    });
+    const viewerUserData = await createUser(app, {
+      email: 'viewer@tooljet.io',
+      role: 'viewer',
+      organization: adminUserData.organization,
+    });
+    const anotherOrgAdminUserData = await createUser(app, {
+      email: 'another@tooljet.io',
+      role: 'admin',
+    });
+    const application = await createApplication(app, {
+      name: 'name',
+      user: adminUserData.user,
+    });
 
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer', organization: adminUserData.organization });
-    const viewerUserData = await createUser(app, { email: 'viewer@tooljet.io', role: 'viewer', organization: adminUserData.organization });
-    const anotherOrgAdminUserData = await createUser(app, { email: 'another@tooljet.io', role: 'admin' });
-    const application = await createApplication(app, { name: 'name', user: adminUserData.user });
+    const dataSourceParams = {
+      name: 'name',
+      options: [{ key: 'foo', value: 'bar', encrypted: 'true' }],
+      kind: 'postgres',
+      app_id: application.id,
+    };
 
-    const dataSourceParams = { 
-      name: 'name', 
-      options: [], 
-      kind: 'postgres', 
-      app_id: application.id
-    }
-
-    for(const userData of [adminUserData, developerUserData]) {
+    for (const userData of [adminUserData, developerUserData]) {
       const response = await request(app.getHttpServer())
         .post(`/data_sources`)
         .set('Authorization', authHeaderForUser(userData.user))
-        .send(dataSourceParams)
+        .send(dataSourceParams);
 
       expect(response.statusCode).toBe(201);
     }
 
+    // encrypted data source options will create credentials
+    expect(await Credential.count()).toBe(2);
+
     // Should not update if viewer or if user of another org
-    for(const userData of [anotherOrgAdminUserData, viewerUserData]) {
+    for (const userData of [anotherOrgAdminUserData, viewerUserData]) {
       const response = await request(app.getHttpServer())
         .post(`/data_sources`)
         .set('Authorization', authHeaderForUser(userData.user))
-        .send(dataSourceParams)
+        .send(dataSourceParams);
 
       expect(response.statusCode).toBe(403);
     }
@@ -50,53 +69,103 @@ describe('data sources controller', () => {
   });
 
   it('should be able to update data sources of an app only if admin/developer of same organization', async () => {
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+      organization: adminUserData.organization,
+    });
+    const viewerUserData = await createUser(app, {
+      email: 'viewer@tooljet.io',
+      role: 'viewer',
+      organization: adminUserData.organization,
+    });
+    const anotherOrgAdminUserData = await createUser(app, {
+      email: 'another@tooljet.io',
+      role: 'admin',
+    });
+    const application = await createApplication(app, {
+      name: 'name',
+      user: adminUserData.user,
+    });
+    const dataSource = await createDataSource(app, {
+      name: 'name',
+      options: [{ key: 'foo', value: 'bar', encrypted: 'true' }],
+      kind: 'postgres',
+      application: application,
+      user: adminUserData.user,
+    });
 
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer', organization: adminUserData.organization });
-    const viewerUserData = await createUser(app, { email: 'viewer@tooljet.io', role: 'viewer', organization: adminUserData.organization });
-    const anotherOrgAdminUserData = await createUser(app, { email: 'another@tooljet.io', role: 'admin' });
-    const application = await createApplication(app, { name: 'name', user: adminUserData.user });
-    const dataSource = await createDataSource(app, { name: 'name', options: [], kind: 'postgres', application: application, user: adminUserData.user });
+    // encrypted data source options will create credentials
+    expect(await Credential.count()).toBe(1);
 
-    for(const userData of [adminUserData, developerUserData]) {
-      const newOptions = [ { key: 'email', value: userData.user.email } ]
+    for (const userData of [adminUserData, developerUserData]) {
+      const newOptions = [{ key: 'email', value: userData.user.email }];
       const response = await request(app.getHttpServer())
         .put(`/data_sources/${dataSource.id}`)
         .set('Authorization', authHeaderForUser(userData.user))
         .send({
-          options: newOptions
-        })
+          options: newOptions,
+        });
 
       expect(response.statusCode).toBe(200);
       await dataSource.reload();
       expect(dataSource.options['email']['value']).toBe(userData.user.email);
     }
 
+    // new credentials will not be created upon data source update
+    expect(await Credential.count()).toBe(1);
+
     // Should not update if viewer or if user of another org
-    for(const userData of [anotherOrgAdminUserData, viewerUserData]) {
+    for (const userData of [anotherOrgAdminUserData, viewerUserData]) {
       const response = await request(app.getHttpServer())
         .put(`/data_sources/${dataSource.id}`)
         .set('Authorization', authHeaderForUser(userData.user))
         .send({
-          options: [ ]
-        })
+          options: [],
+        });
 
       expect(response.statusCode).toBe(403);
     }
   });
 
   it('should be able to list (get) datasources for an app only if admin/developer of same organization', async () => {
-    const adminUserData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
-    const developerUserData = await createUser(app, { email: 'developer@tooljet.io', role: 'developer', organization: adminUserData.organization });
-    const viewerUserData = await createUser(app, { email: 'viewer@tooljet.io', role: 'viewer', organization: adminUserData.organization });
-    const application = await createApplication(app, { name: 'name', user: adminUserData.user });
-    const anotherOrgAdminUserData = await createUser(app, { email: 'another@tooljet.io', role: 'admin' });
-    const dataSource = await createDataSource(app, { name: 'name', kind: 'postgres', application: application, user: adminUserData.user });
+    const adminUserData = await createUser(app, {
+      email: 'admin@tooljet.io',
+      role: 'admin',
+    });
+    const developerUserData = await createUser(app, {
+      email: 'developer@tooljet.io',
+      role: 'developer',
+      organization: adminUserData.organization,
+    });
+    const viewerUserData = await createUser(app, {
+      email: 'viewer@tooljet.io',
+      role: 'viewer',
+      organization: adminUserData.organization,
+    });
+    const application = await createApplication(app, {
+      name: 'name',
+      user: adminUserData.user,
+    });
+    const anotherOrgAdminUserData = await createUser(app, {
+      email: 'another@tooljet.io',
+      role: 'admin',
+    });
+    const dataSource = await createDataSource(app, {
+      name: 'name',
+      kind: 'postgres',
+      application: application,
+      user: adminUserData.user,
+    });
 
-    for(const userData of [adminUserData, developerUserData, viewerUserData]) {
+    for (const userData of [adminUserData, developerUserData, viewerUserData]) {
       const response = await request(app.getHttpServer())
         .get(`/data_sources?app_id=${application.id}`)
-        .set('Authorization', authHeaderForUser(userData.user))
+        .set('Authorization', authHeaderForUser(userData.user));
 
       expect(response.statusCode).toBe(200);
       expect(response.body.data_sources.length).toBe(1);
--- server/test/test.helper.ts
+++ server/test/test.helper.ts
@@ -1,4 +1,5 @@
 import { JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { getConnection, Repository } from 'typeorm';
 import { OrganizationUser } from 'src/entities/organization_user.entity';
@@ -12,14 +13,16 @@ import { AppUser } from 'src/entities/app_user.entity';
 import { AppVersion } from 'src/entities/app_version.entity';
 import { DataQuery } from 'src/entities/data_query.entity';
 import { DataSource } from 'src/entities/data_source.entity';
+import { DataSourcesService } from 'src/services/data_sources.service';
+import { DataSourcesModule } from 'src/modules/data_sources/data_sources.module';
 
 export async function createNestAppInstance() {
   let app: INestApplication;
 
   const moduleRef = await Test.createTestingModule({
     imports: [AppModule],
-  })
-  .compile();
+    providers: [],
+  }).compile();
 
   app = moduleRef.createNestApplication();
   await app.init();
@@ -29,7 +32,9 @@ export async function createNestAppInstance() {
 
 export function authHeaderForUser(user: any) {
   const configService = new ConfigService();
-  const jwtService = new JwtService({secret: configService.get<string>('SECRET_KEY_BASE')});
+  const jwtService = new JwtService({
+    secret: configService.get<string>('SECRET_KEY_BASE'),
+  });
   const authPayload = { username: user.id, sub: user.email };
   const authToken = jwtService.sign(authPayload);
   return `Bearer ${authToken}`;
@@ -39,7 +44,9 @@ export async function clearDB() {
   const entities = getConnection().entityMetadatas;
   for (const entity of entities) {
     const repository = await getConnection().getRepository(entity.name);
-    await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+    await repository.query(
+      `TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`,
+    );
   }
 }
 
@@ -49,24 +56,28 @@ export async function createApplication(app, { name, user, isPublic }: any) {
   let appUsersRepository: Repository<AppUser>;
   appUsersRepository = app.get('AppUserRepository');
 
-  user = user || await (await createUser(app, {})).user;
+  user = user || (await (await createUser(app, {})).user);
 
-  const newApp = await appRepository.save(appRepository.create({
-    name,
-    user,
-    isPublic: isPublic || false,
-    organizationId: user.organization.id,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }));
+  const newApp = await appRepository.save(
+    appRepository.create({
+      name,
+      user,
+      isPublic: isPublic || false,
+      organizationId: user.organization.id,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  );
 
-  await appUsersRepository.save(appUsersRepository.create({
-    app: newApp, 
-    user,
-    role: 'admin',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }));
+  await appUsersRepository.save(
+    appUsersRepository.create({
+      app: newApp,
+      user,
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  );
 
   return newApp;
 }
@@ -75,13 +86,18 @@ export async function createApplicationVersion(app, application) {
   let appVersionsRepository: Repository<AppVersion>;
   appVersionsRepository = app.get('AppVersionRepository');
 
-  return await appVersionsRepository.save(appVersionsRepository.create({
-    app: application,
-    name: 'v0'
-  }));
+  return await appVersionsRepository.save(
+    appVersionsRepository.create({
+      app: application,
+      name: 'v0',
+    }),
+  );
 }
 
-export async function createUser(app, { firstName, lastName, email, role, organization }: any) {
+export async function createUser(
+  app,
+  { firstName, lastName, email, role, organization }: any,
+) {
   let userRepository: Repository<User>;
   let organizationRepository: Repository<Organization>;
   let organizationUsersRepository: Repository<OrganizationUser>;
@@ -90,59 +106,79 @@ export async function createUser(app, { firstName, lastName, email, role, organi
   organizationRepository = app.get('OrganizationRepository');
   organizationUsersRepository = app.get('OrganizationUserRepository');
 
-  organization = organization || await organizationRepository.save(organizationRepository.create({ 
-    name: 'test org', 
-    createdAt: new Date(), 
-    updatedAt: new Date() 
-  }));
+  organization =
+    organization ||
+    (await organizationRepository.save(
+      organizationRepository.create({
+        name: 'test org',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }),
+    ));
 
-  const user = await userRepository.save(userRepository.create({ 
-    firstName: firstName || 'test', 
-    lastName: lastName || 'test', 
-    email: email || 'dev@tooljet.io', 
-    password: 'password', 
-    organization, 
-    createdAt: new Date(), 
-    updatedAt: new Date(), 
-  }));
+  const user = await userRepository.save(
+    userRepository.create({
+      firstName: firstName || 'test',
+      lastName: lastName || 'test',
+      email: email || 'dev@tooljet.io',
+      password: 'password',
+      organization,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  );
 
-  const orgUser = await organizationUsersRepository.save(organizationUsersRepository.create({ 
-    user: user, 
-    organization, 
-    role: role || 'admin', 
-    createdAt: new Date(), 
-    updatedAt: new Date()
-  }));
+  const orgUser = await organizationUsersRepository.save(
+    organizationUsersRepository.create({
+      user: user,
+      organization,
+      role: role || 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  );
 
-  return { organization, user, orgUser }
+  return { organization, user, orgUser };
 }
 
-export async function createDataSource(nestInstance, { name, application, kind, options }: any) {
+export async function createDataSource(
+  nestInstance,
+  { name, application, kind, options }: any,
+) {
   let dataSourceRepository: Repository<DataSource>;
   dataSourceRepository = nestInstance.get('DataSourceRepository');
 
-  return await dataSourceRepository.save(dataSourceRepository.create({
-    name,
-    options,
-    app: application,
-    kind,
-    createdAt: new Date(), 
-    updatedAt: new Date()
-  }));
+  const dataSourcesService = nestInstance
+    .select(DataSourcesModule)
+    .get(DataSourcesService);
+
+  return await dataSourceRepository.save(
+    dataSourceRepository.create({
+      name,
+      options: await dataSourcesService.parseOptionsForCreate(options),
+      app: application,
+      kind,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  );
 }
 
-
-export async function createDataQuery(nestInstance, { application, kind, dataSource, options }: any) {
+export async function createDataQuery(
+  nestInstance,
+  { application, kind, dataSource, options }: any,
+) {
   let dataQueryRepository: Repository<DataQuery>;
   dataQueryRepository = nestInstance.get('DataQueryRepository');
 
-  return await dataQueryRepository.save(dataQueryRepository.create({
-    options,
-    app: application,
-    kind,
-    dataSource,
-    createdAt: new Date(), 
-    updatedAt: new Date()
-  }));
+  return await dataQueryRepository.save(
+    dataQueryRepository.create({
+      options,
+      app: application,
+      kind,
+      dataSource,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  );
 }
-
