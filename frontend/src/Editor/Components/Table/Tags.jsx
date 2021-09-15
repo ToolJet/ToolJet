380358f0713427aadc3233e98b1f994619f91150
gandharv <gandharvkumargarg@gmail.com>
2021-09-15 12:10:28 +0530

    fix: add array check in tags component

--- frontend/src/Editor/Components/Table/Tags.jsx
+++ frontend/src/Editor/Components/Table/Tags.jsx
@@ -1,62 +1,67 @@
 import React, { useState } from 'react';
 
 export const Tags = ({ value, onChange }) => {
+  const isValid = Array.isArray(value);
+  if (!isValid) console.warn('[Tags]: value provided is not an array');
+  value = isValid ? value : [];
 
-    value = value || [];
+  const [showForm, setShowForm] = useState(false);
 
-    const [ showForm, setShowForm ] = useState(false);
-
-    function addTag(text) {
-        if(text !== '') { 
-            value.push(text);
-            onChange(value);
-        } else {
-            setShowForm(false);
-        }
+  function addTag(text) {
+    if (text !== '') {
+      value.push(text);
+      onChange(value);
+    } else {
+      setShowForm(false);
     }
+  }
 
-    function removeTag(text) {
-        const newValue = value.filter(tag => tag !== text);
-        onChange(newValue);
-        setShowForm(false);
-    }
+  function removeTag(text) {
+    const newValue = value.filter((tag) => tag !== text);
+    onChange(newValue);
+    setShowForm(false);
+  }
 
-    function handleFormKeyDown(e) {
-        if(e.key === 'Enter') {
-            addTag(e.target.value)
-        }
+  function handleFormKeyDown(e) {
+    if (e.key === 'Enter') {
+      addTag(e.target.value);
     }
- 
-    function renderTag(text) {
-        return <span className="col-auto badge bg-blue-lt p-2 mx-1 tag mb-2">
-            {text} 
-            <span className="badge badge-pill bg-red-lt remove-tag-button" onClick={() => removeTag(text)}>
-                x
-            </span>
-        </span>;
-    }
+  }
 
+  function renderTag(text) {
     return (
-        <div className="tags row">
-            {value.map((item) => {
-                return renderTag(item)
-            })}
-
-            {!showForm &&
-                <span className="col-auto badge bg-green-lt mx-1 add-tag-button" onClick={() => setShowForm(true)}>{'+'}</span>
-            }
-
-            {showForm &&
-                <span className="col-auto badge bg-green-lt mx-1">
-                    <input 
-                        type="text"
-                        autoFocus
-                        className="form-control-plaintext"
-                        onBlur={(e) => addTag(e.target.value)}
-                        onKeyDown={handleFormKeyDown}
-                    />
-                </span>
-            }
-        </div>
+      <span className="col-auto badge bg-blue-lt p-2 mx-1 tag mb-2">
+        {text}
+        <span className="badge badge-pill bg-red-lt remove-tag-button" onClick={() => removeTag(text)}>
+          x
+        </span>
+      </span>
     );
+  }
+
+  return (
+    <div className="tags row">
+      {value.map((item) => {
+        return renderTag(item);
+      })}
+
+      {!showForm && (
+        <span className="col-auto badge bg-green-lt mx-1 add-tag-button" onClick={() => setShowForm(true)}>
+          {'+'}
+        </span>
+      )}
+
+      {showForm && (
+        <span className="col-auto badge bg-green-lt mx-1">
+          <input
+            type="text"
+            autoFocus
+            className="form-control-plaintext"
+            onBlur={(e) => addTag(e.target.value)}
+            onKeyDown={handleFormKeyDown}
+          />
+        </span>
+      )}
+    </div>
+  );
 };
