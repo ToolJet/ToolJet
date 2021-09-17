74592eb68000880ec38d4bcf2b1be97e539524d4
Gandharv <gandharvkumargarg@gmail.com>
2021-09-17 19:34:15 +0530

    Add devTool only in development for webpack (#774)
    * add devTool only in development
    
    * remove console log

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  devtool: 'inline-source-map',
+  ...(environment === 'development' && { devtool: 'inline-source-map' }),
   module: {
     rules: [
       {
@@ -62,19 +62,15 @@ module.exports = {
           extensions: ['.js', '.jsx'],
         },
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             plugins: [
-              [
-                'import',
-                { libraryName: 'lodash', libraryDirectory: "", camel2DashComponentName: false},
-                'lodash',
-              ],
+              ['import', { libraryName: 'lodash', libraryDirectory: '', camel2DashComponentName: false }, 'lodash'],
             ],
           },
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx', '.png'],
@@ -100,7 +96,7 @@ module.exports = {
     config: JSON.stringify({
       apiUrl: `${API_URL[environment] || ''}/api`,
       assetPath: ASSET_PATH[environment],
-      SERVER_IP: process.env.SERVER_IP
-    })
-  }
+      SERVER_IP: process.env.SERVER_IP,
+    }),
+  },
 };
