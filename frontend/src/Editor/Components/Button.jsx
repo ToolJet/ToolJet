cc9c476de1b4967513bed89940cffcffc5a56f03
arpitnath <arpitnath42@gmail.com>
2021-09-19 18:20:00 +0530

    eslint-setup: rules for frontend and server

--- frontend/src/Editor/Components/Button.jsx
+++ frontend/src/Editor/Components/Button.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { resolveReferences, resolveWidgetFieldValue } from '@/_helpers/utils';
-var tinycolor = require("tinycolor2");
+var tinycolor = require('tinycolor2');
 
-export const Button = function Button({
-  id, width, height, component, onComponentClick, currentState
-}) {
+export const Button = function Button({ id, width, height, component, onComponentClick, currentState }) {
   console.log('currentState', currentState);
 
   const [loadingState, setLoadingState] = useState(false);
@@ -23,12 +21,15 @@ export const Button = function Button({
   const widgetVisibility = component.definition.styles?.visibility?.value ?? true;
   const disabledState = component.definition.styles?.disabledState?.value ?? false;
 
-  const parsedDisabledState = typeof disabledState !== 'boolean' ? resolveWidgetFieldValue(disabledState, currentState) : disabledState;
+  const parsedDisabledState =
+    typeof disabledState !== 'boolean' ? resolveWidgetFieldValue(disabledState, currentState) : disabledState;
   let parsedWidgetVisibility = widgetVisibility;
-  
+
   try {
     parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
-  } catch (err) { console.log(err); }
+  } catch (err) {
+    console.log(err);
+  }
 
   const computedStyles = {
     backgroundColor,
@@ -36,7 +37,7 @@ export const Button = function Button({
     width,
     height,
     display: parsedWidgetVisibility ? '' : 'none',
-    '--tblr-btn-color-darker': tinycolor(backgroundColor).darken(8).toString() 
+    '--tblr-btn-color-darker': tinycolor(backgroundColor).darken(8).toString(),
   };
 
   return (
@@ -44,7 +45,10 @@ export const Button = function Button({
       disabled={parsedDisabledState}
       className={`jet-button btn btn-primary p-1 ${loadingState === true ? ' btn-loading' : ''}`}
       style={computedStyles}
-      onClick={(event) => {event.stopPropagation();  onComponentClick(id, component)}}
+      onClick={(event) => {
+        event.stopPropagation();
+        onComponentClick(id, component);
+      }}
     >
       {text}
     </button>
--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,24 +1,62 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        trailingComma: 'es5',
+        printWidth: 120,
+        singleQuote: true,
+        arrowParens: 'always',
+        proseWrap: 'preserve',
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none' }],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          String: false,
+          Boolean: false,
+          Number: false,
+          Symbol: false,
+          '{}': false,
+          Object: false,
+          object: false,
+          Function: false,
+        },
+        extendDefaults: true,
+      },
+    ],
   },
 };
