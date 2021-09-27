5d0ae0ed693e63a8fa086a1a32dace333945d594
Arpit <arpitnath42@gmail.com>
2021-09-27 15:18:02 +0530

    Canvas grid resized when resizing a component and component snaps to the nearest grid. (#792)
    * canvas grid resized when resizing a component and component snaps to the new next grid size
    
    * resize grid while dragging and resizing a component in the canvas
    
    * values refactored backward
    
    * Minor changes
    
    * Few more fixes
    
    * remove resizer-selected
    
    * Anotehr change
    
    * Revert "Anotehr change"
    
    This reverts commit 63a3649bff25c94f1860250036c73d608206bed6.
    
    * clicking on canvas area, dis-selects the widget and selectedComponent={}
    
    Co-authored-by: navaneeth <navaneethpk@outlook.com>

--- frontend/src/Editor/snapToGrid.js
+++ frontend/src/Editor/snapToGrid.js
@@ -1,5 +1,5 @@
 export function snapToGrid(x, y) {
   const snappedX = Math.round(x / 30) * 30;
-  const snappedY = Math.round(y / 30) * 30;
+  const snappedY = Math.round(y / 10) * 10;
   return [snappedX, snappedY];
 }
