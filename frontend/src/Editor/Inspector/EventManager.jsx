23684f075bd192d94ef4677ae183f59d9a561a54
Viraj Bahulkar <bbahulkar@gmail.com>
2021-09-27 14:58:40 +0530

    Add filter property for dark and light colors

--- frontend/src/Editor/Inspector/EventManager.jsx
+++ frontend/src/Editor/Inspector/EventManager.jsx
@@ -262,7 +262,7 @@ export const EventManager = ({
                 {componentMeta.events[event.eventId]['displayName']}
               </div>
               <div className="col">
-                <small className={darkMode ? 'text-warning' : 'text-muted'}>{actionMeta.name}</small>
+                <small className={darkMode ? 'event-action__dark' : 'event-action__light'}>{actionMeta.name}</small>
               </div>
               <div className="col-auto">
                 <span
