6d66e2e34e549457a65898660111122feb481ca4
gandharv <gandharvkumargarg@gmail.com>
2021-09-27 17:21:54 +0530

    feat: add migrations for thread and comment

---
+++ server/migrations/1632740456613-CreateCommentMigration.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+
+export class CreateCommentMigration1632740456613 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comment',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isGenerated: true,
+            default: 'gen_random_uuid()',
+            isPrimary: true,
+          },
+          {
+            name: 'is_read',
+            type: 'boolean',
+            default: false,
+            isNullable: true,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: true,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+            default: 'now()',
+          },
+        ],
+      }),
+      true
+    );
+
+    await queryRunner.createForeignKey(
+      'comment',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
---
+++ server/migrations/1632740487353-CreateThreadMigration.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+
+export class CreateThreadMigration1632740487353 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'thread',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isGenerated: true,
+            default: 'gen_random_uuid()',
+            isPrimary: true,
+          },
+          {
+            name: 'x',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'y',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'is_resolved',
+            type: 'boolean',
+            default: false,
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: true,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+            default: 'now()',
+          },
+        ],
+      }),
+      true
+    );
+
+    await queryRunner.createForeignKey(
+      'thread',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
