a172564828f4265270783847dd3fa132901c1f99
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-10-01 12:33:10 +0530

    Render app only after ENV variables are fetched from server

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,32 +6,33 @@ import { createBrowserHistory } from 'history';
 import { appService } from '@/_services';
 import { App } from './App';
 
-appService.getConfig().then((config) => {
-  window.public_config = config;
+appService
+  .getConfig()
+  .then((config) => {
+    window.public_config = config;
 
-  if (window.public_config.APM_VENDOR == 'sentry') {
-    const history = createBrowserHistory();
-    const tooljetServerUrl = window.public_config.TOOLJET_SERVER_URL;
-    const tracingOrigins = ['localhost', /^\//];
-    const releaseVersion = window.public_config.RELEASE_VERSION
-      ? `tooljet-${window.public_config.RELEASE_VERSION}`
-      : 'toojet';
+    if (window.public_config.APM_VENDOR == 'sentry') {
+      const history = createBrowserHistory();
+      const tooljetServerUrl = window.public_config.TOOLJET_SERVER_URL;
+      const tracingOrigins = ['localhost', /^\//];
+      const releaseVersion = window.public_config.RELEASE_VERSION
+        ? `tooljet-${window.public_config.RELEASE_VERSION}`
+        : 'toojet';
 
-    if (tooljetServerUrl) tracingOrigins.push(tooljetServerUrl);
+      if (tooljetServerUrl) tracingOrigins.push(tooljetServerUrl);
 
-    Sentry.init({
-      dsn: window.public_config.SENTRY_DNS,
-      debug: !!window.public_config.SENTRY_DEBUG,
-      release: releaseVersion,
-      integrations: [
-        new Integrations.BrowserTracing({
-          routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
-          tracingOrigins: tracingOrigins,
-        }),
-      ],
-      tracesSampleRate: 0.5,
-    });
-  }
-});
-
-render(<App></App>, document.getElementById('app'));
+      Sentry.init({
+        dsn: window.public_config.SENTRY_DNS,
+        debug: !!window.public_config.SENTRY_DEBUG,
+        release: releaseVersion,
+        integrations: [
+          new Integrations.BrowserTracing({
+            routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+            tracingOrigins: tracingOrigins,
+          }),
+        ],
+        tracesSampleRate: 0.5,
+      });
+    }
+  })
+  .then(() => render(<App></App>, document.getElementById('app')));
