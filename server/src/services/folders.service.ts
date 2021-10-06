c9b403d2f53a48352719ae6e657ff840ec3a7d10
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-10-07 03:40:17 +0530

    fix folder app count

--- server/src/services/folders.service.ts
+++ server/src/services/folders.service.ts
@@ -32,15 +32,21 @@ export class FoldersService {
   }
 
   async all(user: User): Promise<Folder[]> {
-    return await this.foldersRepository.find({
-      where: {
-        organizationId: user.organizationId,
-      },
-      relations: ['folderApps'],
-      order: {
-        name: 'ASC',
-      },
-    });
+    return createQueryBuilder(Folder, 'folders')
+      .innerJoin('folders.apps', 'apps')
+      .innerJoin('apps.groupPermissions', 'group_permissions')
+      .innerJoin('apps.appGroupPermissions', 'app_group_permissions')
+      .innerJoin(
+        UserGroupPermission,
+        'user_group_permissions',
+        'app_group_permissions.group_permission_id = user_group_permissions.group_permission_id'
+      )
+      .where('folders.organization_id = :organizationId', { organizationId: user.organizationId })
+      .andWhere('user_group_permissions.user_id = :userId', { userId: user.id })
+      .andWhere('app_group_permissions.read = :value', { value: true })
+      .orWhere('apps.is_public = :value', { value: true })
+      .loadRelationCountAndMap('folders.count', 'folders.apps')
+      .getMany();
   }
 
   async findOne(folderId: string): Promise<Folder> {
@@ -48,15 +54,38 @@ export class FoldersService {
   }
 
   async userAppCount(user: User, folder: Folder) {
-    const result = await this.foldersRepository
-      .createQueryBuilder('folder')
-      .where('id = :id', { id: folder.id })
-      .loadRelationCountAndMap('folder.appCount', 'folder.apps', 'apps', (qb) =>
-        qb.andWhere('apps.user_id = :user_id', { user_id: user.id })
-      )
-      .getMany();
+    if (await this.usersService.hasGroup(user, 'admin')) {
+      const result = await this.foldersRepository
+        .createQueryBuilder('folder')
+        .where('id = :id', { id: folder.id })
+        .loadRelationCountAndMap('folder.appCount', 'folder.apps', 'apps', (qb) =>
+          qb.andWhere('apps.user_id = :user_id', { user_id: user.id })
+        )
+        .getMany();
 
-    return result[0].appCount;
+      return result[0].appCount;
+    } else {
+      const folderApps = await this.folderAppsRepository.find({
+        where: {
+          folderId: folder.id,
+        },
+      });
+      const folderAppIds = folderApps.map((folderApp) => folderApp.appId);
+
+      return createQueryBuilder(App, 'apps')
+        .innerJoin('apps.groupPermissions', 'group_permissions')
+        .innerJoin('apps.appGroupPermissions', 'app_group_permissions')
+        .innerJoin(
+          UserGroupPermission,
+          'user_group_permissions',
+          'app_group_permissions.group_permission_id = user_group_permissions.group_permission_id'
+        )
+        .where('user_group_permissions.user_id = :userId', { userId: user.id })
+        .andWhere('app_group_permissions.read = :value', { value: true })
+        .orWhere('apps.is_public = :value', { value: true })
+        .andWhere('app_group_permissions.app_id IN(:...folderAppIds)', { folderAppIds })
+        .getCount();
+    }
   }
 
   async getAppsFor(user: User, folder: Folder, page: number): Promise<App[]> {
@@ -67,7 +96,6 @@ export class FoldersService {
     });
     const folderAppIds = folderApps.map((folderApp) => folderApp.appId);
 
-    console.log(await this.usersService.hasGroup(user, 'admin'));
     if (await this.usersService.hasGroup(user, 'admin')) {
       const apps = await this.appsRepository.findByIds(folderAppIds, {
         where: {
@@ -96,8 +124,8 @@ export class FoldersService {
         )
         .where('user_group_permissions.user_id = :userId', { userId: user.id })
         .andWhere('app_group_permissions.read = :value', { value: true })
-        .andWhere('app_group_permissions.app_id IN(:...folderAppIds) ', { folderAppIds })
         .orWhere('apps.is_public = :value', { value: true })
+        .andWhere('app_group_permissions.app_id IN(:...folderAppIds)', { folderAppIds })
         .take(10)
         .skip(10 * (page - 1))
         // .orderBy('apps.created_at', 'DESC')
