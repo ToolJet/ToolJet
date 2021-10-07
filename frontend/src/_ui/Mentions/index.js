40d105b26576d7590acf2bba154eff594a4bca5a
gandharv <gandharvkumargarg@gmail.com>
2021-10-07 12:37:21 +0530

    feat: add user mentions, emoji support

--- frontend/src/_ui/Mentions/index.js
+++ frontend/src/_ui/Mentions/index.js
@@ -1,64 +1,166 @@
 import React from 'react';
-import cx from 'classnames';
+// import cx from 'classnames';
+import { organizationService } from '@/_services';
 
-import Textarea from '@/_ui/Textarea';
+import { MentionsInput, Mention } from 'react-mentions';
 
-import usePopover from '@/_hooks/use-popover';
+// import Textarea from '@/_ui/Textarea';
+
+// import usePopover from '@/_hooks/use-popover';
 
 const Mentions = ({ value, setValue, placeholder }) => {
-  const [open, trigger, content, setOpen] = usePopover(false);
-  const handleChange = (e) => {
-    e.stopPropagation();
-    if (e.target.value.includes('@')) {
-      setOpen(true);
-    }
-    setValue(e.target.value);
+  const [emojis, setEmojis] = React.useState([]);
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      'https://gist.githubusercontent.com/oliveratgithub/0bf11a9aff0d6da7b46f1490f86a71eb/raw/d8e4b78cfe66862cf3809443c1dba017f37b61db/emojis.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        setEmojis(jsonData.emojis);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    organizationService.getUsers(null).then((data) => setUsers(data.users));
+  }, []);
+
+  const queryUsers = (query) => {
+    if (query.length === 0) return;
+
+    const matches = users
+      .filter((user) => {
+        return user.name.indexOf(query.toLowerCase()) > -1;
+      })
+      .slice(0, 10);
+    return matches.map(({ user }) => ({ id: user }));
   };
 
-  let conditionalProps = {};
+  const queryEmojis = (query) => {
+    if (query.length === 0) return;
 
-  if (open) {
-    conditionalProps = { ...trigger };
-  }
+    const matches = emojis
+      .filter((emoji) => {
+        return emoji.name.indexOf(query.toLowerCase()) > -1;
+      })
+      .slice(0, 10);
+    return matches.map(({ emoji }) => ({ id: emoji }));
+  };
   return (
-    <>
-      <Textarea
-        {...conditionalProps}
-        value={value}
-        onChange={handleChange}
-        rows="1"
-        className="w-full form-control"
-        placeholder={placeholder}
+    <MentionsInput
+      style={{
+        control: {
+          fontSize: 16,
+          lineHeight: 1.2,
+          minHeight: 40,
+        },
+
+        highlighter: {
+          padding: 9,
+          border: '1px solid transparent',
+        },
+
+        input: {
+          fontSize: 16,
+          lineHeight: 1.2,
+          padding: 9,
+          border: '1px solid silver',
+        },
+
+        suggestions: {
+          list: {
+            backgroundColor: 'white',
+            border: '1px solid rgba(0,0,0,0.15)',
+            fontSize: 16,
+          },
+
+          item: {
+            padding: '5px 15px',
+            borderBottom: '1px solid rgba(0,0,0,0.15)',
+
+            '&focused': {
+              backgroundColor: '#cee4e5',
+            },
+          },
+        },
+      }}
+      value={value}
+      onChange={(e, value) => setValue(value)}
+      placeholder={placeholder}
+    >
+      <Mention
+        trigger="@"
+        regex={/@(\S+)/}
+        displayTransform={(login) => `@${login}`}
+        markup="@__id__"
+        data={queryUsers}
+        style={{
+          backgroundColor: '#cee4e5',
+        }}
+        appendSpaceOnAdd
       />
-      <div
-        {...content}
-        className={cx('card popover mentions-popover', {
-          show: open,
-          hide: !open,
-        })}
-      >
-        <div className="list-group list-group-flush list-group-hoverable">
-          <div className="list-group-item">
-            <div className="row align-items-center">
-              <div className="col-auto">
-                <a href="#">
-                  <span className="avatar">JL</span>
-                </a>
-              </div>
-              <div className="col text-truncate">
-                <a href="#" className="text-body d-block">
-                  Jeffie Lewzey
-                </a>
-                <small className="d-block text-muted text-truncate mt-n1">
-                  justify-content:between ⇒ justify-content:space-between (#29734)
-                </small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      <Mention trigger=":" markup="__id__" regex={/($a)/} data={queryEmojis} appendSpaceOnAdd />
+    </MentionsInput>
   );
 };
+
+// const Mentions = ({ value, setValue, placeholder }) => {
+//   const [open, trigger, content, setOpen] = usePopover(false);
+//   const handleChange = (e) => {
+//     e.stopPropagation();
+//     if (e.target.value.includes('@')) {
+//       setOpen(true);
+//     }
+//     setValue(e.target.value);
+//   };
+
+//   let conditionalProps = {};
+
+//   if (open) {
+//     conditionalProps = { ...trigger };
+//   }
+//   return (
+//     <>
+//       <Textarea
+//         {...conditionalProps}
+//         value={value}
+//         onChange={handleChange}
+//         rows="1"
+//         className="w-full form-control"
+//         placeholder={placeholder}
+//       />
+//       <div
+//         {...content}
+//         className={cx('card popover mentions-popover', {
+//           show: open,
+//           hide: !open,
+//         })}
+//       >
+//         <div className="list-group list-group-flush list-group-hoverable">
+//           <div className="list-group-item">
+//             <div className="row align-items-center">
+//               <div className="col-auto">
+//                 <a href="#">
+//                   <span className="avatar">JL</span>
+//                 </a>
+//               </div>
+//               <div className="col text-truncate">
+//                 <a href="#" className="text-body d-block">
+//                   Jeffie Lewzey
+//                 </a>
+//                 <small className="d-block text-muted text-truncate mt-n1">
+//                   justify-content:between ⇒ justify-content:space-between (#29734)
+//                 </small>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
 export default Mentions;
--- frontend/src/_ui/Textarea/index.js
+++ frontend/src/_ui/Textarea/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Textarea = ({ ...props }) => (
-  <textarea data-bs-toggle="autosize" {...props} />
-);
+const Textarea = ({ ...props }) => <textarea data-bs-toggle="autosize" {...props} />;
 
 export default Textarea;
