a6978810bfab7310bc5b8bd0c3392b797adbd4b0
Sumit Sawant <mr.sumitsawant@gmail.com>
2021-10-14 00:49:13 +0530

    Added Email to commit

--- frontend/src/ManageOrgUsers/ManageOrgUsers.jsx
+++ frontend/src/ManageOrgUsers/ManageOrgUsers.jsx
@@ -106,6 +106,7 @@ class ManageOrgUsers extends React.Component {
 
     if (this.handleValidation()) {
       let fields = {};
+
       Object.keys(fields).forEach((key) => {
         fields[key] = '';
       });
