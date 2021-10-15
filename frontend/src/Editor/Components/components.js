a2b6327804c785a6800982e2ba5fed3fe4c2f9c9
r.vanheerde@arcady.nl <r.vanheerde@arcady.nl>
2021-10-15 16:28:08 +0200

    Added default value of '1' to the dropdown

--- frontend/src/Editor/Components/components.js
+++ frontend/src/Editor/Components/components.js
@@ -774,7 +774,7 @@ export const componentTypes = [
       },
       properties: {
         label: { value: 'Select' },
-        value: { value: '' },
+        value: { value: '1' },
         values: { value: '{{[1,2,3]}}' },
         display_values: { value: '{{["one", "two", "three"]}}' },
         visible: { value: true },
