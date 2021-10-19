ec6441d8cac52ecddbdfec57c6fcabccdaa7046a
arpitnath <arpitnath42@gmail.com>
2021-10-19 11:51:39 +0530

    revert/fix default value for dropdown

--- frontend/src/Editor/Components/components.js
+++ frontend/src/Editor/Components/components.js
@@ -777,7 +777,7 @@ export const componentTypes = [
       },
       properties: {
         label: { value: 'Select' },
-        value: { value: '' },
+        value: { value: '{{2}}' },
         values: { value: '{{[1,2,3]}}' },
         display_values: { value: '{{["one", "two", "three"]}}' },
         visible: { value: true },
