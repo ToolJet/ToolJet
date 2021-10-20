786b1493bd3eed6faf45b3af4ba42ae418b87ff1
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-10-20 17:28:04 +0530

    backfill app_create permission as true for admin group

---
+++ server/migrations/1634729050892-BackfillAppCreatePermissionsAsTruthyForAdminGroup.ts
@@ -0,0 +1,28 @@
+import { EntityManager, In, MigrationInterface, QueryRunner } from "typeorm";
+import { GroupPermission } from "../src/entities/group_permission.entity";
+
+export class BackfillAppCreatePermissionsAsTruthyForAdminGroup1634729050892
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    const GroupPermissionRepostory =
+      entityManager.getRepository(GroupPermission);
+
+    await GroupPermissionRepostory.update(
+      { group: "admin" },
+      { app_create: true }
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    const GroupPermissionRepostory =
+      entityManager.getRepository(GroupPermission);
+
+    await GroupPermissionRepostory.update(
+      { group: "admin" },
+      { app_create: false }
+    );
+  }
+}
