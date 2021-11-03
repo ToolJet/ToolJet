8f7be4aa230deef992ade6495da109c6ec8107ea
SAI SUMANTH KUMAR <saisumanthkumar95509@gmail.com>
2021-11-03 14:12:25 +0530

    Update frontend/src/Editor/LeftSidebar/SidebarComment.jsx
    Co-authored-by: Sherfin Shamsudeen <sherfin94@gmail.com>

--- frontend/src/Editor/LeftSidebar/SidebarComment.jsx
+++ frontend/src/Editor/LeftSidebar/SidebarComment.jsx
@@ -7,7 +7,7 @@ export const LeftSidebarComment = ({ toggleComments, appVersionsId }) => {
   return (
     <LeftSidebarItem
       commentBadge
-      tip={appVersionsId ? 'toggle comments' : 'At least one version of the app should be deployed'}
+      tip={appVersionsId ? 'toggle comments' : 'Comments section will be available once you save this application'}
       icon={`comments`}
       className={cx(`left-sidebar-item sidebar-zoom position-relative`, {
         disabled: !appVersionsId,
