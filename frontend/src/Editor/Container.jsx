ed062716e025924e11bd021a4858bc2f2a98b998
navaneeth <navaneethpk@outlook.com>
2021-11-04 15:33:03 +0530

    Remove initials from comment cursor

--- frontend/src/Editor/Container.jsx
+++ frontend/src/Editor/Container.jsx
@@ -362,7 +362,7 @@ export const Container = ({
   if (showComments) {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const currentUserInitials = `${currentUser.first_name?.charAt(0)}${currentUser.last_name?.charAt(0)}`;
-    styles.cursor = `url("data:image/svg+xml,%3Csvg width='34' height='34' viewBox='0 0 34 34' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='17' cy='17' r='15.25' fill='white' stroke='%23FCAA0D' stroke-width='2.5' opacity='0.5' /%3E%3Ctext x='10' y='20' fill='%23000' opacity='0.5' font-family='inherit' font-size='11.2' font-weight='500' color='%23656d77'%3E${currentUserInitials}%3C/text%3E%3C/svg%3E%0A"), text`;
+    styles.cursor = `url("data:image/svg+xml,%3Csvg width='34' height='34' viewBox='0 0 34 34' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='17' cy='17' r='15.25' fill='white' stroke='%23FCAA0D' stroke-width='2.5' opacity='0.5' /%3E%3Ctext x='10' y='20' fill='%23000' opacity='0.5' font-family='inherit' font-size='11.2' font-weight='500' color='%23656d77'%3E%3C/text%3E%3C/svg%3E%0A"), text`;
   }
 
   return (
