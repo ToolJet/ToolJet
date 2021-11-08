8301ce1f6549f16d4b6456501bfad26d84608162
gandharv <gandharvkumargarg@gmail.com>
2021-11-08 19:18:26 +0530

    api: dataquery - add dynamodb api schema

--- frontend/src/Editor/QueryManager/QueryEditors/index.js
+++ frontend/src/Editor/QueryManager/QueryEditors/index.js
@@ -12,7 +12,7 @@ import { Googlesheets } from './Googlesheets';
 import { Elasticsearch } from './Elasticsearch';
 import { Slack } from './Slack';
 import { Mongodb } from './Mongodb';
-import { Dynamodb } from './Dynamodb';
+// import { Dynamodb } from './Dynamodb';
 // import { Airtable } from './Airtable';
 // import { Graphql } from './Graphql';
 // import { Mssql } from './Mssql';
@@ -23,11 +23,13 @@ import MssqlSchema from './Mssql.schema.json';
 import GraphqlSchema from './Graphql.schema.json';
 import RedisSchema from './Redis.schema.json';
 import AirtableSchema from './Airtable.schema.json';
+import DynamodbSchema from './Dynamodb.schema.json';
 const Mysql = ({ ...rest }) => <DynamicForm schema={MysqlSchema} {...rest} />;
 const Mssql = ({ ...rest }) => <DynamicForm schema={MssqlSchema} {...rest} />;
 const Graphql = ({ ...rest }) => <DynamicForm schema={GraphqlSchema} {...rest} />;
 const Redis = ({ ...rest }) => <DynamicForm schema={RedisSchema} {...rest} />;
 const Airtable = ({ ...rest }) => <DynamicForm schema={AirtableSchema} {...rest} />;
+const Dynamodb = ({ ...rest }) => <DynamicForm schema={DynamodbSchema} {...rest} />;
 
 export const allSources = {
   Restapi,
