e492d1a5c2f6c5bf76e59262a466a674fa1df1b0
gandharv <gandharvkumargarg@gmail.com>
2021-11-08 19:07:35 +0530

    api: dataquery - add airtable api schema

--- frontend/src/Editor/QueryManager/QueryEditors/index.js
+++ frontend/src/Editor/QueryManager/QueryEditors/index.js
@@ -13,7 +13,7 @@ import { Elasticsearch } from './Elasticsearch';
 import { Slack } from './Slack';
 import { Mongodb } from './Mongodb';
 import { Dynamodb } from './Dynamodb';
-import { Airtable } from './Airtable';
+// import { Airtable } from './Airtable';
 // import { Graphql } from './Graphql';
 // import { Mssql } from './Mssql';
 import { S3 } from './S3';
@@ -22,10 +22,12 @@ import MysqlSchema from './Mysql.schema.json';
 import MssqlSchema from './Mssql.schema.json';
 import GraphqlSchema from './Graphql.schema.json';
 import RedisSchema from './Redis.schema.json';
+import AirtableSchema from './Airtable.schema.json';
 const Mysql = ({ ...rest }) => <DynamicForm schema={MysqlSchema} {...rest} />;
 const Mssql = ({ ...rest }) => <DynamicForm schema={MssqlSchema} {...rest} />;
 const Graphql = ({ ...rest }) => <DynamicForm schema={GraphqlSchema} {...rest} />;
 const Redis = ({ ...rest }) => <DynamicForm schema={RedisSchema} {...rest} />;
+const Airtable = ({ ...rest }) => <DynamicForm schema={AirtableSchema} {...rest} />;
 
 export const allSources = {
   Restapi,
