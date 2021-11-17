1750dc73a64134391aedb940db90b4b589205434
Gandharv <gandharvkumargarg@gmail.com>
2021-11-17 22:59:36 +0530

    aadd delete shortcut when a widget is in selectable mode (#1418)
    * feat: add delete shortcut when a widget is in selectable mode
    
    * feat: ignore key events if the target element is input, textarea, iscontenteditable

--- frontend/src/_hooks/use-shortcuts.jsx
+++ frontend/src/_hooks/use-shortcuts.jsx
@@ -1,14 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-// https://keycode.info/ this cheatsheet gives info about key presses
+// https://keycode.info/ this cheat sheet gives info about key presses
 
 import { useEffect, useCallback } from 'react';
-const setsEqual = (setA, setB) => setA.size === setB.size && !Array.from(setA).some((v) => !setB.has(v));
+
 const useShortcuts = (keys, callback, deps) => {
+  const ignoreTags = ['input', 'select', 'textarea'];
+  const ignoreEventsCondition = (event) => {
+    const { target } = event;
+
+    if (target && target.tagName) {
+      const tagName = target.tagName.toLowerCase();
+
+      return ignoreTags.includes(tagName) || target.isContentEditable;
+    } else {
+      return false;
+    }
+  };
+  const setsEqual = (setA, setB) => setA.size === setB.size && !Array.from(setA).some((v) => !setB.has(v));
   const memoizedCallback = useCallback(callback, deps || []);
   const targetKeys = new Set(keys.map((key) => key.toLowerCase()));
   const pressedKeys = new Set();
   function onKeyPressed(event) {
+    if (ignoreEventsCondition(event)) return;
     pressedKeys.add(event.key.toLowerCase());
     if (setsEqual(pressedKeys, targetKeys)) {
       memoizedCallback();
