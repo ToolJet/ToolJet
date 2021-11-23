40a95fa77b86e35d85bcf4f169ffa70d1907fd8f
navaneeth <navaneethpk@outlook.com>
2021-11-23 15:33:38 +0530

    Fix resize

--- frontend/src/Editor/DraggableBox.jsx
+++ frontend/src/Editor/DraggableBox.jsx
@@ -207,6 +207,7 @@ export const DraggableBox = function DraggableBox({
               setDragging(false)
               onDragStop(e, id, direction, currentLayout, currentLayoutOptions)
             }}
+            cancel={`div.table-responsive.jet-data-table > table > thead`}
             onDragStart={(e) => e.stopPropagation()}
             enableResizing={mode === 'edit'}
             onResizeStop={(e, direction, ref, d, position) => {
