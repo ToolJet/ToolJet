e76c7901351648163d569c4dce4f31651423cdc4
navaneeth <navaneethpk@outlook.com>
2021-11-23 15:45:45 +0530

    Fix

--- frontend/src/Editor/DraggableBox.jsx
+++ frontend/src/Editor/DraggableBox.jsx
@@ -207,7 +207,7 @@ export const DraggableBox = function DraggableBox({
               setDragging(false)
               onDragStop(e, id, direction, currentLayout, currentLayoutOptions)
             }}
-            cancel={`div.table-responsive.jet-data-table > table > thead`}
+            cancel={`div.table-responsive.jet-data-table`}
             onDragStart={(e) => e.stopPropagation()}
             enableResizing={mode === 'edit'}
             onResizeStop={(e, direction, ref, d, position) => {
