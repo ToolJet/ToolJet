6b1bd3a40ef203fa20a9fb21fde7963349a7cb45
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-11-24 14:13:31 +0530

    Use papaparse lib for generating csv file

--- frontend/src/_lib/generate-csv.js
+++ frontend/src/_lib/generate-csv.js
@@ -1,14 +1,5 @@
-function generateCsvDataLine(record, keys) {
-  return keys.map((key) => record[key]).join(',');
-}
+import Papa from 'papaparse';
 
 export default function generateCSV(records) {
-  if (records.length === 0) return '';
-
-  const keys = Object.keys(records[0]);
-
-  const csvHeaderLine = keys.join(',');
-  const csvDataLines = records.map((record) => generateCsvDataLine(record, keys)).join('\n');
-
-  return `${csvHeaderLine}\n${csvDataLines}`;
+  return Papa.unparse(records);
 }
