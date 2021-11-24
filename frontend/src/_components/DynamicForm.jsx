3161a1aeefcc95147dfa291b2a7de8eddf571577
gandharv <gandharvkumargarg@gmail.com>
2021-11-24 16:07:40 +0530

    fix rest api defaults

--- frontend/src/_components/DynamicForm.jsx
+++ frontend/src/_components/DynamicForm.jsx
@@ -107,17 +107,17 @@ const DynamicForm = ({
         };
       case 'react-component-oauth-authentication':
         return {
-          grant_type: options.grant_type.value,
-          auth_type: options.auth_type.value,
-          add_token_to: options.add_token_to.value,
-          header_prefix: options.header_prefix.value,
-          access_token_url: options.access_token_url.value,
-          client_id: options.client_id.value,
-          client_secret: options.client_secret.value,
-          client_auth: options.client_auth.value,
-          scopes: options.scopes.value,
-          auth_url: options.auth_url.value,
-          custom_auth_params: options.custom_auth_params.value,
+          grant_type: options.grant_type?.value,
+          auth_type: options.auth_type?.value,
+          add_token_to: options.add_token_to?.value,
+          header_prefix: options.header_prefix?.value,
+          access_token_url: options.access_token_url?.value,
+          client_id: options.client_id?.value,
+          client_secret: options.client_secret?.value,
+          client_auth: options.client_auth?.value,
+          scopes: options.scopes?.value,
+          auth_url: options.auth_url?.value,
+          custom_auth_params: options.custom_auth_params?.value,
           optionchanged,
         };
       case 'react-component-google-sheets':
