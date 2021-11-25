2b308b2f3a6cc69e0c9986f86a5b52186d77abb8
Sherfin Shamsudeen <sherfin@tooljet.io>
2021-11-25 19:34:47 +0530

    Fix width for iFrame and PasswordInput (#1463)

--- frontend/src/Editor/Components/components.js
+++ frontend/src/Editor/Components/components.js
@@ -363,7 +363,7 @@ export const componentTypes = [
     description: 'Password input field for forms',
     component: 'PasswordInput',
     defaultSize: {
-      width: 210,
+      width: 4,
       height: 30,
     },
     others: {
@@ -1285,7 +1285,7 @@ export const componentTypes = [
     displayName: 'Iframe',
     description: 'Display an Iframe',
     defaultSize: {
-      width: 560,
+      width: 10,
       height: 310,
     },
     component: 'IFrame',
