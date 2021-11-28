4ff085c58f6363d84e98cc8f0946132db4900f06
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-11-28 21:45:33 +0530

    Arrange app cards in 3x5 grid on dashboard

---
+++ frontend/src/HomePage/AppCard.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { AppMenu } from './AppMenu';
+import { Link } from 'react-router-dom';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import { renderTooltip } from '@/_helpers/appUtils';
+import moment from 'moment';
+
+export default function AppCard(props) {
+  const { app } = props;
+
+  return (
+    <div className="app-card m-2 p-2" key={app.id}>
+      <span className="app-title mb-3">{app.name}</span>
+      <br />
+      <small className="pt-2 app-description">
+        created {moment(app.created_at).fromNow(true)} ago by {app.user?.first_name} {app.user?.last_name}{' '}
+      </small>
+      <div className="text-muted">
+        {!props.isLoading && props.canUpdateApp(app) && (
+          <Link to={`/apps/${app.id}`} className="d-none d-lg-inline">
+            <OverlayTrigger
+              placement="top"
+              overlay={(props) =>
+                renderTooltip({
+                  props,
+                  text: 'Open in app builder',
+                })
+              }
+            >
+              <span className="badge bg-green-lt">Edit</span>
+            </OverlayTrigger>
+          </Link>
+        )}
+        <Link
+          to={app?.current_version_id ? `/applications/${app.slug}` : ''}
+          target={app?.current_version_id ? '_blank' : ''}
+        >
+          {!props.darkMode && (
+            <OverlayTrigger
+              placement="top"
+              overlay={(props) =>
+                renderTooltip({
+                  props,
+                  text:
+                    app?.current_version_id === null ? 'App does not have a deployed version' : 'Open in app viewer',
+                })
+              }
+            >
+              {
+                <span className={`${app?.current_version_id ? 'badge bg-blue-lt mx-2 ' : 'badge bg-light-grey mx-2'}`}>
+                  launch{' '}
+                </span>
+              }
+            </OverlayTrigger>
+          )}
+          {props.darkMode && (
+            <OverlayTrigger
+              placement="top"
+              overlay={(props) =>
+                renderTooltip({
+                  props,
+                  text:
+                    app?.current_version_id === null ? 'App does not have a deployed version' : 'Open in app viewer',
+                })
+              }
+            >
+              {
+                <span
+                  className={`${
+                    app?.current_version_id === null ? 'badge launch-btn mx-2 ' : 'badge launch-btn bg-azure-lt mx-2'
+                  }`}
+                  style={{
+                    filter: app?.current_version_id === null ? 'brightness(0.3)' : 'brightness(1) invert(1)',
+                  }}
+                >
+                  launch{' '}
+                </span>
+              }
+            </OverlayTrigger>
+          )}
+        </Link>
+
+        {(props.canCreateApp(app) || props.canDeleteApp(app)) && (
+          <AppMenu
+            app={app}
+            canCreateApp={props.canCreateApp()}
+            canDeleteApp={props.canDeleteApp(app)}
+            folders={props.folders}
+            foldersChanged={props.foldersChanged}
+            deleteApp={() => props.deleteApp(app)}
+            cloneApp={() => props.cloneApp(app)}
+            exportApp={() => props.exportApp(app)}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
--- frontend/src/HomePage/AppList.jsx
+++ frontend/src/HomePage/AppList.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { AppMenu } from './AppMenu';
-import { Link } from 'react-router-dom';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import { renderTooltip } from '@/_helpers/appUtils';
-import moment from 'moment';
-import _ from 'lodash';
+import AppCard from './AppCard';
 
 const AppList = (props) => {
   return (
@@ -31,106 +26,28 @@ const AppList = (props) => {
 
       {props.meta.total_count > 0 && (
         <div className="container px-0">
-          {_.chunk(props.apps, 5).map((fiveApps, index) => (
-            <div className="row" key={index}>
-              {fiveApps.map((app) => (
-                <div className="app-card col m-2 p-2" key={app.id}>
-                  <span className="app-title mb-3">{app.name}</span>
-                  <br />
-                  <small className="pt-2 app-description">
-                    created {moment(app.created_at).fromNow(true)} ago by {app.user?.first_name} {app.user?.last_name}{' '}
-                  </small>
-                  <div className="text-muted">
-                    {!props.isLoading && props.canUpdateApp(app) && (
-                      <Link to={`/apps/${app.id}`} className="d-none d-lg-inline">
-                        <OverlayTrigger
-                          placement="top"
-                          overlay={(props) =>
-                            renderTooltip({
-                              props,
-                              text: 'Open in app builder',
-                            })
-                          }
-                        >
-                          <span className="badge bg-green-lt">Edit</span>
-                        </OverlayTrigger>
-                      </Link>
-                    )}
-                    <Link
-                      to={app?.current_version_id ? `/applications/${app.slug}` : ''}
-                      target={app?.current_version_id ? '_blank' : ''}
-                    >
-                      {!props.darkMode && (
-                        <OverlayTrigger
-                          placement="top"
-                          overlay={(props) =>
-                            renderTooltip({
-                              props,
-                              text:
-                                app?.current_version_id === null
-                                  ? 'App does not have a deployed version'
-                                  : 'Open in app viewer',
-                            })
-                          }
-                        >
-                          {
-                            <span
-                              className={`${
-                                app?.current_version_id ? 'badge bg-blue-lt mx-2 ' : 'badge bg-light-grey mx-2'
-                              }`}
-                            >
-                              launch{' '}
-                            </span>
-                          }
-                        </OverlayTrigger>
-                      )}
-                      {props.darkMode && (
-                        <OverlayTrigger
-                          placement="top"
-                          overlay={(props) =>
-                            renderTooltip({
-                              props,
-                              text:
-                                app?.current_version_id === null
-                                  ? 'App does not have a deployed version'
-                                  : 'Open in app viewer',
-                            })
-                          }
-                        >
-                          {
-                            <span
-                              className={`${
-                                app?.current_version_id === null
-                                  ? 'badge launch-btn mx-2 '
-                                  : 'badge launch-btn bg-azure-lt mx-2'
-                              }`}
-                              style={{
-                                filter:
-                                  app?.current_version_id === null ? 'brightness(0.3)' : 'brightness(1) invert(1)',
-                              }}
-                            >
-                              launch{' '}
-                            </span>
-                          }
-                        </OverlayTrigger>
-                      )}
-                    </Link>
-
-                    {(props.canCreateApp(app) || props.canDeleteApp(app)) && (
-                      <AppMenu
-                        app={app}
-                        canCreateApp={props.canCreateApp()}
-                        canDeleteApp={props.canDeleteApp(app)}
-                        folders={props.folders}
-                        foldersChanged={props.foldersChanged}
-                        deleteApp={() => props.deleteApp(app)}
-                        cloneApp={() => props.cloneApp(app)}
-                        exportApp={() => props.exportApp(app)}
-                      />
-                    )}
+          {Array.from(Array(3)).map((_, rowIndex) => (
+            <div className="row" key={rowIndex}>
+              {Array.from(Array(5)).map((_, index) =>
+                props.apps[rowIndex * 3 + index] ? (
+                  <div className="col">
+                    <AppCard
+                      app={props.apps[rowIndex * 3 + index]}
+                      key={props.apps[rowIndex * 3 + index].id}
+                      canCreateApp={props.canCreateApp}
+                      canDeleteApp={props.canDeleteApp}
+                      canUpdateApp={props.canUpdateApp}
+                      folders={props.folders}
+                      foldersChanged={props.foldersChanged}
+                      deleteApp={props.deleteApp}
+                      cloneApp={props.cloneApp}
+                      exportApp={props.exportApp}
+                    />
                   </div>
-                </div>
-              ))}
+                ) : (
+                  <div className="col"></div>
+                )
+              )}
             </div>
           ))}
         </div>
