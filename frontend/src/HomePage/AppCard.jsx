7476b72253d1a221a36e7f6242fd689af4eb7a69
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-11-29 22:48:27 +0530

    Replace edit and launch buttons with new design on app card

--- frontend/src/HomePage/AppCard.jsx
+++ frontend/src/HomePage/AppCard.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { AppMenu } from './AppMenu';
-// import { Link } from 'react-router-dom';
-// import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-// import { renderTooltip } from '@/_helpers/appUtils';
+import { history } from '@/_helpers';
 import moment from 'moment';
 
 export default function AppCard(props) {
@@ -40,71 +38,49 @@ export default function AppCard(props) {
       <small className="pt-2 app-description">
         created {moment(app.created_at).fromNow(true)} ago by {app.user?.first_name} {app.user?.last_name}{' '}
       </small>
-      <div className="text-muted">
-        {/* {!props.isLoading && props.canUpdateApp(app) && (
-          <Link to={`/apps/${app.id}`} className="d-none d-lg-inline">
-            <OverlayTrigger
-              placement="top"
-              overlay={(props) =>
-                renderTooltip({
-                  props,
-                  text: 'Open in app builder',
-                })
-              }
-            >
-              <span className="badge bg-green-lt">Edit</span>
-            </OverlayTrigger>
-          </Link>
-        )} */}
-        {/* <Link
-          to={app?.current_version_id ? `/applications/${app.slug}` : ''}
-          target={app?.current_version_id ? '_blank' : ''}
-        >
-          {!props.darkMode && (
-            <OverlayTrigger
-              placement="top"
-              overlay={(props) =>
-                renderTooltip({
-                  props,
-                  text:
-                    app?.current_version_id === null ? 'App does not have a deployed version' : 'Open in app viewer',
-                })
-              }
-            >
-              {
-                <span className={`${app?.current_version_id ? 'badge bg-blue-lt mx-2 ' : 'badge bg-light-grey mx-2'}`}>
-                  launch{' '}
-                </span>
-              }
-            </OverlayTrigger>
-          )}
-          {props.darkMode && (
-            <OverlayTrigger
-              placement="top"
-              overlay={(props) =>
-                renderTooltip({
-                  props,
-                  text:
-                    app?.current_version_id === null ? 'App does not have a deployed version' : 'Open in app viewer',
-                })
-              }
-            >
-              {
-                <span
-                  className={`${
-                    app?.current_version_id === null ? 'badge launch-btn mx-2 ' : 'badge launch-btn bg-azure-lt mx-2'
-                  }`}
-                  style={{
-                    filter: app?.current_version_id === null ? 'brightness(0.3)' : 'brightness(1) invert(1)',
+      {focused && (
+        <>
+          <div className="container-fluid d-flex flex-column align-content-center px-0 mt-4">
+            <div className="row">
+              <div className="col-6">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-light edit-button"
+                  style={{ width: '100%' }}
+                  onClick={() => history.push(`/apps/${app.id}`)}
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Open in app builder"
+                >
+                  Edit
+                </button>
+              </div>
+              <div
+                className="col-6"
+                data-bs-toggle="tooltip"
+                data-bs-placement="top"
+                title={app?.current_version_id === null ? 'App does not have a deployed version' : 'Open in app viewer'}
+              >
+                <button
+                  type="button"
+                  className="btn btn-sm btn-primary launch-button"
+                  disabled={app?.current_version_id === null}
+                  style={{ width: '100%' }}
+                  onClick={() => {
+                    if (app?.current_version_id) {
+                      window.open(`/applications/${app.slug}`);
+                    } else {
+                      history.push(app?.current_version_id ? `/applications/${app.slug}` : '');
+                    }
                   }}
                 >
-                  launch{' '}
-                </span>
-              }
-            </OverlayTrigger>
-          )}
-        </Link> */}
-      </div>
+                  Launch
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
