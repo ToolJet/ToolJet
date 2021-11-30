9c616393686934d2a630ad633b1cad65fdd7a5a2
arpitnath <arpitnath42@gmail.com>
2021-11-30 14:32:45 +0530

    number input widget

--- frontend/src/Editor/Components/NumberInput.jsx
+++ frontend/src/Editor/Components/NumberInput.jsx
@@ -1,60 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { resolveReferences, resolveWidgetFieldValue } from '@/_helpers/utils';
+import React, { useEffect } from 'react';
 
-export const NumberInput = function NumberInput({
-  id,
-  height,
-  component,
-  onComponentClick,
-  currentState,
-  onComponentOptionChanged,
-}) {
-  const value = component.definition.properties.value ? component.definition.properties.value.value : '';
-  const [number, setNumber] = useState(value);
-
-  const numberInputProperty = component.definition.properties.value;
-  let newNumber = value;
-  if (numberInputProperty && currentState) {
-    newNumber = resolveReferences(numberInputProperty.value, currentState, '');
-  }
-
+export const NumberInput = function NumberInput({ height, properties, exposedVariables, styles, setExposedVariable }) {
   useEffect(() => {
-    setNumber(parseInt(newNumber));
-    onComponentOptionChanged(component, 'value', parseInt(newNumber));
+    setExposedVariable('value', parseInt(properties.value));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newNumber]);
-
-  const placeholder = component.definition.properties.placeholder.value;
-  const widgetVisibility = component.definition.styles?.visibility?.value ?? true;
-  const disabledState = component.definition.styles?.disabledState?.value ?? false;
-
-  const parsedDisabledState =
-    typeof disabledState !== 'boolean' ? resolveWidgetFieldValue(disabledState, currentState) : disabledState;
-
-  let parsedWidgetVisibility = widgetVisibility;
-
-  try {
-    parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
-  } catch (err) {
-    console.log(err);
-  }
+  }, [properties.value]);
 
   return (
     <input
-      disabled={parsedDisabledState}
-      onClick={(event) => {
-        event.stopPropagation();
-        onComponentClick(id, component, event);
-      }}
+      disabled={styles.disabledState}
       onChange={(e) => {
-        setNumber(parseInt(e.target.value));
-        onComponentOptionChanged(component, 'value', parseInt(e.target.value));
+        setExposedVariable('value', parseInt(e.target.value));
       }}
       type="number"
       className="form-control rounded-0"
-      placeholder={placeholder}
-      style={{ height, display: parsedWidgetVisibility ? '' : 'none' }}
-      value={number}
+      placeholder={properties.placeholder}
+      style={{ height, display: styles.visibility ? '' : 'none' }}
+      value={exposedVariables.value}
     />
   );
 };
