e9216c90fcc7a2d5fff1258f9ced786f280556f3
arpitnath <arpitnath42@gmail.com>
2021-11-30 13:52:49 +0530

    text-input widget

--- frontend/src/Editor/Components/TextInput.jsx
+++ frontend/src/Editor/Components/TextInput.jsx
@@ -1,39 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { resolveReferences, resolveWidgetFieldValue } from '@/_helpers/utils';
+import React, { useEffect } from 'react';
 
 export const TextInput = function TextInput({
-  id,
   height,
   component,
-  onComponentClick,
   currentState,
   onComponentOptionChanged,
   validate,
+  properties,
+  exposedVariables,
+  styles,
+  setExposedVariable,
 }) {
-  const placeholder = component.definition.properties.placeholder.value;
-  const widgetVisibility = component.definition.styles?.visibility?.value ?? true;
-  const disabledState = component.definition.styles?.disabledState?.value ?? false;
-
-  const parsedDisabledState =
-    typeof disabledState !== 'boolean' ? resolveWidgetFieldValue(disabledState, currentState) : disabledState;
-
-  let parsedWidgetVisibility = widgetVisibility;
   const value = currentState?.components[component?.name]?.value;
   const currentValidState = currentState?.components[component?.name]?.isValid;
-
-  const [text, setText] = useState(value);
-
-  const textProperty = component.definition.properties.value;
-  let newText = value;
-  if (textProperty && currentState) {
-    newText = resolveReferences(textProperty.value, currentState, '');
-  }
-
-  useEffect(() => {
-    setText(newText);
-    onComponentOptionChanged(component, 'value', newText);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newText]);
 
   const validationData = validate(value);
 
@@ -43,29 +22,27 @@ export const TextInput = function TextInput({
     onComponentOptionChanged(component, 'isValid', isValid);
   }
 
-  try {
-    parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
-  } catch (err) {
-    console.log(err);
-  }
+  useEffect(() => {
+    setExposedVariable('value', properties.value);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [properties.value]);
+
+  useEffect(() => {
+    console.log('isValid', isValid);
+  }, [isValid]);
 
   return (
     <div>
       <input
-        disabled={parsedDisabledState}
-        onClick={(event) => {
-          event.stopPropagation();
-          onComponentClick(id, component, event);
-        }}
+        disabled={styles.disabledState}
         onChange={(e) => {
-          setText(e.target.value);
-          onComponentOptionChanged(component, 'value', e.target.value);
+          setExposedVariable('value', e.target.value);
         }}
         type="text"
         className={`form-control ${!isValid ? 'is-invalid' : ''} validation-without-icon`}
-        placeholder={placeholder}
-        style={{ height, display: parsedWidgetVisibility ? '' : 'none' }}
-        value={text}
+        placeholder={properties.placeholder}
+        style={{ height, display: styles.visibility ? '' : 'none' }}
+        value={exposedVariables.value}
       />
       <div className="invalid-feedback">{validationError}</div>
     </div>
