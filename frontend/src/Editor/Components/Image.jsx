941e0b4c168121b9d4b333bcf18d4fde22279362
arpitnath <arpitnath42@gmail.com>
2021-12-03 00:28:50 +0530

    Revert "new implementation/image"
    This reverts commit 637bd831082c0f32d13941a9d4632ffa872db7ac.

--- frontend/src/Editor/Components/Image.jsx
+++ frontend/src/Editor/Components/Image.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { resolveReferences, resolveWidgetFieldValue } from '@/_helpers/utils';
 import LazyLoad from 'react-lazyload';
 
-export const Image = function Image({ height, properties, styles }) {
-  const source = properties.source;
-  const widgetVisibility = styles.visibility ?? true;
+export const Image = function Image({ id, height, component, onComponentClick, currentState }) {
+  const source = component.definition.properties.source.value;
+  const widgetVisibility = component.definition.styles?.visibility?.value ?? true;
+  const disabledState = component.definition.styles?.disabledState?.value ?? false;
+
+  const parsedDisabledState =
+    typeof disabledState !== 'boolean' ? resolveWidgetFieldValue(disabledState, currentState) : disabledState;
+
+  let data = resolveReferences(source, currentState, null);
+
+  let parsedWidgetVisibility = widgetVisibility;
+
+  try {
+    parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
+  } catch (err) {
+    console.log(err);
+  }
+  if (data === '') data = null;
 
   function Placeholder() {
     return <div className="skeleton-image" style={{ objectFit: 'contain', height }}></div>;
   }
 
   return (
-    <div data-disabled={styles.disabledState} style={{ display: widgetVisibility ? '' : 'none' }}>
+    <div
+      data-disabled={parsedDisabledState}
+      style={{ display: parsedWidgetVisibility ? '' : 'none' }}
+      onClick={(event) => {
+        event.stopPropagation();
+        onComponentClick(id, component, event);
+      }}
+    >
       <LazyLoad height={height} placeholder={<Placeholder />} debounce={500}>
-        <img src={source} height={height} />
+        <img style={{ objectFit: 'contain' }} src={data} height={height} />
       </LazyLoad>
     </div>
   );
