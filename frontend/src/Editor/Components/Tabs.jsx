2fee5884f5a1f5d5bca760fa2da3c2bf13781712
arpitnath <arpitnath42@gmail.com>
2021-12-03 10:20:21 +0530

    new implementation/tabs widget

--- frontend/src/Editor/Components/Tabs.jsx
+++ frontend/src/Editor/Components/Tabs.jsx
@@ -1,53 +1,41 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SubCustomDragLayer } from '../SubCustomDragLayer';
 import { SubContainer } from '../SubContainer';
-import { resolveReferences, resolveWidgetFieldValue } from '@/_helpers/utils';
 
-export const Tabs = function Tabs({ id, component, width, height, containerProps, currentState, removeComponent }) {
-  const widgetVisibility = component.definition.styles?.visibility?.value ?? true;
-  const disabledState = component.definition.styles?.disabledState?.value ?? false;
-  const defaultTab = component.definition.properties.defaultTab.value;
+export const Tabs = function Tabs({
+  id,
+  component,
+  width,
+  height,
+  containerProps,
+  removeComponent,
+  properties,
+  styles,
+}) {
+  const { visibility, disabledState, highlightColor } = styles;
 
   // config for tabs. Includes title
-  const tabs = component.definition.properties?.tabs?.value ?? [];
-  let parsedTabs = tabs;
-  parsedTabs = resolveWidgetFieldValue(parsedTabs, currentState);
-
-  // set index as id if id is not provided
-  parsedTabs = parsedTabs.map((parsedTab, index) => ({ ...parsedTab, id: parsedTab.id ? parsedTab.id : index }));
-
-  // Highlight color - for active tab text and border
-  const highlightColor = component.definition.styles?.highlightColor?.value ?? '';
-  let parsedHighlightColor = highlightColor;
-  parsedHighlightColor = resolveWidgetFieldValue(highlightColor, currentState);
-
-  // Default tab
-  let parsedDefaultTab = defaultTab;
-  parsedDefaultTab = resolveWidgetFieldValue(parsedDefaultTab, currentState, 1);
-
-  const parsedDisabledState =
-    typeof disabledState !== 'boolean' ? resolveWidgetFieldValue(disabledState, currentState) : disabledState;
-
-  let parsedWidgetVisibility = widgetVisibility;
-
-  try {
-    parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
-  } catch (err) {
-    console.log(err);
-  }
+  const tabs = properties.tabs ?? [];
 
   const computedStyles = {
     height,
-    display: parsedWidgetVisibility ? 'flex' : 'none',
+    display: visibility ? 'flex' : 'none',
   };
 
   const parentRef = useRef(null);
 
-  const [currentTab, setCurrentTab] = useState(parsedDefaultTab);
+  const [currentTab, setCurrentTab] = useState(() => properties.defaultTab);
+
+  useEffect(() => {
+    if (!currentTab) {
+      setCurrentTab(properties.defaultTab);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [properties.defaultTab]);
 
   return (
     <div
-      data-disabled={parsedDisabledState}
+      data-disabled={disabledState}
       className="jet-tabs card"
       onClick={() => {
         containerProps.onComponentClick(id, component);
@@ -55,14 +43,12 @@ export const Tabs = function Tabs({ id, component, width, height, containerProps
       style={computedStyles}
     >
       <ul className="nav nav-tabs" data-bs-toggle="tabs">
-        {parsedTabs.map((tab) => (
+        {tabs.map((tab) => (
           <li className="nav-item" onClick={() => setCurrentTab(tab.id)} key={tab.id}>
             <a
               className={`nav-link ${currentTab === tab.id ? 'active' : ''}`}
               style={
-                currentTab === tab.id
-                  ? { color: parsedHighlightColor, borderBottom: `1px solid ${parsedHighlightColor}` }
-                  : {}
+                currentTab === tab.id ? { color: highlightColor, borderBottom: `1px solid ${highlightColor}` } : {}
               }
             >
               {tab.title}
