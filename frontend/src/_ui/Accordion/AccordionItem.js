4c63315eaa11766fe1d4966bbde3563ae1173576
Gandharv <gandharvkumargarg@gmail.com>
2021-12-04 23:11:25 +0530

    Split properties and styles for component inspector (#1467)
    * split properties and styles for component inspector
    
    * add accordion for table component
    
    * add accordion to charts.jsx
    
    * add accordion button css
    
    * css changes
    
    * move +add to right + change basic fields to properties
    
    * keep events and layout closed by default
    
    * center empty text
    
    * add empty text for handler events
    
    * css changes
    
    * add margin bottom for table columns+cards
    
    * css fixes
    
    * add spacing below add+ button

---
+++ frontend/src/_ui/Accordion/AccordionItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import cx from 'classnames';
+
+const AccordionItem = ({ open = true, index, title, children }) => {
+  const [show, setShow] = React.useState(open);
+  return (
+    <div className="accordion-item">
+      <h2 onClick={() => setShow(!show)} className="accordion-header" id={`heading-${index}`}>
+        <button
+          className={cx('accordion-button', { collapsed: !show })}
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`collapse-${index}`}
+          aria-expanded="false"
+        >
+          {title}
+        </button>
+      </h2>
+      <div
+        id={`collapse-${index}`}
+        className={cx('accordion-collapse collapse', { show })}
+        data-bs-parent="#accordion-example"
+      >
+        <div className="accordion-body">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionItem;
