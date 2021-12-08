3477f7ff15bb4d074df3552546133762be00622e
Navaneeth Pk <navaneeth@tooljet.io>
2021-12-08 13:03:08 +0530

    Support for running JavaScript as a query ( client-side execution ) (#1507)
    * Custom js feature
    
    * Disable transformations
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Icon
    
    * Rename variable OthetSources to OtherSources
    
    * Fix
    
    * Fix
    
    Co-authored-by: Sherfin Shamsudeen <sherfin94@gmail.com>

---
+++ frontend/src/Editor/QueryManager/QueryEditors/Runjs.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { CodeHinter } from '../../CodeBuilder/CodeHinter';
+import { changeOption } from './utils';
+import { defaults } from 'lodash';
+
+class Runjs extends React.Component {
+  constructor(props) {
+    super(props);
+    const options = defaults({ ...props.options }, { code: '//Type your JavaScript code here' });
+    this.state = {
+      options,
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <CodeHinter
+          currentState={this.props.currentState}
+          initialValue={this.props.options.code}
+          mode="javascript"
+          theme={this.props.darkMode ? 'monokai' : 'base16-light'}
+          lineNumbers={true}
+          height={400}
+          className="query-hinter"
+          ignoreBraces={true}
+          onChange={(value) => changeOption(this, 'code', value)}
+          isMultiLineJs={false}
+          enablePreview={false}
+        />
+      </div>
+    );
+  }
+}
+
+export { Runjs };
