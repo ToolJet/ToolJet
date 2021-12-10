e5a3ef84987f445396ae0878507c948a041c72ce
navaneeth <navaneethpk@outlook.com>
2021-12-11 04:33:35 +0530

    Fix

--- frontend/src/Editor/Box.jsx
+++ frontend/src/Editor/Box.jsx
@@ -156,8 +156,8 @@ export const Box = function Box({
               <center>
                 <div
                   style={{
-                    width: '18px',
-                    height: '18px',
+                    width: '20px',
+                    height: '20px',
                     backgroundSize: 'contain',
                     backgroundImage: `url(/assets/images/icons/widgets/${component.name.toLowerCase()}.svg)`,
                     backgroundRepeat: 'no-repeat',
