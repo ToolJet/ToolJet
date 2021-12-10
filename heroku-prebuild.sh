bdb9fc759cdc6123a78b67f4218588b4c38454f2
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-12-10 16:50:53 +0530

    test env replaced

--- heroku-prebuild.sh
+++ heroku-prebuild.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo $HEROKU_APP_NAME
-if [ -z "$HEROKU_APP_NAME" ]
+if [ $HEROKU_APP_NAME ]
 then
-    export TOOLJET_HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
-    export TOOLJET_SERVER_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
+    TOOLJET_HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
+    TOOLJET_SERVER_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
 fi
 
-echo $TOOLJET_HOST
-# npm install --prefix server && npm run build --prefix server
+npm install --prefix server && npm run build --prefix server
