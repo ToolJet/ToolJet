e62c3a2232c0dbd410c900737e7824940e380591
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-12-10 14:39:29 +0530

    test env injected

--- heroku-predeploy.sh
+++ heroku-predeploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo $HEROKU_APP_NAME
 if [ -z "$HEROKU_APP_NAME" ]
 then
     export TOOLJET_HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
     export TOOLJET_SERVER_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
 fi
 
-npm install --prefix server && npm run build --prefix server
+echo $TOOLJET_HOST
+# npm install --prefix server && npm run build --prefix server
