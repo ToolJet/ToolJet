eda8c933ebdca75cc4ef272dfb2cf178e294a759
arpitnath <arpitnath42@gmail.com>
2021-12-13 22:11:11 +0530

    resolved

--- frontend/src/Editor/Components/Text.jsx
+++ frontend/src/Editor/Components/Text.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { resolveReferences } from '@/_helpers/utils';
 import DOMPurify from 'dompurify';
 
-export const Text = function Text({ height, currentState, properties, styles }) {
+export const Text = function Text({ height, properties, styles }) {
   const [loadingState, setLoadingState] = useState(false);
 
   const { textColor, visibility, disabledState } = styles;
@@ -11,24 +10,11 @@ export const Text = function Text({ height, currentState, properties, styles }) 
 
   useEffect(() => {
     const loadingStateProperty = properties.loadingState;
-    if (loadingStateProperty || currentState) {
+    if (loadingStateProperty) {
       setLoadingState(loadingStateProperty);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentState]);
-
-  let data = text;
-  if (currentState) {
-    const matchedParams = text.match(/\{\{(.*?)\}\}/g);
-
-    if (matchedParams) {
-      for (const param of matchedParams) {
-        const resolvedParam = resolveReferences(param, currentState, '');
-        console.log('resolved param', param, resolvedParam);
-        data = data.replace(param, resolvedParam);
-      }
-    }
-  }
+  }, [properties.loadingState]);
 
   const computedStyles = {
     color,
@@ -39,7 +25,7 @@ export const Text = function Text({ height, currentState, properties, styles }) 
 
   return (
     <div data-disabled={disabledState} className="text-widget" style={computedStyles}>
-      {!loadingState && <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data) }} />}
+      {!loadingState && <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(text) }} />}
       {loadingState === true && (
         <div>
           <div className="skeleton-line w-10"></div>
