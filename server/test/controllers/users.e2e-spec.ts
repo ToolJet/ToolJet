74aec31b32c7f00587e2028d9a1da6af673cfeb5
Akshay <akshaysasidharan93@gmail.com>
2021-12-14 16:32:03 +0530

    Chore: Update user e2e tests (#1556)
    * Update tests
    
    * remove console log
    
    * remove explicit db connection close
    
    * remove reload usage

--- server/test/controllers/users.e2e-spec.ts
+++ server/test/controllers/users.e2e-spec.ts
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { authHeaderForUser, clearDB, createUser, createNestAppInstance } from '../test.helper';
-import { getConnection } from 'typeorm';
+import { getManager } from 'typeorm';
+import { User } from 'src/entities/user.entity';
 
 describe('users controller', () => {
   let app: INestApplication;
@@ -13,7 +13,42 @@ describe('users controller', () => {
 
   beforeAll(async () => {
     app = await createNestAppInstance();
-    jest.setTimeout(10000);
+  });
+
+  describe('change password', () => {
+    it('should allow users to update their password', async () => {
+      const userData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
+      const { user } = userData;
+
+      const oldPassword = user.password;
+
+      const response = await request(app.getHttpServer())
+        .patch('/api/users/change_password')
+        .set('Authorization', authHeaderForUser(user))
+        .send({ currentPassword: 'password', newPassword: 'new password' });
+
+      expect(response.statusCode).toBe(200);
+
+      const updatedUser = await getManager().findOne(User, { email: user.email });
+      expect(updatedUser.password).not.toEqual(oldPassword);
+    });
+
+    it('should not allow users to update their password if entered current password is wrong', async () => {
+      const userData = await createUser(app, { email: 'admin@tooljet.io', role: 'admin' });
+      const { user } = userData;
+
+      const oldPassword = user.password;
+
+      const response = await request(app.getHttpServer())
+        .patch('/api/users/change_password')
+        .set('Authorization', authHeaderForUser(user))
+        .send({ currentPassword: 'wrong password', newPassword: 'new password' });
+
+      expect(response.statusCode).toBe(403);
+
+      const updatedUser = await getManager().findOne(User, { email: user.email });
+      expect(updatedUser.password).toEqual(oldPassword);
+    });
   });
 
   describe('update user', () => {
@@ -30,15 +65,13 @@ describe('users controller', () => {
 
       expect(response.statusCode).toBe(200);
 
-      await user.reload();
-
-      expect(user.firstName).toEqual(firstName);
-      expect(user.lastName).toEqual(lastName);
+      const updatedUser = await getManager().findOne(User, { email: user.email });
+      expect(updatedUser.firstName).toEqual(firstName);
+      expect(updatedUser.lastName).toEqual(lastName);
     });
   });
 
   afterAll(async () => {
-    await getConnection().close();
     await app.close();
   });
 });
