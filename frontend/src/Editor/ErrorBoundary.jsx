8cdf45f25e55175037cc94908b1fc39014f573a2
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-12-16 00:05:54 +0530

    Add error boundary for components

---
+++ frontend/src/Editor/ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.showFallback ? <h1>Something went wrong.</h1> : <div></div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
