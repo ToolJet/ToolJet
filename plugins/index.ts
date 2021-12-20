937fa89f6253c36cc30d2f984b83d5058eadd11f
gandharv <gandharvkumargarg@gmail.com>
2021-12-20 16:44:25 +0530

    add elastic search

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,8 +1,10 @@
 import airtable from './packages/airtable/lib/airtable'
 import gcs from './packages/gcs/lib/gcs'
 import dynamodb from './packages/dynamodb/lib/dynamodb'
+import elasticsearch from './packages/dynamodb/lib/elasticsearch'
 export default {
   airtable,
   gcs,
-  dynamodb
+  dynamodb,
+  elasticsearch
 }
---
+++ plugins/packages/elasticsearch/__tests__/elasticsearch.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const elasticsearch = require('..');
+
+describe('elasticsearch', () => {
+    it('needs tests');
+});
