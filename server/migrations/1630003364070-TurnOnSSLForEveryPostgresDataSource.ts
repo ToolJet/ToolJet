e9c352f0244372255ffe92b8fe15b7415ff6f4ee
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-12-22 00:35:06 +0530

    add migrations for versioning datasources and dataqueries

--- server/migrations/1630003364070-TurnOnSSLForEveryPostgresDataSource.ts
+++ server/migrations/1630003364070-TurnOnSSLForEveryPostgresDataSource.ts
@@ -1,28 +1,25 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
-import { DataSource } from "../src/entities/data_source.entity";
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { DataSource } from '../src/entities/data_source.entity';
 
 export class TurnOnSSLForEveryPostgresDataSource1630003364070 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     const entityManager = queryRunner.manager;
     const queryBuilder = queryRunner.connection.createQueryBuilder();
     const dataSourceRepository = entityManager.getRepository(DataSource);
 
-    const dataSources = await dataSourceRepository.find({ where: { kind: 'postgresql' } });
+    const dataSources = await dataSourceRepository.find({
+      where: { kind: 'postgresql' },
+      select: ['id', 'options'],
+    });
 
-    for(const dataSource of dataSources) {
+    for (const dataSource of dataSources) {
       const options = dataSource.options;
-      if(options) {
+      if (options) {
         options['ssl_enabled'] = { value: true, encrypted: false };
-        await queryBuilder.update(DataSource)
-        .set({ options })
-        .where('id = :id', { id: dataSource.id })
-        .execute();
+        await queryBuilder.update(DataSource).set({ options }).where('id = :id', { id: dataSource.id }).execute();
       }
     }
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-  }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
