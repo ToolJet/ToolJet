d56c8abc4135cda3ee3ed8f15816a8b95607efca
arpitnath <arpitnath42@gmail.com>
2021-12-21 14:16:45 +0530

    resolves comments

--- frontend/src/Editor/CodeBuilder/CodeHinter.jsx
+++ frontend/src/Editor/CodeBuilder/CodeHinter.jsx
@@ -25,7 +25,6 @@ export function CodeHinter({
   mode,
   theme,
   lineNumbers,
-  className,
   placeholder,
   ignoreBraces,
   enablePreview,
@@ -34,7 +33,7 @@ export function CodeHinter({
   lineWrapping,
   componentName = null,
   usePortalEditor = true,
-  isScrollable = false,
+  className = 'code-hinter',
 }) {
   const darkMode = localStorage.getItem('darkMode') === 'true';
   const options = {
@@ -151,11 +150,11 @@ export function CodeHinter({
   };
   const [, forceUpdate] = React.useReducer((x) => x + 1, 0);
 
-  const defaultClassName = isScrollable ? '' : 'code-hinter';
+  // const defaultClassName = isScrollable ? '' : 'code-hinter';
   return (
     <div className="code-hinter-wrapper" style={{ width: '100%' }}>
       <div
-        className={`${defaultClassName} ${className || 'codehinter-default-input'}`}
+        className={`${className || 'codehinter-default-input'}`}
         key={suggestions.length}
         style={{ height: height || 'auto', minHeight, maxHeight: '320px', overflow: 'auto' }}
       >
