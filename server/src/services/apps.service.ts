3046a52a05269d9c89a7596e8a0fe7c03a66dcf1
Midhun G S <gsmithun4@gmail.com>
2021-12-22 14:37:37 +0530

    Home Page Application search box (#1603)
    * feature-1567 initial commit
    
    * feature-1567
    
    * freture-1567 App search box
    
    * added default value for search key
    
    * Loading fix
    
    * Review comments
    
    * Separated like query
    
    * feature-1567 e2e testcases
    
    * feature-1567 Fix
    
    * feature-1567 fix
    
    * feature-1567 fix

--- server/src/services/apps.service.ts
+++ server/src/services/apps.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { App } from 'src/entities/app.entity';
-import { createQueryBuilder, Repository } from 'typeorm';
+import { createQueryBuilder, Repository, Brackets } from 'typeorm';
 import { User } from 'src/entities/user.entity';
 import { AppUser } from 'src/entities/app_user.entity';
 import { AppVersion } from 'src/entities/app_version.entity';
 import { FolderApp } from 'src/entities/folder_app.entity';
 import { DataSource } from 'src/entities/data_source.entity';
 import { DataQuery } from 'src/entities/data_query.entity';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { GroupPermission } from 'src/entities/group_permission.entity';
 import { AppGroupPermission } from 'src/entities/app_group_permission.entity';
 import { UserGroupPermission } from 'src/entities/user_group_permission.entity';
@@ -134,8 +133,8 @@ export class AppsService {
     return clonedApp;
   }
 
-  async count(user: User): Promise<number> {
-    return await createQueryBuilder(App, 'apps')
+  async count(user: User, searchKey): Promise<number> {
+    const viewableAppsQb = createQueryBuilder(App, 'apps')
       .innerJoin('apps.groupPermissions', 'group_permissions')
       .innerJoin('apps.appGroupPermissions', 'app_group_permissions')
       .innerJoin(
@@ -143,18 +142,27 @@ export class AppsService {
         'user_group_permissions',
         'app_group_permissions.group_permission_id = user_group_permissions.group_permission_id'
       )
-      .where('user_group_permissions.user_id = :userId', { userId: user.id })
-      .andWhere('app_group_permissions.read = :value', { value: true })
-      .orWhere('(apps.is_public = :value AND apps.organization_id = :organizationId) OR apps.user_id = :userId', {
-        value: true,
-        organizationId: user.organizationId,
-        userId: user.id,
-      })
-      .getCount();
+      .where(
+        new Brackets((qb) => {
+          qb.where('user_group_permissions.user_id = :userId', { userId: user.id })
+            .andWhere('app_group_permissions.read = :value', { value: true })
+            .orWhere('(apps.is_public = :value AND apps.organization_id = :organizationId) OR apps.user_id = :userId', {
+              value: true,
+              organizationId: user.organizationId,
+              userId: user.id,
+            });
+        })
+      );
+    if (searchKey) {
+      viewableAppsQb.andWhere('LOWER(apps.name) like :searchKey', {
+        searchKey: `%${searchKey && searchKey.toLowerCase()}%`,
+      });
+    }
+    return await viewableAppsQb.getCount();
   }
 
-  async all(user: User, page: number): Promise<App[]> {
-    const viewableAppsQb = await createQueryBuilder(App, 'apps')
+  async all(user: User, page: number, searchKey: string): Promise<App[]> {
+    const viewableAppsQb = createQueryBuilder(App, 'apps')
       .innerJoin('apps.groupPermissions', 'group_permissions')
       .innerJoinAndSelect('apps.appGroupPermissions', 'app_group_permissions')
       .innerJoinAndSelect('apps.user', 'user')
@@ -163,20 +171,24 @@ export class AppsService {
         'user_group_permissions',
         'app_group_permissions.group_permission_id = user_group_permissions.group_permission_id'
       )
-      .where('user_group_permissions.user_id = :userId', { userId: user.id })
-      .andWhere('app_group_permissions.read = :value', { value: true })
-      .orWhere('(apps.is_public = :value AND apps.organization_id = :organizationId) OR apps.user_id = :userId', {
-        value: true,
-        organizationId: user.organizationId,
-        userId: user.id,
-      })
-      .orderBy('apps.createdAt', 'DESC');
+      .where(
+        new Brackets((qb) => {
+          qb.where('user_group_permissions.user_id = :userId', { userId: user.id })
+            .andWhere('app_group_permissions.read = :value', { value: true })
+            .orWhere('(apps.is_public = :value AND apps.organization_id = :organizationId) OR apps.user_id = :userId', {
+              value: true,
+              organizationId: user.organizationId,
+              userId: user.id,
+            });
+        })
+      );
+    if (searchKey) {
+      viewableAppsQb.andWhere('LOWER(apps.name) like :searchKey', {
+        searchKey: `%${searchKey && searchKey.toLowerCase()}%`,
+      });
+    }
+    viewableAppsQb.orderBy('apps.createdAt', 'DESC');
 
-    // FIXME:
-    // Fixed based on https://github.com/typeorm/typeorm/issues/747#issuecomment-519553920
-    // TypeORM gives error when using query builder with order by
-    // https://github.com/typeorm/typeorm/issues/8213
-    // hence sorting results in memory
     if (page) {
       return await viewableAppsQb
         .take(10)
@@ -184,7 +196,7 @@ export class AppsService {
         .getMany();
     }
 
-    return await viewableAppsQb.orderBy('apps.created_at', 'DESC').getMany();
+    return await viewableAppsQb.getMany();
   }
 
   async update(user: User, appId: string, params: any) {
