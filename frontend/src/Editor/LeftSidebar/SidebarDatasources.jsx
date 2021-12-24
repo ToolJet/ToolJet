ae5204f1660f662e88536c1bf45221d01649fead
arpitnath <arpitnath42@gmail.com>
2021-12-24 19:10:45 +0530

    popup btn

--- frontend/src/Editor/LeftSidebar/SidebarDatasources.jsx
+++ frontend/src/Editor/LeftSidebar/SidebarDatasources.jsx
@@ -84,29 +84,19 @@ const LeftSidebarDataSourcesContainer = ({ renderDataSource, dataSources = [], t
     <div className="card-body">
       <div>
         <div className="row">
-          <div className="col-10">
-            <span className="font-500">Data sources</span>
+          <div className="col">
+            <h5 className="text-muted">Data sources</h5>
           </div>
-          <div className="col-2">
+          <div className="col-auto">
             <OverlayTrigger
               trigger={['hover', 'focus']}
               placement="top"
               delay={{ show: 800, hide: 100 }}
               overlay={<Tooltip id="button-tooltip">{'Add datasource'}</Tooltip>}
             >
-              <span onClick={() => toggleDataSourceManagerModal(true)} className="cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="#4D72FA"
-                  viewBox="0 0 24 24"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                </svg>
-              </span>
+              <button onClick={() => toggleDataSourceManagerModal(true)} className="btn btn-sm add-btn">
+                <img className="" src="/assets/images/icons/plus.svg" width="12" height="12" />
+              </button>
             </OverlayTrigger>
           </div>
         </div>
