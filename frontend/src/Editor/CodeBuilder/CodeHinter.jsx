ded0a2c2c0f02eab97a79c0caffb9844089ba330
Arpit <arpitnath42@gmail.com>
2021-12-25 08:51:19 +0530

    Bugfix: Scrolling is broken for code editor on postgresql query manager (#1583)
    * fixes: Scrolling is broken for code editor on postgresql query manager
    
    * resolves comments

--- frontend/src/Editor/CodeBuilder/CodeHinter.jsx
+++ frontend/src/Editor/CodeBuilder/CodeHinter.jsx
@@ -25,7 +25,6 @@ export function CodeHinter({
   mode,
   theme,
   lineNumbers,
-  className,
   placeholder,
   ignoreBraces,
   enablePreview,
@@ -34,6 +33,7 @@ export function CodeHinter({
   lineWrapping,
   componentName = null,
   usePortalEditor = true,
+  className = 'code-hinter',
 }) {
   const darkMode = localStorage.getItem('darkMode') === 'true';
   const options = {
@@ -150,10 +150,11 @@ export function CodeHinter({
   };
   const [, forceUpdate] = React.useReducer((x) => x + 1, 0);
 
+  // const defaultClassName = isScrollable ? '' : 'code-hinter';
   return (
     <div className="code-hinter-wrapper" style={{ width: '100%' }}>
       <div
-        className={`code-hinter ${className || 'codehinter-default-input'}`}
+        className={`${className || 'codehinter-default-input'}`}
         key={suggestions.length}
         style={{ height: height || 'auto', minHeight, maxHeight: '320px', overflow: 'auto' }}
       >
