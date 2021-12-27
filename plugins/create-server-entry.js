13b39155bf0d2589becbba7133b02e9ca399055b
gandharv <gandharvkumargarg@gmail.com>
2021-12-27 13:53:27 +0530

    rename file for server entry

--- plugins/create-server-entry.js
+++ plugins/create-server-entry.js
@@ -3,9 +3,16 @@ const { readdirSync, writeFileSync } = require('fs');
 const packages = readdirSync('./packages', { withFileTypes: true })
 .filter((dirent) => dirent.isDirectory() && dirent.name !== 'common')
 
-const imports = packages.map((dirent) => `import ${dirent.name} from './packages/${dirent.name}/lib'`);
+const mapImportNameToOldValue = str => {
+  if(str === 'mongo') return 'mongodb'
+  if(str === 'twilioapi') return 'twilio'
+  if(str === 'typesenseapi') return 'typesense'
+  return str
+}
 
-const outs = `export default {\n${packages.map((dirent) => dirent.name).join(',\n')} \n }`;
+const imports = packages.map((dirent) => `import ${mapImportNameToOldValue(dirent.name)} from './packages/${dirent.name}/lib'`);
+
+const outs = `export default {\n${packages.map((dirent) => mapImportNameToOldValue(dirent.name)).join(',\n')} \n }`;
 
 const content = `
 ${imports.join('\n')} \n
