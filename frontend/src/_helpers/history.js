f8456539141f49f9a5ce14e47103a562420a52ff
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-12-30 19:51:56 +0530

    Reload page for goto app action

--- frontend/src/_helpers/history.js
+++ frontend/src/_helpers/history.js
@@ -1,4 +1,3 @@
 import { createBrowserHistory } from 'history';
 
-// using force refresh as a temporary fix for improper rendering, need to figure out what's going wrong
-export const history = createBrowserHistory({ forceRefresh: true });
+export const history = createBrowserHistory();
