8fa8521b78b02a4478026fb2b621437683c563fc
Muhsin Shah <muhsinshah21@gmail.com>
2022-01-03 13:48:12 +0530

    added scrollbar & space between tags

--- frontend/src/Editor/Components/Tags.jsx
+++ frontend/src/Editor/Components/Tags.jsx
@@ -9,16 +9,19 @@ export const Tags = function Tags({ height, properties, styles }) {
     height,
     display: visibility ? '' : 'none',
     color: textColor,
+    overflowY: 'auto',
   };
 
   function renderTag(item, index) {
     const tagComputedStyles = {
       backgroundColor: item.color,
       color: item.textColor,
+      textTransform: 'none',
+      height,
     };
 
     return (
-      <span className="col-auto badge p-2 mx-1 tag" style={tagComputedStyles} key={index}>
+      <span className="badge mx-1 mb-1" style={tagComputedStyles} key={index}>
         {item.title}
       </span>
     );
