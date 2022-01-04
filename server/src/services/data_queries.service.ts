29bc45959403ff6663ee3b5161af6cdb74cbd405
gandharv <gandharvkumargarg@gmail.com>
2022-01-05 01:55:55 +0530

    import server.ts

--- server/src/services/data_queries.service.ts
+++ server/src/services/data_queries.service.ts
@@ -1,4 +1,4 @@
-import allPlugins from '@tooljet/plugins/server';
+import allPlugins from '@tooljet/plugins/dist/server';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
