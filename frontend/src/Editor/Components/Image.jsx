02bfb76472a814d68a531f63e47f57101005085b
Sherfin Shamsudeen <sherfin94@gmail.com>
2022-01-05 11:38:19 +0530

    Remove usage of useEffect and useState from Image widget

--- frontend/src/Editor/Components/Image.jsx
+++ frontend/src/Editor/Components/Image.jsx
@@ -1,19 +1,14 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 import LazyLoad from 'react-lazyload';
 
 export const Image = function Image({ height, properties, styles, fireEvent }) {
   const source = properties.source;
   const widgetVisibility = styles.visibility ?? true;
   const imageRef = useRef(null);
-  const [imageOffset, setImageOffset] = useState(0);
 
   function Placeholder() {
     return <div className="skeleton-image" style={{ objectFit: 'contain', height }}></div>;
   }
-
-  useEffect(() => {
-    setImageOffset(computeOffset());
-  }, [imageRef]);
 
   function computeOffset() {
     if (imageRef.current) {
@@ -23,6 +18,8 @@ export const Image = function Image({ height, properties, styles, fireEvent }) {
     }
     return 0;
   }
+
+  const imageOffset = computeOffset();
 
   return (
     <div data-disabled={styles.disabledState} style={{ display: widgetVisibility ? '' : 'none' }} ref={imageRef}>
