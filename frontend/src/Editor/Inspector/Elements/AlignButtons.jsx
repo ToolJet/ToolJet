a7f208438311351600bf37095e763f3dff670ff2
Muhsin Shah <muhsinshah21@gmail.com>
2022-01-12 19:09:50 +0530

    changed colors and icons

--- frontend/src/Editor/Inspector/Elements/AlignButtons.jsx
+++ frontend/src/Editor/Inspector/Elements/AlignButtons.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { ToolTip } from './Components/ToolTip';
-import AlignLeftIcon from '@/Editor/Icons/align-left.svg';
-import AlignRightIcon from '@/Editor/Icons/align-right.svg';
-import AlignCenterIcon from '@/Editor/Icons/align-center.svg';
-import AlignJustifiedIcon from '@/Editor/Icons/align-justified.svg';
 
 export const AlignButtons = ({ param, definition, onChange, paramType, componentMeta }) => {
   const initialValue = definition ? definition.value : '';
@@ -16,7 +12,7 @@ export const AlignButtons = ({ param, definition, onChange, paramType, component
   }
 
   return (
-    <div className={`mb-2 field ${options.className}`}>
+    <div className={`mb-3 field ${options.className}`}>
       <ToolTip label={displayName} meta={paramMeta} />
       <div style={{ display: 'flex' }}>
         <label className="radio-img">
@@ -28,23 +24,16 @@ export const AlignButtons = ({ param, definition, onChange, paramType, component
             checked={initialValue === 'left'}
           />
           <div className="action-icon">
-            <AlignLeftIcon />
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M2 4H14M2 8H8.66667M2 12H12"
+                stroke={initialValue == 'left' ? '#fff' : '#8092AC'}
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              />
+            </svg>
           </div>
           <span className="tooltiptext">Left</span>
-        </label>
-
-        <label className="radio-img">
-          <input
-            type="radio"
-            name="alignment"
-            value="right"
-            onChange={handleOptionChanged}
-            checked={initialValue === 'right'}
-          />
-          <div className="action-icon">
-            <AlignRightIcon />
-          </div>
-          <span className="tooltiptext">Right</span>
         </label>
 
         <label className="radio-img">
@@ -56,9 +45,44 @@ export const AlignButtons = ({ param, definition, onChange, paramType, component
             checked={initialValue === 'center'}
           />
           <div className="action-icon">
-            <AlignCenterIcon />
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="svg-icon"
+            >
+              <path
+                d="M2 4H14M4.66667 8H11.3333M3.33333 12H12.6667"
+                stroke={initialValue == 'center' ? '#fff' : '#8092AC'}
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              />
+            </svg>
           </div>
           <span className="tooltiptext">Center</span>
+        </label>
+
+        <label className="radio-img">
+          <input
+            type="radio"
+            name="alignment"
+            value="right"
+            onChange={handleOptionChanged}
+            checked={initialValue === 'right'}
+          />
+          <div className="action-icon">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M2 4H14M7.33333 8H14M4 12H14"
+                stroke={initialValue == 'right' ? '#fff' : '#8092AC'}
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              />
+            </svg>
+          </div>
+          <span className="tooltiptext">Right</span>
         </label>
 
         <label className="radio-img">
@@ -70,7 +94,15 @@ export const AlignButtons = ({ param, definition, onChange, paramType, component
             checked={initialValue === 'justify'}
           />
           <div className="action-icon">
-            <AlignJustifiedIcon />
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M2 8H14M2 12H14M2 4H8H14H2Z"
+                stroke={initialValue == 'justify' ? '#fff' : '#8092AC'}
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </div>
           <span className="tooltiptext">Justified</span>
         </label>
