2b88a1c75198753c71a5000458610769befa97c3
Karl Rezansoff <karlrez123@gmail.com>
2022-01-13 03:25:37 -0800

    added logos to querymanager (#1744)
    * added logos to querymanager
    
    * fixed to pass eslint check

--- frontend/src/Editor/QueryManager/QueryManager.jsx
+++ frontend/src/Editor/QueryManager/QueryManager.jsx
@@ -209,11 +209,17 @@ let QueryManager = class QueryManager extends React.Component {
   };
 
   renderDataSourceOption = (props, option, snapshot, className) => {
+    const icon = option.kind ? `/assets/images/icons/editor/datasources/${option.kind.toLowerCase() + '.svg'}` : null;
     return (
       <button {...props} className={className} type="button">
         <div className="row">
           <div className="col-md-9">
-            <span className="text-muted mx-2">{option.name}</span>
+            <span className="text-muted mx-2">
+              {icon && (
+                <img src={icon} style={{ objectFit: 'contain' }} height="25" width="25" className="mt-1·col-md-2"></img>
+              )}
+              {option.name}
+            </span>
           </div>
         </div>
       </button>
@@ -366,7 +372,7 @@ let QueryManager = class QueryManager extends React.Component {
                     <SelectSearch
                       options={[
                         ...dataSources.map((source) => {
-                          return { name: source.name, value: source.id };
+                          return { name: source.name, value: source.id, kind: source.kind };
                         }),
                         ...staticDataSources.map((source) => {
                           return { name: source.name, value: source.id };
