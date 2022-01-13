3fdb0a2bb33e845da035189790ecf6a22c286368
Sherfin Shamsudeen <sherfin94@gmail.com>
2022-01-13 20:00:50 +0530

    Show "no results" if templates are not found

--- frontend/src/HomePage/TemplateLibraryModal/AppList.jsx
+++ frontend/src/HomePage/TemplateLibraryModal/AppList.jsx
@@ -4,21 +4,20 @@ import { ListGroup, InputGroup, FormControl } from 'react-bootstrap';
 export default function AppList(props) {
   const { apps, selectedApp, selectApp } = props;
   const [searchText, searchFor] = useState('');
+  const filteredApps = apps.filter((app) => app.name.toLowerCase().includes(searchText.toLowerCase()));
 
   return (
-    <div>
-      <InputGroup className="mt-2">
-        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+    <div className="template-list">
+      <InputGroup className="mt-2 template-search-box">
         <FormControl placeholder="Search" aria-label="search" onChange={(event) => searchFor(event.target.value)} />
       </InputGroup>
       <ListGroup className="mt-2 template-app-list">
-        {apps
-          .filter((app) => app.name.toLowerCase().includes(searchText.toLowerCase()))
-          .map((app) => (
-            <ListGroup.Item key={app.id} action active={app.id === selectedApp?.id} onClick={() => selectApp(app)}>
-              {app.name}
-            </ListGroup.Item>
-          ))}
+        {filteredApps.length === 0 ? <ListGroup.Item variant="light">No results</ListGroup.Item> : <div></div>}
+        {filteredApps.map((app) => (
+          <ListGroup.Item key={app.id} action active={app.id === selectedApp?.id} onClick={() => selectApp(app)}>
+            {app.name}
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </div>
   );
