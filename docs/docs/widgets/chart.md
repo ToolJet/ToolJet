59a133b5e3a0f06c1b252a98cd14e5cb5fe99a11
Muhsin Shah C P <muhsinshah21@gmail.com>
2022-01-17 16:43:02 +0530

    Added Json description feature (#1732)
    * Added Json description feature
    
    * Added a toggle button for toggle from chart data to json data
    
    * toggle effects for chart-data and json-data is added
    
    * documentation about json part is added
    
    * replaced the position of json toggle . changed the section and label name
    
    * marker color field will hide if plotly is enabled

--- docs/docs/widgets/chart.md
+++ docs/docs/widgets/chart.md
@@ -64,3 +64,16 @@ Example:
 
 The chart will look like this:
 <img class="screenshot-full" src="/img/widgets/chart/pie.png" alt="ToolJet - line charts" height="420"/>
+
+
+## Visually using JSON
+
+In the chart widget, you can also plot a chart with JSON data. So the JSON will contain the information about the chart type, chart data, etc. You can learn more about the JSON properties from [here](https://plotly.com/javascript/reference/). 
+
+Anyway, to work with the JSON you need to enable the ``Use Plotly JSON schema`` toggle from the properties section.
+<img class="screenshot-full" src="/img/widgets/chart/plot_from_json.png" alt="ToolJet - line charts" height="420"/>
+
+It will show the ``Json description`` field to put the JSON data.
+
+The end result will be like this:
+<img class="screenshot-full" src="/img/widgets/chart/from_json.png" alt="ToolJet - line charts" height="420"/>
--- frontend/src/Editor/Components/Chart.jsx
+++ frontend/src/Editor/Components/Chart.jsx
@@ -3,13 +3,14 @@ import React, { useState, useEffect, useMemo } from 'react';
 // Use plotly basic bundle
 import Plotly from 'plotly.js-basic-dist-min';
 import createPlotlyComponent from 'react-plotly.js/factory';
+import { isJson } from '@/_helpers/utils';
 const Plot = createPlotlyComponent(Plotly);
 
 export const Chart = function Chart({ width, height, darkMode, properties, styles }) {
   const [loadingState, setLoadingState] = useState(false);
 
   const { visibility, disabledState } = styles;
-  const { title, markerColor, showGridLines, type, data } = properties;
+  const { title, markerColor, showGridLines, type, data, jsonDescription, plotFromJson } = properties;
 
   useEffect(() => {
     const loadingStateProperty = properties.loadingState;
@@ -28,6 +29,8 @@ export const Chart = function Chart({ width, height, darkMode, properties, style
   const dataString = data ?? [];
 
   const chartType = type;
+
+  const jsonChartData = isJson(jsonDescription) ? JSON.parse(jsonDescription).data : [];
 
   const fontColor = darkMode ? '#c3c3c3' : null;
 
@@ -111,7 +114,7 @@ export const Chart = function Chart({ width, height, darkMode, properties, style
         </div>
       ) : (
         <Plot
-          data={memoizedChartData}
+          data={plotFromJson ? jsonChartData : memoizedChartData}
           layout={layout}
           config={{
             displayModeBar: false,
