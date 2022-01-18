5e6068edbe17659ee9042728cfa9474db842a51b
stepinfwd <stepinfwd@gmail.com>
2022-01-19 02:07:12 +0530

    updated style for dropdown item

--- frontend/src/HomePage/Header.jsx
+++ frontend/src/HomePage/Header.jsx
@@ -13,6 +13,32 @@ export default function Header({
   showTemplateLibraryModal,
   fileInput,
 }) {
+
+  const dropdownStyle={
+    minWidth: '11rem',
+    display: 'flex',
+    alignItems: 'center',
+    margin: 0,
+    lineHeight:' 1.4285714',
+    display: 'block',
+    width: '100%',
+    padding:' 0.5rem 0.75rem',
+    clear: 'both',
+    fontWeight: '400',
+    color: 'inherit',
+    textAlign: 'inherit',
+    whiteSpace: 'nowrap',
+    // backgroundColor: 'transparent',
+    border: '0',
+    cursor:'pointer',
+    backgroundColor: {
+      default: 'white',
+      hover: 'red',
+      active: 'navy',
+      focus: 'white',
+     },
+  }
+  
   return (
     <div className="row">
       <div className="col-4">
@@ -23,11 +49,6 @@ export default function Header({
         <SearchBox onSubmit={onSearchSubmit} />
         {canCreateApp() && (
           <>
-            {/* <label className="btn btn-default d-none d-lg-inline mb-3 ms-2" onChange={handleImportApp}>
-              {isImportingApp && <span className="spinner-border spinner-border-sm mx-2" role="status"></span>}
-              Import
-              <input type="file" accept=".json" ref={fileInput} style={{ display: 'none' }} />
-            </label> */}
             {canCreateApp() && (
               <Dropdown as={ButtonGroup}>
                 <Button
@@ -37,15 +58,13 @@ export default function Header({
                   {isImportingApp && <span className="spinner-border spinner-border-sm mx-2" role="status"></span>}
                   Create new application
                 </Button>
-
                 <Dropdown.Toggle split className="btn btn-primary d-none d-lg-inline mb-3" />
-
                 <Dropdown.Menu>
                   <Dropdown.Item onClick={showTemplateLibraryModal}>Choose from template</Dropdown.Item>
-                  <Dropdown.Item   onSelect={handleImportApp}>
-                    Import <input type="file" accept=".json"  ref={fileInput} style={{ display: 'none' ,zIndex:}} />
-                  </Dropdown.Item>
-
+                  <label style={dropdownStyle} onChange={handleImportApp}>  
+                    Import
+                    < input type="file" accept=".json" ref={fileInput} style={{ display: 'none', zIndex: '100' }} />
+                  </label>
                 </Dropdown.Menu>
               </Dropdown>
             )}
