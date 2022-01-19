94bac0cb38b51ac676ec273287eff05c3518d944
arpitnath <arpitnath42@gmail.com>
2022-01-19 17:49:26 +0530

    text input widget updated

--- frontend/src/Editor/Components/TextInput.jsx
+++ frontend/src/Editor/Components/TextInput.jsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
 export const TextInput = function TextInput({ height, validate, properties, styles, setExposedVariable, fireEvent }) {
-  const [value, setValue] = useState(properties.value);
-  const { isValid, validationError } = validate(value);
+  const inputValue = typeof properties.value === 'boolean' ? properties.value.toString() : properties.value;
+  // const [value, setValue] = useState(() =>
+  //   typeof properties.value === 'boolean' ? properties.value.toString() : properties.value
+  // );
+  const { isValid, validationError } = validate(inputValue);
 
   useEffect(() => {
     setExposedVariable('isValid', isValid);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isValid]);
 
-  useEffect(() => {
-    setValue(properties.value);
-    setExposedVariable('value', properties.value);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [properties.value]);
+  // useEffect(() => {
+  //   setValue(properties.value);
+  //   setExposedVariable('value', properties.value);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [properties.value]);
 
   return (
     <div className="text-input">
       <input
         disabled={styles.disabledState}
         onChange={(e) => {
-          setValue(e.target.value);
+          // setValue(e.target.value);
           setExposedVariable('value', e.target.value);
           fireEvent('onChange');
         }}
@@ -28,7 +31,7 @@ export const TextInput = function TextInput({ height, validate, properties, styl
         className={`form-control ${!isValid ? 'is-invalid' : ''} validation-without-icon`}
         placeholder={properties.placeholder}
         style={{ height, display: styles.visibility ? '' : 'none' }}
-        value={value}
+        value={inputValue}
       />
       <div className="invalid-feedback">{validationError}</div>
     </div>
