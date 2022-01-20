03d3188d63d0f7afe076bf4aee18e27373c4e5c0
stepinfwd <stepinfwd@gmail.com>
2022-01-20 22:16:11 +0530

    pr review changes : cleanup and css moved to theme file from internal

--- frontend/src/HomePage/Header.jsx
+++ frontend/src/HomePage/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { SearchBox } from '@/_components/SearchBox';
 import { Button, ButtonGroup, Dropdown } from 'react-bootstrap';
-import useMediaQuery from "./useMediaquery";
 
 export default function Header({
   folderName,
@@ -15,40 +14,18 @@ export default function Header({
   fileInput,
 }) {
 
-  const [bgColour, setBgColour] = useState("#ffffff")
-  
-  const dropdownStyle = {
-    minWidth: '11rem',
-    display: 'flex',
-    alignItems: 'center',
-    margin: 0,
-    lineHeight: ' 1.4285714',
-    display: 'block',
-    width: '100%',
-    padding: ' 0.5rem 0.75rem',
-    clear: 'both',
-    fontWeight: '400',
-    color: 'inherit',
-    textAlign: 'inherit',
-    whiteSpace: 'nowrap',
-    background: `${bgColour}`,
-    border: '0',
-    cursor: 'pointer',
-  }
-  const matches = useMediaQuery("(min-width: 1699px)");
 
   return (
     <div className="row">
       <div className="col-4">
         <h2 className="page-title px-2">{folderName}</h2>
       </div>
-
       <div className="col-8 ms-auto d-print-none d-flex flex-row justify-content-end">
         <SearchBox onSubmit={onSearchSubmit} />
         {canCreateApp() && (
           <>
             {canCreateApp() && (
-              <Dropdown as={ButtonGroup}  >
+              <Dropdown as={ButtonGroup}>
                 <Button
                   className={`btn btn-primary d-none d-lg-inline mb-3 ms-2 ${creatingApp ? 'btn-loading' : ''}`}
                   onClick={createApp}
@@ -57,11 +34,10 @@ export default function Header({
                   Create new application
                 </Button>
                 <Dropdown.Toggle split className="btn btn-primary d-none d-lg-inline mb-3 " />
-                <Dropdown.Menu className={matches&&"import-lg-position"} >
+                <Dropdown.Menu className="import-lg-position">
                   <Dropdown.Item onClick={showTemplateLibraryModal}>Choose from template</Dropdown.Item>
-                  <label style={dropdownStyle} onChange={handleImportApp} onMouseEnter={() => setBgColour("rgba(101, 109, 119, 0.06)")}>
-                    Import
-                    < input type="file" accept=".json" ref={fileInput} style={{ display: 'none' }} />
+                  <label className="homepage-dropdown-style" onChange={handleImportApp} >
+                    <input type="file" accept=".json" ref={fileInput} style={{ display: 'none' }} />
                   </label>
                 </Dropdown.Menu>
               </Dropdown>
