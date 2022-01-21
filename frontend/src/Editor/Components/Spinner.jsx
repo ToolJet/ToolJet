d1388ec975089b2f6929d9afad4755b9077bdfbf
stepinfwd <stepinfwd@gmail.com>
2022-01-21 12:47:57 +0530

    cleanup

--- frontend/src/Editor/Components/Spinner.jsx
+++ frontend/src/Editor/Components/Spinner.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export const Spinner = ({ styles, height, darkMode }) => {
+export const Spinner = ({ styles, height}) => {
     const { colour, size, } = styles;
 
     return (
