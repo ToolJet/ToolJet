eba4a14e97c03a365419d94ef9bf14130eef2ec9
stepinfwd <stepinfwd@gmail.com>
2022-01-21 12:47:19 +0530

    pr reveiew changes , cleanup , making bg transparent

--- frontend/src/Editor/Components/Spinner.jsx
+++ frontend/src/Editor/Components/Spinner.jsx
@@ -3,15 +3,8 @@ import React, { useState, useEffect } from 'react';
 export const Spinner = ({ styles, height, darkMode }) => {
     const { colour, size, } = styles;
 
-    const spinnerBaseStyle = {
-        height,
-        backgroundColor: darkMode ? "#47505D" : "#ffffff",
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
     return (
-        <div style={spinnerBaseStyle}>
+        <div className='spinner-container' style={{height}}>
             <div className={`spinner-border spinner-border-${size}`} role="status" style={{ color: colour }}></div>
         </div>
     );
