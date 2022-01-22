f194d584e1484a5a263c605454cb1a8f9d57e4ac
Arpit <arpitnath42@gmail.com>
2022-01-22 10:45:27 +0530

    [Feature] file parser option for FilePicker widget [csv support] (#1764)
    * file parser
    
    * removes unwanted comments
    
    * refactor
    
    * add docs
    
    * resolves comments
    
    * handle csv bad data
    
    * remove pdf option
    
    * adds isParsing to filePicker

---
+++ frontend/src/Editor/Inspector/Components/DefaultComponent.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import Accordion from '@/_ui/Accordion';
+import { EventManager } from '../EventManager';
+import { renderElement } from '../Utils';
+
+export const DefaultComponent = ({ componentMeta, darkMode, ...restProps }) => {
+  const {
+    layoutPropertyChanged,
+    component,
+    paramUpdated,
+    dataQueries,
+    currentState,
+    eventsChanged,
+    apps,
+    allComponents,
+  } = restProps;
+
+  const properties = Object.keys(componentMeta.properties);
+  const events = Object.keys(componentMeta.events);
+  const validations = Object.keys(componentMeta.validation || {});
+
+  const accordionItems = baseComponentProperties(
+    properties,
+    events,
+    component,
+    componentMeta,
+    layoutPropertyChanged,
+    paramUpdated,
+    dataQueries,
+    currentState,
+    eventsChanged,
+    apps,
+    allComponents,
+    validations,
+    darkMode
+  );
+
+  return <Accordion items={accordionItems} />;
+};
+
+export const baseComponentProperties = (
+  properties,
+  events,
+  component,
+  componentMeta,
+  layoutPropertyChanged,
+  paramUpdated,
+  dataQueries,
+  currentState,
+  eventsChanged,
+  apps,
+  allComponents,
+  validations,
+  darkMode
+) => {
+  let items = [];
+  items.push({
+    title: 'Properties',
+    children: properties.map((property) =>
+      renderElement(
+        component,
+        componentMeta,
+        paramUpdated,
+        dataQueries,
+        property,
+        'properties',
+        currentState,
+        allComponents,
+        darkMode
+      )
+    ),
+  });
+
+  if (events.length > 0) {
+    items.push({
+      title: 'Events',
+      isOpen: false,
+      children: (
+        <EventManager
+          component={component}
+          componentMeta={componentMeta}
+          currentState={currentState}
+          dataQueries={dataQueries}
+          components={allComponents}
+          eventsChanged={eventsChanged}
+          apps={apps}
+          darkMode={darkMode}
+        />
+      ),
+    });
+  }
+
+  if (validations.length > 0) {
+    items.push({
+      title: 'Validation',
+      children: validations.map((property) =>
+        renderElement(
+          component,
+          componentMeta,
+          paramUpdated,
+          dataQueries,
+          property,
+          'validation',
+          currentState,
+          allComponents,
+          darkMode
+        )
+      ),
+    });
+  }
+
+  items.push({
+    title: 'Layout',
+    isOpen: false,
+    children: (
+      <>
+        {renderElement(
+          component,
+          componentMeta,
+          layoutPropertyChanged,
+          dataQueries,
+          'showOnDesktop',
+          'others',
+          currentState,
+          allComponents
+        )}
+        {renderElement(
+          component,
+          componentMeta,
+          layoutPropertyChanged,
+          dataQueries,
+          'showOnMobile',
+          'others',
+          currentState,
+          allComponents
+        )}
+      </>
+    ),
+  });
+
+  return items;
+};
