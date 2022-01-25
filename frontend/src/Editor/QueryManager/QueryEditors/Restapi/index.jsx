e815dc3a5f19fa70166f99e55e99441a7044079f
arpitnath <arpitnath42@gmail.com>
2022-01-26 01:34:35 +0530

    adds margin top to query-panel tabs

--- frontend/src/Editor/QueryManager/QueryEditors/Restapi/index.jsx
+++ frontend/src/Editor/QueryManager/QueryEditors/Restapi/index.jsx
@@ -122,7 +122,7 @@ class Restapi extends React.Component {
           </div>
         </div>
 
-        <div className={`query-pane-restapi-tabs mt-2 ${this.props.darkMode ? 'dark' : ''}`}>
+        <div className={`query-pane-restapi-tabs mt-3 ${this.props.darkMode ? 'dark' : ''}`}>
           <Tabs
             theme={this.props.darkMode ? 'monokai' : 'default'}
             options={this.state.options}
