33cf055e89acf11c6f58ce3ba0c3f885e1354ca0
Midhun G S <gsmithun4@gmail.com>
2022-01-27 15:21:17 +0530

    Github OAuth integration (#1837)
    * Initial commit
    
    * git auth
    
    * git sso
    
    * Git sso
    
    * Git Oauth rebase
    
    * review comments
    
    * added email validation
    
    * improvement
    
    * Package lock revert
    
    * E2E test case fix
    
    * fix data population in migration
    
    * Supress initial error thrown by google
    
    * Fix
    
    * e2e test cases
    
    * added e2e test cases
    
    * isValidDomain improvement
    
    * e2e test cases fix
    
    Co-authored-by: Akshay Sasidharan <akshaysasidharan93@gmail.com>

---
+++ frontend/ee/components/LoginPage/GitSSOLoginButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { buildURLWithQuery } from '@/_helpers/utils';
+
+export default function GitSSOLoginButton() {
+  const clientId = window.public_config.SSO_GIT_OAUTH2_CLIENT_ID;
+  const gitLogin = (e) => {
+    e.preventDefault();
+    window.location.href = buildURLWithQuery('https://github.com/login/oauth/authorize', { client_id: clientId });
+  };
+  return (
+    <div>
+      <button onClick={gitLogin} className="btn border-0 rounded-2">
+        <img src="/assets/images/sso-buttons/git.svg" className="h-4" />
+        <span className="px-1">Sign in with Github</span>
+      </button>
+    </div>
+  );
+}
---
+++ server/ee/services/oauth/models/user_response.ts
@@ -0,0 +1,8 @@
+export default interface UserResponse {
+  userSSOId: string;
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  domain?: string;
+  sso: string;
+}
