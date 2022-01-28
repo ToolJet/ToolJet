b665d9564206ce40877d67985e7dbbac62972d3c
stepinfwd <stepinfwd@gmail.com>
2022-01-28 19:45:40 +0530

    onboarding page ui

--- frontend/src/HomePage/BlankPage.jsx
+++ frontend/src/HomePage/BlankPage.jsx
@@ -1,64 +1,71 @@
 import React from 'react';
+import OnBordingCard from './OnBordingCard';
 
 export const BlankPage = function BlankPage({ createApp, handleImportApp, isImportingApp, fileInput }) {
+  const getHelpData = [
+    {
+      title: 'Documentation',
+      description: 'Learn in Details',
+      icon: 'tutorials',
+      link: 'https://docs.tooljet.com/docs',
+    },
+    {
+      title: "Blog's",
+      description: 'Learn in Details',
+      icon: 'blog',
+      link: 'https://blog.tooljet.com/',
+    },
+    {
+      title: 'Tutorials',
+      description: 'Master Tooljet',
+      icon: 'documents',
+      link: 'https://docs.tooljet.com/docs/intro',
+    },
+  ];
   return (
-    <div>
-      <div className="page-wrapper">
-        <div className="container-xl"></div>
-        <div className="page-body">
-          <div className="container-xl d-flex flex-column justify-content-center">
-            <div className="empty">
-              <div className="empty-img">
-                <img src="/assets/images/blank.svg" height="128" alt="" />
-              </div>
-              <p className="empty-title">
-                Welcome to ToolJet ! You can get started by creating a new application or by creating an application
-                using a template in ToolJet Library.
-              </p>
-              <div className="empty-action">
-                <a onClick={createApp} className="btn btn-primary text-light mx-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <line x1="12" y1="5" x2="12" y2="19" />
-                    <line x1="5" y1="12" x2="19" y2="12" />
-                  </svg>
-                  Create your first app
-                </a>
-                <a
-                  href="https://docs.tooljet.io"
-                  target="_blank"
-                  className="btn btn-primary text-light mx-1"
-                  rel="noreferrer"
-                  onChange={handleImportApp}
-                >
-                  <label>
-                    {isImportingApp && <span className="spinner-border spinner-border-sm me-2" role="status"></span>}
-                    Import an app
-                    <input type="file" ref={fileInput} style={{ display: 'none' }} />
-                  </label>
-                </a>
-                <a
-                  href="https://docs.tooljet.io"
-                  target="_blank"
-                  className="btn btn-primary text-light mx-1"
-                  rel="noreferrer"
-                >
-                  Read documentation
-                </a>
-              </div>
-            </div>
-          </div>
+    <div className="onboarding-page-wrapper">
+      <div className="onboarding-header-wrapper">
+        <h1 className="onboarding-header">Welcome to Tooljet!</h1>
+        <span className="onboarding-confetti">
+          <img src="/assets/images/confetti.svg" alt="" />
+        </span>
+      </div>
+      <p className="onboarding-subheader">
+        You can get started by creating a new application or by creating an application using a template in ToolJet
+        Library.
+      </p>
+      <hr className="onboarding-seperator" />
+      <p className="onbaording-info">To better use, here is some helpful resources to get started:</p>
+      <h1 className="onboarding-topics">Get Help</h1>
+      <div>
+        <div className="onboarding-card-container">
+          {getHelpData.map((item, index) => (
+            <OnBordingCard key={index} cardData={item} />
+          ))}
+        </div>
+      </div>
+      <hr className="onboarding-seperator" />
+      <div className="onboarding-app-creation">
+        <h1 className="onboarding-topics ">
+          Ready to go? <span>Create your App</span>{' '}
+        </h1>
+        <div className="empty-action">
+          <a
+            href="https://docs.tooljet.io"
+            target="_blank"
+            className="btn  mx-1 onboard-import-button"
+            rel="noreferrer"
+            onChange={handleImportApp}
+          >
+            <label>
+              {isImportingApp && <span className="spinner-border spinner-border-sm me-2 " role="status"></span>}
+              Import
+              <input type="file" ref={fileInput} style={{ display: 'none' }} />
+            </label>
+          </a>
+          <a onClick={createApp} className="btn btn-primary text-light mx-1">
+            Create new app
+          </a>
         </div>
       </div>
     </div>
