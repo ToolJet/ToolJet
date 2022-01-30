bb15d00c6bb3169d467d788f3edf25436dedec5a
arpitnath <arpitnath42@gmail.com>
2022-01-31 03:21:23 +0530

    fixes preview button spinner size

--- frontend/src/Editor/QueryManager/QueryManager.jsx
+++ frontend/src/Editor/QueryManager/QueryManager.jsx
@@ -321,7 +321,7 @@ let QueryManager = class QueryManager extends React.Component {
           )}
           <div className="col-auto px-1 m-auto">
             {(addingQuery || editingQuery) && (
-              <span
+              <button
                 onClick={() => {
                   const _options = { ...options };
 
@@ -338,13 +338,13 @@ let QueryManager = class QueryManager extends React.Component {
                       console.log(error, data);
                     });
                 }}
-                className={`btn button-family-secondary m-1 float-right1  ${previewLoading ? 'button-loading' : ''} ${
+                className={`btn button-family-secondary m-1 float-right1 ${previewLoading ? 'button-loading' : ''} ${
                   this.props.darkMode ? 'dark' : ''
                 } `}
                 style={{ width: '72px', height: '28px' }}
               >
                 Preview
-              </span>
+              </button>
             )}
             {(addingQuery || editingQuery) && (
               <button
