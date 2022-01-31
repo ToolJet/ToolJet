2380f3bb5442f4fe5186623c5683041ffd9fadd0
stepinfwd <stepinfwd@gmail.com>
2022-02-01 00:29:57 +0530

    new ui for onboarding screen

--- frontend/src/HomePage/BlankPage.jsx
+++ frontend/src/HomePage/BlankPage.jsx
@@ -9,53 +9,38 @@ export const BlankPage = function BlankPage({ createApp, handleImportApp, isImpo
           <div className="container-xl d-flex flex-column justify-content-center">
             <div className="empty">
               <div className="empty-img">
-                <img src="/assets/images/blank.svg" height="128" alt="" />
+                <img src="/assets/images/onboarding.svg" alt="" />
               </div>
+              <h3 className="empty-welcome-header">Welcome to Tooljet!</h3>
               <p className="empty-title">
                 Welcome to ToolJet ! You can get started by creating a new application or by creating an application
                 using a template in ToolJet Library.
               </p>
               <div className="empty-action">
-                <a onClick={createApp} className="btn btn-primary text-light mx-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <line x1="12" y1="5" x2="12" y2="19" />
-                    <line x1="5" y1="12" x2="19" y2="12" />
-                  </svg>
-                  Create your first app
+                <a onClick={createApp} className="btn text-light mx-1">
+                  Create new app
                 </a>
                 <a
                   href="https://docs.tooljet.io"
                   target="_blank"
-                  className="btn btn-primary text-light mx-1"
+                  className="btn text-light mx-1 empty-import-button"
                   rel="noreferrer"
                   onChange={handleImportApp}
                 >
                   <label>
                     {isImportingApp && <span className="spinner-border spinner-border-sm me-2" role="status"></span>}
-                    Import an app
+                    Import App
                     <input type="file" ref={fileInput} style={{ display: 'none' }} />
                   </label>
                 </a>
-                <a
+                {/* <a
                   href="https://docs.tooljet.io"
                   target="_blank"
                   className="btn btn-primary text-light mx-1"
                   rel="noreferrer"
                 >
                   Read documentation
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
