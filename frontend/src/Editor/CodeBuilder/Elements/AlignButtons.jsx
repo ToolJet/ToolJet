5d3c2f846c57487ae3e2cd0542b8dfcce8335513
Sherfin Shamsudeen <sherfin94@gmail.com>
2022-02-01 11:26:50 +0530

    Add Fx to align property type

--- frontend/src/Editor/CodeBuilder/Elements/AlignButtons.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/AlignButtons.jsx
@@ -1,104 +1,105 @@
 import React from 'react';
-import { ToolTip } from './Components/ToolTip';
+import FxButton from './FxButton';
 
-export const AlignButtons = ({ param, definition, onChange, paramType, componentMeta }) => {
-  const initialValue = definition ? definition.value : '';
-  const paramMeta = componentMeta[paramType][param.name];
-  const displayName = paramMeta.displayName || param.name;
-  const options = paramMeta.options || {};
-
+export const AlignButtons = ({ value, onChange, forceCodeBox, meta }) => {
   function handleOptionChanged(event) {
-    onChange(param, 'value', event.currentTarget.value, paramType);
+    onChange(event.currentTarget.value);
   }
 
   return (
-    <div className={`mb-3 field ${options.className}`}>
-      <ToolTip label={displayName} meta={paramMeta} />
-      <div style={{ display: 'flex', gap: 10 }}>
-        <label className="radio-img">
-          <input
-            type="radio"
-            name="alignment"
-            value="left"
-            onChange={handleOptionChanged}
-            checked={initialValue === 'left'}
-          />
-          <div className="action-icon">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2 4H14M2 8H8.66667M2 12H12"
-                stroke={initialValue == 'left' ? '#fff' : '#8092AC'}
-                strokeWidth="1.5"
-                strokeLinecap="round"
+    <div className="row">
+      <div className="col-10">
+        <div className={`mb-3 field ${meta?.options?.className}`}>
+          <div style={{ display: 'flex', gap: 10 }}>
+            <label className="radio-img">
+              <input
+                type="radio"
+                name="alignment"
+                value="left"
+                onChange={handleOptionChanged}
+                checked={value === 'left'}
               />
-            </svg>
-          </div>
-          <span className="tooltiptext">Left</span>
-        </label>
+              <div className="action-icon">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2 4H14M2 8H8.66667M2 12H12"
+                    stroke={value == 'left' ? '#fff' : '#8092AC'}
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </div>
+              <span className="tooltiptext">Left</span>
+            </label>
 
-        <label className="radio-img">
-          <input
-            type="radio"
-            name="alignment"
-            value="center"
-            onChange={handleOptionChanged}
-            checked={initialValue === 'center'}
-          />
-          <div className="action-icon">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2 4H14M4.66667 8H11.3333M3.33333 12H12.6667"
-                stroke={initialValue == 'center' ? '#fff' : '#8092AC'}
-                strokeWidth="1.5"
-                strokeLinecap="round"
+            <label className="radio-img">
+              <input
+                type="radio"
+                name="alignment"
+                value="center"
+                onChange={handleOptionChanged}
+                checked={value === 'center'}
               />
-            </svg>
-          </div>
-          <span className="tooltiptext">Center</span>
-        </label>
+              <div className="action-icon">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2 4H14M4.66667 8H11.3333M3.33333 12H12.6667"
+                    stroke={value == 'center' ? '#fff' : '#8092AC'}
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </div>
+              <span className="tooltiptext">Center</span>
+            </label>
 
-        <label className="radio-img">
-          <input
-            type="radio"
-            name="alignment"
-            value="right"
-            onChange={handleOptionChanged}
-            checked={initialValue === 'right'}
-          />
-          <div className="action-icon">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2 4H14M7.33333 8H14M4 12H14"
-                stroke={initialValue == 'right' ? '#fff' : '#8092AC'}
-                strokeWidth="1.5"
-                strokeLinecap="round"
+            <label className="radio-img">
+              <input
+                type="radio"
+                name="alignment"
+                value="right"
+                onChange={handleOptionChanged}
+                checked={value === 'right'}
               />
-            </svg>
-          </div>
-          <span className="tooltiptext">Right</span>
-        </label>
+              <div className="action-icon">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2 4H14M7.33333 8H14M4 12H14"
+                    stroke={value == 'right' ? '#fff' : '#8092AC'}
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </div>
+              <span className="tooltiptext">Right</span>
+            </label>
 
-        <label className="radio-img">
-          <input
-            type="radio"
-            name="alignment"
-            value="justify"
-            onChange={handleOptionChanged}
-            checked={initialValue === 'justify'}
-          />
-          <div className="action-icon">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2 8H14M2 12H14M2 4H8H14H2Z"
-                stroke={initialValue == 'justify' ? '#fff' : '#8092AC'}
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+            <label className="radio-img">
+              <input
+                type="radio"
+                name="alignment"
+                value="justify"
+                onChange={handleOptionChanged}
+                checked={value === 'justify'}
               />
-            </svg>
+              <div className="action-icon">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2 8H14M2 12H14M2 4H8H14H2Z"
+                    stroke={value == 'justify' ? '#fff' : '#8092AC'}
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <span className="tooltiptext">Justified</span>
+            </label>
           </div>
-          <span className="tooltiptext">Justified</span>
-        </label>
+        </div>
+      </div>
+      <div className="col-2 pt-1">
+        <FxButton active={false} onPress={forceCodeBox} />
       </div>
     </div>
   );
