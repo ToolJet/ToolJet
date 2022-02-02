315bdf15502f547070191e7b0eba6be521608c30
stepinfwd <stepinfwd@gmail.com>
2022-02-03 03:19:51 +0530

    updating functinal logic for number type

--- frontend/src/Editor/CodeBuilder/Elements/Number.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Number.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FxButton from './FxButton';
 
 export const Number = ({ value, onChange, forceCodeBox }) => {
+  const [number, setNumber] = useState(value);
+
   return (
     <>
       <div className="row">
         <div className="col-10">
           <div className="field mb-3">
             <label className="form-check form-switch my-2">
-              <input type="text" onClick={() => onChange(`{{${value}}}`)} value={value} />
+              <input
+                type="text"
+                onChange={(e) => {
+                  setNumber(e.target.value);
+                  onChange(`{{${e.target.value}}}`);
+                }}
+                value={number}
+              />
             </label>
           </div>
         </div>
