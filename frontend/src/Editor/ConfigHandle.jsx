496222089b214f038c5fc0bf48e8b07b9fef056c
Muhsin Shah <muhsinshah21@gmail.com>
2022-02-03 16:34:22 +0530

    Changed widget handle design

--- frontend/src/Editor/ConfigHandle.jsx
+++ frontend/src/Editor/ConfigHandle.jsx
@@ -27,25 +27,28 @@ export const ConfigHandle = function ConfigHandle({
         className="badge handle-content"
         role="button"
       >
-        <img
-          style={{ cursor: 'pointer', marginRight: '5px' }}
-          src="/assets/images/icons/settings.svg"
-          width="12"
-          height="12"
-          draggable="false"
-        />
-        {component.name}
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            style={{ cursor: 'pointer', marginRight: '5px', verticalAlign: 'middle' }}
+            src="/assets/images/icons/settings.svg"
+            width="12"
+            height="12"
+            draggable="false"
+          />
+          <span>{component.name}</span>
+        </div>
+        <div className="delete-part">
+          <img
+            style={{ cursor: 'pointer', marginLeft: '5px' }}
+            src="/assets/images/icons/trash-light.svg"
+            width="12"
+            role="button"
+            height="12"
+            draggable="false"
+            onClick={() => removeComponent({ id })}
+          />
+        </div>
       </span>
-      <img
-        style={{ cursor: 'pointer', marginRight: '5px' }}
-        src="/assets/images/icons/trash.svg"
-        width="12"
-        role="button"
-        className="mx-2"
-        height="12"
-        draggable="false"
-        onClick={() => removeComponent({ id })}
-      />
     </div>
   );
 };
