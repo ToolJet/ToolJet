9d5e1109a8e4ed8c0b1aa20e7d1d0dba72932cf8
stepinfwd <stepinfwd@gmail.com>
2022-02-03 14:05:53 +0530

    style updated

--- frontend/src/Editor/CodeBuilder/Elements/Number.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Number.jsx
@@ -2,23 +2,28 @@ import React, { useState } from 'react';
 import FxButton from './FxButton';
 
 export const Number = ({ value, onChange, forceCodeBox }) => {
-  const [number, setNumber] = useState(value);
+  const [number, setNumber] = useState(value ? value : 0);
+  const darkMode = localStorage.getItem('darkMode') === 'true';
 
+  const numberTheme = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: darkMode && '#fff',
+  };
   return (
     <>
       <div className="row">
         <div className="col-10">
-          <div className="field mb-3">
-            <label className="form-check form-switch my-2">
-              <input
-                type="text"
-                onChange={(e) => {
-                  setNumber(e.target.value);
-                  onChange(`{{${e.target.value}}}`);
-                }}
-                value={number}
-              />
-            </label>
+          <div className="field mb-3 form-control">
+            <input
+              style={{ numberTheme }}
+              type="text"
+              onChange={(e) => {
+                setNumber(e.target.value);
+                onChange(`{{${e.target.value}}}`);
+              }}
+              value={number}
+            />
           </div>
         </div>
         <div className="col-2 pt-2">
