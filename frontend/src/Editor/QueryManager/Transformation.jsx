b6fcbc44c3973016b5c1b6471bb673f7e1e0a217
Muhsin Shah C P <muhsinshah21@gmail.com>
2022-02-07 22:09:33 +0530

    Fix: Transform label and toggle should be in one row  (#2175)
    * Fixed transform toggle ui
    
    * Changed to correct positions

--- frontend/src/Editor/QueryManager/Transformation.jsx
+++ frontend/src/Editor/QueryManager/Transformation.jsx
@@ -31,16 +31,18 @@ return data.filter(row => row.amount > 1000);`;
 
   return (
     <div className="field mb-2 transformation-editor">
-      <span style={{ fontWeight: 600 }} className="form-check-label">
-        Transformations
-      </span>
-      <div className="form-check form-switch my-2">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          onClick={toggleEnableTransformation}
-          checked={enableTransformation}
-        />
+      <div className="mb-2" style={{ display: 'flex' }}>
+        <div className="form-check form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onClick={toggleEnableTransformation}
+            checked={enableTransformation}
+          />
+        </div>
+        <span style={{ fontWeight: 600 }} className="form-check-label mx-1">
+          Transformations
+        </span>
       </div>
       {!enableTransformation && (
         <Alert svg="circular-info">
