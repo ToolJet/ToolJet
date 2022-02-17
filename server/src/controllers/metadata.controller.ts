eca3704ec2ec7b76e22a4aa28d498c4b345c64e3
Akshay <akshaysasidharan93@gmail.com>
2022-02-17 21:49:53 +0530

    Feature: Adds toojet hub telemetry endpoint (#2283)
    * adds endpoint for telemetry on tooljet hub
    
    * remove optional params
    
    * enable telemetry only for prod env
    
    * remove org scope

--- server/src/controllers/metadata.controller.ts
+++ server/src/controllers/metadata.controller.ts
@@ -13,7 +13,9 @@ export class MetadataController {
     const installedVersion = globalThis.TOOLJET_VERSION;
 
     const metadata = await this.metadataService.getMetaData();
-    await this.metadataService.finishInstallation(metadata, installedVersion, name, email, org);
+    if (process.env.NODE_ENV == 'production') {
+      await this.metadataService.finishInstallation(metadata, installedVersion, name, email, org);
+    }
 
     await this.metadataService.updateMetaData({
       onboarded: true,
@@ -56,10 +58,16 @@ export class MetadataController {
     const updateLastCheckedAt = new Date(data['last_checked'] || null);
     const diffTime = (now.getTime() - updateLastCheckedAt.getTime()) / 1000;
 
-    if (diffTime > 86400) {
-      const result = await this.metadataService.checkForUpdates(installedVersion, ignoredVersion);
-      latestVersion = result.latestVersion;
-      versionIgnored = false;
+    if (diffTime > 86400 && process.env.NODE_ENV == 'production') {
+      if (process.env.CHECK_FOR_UPDATES) {
+        const result = await this.metadataService.checkForUpdates(installedVersion, ignoredVersion);
+        latestVersion = result.latestVersion;
+        versionIgnored = false;
+      }
+
+      if (!process.env.DISABLED_TOOLJET_TELEMETRY) {
+        await this.metadataService.sendTelemetryData(metadata);
+      }
     }
 
     return {
