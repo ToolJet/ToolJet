5a28dadd44dd36af0650e26ac0dfe4ef119dba55
gandharv <gandharvkumargarg@gmail.com>
2022-02-21 14:54:10 +0500

    refactor: create component for walkthrough

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -25,8 +25,6 @@ import {
   setStateAsync,
   computeComponentState,
   getSvgIcon,
-  addToLocalStorage,
-  getDataFromLocalStorage,
 } from '@/_helpers/appUtils';
 import { Confirm } from './Viewer/Confirm';
 import ReactTooltip from 'react-tooltip';
@@ -45,9 +43,8 @@ import DesktopSelectedIcon from './Icons/desktop-selected.svg';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { AppVersionsManager } from './AppVersionsManager';
-import * as Driver from 'driver.js';
-import 'driver.js/dist/driver.min.css';
 import { SearchBoxComponent } from '@/_ui/Search';
+import { initEditorWalkThrough } from '@/_helpers/createWalkThrough';
 
 setAutoFreeze(false);
 enablePatches();
@@ -144,93 +141,6 @@ class Editor extends React.Component {
       currentSidebarTab: 2,
       selectedComponent: null,
     });
-  }
-
-  initWalkThrough() {
-    const driver = new Driver({
-      allowClose: true,
-      closeBtnText: 'Skip',
-      nextBtnText: 'Next',
-      prevBtnText: 'Previous',
-      padding: 2,
-      onReset: () => {
-        // Here we need to write the logic to update walkthroughCompleted column of the current user.
-        addToLocalStorage({ key: 'walkthroughCompleted', value: true });
-      },
-      className: `${this.props.darkMode ? 'dark-theme' : 'light-theme'}-walkthrough`,
-    });
-
-    if (
-      getDataFromLocalStorage('walkthroughCompleted') == undefined ||
-      !getDataFromLocalStorage('walkthroughCompleted')
-    ) {
-      driver.defineSteps([
-        {
-          element: '.component-image-holder',
-          popover: {
-            title: 'Drag and drop widgets',
-            description: 'From the widget sidebar, drag and drop widgets to the canvas.',
-            position: 'left',
-            closeBtnText: 'Skip (1/6)',
-          },
-        },
-        {
-          element: '.sidebar-datasources',
-          popover: {
-            title: 'Connect to data sources',
-            description: 'You can manage your data sources from here.',
-            position: 'right',
-            closeBtnText: 'Skip (2/6)',
-          },
-        },
-        {
-          element: '.left-sidebar-inspector',
-          popover: {
-            title: 'Inspector',
-            description: 'Inspector lets you check the properties of widgets, results of queries etc.',
-            position: 'right',
-            closeBtnText: 'Skip (3/6)',
-          },
-        },
-        {
-          element: '.queries-header ',
-          popover: {
-            title: 'Create queries',
-            description:
-              'Create queries to interact with your data sources, run JavaScript snippets and to make API requests.',
-            position: 'top',
-            closeBtnText: 'Skip (4/6)',
-          },
-        },
-        {
-          element: '.release-buttons',
-          popover: {
-            title: 'Preview, release & share',
-            description:
-              'Click on preview to view the current changes on app viewer. Click on share button to view the sharing options. Release the editing version to make the changes live. Released versions cannot be modified, you will have to create another version to make more changes.',
-            position: 'bottom',
-            closeBtnText: 'Skip (5/6)',
-          },
-        },
-        {
-          element: '.sidebar-comments',
-          popover: {
-            title: 'Collaborate',
-            description: 'Add comments on canvas and tag your team members to collaborate.',
-            position: 'right',
-            closeBtnText: 'Skip (6/6)',
-          },
-        },
-      ]);
-
-      driver.start();
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.editingVersion == undefined && this.state.editingVersion) {
-      this.initWalkThrough();
-    }
   }
 
   isVersionReleased = (version = this.state.editingVersion) => {
@@ -912,12 +822,14 @@ class Editor extends React.Component {
       </span>
     );
   };
+
   handleKeyPress = (event) => {
     if (event.key === 'Enter') {
       // eslint-disable-next-line no-undef
       this.createInitVersion();
     }
   };
+
   createInitVersion = () => {
     const newVersionName = this.state.initVersionName;
     const appId = this.state.appId;
@@ -925,10 +837,15 @@ class Editor extends React.Component {
     if (!isEmpty(newVersionName?.trim())) {
       this.setState({ isCreatingInitVersion: true });
       appVersionService.create(appId, newVersionName).then(() => {
-        this.setState({
-          showInitVersionCreateModal: false,
-          isCreatingInitVersion: false,
-        });
+        this.setState(
+          {
+            showInitVersionCreateModal: false,
+            isCreatingInitVersion: false,
+          },
+          () => {
+            initEditorWalkThrough();
+          }
+        );
         toast.success('Version Created');
         this.fetchApp();
       });
@@ -961,7 +878,6 @@ class Editor extends React.Component {
         enforceFocus={false}
         animation={false}
         centered={true}
-        // eslint-disable-next-line no-undef
       >
         <Modal.Header>
           <Modal.Title>Create Version</Modal.Title>
