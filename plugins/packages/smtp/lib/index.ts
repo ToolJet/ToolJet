614492d6e9a11f0e66dd2b1dd58642316a6fcf28
Muhsin Shah <muhsinshah21@gmail.com>
2022-02-22 10:41:48 +0530

    Fixed minor code issues

--- plugins/packages/smtp/lib/index.ts
+++ plugins/packages/smtp/lib/index.ts
@@ -5,13 +5,7 @@ import nodemailer from 'nodemailer';
 export default class Smtp implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     const nodemailerTransport = await this.getConnection(sourceOptions);
-
-    const from = queryOptions.from;
-    const from_name = queryOptions.from_name;
-    const to = queryOptions.to;
-    const subject = queryOptions.subject;
-    const textContent = queryOptions.textContent;
-    const htmlContent = queryOptions.htmlContent;
+    const { from, to, from_name, subject, textContent, htmlContent } = queryOptions
     const attachments = queryOptions['attachment_array'] && typeof queryOptions['attachment_array'] === 'string' ? JSON.parse(queryOptions['attachment_array']) : queryOptions['attachment_array'];
 
     const filesData = (array:any)=>{
@@ -34,14 +28,8 @@ export default class Smtp implements QueryService {
     };
  
     try {
-      await new Promise((resolve,reject)=>nodemailerTransport.sendMail(mailOptions, (err:any,success:any)=>{
-        if(err){
-          reject(err);
-        }
-        resolve(success);
-      }));
+      await nodemailerTransport.sendMail(mailOptions);
     }catch(error){
-      console.log(error);
       throw new QueryError('Query could not be completed', error.message, {});
     }
 
@@ -59,17 +47,7 @@ export default class Smtp implements QueryService {
     }
 
     try{
-      await new Promise((resolve,reject)=>{
-        transporter.verify(function (error:any, success:any) {
-          if (error) {
-            console.log(error);
-            reject(error);
-          } else {
-            console.log("Server is ready to take our messages");
-            resolve(success);
-          }
-        }
-      )});
+      await transporter.verify();
     }catch(err){
       throw new Error('Invalid credentials');
     }
@@ -80,18 +58,16 @@ export default class Smtp implements QueryService {
   }
 
   async getConnection(sourceOptions: SourceOptions, _options?: object): Promise<any> {
-    const host = sourceOptions.host;
+    const { host, user, password } = sourceOptions;
     const port = Number(sourceOptions.port);
-    const user = sourceOptions.user;
-    const pass = sourceOptions.password;
-    
-    let transport = nodemailer.createTransport({
+
+    const transport = nodemailer.createTransport({
       port,
       host,
       secure: port == 465,
       auth: {
         user,
-        pass,
+        pass:password,
       }
     });
     
