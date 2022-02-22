8c3788ebb3ab642bd11955ac85a16cd9b12c2c6d
arpitnath <arpitnath42@gmail.com>
2022-02-22 16:42:23 +0530

    migrates to react-select: selection dropdown component

--- frontend/src/Editor/CodeBuilder/Elements/Select.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Select.jsx
@@ -1,21 +1,79 @@
 import React from 'react';
-import SelectSearch, { fuzzySearch } from 'react-select-search';
+import SelectComponent from 'react-select';
 import FxButton from './FxButton';
 
 export const Select = ({ value, onChange, forceCodeBox, meta }) => {
   const darkMode = localStorage.getItem('darkMode') === 'true';
+
+  const options = meta.options.map((option) => {
+    return {
+      value: option.value,
+      label: option.name,
+    };
+  });
+
+  const selectStyles = {
+    container: (provided) => ({
+      ...provided,
+      width: '100%',
+      height: 32,
+    }),
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: darkMode ? '#2b3547' : '#fff',
+      height: '32px!important',
+      minHeight: '32px!important',
+    }),
+    valueContainer: (provided, _state) => ({
+      ...provided,
+      height: 32,
+      marginBottom: '4px',
+    }),
+    indicatorsContainer: (provided, _state) => ({
+      ...provided,
+      height: 32,
+    }),
+    indicatorSeparator: (_state) => ({
+      display: 'none',
+    }),
+    input: (provided) => ({
+      ...provided,
+      color: darkMode ? '#fff' : '#232e3c',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 2,
+      backgroundColor: darkMode ? 'rgb(31,40,55)' : 'white',
+    }),
+    option: (provided) => ({
+      ...provided,
+      backgroundColor: darkMode ? '#2b3547' : '#fff',
+      color: darkMode ? '#fff' : '#232e3c',
+      ':hover': {
+        backgroundColor: darkMode ? '#323C4B' : '#d8dce9',
+      },
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: darkMode ? '#fff' : '#808080',
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: darkMode ? '#fff' : '#232e3c',
+    }),
+  };
+
   return (
     <div className="row">
       <div className="col">
         <div className="field mb-3">
-          <SelectSearch
-            options={meta.options}
+          <SelectComponent
+            options={options}
             value={value}
             search={true}
             onChange={onChange}
-            filterOptions={fuzzySearch}
             placeholder="Select.."
-            className={`${darkMode ? 'select-search-dark' : 'select-search'}`}
+            styles={selectStyles}
           />
         </div>
       </div>
