afd0fdb8a091f84e8ee18add068f0d2bc7824e5e
stepinfwd <stepinfwd@gmail.com>
2022-02-23 18:00:13 +0530

    crud updates

--- plugins/packages/couchdb/lib/index.ts
+++ plugins/packages/couchdb/lib/index.ts
@@ -1,13 +1,7 @@
 import { QueryError, QueryResult, QueryService } from "@tooljet-plugins/common";
 import { SourceOptions, QueryOptions } from "./types";
 import got, { Headers } from "got";
-
 export default class Couchdb implements QueryService {
-
-  authHeader(): Headers {
-    return { Authorization: `Basic {btoa("admin":"password")}` };
-  }
-
   async run(
     sourceOptions: SourceOptions,
     queryOptions: QueryOptions
@@ -18,36 +12,40 @@ export default class Couchdb implements QueryService {
     const recordId = queryOptions.record_id;
     const username = sourceOptions.username;
     const password = sourceOptions.password;
+    const port = sourceOptions.port;
 
+    const authHeader = () => {
+      const combined = `${username}:${password}`;
+      const key = Buffer.from(combined).toString("base64");
+      return { Authorization: `Basic ${key}` };
+    };
 
     try {
       switch (operation) {
         case "list_records": {
           response = await got(`http://127.0.0.1:5984/users`, {
             method: "get",
-            headers: this.authHeader(),
+            headers:{ Authorization: `Basic YWRtaW46cGFzc3dvcmQ=` },
           });
           result = JSON.parse(response.body);
+          console.log("checkkkkkkkkkkkkk,result",result)
           break;
         }
 
         case "retrieve_record": {
-          response = await got(
-            `http://127.0.0.1:5984/users/${recordId}`,
-            {
-              headers: this.authHeader(),
-              method: "get",
-            }
-          );
+          response = await got(`http://127.0.0.1:${port}/users/${recordId}`, {
+            headers: { Authorization: `Basic YWRtaW46cGFzc3dvcmQ=` },
+            method: "get",
+          });
 
           result = JSON.parse(response.body);
           break;
         }
 
         case "create_record": {
-          response = await got(`http://127.0.0.1:5984/users`, {
+          response = await got(`http://127.0.0.1:${port}/users`, {
             method: "post",
-            headers: this.authHeader(),
+            headers: authHeader(),
             json: JSON.parse(queryOptions.body),
           });
           result = JSON.parse(response.body);
@@ -55,32 +53,26 @@ export default class Couchdb implements QueryService {
         }
 
         case "update_record": {
-          response = await got(
-            `http://127.0.0.1:5984/users/${recordId}`,
-            {
-              method: "put",
-              headers: this.authHeader(),
-              json: {
-                category: JSON.parse(queryOptions.body),
-              },
-            }
-          );
+          response = await got(`http://127.0.0.1:${port}/users/${recordId}`, {
+            method: "put",
+            headers: authHeader(),
+            json: {
+              category: JSON.parse(queryOptions.body),
+            },
+          });
 
           result = JSON.parse(response.body);
           break;
         }
 
         case "delete_record": {
-          response = await got(
-            `http://127.0.0.1:5984/users/${recordId}`,
-            {
-              method: "delete",
-              headers: this.authHeader(),
-              json: {
-                _rev: queryOptions.rev_id,
-              },
-            }
-          );
+          response = await got(`http://127.0.0.1:${port}/users/${recordId}`, {
+            method: "delete",
+            headers: authHeader(),
+            json: {
+              _rev: queryOptions.rev_id,
+            },
+          });
           result = JSON.parse(response.body);
           break;
         }
