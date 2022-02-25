3e9d16fa916a4934d91d4a6c7656160d83b83b1d
Muhsin Shah <muhsinshah21@gmail.com>
2022-02-25 17:25:25 +0530

    Implemented connection and operations

--- plugins/packages/oracledb/lib/types.ts
+++ plugins/packages/oracledb/lib/types.ts
@@ -1,4 +1,13 @@
-export type SourceOptions = {};
-export type QueryOptions = {
+export type SourceOptions = { 
+  database: string; 
+  host: string; 
+  port: string; 
+  username: string;
+  password: string;
+  ssl_enabled: boolean; 
+};
+export type QueryOptions = { 
   operation: string;
+  query: string;
+  mode: string;
 };
