83d334b79db16e86de94b36c02dca12521858025
stepinfwd <stepinfwd@gmail.com>
2022-03-01 01:34:35 +0530

    marking optional params

--- plugins/packages/couchdb/lib/types.ts
+++ plugins/packages/couchdb/lib/types.ts
@@ -8,14 +8,13 @@ export type SourceOptions = {
 };
 export type QueryOptions = {
   operation: string;
-  doc_name: string;
   record_id: string;
   body: string;
   rev_id:string;
   view_url:string;
-  start_key:string;
-  end_key:string;
-  limit:string;
-  skip:string;
-  descending:boolean;
+  start_key?:string;
+  end_key?:string;
+  limit?:string;
+  skip?:string;
+  descending?:boolean;
 };
