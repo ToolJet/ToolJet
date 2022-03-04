f018d3adcc73b5ebe12243161f08f964210adbb2
Navaneeth Pk <navaneeth@tooljet.io>
2022-03-04 16:48:15 +0530

    Fix the order of migrations

--- server/migrations/1639734070614-AddApplicationIconColumn.ts
+++ server/migrations/1639734070614-AddApplicationIconColumn.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export class AddApplicationIconColumn1641809680591 implements MigrationInterface {
+export class AddApplicationIconColumn1639734070614 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
       'apps',
