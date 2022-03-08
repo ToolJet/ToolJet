ad99fff1d09236f9de9d5ce3740aacaf60c1e270
Sherfin Shamsudeen <sherfin94@gmail.com>
2022-03-08 13:31:20 +0530

    Do not create application_icon column if it already exists

--- server/migrations/1639734070614-AddApplicationIconColumn.ts
+++ server/migrations/1639734070614-AddApplicationIconColumn.ts
@@ -1,15 +1,8 @@
-import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddApplicationIconColumn1639734070614 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn(
-      'apps',
-      new TableColumn({
-        name: 'icon',
-        type: 'varchar',
-        isNullable: true,
-      })
-    );
+    await queryRunner.query(`ALTER TABLE "apps" ADD COLUMN IF NOT EXISTS "icon" VARCHAR(255) DEFAULT NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
