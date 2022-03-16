041fdbcd126c518b33d489eb2a63cb7a42f69a82
Gandharv <gandharvkumargarg@gmail.com>
2022-03-16 21:42:24 +0500

    Add tooltip for fx button (#2531)

--- frontend/src/Editor/CodeBuilder/Elements/FxButton.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/FxButton.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 export default function FxButton({ active, onPress }) {
   return (
-    <div className={`fx-button ${active ? 'active' : ''} unselectable`} onClick={onPress}>
+    <div
+      title="Use fx for property to have a programmatically determined value instead of a fixed value"
+      className={`fx-button ${active ? 'active' : ''} unselectable`}
+      onClick={onPress}
+    >
       Fx
     </div>
   );
