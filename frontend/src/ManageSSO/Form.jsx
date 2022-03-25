20a3d422a63b58f6838737b719d65ae17606aa1f
gsmithun4 <gsmithun4@gmail.com>
2022-03-26 03:18:53 +0530

    Remove enable signup for form login

--- frontend/src/ManageSSO/Form.jsx
+++ frontend/src/ManageSSO/Form.jsx
@@ -4,30 +4,7 @@ import { toast } from 'react-hot-toast';
 
 export function Form({ settings, updateData }) {
   const [enabled, setEnabled] = useState(settings?.enabled || false);
-  const [enableSignUp, setEnableSignUp] = useState(settings?.configs?.enable_sign_up || false);
-  const [isSaving, setSaving] = useState(false);
-  const single_organization = window.public_config?.MULTI_ORGANIZATION !== 'true';
 
-  const reset = () => {
-    setEnableSignUp(settings?.configs?.enable_sign_up || false);
-  };
-  const saveSettings = () => {
-    setSaving(true);
-    organizationService.editOrganizationConfigs({ type: 'form', configs: { enableSignUp } }).then(
-      (data) => {
-        updateData('form', { id: data.id, configs: { enable_sign_up: enableSignUp } });
-        toast.success('updated sso configurations', {
-          position: 'top-center',
-        });
-      },
-      () => {
-        toast.error('Error saving sso configurations', {
-          position: 'top-center',
-        });
-      }
-    );
-    setSaving(false);
-  };
   const changeStatus = () => {
     organizationService.editOrganizationConfigs({ type: 'form', enabled: !enabled }).then(
       (data) => {
@@ -61,36 +38,6 @@ export function Form({ settings, updateData }) {
           </div>
         </div>
       </div>
-      {single_organization && (
-        <div className="card-body">
-          <form noValidate>
-            <div className="form-group mb-3">
-              <label className="form-check form-switch">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  onChange={() => setEnableSignUp((enableSignUp) => !enableSignUp)}
-                  checked={enableSignUp}
-                />
-                <span className="form-check-label">Enable signup</span>
-              </label>
-            </div>
-            <div className="form-footer">
-              <button type="button" className="btn btn-light mr-2" onClick={reset}>
-                Cancel
-              </button>
-              <button
-                type="button"
-                className={`btn mx-2 btn-primary ${isSaving ? 'btn-loading' : ''}`}
-                disabled={isSaving}
-                onClick={saveSettings}
-              >
-                Save
-              </button>
-            </div>
-          </form>
-        </div>
-      )}
     </div>
   );
 }
