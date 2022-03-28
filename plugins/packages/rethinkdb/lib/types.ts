31029bbafd7c21879ca32f2fe7d1312d7fe90f36
stepinfwd <stepinfwd@gmail.com>
2022-03-28 19:15:04 +0530

    types intial

---
+++ plugins/packages/rethinkdb/lib/types.ts
@@ -0,0 +1,12 @@
+export type SourceOptions = {
+  username: string;
+  database: string;
+  port: string;
+  host: string;
+  protocol: string;
+  ssl: string;
+  password: string;
+};
+export type QueryOptions = {
+  operation: string;
+};
