1d67752aca3c184fe3a95fd9c1e538ac70da5afc
Kiran Ashok <stepinfwd@gmail.com>
2022-03-31 15:14:45 +0530

    Bugfix ::  Fixing alignment of FX button  (#2438)
    * fx alignment debugging
    
    * updates
    
    * fixing positioning
    
    * fixing positioning
    
    * resolving undefined issue
    
    * fixing positioning in style tab pane
    
    * code cleanup
    
    * pr changes :: aligning fx with label
    
    * adjusting margin

--- frontend/src/Editor/CodeBuilder/Elements/Number.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Number.jsx
@@ -12,7 +12,7 @@ export const Number = ({ value, onChange, forceCodeBox }) => {
   };
   return (
     <>
-      <div className="row">
+      <div className="row fx-container">
         <div className="col">
           <div className="field form-control" style={{ padding: '0.225rem 0.35rem' }}>
             <input
@@ -26,7 +26,7 @@ export const Number = ({ value, onChange, forceCodeBox }) => {
             />
           </div>
         </div>
-        <div className="col-auto pt-2">
+        <div className="col-auto pt-2 style-fx fx-common">
           <FxButton active={false} onPress={forceCodeBox} />
         </div>
       </div>
