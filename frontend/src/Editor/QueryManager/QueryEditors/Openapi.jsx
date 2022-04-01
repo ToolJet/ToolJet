2a5902e474fdf9d434ec17ef876558e468c9c9a6
Muhsin Shah <muhsinshah21@gmail.com>
2022-04-01 14:53:32 +0530

    Added params and body ui

--- frontend/src/Editor/QueryManager/QueryEditors/Openapi.jsx
+++ frontend/src/Editor/QueryManager/QueryEditors/Openapi.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { openapiService } from '@/_services';
 import SelectSearch, { fuzzySearch } from 'react-select-search';
 import DOMPurify from 'dompurify';
+import { CodeHinter } from '../../CodeBuilder/CodeHinter';
 
 const operationColorMapping = {
   get: 'azure',
@@ -20,6 +21,7 @@ class Openapi extends React.Component {
           path: {},
           query: {},
           request: {},
+          headers: {},
         },
       },
     };
@@ -99,8 +101,46 @@ class Openapi extends React.Component {
     return options;
   };
 
+  changeParam = (paramType, paramName, value) => {
+    const options = this.state.options;
+    const newOptions = {
+      ...options,
+      params: {
+        ...options.params,
+        [paramType]: {
+          ...options.params[paramType],
+          [paramName]: value,
+        },
+      },
+    };
+
+    this.setState({
+      options: newOptions,
+    });
+
+    this.props.optionsChanged(newOptions);
+  };
+
   render() {
     const { options, spec, loadingSpec, selectedOperation } = this.state;
+    let pathParams = [];
+    let headerParams = [];
+    let queryParams = [];
+    let requestBody = [];
+
+    if (selectedOperation) {
+      if (selectedOperation.parameters) {
+        pathParams = selectedOperation.parameters.filter((param) => param.in === 'path');
+        queryParams = selectedOperation.parameters.filter((param) => param.in === 'query');
+        headerParams = selectedOperation.parameters.filter((param) => param.in === 'header');
+      }
+
+      if (selectedOperation.requestBody) {
+        const requestType = Object.keys(selectedOperation.requestBody.content)[0];
+        requestBody = selectedOperation.requestBody.content[requestType];
+      }
+    }
+
     return (
       <div>
         {loadingSpec && (
@@ -116,7 +156,7 @@ class Openapi extends React.Component {
               <div className="col-12">
                 <label className="form-label pt-2">Operation</label>
               </div>
-              <div className="col stripe-operation-options" style={{ width: '90px', marginTop: 0 }}>
+              <div className="col openapi-operation-options">
                 <SelectSearch
                   options={this.computeOperationSelectionOptions(spec.paths)}
                   value="sv"
@@ -129,13 +169,162 @@ class Openapi extends React.Component {
 
                 {selectedOperation && (
                   <small
-                    style={{ margintTop: '10px' }}
-                    className="my-2"
-                    dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(selectedOperation.description) }}
+                    className="mt-4 mb-2"
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(selectedOperation.description ?? selectedOperation.summary),
+                    }}
                   />
                 )}
               </div>
             </div>
+          </div>
+        )}
+
+        {selectedOperation && (
+          <div className="row mt-2">
+            {headerParams.length > 0 && (
+              <div className="mt-2">
+                <h5 className="text-muted">HEADER</h5>
+                {headerParams.map((param) => (
+                  <div className="row input-group my-1" key={param.name}>
+                    <div className="col-4 field field-width-268">
+                      <input type="text" value={param.name} className="form-control" placeholder="key" />
+                    </div>
+                    <div className="col-6 field" style={{ width: '300px' }}>
+                      <CodeHinter
+                        currentState={this.props.currentState}
+                        initialValue={this.state.options.params.path[param.name]}
+                        mode="text"
+                        placeholder={'value'}
+                        theme={this.props.darkMode ? 'monokai' : 'duotone-light'}
+                        lineNumbers={false}
+                        onChange={(value) => this.changeParam('path', param.name, value)}
+                        height={'36px'}
+                        width="268px"
+                      />
+                    </div>
+                    <span className="btn-sm col-2 mt-2" role="button">
+                      <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.99931 6.97508L11.0242 12.0014L12 11.027L6.9737 6.00069L12 0.975767L11.0256 0L5.99931 5.0263L0.974388 0L0 0.975767L5.02492 6.00069L0 11.0256L0.974388 12.0014L5.99931 6.97508Z"
+                          fill="#8092AC"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {pathParams.length > 0 && (
+              <div className="mt-2">
+                <h5 className="text-muted">PATH</h5>
+                {pathParams.map((param) => (
+                  <div className="row input-group my-1" key={param.name}>
+                    <div className="col-4 field field-width-268">
+                      <input type="text" value={param.name} className="form-control" placeholder="key" />
+                    </div>
+                    <div className="col-6 field" style={{ width: '300px' }}>
+                      <CodeHinter
+                        currentState={this.props.currentState}
+                        initialValue={this.state.options.params.path[param.name]}
+                        mode="text"
+                        placeholder={'value'}
+                        theme={this.props.darkMode ? 'monokai' : 'duotone-light'}
+                        lineNumbers={false}
+                        onChange={(value) => this.changeParam('path', param.name, value)}
+                        height={'36px'}
+                        width="268px"
+                      />
+                    </div>
+                    <span className="btn-sm col-2 mt-2" role="button">
+                      <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.99931 6.97508L11.0242 12.0014L12 11.027L6.9737 6.00069L12 0.975767L11.0256 0L5.99931 5.0263L0.974388 0L0 0.975767L5.02492 6.00069L0 11.0256L0.974388 12.0014L5.99931 6.97508Z"
+                          fill="#8092AC"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {queryParams.length > 0 && (
+              <div className="mt-2">
+                <h5 className="text-muted">QUERY</h5>
+                {queryParams.map((param) => (
+                  <div className="row input-group my-1" key={param.name}>
+                    <div className="col-4 field field-width-268">
+                      <input type="text" value={param.name} className="form-control" placeholder="key" disabled />
+                    </div>
+                    <div className="col-6 field" style={{ width: '300px' }}>
+                      <CodeHinter
+                        currentState={this.props.currentState}
+                        initialValue={this.state.options.params.query[param.name]}
+                        mode="text"
+                        placeholder={'value'}
+                        theme={this.props.darkMode ? 'monokai' : 'duotone-light'}
+                        lineNumbers={false}
+                        onChange={(value) => this.changeParam('query', param.name, value)}
+                        height={'36px'}
+                        width="268px"
+                      />
+                    </div>
+                    <span className="btn-sm col-2 mt-2" role="button">
+                      <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.99931 6.97508L11.0242 12.0014L12 11.027L6.9737 6.00069L12 0.975767L11.0256 0L5.99931 5.0263L0.974388 0L0 0.975767L5.02492 6.00069L0 11.0256L0.974388 12.0014L5.99931 6.97508Z"
+                          fill="#8092AC"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {requestBody?.schema?.properties && (
+              <div className="mt-2">
+                <h5 className="text-muted">REQUEST BODY</h5>
+                {Object.keys(requestBody.schema.properties).map((param) => (
+                  <div className="row input-group my-1" key={param.name}>
+                    <div className="col-4 field field-width-268">
+                      <input type="text" value={param} className="form-control" placeholder="key" disabled />
+                    </div>
+                    <div className="col-6 field" style={{ width: '300px' }}>
+                      <CodeHinter
+                        currentState={this.props.currentState}
+                        initialValue={this.state.options.params.request[param.name]}
+                        mode="text"
+                        placeholder={'value'}
+                        theme={this.props.darkMode ? 'monokai' : 'duotone-light'}
+                        lineNumbers={false}
+                        onChange={(value) => this.changeParam('request', param.name, value)}
+                        height={'36px'}
+                        width="268px"
+                      />
+                    </div>
+                    <span className="btn-sm col-2 mt-2" role="button">
+                      <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.99931 6.97508L11.0242 12.0014L12 11.027L6.9737 6.00069L12 0.975767L11.0256 0L5.99931 5.0263L0.974388 0L0 0.975767L5.02492 6.00069L0 11.0256L0.974388 12.0014L5.99931 6.97508Z"
+                          fill="#8092AC"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         )}
       </div>
