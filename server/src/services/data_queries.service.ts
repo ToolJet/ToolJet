75123b8e28dc42d65476c352498c43316df092a8
Navaneeth Pk <navaneeth@tooljet.io>
2022-04-03 11:22:25 +0530

    Fixes sheets

--- server/src/services/data_queries.service.ts
+++ server/src/services/data_queries.service.ts
@@ -93,7 +93,7 @@ export class DataQueriesService {
     try {
       return await service.run(sourceOptions, parsedQueryOptions, dataSource.id, dataSource.updatedAt);
     } catch (error) {
-      const statusCode = error?.data?.responseObject.statusCode;
+      const statusCode = error?.data?.responseObject?.statusCode;
 
       if (
         error.constructor.name === 'OAuthUnauthorizedClientError' ||
