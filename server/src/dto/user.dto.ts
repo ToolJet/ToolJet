0e857c76ea52192a92e7aea831b15c4e4cba7d18
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2022-04-07 14:35:50 +0530

    update user dto

--- server/src/dto/user.dto.ts
+++ server/src/dto/user.dto.ts
@@ -5,16 +5,19 @@ import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateUserDto {
   @IsString()
+  @IsOptional()
   @IsNotEmpty()
   @Transform(({ value }) => sanitizeInput(value))
   first_name: string;
 
   @IsString()
+  @IsOptional()
   @IsNotEmpty()
   @Transform(({ value }) => sanitizeInput(value))
   last_name: string;
 
   @IsString()
+  @IsOptional()
   @IsNotEmpty()
   @Transform(({ value }) => sanitizeInput(value))
   organization: string;
