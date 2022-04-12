0790f782335983f03acfe79b20f2720e4cc6727a
ajith-k-v <ajith.jaban@gmail.com>
2022-04-12 21:39:47 +0530

    Added cypress test for login functionality

--- cypress-test/cypress/support/commands.js
+++ cypress-test/cypress/support/commands.js
@@ -1,25 +1,19 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { loginSelectors} from "Selectors/login";
+
+Cypress.Commands.add("login",(email,password)=>{
+    cy.visit("/");
+    cy.clearAndType(loginSelectors.emailField, email);
+    cy.clearAndType(loginSelectors.passwordField, password);
+    cy.get(loginSelectors.signInButton).click();
+    cy.get(loginSelectors.homePage).should("be.visible");
+})
+
+Cypress.Commands.add("clearAndType", (selector, text) => {
+    cy.get(selector).clear().type(text);
+  });
+
+  Cypress.Commands.add("verifyToastMessage", (selector,message) => {
+    cy.get(selector)
+      .should("be.visible")
+      .should("have.text", message);
+})
