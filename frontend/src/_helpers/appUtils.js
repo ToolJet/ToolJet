fda71f04e01f99f3cc9f30b89060936e82fde869
Sherfin Shamsudeen <sherfin94@gmail.com>
2022-04-13 15:42:00 +0530

    Ensure listItem is exposed for set/unset custom variables handler (#2813)

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -290,26 +290,26 @@ function executeAction(_ref, event, mode, customVariables) {
       case 'set-custom-variable': {
         const key = resolveReferences(event.key, _ref.state.currentState, undefined, customVariables);
         const value = resolveReferences(event.value, _ref.state.currentState, undefined, customVariables);
-        const customVariables = { ..._ref.state.currentState.variables };
-        customVariables[key] = value;
+        const customAppVariables = { ..._ref.state.currentState.variables };
+        customAppVariables[key] = value;
 
         return _ref.setState({
           currentState: {
             ..._ref.state.currentState,
-            variables: customVariables,
+            variables: customAppVariables,
           },
         });
       }
 
       case 'unset-custom-variable': {
         const key = resolveReferences(event.key, _ref.state.currentState, undefined, customVariables);
-        const customVariables = { ..._ref.state.currentState.variables };
-        delete customVariables[key];
+        const customAppVariables = { ..._ref.state.currentState.variables };
+        delete customAppVariables[key];
 
         return _ref.setState({
           currentState: {
             ..._ref.state.currentState,
-            variables: customVariables,
+            variables: customAppVariables,
           },
         });
       }
