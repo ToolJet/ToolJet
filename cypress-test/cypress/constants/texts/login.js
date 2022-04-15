06872a3194f441d2fb6a34efaccd07fc94ae3ee4
Ajith KV <ajith.jaban@gmail.com>
2022-04-15 14:29:17 +0530

    Cypress test for login functionality (#2801)
    * Added cypress and other denpendencies
    
    * Added data-cy attribute for elements
    
    * Added cypress test for login functionality
    
    * Reverted unwanted files

---
+++ cypress-test/cypress/constants/texts/login.js
@@ -0,0 +1,11 @@
+export const loginTexts={
+    cardTitle:"Login to your account",
+    emailLabel:"Email address",
+    passwordLabel:"Password",
+    forgotPassword:"Forgot password",
+    showPassword:"show password",
+    signIn:"Sign in",
+    signUpText:"Don't have account yet?",
+    signUpLink:"Sign up",
+    toastMessage:"Invalid email or password",
+}
