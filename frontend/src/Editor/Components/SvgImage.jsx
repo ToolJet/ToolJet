dcaf931e95583f406cad6e5d87202b4d14bc76ad
stepinfwd <stepinfwd@gmail.com>
2022-04-18 01:45:02 +0530

    fixing overflow issue

--- frontend/src/Editor/Components/SvgImage.jsx
+++ frontend/src/Editor/Components/SvgImage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import DOMPurify from 'dompurify';
 
-export const SvgImage = function Timeline({ properties, styles }) {
+export const SvgImage = function Timeline({ properties, styles, height }) {
   const { visibility } = styles;
   const { data } = properties;
   return (
-    <div style={{ display: visibility ? '' : 'none', overflow: 'hidden' }}>
-      <div
-        // style={{ display: visibility ? '' : 'none', overflow: 'hidden' }}
-        style={{ overflow: 'hidden' }}
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data) }}
-      ></div>
+    <div style={{ display: visibility ? '' : 'none', overflow: 'hidden', height: height }}>
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data) }}></div>
     </div>
   );
 };
