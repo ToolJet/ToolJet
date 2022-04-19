78c4724eddc3888cbc3723cc6b7be1f39467b650
stepinfwd <stepinfwd@gmail.com>
2022-04-19 21:37:39 +0530

    removing unwanted types

--- plugins/packages/woocommerce/lib/types.ts
+++ plugins/packages/woocommerce/lib/types.ts
@@ -1,13 +1,10 @@
 export type SourceOptions = {
   consumer_key: string;
   consumer_secret: string;
-  port: string;
   host: string;
-  protocol: string;
 };
 export type QueryOptions = {
   operation: string;
-  key: string;
   body: any;
   product_id: string;
   order_id: string;
