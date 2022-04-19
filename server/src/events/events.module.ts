40420ee59112e6f92aa945ed32c12bded762ae40
Gandharv <gandharvkumargarg@gmail.com>
2022-04-19 16:07:39 +0530

    feat: Add realtime collaboration (#2517)
    * feat: add realtime collaboration
    
    * reduce delay in debounce
    
    * feat: add realtime cursors
    
    * feat: add realtime avatars
    
    * add current user to the online users list
    
    * add border color to the avatar
    
    * feat: allow multi user to edit multi-versions simultaneously
    
    * add getOne fn for fetching single api + avatar component
    
    * fix: set appDef from api call
    
    * replace webrtc with ws
    
    * move yjs to separate port (temp)
    
    * run on same port
    
    * fix: update ymap when styles have changed
    
    * - Add static list of colors
    - Show the username on hover
    
    * fix: cursors not showing up
    
    * fix: review comments
    
    * remove unused vars
    
    * add new cursor style
    
    * - Run yjs connection on a different path
    - Add zindex to cursor such that query editor has precedence
    
    * fix: event emitter bug + position bug
    
    * use shorthand
    
    * cursor style changes
    
    * fix cursor positions
    
    * inc zindex
    
    * change max avatars to 3

--- server/src/events/events.module.ts
+++ server/src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { YjsGateway } from './yjs.gateway';
 import { AuthModule } from 'src/modules/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  providers: [EventsGateway],
+  providers: [EventsGateway, YjsGateway],
 })
 export class EventsModule {}
