2e86c12a2a835bdf2a6ee615ad25af9f914160b9
gsmithun4 <gsmithun4@gmail.com>
2022-04-21 02:20:24 +0530

    migration fixes

--- server/migrations/1650485473528-PopulateSSOConfigs.ts
+++ server/migrations/1650485473528-PopulateSSOConfigs.ts
@@ -2,15 +2,19 @@ import { Organization } from 'src/entities/organization.entity';
 import { SSOConfigs } from 'src/entities/sso_config.entity';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class PopulateSSOConfigs1648589182504 implements MigrationInterface {
+export class PopulateSSOConfigs1650485473528 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const entityManager = queryRunner.manager;
     const OrganizationRepository = entityManager.getRepository(Organization);
+
+    const enableSignUp = process.env.SSO_DISABLE_SIGNUP !== 'true';
+    const domain = process.env.SSO_RESTRICTED_DOMAIN;
 
     const organizations: Organization[] = await OrganizationRepository.find({ relations: ['ssoConfigs'] });
 
     if (organizations && organizations.length > 0) {
       for (const organization of organizations) {
+        await OrganizationRepository.update({ id: organization.id }, { enableSignUp, ...(domain ? { domain } : {}) });
         // adding form configs for organizations which does not have any
         if (
           !organization.ssoConfigs?.some((og) => {
