1f5e4eaf9c26fec8cded23ee2e066da7260a3654
gandharv <gandharvkumargarg@gmail.com>
2022-04-29 13:09:41 +0500

    added style

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1022,8 +1022,6 @@ class Editor extends React.Component {
                   closeCreateVersionModalPrompt={this.closeCreateVersionModalPrompt}
                 />
               )}
-
-              <div className="layout-buttons cursor-pointer">{this.renderLayoutIcon(currentLayout === 'desktop')}</div>
               <div className="navbar-nav flex-row order-md-last release-buttons">
                 <div className="nav-item dropdown d-none d-md-flex me-2">
                   <a
@@ -1310,9 +1308,11 @@ class Editor extends React.Component {
             </div>
             <div className="editor-sidebar">
               <div className="editor-actions col-md-12">
+                <div className="layout-buttons cursor-pointer">
+                  {this.renderLayoutIcon(currentLayout === 'desktop')}
+                </div>
                 <div>
                   <svg
-                    title="undo"
                     onClick={this.handleUndo}
                     xmlns="http://www.w3.org/2000/svg"
                     className={cx('cursor-pointer icon icon-tabler icon-tabler-arrow-back-up', {
@@ -1327,8 +1327,12 @@ class Editor extends React.Component {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1" />
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none">
+                      <title>undo</title>
+                    </path>
+                    <path d="M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1">
+                      <title>undo</title>
+                    </path>
                   </svg>
                   <svg
                     title="redo"
