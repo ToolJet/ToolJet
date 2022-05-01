1b38be0016e48d8f7b6dfd41bb914b3911a4a269
Manish Kushare <kushare.manish9@gmail.com>
2022-05-01 15:25:36 +0530

    added HTML widget

---
+++ frontend/src/Editor/Components/Html.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import DOMPurify from 'dompurify';
+
+export const Html = function ({ width, height, properties, styles, darkMode }) {
+  const { rawHtml: stringifyHTML, rawCSS: stringifyCSS } = properties;
+  const css = stringifyCSS
+    .split('}')
+    .reduce((acc, cv) => {
+      if (cv) {
+        acc.push(`.custom_css ${cv}}`);
+      }
+      return acc;
+    }, [])
+    .join('');
+  console.log(css);
+  const baseStyle = stringifyCSS
+    ? null
+    : {
+        backgroundColor: darkMode ? '#47505D' : '#ffffff',
+        color: darkMode ? 'white' : 'black',
+      };
+  console.log(baseStyle);
+  const { visibility } = styles;
+
+  const [rawHtml, setRawHtml] = useState('');
+  useEffect(() => {
+    console.log(css);
+    setRawHtml(stringifyHTML + `<style>${css}</style>`);
+  }, [stringifyHTML, stringifyCSS]);
+
+  return (
+    <div style={{ display: visibility ? '' : 'none', width: '100%', height, overflowY: 'auto' }}>
+      {
+        <div
+          className="custom_css"
+          style={baseStyle}
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHtml, { FORCE_BODY: true }) }}
+        />
+      }
+    </div>
+  );
+};
--- frontend/src/Editor/Components/components.js
+++ frontend/src/Editor/Components/components.js
@@ -2023,4 +2023,46 @@ export const componentTypes = [
       },
     },
   },
+  {
+    name: 'Html',
+    displayName: 'HTML Viewer',
+    description: 'HTML Viewer',
+    component: 'Html',
+    defaultSize: {
+      width: 10,
+      height: 310,
+    },
+    properties: {
+      rawHtml: { type: 'code', displayName: 'Raw HTML' },
+      rawCSS: { type: 'code', displayName: 'Raw CSS' },
+    },
+    others: {
+      showOnDesktop: { type: 'toggle', displayName: 'Show on desktop' },
+      showOnMobile: { type: 'toggle', displayName: 'Show on mobile' },
+    },
+    events: {},
+    styles: {
+      visibility: { type: 'toggle', displayName: 'Visibility' },
+    },
+    exposedVariables: {},
+    definition: {
+      others: {
+        showOnDesktop: { value: '{{true}}' },
+        showOnMobile: { value: '{{false}}' },
+      },
+      properties: {
+        rawHtml: {
+          value: `<body><main><section class="hero" style="height:310px;display: flex;
+          justify-content: center;padding:0 1px;align-items: center;text-align:center">You can build your custom HTML-CSS template here</section></main></body>`,
+        },
+        rawCSS: {
+          value: '',
+        },
+      },
+      events: [],
+      styles: {
+        visibility: { value: '{{true}}' },
+      },
+    },
+  },
 ];
