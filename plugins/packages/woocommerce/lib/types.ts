26c8a55972c59185d237604920d08333b839dc32
stepinfwd <stepinfwd@gmail.com>
2022-05-02 15:18:33 +0530

    updating all additional param types

--- plugins/packages/woocommerce/lib/types.ts
+++ plugins/packages/woocommerce/lib/types.ts
@@ -13,14 +13,43 @@ export type QueryOptions = {
 
   // customer params
   context?: string;
-  page?: string;
-  per_page?: string;
+  page?: number;
+  per_page?: number;
   search?: string;
-  exclude?: string;
-  include?: string;
-  offset?: string;
+  exclude?: Array<string>;
+  include?: Array<string>;
+  offset?: number;
   order?: string;
   orderby?: string;
   email?: string;
   role?: string;
+
+  //[product params]
+  after?: string;
+  before?: string;
+  parent?: Array<string>;
+  parent_exclude?: Array<string>;
+  slug?: string;
+  status?: Array<string>;
+  type?: string;
+  sku?: string;
+  featured?: boolean;
+  category?: string;
+  tag?: string;
+  shipping_class?: string;
+  attribute?: string;
+  attribute_term?: string;
+  tax_class?: string;
+  on_sale?: boolean;
+  min_price?: string;
+  max_price?: string;
+  stock_status?: string;
+
+  // [order params]
+  customer?: string;
+  product?: number;
+  dp?: number;
+
+  // coupon params
+  code?: string;
 };
