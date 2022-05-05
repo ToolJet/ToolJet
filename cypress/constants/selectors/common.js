1bc3781b632466a08b73c64bca04a1adddd0ed61
ajith-k-v <ajith.jaban@gmail.com>
2022-05-05 20:27:25 +0530

    Updated common selectors

--- cypress/constants/selectors/common.js
+++ cypress/constants/selectors/common.js
@@ -1,3 +1,11 @@
 export const commonSelectors={
-    toastMessage: ".go318386747"
+    toastMessage: ".go318386747",
+    appCard: "[data-cy=app-card]",
+    editButton: "[data-cy=edit-button]",
+    searchField:"[data-cy=widget-search-box]",
+    firstWidget:"[data-cy=widget-list]:eq(0)",
+    canvas:"[data-cy=real-canvas]",
+    appCardOptions: "[data-cy=app-card-menu-icon]",
+    deleteApp: "[data-cy=card-options] :nth-child(5)>span",
+    confirmButton: "[data-cy=confirm-yes-button]",
 }
