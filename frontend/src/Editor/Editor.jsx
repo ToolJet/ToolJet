8878a094eb27ddc89f8d8f46196d39b1a0f45bb3
Gandharv <gandharvkumargarg@gmail.com>
2022-05-05 17:22:14 +0530

    Added undo redo actions to right sidebar (#2973)
    * style: add undo-redo action buttons
    
    * added style
    
    * dark mode fixes
    
    * reverse order
    
    * update ymap on undo-redo

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-named-as-default */
 import React, { createRef } from 'react';
+import cx from 'classnames';
 import { datasourceService, dataqueryService, appService, authenticationService, appVersionService } from '@/_services';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -462,9 +463,17 @@ class Editor extends React.Component {
       this.canRedo = true;
 
       if (!appDefinition) return;
-      this.setState({
-        appDefinition,
-      });
+      this.setState(
+        {
+          appDefinition,
+        },
+        () => {
+          this.props.ymap.set('appDef', {
+            newDefinition: appDefinition,
+            editingVersionId: this.state.editingVersion?.id,
+          });
+        }
+      );
     }
   };
 
@@ -479,9 +488,17 @@ class Editor extends React.Component {
       this.canRedo = this.currentVersionChanges.hasOwnProperty(this.currentVersion + 1);
 
       if (!appDefinition) return;
-      this.setState({
-        appDefinition,
-      });
+      this.setState(
+        {
+          appDefinition,
+        },
+        () => {
+          this.props.ymap.set('appDef', {
+            newDefinition: appDefinition,
+            editingVersionId: this.state.editingVersion?.id,
+          });
+        }
+      );
     }
   };
 
@@ -1022,8 +1039,6 @@ class Editor extends React.Component {
                   closeCreateVersionModalPrompt={this.closeCreateVersionModalPrompt}
                 />
               )}
-
-              <div className="layout-buttons cursor-pointer">{this.renderLayoutIcon(currentLayout === 'desktop')}</div>
               <div className="navbar-nav flex-row order-md-last release-buttons">
                 <div className="nav-item dropdown d-none d-md-flex me-2">
                   <a
@@ -1318,10 +1333,56 @@ class Editor extends React.Component {
               </div>
             </div>
             <div className="editor-sidebar">
-              <div className="col-md-12">
-                <div></div>
+              <div className="editor-actions col-md-12">
+                <div className="m-auto undo-redo-buttons">
+                  <svg
+                    onClick={this.handleUndo}
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={cx('cursor-pointer icon icon-tabler icon-tabler-arrow-back-up', {
+                      disabled: !this.canUndo,
+                    })}
+                    width="44"
+                    height="44"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke={this.props.darkMode ? '#fff' : '#2c3e50'}
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none">
+                      <title>undo</title>
+                    </path>
+                    <path d="M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1" fill="none">
+                      <title>undo</title>
+                    </path>
+                  </svg>
+                  <svg
+                    title="redo"
+                    onClick={this.handleRedo}
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={cx('cursor-pointer icon icon-tabler icon-tabler-arrow-forward-up', {
+                      disabled: !this.canRedo,
+                    })}
+                    width="44"
+                    height="44"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke={this.props.darkMode ? '#fff' : '#2c3e50'}
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none">
+                      <title>redo</title>
+                    </path>
+                    <path d="M15 13l4 -4l-4 -4m4 4h-11a4 4 0 0 0 0 8h1" />
+                  </svg>
+                </div>
+                <div className="layout-buttons cursor-pointer">
+                  {this.renderLayoutIcon(currentLayout === 'desktop')}
+                </div>
               </div>
-
               {currentSidebarTab === 1 && (
                 <div className="pages-container">
                   {selectedComponent &&
