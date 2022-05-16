1ee7e8ada024a0c5e7c257c7912b3730c920d905
ajith-k-v <ajith.jaban@gmail.com>
2022-05-16 10:31:20 +0530

    Added data-cy attribute to empty dashboard elements

--- frontend/src/HomePage/BlankPage.jsx
+++ frontend/src/HomePage/BlankPage.jsx
@@ -23,29 +23,43 @@ export const BlankPage = function BlankPage({
                 <img
                   src={darkMode ? '/assets/images/dark-mode-onboarding.svg' : '/assets/images/onboarding.svg'}
                   alt=""
+                  data-cy="empty-img"
                 />
               </div>
-              <h3 className="empty-welcome-header" style={{ color: darkMode && '#ffffff' }}>
+              <h3
+                className="empty-welcome-header"
+                style={{ color: darkMode && '#ffffff' }}
+                data-cy="empty-welcome-header"
+              >
                 Welcome to ToolJet!
               </h3>
-              <p className={`empty-title ${darkMode && 'text-white-50'}`}>
+              <p className={`empty-title ${darkMode && 'text-white-50'}`} data-cy="empty-description">
                 You can get started by creating a new application or by creating an application using a template in
                 ToolJet Library.
               </p>
               <div className="empty-action">
-                <a onClick={createApp} className={`btn btn-primary ${creatingApp ? 'btn-loading' : ''}`}>
+                <a
+                  onClick={createApp}
+                  className={`btn btn-primary ${creatingApp ? 'btn-loading' : ''}`}
+                  data-cy="create-new-application"
+                >
                   Create new application
                 </a>
                 <a
                   className={`btn empty-import-button ${isImportingApp ? 'btn-loading' : ''}`}
                   onChange={handleImportApp}
                 >
-                  <label style={{ visibility: isImportingApp ? 'hidden' : 'visible' }}>
+                  <label style={{ visibility: isImportingApp ? 'hidden' : 'visible' }} data-cy="import-an-application">
                     Import an application
                     <input type="file" ref={fileInput} style={{ display: 'none' }} />
                   </label>
                 </a>
-                <a onClick={viewTemplateLibraryModal} className="btn btn-primary" style={{ marginLeft: '24px' }}>
+                <a
+                  onClick={viewTemplateLibraryModal}
+                  className="btn btn-primary"
+                  style={{ marginLeft: '24px' }}
+                  data-cy="choose-from-template"
+                >
                   Choose from template
                 </a>
               </div>
