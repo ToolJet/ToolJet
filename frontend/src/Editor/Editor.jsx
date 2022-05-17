d1422d93a5828caeef77c166c1888a64dde8dcf1
Akshay <akshaysasidharan93@gmail.com>
2022-05-17 11:10:52 +0530

    Always enable preview button (#3061)

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1175,9 +1175,7 @@ class Editor extends React.Component {
                   <a
                     href={appVersionPreviewLink}
                     target="_blank"
-                    className={`btn btn-sm font-500 color-primary border-0  ${
-                      app?.current_version_id ? '' : 'disabled'
-                    }`}
+                    className="btn btn-sm font-500 color-primary border-0"
                     rel="noreferrer"
                   >
                     Preview
