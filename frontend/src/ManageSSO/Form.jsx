6bd75444bf751be4e39341f600831e541c99dd30
Sam Kohl <Samkohl@Samkohl.com>
2022-05-16 20:48:14 -0700

    Fixed missing ) after temperate literal string change

--- frontend/src/ManageSSO/Form.jsx
+++ frontend/src/ManageSSO/Form.jsx
@@ -11,33 +11,33 @@ export function Form({ settings, updateData }) {
         const enabled_tmp = !enabled;
         setEnabled(enabled_tmp);
         updateData('form', { id: data.id, enabled: enabled_tmp });
-        toast.success(`${enabled_tmp ? 'Enabled' : 'Disabled'} Password login`, {
+        toast.success(`${enabled_tmp ? 'Enabled' : 'Disabled'} Password login`), {
           position: 'top-center',
         });
-      },
-      () => {
-        toast.error('Error saving sso configurations', {
-          position: 'top-center',
-        });
-      }
+  },
+  () => {
+    toast.error('Error saving sso configurations', {
+      position: 'top-center',
+    });
+  }
     );
-  };
+};
 
-  return (
-    <div className="card">
-      <div className="card-header">
-        <div className="d-flex justify-content-between title-with-toggle">
-          <div className="card-title">
-            Password Login
-            <span className={`badge bg-${enabled ? 'green' : 'grey'} ms-1`}>{enabled ? 'Enabled' : 'Disabled'}</span>
-          </div>
-          <div>
-            <label className="form-check form-switch">
-              <input className="form-check-input" type="checkbox" checked={enabled} onChange={changeStatus} />
-            </label>
-          </div>
+return (
+  <div className="card">
+    <div className="card-header">
+      <div className="d-flex justify-content-between title-with-toggle">
+        <div className="card-title">
+          Password Login
+          <span className={`badge bg-${enabled ? 'green' : 'grey'} ms-1`}>{enabled ? 'Enabled' : 'Disabled'}</span>
+        </div>
+        <div>
+          <label className="form-check form-switch">
+            <input className="form-check-input" type="checkbox" checked={enabled} onChange={changeStatus} />
+          </label>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 }
