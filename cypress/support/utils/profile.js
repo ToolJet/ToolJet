59a1cfa3074b1527bbc6d2d644e599ea4eebce1e
ajith-k-v <ajith.jaban@gmail.com>
2022-05-24 12:49:48 +0530

    Added common utils file

--- cypress/support/utils/profile.js
+++ cypress/support/utils/profile.js
@@ -1,18 +1,5 @@
-import { path } from "Texts/common";
 import { profileSelector } from "Selectors/profile";
 import { profileText } from "Texts/profile";
-
-export const navigateToProfile=()=>{
- cy.get(profileSelector.profileDropdown).invoke("show");
- cy.contains("Profile").click();
- cy.url().should("include", path.profilePath);
-};
-
-export const logout=()=>{
- cy.get(profileSelector.profileDropdown).invoke("show");
- cy.contains("Logout").click();
- cy.url().should("include",path.loginPath);
-};
 
 export const profilePageElements = () =>{
  for(const elements in profileSelector.profileElements ){
