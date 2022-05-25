2b526552504e65136e44b6edaa252a4b3dad4f65
arpitnath <arpitnath42@gmail.com>
2022-05-25 16:23:15 +0530

    handles editing card title

--- frontend/src/Editor/Components/KanbanBoard/Card.jsx
+++ frontend/src/Editor/Components/KanbanBoard/Card.jsx
@@ -4,6 +4,24 @@ import { Draggable } from 'react-beautiful-dnd';
 export const Card = ({ item, index, state, updateCb, getItemStyle, keyIndex }) => {
   const [isHovered, setIsHovered] = React.useState(false);
 
+  const updateCardTitle = (newTitle, colIndex, cardIndex) => {
+    const newState = [...state];
+    newState[colIndex]['cards'][cardIndex]['title'] = newTitle;
+    updateCb(newState);
+  };
+
+  const flipTitleToEditMode = (colIndex, cardIndex) => {
+    const newState = [...state];
+    const isEditing = newState[colIndex]['cards'][cardIndex]['isEditing'];
+
+    if (isEditing === true) {
+      newState[colIndex]['cards'][cardIndex]['isEditing'] = false;
+    } else {
+      newState[colIndex]['cards'][cardIndex]['isEditing'] = true;
+    }
+    updateCb(newState);
+  };
+
   return (
     <Draggable key={item.id} draggableId={item.id} index={index}>
       {(dndProps, dndState) => (
@@ -19,8 +37,34 @@ export const Card = ({ item, index, state, updateCb, getItemStyle, keyIndex }) =
           {console.log('dndProps => ', dndProps, 'snapshot =>', dndState)}
 
           <div className="card-body d-flex">
-            <span className="text-muted flex-grow-1"> {item.title}</span>
-            {isHovered && (
+            {item.isEditing ? (
+              <input
+                style={{ height: 20, padding: 0 }}
+                type="text"
+                className="form-control form-control-flush flex-grow-1"
+                name="Form control flush"
+                defaultValue={item.title}
+                onBlur={(e) => {
+                  updateCardTitle(e.target.value, keyIndex, index);
+                  flipTitleToEditMode(keyIndex, index);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateCardTitle(e.target.value, keyIndex, index);
+                    flipTitleToEditMode(keyIndex, index);
+                  }
+                }}
+              />
+            ) : (
+              <span
+                onClick={() => flipTitleToEditMode(keyIndex, index)}
+                className="text-muted flex-grow-1 cursor-pointer"
+              >
+                {' '}
+                {item.title}
+              </span>
+            )}
+            {isHovered && !item.isEditing && (
               <span
                 className="cursor-pointer"
                 type="btn btn-sm btn-danger"
