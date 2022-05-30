8950e43bc38ee442984984371ef5f089a9b3c58a
stepinfwd <stepinfwd@gmail.com>
2022-05-30 15:00:06 +0530

    fixing stripe post refund bug

--- plugins/packages/stripe/lib/index.ts
+++ plugins/packages/stripe/lib/index.ts
@@ -2,18 +2,7 @@ import { QueryError, QueryResult, QueryService } from '@tooljet-plugins/common';
 import got, { Headers } from 'got';
 import { SourceOptions } from './types';
 import Stripe from 'stripe';
-const stripe = new Stripe(
-  'sk_test_51KPPFsSIMIhrzXWAUfJ82xsqvqFYBaTT9tyqX75ji2j5Hp8fGm4G7dxL40k6vhH7W9vnJBXffhRw43FFDKa3TjFU00Yt7oEwoo',
-  {
-    apiVersion: '2020-08-27',
-  }
-);
-// const stripe = new Stripe(
-//   'sk_test_51KPPFsSIMIhrzXWAUfJ82xsqvqFYBaTT9tyqX75ji2j5Hp8fGm4G7dxL40k6vhH7W9vnJBXffhRw43FFDKa3TjFU00Yt7oEwoo',
-//   {
-//     apiVersion: '2020-08-27',
-//   }
-// );
+
 export default class StripeQueryService implements QueryService {
   authHeader(token: string): Headers {
     return { Authorization: `Bearer ${token}` };
@@ -22,7 +11,9 @@ export default class StripeQueryService implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: any, dataSourceId: string): Promise<QueryResult> {
     let result = {};
     const operation = queryOptions.operation;
-
+    const stripe = new Stripe(`${sourceOptions.api_key}`, {
+      apiVersion: '2020-08-27',
+    });
     const apiKey = sourceOptions.api_key;
     const baseUrl = 'https://api.stripe.com';
     const path = queryOptions['path'];
@@ -36,31 +27,37 @@ export default class StripeQueryService implements QueryService {
     for (const param of Object.keys(pathParams)) {
       url = url.replace(`{${param}}`, pathParams[param]);
     }
+    console.log('path poram************', pathParams, queryParams, queryOptions['params']['request']);
 
     let response = null;
+    if (url == 'https://api.stripe.com/v1/refunds' && operation === 'post') {
+      console.log('ENTRY DONE ________________++++++++++');
 
-    try {
-      if (operation === 'get') {
-        response = await got(url, {
-          method: operation,
-          headers: this.authHeader(apiKey),
-          searchParams: queryParams,
-        });
-        console.log('***', response);
-      } else {
-        const refund = await stripe.refunds.retrieve('re_3L4rLk2eZvKYlo2C1enubJKW');
-        console.log('_____&&&&!!!!!!!!', refund);
-        response = await got(url, {
-          method: operation,
-          headers: this.authHeader(apiKey),
-          json: bodyParams,
-          searchParams: queryParams,
-        });
+      const refund = await stripe.refunds.create(queryOptions['params']['request']);
+      console.log('refund', refund);
+    } else {
+      console.log('ENTRY DONE%%%%%%%%%%%%%%%%%%%');
+
+      try {
+        if (operation === 'get') {
+          response = await got(url, {
+            method: operation,
+            headers: this.authHeader(apiKey),
+            searchParams: queryParams,
+          });
+        } else {
+          response = await got(url, {
+            method: operation,
+            headers: this.authHeader(apiKey),
+            json: bodyParams,
+            searchParams: queryParams,
+          });
+        }
+
+        result = JSON.parse(response.body);
+      } catch (error) {
+        throw new QueryError('Query could not be completed', error.message, {});
       }
-
-      result = JSON.parse(response.body);
-    } catch (error) {
-      throw new QueryError('Query could not be completed', error.message, {});
     }
 
     return {
