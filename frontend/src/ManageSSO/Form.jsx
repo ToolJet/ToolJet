cb75e3faa81832d2a50c711cc255d8e3f8f0e846
Sam Kohl <Samkohl@Samkohl.com>
2022-05-30 18:40:20 -0700

    Removed unneccesary ) causing lint error

--- frontend/src/ManageSSO/Form.jsx
+++ frontend/src/ManageSSO/Form.jsx
@@ -13,31 +13,31 @@ export function Form({ settings, updateData }) {
         updateData('form', { id: data.id, enabled: enabled_tmp });
         toast.success(`${enabled_tmp ? 'Enabled' : 'Disabled'} Password login`), {
           position: 'top-center',
+        };
+      },
+      () => {
+        toast.error('Error saving sso configurations', {
+          position: 'top-center',
         });
-  },
-  () => {
-    toast.error('Error saving sso configurations', {
-      position: 'top-center',
-    });
-  }
+      }
     );
-};
+  };
 
-return (
-  <div className="card">
-    <div className="card-header">
-      <div className="d-flex justify-content-between title-with-toggle">
-        <div className="card-title">
-          Password Login
-          <span className={`badge bg-${enabled ? 'green' : 'grey'} ms-1`}>{enabled ? 'Enabled' : 'Disabled'}</span>
-        </div>
-        <div>
-          <label className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" checked={enabled} onChange={changeStatus} />
-          </label>
+  return (
+    <div className="card">
+      <div className="card-header">
+        <div className="d-flex justify-content-between title-with-toggle">
+          <div className="card-title">
+            Password Login
+            <span className={`badge bg-${enabled ? 'green' : 'grey'} ms-1`}>{enabled ? 'Enabled' : 'Disabled'}</span>
+          </div>
+          <div>
+            <label className="form-check form-switch">
+              <input className="form-check-input" type="checkbox" checked={enabled} onChange={changeStatus} />
+            </label>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
