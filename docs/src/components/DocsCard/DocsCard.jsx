a383d4ed17cff7c95a5f9c74d343f3434d633012
Manish Kushare <kushare.manish9@gmail.com>
2022-06-02 17:54:18 +0530

    made the card body vertically stack in the mobile view

--- docs/src/components/DocsCard/DocsCard.jsx
+++ docs/src/components/DocsCard/DocsCard.jsx
@@ -8,15 +8,15 @@ export const DocsCard = ({label, imgSrc, link, height=40, width=40,title}) => {
   const imagePath = imgSrc.includes('kubernetes') ? kubernetesSvg : `/img/setup/icons/${imgSrc}.svg`
 
   const description = {
-    Docker: "Follow the steps below to deploy ToolJet on a server using docker-compose. This setup will deploy both ToolJet server and ToolJet client.",
-    Heroku: "Follow the steps below to deploy ToolJet on Heroku:",
+    Docker: "You should setup a PostgreSQL database manually to be used by the ToolJet server.",
+    Heroku: "You should setup a PostgreSQL database manually to be used by the ToolJet server.",
     "AWS EC2": "You should setup a PostgreSQL database manually to be used by the ToolJet server.",
     Kubernetes :"You should setup a PostgreSQL database manually to be used by ToolJet.",
     "Kubernetes (GKE)":"You should setup a PostgreSQL database manually to be used by ToolJet.",
     "Kubernetes (AKS)":"You should setup a PostgreSQL database manually to be used by ToolJet.",
     "Google Cloud Run":"You should setup a PostgreSQL database manually to be used by ToolJet.",
-    "Deploying ToolJet client":"ToolJet client is a standalone application and can be deployed on static website hosting services such as Netlify, Firebase, S3/Cloudfront, etc.",
-    "Environment variables":"Both the ToolJet server and client requires some environment variables to start running."
+    "Deploying ToolJet client":"You should setup a PostgreSQL database manually to be used by the ToolJet server.",
+    "Environment variables":"You should setup a PostgreSQL database manually to be used by the ToolJet server."
   }
 
   return (
@@ -24,12 +24,12 @@ export const DocsCard = ({label, imgSrc, link, height=40, width=40,title}) => {
     <a href={link} className="card" style={{textDecoration:"none", color: "inherit"}}>
       <div className="card-body">
           <div  className="card-icon">
-            <img src={imagePath} width="100%" />
+            <img className='img' src={imagePath} width="100%" />
           </div>
           <div className="card-info">
               <h3 style={{margin:"0", paddingBottom:"0.5rem"}}>{label}</h3>
               <p>
-                {description[label].slice(0,50)+"..."}
+                {description[label]}
               </p>
           </div>
         </div>
