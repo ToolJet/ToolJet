cf6e2a09b8fdee3040e3c819cb12ce71d60e44cd
stepinfwd <stepinfwd@gmail.com>
2022-06-03 19:49:13 +0530

    active item logic

--- frontend/src/Editor/Components/Steps.jsx
+++ frontend/src/Editor/Components/Steps.jsx
@@ -1,23 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
 export const Steps = function Button({ height, properties, styles, fireEvent, setExposedVariable }) {
-  const { values, labels, label, defaultSelected, multiSelection } = properties;
+  const { currentStep, stepsSelectable, steps } = properties;
   const { color, theme, visibility, disabledState } = styles;
 
+  const [activeStep, setActiveStep] = useState();
+
+  const activeStepHandler = (id) => {
+    const active = steps.filter((item) => item.id == id);
+    console.log(active);
+    setActiveStep(active[0].id);
+  };
+
+  useEffect(() => {
+    console.log('step', activeStep);
+  }, [activeStep]);
+
   return (
-    <div className="steps">
-      <a href="#" className="step-item">
-        Step 1
-      </a>
-      <a href="#" className="step-item">
-        Step 2
-      </a>
-      <a href="#" className="step-item active">
-        Step 3
-      </a>
-      <span href="#" className="step-item">
-        Step 4
-      </span>
+    <div className={`steps steps-widget`}>
+      {steps.map((item) => (
+        <a
+          key={item.id}
+          href="#"
+          className={`step-item ${item.id == activeStep && 'active'}`}
+          data-bs-toggle="tooltip"
+          title="Step 1 description"
+          onClick={() => stepsSelectable && activeStepHandler(item.id)}
+        >
+          {item.name}
+        </a>
+      ))}
     </div>
   );
 };
