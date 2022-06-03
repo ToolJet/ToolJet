67c0daa0adb4cd0a8081d4cf0937f5000fd17aab
Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
2022-06-03 17:47:43 +0530

    [Feature]: Inline style support for custom component (#3076)
    * Inline style support for custom component
    
    * Added skipResolve check

--- frontend/src/Editor/CodeBuilder/CodeHinter.jsx
+++ frontend/src/Editor/CodeBuilder/CodeHinter.jsx
@@ -60,6 +60,7 @@ export function CodeHinter({
   fieldMeta,
   onFxPress,
   fxActive,
+  hideSuggestion = false,
 }) {
   const darkMode = localStorage.getItem('darkMode') === 'true';
   const options = {
@@ -116,6 +117,7 @@ export function CodeHinter({
   }, [wrapperRef, isFocused, isPreviewFocused, currentValue, prevCountRef, isOpen]);
 
   let suggestions = useMemo(() => {
+    if (hideSuggestion) return [];
     return getSuggestionKeys(realState);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [realState.components, realState.queries]);
--- frontend/src/Editor/component-properties-resolution.js
+++ frontend/src/Editor/component-properties-resolution.js
@@ -5,7 +5,11 @@ export const resolveProperties = (component, currentState, defaultValue, customR
     return Object.entries(component.definition.properties).reduce(
       (properties, entry) => ({
         ...properties,
-        ...{ [entry[0]]: resolveReferences(entry[1].value, currentState, defaultValue, customResolvables) },
+        ...{
+          [entry[0]]: entry[1]?.skipResolve
+            ? entry[1].value
+            : resolveReferences(entry[1].value, currentState, defaultValue, customResolvables),
+        },
       }),
       {}
     );
@@ -17,7 +21,9 @@ export const resolveStyles = (component, currentState, defaultValue, customResol
     const styles = component.definition.styles;
     return Object.entries(styles).reduce((resolvedStyles, entry) => {
       const key = entry[0];
-      const value = resolveReferences(entry[1].value, currentState, defaultValue, customResolvables);
+      const value = entry[1]?.skipResolve
+        ? entry[1].value
+        : resolveReferences(entry[1].value, currentState, defaultValue, customResolvables);
       return {
         ...resolvedStyles,
         ...{ [key]: value },
@@ -33,7 +39,9 @@ export const resolveGeneralProperties = (component, currentState, defaultValue, 
     const generalProperties = component.definition?.general ?? {};
     return Object.entries(generalProperties).reduce((resolvedGeneral, entry) => {
       const key = entry[0];
-      const value = resolveReferences(entry[1].value, currentState, defaultValue, customResolvables);
+      const value = entry[1]?.skipResolve
+        ? entry[1].value
+        : resolveReferences(entry[1].value, currentState, defaultValue, customResolvables);
       return {
         ...resolvedGeneral,
         ...{ [key]: value },
