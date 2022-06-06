11eec5ba84d155a26e510a03e26b3468cf0bb27e
Muhsin Shah <muhsinshah21@gmail.com>
2022-06-06 19:15:11 +0530

    Edited update and delete spec cases

--- server/test/controllers/folders.e2e-spec.ts
+++ server/test/controllers/folders.e2e-spec.ts
@@ -295,50 +295,110 @@ describe('folders controller', () => {
   });
 
   describe('PUT /api/folders/:id', () => {
-    it('should update the specific folder in an organization', async () => {
+    it('should be able to update an existing folder if group is admin or has update permission in the same organization', async () => {
       const adminUserData = await createUser(nestApp, {
         email: 'admin@tooljet.io',
+        groups: ['all_users', 'admin'],
       });
-      const { user } = adminUserData;
+      const developerUserData = await createUser(nestApp, {
+        email: 'dev@tooljet.io',
+        groups: ['all_users', 'developer'],
+        organization: adminUserData.organization,
+      });
+
+      const viewerUserData = await createUser(nestApp, {
+        email: 'viewer@tooljet.io',
+        groups: ['viewer', 'all_users'],
+        organization: adminUserData.organization,
+      });
+
+      const developerGroup = await getManager().findOneOrFail(GroupPermission, {
+        where: { group: 'developer' },
+      });
+
+      await getManager().update(GroupPermission, developerGroup.id, {
+        folderUpdate: true,
+      });
 
       const folder = await getManager().save(Folder, {
         name: 'Folder1',
         organizationId: adminUserData.organization.id,
       });
 
-      const response = await request(nestApp.getHttpServer())
+      for (const userData of [adminUserData, developerUserData]) {
+        await request(nestApp.getHttpServer())
+          .put(`/api/folders/${folder.id}`)
+          .set('Authorization', authHeaderForUser(userData.user))
+          .send({ name: 'My folder' })
+          .expect(200);
+
+        const updatedFolder = await getManager().findOne(Folder, folder.id);
+
+        expect(updatedFolder.name).toEqual('My folder');
+      }
+
+      await request(nestApp.getHttpServer())
         .put(`/api/folders/${folder.id}`)
-        .set('Authorization', authHeaderForUser(user))
-        .send({ name: 'My folder' });
-
-      const updatedFolder = await getManager().findOne(Folder, folder.id);
-
-      expect(response.statusCode).toBe(200);
-      expect(updatedFolder.name).toEqual('My folder');
+        .set('Authorization', authHeaderForUser(viewerUserData.user))
+        .send({ name: 'My folder' })
+        .expect(403);
     });
   });
 
   describe('DELETE /api/folders/:id', () => {
-    it('should delete the specific folder in an organization', async () => {
+    it('should be able to delete an existing folder if group is admin or has delete permission in the same organization', async () => {
       const adminUserData = await createUser(nestApp, {
         email: 'admin@tooljet.io',
+        groups: ['all_users', 'admin'],
       });
-      const { user } = adminUserData;
+      const developerUserData = await createUser(nestApp, {
+        email: 'dev@tooljet.io',
+        groups: ['all_users', 'developer'],
+        organization: adminUserData.organization,
+      });
+
+      const viewerUserData = await createUser(nestApp, {
+        email: 'viewer@tooljet.io',
+        groups: ['viewer', 'all_users'],
+        organization: adminUserData.organization,
+      });
+
+      const developerGroup = await getManager().findOneOrFail(GroupPermission, {
+        where: { group: 'developer' },
+      });
+
+      await getManager().update(GroupPermission, developerGroup.id, {
+        folderDelete: true,
+      });
+
+      for (const userData of [adminUserData, developerUserData]) {
+        const folder = await getManager().save(Folder, {
+          name: 'Folder1',
+          organizationId: adminUserData.organization.id,
+        });
+
+        const preCount = await getManager().count(Folder);
+
+        await request(nestApp.getHttpServer())
+          .delete(`/api/folders/${folder.id}`)
+          .set('Authorization', authHeaderForUser(userData.user))
+          .send()
+          .expect(200);
+
+        const postCount = await getManager().count(Folder);
+        expect(postCount).toEqual(preCount - 1);
+      }
 
       const folder = await getManager().save(Folder, {
         name: 'Folder1',
         organizationId: adminUserData.organization.id,
       });
 
-      const preCount = await getManager().count(Folder);
-      const response = await request(nestApp.getHttpServer())
+      await request(nestApp.getHttpServer())
         .delete(`/api/folders/${folder.id}`)
-        .set('Authorization', authHeaderForUser(user))
-        .send();
-      const postCount = await getManager().count(Folder);
-
-      expect(response.statusCode).toBe(200);
-      expect(postCount).toEqual(preCount - 1);
+        .set('Authorization', authHeaderForUser(viewerUserData.user))
+        .send()
+        .expect(403);
     });
   });
 
