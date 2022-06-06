31c3c75d0edbe1ce6f6d69f42f69ce1c8ab85c88
gsmithun4 <gsmithun4@gmail.com>
2022-06-07 01:14:42 +0530

    Fixes

--- server/src/services/organization_users.service.ts
+++ server/src/services/organization_users.service.ts
@@ -8,6 +8,7 @@ import { BadRequestException } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { Organization } from 'src/entities/organization.entity';
 import { GroupPermission } from 'src/entities/group_permission.entity';
+import { ConfigService } from '@nestjs/config';
 const uuid = require('uuid');
 
 @Injectable()
@@ -16,7 +17,8 @@ export class OrganizationUsersService {
     @InjectRepository(OrganizationUser)
     private organizationUsersRepository: Repository<OrganizationUser>,
     private usersService: UsersService,
-    private emailService: EmailService
+    private emailService: EmailService,
+    private configService: ConfigService
   ) {}
 
   async findOrganization(id: string): Promise<OrganizationUser> {
@@ -79,7 +81,10 @@ export class OrganizationUsersService {
         status: 'invited',
         invitationToken,
       });
-      await manager.update(User, organizationUser.userId, { password: uuid.v4() });
+
+      if (this.configService.get<string>('DISABLE_MULTI_WORKSPACE') === 'true') {
+        await manager.update(User, organizationUser.userId, { password: uuid.v4() });
+      }
     });
 
     const updatedUser = await this.usersService.findOne(organizationUser.userId);
