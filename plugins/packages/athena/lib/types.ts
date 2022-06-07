4fddb131933c8683451cbdd94115864690ce2b91
stepinfwd <stepinfwd@gmail.com>
2022-06-07 16:47:08 +0530

    updates

--- plugins/packages/athena/lib/types.ts
+++ plugins/packages/athena/lib/types.ts
@@ -2,6 +2,8 @@ export type SourceOptions = {
   access_key: string;
   secret_key: string;
   region: string;
+  output_location: string;
+  database: string;
 };
 export type QueryOptions = {
   operation: string;
