5ca7448b30c21e3a6398afc7bca7c264432a6342
stepinfwd <stepinfwd@gmail.com>
2022-06-14 13:57:58 +0530

    testing

--- plugins/packages/athena/lib/index.ts
+++ plugins/packages/athena/lib/index.ts
@@ -16,18 +16,19 @@ export default class Athena implements QueryService {
 
     const athenaExpressConfig = {
       aws: AWS,
-      s3: sourceOptions.output_location,
       getStats: true,
+      db: 'mydatabase',
     };
 
     const athenaExpress = new AthenaExpress(athenaExpressConfig);
     const myQuery = {
-      sql: queryOptions.query,
-      db: sourceOptions.database,
+      sql: `CREATE DATABASE clickstreams`,
+      getStats: true,
     };
 
     try {
-      result = await athenaExpress.query(myQuery);
+      console.log(JSON.stringify(myQuery, (_, v) => (typeof v === 'bigint' ? v.toString() : v)));
+      result = await athenaExpress.query('SHOW TABLES');
       console.log('result ::: ', result);
     } catch (error) {
       throw new QueryError('Query could not be completed', error.message, {});
@@ -56,7 +57,7 @@ export default class Athena implements QueryService {
     AWS.config.update(awsCredentials);
     const athenaExpress = new AthenaExpress(awsCredentials);
     const myQuery = {
-      sql: 'SELECT * FROM Customers',
+      sql: `SELECT os, COUNT(*) count FROM cloudfront_logs WHERE date BETWEEN date 2014-07-05 AND date 2014-08-05 GROUP BY os`,
       db: sourceOptions.database,
     };
     const client = athenaExpress
@@ -68,5 +69,8 @@ export default class Athena implements QueryService {
         throw new Error('Invalid credentials');
       });
     return { client };
+  }
+  private toJson(data) {
+    return JSON.stringify(data, (_, v) => (typeof v === 'bigint' ? `${v}n` : v)).replace(/"(-?\d+)n"/g, (_, a) => a);
   }
 }
