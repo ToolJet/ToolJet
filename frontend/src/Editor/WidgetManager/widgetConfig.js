22f9e1f97c75758ea6702ef8797f4c0e1bf04ead
Manish Kushare <kushare.manish9@gmail.com>
2022-06-16 17:58:11 +0530

    added HTML-CSS widget  (#2938)
    * added HTML widget
    
    * removed console
    
    * removed injecting css to the html page
    
    * Shorten the div of default code for HTML widget
    
    * Updated widget icon from tabler icons
    
    Co-authored-by: Sherfin Shamsudeen <sherfin94@gmail.com>

--- frontend/src/Editor/WidgetManager/widgetConfig.js
+++ frontend/src/Editor/WidgetManager/widgetConfig.js
@@ -2114,6 +2114,44 @@ export const widgets = [
     },
   },
   {
+    name: 'Html',
+    displayName: 'HTML Viewer',
+    description: 'HTML Viewer',
+    component: 'Html',
+    defaultSize: {
+      width: 10,
+      height: 310,
+    },
+    properties: {
+      rawHtml: { type: 'code', displayName: 'Raw HTML' },
+    },
+    others: {
+      showOnDesktop: { type: 'toggle', displayName: 'Show on desktop' },
+      showOnMobile: { type: 'toggle', displayName: 'Show on mobile' },
+    },
+    events: {},
+    styles: {
+      visibility: { type: 'toggle', displayName: 'Visibility' },
+    },
+    exposedVariables: {},
+    definition: {
+      others: {
+        showOnDesktop: { value: '{{true}}' },
+        showOnMobile: { value: '{{false}}' },
+      },
+      properties: {
+        rawHtml: {
+          value: `<body><main><section class="hero" style="height:306px;display: flex;
+          justify-content: center;padding:0 1px;align-items: center;text-align:center">You can build your custom HTML-CSS template here</section></main></body>`,
+        },
+      },
+      events: [],
+      styles: {
+        visibility: { value: '{{true}}' },
+      },
+    },
+  },
+  {
     name: 'VerticalDivider',
     displayName: 'Vertical Divider',
     description: 'Vertical Separator between components',
