a562850a312f624d68bf78fe724580414cb08db3
ajith-k-v <ajith.jaban@gmail.com>
2022-06-18 21:16:35 +0530

    Added data-cy attributes for the elements

--- frontend/src/_components/Menu.jsx
+++ frontend/src/_components/Menu.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Menu({ onChange, items, selected }) {
   return (
     <div className="left-menu">
-      <ul>
+      <ul data-cy="left-menu-items">
         {items &&
           items.map((item) => (
             <li key={item.id} onClick={() => onChange(item.id)} className={selected === item.id ? 'active' : ''}>
