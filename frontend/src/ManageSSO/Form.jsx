97e29624334f70a098df36ef59ede569631db48a
Sam Kohl <55670463+Sam-Kohl@users.noreply.github.com>
2022-06-19 22:26:52 -0700

    3066 incorrect message while enabling disabling password (#3110)
    * Changed Form temp literal string to Password at line 14
    
    * Fixed missing ) after temperate literal string change
    
    * Removed unneccesary ) causing lint error

--- frontend/src/ManageSSO/Form.jsx
+++ frontend/src/ManageSSO/Form.jsx
@@ -11,9 +11,9 @@ export function Form({ settings, updateData }) {
         const enabled_tmp = !enabled;
         setEnabled(enabled_tmp);
         updateData('form', { id: data.id, enabled: enabled_tmp });
-        toast.success(`${enabled_tmp ? 'Enabled' : 'Disabled'} Form login`, {
+        toast.success(`${enabled_tmp ? 'Enabled' : 'Disabled'} Password login`), {
           position: 'top-center',
-        });
+        };
       },
       () => {
         toast.error('Error saving sso configurations', {
