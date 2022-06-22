43db21e76b4a28d491576472b86891a24a010237
Muhsin Shah <muhsinshah21@gmail.com>
2022-06-22 22:34:41 +0530

    edited the migration

--- server/migrations/1655726247638-AddUniqueConstraintToVersionName.ts
+++ server/migrations/1655726247638-AddUniqueConstraintToVersionName.ts
@@ -1,3 +1,4 @@
+import { App } from 'src/entities/app.entity';
 import { AppVersion } from 'src/entities/app_version.entity';
 import { EntityManager, MigrationInterface, QueryRunner, TableUnique } from 'typeorm';
 
@@ -19,24 +20,26 @@ export class AddUniqueConstraintToVersionName1655726247638 implements MigrationI
   }
 
   private async migrateVersions(entityManager: EntityManager) {
-    const versions = await entityManager.find(AppVersion);
+    const apps = await entityManager.find(App, { relations: ['appVersions'] });
+
     return (async () => {
-      const versionsChanged = [];
-      for (const version of versions) {
-        const { appId, name, id } = version;
-        if (versionsChanged.includes(id)) continue;
-        const sameVersions = await entityManager.find(AppVersion, {
-          where: { appId, name },
-        });
-        const versionsNeedToChange = sameVersions.filter((sameVersion) => sameVersion.id !== id);
+      for (const app of apps) {
+        const { appVersions } = app;
         await (async () => {
-          for (const versionToChange of versionsNeedToChange) {
-            const result = await entityManager.update(
-              AppVersion,
-              { id: versionToChange.id },
-              { name: `${versionToChange.name}_${Date.now()}` }
+          for (const version of appVersions) {
+            const { name, id } = version;
+            const versionsNeedToChange = appVersions.filter(
+              (appVersion) => appVersion.id !== id && appVersion.name === name
             );
-            result.affected == 1 && versionsChanged.push(versionToChange.id);
+            await (async () => {
+              for (const versionToChange of versionsNeedToChange) {
+                await entityManager.update(
+                  AppVersion,
+                  { id: versionToChange.id },
+                  { name: `${versionToChange.name}_${Date.now()}` }
+                );
+              }
+            })();
           }
         })();
       }
