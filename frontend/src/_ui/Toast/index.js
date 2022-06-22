808a879409a7b2d79705feaa9128b1a6da818539
Don Romaniello <don.romaniello@gmail.com>
2022-06-22 18:17:44 -0400

    Toast ui element with implemented changes.

--- frontend/src/_ui/Toast/index.js
+++ frontend/src/_ui/Toast/index.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { toast, Toaster, ToastBar } from 'react-hot-toast';
 
-const Toast = ({t, icon, message}) => {
-
+const Toast = ({ toastOptions }) => {
   return (
-    <>
-      <Toaster>
-        {(t) => (
-          <ToastBar toast={t}>
-            {({ icon, message }) => (
-              <>
-                {icon}
-                {message}
-                <button
-                  className="btn-close"
-                  onClick={() => toast.dismiss(t.id)} />
-              </>
-            )}
-          </ToastBar>
-        )}
-      </Toaster>
-    </>
-  )
-}
+    <Toaster toastOptions={toastOptions}>
+      {(t) => (
+        <ToastBar toast={t}>
+          {({ icon, message }) => (
+            <>
+              {icon}
+              {message}
+              <button className="btn-close" onClick={() => toast.dismiss(t.id)} />
+            </>
+          )}
+        </ToastBar>
+      )}
+    </Toaster>
+  );
+};
 
 export default Toast;
