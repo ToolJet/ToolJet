89db45f1b0ab1a98c893e4a3c85efd48c6b64600
Syed Ansar <82027712+Syed-Ansar@users.noreply.github.com>
2022-06-22 07:22:23 +0530

    Added query name to run query confirmation box (#3330)

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1132,7 +1132,7 @@ class Editor extends React.Component {
         {/* This is for viewer to show query confirmations */}
         <Confirm
           show={showQueryConfirmation}
-          message={'Do you want to run this query?'}
+          message={`Do you want to run this query - ${this.state.queryConfirmationData?.queryName}?`}
           onConfirm={(queryConfirmationData) => onQueryConfirm(this, queryConfirmationData)}
           onCancel={() => onQueryCancel(this)}
           queryConfirmationData={this.state.queryConfirmationData}
