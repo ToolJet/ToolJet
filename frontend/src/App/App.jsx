3da4c6e1b876fda99052a9df5112df28fc097f43
Manish Kushare <kushare.manish9@gmail.com>
2022-06-23 14:57:51 +0530

    Enhancement : displaying version name in the dashboard (#3338)
    * displaying version name in the dashboard
    
    * replaced useEffect with useMemo
    
    * Revert "replaced useEffect with useMemo"
    
    This reverts commit c328402c7937a9d8c1d20c847fe5dbf1d5477bad.
    
    * removed useState and useEffect for current version and displaying current version , if it is available
    
    * displaying current version in the dashboard using local storage

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -43,8 +43,8 @@ class App extends React.Component {
   fetchMetadata = () => {
     if (this.state.currentUser) {
       tooljetService.fetchMetaData().then((data) => {
+        localStorage.setItem('currentVersion', data.installed_version);
         this.setState({ onboarded: data.onboarded });
-        config.currentVersion = data.installed_version;
         if (data.latest_version && lt(data.installed_version, data.latest_version) && data.version_ignored === false) {
           this.setState({ updateAvailable: true });
         }
