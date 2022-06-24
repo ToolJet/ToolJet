d3fa7086d631243745bce9e0f11b0084cd1b7307
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2022-06-24 17:02:31 +0530

    test

--- server/scripts/digitalocean-postbuild.sh
+++ server/scripts/digitalocean-postbuild.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo $CA_CERT > ca-certificate.pem
+cat ca-certificate.pem
 (
   export NODE_EXTRA_CA_CERTS="$(pwd)/ca-certificate.pem"; \
+  echo $NODE_EXTRA_CA_CERTS && \
   npm run db:migrate:prod && \
   npm run db:seed && \
   npm run start:prod
