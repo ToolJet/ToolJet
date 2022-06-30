5a3c5d65968d49bf665de46330a3853f529a8bb4
arpitnath <arpitnath42@gmail.com>
2022-06-30 13:38:31 +0530

    removes console.log

--- frontend/src/Editor/Container.jsx
+++ frontend/src/Editor/Container.jsx
@@ -401,10 +401,6 @@ export const Container = ({
     styles.cursor = `url("data:image/svg+xml,%3Csvg width='34' height='34' viewBox='0 0 34 34' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='17' cy='17' r='15.25' fill='white' stroke='%23FCAA0D' stroke-width='2.5' opacity='0.5' /%3E%3Ctext x='10' y='20' fill='%23000' opacity='0.5' font-family='inherit' font-size='11.2' font-weight='500' color='%23656d77'%3E%3C/text%3E%3C/svg%3E%0A"), text`;
   }
 
-  React.useEffect(() => {
-    console.log('isHotKeyDragActive', isHotKeyDragActive);
-  }, [isHotKeyDragActive]);
-
   return (
     <div
       {...(config.COMMENT_FEATURE_ENABLE && showComments && { onClick: handleAddThread })}
