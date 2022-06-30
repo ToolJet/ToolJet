c61007c523d20fbf82aa92246d1ecda385ac9007
Muhsin Shah <muhsinshah21@gmail.com>
2022-06-30 13:37:10 +0530

    Fixed migration issue

--- server/migrations/1654596810662-ConvertAllUserEmailsToLowercaseAndDeleteDuplicateUsers.ts
+++ server/migrations/1654596810662-ConvertAllUserEmailsToLowercaseAndDeleteDuplicateUsers.ts
@@ -149,7 +149,13 @@ export class ConvertAllUserEmailsToLowercaseAndDeleteDuplicateUsers1654596810662
   }
 
   private async migrateThreads(entityManager: EntityManager, deletingUserId: string, originalUser: User) {
-    const threads = await entityManager.find(Thread, { userId: deletingUserId });
+    const threads = await entityManager
+      .getRepository(Thread)
+      .createQueryBuilder('threads')
+      .select(['threads.id'])
+      .where('threads.userId = :userId', { userId: deletingUserId })
+      .getMany();
+
     return await Promise.all(
       threads.map(async (thread) => {
         await entityManager.update(Thread, thread.id, {
@@ -161,7 +167,13 @@ export class ConvertAllUserEmailsToLowercaseAndDeleteDuplicateUsers1654596810662
 
   //error here
   private async migrateComments(entityManager: EntityManager, deletingUserId: string, originalUser: User) {
-    const comments = await entityManager.find(Comment, { userId: deletingUserId });
+    const comments = await entityManager
+      .getRepository(Comment)
+      .createQueryBuilder('comments')
+      .select(['comments.id'])
+      .where('comments.userId = :userId', { userId: deletingUserId })
+      .getMany();
+
     return await Promise.all(
       comments.map(async (comment) => {
         await entityManager.update(Comment, comment.id, {
