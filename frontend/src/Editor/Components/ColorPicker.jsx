a5267ad4c23d6f6a52c7092820430c57b765b4e7
manishkushare <kushare.manish9@gmail.com>
2022-07-05 17:10:57 +0530

    Added component specific action to Color Picker

--- frontend/src/Editor/Components/ColorPicker.jsx
+++ frontend/src/Editor/Components/ColorPicker.jsx
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import { SketchPicker } from 'react-color';
 
-export const ColorPicker = function ({ width, properties, styles, setExposedVariable, darkMode, height }) {
+export const ColorPicker = function ({
+  width,
+  properties,
+  styles,
+  setExposedVariable,
+  darkMode,
+  height,
+  registerAction,
+}) {
   const { visibility } = styles;
   const defaultColor = properties.defaultColor;
   const [showColorPicker, setShowColorPicker] = useState(false);
   const [color, setColor] = useState(defaultColor);
 
+  const getRGBAValueFromHex = (hex) => {
+    let c = hex.substring(1).split('');
+    switch (c.length) {
+      case 3:
+        c = [c[0] + c[0], c[1] + c[1], c[2] + c[2], 'ff'];
+        break;
+      case 4:
+        c = [c[0] + c[0], c[1] + c[1], c[2] + c[2], c[3] + c[3]];
+        break;
+      case 6:
+        c = [c[0] + c[1], c[2] + c[3], c[4] + c[5], 'ff'];
+        break;
+      case 8:
+        c = [c[0] + c[1], c[2] + c[3], c[4] + c[5], c[6] + c[7]];
+        break;
+    }
+    c = c.map((char) => parseInt(char, 16).toString());
+    c[3] = (Math.round((parseInt(c[3], 10) / 255) * 100) / 100).toString();
+    return c;
+  };
+  const hexToRgba = (hex) => {
+    const rgbaArray = getRGBAValueFromHex(hex);
+    return `rgba(${rgbaArray[0]}, ${rgbaArray[1]}, ${rgbaArray[2]}, ${rgbaArray[3]})`;
+  };
+  const hexToRgb = (hex) => {
+    const rgbaArray = getRGBAValueFromHex(hex);
+    return `rgba(${rgbaArray[0]}, ${rgbaArray[1]}, ${rgbaArray[2]})`;
+  };
+
+  registerAction('setColor', async function (color) {
+    if (/^#(([\dA-Fa-f]{3}){1,2}|([\dA-Fa-f]{4}){1,2})$/.test(color)) {
+      setExposedVariable('selectedColorHex', `${color}`);
+      setExposedVariable('selectedColorRGB', hexToRgb(color));
+      setExposedVariable('selectedColorRGBA', hexToRgba(color));
+      setColor(color);
+    } else {
+      setExposedVariable('selectedColorHex', 'undefined');
+      setExposedVariable('selectedColorRGB', 'undefined');
+      setExposedVariable('selectedColorRGBA', 'undefined');
+      setColor('Invalid Color');
+    }
+  });
+
   useEffect(() => {
     if (/^#(([\dA-Fa-f]{3}){1,2}|([\dA-Fa-f]{4}){1,2})$/.test(defaultColor)) {
-      const getRGBAValueFromHex = (hex) => {
-        let c = hex.substring(1).split('');
-        switch (c.length) {
-          case 3:
-            c = [c[0] + c[0], c[1] + c[1], c[2] + c[2], 'ff'];
-            break;
-          case 4:
-            c = [c[0] + c[0], c[1] + c[1], c[2] + c[2], c[3] + c[3]];
-            break;
-          case 6:
-            c = [c[0] + c[1], c[2] + c[3], c[4] + c[5], 'ff'];
-            break;
-          case 8:
-            c = [c[0] + c[1], c[2] + c[3], c[4] + c[5], c[6] + c[7]];
-            break;
-        }
-        c = c.map((char) => parseInt(char, 16).toString());
-        c[3] = (Math.round((parseInt(c[3], 10) / 255) * 100) / 100).toString();
-        // return `rgba(${c[0]}, ${c[1]}, ${c[2]}, ${c[3]})`;
-        return c;
-      };
-      const hexToRgba = (hex) => {
-        const rgbaArray = getRGBAValueFromHex(hex);
-        return `rgba(${rgbaArray[0]}, ${rgbaArray[1]}, ${rgbaArray[2]}, ${rgbaArray[3]})`;
-      };
-      const hexToRgb = (hex) => {
-        const rgbaArray = getRGBAValueFromHex(hex);
-        return `rgba(${rgbaArray[0]}, ${rgbaArray[1]}, ${rgbaArray[2]})`;
-      };
       setExposedVariable('selectedColorHex', `${defaultColor}`);
       setExposedVariable('selectedColorRGB', hexToRgb(defaultColor));
       setExposedVariable('selectedColorRGBA', hexToRgba(defaultColor));
@@ -82,6 +104,7 @@ export const ColorPicker = function ({ width, properties, styles, setExposedVari
 
   return (
     <div style={baseStyle} className="form-control">
+      {console.log('render')}
       <div className="d-flex h-100 justify-content-between align-items-center" onClick={() => setShowColorPicker(true)}>
         <span>{color}</span>
         {!(color === `Invalid Color`) && <div style={backgroundColorDivStyle}></div>}
