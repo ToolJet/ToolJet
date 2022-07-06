c7f5e15078b4513a712ad20b0c12b2ba373bead1
Shubhendra Singh Chauhan <withshubh@gmail.com>
2022-07-06 16:56:52 +0530

    [docs] Google - OpenID Connect (#3472)
    * [docs]google - open id connect
    
    * Update docs/docs/user-authentication/sso/openid/google-openid.md
    
    Co-authored-by: Midhun G S <gsmithun4@gmail.com>
    
    Co-authored-by: Midhun G S <gsmithun4@gmail.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -218,9 +218,10 @@ const sidebars = {
           },
           collapsed: false,
           items: [
-            'user-authentication/sso/setup',
-            'user-authentication/sso/okta',
-            'user-authentication/sso/azuread',
+            'user-authentication/sso/openid/setup',
+            'user-authentication/sso/openid/azuread',
+            'user-authentication/sso/openid/okta',
+            'user-authentication/sso/openid/google-openid',
           ],
         }
           ],
