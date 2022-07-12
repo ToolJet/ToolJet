4a72b469c03babd664a42e66b894b2f1f4957277
emidhun <midhun752@gmail.com>
2022-07-12 13:10:46 +0530

    Add common utils and optimized commands

--- cypress/support/utils/common.js
+++ cypress/support/utils/common.js
@@ -2,33 +2,44 @@ import { path } from "Texts/common";
 import { usersSelector } from "Selectors/manageUsers";
 import { profileSelector } from "Selectors/profile";
 import { commonSelectors } from "Selectors/common";
+import moment from "moment";
 
-export const navigateToProfile=()=>{
- cy.get(profileSelector.profileDropdown).invoke("show");
- cy.contains("Profile").click();
- cy.url().should("include", path.profilePath);
+export const navigateToProfile = () => {
+  cy.get(profileSelector.profileDropdown).invoke("show");
+  cy.contains("Profile").click();
+  cy.url().should("include", path.profilePath);
 };
 
-export const logout=()=>{
- cy.get(profileSelector.profileDropdown).invoke("show");
- cy.contains("Logout").click();
- cy.url().should("include",path.loginPath);
+export const logout = () => {
+  cy.get(profileSelector.profileDropdown).invoke("show");
+  cy.contains("Logout").click();
+  cy.url().should("include", path.loginPath);
 };
 
-export const navigateToManageUsers=()=>{
- cy.get(usersSelector.dropdown).invoke("show");
- cy.contains("Manage Users").click();
- cy.url().should("include",path.manageUsers );
+export const navigateToManageUsers = () => {
+  cy.get(usersSelector.dropdown).invoke("show");
+  cy.contains("Manage Users").click();
+  cy.url().should("include", path.manageUsers);
 };
 
-export const navigateToManageGroups=()=>{
+export const navigateToManageGroups = () => {
   cy.get(commonSelectors.dropdown).invoke("show");
   cy.contains("Manage Groups").click();
-  cy.url().should("include",path.manageGroups );
-}
+  cy.url().should("include", path.manageGroups);
+};
 
-export const navigateToManageSSO=()=>{
+export const navigateToManageSSO = () => {
   cy.get(commonSelectors.dropdown).invoke("show");
   cy.contains("Manage SSO").click();
-  cy.url().should("include",path.manageSSO );
+  cy.url().should("include", path.manageSSO);
+};
+
+export const randomDateOrTime = (format = "DD/MM/YYYY") => {
+  let endDate = new Date();
+  let startDate = new Date(2018, 0, 1);
+  startDate = new Date(
+    startDate.getTime() +
+      Math.random() * (endDate.getTime() - startDate.getTime())
+  );
+  return moment(startDate).format(format);
 };
