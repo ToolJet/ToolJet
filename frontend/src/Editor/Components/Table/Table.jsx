1364d3110e1c9e41c3c28b32dadcb7ee50694411
stepinfwd <stepinfwd@gmail.com>
2022-07-14 17:55:24 +0530

    commenting out row select operations

--- frontend/src/Editor/Components/Table/Table.jsx
+++ frontend/src/Editor/Components/Table/Table.jsx
@@ -815,33 +815,32 @@ export function Table({
     ['targetPageIndex']
   );
 
-  registerAction('selectRowByKeyAndValue', async function (key, value) {
-    const item = currentState.components[component.name]['currentData'].filter((item) => item[key] == value);
+  // registerAction('selectRowByKeyAndValue', async (key, value) => {
+  //   const item = currentState.components[component.name]['currentData'].filter((item) => item[key] == value);
+  //   let rowId = '';
+  //   let original = '';
 
-    let rowId = '';
-    let original = '';
+  //   page.map((item, index) => {
+  //     if (item.original[key] == value) {
+  //       rowId = item.id;
+  //       original = item.original;
+  //     }
+  //   });
+  //   setcomponentState((prevState) => {
+  //     return { ...prevState, selectedRow: item[0], selectedRowId: rowId };
+  //   });
+  //   onEvent('onRowClicked', { component, data: original, rowId: rowId });
+  // });
 
-    page.map((item, index) => {
-      if (item.original[key] == value) {
-        rowId = item.id;
-        original = item.original;
-      }
-    });
-    setcomponentState((prevState) => {
-      return { ...prevState, selectedRow: item[0], selectedRowId: rowId };
-    });
-    onEvent('onRowClicked', { component, data: original, rowId: rowId });
-  });
+  // registerAction('selectRowByIndex', async function (index) {
+  //   const rowId = page?.[index]?.id;
+  //   const original = page?.[index]?.original;
 
-  registerAction('selectRowByIndex', async function (index) {
-    const rowId = page?.[index]?.id;
-    const original = page?.[index]?.original;
-
-    setcomponentState((prevState) => {
-      return { ...prevState, selectedRow: original, selectedRowId: rowId };
-    });
-    onEvent('onRowClicked', { component, data: original, rowId: rowId });
-  });
+  //   setcomponentState((prevState) => {
+  //     return { ...prevState, selectedRow: original, selectedRowId: rowId };
+  //   });
+  //   onEvent('onRowClicked', { component, data: original, rowId: rowId });
+  // });
 
   registerAction('clear', async function () {
     setTableData([]);
