ae9c9efb9f3e5c59e217bf72e37a235ab579bffb
manishkushare <kushare.manish9@gmail.com>
2022-07-25 12:23:24 +0530

    Bug Fix : converted ms into second by multiplying it with 1000 for success notification

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -752,7 +752,7 @@ export function runQuery(_ref, queryId, queryName, confirmed = undefined, mode) 
           }
 
           if (dataQuery.options.showSuccessNotification) {
-            const notificationDuration = dataQuery.options.notificationDuration || 5000;
+            const notificationDuration = dataQuery.options.notificationDuration * 1000 || 5000;
             toast.success(dataQuery.options.successMessage, {
               duration: notificationDuration,
             });
