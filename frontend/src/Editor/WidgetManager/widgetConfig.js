3fe27abafcf6c5773eeb4faa6a6222c441aa63ef
Manish Kushare <manish.altcampus@gmail.com>
2022-08-04 17:03:33 +0530

    Added 'Microsoft Excel as a file type along with CSV and auto detect'

--- frontend/src/Editor/WidgetManager/widgetConfig.js
+++ frontend/src/Editor/WidgetManager/widgetConfig.js
@@ -2620,6 +2620,11 @@ export const widgets = [
         options: [
           { name: 'Autodetect from extension', value: 'auto-detect' },
           { name: 'CSV', value: 'csv' },
+          { name: 'Microsoft Excel - xls', value: 'vnd.ms-excel' },
+          {
+            name: 'Microsoft Excel - xlsx',
+            value: 'vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          },
         ],
         validation: {
           schema: {
