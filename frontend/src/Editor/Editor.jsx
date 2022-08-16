f6dce462cd51045d9bc891abe3e4ffb2352cd3e9
Kiran Ashok <stepinfwd@gmail.com>
2022-08-16 07:44:59 +0530

    Removing setSelectedComponent (#3774)

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1721,7 +1721,7 @@ class Editor extends React.Component {
                         switchSidebarTab={this.switchSidebarTab}
                         apps={apps}
                         darkMode={this.props.darkMode}
-                        setSelectedComponent={this.setSelectedComponent}
+                        handleEditorEscapeKeyPress={this.handleEditorEscapeKeyPress}
                       ></Inspector>
                     ) : (
                       <center className="mt-5 p-2">Please select a component to inspect</center>
