d1faa8a87fcb960ccd0fd38777b7fc62a4f59f59
arpitnath <arpitnath42@gmail.com>
2022-08-20 01:15:14 +0530

    test connection

--- plugins/packages/cosmosdb/lib/index.ts
+++ plugins/packages/cosmosdb/lib/index.ts
@@ -1,11 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions } from './types';
+import { CosmosClient } from '@azure/cosmos';
 
 export default class Cosmosdb implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     return {
       status: 'ok',
       data: {},
+    };
+  }
+
+  async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
+    const { endpoint, key } = sourceOptions;
+    const genericClient = new CosmosClient({ endpoint, key });
+
+    await genericClient.databases.readAll({}).fetchAll();
+
+    return {
+      status: 'ok',
     };
   }
 }
--- plugins/packages/cosmosdb/lib/types.ts
+++ plugins/packages/cosmosdb/lib/types.ts
@@ -1,4 +1,7 @@
-export type SourceOptions = {};
+export type SourceOptions = {
+  endpoint: string;
+  key: string;
+};
 export type QueryOptions = {
   operation: string;
 };
