3a4dd2e56c3dfaa92402df0c66a408b1d1c7d80b
Arpit <arpitnath42@gmail.com>
2022-08-25 03:05:03 +0530

    [Refactor/Improvement] Debugger (#3794)
    * init
    
    * clean up
    
    * clean up
    
    * adds margin-right to the title expand icon
    
    * refactor
    
    * refactor: unReadErrorCount
    
    * remove effectiveProperties from debugger
    
    * remove effectiveProperties from debugger
    
    * moved a props to generalProps for errorLog

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1104,6 +1104,7 @@ class Editor extends React.Component {
     flush: () => {
       debuggerActions.flush(this);
     },
+    generateErrorLogs: (errors) => debuggerActions.generateErrorLogs(errors),
   };
 
   changeDarkMode = (newMode) => {
