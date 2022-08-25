2b42611938df0961c85cc34c771a76e306c2668a
manishkushare <kushare.manish9@gmail.com>
2022-08-25 15:38:34 +0530

    removed form-control class

--- frontend/src/Editor/QueryManager/QueryManager.jsx
+++ frontend/src/Editor/QueryManager/QueryManager.jsx
@@ -694,7 +694,6 @@ let QueryManager = class QueryManager extends React.Component {
                           currentState={this.props.currentState}
                           initialValue={this.state.options.successMessage}
                           height="36px"
-                          className="form-control p-0"
                           theme={this.props.darkMode ? 'monokai' : 'default'}
                           onChange={(value) => this.optionchanged('successMessage', value)}
                           placeholder="Query ran successfully"
