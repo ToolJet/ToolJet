7a4fc97928a14b3e092d10d9557c0a7cdf33bbe3
Muhsin Shah C P <muhsinshah21@gmail.com>
2022-08-26 11:04:40 +0530

    [Fixed] User can reset their password with a space  (#3825)
    * added trim fn
    
    * Added trim fn to dto
    
    * Refactored the code

--- frontend/src/ResetPassword/ResetPasswordPage.jsx
+++ frontend/src/ResetPassword/ResetPasswordPage.jsx
@@ -15,7 +15,7 @@ class ResetPassword extends React.Component {
   }
 
   handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: event.target.value?.trim() });
   };
 
   handleClick = (event) => {
