9372986c42061445ea4324afd63883f4f3fba84d
manishkushare <kushare.manish9@gmail.com>
2022-08-29 23:11:55 +0530

    Fixed : If we pick white (#FFFFFF) then the dark mode is not handled.

--- frontend/src/Editor/Components/Listview.jsx
+++ frontend/src/Editor/Components/Listview.jsx
@@ -21,7 +21,7 @@ export const Listview = function Listview({
   const { data, rowHeight, showBorder } = { ...fallbackProperties, ...properties };
   const { visibility, disabledState, borderRadius } = { ...fallbackStyles, ...styles };
   const backgroundColor =
-    ['#fff', '#ffffff'].includes(styles.backgroundColor) && darkMode ? '#232E3C' : styles.backgroundColor;
+    ['#fff', '#ffffffff'].includes(styles.backgroundColor) && darkMode ? '#232E3C' : styles.backgroundColor;
   const computedStyles = {
     backgroundColor,
     height,
