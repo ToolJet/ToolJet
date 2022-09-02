854e648938634134a2704453805de9e824079794
arpitnath <arpitnath42@gmail.com>
2022-09-02 17:10:47 +0530

    zendesk integration 🚀

--- plugins/packages/zendesk/lib/types.ts
+++ plugins/packages/zendesk/lib/types.ts
@@ -1,5 +1,11 @@
-/* eslint-disable @typescript-eslint/ban-types */
-export type SourceOptions = {};
+export type SourceOptions = {
+  clientId: string;
+  clientSecret: string;
+  subdomain: string;
+  scope: string;
+  accessToken?: string;
+  refreshToken?: string;
+};
 export type QueryOptions = {
   operation: string;
 };
