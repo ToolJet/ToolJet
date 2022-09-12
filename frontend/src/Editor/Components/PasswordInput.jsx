50054a13c41089162257ab48129fc322dc8d670d
arpitnath <arpitnath42@gmail.com>
2022-09-12 11:21:56 +0530

    fixes app freeze, when password widget drops inside a listview

--- frontend/src/Editor/Components/PasswordInput.jsx
+++ frontend/src/Editor/Components/PasswordInput.jsx
@@ -1,41 +1,29 @@
 import React from 'react';
 
-export const PasswordInput = ({
-  height,
-  validate,
-  properties,
-  styles,
-  exposedVariables,
-  setExposedVariable,
-  darkMode,
-}) => {
-  const value = exposedVariables.value;
+export const PasswordInput = ({ height, validate, properties, styles, setExposedVariable }) => {
   const { visibility, disabledState, borderRadius } = styles;
   const placeholder = properties.placeholder;
 
-  const currentValidState = exposedVariables.isValid;
+  const [passwordValue, setPasswordValue] = React.useState('');
+  const { isValid, validationError } = validate(passwordValue);
 
-  const validationData = validate(value);
-
-  const { isValid, validationError } = validationData;
-
-  if (currentValidState !== isValid) {
+  React.useEffect(() => {
     setExposedVariable('isValid', isValid);
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [passwordValue, isValid]);
 
   return (
     <div>
       <input
         disabled={disabledState}
         onChange={(e) => {
+          setPasswordValue(e.target.value);
           setExposedVariable('value', e.target.value);
         }}
         type={'password'}
-        className={`form-control ${!isValid ? 'is-invalid' : ''} validation-without-icon ${
-          darkMode && 'dark-theme-placeholder'
-        }`}
+        className={`form-control ${!isValid ? 'is-invalid' : ''} validation-without-icon`}
         placeholder={placeholder}
-        value={exposedVariables.value}
+        value={passwordValue}
         style={{ height, display: visibility ? '' : 'none', borderRadius: `${borderRadius}px` }}
       />
 
