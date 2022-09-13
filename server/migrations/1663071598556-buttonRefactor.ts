389f7c284d2284f3387c1f9b0c4f5921be5381b3
manishkushare <kushare.manish9@gmail.com>
2022-09-13 17:56:37 +0530

    added migration for visibility and disabled prop

---
+++ server/migrations/1663071598556-buttonRefactor.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { AppVersion } from '../src/entities/app_version.entity';
+
+export class buttonRefactor1663071598556 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    const appVersions = await entityManager.find(AppVersion);
+    for (const version of appVersions) {
+      const definition = version['definition'];
+
+      if (definition) {
+        const components = definition['components'];
+
+        for (const componentId of Object.keys(components)) {
+          const component = components[componentId];
+
+          if (component.component.component === 'Button') {
+            component.component.definition.properties.disabledState = { value: false };
+            component.component.definition.properties.visibility = { value: true };
+            components[componentId] = {
+              ...component,
+              component: {
+                ...component.component,
+                definition: {
+                  ...component.component.definition,
+                },
+              },
+            };
+          }
+        }
+
+        definition['components'] = components;
+        version.definition = definition;
+
+        await entityManager.update(AppVersion, { id: version.id }, { definition });
+      }
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
