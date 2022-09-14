936fa0472073ff41fee11d79e26e8ba8b0f021dc
Muhsin Shah <muhsinshah21@gmail.com>
2022-09-14 14:30:11 +0530

    fixed white space issue

--- frontend/ee/components/UsersPage/UsersTable.jsx
+++ frontend/ee/components/UsersPage/UsersTable.jsx
@@ -15,7 +15,7 @@ const UsersTable = ({
   archiveOrgUser,
 }) => {
   return (
-    <div className="container-xl mb-4">
+    <div className="container-xl mb-4 pb-4">
       <div className="card">
         <div className="card-table fixedHeader table-responsive table-bordered">
           <table data-testid="usersTable" className="table table-vcenter h-100">
