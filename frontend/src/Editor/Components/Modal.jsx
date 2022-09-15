bf344986689d95086a84079ec37370dee94de6e5
arpitnath <arpitnath42@gmail.com>
2022-09-15 17:30:15 +0530

    init

--- frontend/src/Editor/Components/Modal.jsx
+++ frontend/src/Editor/Components/Modal.jsx
@@ -3,11 +3,9 @@ import { default as BootstrapModal } from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { SubCustomDragLayer } from '../SubCustomDragLayer';
 import { SubContainer } from '../SubContainer';
-import { ConfigHandle } from '../ConfigHandle';
 
 export const Modal = function Modal({
   id,
-  component,
   height,
   containerProps,
   darkMode,
@@ -47,43 +45,60 @@ export const Modal = function Modal({
   }
 
   return (
-    <div data-disabled={disabledState}>
-      <BootstrapModal
-        contentClassName="modal-component"
+    <div className="container" data-disabled={disabledState}>
+      <Button variant="primary" onClick={() => setShowModal(true)}>
+        Show Modal
+      </Button>
+      <Modal.Component
         show={showModal}
+        onHide={hideModal}
+        contentClassName="modal-component"
         container={document.getElementsByClassName('canvas-area')[0]}
         size={size}
-        backdrop={true}
         keyboard={true}
         enforceFocus={false}
         animation={false}
         onEscapeKeyDown={() => hideOnEsc && hideModal()}
         id="modal-container"
+        backdrop={containerProps.mode === 'edit' ? 'static' : true}
+        modalBodyProps={{ height, parentRef, id, title, hideTitleBar, hideCloseButton, hideModal, darkMode }}
       >
-        {containerProps.mode === 'edit' && <ConfigHandle id={id} component={component} />}
-        {!hideTitleBar && (
-          <BootstrapModal.Header>
-            <BootstrapModal.Title>{title}</BootstrapModal.Title>
-            {!hideCloseButton && (
-              <div>
-                <Button variant={darkMode ? 'secondary' : 'light'} size="sm" onClick={() => hideModal()}>
-                  x
-                </Button>
-              </div>
-            )}
-          </BootstrapModal.Header>
-        )}
-
-        <BootstrapModal.Body style={{ height }} ref={parentRef} id={id}>
-          <SubContainer parent={id} {...containerProps} parentRef={parentRef} />
-          <SubCustomDragLayer
-            snapToGrid={true}
-            parentRef={parentRef}
-            parent={id}
-            currentLayout={containerProps.currentLayout}
-          />
-        </BootstrapModal.Body>
-      </BootstrapModal>
+        <SubContainer parent={id} {...containerProps} parentRef={parentRef} />
+        <SubCustomDragLayer
+          snapToGrid={true}
+          parentRef={parentRef}
+          parent={id}
+          currentLayout={containerProps.currentLayout}
+        />
+      </Modal.Component>
     </div>
   );
 };
+
+const Component = ({ children, ...restProps }) => {
+  const { height, parentRef, id, title, hideTitleBar, hideCloseButton, hideModal, darkMode } =
+    restProps['modalBodyProps'];
+
+  return (
+    <BootstrapModal {...restProps}>
+      {!hideTitleBar && (
+        <BootstrapModal.Header>
+          <BootstrapModal.Title id="contained-modal-title-vcenter">{title}</BootstrapModal.Title>
+          {!hideCloseButton && (
+            <div>
+              <Button variant={darkMode ? 'secondary' : 'light'} size="sm" onClick={() => hideModal()}>
+                x
+              </Button>
+            </div>
+          )}
+        </BootstrapModal.Header>
+      )}
+      <BootstrapModal.Body style={{ height }} ref={parentRef} id={id}>
+        {children}
+      </BootstrapModal.Body>
+      <BootstrapModal.Footer></BootstrapModal.Footer>
+    </BootstrapModal>
+  );
+};
+
+Modal.Component = Component;
