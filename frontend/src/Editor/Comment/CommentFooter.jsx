fa4c8fc90915898f47a0135edf5985c829fbcb9b
Muhsin Shah C P <muhsinshah21@gmail.com>
2022-09-16 10:42:53 +0530

    [feature] Added pagination and filtering features to users page (#3921)
    * added pagination and filtering in backend
    
    * added pagination
        - created a seperate component for users table
        - added pagination
    
    * Added filter UI
    
    * temporary css fix for pagination footer
    
    * fixed pagination width issue
    
    * now result will also clear when user clicks on clear icon
    
    * Added seperate api for comment mentions
    
    * Now we can search mentions by email, first and last names
    
    * Fixed a bug
    - email didn't send for comment mentions
    
    * refactoring the code
    
    * resolved PR changes
    
    * Added isAdmin guard
    
    * adding some checks
    
    * fixed lint errors
    
    * added wild card search
    
    * Added no result found text
    
    * fixed failing test case
    
    * Working on PR changes
    
    * Now users table avatars will load image too
    
    * replaced skeleton classes with skeleton library component
    
    * Completed PR changes
    
    * added orderby
    
    * Fixed some issues
    
    * fixed failed test case
    
    * have fixed some css issues
    
    * replaced query with quersrting package
    
    * fixed minor width issue
    
    * Fixed some css issues
    
    * fixed darkMode issue
    
    * implemented on enter press search
    
    * Refactored the code
    
    * fixed white space issue
    
    * refactored the code
    
    * fixed overlapping issue
    
    * refactored the code
    
    * fixing some issues
    
    * fixes
    
    * removed guard
    
    * code cleanup
    
    * comments notification fix
    
    * fixed conflict issues
    
    * fixed css height issue
    
    Co-authored-by: gsmithun4 <gsmithun4@gmail.com>

--- frontend/src/Editor/Comment/CommentFooter.jsx
+++ frontend/src/Editor/Comment/CommentFooter.jsx
@@ -8,7 +8,14 @@ import usePopover from '@/_hooks/use-popover';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useTranslation } from 'react-i18next';
 
-function CommentFooter({ users, editComment = '', setMentionedUsers, editCommentId, setEditCommentId, handleSubmit }) {
+function CommentFooter({
+  searchUser,
+  editComment = '',
+  setMentionedUsers,
+  editCommentId,
+  setEditCommentId,
+  handleSubmit,
+}) {
   const [comment, setComment] = React.useState(editComment);
   const [loading, setLoading] = React.useState(false);
   const [open, trigger, content, setOpen] = usePopover(false);
@@ -42,7 +49,7 @@ function CommentFooter({ users, editComment = '', setMentionedUsers, editComment
         <div className="row align-items-center">
           <div className="col-8">
             <TextareaMentions
-              users={users}
+              searchUser={searchUser}
               setMentionedUsers={setMentionedUsers}
               value={comment}
               setValue={setComment}
