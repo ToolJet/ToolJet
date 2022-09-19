0903a672e48077e2b1520d6e8b9953f44e9308a5
arpitnath <arpitnath42@gmail.com>
2022-09-19 12:40:05 +0530

    updates modal close icon

--- frontend/src/Editor/Components/Modal.jsx
+++ frontend/src/Editor/Components/Modal.jsx
@@ -179,11 +179,24 @@ const Component = ({ children, ...restProps }) => {
         <BootstrapModal.Header style={{ ...customStyles.modalHeader }}>
           <BootstrapModal.Title id="contained-modal-title-vcenter">{title}</BootstrapModal.Title>
           {!hideCloseButton && (
-            <div>
-              <Button variant={darkMode ? 'secondary' : 'light'} size="sm" onClick={() => hideModal()}>
-                x
-              </Button>
-            </div>
+            <span className="cursor-pointer" size="sm" onClick={() => hideModal()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-x"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </span>
           )}
         </BootstrapModal.Header>
       )}
