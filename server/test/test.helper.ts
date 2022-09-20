5df55fcf6242612c67578a90c83c9b6b60817114
gsmithun4 <gsmithun4@gmail.com>
2022-09-20 23:07:21 +0530

    fix

--- server/test/test.helper.ts
+++ server/test/test.helper.ts
@@ -90,8 +90,10 @@ export function authHeaderForUser(user: User, organizationId?: string, isPasswor
 export async function clearDB() {
   const entities = getConnection().entityMetadatas;
   for (const entity of entities) {
-    const repository = getConnection().getRepository(entity.name);
-    await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+    if (entity.tableName !== 'instance_settings') {
+      const repository = getConnection().getRepository(entity.name);
+      await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+    }
   }
 }
 
