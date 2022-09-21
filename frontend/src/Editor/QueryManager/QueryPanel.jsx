3650538d672827d1f6169ac329d9cbb759d74fc0
manishkushare <kushare.manish9@gmail.com>
2022-09-21 16:58:35 +0530

    added localStorage support for query panel component

--- frontend/src/Editor/QueryManager/QueryPanel.jsx
+++ frontend/src/Editor/QueryManager/QueryPanel.jsx
@@ -2,13 +2,25 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useEventListener } from '@/_hooks/use-event-listener';
 
 const QueryPanel = ({ queryPanelHeight, children }) => {
+  const queryManagerPreferces = JSON.parse(localStorage.getItem('queryManagerPreferces')) ?? {};
+  const [isExpanded, setExpanded] = useState(queryManagerPreferces?.isExpanded ?? true);
+  const isComponentMounted = useRef(false);
   const queryPaneRef = useRef(null);
   const [isDragging, setDragging] = useState(false);
-  const [height, setHeight] = useState(queryPanelHeight);
+  const [height, setHeight] = useState(queryManagerPreferces?.queryPanelHeight ?? queryPanelHeight);
   const [isTopOfQueryPanel, setTopOfQueryPanel] = useState(false);
 
   useEffect(() => {
-    setHeight(queryPanelHeight);
+    if (isComponentMounted.current) {
+      localStorage.setItem(
+        'queryManagerPreferces',
+        JSON.stringify({ ...queryManagerPreferces, isExpanded: !isExpanded })
+      );
+      setExpanded(!isExpanded);
+    } else {
+      isComponentMounted.current = true;
+    }
+    // setHeight(queryPanelHeight);
   }, [queryPanelHeight]);
 
   const onMouseUp = () => {
@@ -35,7 +47,11 @@ const QueryPanel = ({ queryPanelHeight, children }) => {
 
         if (height > 95) height = 100;
         if (height < 4.5) height = 4.5;
-
+        localStorage.setItem(
+          'queryManagerPreferces',
+          JSON.stringify({ ...queryManagerPreferces, queryPanelHeight: height, isExpanded: true })
+        );
+        setExpanded(true);
         setHeight(height);
       }
     }
@@ -50,7 +66,7 @@ const QueryPanel = ({ queryPanelHeight, children }) => {
       onMouseDown={onMouseDown}
       className="query-pane"
       style={{
-        height: `calc(100% - ${height}%)`,
+        height: `calc(100% - ${isExpanded ? height : 100}%)`,
         cursor: isDragging || isTopOfQueryPanel ? 'row-resize' : 'default',
       }}
     >
