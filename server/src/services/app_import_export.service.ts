e4e9353c5c0f5251270177729445d7df34d3151c
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2022-09-26 19:32:30 +0530

    handle empty versions case

--- server/src/services/app_import_export.service.ts
+++ server/src/services/app_import_export.service.ts
@@ -10,6 +10,7 @@ import { GroupPermission } from 'src/entities/group_permission.entity';
 import { AppGroupPermission } from 'src/entities/app_group_permission.entity';
 import { DataSourcesService } from './data_sources.service';
 import { dbTransactionWrap } from 'src/helpers/utils.helper';
+import { isEmpty } from 'lodash';
 
 @Injectable()
 export class AppImportExportService {
@@ -195,6 +196,8 @@ export class AppImportExportService {
   }
 
   async setEditingVersionAsLatestVersion(manager: EntityManager, appVersionMapping: any, appVersions: Array<any>) {
+    if (isEmpty(appVersions)) return;
+
     const lastVersionFromImport = appVersions[appVersions.length - 1];
     const lastVersionIdToUpdate = appVersionMapping[lastVersionFromImport.id];
 
