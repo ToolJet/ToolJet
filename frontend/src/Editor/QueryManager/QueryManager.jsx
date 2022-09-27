e2a3f22e14922977487c23ef31c5c9f06ef117a0
Manish Kushare <kushare.manish9@gmail.com>
2022-09-27 13:18:04 +0530

    bug fixed : The result of the previous previewed query remains (#4115)
    * bug fixed : The result of the previous previewed query remains
    
    * moved the previous added code in setStateFromProps to avoid extra rendering

--- frontend/src/Editor/QueryManager/QueryManager.jsx
+++ frontend/src/Editor/QueryManager/QueryManager.jsx
@@ -76,6 +76,7 @@ class QueryManagerComponent extends React.Component {
         isSourceSelected: paneHeightChanged || queryPaneDragged ? this.state.isSourceSelected : props.isSourceSelected,
         selectedDataSource:
           paneHeightChanged || queryPaneDragged ? this.state.selectedDataSource : props.selectedDataSource,
+        queryPreviewData: this.state.selectedQuery?.id !== props.selectedQuery?.id ? undefined : props.queryPreviewData,
         theme: {
           scheme: 'bright',
           author: 'chris kempson (http://chriskempson.com)',
@@ -413,7 +414,6 @@ class QueryManagerComponent extends React.Component {
       queryPreviewData,
       dataSourceMeta,
     } = this.state;
-
     let ElementToRender = '';
 
     if (selectedDataSource) {
