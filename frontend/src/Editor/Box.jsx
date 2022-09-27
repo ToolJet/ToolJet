3a78cf6155b6479cec887f784206e2ac8ee8ee14
Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
2022-09-27 11:18:32 +0530

    [Feature]: Icon Widget (#3936)
    * Created Icon Widget
    
    * Installed tabler icons
    
    * Updated visibility logic for icon widget
    
    * Added doc for icon widget
    
    * Updated hidden field & moved hidden logic to cx
    
    * Dark theme for icon widget & updated visibility
    
    * Moved visibility logic to useEffect
    
    * Updated visibility CSA label
    
    * Removed resolveWidgetFieldValuekey usage in the Icon widget
    
    * Moved register action to useEffect
    
    * Removed registerActions on useEffect
    
    * Add setIconVisibility to dependency array of registerAction
    
    Co-authored-by: Sherfin Shamsudeen <sherfin94@gmail.com>

--- frontend/src/Editor/Box.jsx
+++ frontend/src/Editor/Box.jsx
@@ -47,6 +47,7 @@ import { ColorPicker } from './Components/ColorPicker';
 import { KanbanBoard } from './Components/KanbanBoard/KanbanBoard';
 import { Steps } from './Components/Steps';
 import { TreeSelect } from './Components/TreeSelect';
+import { Icon } from './Components/Icon';
 import { Link } from './Components/Link';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import '@/_styles/custom.scss';
@@ -112,6 +113,7 @@ const AllComponents = {
   Steps,
   TreeSelect,
   Link,
+  Icon,
 };
 
 export const Box = function Box({
