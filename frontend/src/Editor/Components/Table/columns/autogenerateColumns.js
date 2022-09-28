f8b77d5dd41f096085ddfbf751754f85abe2f282
Sherfin Shamsudeen <sherfin94@gmail.com>
2022-09-28 18:38:27 +0530

    Use custom mapping for autogeneration of table columns

--- frontend/src/Editor/Components/Table/columns/autogenerateColumns.js
+++ frontend/src/Editor/Components/Table/columns/autogenerateColumns.js
@@ -5,15 +5,25 @@ export default function autogenerateColumns(tableData, existingColumns, setPrope
   const [keysAndValues] = tableData.slice(0, 1).map(Object.entries);
   const keys = keysAndValues?.map(([key, _value]) => key) ?? [];
   const keysAndDataTypes = keysAndValues?.map(([key, value]) => [key, typeof value]) ?? [];
+
   const generatedColumns = keysAndDataTypes.map(([key, dataType]) => ({
     id: uuidv4(),
     name: key,
-    columnType: dataType,
+    columnType: convertDataTypeToColumnType(dataType),
   }));
+
   const newColumns = _.uniqBy([...existingColumns, ...generatedColumns], 'name').filter((column) =>
     keys.includes(column.name)
   );
-  setProperty('columns', newColumns);
 
-  // console.log({ keysAndValues, keysAndDataTypes, generatedColumns, existingColumns, newColumns, keys });
+  setProperty('columns', newColumns);
 }
+
+const dataTypeToColumnTypeMapping = {
+  string: 'string',
+};
+
+const convertDataTypeToColumnType = (dataType) => {
+  if (Object.keys(dataTypeToColumnTypeMapping).includes(dataType)) return dataTypeToColumnTypeMapping[dataType];
+  else return 'default';
+};
