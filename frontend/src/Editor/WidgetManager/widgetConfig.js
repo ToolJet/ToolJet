04cc884bbce70d37d3d3c99dffb4ee26c022b959
Paarth Agarwal <86092410+PaarthAgarwal@users.noreply.github.com>
2022-10-10 08:14:52 +0530

    Updated default text color for table widget (#4247)

--- frontend/src/Editor/WidgetManager/widgetConfig.js
+++ frontend/src/Editor/WidgetManager/widgetConfig.js
@@ -363,7 +363,7 @@ export const widgets = [
       },
       events: [],
       styles: {
-        textColor: { value: '' },
+        textColor: { value: '#000' },
         actionButtonRadius: { value: '0' },
         visibility: { value: '{{true}}' },
         disabledState: { value: '{{false}}' },
