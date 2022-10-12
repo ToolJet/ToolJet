b1c120aedfe56833663c0c987711d4ca7a45e4c5
Muhsin Shah <muhsinshah21@gmail.com>
2022-10-12 15:56:34 +0530

    Added loader skeletons

--- frontend/src/AppLoader/AppLoader.jsx
+++ frontend/src/AppLoader/AppLoader.jsx
@@ -5,7 +5,7 @@ import { Editor } from '../Editor/Editor';
 import { RealtimeEditor } from '@/Editor/RealtimeEditor';
 import config from 'config';
 import { Redirect } from 'react-router-dom';
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 const AppLoaderComponent = (props) => {
   const appId = props.match.params.id;
@@ -40,9 +40,33 @@ const AppLoaderComponent = (props) => {
 
   const AppSkeleton = () => {
     return (
-      <div>
-        <Skeleton />
-      </div>
+      <SkeletonTheme color={props.darkMode ? '#1F2936' : ''} highlightColor={props.darkMode ? '#2B394B' : ''}>
+        <div className="apploader">
+          <div className="app-container">
+            <div className="editor-header px-1">
+              <Skeleton height={'100%'} />
+            </div>
+            <div className="row editor-body p-0 m-0">
+              <div className="skeleton editor-left-panel">
+                <Skeleton height={'100%'} />
+              </div>
+              <div className="col col-*">
+                <div className="flex-column justify-content-between h-100">
+                  <div className="skeleton" style={{ height: '70%' }}>
+                    <Skeleton height={'100%'} />
+                  </div>
+                  <div className="skeleton h-30" style={{ height: '30%' }}>
+                    <Skeleton height={'100%'} />
+                  </div>
+                </div>
+              </div>
+              <div className="col col-2 skeleton">
+                <Skeleton height={'100%'} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </SkeletonTheme>
     );
   };
 
