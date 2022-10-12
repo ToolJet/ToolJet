4db4ddcc8175ef1150ff6778eb808ac9e734fee1
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-12 20:59:28 +0530

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -66,7 +66,7 @@ module.exports = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: 'https://docs.tooljet.com/docs/category/tutorial',
             },
           ],
         },
