dda7bf23e178d753ae198650d8470f582621e2ae
emidhun <midhun752@gmail.com>
2022-10-18 12:30:16 +0530

    Add common method for postgreSQL

--- cypress-tests/cypress/support/utils/postgreSql.js
+++ cypress-tests/cypress/support/utils/postgreSql.js
@@ -1,4 +1,8 @@
-import { commonSelectors, commonWidgetSelector } from "Selectors/common";
+import {
+  commonSelectors,
+  cyParamName,
+  commonWidgetSelector,
+} from "Selectors/common";
 
 export const addQuery = (queryName, query) => {
   cy.get('[data-cy="button-add-new-queries"]').click();
@@ -15,4 +19,85 @@ export const addQuery = (queryName, query) => {
     commonSelectors.toastMessage,
     `Query (${queryName}) completed.`
   );
+};
+
+export const addQueryOnGui = (queryName, query) => {
+  cy.get('[data-cy="button-add-new-queries"]').click();
+  cy.get('[data-cy="cypress-postgresql"]')
+    .should("contain", "cypress-postgresql")
+    .click();
+
+  cy.get('[data-cy="query-label-input-field"]').clear().type(queryName);
+  cy.get('[data-cy="query-input-field"]')
+    .should("be.visible")
+    .clearAndTypeOnCodeMirror(query);
+  cy.get('[data-cy="query-create-and-run-button"]').click();
+  cy.verifyToastMessage(
+    commonSelectors.toastMessage,
+    `Query (${queryName}) completed.`
+  );
+};
+export const selectDataSource = (dataSource) => {
+  cy.get("[data-cy='left-sidebar-sources-button']").click();
+  cy.get("[data-cy='add-datasource-link']").click();
+  cy.get('[data-cy="datasource-search-input"]').type(dataSource);
+  cy.get(`[data-cy='data-source-${dataSource.toLowerCase()}']`).click();
+};
+
+export const fillConnectionForm = (data) => {
+  // cy.clearAndType('[data-cy="host-text-field"]', host);
+  // cy.clearAndType('[data-cy="port-text-field"]', port);
+  // cy.clearAndType('[data-cy="database-name-text-field"]', dbName);
+  // cy.clearAndType('[data-cy="username-text-field"]', userName);
+  // cy.clearAndType('[data-cy="password-text-field"]', password);
+
+  for (const property in data) {
+    cy.clearAndType(
+      `[data-cy="${cyParamName(property)}-text-field"]`,
+      `${data[property]}`
+    );
+  }
+  cy.get('[data-cy="test-connection-button"]').click();
+  cy.get('[data-cy="test-connection-verified-text"]', {
+    timeout: 7000,
+  }).should("have.text", "connection verified");
+  cy.get('[data-cy="db-connection-save-button"]').click();
+};
+
+export const fillDataSourceTextField = (fieldName, placeholder, input) => {
+  cy.get(`[data-cy="label-${cyParamName(fieldName)}"]`).should(
+    "have.text",
+    fieldName
+  );
+  cy.get(`[data-cy="${cyParamName(fieldName)}-text-field"]`)
+    .invoke("attr", "placeholder")
+    .should("eq", placeholder.replace(/\u00a0/g, " "));
+  cy.clearAndType(`[data-cy="${cyParamName(fieldName)}-text-field"]`, input);
+};
+
+export const openQueryEditor = (dataSourceName) => {
+  cy.reload(); // remove later
+  cy.wait(5000);
+  cy.get('[data-cy="button-add-new-queries"]').click();
+  // cy.get(".spinner-border", { timeout: 10000 }).should("not.be.visible");
+  cy.get(`[data-cy="${cyParamName(dataSourceName)}-add-query-card"]`).click();
+};
+
+export const selectQueryMode = (mode, index = 2) => {
+  cy.get("[data-cy='query-select-dropdown']:eq(0)").click();
+
+  mode == "SQL mode"
+    ? cy.get(`#react-select-${index}-option-0`).click()
+    : cy.get(`#react-select-${index}-option-1`).click();
+};
+
+export const addGuiQuery = (tableName, primaryKey, Data) => {
+  cy.get("[data-cy='query-select-dropdown']:eq(1)").click();
+  cy.get("#react-select-3-option-0").click();
+
+  cy.get('[data-cy="table-input-field"]').type(tableName);
+  cy.get('[data-cy="primary_key_column-input-field"]').type(primaryKey);
+  cy.get('[data-cy="records-input-field"]')
+    .type(`[{{}name:'midhun',email:'mid@example.com'}]}}`)
+    .type("{home}{{");
 };
