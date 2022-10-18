b56161e1c29771239d856f8fb943dfcb35857450
3t8 <62209650+3t8@users.noreply.github.com>
2022-10-18 03:07:45 +0200

    Changed "on sorting columns" label in table

--- frontend/src/Editor/WidgetManager/widgetConfig.js
+++ frontend/src/Editor/WidgetManager/widgetConfig.js
@@ -276,7 +276,7 @@ export const widgets = [
       onPageChanged: { displayName: 'Page changed' },
       onSearch: { displayName: 'Search' },
       onCancelChanges: { displayName: 'Cancel changes' },
-      onSort: { displayName: 'On sorting columns' },
+      onSort: { displayName: 'Sort applied' },
       onCellValueChanged: { displayName: 'Cell value changed' },
       onFilterChanged: { displayName: 'Filter changed' },
     },
