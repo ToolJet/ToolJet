68c708ed704e96817caa8e99cdb1bffba110efec
ajith-k-v <ajith.jaban@gmail.com>
2022-11-02 15:32:54 +0530

    Add data-cy

--- frontend/src/_components/Header.jsx
+++ frontend/src/_components/Header.jsx
@@ -82,6 +82,7 @@ export const Header = function Header({ switchDarkMode, darkMode }) {
                     style={{
                       backgroundImage: `url(${avatar})`,
                     }}
+                    data-cy="user-avatar"
                   />
                 ) : (
                   <span className={`avatar bg-secondary-lt ${darkMode && 'text-muted'}`}>
