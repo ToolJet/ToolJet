594c8dd7c30a86a0a0c405a48707bc9c4c8ff660
manishkushare <kushare.manish9@gmail.com>
2022-11-03 10:44:05 +0530

    changed the fill of rename query and delete icon

--- frontend/src/Editor/QueryManager/QueryEditors/Restapi/TabHeaders.jsx
+++ frontend/src/Editor/QueryManager/QueryEditors/Restapi/TabHeaders.jsx
@@ -13,7 +13,7 @@ export default ({
 }) => {
   return (
     <>
-      <GroupHeader addNewKeyValuePair={addNewKeyValuePair} paramType={'headers'} descText="Request Headers" />
+      <GroupHeader addNewKeyValuePair={addNewKeyValuePair} paramType={'headers'} descText="Query Headers" />
       <TabContent
         options={options}
         currentState={currentState}
