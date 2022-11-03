71ac94014522bb6e76281fbc430a01d6bb9b0b1c
manishkushare <kushare.manish9@gmail.com>
2022-11-03 13:32:01 +0530

    base url consistency in dark theme

--- frontend/src/Editor/QueryManager/QueryEditors/Restapi/index.jsx
+++ frontend/src/Editor/QueryManager/QueryEditors/Restapi/index.jsx
@@ -120,7 +120,10 @@ class Restapi extends React.Component {
             />
           </div>
 
-          <div className="col field w-100 rest-methods-url" style={{ display: 'flex' }}>
+          <div
+            className={`col field w-100 rest-methods-url ${this.props.darkMode && 'dark'}`}
+            style={{ display: 'flex' }}
+          >
             {dataSourceURL && (
               <BaseUrl theme={this.props.darkMode ? 'monokai' : 'default'} dataSourceURL={dataSourceURL} />
             )}
