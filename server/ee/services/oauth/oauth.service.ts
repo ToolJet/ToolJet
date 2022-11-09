214c8c3008a25af6128a75bfb4fbe7b51d0df996
Vijaykant Yadav <vjy239@gmail.com>
2022-11-09 19:37:08 +0530

    fix: test cases for oauth

--- server/ee/services/oauth/oauth.service.ts
+++ server/ee/services/oauth/oauth.service.ts
@@ -33,7 +33,7 @@ export class OauthService {
     private readonly googleOAuthService: GoogleOAuthService,
     private readonly gitOAuthService: GitOAuthService,
     private configService: ConfigService
-  ) {}
+  ) { }
 
   #isValidDomain(email: string, restrictedDomain: string): boolean {
     if (!email) {
@@ -302,9 +302,8 @@ export class OauthService {
 
           if (this.configService.get<string>('DISABLE_MULTI_WORKSPACE') !== 'true') {
             return decamelizeKeys({
-              redirectUrl: `${this.configService.get<string>('TOOLJET_HOST')}/invitations/${
-                userDetails.invitationToken
-              }/workspaces/${organizationToken}?oid=${organization.id}&source=${URL_SSO_SOURCE}`,
+              redirectUrl: `${this.configService.get<string>('TOOLJET_HOST')}/invitations/${userDetails.invitationToken
+                }/workspaces/${organizationToken}?oid=${organization.id}&source=${URL_SSO_SOURCE}`,
             });
           } else {
             return decamelizeKeys({
@@ -324,9 +323,8 @@ export class OauthService {
           manager
         );
         return decamelizeKeys({
-          redirectUrl: `${this.configService.get<string>('TOOLJET_HOST')}/invitations/${
-            userDetails.invitationToken
-          }?source=${URL_SSO_SOURCE}`,
+          redirectUrl: `${this.configService.get<string>('TOOLJET_HOST')}/invitations/${userDetails.invitationToken
+            }?source=${URL_SSO_SOURCE}`,
         });
       }
       return await this.authService.generateLoginResultPayload(
