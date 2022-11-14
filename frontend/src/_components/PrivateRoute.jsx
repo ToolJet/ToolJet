8a51d99d72ac778666c8b8cef9779b0729d01f19
Muhsin Shah C P <muhsinshah21@gmail.com>
2022-11-14 12:07:47 +0530

    [enahancement] Added app loader page  (#4389)
    * Working on an issue
    
    * Added loader skeletons
    
    * Working on skeletons
    
    * Completed basic app loader design
    - dark theme
    
    * Added switch organization function
    
    * cleaning code
    
    * Added loader to viewer page
    
    * Fixed svg size
    
    * style fix
    
    * added loader
    
    * have added try-cath
    
    * added redirect path for sso login
    
    * handle json parse error
    
    * Fixed edit app issue
    
    * Code refactoring
    - fixed some issues
    
    * fix
    
    * Worked and tested some sub_path cases
    
    Co-authored-by: Midhun G S <gsmithun4@gmail.com>

--- frontend/src/_components/PrivateRoute.jsx
+++ frontend/src/_components/PrivateRoute.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
 import { authenticationService } from '@/_services';
 
 export const PrivateRoute = ({ component: Component, switchDarkMode, darkMode, ...rest }) => (
