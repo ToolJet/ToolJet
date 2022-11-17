feb9c57dd0fdd64ba83bf406cbef0f84aa40c443
stepinfwd <stepinfwd@gmail.com>
2022-11-17 11:13:35 +0530

    fix :: path

--- frontend/ee/components/LoginPage/GitSSOLoginButton.jsx
+++ frontend/ee/components/LoginPage/GitSSOLoginButton.jsx
@@ -12,7 +12,7 @@ export default function GitSSOLoginButton({ configs, text }) {
   return (
     <div data-cy="git-tile">
       <div onClick={gitLogin} className="sso-button border-0 rounded-2">
-        <img src="assets/images/onboardingassets/SSO/GitHub.svg" data-cy="git-icon" />
+        <img src="../../assets/images/onboardingassets/SSO/GitHub.svg" data-cy="git-icon" />
         <span className="px-1 sso-info-text" data-cy="git-sign-in-text">
           {text || 'Sign in with GitHub'}
         </span>
