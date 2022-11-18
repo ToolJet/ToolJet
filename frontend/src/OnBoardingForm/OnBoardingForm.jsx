8082d57cbdbb5bb1dba4374c7e53bf8f06b53ddc
stepinfwd <stepinfwd@gmail.com>
2022-11-18 15:55:01 +0530

    fix :: edge case name being empty when user types multiple spaces

--- frontend/src/OnBoardingForm/OnBoardingForm.jsx
+++ frontend/src/OnBoardingForm/OnBoardingForm.jsx
@@ -49,7 +49,8 @@ function OnBoardingForm({ userDetails = {}, token = '', organizationToken = '', 
 
   const getuserName = () => {
     let nameArray = userDetails.name.split(' ');
-    if (nameArray.length > 0) return `${nameArray?.[0][0]}${nameArray?.[1] != undefined ? nameArray?.[1][0] : ''} `;
+    if (nameArray.length > 0)
+      return `${nameArray?.[0][0]}${nameArray?.[1] != undefined && nameArray?.[1] != '' ? nameArray?.[1][0] : ''} `;
     return '';
   };
 
