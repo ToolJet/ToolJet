f9da56ea6908ab1828aecfe5c1b0ef0debeb45b4
manishkushare <kushare.manish9@gmail.com>
2022-11-21 13:29:01 +0530

    bug fixed : removed word-break warning

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react';
+// eslint-disable-next-line no-unused-vars
 import config from 'config';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { history } from '@/_helpers';
@@ -74,7 +75,7 @@ class App extends React.Component {
     const { updateAvailable, onboarded, darkMode } = this.state;
     let toastOptions = {
       style: {
-        'word-break': 'break-all',
+        wordBreak: 'break-all',
       },
     };
 
@@ -85,7 +86,7 @@ class App extends React.Component {
           borderRadius: '10px',
           background: '#333',
           color: '#fff',
-          'word-break': 'break-all',
+          wordBreak: 'break-all',
         },
       };
     }
