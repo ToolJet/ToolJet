977e1bd46710049dd309a1102e023965649a311a
gsmithun4 <gsmithun4@gmail.com>
2022-12-05 21:20:56 +0530

    migration fixes

--- server/data-migrations/1667076251897-BackfillDataSources.ts
+++ server/data-migrations/1667076251897-BackfillDataSources.ts
@@ -3,34 +3,50 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class BackfillDataSources1667076251897 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const entityManager = queryRunner.manager;
-    const dataQueries = await entityManager
+
+    const versions = await entityManager
       .createQueryBuilder()
       .select()
-      .from('data_queries', 'data_queries')
-      .andWhere('data_queries.data_source_id IS NULL')
+      .from('app_versions', 'app_versions')
       .getRawMany();
 
-    for (const dataQuery of dataQueries) {
-      const dataSourceResult = await entityManager
-        .createQueryBuilder()
-        .insert()
-        .into('data_sources')
-        .values({
-          name: `${dataQuery.name}default`,
-          kind: `${dataQuery.kind}default`,
-          plugin_id: dataQuery.plugin_id,
-          app_version_id: dataQuery.app_version_id,
-        })
-        .execute();
+    for (const version of versions) {
+      let runjsDS, restapiDS;
+      for (const kind of ['runjs', 'restapi']) {
+        const dataSourceResult = await entityManager
+          .createQueryBuilder()
+          .insert()
+          .into('data_sources')
+          .values({
+            name: `${kind}default`,
+            kind: `${kind}default`,
+            app_version_id: version.id,
+          })
+          .execute();
 
-      const dataSource = dataSourceResult.generatedMaps[0];
+        if (kind === 'runjs') {
+          runjsDS = dataSourceResult.generatedMaps[0].id;
+        } else {
+          restapiDS = dataSourceResult.generatedMaps[0].id;
+        }
 
-      await entityManager
-        .createQueryBuilder()
-        .update('data_queries')
-        .set({ data_source_id: dataSource.id })
-        .where({ id: dataQuery.id })
-        .execute();
+        const dataQueries = await entityManager
+          .createQueryBuilder()
+          .select()
+          .from('data_queries', 'data_queries')
+          .andWhere('data_queries.data_source_id IS NULL')
+          .andWhere('data_queries.app_version_id = :app_version_id', { app_version_id: version.id })
+          .getRawMany();
+
+        for (const dataQuery of dataQueries) {
+          await entityManager
+            .createQueryBuilder()
+            .update('data_queries')
+            .set({ data_source_id: dataQuery.kind === 'runjs' ? runjsDS : restapiDS })
+            .where({ id: dataQuery.id })
+            .execute();
+        }
+      }
     }
   }
 
