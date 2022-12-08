f6a9321d9afba3cebcc7823a96f9b51a9b33386e
Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
2022-12-08 14:41:02 +0530

    [Feature]: Form Widget (#4595)
    * Created form widget
    
    * Fixed the same name issue in defaultChildren
    
    * Dynamically added buttons to the form inspector
    
    * Added data to exposed variable and handled button click
    
    * Added Reset functionality, CSA to the form widget
    
    * Added Form widget icon
    
    * form component docs
    
    Co-authored-by: Shubhendra <withshubh@gmail.com>

---
+++ frontend/src/Editor/Inspector/Components/Form.jsx
@@ -0,0 +1,175 @@
+import React from 'react';
+import Accordion from '@/_ui/Accordion';
+import { EventManager } from '../EventManager';
+import { renderElement } from '../Utils';
+// eslint-disable-next-line import/no-unresolved
+import i18next from 'i18next';
+
+export const Form = ({ componentMeta, darkMode, ...restProps }) => {
+  const {
+    layoutPropertyChanged,
+    component,
+    paramUpdated,
+    dataQueries,
+    currentState,
+    eventsChanged,
+    apps,
+    allComponents,
+  } = restProps;
+
+  const properties = Object.keys(componentMeta.properties);
+  const events = Object.keys(componentMeta.events);
+  const validations = Object.keys(componentMeta.validation || {});
+
+  const { id } = component;
+  const newOptions = [{ name: 'None', value: 'none' }];
+  Object.entries(allComponents).forEach(([componentId, component]) => {
+    if (component.parent === id && component?.component?.component === 'Button') {
+      newOptions.push({ name: component.component.name, value: componentId });
+    }
+  });
+
+  componentMeta.properties.buttonToSubmit.options = newOptions;
+
+  const accordionItems = baseComponentProperties(
+    properties,
+    events,
+    component,
+    componentMeta,
+    layoutPropertyChanged,
+    paramUpdated,
+    dataQueries,
+    currentState,
+    eventsChanged,
+    apps,
+    allComponents,
+    validations,
+    darkMode
+  );
+
+  return <Accordion items={accordionItems} />;
+};
+
+export const baseComponentProperties = (
+  properties,
+  events,
+  component,
+  componentMeta,
+  layoutPropertyChanged,
+  paramUpdated,
+  dataQueries,
+  currentState,
+  eventsChanged,
+  apps,
+  allComponents,
+  validations,
+  darkMode
+) => {
+  let items = [];
+  if (properties.length > 0) {
+    items.push({
+      title: `${i18next.t('widget.common.properties', 'Properties')}`,
+      children: properties.map((property) =>
+        renderElement(
+          component,
+          componentMeta,
+          paramUpdated,
+          dataQueries,
+          property,
+          'properties',
+          currentState,
+          allComponents,
+          darkMode
+        )
+      ),
+    });
+  }
+
+  if (events.length > 0) {
+    items.push({
+      title: `${i18next.t('widget.common.events', 'Events')}`,
+      isOpen: true,
+      children: (
+        <EventManager
+          component={component}
+          componentMeta={componentMeta}
+          currentState={currentState}
+          dataQueries={dataQueries}
+          components={allComponents}
+          eventsChanged={eventsChanged}
+          apps={apps}
+          darkMode={darkMode}
+        />
+      ),
+    });
+  }
+
+  if (validations.length > 0) {
+    items.push({
+      title: `${i18next.t('widget.common.validation', 'Validation')}`,
+      children: validations.map((property) =>
+        renderElement(
+          component,
+          componentMeta,
+          paramUpdated,
+          dataQueries,
+          property,
+          'validation',
+          currentState,
+          allComponents,
+          darkMode
+        )
+      ),
+    });
+  }
+
+  items.push({
+    title: `${i18next.t('widget.common.general', 'General')}`,
+    isOpen: true,
+    children: (
+      <>
+        {renderElement(
+          component,
+          componentMeta,
+          layoutPropertyChanged,
+          dataQueries,
+          'tooltip',
+          'general',
+          currentState,
+          allComponents
+        )}
+      </>
+    ),
+  });
+
+  items.push({
+    title: `${i18next.t('widget.common.layout', 'Layout')}`,
+    isOpen: true,
+    children: (
+      <>
+        {renderElement(
+          component,
+          componentMeta,
+          layoutPropertyChanged,
+          dataQueries,
+          'showOnDesktop',
+          'others',
+          currentState,
+          allComponents
+        )}
+        {renderElement(
+          component,
+          componentMeta,
+          layoutPropertyChanged,
+          dataQueries,
+          'showOnMobile',
+          'others',
+          currentState,
+          allComponents
+        )}
+      </>
+    ),
+  });
+
+  return items;
+};
