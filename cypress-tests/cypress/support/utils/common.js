d4ff5af7f856beddeb38ee8d9e6da1f3aa5d0838
Ajith KV <ajith.jaban@gmail.com>
2022-12-09 18:42:31 +0530

    [Cypress] Modify manage sso spec in multi workspace (#4865)
    * Modify manage sso spec and add cypress test to verify workspace login page
    
    * Modify manage sso spec and add cypress test to verify login and sign up page
    
    * Add user onboarding test using GitHub and Google SSO
    
    * Modify the spec with new methods
    
    * Update sso id

--- cypress-tests/cypress/support/utils/common.js
+++ cypress-tests/cypress/support/utils/common.js
@@ -3,6 +3,7 @@ import { usersSelector } from "Selectors/manageUsers";
 import { profileSelector } from "Selectors/profile";
 import { commonSelectors } from "Selectors/common";
 import moment from "moment";
+import { dashboardSelector } from "Selectors/dashboard";
 
 export const navigateToProfile = () => {
   cy.get(profileSelector.profileDropdown).invoke("show");
@@ -29,9 +30,12 @@ export const navigateToManageGroups = () => {
 };
 
 export const navigateToManageSSO = () => {
-  cy.get(commonSelectors.dropdown).invoke("show");
-  cy.contains("Manage SSO").click();
-  cy.url().should("include", path.manageSSO);
+  cy.url().then(($url) => {
+    if (!$url.includes(path.manageSSO)) {
+      cy.get(commonSelectors.dropdown).invoke("show");
+      cy.contains("Manage SSO").click();
+    }
+  });
 };
 
 export const randomDateOrTime = (format = "DD/MM/YYYY") => {
@@ -151,4 +155,14 @@ export const manageUsersPagination = (email) => {
 export const searchUser = (email) => {
   cy.clearAndType(commonSelectors.emailFilterInput, email);
   cy.get(commonSelectors.filterButton).click();
-}
+};
+
+export const createWorkspace = (workspaceName) => {
+  cy.get(usersSelector.dropdown).invoke("show");
+  cy.get(commonSelectors.addWorkspaceButton).click();
+  cy.clearAndType(commonSelectors.workspaceNameInput, workspaceName);
+  cy.intercept("GET", "/api/apps?page=1&folder=&searchKey=").as("homePage");
+  cy.get(commonSelectors.createWorkspaceButton).click();
+  cy.wait("@homePage");
+  cy.get(dashboardSelector.modeToggle, { timeout: 10000 }).should("be.visible");
+};
