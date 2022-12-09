cbc74e84d19d9a5c1d897d6d6c391e83634e4282
Kavin Venkatachalam <kavin.saratha@gmail.com>
2022-12-09 12:36:31 +0530

    Fixed HTML entities shown when creating new versions

--- server/src/dto/version-create.dto.ts
+++ server/src/dto/version-create.dto.ts
@@ -1,10 +1,7 @@
 import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
-import { Transform } from 'class-transformer';
-import { sanitizeInput } from '../helpers/utils.helper';
 
 export class VersionCreateDto {
   @IsString()
-  @Transform(({ value }) => sanitizeInput(value))
   @IsNotEmpty()
   versionName: string;
 
