6e6201414c9f3c13af931c4ea76c3cbce62abf69
Michael <michaelbarr24@hotmail.com>
2022-12-12 19:52:54 +0000

    fix: add dto validation to folder name

--- server/src/dto/create-folder.dto.ts
+++ server/src/dto/create-folder.dto.ts
@@ -1,10 +1,12 @@
 import { Transform } from 'class-transformer';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 import { sanitizeInput } from 'src/helpers/utils.helper';
 
 export class CreateFolderDto {
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => sanitizeInput(value))
+  @MaxLength(25, { message: 'Folder name cannot be longer than 25 characters' })
+  @MinLength(0, { message: 'Folder name cannot be empty' })
   name: string;
 }
