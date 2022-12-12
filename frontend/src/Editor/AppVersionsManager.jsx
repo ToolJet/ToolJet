7e334e8aeaf88236fedaaba57f484311198230f6
Kavin Venkatachalam <kavin.saratha@gmail.com>
2022-12-12 10:35:31 +0530

    Reverted to the default

--- frontend/src/Editor/AppVersionsManager.jsx
+++ frontend/src/Editor/AppVersionsManager.jsx
@@ -6,6 +6,7 @@ import { Confirm } from './Viewer/Confirm';
 import Select from '../_ui/Select';
 import defaultStyle from '../_ui/Select/styles';
 import { useTranslation } from 'react-i18next';
+import DOMPurify from 'dompurify';
 
 export const AppVersionsManager = function AppVersionsManager({
   appId,
@@ -185,7 +186,7 @@ export const AppVersionsManager = function AppVersionsManager({
       >
         <span className={`${releasedVersionId === editingAppVersion.id ? 'released' : ''}`}>
           {releasedVersionId === editingAppVersion.id && <img src={'assets/images/icons/editor/deploy-rocket.svg'} />}
-          <span className="px-1">{editingAppVersion.name}</span>
+          <span className="px-1" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(editingAppVersion.name) }} />
         </span>
         {showDropDown && (
           <>
@@ -199,7 +200,10 @@ export const AppVersionsManager = function AppVersionsManager({
                         key={version.id}
                         onClick={() => selectVersion(version)}
                       >
-                        <div className="col-md-4">{version.name}</div>
+                        <div
+                          className="col-md-4"
+                          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(version.name) }}
+                        />
                         <div className="released-subtext">
                           <img src={'/assets/images/icons/editor/deploy-rocket.svg'} />
                           <span className="px-1">
@@ -218,7 +222,10 @@ export const AppVersionsManager = function AppVersionsManager({
                         onMouseEnter={() => setMouseHoveredOnVersion(version.id)}
                         onMouseLeave={() => setMouseHoveredOnVersion(null)}
                       >
-                        <div className="col-md-4">{version.name}</div>
+                        <div
+                          className="col-md-4"
+                          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(version.name) }}
+                        />
 
                         <div className="col-md-2 offset-md-5 d-flex" style={{ gap: 5, paddingLeft: 10 }}>
                           <button
--- server/src/dto/version-create.dto.ts
+++ server/src/dto/version-create.dto.ts
@@ -1,7 +1,10 @@
 import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { sanitizeInput } from '../helpers/utils.helper';
 
 export class VersionCreateDto {
   @IsString()
+  @Transform(({ value }) => sanitizeInput(value))
   @IsNotEmpty()
   versionName: string;
 
