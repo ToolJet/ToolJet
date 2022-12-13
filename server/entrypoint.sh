2f7f86033a6337a75b1f8a6017ea5f1edab9bb13
Michael <michaelbarr24@hotmail.com>
2022-12-13 07:31:39 +0000

    fix: dto version name validation

---
+++ server/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [ -d "./server/dist" ]
+then
+  SETUP_CMD='npm run db:setup:prod'
+else
+  SETUP_CMD='npm run db:setup'
+fi
+
+if [ -f "./.env" ]
+then
+  declare $(grep -v '^#' ./.env | xargs) 
+fi
+
+./server/scripts/wait-for-it.sh $PG_HOST:${PG_PORT:-5432} --strict --timeout=300 -- $SETUP_CMD
+
+exec "$@"
