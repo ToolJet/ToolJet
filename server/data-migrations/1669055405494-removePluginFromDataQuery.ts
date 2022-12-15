145a55fe64748300d192a42a640626f526f2e64f
Midhun G S <gsmithun4@gmail.com>
2022-12-15 16:01:10 +0530

    fix for app preview error (#4994)

--- server/data-migrations/1669055405494-removePluginFromDataQuery.ts
+++ server/data-migrations/1669055405494-removePluginFromDataQuery.ts
@@ -3,43 +3,18 @@ import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 export class removePluginFromDataQuery1669055405494 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('data_queries', 'plugin_id');
-
-    await queryRunner.changeColumn(
-      'data_queries',
-      new TableColumn({
-        name: 'data_source_id',
-        type: 'uuid',
-      }),
-      new TableColumn({
-        name: 'data_source_id',
-        type: 'uuid',
-        isNullable: false,
-      })
-    );
+    await queryRunner.query('ALTER TABLE data_queries ALTER COLUMN data_source_id DROP NOT NULL;');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
-      'data_sources',
+      'data_queries',
       new TableColumn({
         name: 'plugin_id',
         type: 'uuid',
         isNullable: true,
       })
     );
-
-    await queryRunner.changeColumn(
-      'data_queries',
-      new TableColumn({
-        name: 'data_source_id',
-        type: 'uuid',
-        isNullable: false,
-      }),
-      new TableColumn({
-        name: 'data_source_id',
-        type: 'uuid',
-        isNullable: true,
-      })
-    );
+    await queryRunner.query('ALTER TABLE data_queries ALTER COLUMN data_source_id SET NOT NULL;');
   }
 }
