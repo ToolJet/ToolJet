68393cecb2a322531220a9e2752c02d8273412dd
vjaris42 <vjy239@gmail.com>
2022-12-21 12:32:39 +0530

    [Feature] :: Allow users to invite users in bulk (#4995)
    * add: ability to bulk upload users
    
    * refactor: inviting users asynchronously
    
    * chore: add transaction wrap
    
    * chore: fetch existing groups initially
    
    * chore: add transaction wrap
    
    * chore: add package-lock
    
    * fix: namespace issue
    
    * fix: empty group names

--- server/src/services/users.service.ts
+++ server/src/services/users.service.ts
@@ -28,26 +28,38 @@ export class UsersService {
     return this.usersRepository.findOne({ where: { id } });
   }
 
-  async findByEmail(email: string, organizationId?: string, status?: string | Array<string>): Promise<User> {
-    if (!organizationId) {
-      return this.usersRepository.findOne({
-        where: { email },
-      });
-    } else {
-      const statusList = status ? (typeof status === 'object' ? status : [status]) : ['active', 'invited', 'archived'];
-      return await createQueryBuilder(User, 'users')
-        .innerJoinAndSelect(
-          'users.organizationUsers',
-          'organization_users',
-          'organization_users.organizationId = :organizationId',
-          { organizationId }
-        )
-        .where('organization_users.status IN(:...statusList)', {
-          statusList,
-        })
-        .andWhere('users.email = :email', { email })
-        .getOne();
-    }
+  async findByEmail(
+    email: string,
+    organizationId?: string,
+    status?: string | Array<string>,
+    manager?: EntityManager
+  ): Promise<User> {
+    return await dbTransactionWrap(async (manager: EntityManager) => {
+      if (!organizationId) {
+        return manager.findOne(User, {
+          where: { email },
+        });
+      } else {
+        const statusList = status
+          ? typeof status === 'object'
+            ? status
+            : [status]
+          : ['active', 'invited', 'archived'];
+        return await manager
+          .createQueryBuilder(User, 'users')
+          .innerJoinAndSelect(
+            'users.organizationUsers',
+            'organization_users',
+            'organization_users.organizationId = :organizationId',
+            { organizationId }
+          )
+          .where('organization_users.status IN(:...statusList)', {
+            statusList,
+          })
+          .andWhere('users.email = :email', { email })
+          .getOne();
+      }
+    }, manager);
   }
 
   async findByPasswordResetToken(token: string): Promise<User> {
