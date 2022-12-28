f87f3afd26b0bd140bd345b07a8733f02c4e1ad3
Mohamed ABDELLANI <abdellani@gmail.com>
2022-12-28 23:44:53 +0100

    implement test connection

--- plugins/packages/mindsdb/lib/index.ts
+++ plugins/packages/mindsdb/lib/index.ts
@@ -1,9 +1,10 @@
-import { QueryResult, QueryService } from '@tooljet-plugins/common';
+import { ConnectionTestResult, QueryResult, QueryService } from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions } from './types';
 import MindsDB from 'mindsdb-js-sdk';
 
 export default class Mindsdb implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
+    this.connect(sourceOptions);
     return {
       status: 'ok',
       data: {},
@@ -11,5 +12,12 @@ export default class Mindsdb implements QueryService {
   }
   connect(sourceOptions: SourceOptions) {
     MindsDB.connect(sourceOptions.url, [{ key: 'apikey', value: sourceOptions.apikey }]);
+  }
+  async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
+    this.connect(sourceOptions);
+    const result = await MindsDB.ping();
+    return {
+      status: result ? 'ok' : 'failed',
+    };
   }
 }
