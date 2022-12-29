0436d095454d798645a5f0bedf237e69e691b6b1
emidhun <midhun752@gmail.com>
2022-12-29 10:51:22 +0530

    Add automation for MongoDB

---
+++ cypress-tests/cypress/constants/texts/mongoDb.js
@@ -0,0 +1,13 @@
+export const mongoDbText = {
+  mongoDb: "MongoDB",
+  cypressMongoDb: "cypress-mongodb",
+  errorConnectionRefused: "connect ECONNREFUSED 127.0.0.1:27017",
+
+  optionConnectUsingConnectionString: "Connect using connection string{enter}",
+  labelConnectionString: "Connection string",
+
+  errorInvalisScheme:
+    'Invalid scheme, expected connection string to start with "mongodb://" or "mongodb+srv://"',
+  connectionStringPlaceholder:
+    "mongodb+srv://tooljet:<password>@cluster0.i1vq4.mongodb.net/mydb?retryWrites=true&w=majority",
+};
---
+++ cypress-tests/cypress/e2e/editor/data-source/mongoDbHappyPath.cy.js
@@ -0,0 +1,183 @@
+import { postgreSqlSelector } from "Selectors/postgreSql";
+import { postgreSqlText } from "Texts/postgreSql";
+import { mongoDbText } from "Texts/mongoDb";
+import { commonSelectors } from "Selectors/common";
+import {
+  fillDataSourceTextField,
+  selectDataSource,
+} from "Support/utils/postgreSql";
+import { verifyCouldnotConnectWithAlert } from "Support/utils/dataSource";
+
+describe("Data source MongoDB", () => {
+  beforeEach(() => {
+    cy.appUILogin();
+    cy.createApp();
+  });
+
+  it("Should verify elements on MongoDB connection form", () => {
+    cy.get(postgreSqlSelector.leftSidebarDatasourceButton).click();
+    cy.get(postgreSqlSelector.labelDataSources).should(
+      "have.text",
+      postgreSqlText.labelDataSources
+    );
+
+    cy.get(postgreSqlSelector.addDatasourceLink)
+      .should("have.text", postgreSqlText.labelAddDataSource)
+      .click();
+
+    cy.get(postgreSqlSelector.allDatasourceLabelAndCount).should(
+      "have.text",
+      postgreSqlText.allDataSources
+    );
+    cy.get(postgreSqlSelector.databaseLabelAndCount).should(
+      "have.text",
+      postgreSqlText.allDatabase
+    );
+    cy.get(postgreSqlSelector.apiLabelAndCount).should(
+      "have.text",
+      postgreSqlText.allApis
+    );
+    cy.get(postgreSqlSelector.cloudStorageLabelAndCount).should(
+      "have.text",
+      postgreSqlText.allCloudStorage
+    );
+
+    cy.get(postgreSqlSelector.dataSourceSearchInputField).type(
+      mongoDbText.mongoDb
+    );
+    cy.get("[data-cy*='data-source-']")
+      .eq(0)
+      .should("contain", mongoDbText.mongoDb);
+    cy.get('[data-cy="data-source-mongodb"]').click();
+
+    cy.get(postgreSqlSelector.dataSourceNameInputField).should(
+      "have.value",
+      mongoDbText.mongoDb
+    );
+    cy.get(postgreSqlSelector.labelHost).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelHost
+    );
+    cy.get(postgreSqlSelector.labelPort).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelPort
+    );
+    cy.get(postgreSqlSelector.labelDbName).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelDbName
+    );
+    cy.get(postgreSqlSelector.labelUserName).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelUserName
+    );
+    cy.get(postgreSqlSelector.labelPassword).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelPassword
+    );
+    cy.get(postgreSqlSelector.labelIpWhitelist).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.whiteListIpText
+    );
+    cy.get(postgreSqlSelector.buttonCopyIp).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.textCopy
+    );
+
+    cy.get(postgreSqlSelector.linkReadDocumentation).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.readDocumentation
+    );
+    cy.get(postgreSqlSelector.buttonTestConnection)
+      .verifyVisibleElement(
+        "have.text",
+        postgreSqlText.buttonTextTestConnection
+      )
+      .click();
+    cy.get(postgreSqlSelector.connectionFailedText).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.couldNotConnect,
+      { timeout: 65000 }
+    );
+    cy.get(postgreSqlSelector.buttonSave).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.buttonTextSave
+    );
+    cy.get(postgreSqlSelector.dangerAlertNotSupportSSL).verifyVisibleElement(
+      "have.text",
+      mongoDbText.errorConnectionRefused
+    );
+    cy.get('[data-cy="query-select-dropdown"]').type(
+      mongoDbText.optionConnectUsingConnectionString
+    );
+    cy.get('[data-cy="label-connection-string"]').verifyVisibleElement(
+      "have.text",
+      mongoDbText.labelConnectionString
+    );
+    cy.get(postgreSqlSelector.labelIpWhitelist).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.whiteListIpText
+    );
+    cy.get(postgreSqlSelector.buttonCopyIp).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.textCopy
+    );
+
+    cy.get(postgreSqlSelector.linkReadDocumentation).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.readDocumentation
+    );
+    cy.get(postgreSqlSelector.buttonTestConnection)
+      .verifyVisibleElement(
+        "have.text",
+        postgreSqlText.buttonTextTestConnection
+      )
+      .click();
+    cy.get(postgreSqlSelector.connectionFailedText).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.couldNotConnect,
+      { timeout: 60000 }
+    );
+    verifyCouldnotConnectWithAlert(mongoDbText.errorInvalisScheme);
+    cy.get(postgreSqlSelector.buttonSave).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.buttonTextSave
+    );
+  });
+
+  it("Should verify the functionality of MongoDB connection form.", () => {
+    selectDataSource(mongoDbText.mongoDb);
+
+    cy.clearAndType(
+      '[data-cy="data-source-name-input-filed"]',
+      mongoDbText.cypressMongoDb
+    );
+
+    cy.get('[data-cy="query-select-dropdown"]').type(
+      mongoDbText.optionConnectUsingConnectionString
+    );
+
+    fillDataSourceTextField(
+      mongoDbText.labelConnectionString,
+      mongoDbText.connectionStringPlaceholder,
+      Cypress.env("mongodb_connString"),
+      "contain",
+      { parseSpecialCharSequences: false, delay: 0 }
+    );
+    cy.get(postgreSqlSelector.buttonTestConnection).click();
+    cy.get(postgreSqlSelector.textConnectionVerified, {
+      timeout: 10000,
+    }).should("have.text", postgreSqlText.labelConnectionVerified);
+    cy.get(postgreSqlSelector.buttonSave).click();
+
+    cy.verifyToastMessage(
+      commonSelectors.toastMessage,
+      postgreSqlText.toastDSAdded
+    );
+
+    cy.get(postgreSqlSelector.leftSidebarDatasourceButton).click();
+    cy.get(postgreSqlSelector.datasourceLabelOnList)
+      .should("have.text", mongoDbText.cypressMongoDb)
+      .find("button")
+      .should("be.visible");
+  });
+});
