3d0599498287c4c5e4f956449a764a594d72b069
Mohamed ABDELLANI <abdellani@gmail.com>
2022-12-30 01:00:16 +0100

    implement operations

--- plugins/packages/mindsdb/lib/index.ts
+++ plugins/packages/mindsdb/lib/index.ts
@@ -1,13 +1,30 @@
-import { ConnectionTestResult, QueryResult, QueryService } from '@tooljet-plugins/common';
+import { ConnectionTestResult, QueryError, QueryResult, QueryService } from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions } from './types';
 import MindsDB from 'mindsdb-js-sdk';
 
 export default class Mindsdb implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     this.connect(sourceOptions);
+    let result = {};
+    const operation = queryOptions.operation;
+    try {
+      switch (operation) {
+        case 'list_predictors':
+          result = await MindsDB.predictors();
+          break;
+        case 'list_data_sources':
+          result = await MindsDB.dataSources();
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+      throw new QueryError('Query could not be completed', error, {});
+    } finally {
+      MindsDB.disconnect();
+    }
     return {
       status: 'ok',
-      data: {},
+      data: result,
     };
   }
   connect(sourceOptions: SourceOptions) {
@@ -20,4 +37,12 @@ export default class Mindsdb implements QueryService {
       status: result ? 'ok' : 'failed',
     };
   }
+  /*
+  listPredictors
+  listDataSources
+  getDataSource
+  getPredictor
+  queryPredictor
+  dataSourceLoadData
+*/
 }
