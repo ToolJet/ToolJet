1013e3ef940107afc03cf176b22e5dee7c84b50b
manishkushare <kushare.manish9@gmail.com>
2023-01-02 12:49:25 +0530

    made the suggested changes

--- frontend/src/Editor/AppVersionsManager/EditVersionModal.jsx
+++ frontend/src/Editor/AppVersionsManager/EditVersionModal.jsx
@@ -44,7 +44,7 @@ export const EditVersion = ({
   return (
     <AlertDialog
       show={showEditAppVersion}
-      closeModal={() => setShowEditAppVersion(!showEditAppVersion)}
+      closeModal={() => setShowEditAppVersion(false)}
       title={t('editor.appVersionManager.editVersion', 'Edit Version')}
     >
       <div className="row mb-3">
