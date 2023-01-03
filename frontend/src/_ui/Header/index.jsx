26d1584bec9f1e537004abcd097eed2a44809184
gandharv <gandharvkumargarg@gmail.com>
2023-01-03 18:58:27 +0530

    hide selector if single workspace

--- frontend/src/_ui/Header/index.jsx
+++ frontend/src/_ui/Header/index.jsx
@@ -6,11 +6,13 @@ import { OrganizationList } from '@/_components/OrganizationManager/List';
 function Header() {
   const currentVersion = localStorage.getItem('currentVersion');
   const darkMode = localStorage.getItem('darkMode') === 'true';
+  const isSingleOrganization = window.public_config?.DISABLE_MULTI_WORKSPACE === 'true';
+
   return (
     <header className="layout-header">
       <div className="row w-100 gx-0">
         <div className="organization-selector col border-end border-bottom">
-          <OrganizationList />
+          {!isSingleOrganization && <OrganizationList />}
         </div>
         <div className="col border-bottom m-auto" style={{ padding: 13.5 }}>
           <div className="d-flex justify-content-sm-between">
