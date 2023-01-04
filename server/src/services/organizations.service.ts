62d18c1a02420e46f31875ae4a2e42e89b117274
Vijaykant Yadav <vjy239@gmail.com>
2023-01-04 13:48:53 +0530

    fix: avatar and last name blank issues

--- server/src/services/organizations.service.ts
+++ server/src/services/organizations.service.ts
@@ -585,7 +585,7 @@ export class OrganizationsService {
             user.invitationToken,
             `${organizationUser.invitationToken}?oid=${organizationUser.organizationId}`,
             currentOrganization.name,
-            `${currentUser.firstName} ${currentUser.lastName}`
+            `${currentUser.firstName} ${currentUser.lastName ?? ''}`
           )
           .catch((err) => console.error('Error while sending welcome mail', err));
       } else {
@@ -593,7 +593,7 @@ export class OrganizationsService {
           .sendOrganizationUserWelcomeEmail(
             user.email,
             user.firstName,
-            `${currentUser.firstName} ${currentUser.lastName}`,
+            `${currentUser.firstName} ${currentUser.lastName ?? ''}`,
             `${organizationUser.invitationToken}?oid=${organizationUser.organizationId}`,
             currentOrganization.name
           )
