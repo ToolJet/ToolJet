92b26cd9a74b405c81e3bdd04ba345948bff0c9a
Sherfin Shamsudeen <sherfin94@gmail.com>
2023-01-07 23:18:50 +0530

    Change the position of id inside page object

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1622,10 +1622,10 @@ class EditorComponent extends React.Component {
     const { globals: existingGlobals } = this.state.currentState;
 
     const page = {
+      id: pageId,
       name,
       handle,
       variables: this.state.pages?.[pageId]?.variables ?? {},
-      id: pageId,
     };
 
     const globals = {
