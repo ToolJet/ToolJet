bc9c29e78806e793a05e61a7ca2b9983371b1659
Jittojoyes98 <51293782+Jittojoyes98@users.noreply.github.com>
2023-01-09 12:23:53 +0530

    [docs] Fixed edit page link (#5253)
    * [docs]:fixed edit page link
    
    * [docs]:fixed edit page link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -115,7 +115,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/ToolJet/Tooljet/blob/main/docs/',
+            'https://github.com/ToolJet/Tooljet/blob/develop/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
