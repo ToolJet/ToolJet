7fcf859661a73be91dfa0cec5da188e10426375f
Muhsin Shah <muhsinshah21@gmail.com>
2023-01-09 16:37:23 +0530

    typo

--- server/test/controllers/tooljet_db.e2e-spec.ts
+++ server/test/controllers/tooljet_db.e2e-spec.ts
@@ -9,7 +9,7 @@ import got from 'got';
 jest.mock('got');
 const mockedGot = mocked(got);
 
-//TODO: this spec will need postgrest intance to run (skipping for now)
+//TODO: this spec will need postgrest instance to run (skipping for now)
 describe.skip('Tooljet DB controller', () => {
   let nestApp: INestApplication;
   let mockConfig;
