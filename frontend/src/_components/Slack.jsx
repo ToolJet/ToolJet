9635eca16af521a8eab3f6d431b0eb9f07f48d14
arpitnath <arpitnath42@gmail.com>
2023-01-10 14:47:33 +0530

    undefined selected ds fix:slack

--- frontend/src/_components/Slack.jsx
+++ frontend/src/_components/Slack.jsx
@@ -13,7 +13,7 @@ const Slack = ({ optionchanged, createDataSource, options, isSaving, selectedDat
     setAuthStatus('waiting_for_url');
 
     const scope =
-      options.access_type === 'chat:write'
+      options?.access_type === 'chat:write'
         ? 'chat:write,users:read,chat:write:bot,chat:write:user'
         : 'chat:write,users:read';
 
@@ -56,7 +56,7 @@ const Slack = ({ optionchanged, createDataSource, options, isSaving, selectedDat
                   className="form-check-input"
                   type="radio"
                   onClick={() => optionchanged('access_type', 'chat:write')}
-                  checked={options.access_type?.value === 'chat:write'}
+                  checked={options?.access_type?.value === 'chat:write'}
                   disabled={authStatus === 'waiting_for_token'}
                 />
                 <span className="form-check-label">
