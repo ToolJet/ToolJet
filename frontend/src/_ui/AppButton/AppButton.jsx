440a485ed927c0496dc1df4d7e16ee4ced6ba0dc
stepinfwd <stepinfwd@gmail.com>
2023-01-18 00:44:49 +0530

    feat :: adding button styles and button component

--- frontend/src/_ui/AppButton/AppButton.jsx
+++ frontend/src/_ui/AppButton/AppButton.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 export const ButtonBase = function ButtonBase(props) {
   const mapBaseSize = {
-    base: 'tj-btn-base',
-    lg: 'tj-btn-large',
+    lg: 'tj-large-btn',
+    md: 'tj-medium-btn',
+    sm: 'tj-small-btn',
+    xs: 'tj-extra-small-btn',
   };
 
   const {
     className,
-    size = 'base',
+    size = 'lg',
     as = 'button', // render it as a button or an anchor.
     children,
     disabled,
@@ -18,7 +20,7 @@ export const ButtonBase = function ButtonBase(props) {
   const Element = as;
 
   return (
-    <Element {...restProps} className={`tj-btn ${mapBaseSize[size]}  ${className}`} disabled={disabled}>
+    <Element {...restProps} className={`tj-base-btn ${mapBaseSize[size]}  ${className}`} disabled={disabled}>
       {children}
     </Element>
   );
@@ -26,11 +28,13 @@ export const ButtonBase = function ButtonBase(props) {
 
 export const ButtonSolid = function ButtonSolid(props) {
   const mapVariant = {
-    primary: 'tj-btn-primary',
-    ghost: 'tj-btn-ghost',
-    secondary: 'tj-btn-secondary',
-    tertiary: 'tj-btn-tertiary',
-    disabled: 'tj-btn-disabled',
+    primary: 'tj-primary-btn',
+    ghostBlue: 'tj-ghost-blue-btn',
+    ghostBlack: 'tj-ghost-black-btn',
+    secondary: 'tj-secondary-btn',
+    tertiary: 'tj-tertiary-btn',
+    disabled: 'tj-disabled-btn',
+    danger: 'tj-danger-btn',
   };
 
   const { variant = 'primary', className, ...restProps } = props;
@@ -38,23 +42,13 @@ export const ButtonSolid = function ButtonSolid(props) {
 };
 
 export const IconButton = function IconButton(props) {
-  const { className, size = 'base', as = 'button', Icon, ...restProps } = props;
+  const { className, size = 'lg', as = 'button', Icon, ...restProps } = props;
 
   const Element = as;
 
   return (
-    <Element {...restProps} className={`generated-icon-classnames ${className}`}>
+    <Element {...restProps} className={`tj-icon-btn ${className}`}>
       {Icon}
     </Element>
   );
 };
-
-{
-  /* <li className="text-center mt-3 cursor-pointer">
-<Link to="/workspace-settings">
-  <ToolTip message="Workspace settings" placement="right">
-  
-  </ToolTip>
-</Link>
-</li> */
-}
