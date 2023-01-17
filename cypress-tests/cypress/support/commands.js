9c8cddebe93de291f458f049bbab72ff0126af59
Ajith KV <ajith.jaban@gmail.com>
2023-01-17 17:47:53 +0530

    Fix and modify login spec (#5345)

--- cypress-tests/cypress/support/commands.js
+++ cypress-tests/cypress/support/commands.js
@@ -1,18 +1,16 @@
 import { commonSelectors, commonWidgetSelector } from "Selectors/common";
 import { dashboardSelector } from "Selectors/dashboard";
-import { loginSelectors } from "Selectors/login";
 import { ssoSelector } from "Selectors/manageSSO";
 import { commonText, createBackspaceText } from "Texts/common";
 import { passwordInputText } from "Texts/passwordInput";
 
 Cypress.Commands.add("login", (email, password) => {
   cy.visit("/");
-  cy.clearAndType(loginSelectors.emailField, email);
-  cy.clearAndType(loginSelectors.passwordField, password);
-  cy.intercept("GET", "/api/apps?page=1&folder=&searchKey=").as("homePage");
-  cy.get(loginSelectors.signInButton).click();
-  cy.get(loginSelectors.homePage).should("be.visible");
-  cy.wait("@homePage");
+  cy.clearAndType(commonSelectors.workEmailInputField, "dev@tooljet.io");
+  cy.clearAndType(commonSelectors.passwordInputField, "password");
+  cy.get(commonSelectors.signInButton).click();
+  cy.get(commonSelectors.homePageLogo).should("be.visible");
+  cy.wait(2000)
 });
 
 Cypress.Commands.add("clearAndType", (selector, text) => {
@@ -129,7 +127,7 @@ Cypress.Commands.add("appUILogin", () => {
   cy.visit("/");
   cy.clearAndType(commonSelectors.workEmailInputField, "dev@tooljet.io");
   cy.clearAndType(commonSelectors.passwordInputField, "password");
-  cy.get(loginSelectors.signInButton).click();
+  cy.get(commonSelectors.signInButton).click();
   cy.get(commonSelectors.homePageLogo).should("be.visible");
   cy.wait(2000);
   cy.get("body").then(($el) => {
