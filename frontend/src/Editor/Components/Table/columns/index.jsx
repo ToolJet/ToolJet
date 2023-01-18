c220fcc57113f56aef53d1e6ad7e4a4ed5146dbb
manishkushare <kushare.manish9@gmail.com>
2023-01-18 16:38:34 +0530

    bug fixed - shwowing blank spaces for undefined and null values in the respective cells

--- frontend/src/Editor/Components/Table/columns/index.jsx
+++ frontend/src/Editor/Components/Table/columns/index.jsx
@@ -68,7 +68,7 @@ export default function generateColumnsData({
       isEditable: column.isEditable,
       Cell: function (cell) {
         const rowChangeSet = changeSet ? changeSet[cell.row.index] : null;
-        const cellValue = rowChangeSet ? rowChangeSet[column.name] || cell.value : cell.value;
+        let cellValue = rowChangeSet ? rowChangeSet[column.name] || cell.value : cell.value;
         const rowData = tableData[cell.row.index];
 
         if (
@@ -80,6 +80,13 @@ export default function generateColumnsData({
           customResolvables[id] = { ...variablesExposedForPreview[id], rowData };
           exposeToCodeHinter((prevState) => ({ ...prevState, ...customResolvables }));
         }
+        cellValue =
+          cellValue === undefined ||
+          cellValue === null ||
+          (typeof cellValue === 'string' && (cellValue.trim() === 'null' || cellValue.trim() === 'undefined'))
+            ? ''
+            : cellValue;
+
         switch (columnType) {
           case 'string':
           case undefined:
@@ -158,7 +165,6 @@ export default function generateColumnsData({
             const cellStyles = {
               color: textColor ?? '',
             };
-
             if (column.isEditable) {
               const validationData = validateWidget({
                 validationObject: {
