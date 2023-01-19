f7acf712f0df5a989f91ee4c1c8800acc3cdd4de
gsmithun4 <gsmithun4@gmail.com>
2023-01-19 20:54:22 +0530

    improvement

--- server/migrations/1674131104527-addUserType.ts
+++ server/migrations/1674131104527-addUserType.ts
@@ -4,15 +4,8 @@ export class addUserType1674131104527 implements MigrationInterface {
   // This migration is for customers upgrading CE to EE
   // User type is not added in CE, checking if user type exist in users table. If not adding same
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const userColumns = await queryRunner.query(
-      "SELECT column_name FROM information_schema.columns where table_name = 'users'"
-    );
-    console.log(
-      'available columns in users table ',
-      userColumns?.map((uc) => uc.column_name)
-    );
-    if (!userColumns?.some((uc) => uc.column_name === 'user_type')) {
-      console.log('adding user_type to users');
+    const type = await queryRunner.query("SELECT 1 FROM pg_type WHERE typname = 'user_type'");
+    if (!type?.length) {
       await queryRunner.addColumn(
         'users',
         new TableColumn({
