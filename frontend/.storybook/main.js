62d7a545056d3660bfe980fc928d1cf991d8a039
stepinfwd <stepinfwd@gmail.com>
2023-01-19 13:12:50 +0530

    feat :: added sass addon

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
   ],
   "framework": "@storybook/react",
   "core": {
