0c59c05641ec59a35c8dda3fdbe96206230b6959
Muhsin Shah <muhsinshah21@gmail.com>
2023-01-24 13:06:06 +0530

    refactoring the code

--- server/migrations/1673424772525-ChangeReadOnDashboardColumnName.ts
+++ server/migrations/1673424772525-ChangeReadOnDashboardColumnName.ts
@@ -2,11 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ChangeReadOnDashboardColumnName1673424772525 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    try {
-      await queryRunner.renameColumn('app_group_permissions', 'read_on_dashboard', 'hide_from_dashboard');
-    } catch {
-      console.log('No column found, Already changed the column name');
-    }
+    await queryRunner.renameColumn('app_group_permissions', 'read_on_dashboard', 'hide_from_dashboard');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
