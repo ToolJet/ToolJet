3ead224651abdf0f2723d3cadc5e0283f5a6fb4e
Muhsin Shah C P <muhsinshah21@gmail.com>
2023-01-25 18:55:00 +0530

    [bug-fix] User can't update the single workspace name (#5426)
    * fix: user can update the workspace name
    
    * fixed a bug
    
    * fixed multi-org list issues

--- frontend/src/_ui/Header/index.jsx
+++ frontend/src/_ui/Header/index.jsx
@@ -2,23 +2,16 @@ import React from 'react';
 import cx from 'classnames';
 import Breadcrumbs from '../Breadcrumbs';
 import { OrganizationList } from '@/_components/OrganizationManager/List';
-import { authenticationService } from '@/_services';
 
 function Header() {
   const currentVersion = localStorage.getItem('currentVersion');
   const darkMode = localStorage.getItem('darkMode') === 'true';
-  const isSingleOrganization = window.public_config?.DISABLE_MULTI_WORKSPACE === 'true';
-  const { organization } = authenticationService.currentUserValue;
 
   return (
     <header className="layout-header">
       <div className="row w-100 gx-0">
         <div className="organization-selector col border-end border-bottom">
-          {isSingleOrganization ? (
-            <span className="d-flex align-items-center h-100">{organization}</span>
-          ) : (
-            <OrganizationList />
-          )}
+          <OrganizationList />
         </div>
         <div className="col border-bottom m-auto" style={{ padding: 13.5 }}>
           <div className="d-flex justify-content-sm-between">
