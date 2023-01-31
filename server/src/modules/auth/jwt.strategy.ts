c881cb650cb70bdb5b48d4b5d9457274f737fd5e
gsmithun4 <gsmithun4@gmail.com>
2023-01-31 14:15:25 +0530

    initial commit

--- server/src/modules/auth/jwt.strategy.ts
+++ server/src/modules/auth/jwt.strategy.ts
@@ -5,6 +5,7 @@ import { UsersService } from '../../../src/services/users.service';
 import { ConfigService } from '@nestjs/config';
 import { User } from 'src/entities/user.entity';
 import { USER_STATUS, WORKSPACE_USER_STATUS } from 'src/helpers/user_lifecycle';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,22 +14,49 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('SECRET_KEY_BASE'),
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
-    if (!payload.organizationId) return false;
-    const user: User = await this.usersService.findByEmail(
-      payload.sub,
-      payload.organizationId,
-      WORKSPACE_USER_STATUS.ACTIVE
-    );
+  async validate(req: Request, payload: JWTPayload) {
+    const organizationId =
+      typeof req.headers['tj-workspace-id'] === 'object'
+        ? req.headers['tj-workspace-id'][0]
+        : req.headers['tj-workspace-id'];
+
+    // header des not exist
+    if (!organizationId) return false;
+
+    // Backward compatibility - existing sessions payload.organizationId will be present
+    if (payload.organizationId && organizationId !== payload.organizationId) {
+      return false;
+    }
+
+    // Backward compatibility - organizationIds or organizationId should be present
+    if (!(payload.organizationIds?.length || payload.organizationId)) {
+      return false;
+    }
+
+    // requested workspace not authenticated
+    if (payload.organizationIds?.length && !payload.organizationIds.some((oid) => oid === organizationId)) {
+      return false;
+    }
+
+    const user: User = await this.usersService.findByEmail(payload.sub, organizationId, WORKSPACE_USER_STATUS.ACTIVE);
     if (!user || user.status !== USER_STATUS.ACTIVE) return false;
 
-    user.organizationId = payload.organizationId;
+    user.organizationId = organizationId;
     user.isPasswordLogin = payload.isPasswordLogin;
     user.isSSOLogin = payload.isSSOLogin;
 
     return user;
   }
 }
+
+type JWTPayload = {
+  sub: string;
+  organizationId?: string;
+  organizationIds?: Array<string>;
+  isPasswordLogin: boolean;
+  isSSOLogin: boolean;
+};
