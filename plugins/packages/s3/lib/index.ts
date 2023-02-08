54128814d407f30e88f21a0a9e98d276f7ebff57
arpitnath <arpitnath42@gmail.com>
2023-02-08 18:42:18 +0530

    s3

--- plugins/packages/s3/lib/index.ts
+++ plugins/packages/s3/lib/index.ts
@@ -7,7 +7,8 @@ import {
   signedUrlForPut,
   removeObject,
 } from './operations';
-import { S3Client } from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
+// import { S3Client } from '@aws-sdk/client-s3';
 import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions, Operation } from './types';
 
@@ -52,23 +53,66 @@ export default class S3QueryService implements QueryService {
   }
 
   async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
-    const client: S3Client = await this.getConnection(sourceOptions);
-    await listBuckets(client, {});
+    const isSuccess = await this.getConnection(sourceOptions);
+
+    if (isSuccess) {
+      const s3CLient = new AWS.S3();
+
+      const buckets = await s3CLient.listBuckets().promise();
+
+      console.log('buckets 2.o ===>', buckets);
+      return {
+        status: 'ok',
+        message: 'Connection successful',
+      };
+    }
 
     return {
-      status: 'ok',
+      status: 'failed',
+      message: 'Connection failed',
     };
   }
 
   async getConnection(sourceOptions: SourceOptions): Promise<any> {
-    const credentials = {
-      accessKeyId: sourceOptions.access_key,
-      secretAccessKey: sourceOptions.secret_key,
-    };
-    const endpointOptions = sourceOptions.endpoint_enabled && {
-      endpoint: sourceOptions?.endpoint,
-      forcePathStyle: true,
-    };
-    return new S3Client({ region: sourceOptions.region, credentials, ...endpointOptions });
+    const useAWSInstanceProfile = sourceOptions['instance_metadata_credentials'] === 'aws_instance_credentials';
+    const region = sourceOptions['region'];
+
+    let credentials = null;
+    if (useAWSInstanceProfile) {
+      const metadataCredentials = new AWS.EC2MetadataCredentials({
+        httpOptions: { timeout: 5000 },
+        maxRetries: 10,
+      });
+
+      return metadataCredentials.refresh(async (error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          credentials = new AWS.Credentials(
+            metadataCredentials['metadata'].AccessKeyId,
+            metadataCredentials['metadata'].SecretAccessKey,
+            metadataCredentials['metadata'].Token
+          );
+          console.log('--meta creds 1.0-----', credentials);
+          AWS.config.update({ region, credentials });
+
+          return true;
+        }
+      });
+    } else {
+      const credentials = new AWS.Credentials(sourceOptions['access_key'], sourceOptions['secret_key']);
+      const endpointOptions = sourceOptions.endpoint_enabled && {
+        endpoint: sourceOptions?.endpoint,
+        forcePathStyle: true,
+      };
+
+      AWS.config.update({
+        credentials: credentials,
+        region: region,
+        ...endpointOptions,
+      });
+
+      return true;
+    }
   }
 }
