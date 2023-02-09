37649fdd97520c9da8c3ed018a50dea1206ce5ef
Midhun Kumar E <midhun752@gmail.com>
2023-02-09 10:48:20 +0530

    Add automation for DSs (#5512)

--- cypress-tests/cypress/e2e/editor/data-source/clickHouseHappyPath.cy.js
+++ cypress-tests/cypress/e2e/editor/data-source/clickHouseHappyPath.cy.js
@@ -1,20 +1,25 @@
 import { postgreSqlSelector } from "Selectors/postgreSql";
 import { postgreSqlText } from "Texts/postgreSql";
-import { redisText } from "Texts/redis";
-import { commonSelectors } from "Selectors/common";
+import { commonWidgetText } from "Texts/common";
+import { commonSelectors, commonWidgetSelector } from "Selectors/common";
 import {
+  addQuery,
   fillDataSourceTextField,
+  fillConnectionForm,
   selectDataSource,
+  openQueryEditor,
+  selectQueryMode,
+  addGuiQuery,
+  addWidgetsToAddUser,
 } from "Support/utils/postgreSql";
-import { verifyCouldnotConnectWithAlert } from "Support/utils/dataSource";
 
-describe("Data source Redis", () => {
+describe("Data sources", () => {
   beforeEach(() => {
     cy.appUILogin();
     cy.createApp();
   });
 
-  it("Should verify elements on connecti Redison form", () => {
+  it("Should verify elements on connection form", () => {
     cy.get(postgreSqlSelector.leftSidebarDatasourceButton).click();
     cy.get(postgreSqlSelector.labelDataSources).should(
       "have.text",
@@ -42,28 +47,17 @@ describe("Data source Redis", () => {
       postgreSqlText.allCloudStorage
     );
 
-    cy.get(postgreSqlSelector.dataSourceSearchInputField).type(redisText.redis);
+    cy.get(postgreSqlSelector.dataSourceSearchInputField).type(
+      'ClickHouse'
+    );
     cy.get("[data-cy*='data-source-']")
       .eq(0)
-      .should("contain", redisText.redis);
-    cy.get('[data-cy="data-source-redis"]').click();
+      .should("contain", 'ClickHouse');
+    cy.get('[data-cy="data-source-clickhouse"]').click();
 
-    cy.get(postgreSqlSelector.dataSourceNameInputField).should(
+    cy.get(postgreSqlSelector.dataSourceNameInputField).should(               //username,password,host,port,protocol,dbname,usepost, trimquery,gzip,debug,raw 
       "have.value",
-      redisText.redis
-    );
-    cy.get(postgreSqlSelector.labelHost).verifyVisibleElement(
-      "have.text",
-      postgreSqlText.labelHost
-    );
-    fillDataSourceTextField(
-      postgreSqlText.labelHost,
-      postgreSqlText.placeholderEnterHost,
-      "redis_host"
-    );
-    cy.get(postgreSqlSelector.labelPort).verifyVisibleElement(
-      "have.text",
-      postgreSqlText.labelPort
+      'ClickHouse'
     );
     cy.get(postgreSqlSelector.labelUserName).verifyVisibleElement(
       "have.text",
@@ -73,11 +67,48 @@ describe("Data source Redis", () => {
       "have.text",
       'Password'
     );
-    cy.get(postgreSqlSelector.labelIpWhitelist).verifyVisibleElement(
+    cy.get(postgreSqlSelector.labelHost).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelHost
+    );
+    cy.get(postgreSqlSelector.labelPort).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelPort
+    );
+
+    cy.get(postgreSqlSelector.labelDbName).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.labelDbName
+    );
+    cy.get('[data-cy="label-protocol"]').verifyVisibleElement(
+      "have.text",
+      'Protocol'
+    );
+    cy.get('[data-cy="label-use-post"]').verifyVisibleElement(
+      "have.text",
+      'Use Post'
+    );
+    cy.get('[data-cy="label-trim-query"]').verifyVisibleElement(
+      "have.text",
+      'Trim Query'
+    );
+    cy.get('[data-cy="label-use-gzip"]').verifyVisibleElement(
+      "have.text",
+      'Use Gzip'
+    );
+    cy.get('[data-cy="label-debug"]').verifyVisibleElement(
+      "have.text",
+      'Debug'
+    );
+    cy.get('[data-cy="label-raw"]').verifyVisibleElement(
+      "have.text",
+      'Raw'
+    );
+    cy.get(postgreSqlSelector.labelIpWhitelist).scrollIntoView().verifyVisibleElement(
       "have.text",
       postgreSqlText.whiteListIpText
     );
-    cy.get(postgreSqlSelector.buttonCopyIp).verifyVisibleElement(
+    cy.get(postgreSqlSelector.buttonCopyIp).scrollIntoView().verifyVisibleElement(
       "have.text",
       postgreSqlText.textCopy
     );
@@ -92,82 +123,53 @@ describe("Data source Redis", () => {
         postgreSqlText.buttonTextTestConnection
       )
       .click();
+    cy.get(postgreSqlSelector.connectionFailedText).verifyVisibleElement(
+      "have.text",
+      postgreSqlText.couldNotConnect
+    );
     cy.get(postgreSqlSelector.buttonSave).verifyVisibleElement(
       "have.text",
       postgreSqlText.buttonTextSave
     );
-    verifyCouldnotConnectWithAlert(redisText.errorMaxRetries);
+    cy.get(postgreSqlSelector.dangerAlertNotSupportSSL).scrollIntoView().verifyVisibleElement(
+      "have.text",
+      'getaddrinfo ENOTFOUND undefined'
+    );
   });
-  it("Should verify the functionality of Redis connection form.", () => {
-    selectDataSource(redisText.redis);
+
+  it("Should verify the functionality of PostgreSQL connection form.", () => {
+    selectDataSource('ClickHouse');
 
     cy.clearAndType(
       '[data-cy="data-source-name-input-filed"]',
-      redisText.cypressRedis
+      postgreSqlText.psqlName
     );
 
     fillDataSourceTextField(
       postgreSqlText.labelHost,
       postgreSqlText.placeholderEnterHost,
-      "redis_host"
+      Cypress.env("pg_host")
     );
     fillDataSourceTextField(
       postgreSqlText.labelPort,
       postgreSqlText.placeholderEnterPort,
-      Cypress.env("redis_port")
+      "5432"
+    );
+    fillDataSourceTextField(
+      postgreSqlText.labelDbName,
+      postgreSqlText.placeholderNameOfDB,
+      "postgres"
     );
     fillDataSourceTextField(
       postgreSqlText.labelUserName,
       postgreSqlText.placeholderEnterUserName,
-      "dev@tooljet.io"
+      "postgres"
     );
-    cy.get(postgreSqlSelector.passwordTextField).type(
-      Cypress.env("redis_password")
-    );
-
-    cy.get(postgreSqlSelector.buttonTestConnection).click();
-    verifyCouldnotConnectWithAlert(redisText.errorMaxRetries);
-
-    fillDataSourceTextField(
-      postgreSqlText.labelHost,
-      postgreSqlText.placeholderEnterHost,
-      Cypress.env("redis_host")
-    );
-    fillDataSourceTextField(
-      postgreSqlText.labelPort,
-      postgreSqlText.placeholderEnterPort,
-      "108299"
-    );
-    cy.get(postgreSqlSelector.buttonTestConnection).click();
-    verifyCouldnotConnectWithAlert(redisText.errorPort);
-
-    fillDataSourceTextField(
-      postgreSqlText.labelPort,
-      postgreSqlText.placeholderEnterPort,
-      Cypress.env("redis_port")
-    );
-    cy.get(postgreSqlSelector.passwordTextField).type(
-      `{selectAll}{backspace}"redis_password"`
-    );
-    cy.get(postgreSqlSelector.buttonTestConnection).click();
-    verifyCouldnotConnectWithAlert(redisText.errorInvalidUserOrPassword);
 
     cy.get(postgreSqlSelector.passwordTextField).type(
-      `{selectAll}{backspace}${Cypress.env("redis_password")}`
+      Cypress.env("pg_password")
     );
-    fillDataSourceTextField(
-      postgreSqlText.labelUserName,
-      postgreSqlText.placeholderEnterUserName,
-      "redis"
-    );
-    cy.get(postgreSqlSelector.buttonTestConnection).click();
-    verifyCouldnotConnectWithAlert(redisText.errorInvalidUserOrPassword);
 
-    fillDataSourceTextField(
-      postgreSqlText.labelUserName,
-      postgreSqlText.placeholderEnterUserName,
-      "redis"
-    );
     cy.get(postgreSqlSelector.buttonTestConnection).click();
     cy.get(postgreSqlSelector.textConnectionVerified, {
       timeout: 10000,
@@ -181,7 +183,7 @@ describe("Data source Redis", () => {
 
     cy.get(postgreSqlSelector.leftSidebarDatasourceButton).click();
     cy.get(postgreSqlSelector.datasourceLabelOnList)
-      .should("have.text", redisText.cypressRedis)
+      .should("have.text", postgreSqlText.psqlName)
       .find("button")
       .should("be.visible");
   });
