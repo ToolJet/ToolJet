6a8fd6e93543ade150bcd453995817902c337c60
Joyjeet Sarkar <joy@amoga.io>
2023-02-12 23:12:06 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -34,12 +34,24 @@ module.exports = {
     ],
     splitChunks: {
       cacheGroups: {
-        vendors: {
+        defaultVendors: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
         },
       },
+      chunks: "async",
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
     },
   },
   target: 'web',
