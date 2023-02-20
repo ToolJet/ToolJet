7a43745098a974f55ce5b94c33cc4493e7247650
Ajith KV <ajith.jaban@gmail.com>
2023-02-20 09:59:13 +0530

    Add data-cy for forgot password page elements (#5587)

--- frontend/src/SuccessInfoScreen/ForgotPasswordInfoScreen.jsx
+++ frontend/src/SuccessInfoScreen/ForgotPasswordInfoScreen.jsx
@@ -15,15 +15,19 @@ export const ForgotPasswordInfoScreen = function ForgotPasswordInfoScreen({ prop
           loading="lazy"
           data-cy="email-image"
         />
-        <h1 className="common-auth-section-header">Check your mail</h1>
-        <p className="info-screen-description">
+        <h1 className="common-auth-section-header" data-cy="onboarding-page-header">
+          Check your mail
+        </h1>
+        <p className="info-screen-description" data-cy="onboarding-page-description">
           We’ve sent an email to {email} with a password reset link. Please click on that link to reset your password.
         </p>
-        <p className="info-screen-spam-msg">Did not receive an email? Check your spam folder.</p>
+        <p className="info-screen-spam-msg" data-cy="email-page-spam-msg">
+          Did not receive an email? Check your spam folder.
+        </p>
         <div className="separator-onboarding">
-          <div className="separator" data-cy="onboarding-separator">
-            <h2>
-              <span>OR</span>
+          <div className="separator">
+            <h2 data-cy="onboarding-separator">
+              <span data-cy="onboarding-separator-text">OR</span>
             </h2>
           </div>
         </div>
