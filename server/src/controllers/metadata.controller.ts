7c86dfc68e1d659e6dd7a45422333f06b91fc445
Muhsin Shah <muhsinshah21@gmail.com>
2023-02-22 14:58:47 +0530

    redo: auth guard

--- server/src/controllers/metadata.controller.ts
+++ server/src/controllers/metadata.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { MetadataService } from '@services/metadata.service';
+import { JwtAuthGuard } from '../modules/auth/jwt-auth.guard';
 
 @Controller('metadata')
 export class MetadataController {
   constructor(private metadataService: MetadataService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('skip_version')
   async skipVersion() {
     const metadata = await this.metadataService.getMetaData();
