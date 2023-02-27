6d667d36aeb762c3e4582f4384e23c4a4298d14e
manishkushare <kushare.manish9@gmail.com>
2023-02-27 18:00:12 +0530

    removed console in actions file

--- frontend/src/Editor/Components/Table/columns/actions.jsx
+++ frontend/src/Editor/Components/Table/columns/actions.jsx
@@ -14,40 +14,33 @@ const generateActionsData = ({ actions, columnSizes, defaultColumn, fireEvent, s
             accessor: 'edit',
             width: columnSizes.leftActions || defaultColumn.width,
             Cell: (cell) => {
-              return leftActions().map((action) => {
-                console.log(
-                  'action.disableActionButton',
-                  resolveReferences(action?.disableActionButton ?? false, currentState),
-                  action.disableActionButton
-                );
-                return (
-                  <button
-                    key={action.name}
-                    className="btn btn-sm m-1 btn-light"
-                    style={{
-                      background: action.backgroundColor,
-                      color: action.textColor,
-                      borderRadius: action.actionButtonRadius,
-                    }}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setExposedVariables({
-                        selectedRowId: cell.row.id,
-                        selectedRow: cell.row.original,
-                      }).then(() => {
-                        fireEvent('onTableActionButtonClicked', {
-                          data: cell.row.original,
-                          rowId: cell.row.id,
-                          action,
-                        });
+              return leftActions().map((action) => (
+                <button
+                  key={action.name}
+                  className="btn btn-sm m-1 btn-light"
+                  style={{
+                    background: action.backgroundColor,
+                    color: action.textColor,
+                    borderRadius: action.actionButtonRadius,
+                  }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setExposedVariables({
+                      selectedRowId: cell.row.id,
+                      selectedRow: cell.row.original,
+                    }).then(() => {
+                      fireEvent('onTableActionButtonClicked', {
+                        data: cell.row.original,
+                        rowId: cell.row.id,
+                        action,
                       });
-                    }}
-                    disabled={resolveReferences(action?.disableActionButton ?? false, currentState)}
-                  >
-                    {action.buttonText}
-                  </button>
-                );
-              });
+                    });
+                  }}
+                  disabled={resolveReferences(action?.disableActionButton ?? false, currentState)}
+                >
+                  {action.buttonText}
+                </button>
+              ));
             },
           },
         ]
