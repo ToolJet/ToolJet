b0ec345440cfd066f6b5ed8c226c744303872e40
emidhun <midhun752@gmail.com>
2023-02-28 16:41:57 +0530

    Add google sheet labels and texts

---
+++ cypress-tests/cypress/constants/texts/googleSheets.js
@@ -0,0 +1,34 @@
+export const googleSheetsText = {
+  dataSourceGoogleSheets: "Google Sheets",
+  connectionFormHeader: "Authorize",
+  connectionFormDescription:
+    "If you want your ToolJet apps to modify your Google sheets, make sure to select read and write access",
+
+  labelReadonly:
+    "Read only Your ToolJet apps can only read data from Google sheets",
+  subLabelReadonly: "Your ToolJet apps can only read data from Google sheets",
+
+  labelReadAndWrite:
+    "Read and write Your ToolJet apps can read data from sheets, modify sheets, and more.",
+  subLabelReadAndWrite:
+    "Your ToolJet apps can read data from sheets, modify sheets, and more.",
+
+  readDataFromSpreadsheet: "Read data from a spreadsheet",
+  appendDataToSheet: "Append data to a spreadsheet",
+  getSheetInfo: "Get spreadsheet info",
+  updateDataToSheet: "Update data to a spreadsheet",
+  deleteRowFromSheet: "Delete row from a spreadsheet",
+
+  spreadSheetId: "Spreadsheet ID",
+  labelRange: "Range",
+  labelSheet: "Sheet",
+  labelRows: "Rows",
+  labelSheetName: "Sheet name",
+  labelWhere: "Where",
+  labelOperator: "Operator",
+  labelValue: "Value",
+  labelBody: "Body",
+  labelGid: "GID",
+  labelDeleteRowNumber: "Delete row number",
+};
+    
