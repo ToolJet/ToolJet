39580c0acb4e7f0a27d65d819c9721530e6bf523
emidhun <midhun752@gmail.com>
2023-02-28 16:41:32 +0530

    Add google sheet selectors

---
+++ cypress-tests/cypress/constants/selectors/googleSheets.js
@@ -0,0 +1,45 @@
+export const googleSheetsSelector = {
+  dataSourceGoogleSheets: "[data-cy='data-source-google sheets']",
+  connectionFormHeader: '[data-cy="google-sheet-connection-form-header"]',
+  connectionFormDescription:
+    '[data-cy="google-sheet-connection-form-description"]',
+
+  checkboxReadonly: '[data-cy="read-only-input"]',
+  labelReadonly: '[data-cy="read-only-label"]',
+  subLabelReadonly: '[data-cy="read-only-sub-label"]',
+
+  checkboxReadAndWrite: '[data-cy="read-and-write-input"]',
+  labelReadAndWrite: '[data-cy="read-and-write-label"]',
+  subLabelReadAndWrite: '[data-cy="read-and-write-sub-label"]',
+
+  spreadSheetId: '[data-cy="label-spreadsheet-id"]',
+  spreadSheetIdInputField: '[data-cy="spreadsheet_id-input-field"]',
+
+  labelRange: '[data-cy="label-range"]',
+  rangeInputField: '[data-cy="spreadsheet_range-input-field"]',
+
+  labelSheet: '[data-cy="label-sheet"]',
+  sheetInputField: '[data-cy="sheet-input-field"]',
+
+  labelRows: '[data-cy="label-rows"]',
+  rowsInputField: '[data-cy="rows-input-field"]',
+
+  labelSheetName: '[data-cy="label-sheet-name"]',
+
+  labelWhere: '[data-cy="label-where"]',
+  whereInputField: '[data-cy="where_field-input-field"]',
+
+  labelOperator: '[data-cy="label-operator"]',
+
+  labelValue: '[data-cy="label-value"]',
+  valueInputField: '[data-cy="where_value-input-field"]',
+
+  labelBody: '[data-cy="label-body"]',
+  bodyInputField: '[data-cy="body-input-field"]',
+
+  labelGid: '[data-cy="label-gid"]',
+
+  labelDeleteRowNumber: '[data-cy="label-delete-row-number"]',
+  deleteRowIndexInputField: '[data-cy="row_index-input-field"]',
+};
+      
