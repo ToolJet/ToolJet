945de0584ff07ef5921ac2e63f84aab71926c743
stepinfwd <stepinfwd@gmail.com>
2023-03-01 17:06:17 +0530

    feat :: ui for version export modal

--- frontend/src/Editor/Header/GlobalSettings.jsx
+++ frontend/src/Editor/Header/GlobalSettings.jsx
@@ -10,6 +10,7 @@ import { CodeHinter } from '../CodeBuilder/CodeHinter';
 import { resolveReferences } from '@/_helpers/utils';
 import { useTranslation } from 'react-i18next';
 import _ from 'lodash';
+import ExportAppModal from '../../HomePage/ExportAppModal';
 
 export const GlobalSettings = ({
   globalSettings,
@@ -27,7 +28,7 @@ export const GlobalSettings = ({
   const [realState, setRealState] = React.useState(currentState);
   const [showConfirmation, setConfirmationShow] = React.useState(false);
   const [show, setShow] = React.useState('');
-
+  const [isExportingApp, setIsExportingApp] = React.useState('');
   const coverStyles = {
     position: 'fixed',
     top: '0px',
@@ -96,7 +97,7 @@ export const GlobalSettings = ({
                     value={canvasMaxWidth}
                   />
                   <select
-                    className="form-select maximum-canvas-width-input-select"
+                    className="maximum-canvas-width-input-select"
                     aria-label="Select canvas width type"
                     onChange={(event) => {
                       const newCanvasMaxWidthType = event.currentTarget.value;
@@ -206,7 +207,17 @@ export const GlobalSettings = ({
             </div>
             <div className="d-flex align-items-center  global-popover-div-wrap">
               <p className="global-popover-text">Export app</p>
-              <button className="export-app-btn ">Export this app</button>
+              <button className="export-app-btn" onClick={() => setIsExportingApp(true)}>
+                <svg width="16" height="16" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M2.5 5.78906V17.2891V18.0391C2.5 19.834 3.98027 21.2891 5.77519 21.2891C7.5425 21.2891 9 19.8564 9 18.0891C9 17.6472 9.35817 17.2891 9.8 17.2891H18.5V5.78906C18.5 4.13221 17.1569 2.78906 15.5 2.78906H5.5C3.84315 2.78906 2.5 4.13221 2.5 5.78906ZM9.75 9.61723C9.70334 9.65231 9.65858 9.69108 9.61612 9.73355L8.03033 11.3193C7.73744 11.6122 7.26256 11.6122 6.96967 11.3193C6.67678 11.0264 6.67678 10.5516 6.96967 10.2587L8.55546 8.67289C9.6294 7.59895 11.3706 7.59895 12.4445 8.67289L14.0303 10.2587C14.3232 10.5516 14.3232 11.0264 14.0303 11.3193C13.7374 11.6122 13.2626 11.6122 12.9697 11.3193L11.3839 9.73355C11.3414 9.69108 11.2967 9.65231 11.25 9.61723V13.789C11.25 14.2032 10.9142 14.539 10.5 14.539C10.0858 14.539 9.75 14.2032 9.75 13.789V9.61723ZM22.3766 19.7789C21.9361 21.5093 20.3675 22.7891 18.5 22.7891H6.5C8.36748 22.7891 9.93606 21.5093 10.3766 19.7789C10.5128 19.2437 10.9477 18.7891 11.5 18.7891H21.5C22.0523 18.7891 22.5128 19.2437 22.3766 19.7789Z"
+                    fill="#3E63DD"
+                  />
+                </svg>
+                <span style={{ paddingLeft: '6px' }}>Export this app</span>
+              </button>
             </div>
           </div>
         </div>
@@ -215,7 +226,7 @@ export const GlobalSettings = ({
   );
 
   return (
-    <>
+    <div className="global-settings-wrapper-container">
       <Confirm
         show={showConfirmation}
         message={
@@ -227,6 +238,18 @@ export const GlobalSettings = ({
         onCancel={() => setConfirmationShow(false)}
         darkMode={darkMode}
       />
+      {/* {isExportingApp && app.hasOwnProperty('id') && ( */}
+      <ExportAppModal
+        show={isExportingApp}
+        closeModal={() => {
+          setIsExportingApp(false);
+        }}
+        customClassName="modal-version-lists"
+        title={'Select a version to export'}
+        // app={app}
+        // darkMode={this.props.darkMode}
+      />
+      {/* )} */}
       <OverlayTrigger
         onToggle={(show) => {
           if (show) setShow('settings');
@@ -245,6 +268,6 @@ export const GlobalSettings = ({
           tip="Settings"
         />
       </OverlayTrigger>
-    </>
+    </div>
   );
 };
