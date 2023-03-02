0271dbf17de4d2de98fc3edfc98a1f8b565dfc2c
Mekhla Asopa <59684099+Mekhla-Asopa@users.noreply.github.com>
2023-03-02 14:33:00 +0530

    Add data-cy for database functionality  (#5455)
    * Added data-cy for table header and subheader
    
    * added data-cy for table operations
    
    * added data-cy for table and text
    
    * Added data-cy for delete icon and fields
    
    * Updated and removed duplicate data-cy
    
    * deleted unused files
    
    * Updated data-cy for filter section

--- frontend/src/TooljetDatabase/TooljetDatabasePage/index.jsx
+++ frontend/src/TooljetDatabase/TooljetDatabasePage/index.jsx
@@ -40,9 +40,13 @@ const TooljetDatabasePage = ({ totalTables }) => {
           <EmptyFoldersIllustration />
         </div>
         <div className="text-center">
-          <div className="text-h3">You don&apos;t have any tables yet.</div>
+          <div className="text-h3" data-cy="do-not-have-table-text">
+            You don&apos;t have any tables yet.
+          </div>
         </div>
-        <div className="text-h5 text-secondary">Create a table to get started!</div>
+        <div className="text-h5 text-secondary" data-cy="create-table-to-get-started-text">
+          Create a table to get started!
+        </div>
       </div>
     );
   };
@@ -61,7 +65,12 @@ const TooljetDatabasePage = ({ totalTables }) => {
           <>
             <div className="database-table-header-wrapper">
               <div className="card border-0 px-3 py-2">
-                <span className="text-h3 font-weight-500">{selectedTable}</span>
+                <span
+                  className="text-h3 font-weight-500"
+                  data-cy={`${String(selectedTable).toLowerCase().replace(/\s+/g, '-')}-table-name-header`}
+                >
+                  {selectedTable}
+                </span>
               </div>
               <div className="card border-0">
                 <div className="card-body p-0 py-2">
