b130e19fe49f6354d574621e032de569169e55c4
stepinfwd <stepinfwd@gmail.com>
2023-03-16 14:32:09 +0530

    svg to jsx : bulk icons

--- frontend/src/_components/SearchBox.jsx
+++ frontend/src/_components/SearchBox.jsx
@@ -61,22 +61,7 @@ export function SearchBox({
         {isFocused && searchText && (
           <span className="input-icon-addon end">
             <div className="d-flex" onMouseDown={clearSearchText} title="clear">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
+              <SolidIcon name="remove" />
             </div>
           </span>
         )}
