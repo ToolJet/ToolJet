97f12f317d8ed3928dc919acc163d98f01fb0449
arpitnath <arpitnath42@gmail.com>
2023-03-20 15:43:55 +0530

    typo

--- marketplace/plugins/github/lib/index.ts
+++ marketplace/plugins/github/lib/index.ts
@@ -8,7 +8,7 @@ export default class Github implements QueryService {
 
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     const operation: Operation = queryOptions.operation;
-    console.log('---Pikuuuu ++', {
+    console.log('---TJ ++', {
       queryOptions
 
     });
