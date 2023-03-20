f0597c98b2fbaf731ac23e31710dfa51aa0a181c
Mekhla Asopa <59684099+Mekhla-Asopa@users.noreply.github.com>
2023-03-20 18:48:10 +0530

    Added automation for database (#5589)
    * Added data-cy for table header and subheader
    
    * added data-cy for table operations
    
    * added data-cy for table and text
    
    * Added selectors and texts for database
    
    * Added method for table edit and delete operation
    
    * Added data-cy for delete icon and fields
    
    * Added method for table operation
    
    * Add workaround to handle re-rendering of filter
    
    * Updated and removed duplicate data-cy
    
    * fix toast failure
    
    * deleted unused files
    
    * Updated data-cy for filter section
    
    * updated spec
    
    * Fix failure
    
    * resolved conflicts
    
    ---------
    
    Co-authored-by: emidhun <midhun752@gmail.com>

---
+++ cypress-tests/cypress/constants/texts/database.js
@@ -0,0 +1,75 @@
+export const databaseText = {
+  allTableSubheader: "All tables",
+  doNotHaveTableText: "You don't have any tables yet.",
+  tablePageHeader: "Tables",
+
+  createNewTableHeader: "Create a new table",
+  tableNameLabel: "Table name",
+  addColumnHeader: "Add columns",
+  nameLabel: "Name",
+  typeLabel: "Type",
+  defaultLabel: "Default",
+  primaryKeyLabel: "Primary Key",
+  addMoreColumnsButton: "   Add more columns",
+
+  editTableHeader: "Edit table",
+  idColumnHeader: "id",
+  noRecordsText: "You don't have any records yet.",
+  deleteRecordButton: "Delete records",
+
+  tableCreatedSuccessfullyToast: (tableName) => {
+    return `${tableName} created successfully`;
+  },
+  tableDeletedSuccessfullyToast: (tableName) => {
+    return `Table "${tableName}" deleted successfully`
+  },
+  tableEditedSuccessfullyToast: (tableName) => {
+    return `${tableName} edited successfully`;
+  },
+  tableExistsToast: (tableName) => {
+    return `Table name already exists: ${tableName}`;
+  },
+  deleteRowToast: (tableName, rowNumber) => {
+    return `Deleted ${rowNumber} rows from table "${tableName}"`
+  }
+};
+
+export const createNewColumnText = {
+  createNewColumnHeader: "Create a new column",
+  columnNameLabel: 'Column name',
+  dataTypeLabel: 'Data type',
+  defaultValueLabel: 'Default value',
+  columnCreatedSuccessfullyToast: 'Column created successfully',
+
+}
+
+export const createNewRowText = {
+  createNewRowHeader: 'Create a new row',
+  serialDataTypeLabel: 'SERIAL',
+  rowCreatedSuccessfullyToast: 'Row created successfully',
+}
+
+export const filterText = {
+  filterText: '  Filter',
+  operation: {
+    equals: "equals",
+    greaterThan: "greater than",
+    greaterThanEqual: "greater than or equal",
+    lessThan: "less than",
+    lessThanEqual: "less than or equal",
+    notEqual: "not equal",
+    like: "like",
+    ilike: "ilike",
+    match: "match",
+    imatch: "imatch",
+    in: "in",
+  },
+}
+
+export const sortText = {
+  sortByText: 'Sort by',
+  order: {
+    ascending: "Ascending",
+    descending: "Descending"
+  }
+}
