deaf71c90799a80b18309cfe33acec6a9ea68b37
emidhun <midhun752@gmail.com>
2023-03-21 16:03:37 +0530

    Add mongoDB utils

--- cypress-tests/cypress/support/utils/mongoDB.js
+++ cypress-tests/cypress/support/utils/mongoDB.js
@@ -6,48 +6,46 @@ import { mongoDbText } from "Texts/mongoDb";
 import { postgreSqlSelector } from "Selectors/postgreSql";
 import { postgreSqlText } from "Texts/postgreSql";
 
+export const connectMongo = () => {
+  selectDataSource(mongoDbText.mongoDb);
 
+  cy.clearAndType(
+    '[data-cy="data-source-name-input-filed"]',
+    mongoDbText.cypressMongoDb
+  );
 
-export const connectMongo=()=>{
-    selectDataSource(mongoDbText.mongoDb);
+  cy.get('[data-cy="query-select-dropdown"]').type(
+    mongoDbText.optionConnectUsingConnectionString
+  );
 
-    cy.clearAndType(
-      '[data-cy="data-source-name-input-filed"]',
-      mongoDbText.cypressMongoDb
-    );
+  fillDataSourceTextField(
+    mongoDbText.labelConnectionString,
+    mongoDbText.connectionStringPlaceholder,
+    Cypress.env("mongodb_connString"),
+    "contain",
+    { parseSpecialCharSequences: false, delay: 0 }
+  );
+  cy.get(postgreSqlSelector.buttonTestConnection).click();
+  cy.get(postgreSqlSelector.textConnectionVerified, {
+    timeout: 10000,
+  }).should("have.text", postgreSqlText.labelConnectionVerified);
+  cy.get(postgreSqlSelector.buttonSave).click();
 
-    cy.get('[data-cy="query-select-dropdown"]').type(
-      mongoDbText.optionConnectUsingConnectionString
-    );
+  cy.get(postgreSqlSelector.leftSidebarDatasourceButton).click();
+  cy.get(postgreSqlSelector.datasourceLabelOnList)
+    .should("have.text", mongoDbText.cypressMongoDb)
+    .find("button")
+    .invoke("show")
+    .should("be.visible");
+};
 
-    fillDataSourceTextField(
-      mongoDbText.labelConnectionString,
-      mongoDbText.connectionStringPlaceholder,
-      Cypress.env("mongodb_connString"),
-      "contain",
-      { parseSpecialCharSequences: false, delay: 0 }
-    );
-    cy.get(postgreSqlSelector.buttonTestConnection).click();
-    cy.get(postgreSqlSelector.textConnectionVerified, {
-      timeout: 10000,
-    }).should("have.text", postgreSqlText.labelConnectionVerified);
-    cy.get(postgreSqlSelector.buttonSave).click();
-
-    cy.get(postgreSqlSelector.leftSidebarDatasourceButton).click();
-    cy.get(postgreSqlSelector.datasourceLabelOnList)
-      .should("have.text", mongoDbText.cypressMongoDb)
-      .find("button")
-      .invoke("show")
-      .should("be.visible");
-}
-
-export const openMongoQueryEditor=(dbName=mongoDbText.cypressMongoDb)=>{
-    cy.get(postgreSqlSelector.buttonAddNewQueries).click();
+export const openMongoQueryEditor = (dbName = mongoDbText.cypressMongoDb) => {
+  cy.get(postgreSqlSelector.buttonAddNewQueries).click();
   cy.get(`[data-cy="${dbName}-add-query-card"]`)
     .should("contain", dbName)
     .click();
-}
+};
 
-export const selectQueryType=(type)=>{
-    cy.get('[data-cy="query-select-dropdown"]').click().type(`${type}{enter}`)
-}
+export const selectQueryType = (type) => {
+  cy.get('[data-cy="query-select-dropdown"]').click().type(`${type}{enter}`);
+};
