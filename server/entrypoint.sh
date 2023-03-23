770c826a71aba984e04a3d239d5c7e258ebed0e3
Adish M <adish.madhu@gmail.com>
2023-03-23 13:05:29 +0530

    Fix for entrypoint.sh file take default port value 5432 in database_url

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -17,8 +17,8 @@ if [ -z "$DATABASE_URL" ]
 then
      ./server/scripts/wait-for-it.sh $PG_HOST:${PG_PORT:-5432} --strict --timeout=300 -- $SETUP_CMD
 else 
-     PG_HOST=$(echo "$DATABASE_URL" | sed -e 's/postgres:\/\///' -e 's/\([^@]*@\)\?//' | cut -d ':' -f 1)
-     PG_PORT=$(echo "$DATABASE_URL" | sed -e 's/postgres:\/\///' -e 's/\([^@]*@\)\?//' | cut -d ':' -f 2 | cut -d '/' -f 1)
+     PG_HOST=$(echo "$DATABASE_URL" | awk -F[@/:] '{print $4}')
+     PG_PORT=$(echo "$DATABASE_URL" | awk -F'[@/]' '{split($5,a,":"); print a[2] ? a[2] : "5432"}')
      ./server/scripts/wait-for-it.sh "$PG_HOST:$PG_PORT" --strict --timeout=300 -- $SETUP_CMD
 fi
 
