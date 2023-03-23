b0a64fdd7bda841f966711d21e999615270cba66
arpitnath <arpitnath42@gmail.com>
2023-03-24 03:38:20 +0530

    popover for recommendation

--- frontend/src/Editor/QueryManager/Transformation.jsx
+++ frontend/src/Editor/QueryManager/Transformation.jsx
@@ -45,7 +45,7 @@ return [row for row in data if row['amount'] > 1000]
     changeOption('transformation', recommendation);
   };
 
-  function toggleEnableTransformation() {
+  function toggleEnableTransformation () {
     setEnableTransformation((prev) => !prev);
     changeOption('enableTransformation', !enableTransformation);
   }
@@ -89,7 +89,7 @@ return [row for row in data if row['amount'] > 1000]
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [queryId]);
 
-  function getNonActiveTransformations(activeLang) {
+  function getNonActiveTransformations (activeLang) {
     switch (activeLang) {
       case 'javascript':
         return {
@@ -148,6 +148,33 @@ return [row for row in data if row['amount'] > 1000]
     </Popover>
   );
 
+
+  const popoverForRecommendation = (
+    <Popover id="transformation-popover-container">
+      <div className='transformation-popover card text-center'>
+        <img src="/assets/images/icons/copilot.svg" alt="AI copilot" height={64} width={64} />
+        <div className='d-flex flex-column card-body'>
+          <h4 className='mb-2'>
+            ToolJet x OpenAI
+          </h4>
+          <p className='mb-2'>
+            <strong style={{ fontWeight: 700, color: '#3E63DD' }}>AI copilot</strong> helps you write your queries faster. It uses OpenAI's GPT-3.5 to suggest queries based on your data.
+          </p>
+
+          <Button
+            darkMode={darkMode}
+            size="sm"
+            classNames='default-secondary-button'
+            styles={{ width: '100%', fontSize: '12px', fontWeight: 700, borderColor: darkMode && 'transparent' }}
+          >
+            <Button.Content title={'Read more'} />
+          </Button>
+        </div>
+
+      </div >
+    </Popover >
+  )
+
   const EducativeLebel = () => {
     const title = () => {
       return (
@@ -157,9 +184,29 @@ return [row for row in data if row['amount'] > 1000]
       );
     };
     return (
-      <Button.UnstyledButton styles={{ height: '28px' }} darkMode={darkMode}>
-        <Button.Content title={title} iconSrc={'assets/images/icons/flash.svg'} direction="left" />
-      </Button.UnstyledButton>
+      <div className='d-flex'>
+        <Button.UnstyledButton styles={{ height: '28px' }} darkMode={darkMode} classNames='mx-1'>
+          <Button.Content title={title} iconSrc={'assets/images/icons/flash.svg'} direction="left" />
+        </Button.UnstyledButton>
+        <OverlayTrigger trigger="click" placement="left" overlay={popoverForRecommendation} rootClose>
+          <svg
+            width="16.7"
+            height="16.7"
+            viewBox="0 0 20 21"
+            fill="#3E63DD"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ cursor: 'pointer' }}
+            data-cy={`transformation-info-icon`}
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10 2.5C5.58172 2.5 2 6.08172 2 10.5C2 14.9183 5.58172 18.5 10 18.5C14.4183 18.5 18 14.9183 18 10.5C18 6.08172 14.4183 2.5 10 2.5ZM0 10.5C0 4.97715 4.47715 0.5 10 0.5C15.5228 0.5 20 4.97715 20 10.5C20 16.0228 15.5228 20.5 10 20.5C4.47715 20.5 0 16.0228 0 10.5ZM9 6.5C9 5.94772 9.44771 5.5 10 5.5H10.01C10.5623 5.5 11.01 5.94772 11.01 6.5C11.01 7.05228 10.5623 7.5 10.01 7.5H10C9.44771 7.5 9 7.05228 9 6.5ZM8 10.5C8 9.94771 8.44772 9.5 9 9.5H10C10.5523 9.5 11 9.94771 11 10.5V13.5C11.5523 13.5 12 13.9477 12 14.5C12 15.0523 11.5523 15.5 11 15.5H10C9.44771 15.5 9 15.0523 9 14.5V11.5C8.44772 11.5 8 11.0523 8 10.5Z"
+              fill="#3E63DD"
+            />
+          </svg>
+        </OverlayTrigger>
+      </div>
     );
   };
 
@@ -233,9 +280,8 @@ return [row for row in data if row['amount'] > 1000]
             <div>
               <button
                 onClick={handleCallToGPT}
-                className={`default-tertiary-button ${darkMode ? 'theme-dark' : ''} ${
-                  fetchingRecommendation ? (darkMode ? 'btn-loading' : 'button-loading') : ''
-                }`}
+                className={`default-tertiary-button ${darkMode ? 'theme-dark' : ''} ${fetchingRecommendation ? (darkMode ? 'btn-loading' : 'button-loading') : ''
+                  }`}
               >
                 Load recommendations ⌘+L
               </button>
