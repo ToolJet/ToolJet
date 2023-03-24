f12ca8ecbc277a96c2f05051c05b896ed6cb4eff
Manish Kushare <kushare.manish9@gmail.com>
2023-03-24 18:59:35 +0530

    Tree component is not reflecting data correctly (#5759)
    * bug fixed: tree select is getting crashed, when data,checkedData,expandedData props are not valid
    
    * bug fixed: tabs component is crashing when in valid data provided for the tabs property
    
    * removed unwanted commented line
    
    * bug fixed for button group component
    
    * bug fixed for steps components
    
    * app is becoming unresponsive for invalid data type in labels property of Button group widget
    
    * app is becoming unresponsive for invalid data type in currentStep property of Steps  widget
    
    * made sugggested changes
    
    * new buttons are getting added to the button groups if labels length exceeds values length
    
    * reverting back last commit
    
    * removing unwanted commented code

--- frontend/src/_helpers/utils.js
+++ frontend/src/_helpers/utils.js
@@ -666,3 +666,30 @@ export const getuserName = (formData) => {
     return `${nameArray?.[0][0]}${nameArray?.[1] != undefined && nameArray?.[1] != '' ? nameArray?.[1][0] : ''} `;
   return '';
 };
+
+export function isExpectedDataType(data, expectedDataType) {
+  function getCurrentDataType(node) {
+    return Object.prototype.toString.call(node).slice(8, -1).toLowerCase();
+  }
+
+  const currentDataType = getCurrentDataType(data);
+
+  if (currentDataType !== expectedDataType) {
+    switch (expectedDataType) {
+      case 'string':
+        return String(data) ? data : undefined;
+      case 'number':
+        return Object.prototype.toString.call(data).slice(8, -1).toLowerCase() === 'number' ? data : undefined;
+      case 'boolean':
+        return Boolean();
+      case 'array':
+        return Object.prototype.toString.call(data).slice(8, -1).toLowerCase() === 'array' ? data : [];
+      case 'object':
+        return Object.prototype.toString.call(data).slice(8, -1).toLowerCase() === 'object' ? data : {};
+      default:
+        return null;
+    }
+  }
+
+  return data;
+}
