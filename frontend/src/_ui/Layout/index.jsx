85e7b6224df3f47781f992ff6d3cd577d6bded50
Arpit <arpitnath42@gmail.com>
2023-03-29 14:31:54 +0530

    Improvements marketplace - beta (#5858)
    * adds marketplace icon to the dashboard and minor UI fixes
    
    * fixes: capitalizeFirstLetter for toast message
    
    * updates to docs: how to build marketplace plugin
    
    * update confirm dialog box
    
    * removing beta tag from leftsidebar
    
    * updated tooltip

--- frontend/src/_ui/Layout/index.jsx
+++ frontend/src/_ui/Layout/index.jsx
@@ -7,11 +7,12 @@ import { NotificationCenter } from '@/_components/NotificationCenter';
 import Logo from '@assets/images/rocket.svg';
 import Header from '../Header';
 import { authenticationService } from '@/_services';
+import config from 'config';
 
 function Layout({ children, switchDarkMode, darkMode }) {
   const router = useRouter();
   const { admin } = authenticationService.currentUserValue;
-
+  const marketplaceEnabled = config.ENABLE_MARKETPLACE_FEATURE === 'true';
   return (
     <div className="row m-auto">
       <div className="col-auto p-0">
@@ -145,6 +146,47 @@ function Layout({ children, switchDarkMode, darkMode }) {
                           fill={router.pathname === '/global-datasources' ? '#3E63DD' : '#C1C8CD'}
                         />
                       </svg>
+                    </ToolTip>
+                  </Link>
+                </li>
+              )}
+              {marketplaceEnabled && (
+                <li className="text-center mt-3 d-flex flex-column">
+                  <Link to="/integrations">
+                    <ToolTip message="Marketplace (Beta)" placement="right">
+                      <div
+                        className="layout-sidebar-icon cursor-pointer"
+                        style={{
+                          width: '32px',
+                          height: '33px',
+                          padding: '4px 6px',
+                          backgroundColor: router.pathname === '/integrations' ? '#E6EDFE' : '#none',
+                          borderRadius: '4px',
+                          marginLeft: '2px',
+                        }}
+                      >
+                        <svg
+                          width="auto"
+                          height="auto"
+                          viewBox="0 0 32 33"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <rect
+                            y="0.325684"
+                            width="32"
+                            height="33"
+                            rx="4"
+                            fill={router.pathname === '/integrations' ? '#E6EDFE' : '#none'}
+                          />
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M1.62969 5.61618C2.77971 2.2981 5.61934 0 8.90009 0H23.1C26.3808 0 29.2204 2.2981 30.3703 5.61618L31.4989 8.87221C31.8601 9.91437 32.199 11.2583 31.8603 12.6375C31.4447 14.33 30.4545 15.8106 29.1062 16.732V25.6641C29.1062 29.7155 25.9046 33 21.955 33H10.0443C6.0949 33 2.89327 29.7155 2.89327 25.6641V16.7315C1.54526 15.8101 0.555389 14.3297 0.139727 12.6375C-0.199061 11.2583 0.13995 9.91437 0.501169 8.87219L1.62969 5.61618ZM5.2846 17.6707V25.6641C5.2846 28.3608 7.4156 30.5468 10.0443 30.5468H21.955C24.5839 30.5468 26.7149 28.3608 26.7149 25.6641V17.6709C26.4575 17.7075 26.1941 17.7266 25.9256 17.7266C23.8362 17.7266 22.0625 16.5866 20.9628 14.8887C19.8633 16.5866 18.0895 17.7266 16 17.7266C13.9106 17.7266 12.1368 16.5866 11.0372 14.8887C9.93758 16.5866 8.16383 17.7266 6.07436 17.7266C5.80578 17.7266 5.54226 17.7075 5.2846 17.6707ZM12.2329 10.7725C12.2329 13.3981 14.048 15.2735 16 15.2735C17.9521 15.2735 19.7671 13.3981 19.7671 10.7725C19.7671 10.095 20.3025 9.54589 20.9628 9.54589C21.6231 9.54589 22.1585 10.095 22.1585 10.7725C22.1585 13.3981 23.9737 15.2735 25.9256 15.2735C27.5508 15.2735 29.0597 13.9991 29.5415 12.0381C29.7011 11.3886 29.5678 10.6231 29.2457 9.69403L28.1172 6.438C27.2574 3.9574 25.2307 2.45316 23.1 2.45316H8.90009C6.7693 2.45316 4.7426 3.9574 3.88283 6.438L2.75431 9.69403C2.4323 10.6231 2.29904 11.3886 2.45859 12.0381C2.94028 13.9991 4.44929 15.2735 6.07436 15.2735C8.02641 15.2735 9.84152 13.3981 9.84152 10.7725C9.84152 10.095 10.3768 9.54589 11.0372 9.54589C11.6975 9.54589 12.2329 10.095 12.2329 10.7725ZM21.5079 23.5342C21.8321 24.1245 21.6287 24.8725 21.0535 25.2052C19.2402 26.254 17.6675 26.8874 16.0041 26.8907C14.3385 26.8939 12.7627 26.2654 10.942 25.2029C10.368 24.8679 10.1674 24.1191 10.4939 23.5302C10.8204 22.9414 11.5504 22.7357 12.1244 23.0706C13.8116 24.0551 14.9511 24.4395 15.9995 24.4375C17.0499 24.4354 18.19 24.0452 19.879 23.0683C20.4542 22.7355 21.1834 22.9442 21.5079 23.5342Z"
+                            fill={router.pathname === '/integrations' ? '#3E63DD' : '#C1C8CD'}
+                          />
+                        </svg>
+                      </div>
                     </ToolTip>
                   </Link>
                 </li>
