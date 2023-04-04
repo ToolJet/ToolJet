063aad9ba27530d8afe85f5e161c75282be80c18
emidhun <midhun752@gmail.com>
2023-04-04 14:09:52 +0530

    Minor typo fix

--- cypress-tests/cypress/support/utils/basicComponents.js
+++ cypress-tests/cypress/support/utils/basicComponents.js
@@ -13,7 +13,7 @@ export const verifyComponent = (widgetName) => {
 export const deleteComponentAndVerify = (widgetName) => {
   cy.get(commonWidgetSelector.draggableWidget(widgetName)).click();
   cy.get(`[data-cy="${widgetName}-delete-button"]`).last().realClick();
-  cy.verifyToastMessage(`.go3958317564`,'Component deleted! (⌘ + Z to undo)')
+  cy.verifyToastMessage(`.go3958317564`,'Component deleted! (ctrl + Z to undo)')
   cy.notVisible(commonWidgetSelector.draggableWidget(widgetName));
 };
 
