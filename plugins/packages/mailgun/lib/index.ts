3eb7c0b836226c7334b3f335be3566a5e845b2b1
stepinfwd <stepinfwd@gmail.com>
2023-04-05 15:26:46 +0530

    init :: working state mailgun

--- plugins/packages/mailgun/lib/index.ts
+++ plugins/packages/mailgun/lib/index.ts
@@ -1,39 +1,38 @@
 import { QueryError, QueryResult, QueryService } from '@tooljet-plugins/common';
-import { SourceOptions, QueryOptions, EmailOptions } from './types';
-import MailgunSdk from 'mailgun.js';
-import FormData from 'form-data';
+import { SourceOptions, QueryOptions } from './types';
 
 export default class Mailgun implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
-    if (!(queryOptions && sourceOptions.api_key)) {
-      throw new QueryError('Query could not be completed as API key is not set', 'Missing API key', {});
-    }
-
-    const sdk = new MailgunSdk(FormData);
-    const mailgunOptions = { username: 'api', key: sourceOptions.api_key, url: null };
-    if (sourceOptions.eu_hosted) {
-      mailgunOptions.url = 'https://api.eu.mailgun.net';
-    }
-    const mailGunClient = sdk.client(mailgunOptions);
-
     let result = {};
-    const emailOptions: EmailOptions = {
-      to: queryOptions.send_mail_to,
-      from: queryOptions.send_mail_from,
-      subject: queryOptions.subject,
-      text: queryOptions.text,
-    };
-
-    if (queryOptions.html && queryOptions.html.length > 0) {
-      emailOptions.html = queryOptions.html;
-    }
+    let response = null;
+    const { operation } = queryOptions;
 
     try {
-      result = await mailGunClient.messages.create(sourceOptions.domain, emailOptions);
+      switch (operation) {
+        case 'mail_service': {
+          const form = new FormData();
+          form.append('from', 'adish@tooljet.com');
+          form.append('to', 'stepinfwd@gmail.com');
+          form.append('subject', 'Hello');
+          form.append('text', 'Testing some Mailgun awesomness!');
+          response = await fetch(`https://api.mailgun.net/v3/${sourceOptions.domain}/messages`, {
+            method: 'POST',
+            headers: {
+              Authorization: 'Basic ' + btoa(`api:${sourceOptions.api_key}`),
+            },
+            body: form,
+          });
+          console.log('respxx', response);
+
+          result = response.body;
+          break;
+        }
+      }
     } catch (error) {
-      console.error(error);
+      console.log(error);
       throw new QueryError('Query could not be completed', error.message, {});
     }
+
     return {
       status: 'ok',
       data: result,
