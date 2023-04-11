ff329e4336f2f23780e59284320d7c4a595cd1ff
Mekhla Asopa <dadhichmekhla@gmail.com>
2023-04-11 19:00:37 +0530

    Fix cypress failure

--- cypress-tests/cypress/e2e/exportImport/export.cy.js
+++ cypress-tests/cypress/e2e/exportImport/export.cy.js
@@ -92,6 +92,9 @@ describe("App Export Functionality", () => {
       .should("be.visible")
       .click();
     createNewVersion(otherVersions = ["v2"], currentVersion = "v1");
+    cy.wait(500);
+    cy.dragAndDropWidget("Toggle Switch", 50, 50);
+    cy.waitForAutoSave();
     cy.get(appVersionSelectors.currentVersionField((otherVersions = "v2")))
       .should("be.visible")
       .invoke("text")
