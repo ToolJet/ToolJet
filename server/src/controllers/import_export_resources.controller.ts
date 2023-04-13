eb439c7f986c5a7f2cb2128a506041180fb4af67
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2023-04-13 13:45:30 +0530

    make use of apps ability for import export resource

--- server/src/controllers/import_export_resources.controller.ts
+++ server/src/controllers/import_export_resources.controller.ts
@@ -1,20 +1,30 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, ForbiddenException } from '@nestjs/common';
 import { JwtAuthGuard } from '../../src/modules/auth/jwt-auth.guard';
 import { User } from 'src/decorators/user.decorator';
 import { ExportResourcesDto } from '@dto/export-resources.dto';
 import { ImportResourcesDto } from '@dto/import-resources.dto';
 import { ImportExportResourcesService } from '@services/import_export_resources.service';
+import { App } from 'src/entities/app.entity';
+import { AppsAbilityFactory } from 'src/modules/casl/abilities/apps-ability.factory';
 
 @Controller({
   path: 'resources',
   version: '2',
 })
 export class ImportExportResourcesController {
-  constructor(private importExportResourcesService: ImportExportResourcesService) {}
+  constructor(
+    private importExportResourcesService: ImportExportResourcesService,
+    private appsAbilityFactory: AppsAbilityFactory
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('/export')
   async export(@User() user, @Body() exportResourcesDto: ExportResourcesDto) {
+    const ability = await this.appsAbilityFactory.appsActions(user);
+
+    if (!ability.can('createApp', App)) {
+      throw new ForbiddenException('You do not have permissions to perform this action');
+    }
     const result = await this.importExportResourcesService.export(user, exportResourcesDto);
     return {
       ...result,
@@ -25,6 +35,12 @@ export class ImportExportResourcesController {
   @UseGuards(JwtAuthGuard)
   @Post('/import')
   async import(@User() user, @Body() importResourcesDto: ImportResourcesDto) {
+    const ability = await this.appsAbilityFactory.appsActions(user);
+
+    if (!ability.can('cloneApp', App)) {
+      throw new ForbiddenException('You do not have permissions to perform this action');
+    }
+
     const imports = await this.importExportResourcesService.import(user, importResourcesDto);
     return { imports, success: true };
   }
--- server/src/modules/import_export_resources/import_export_resources.module.ts
+++ server/src/modules/import_export_resources/import_export_resources.module.ts
@@ -1,32 +1,23 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ImportExportResourcesController } from "@controllers/import_export_resources.controller";
-import { TooljetDbService } from "@services/tooljet_db.service";
-import { ImportExportResourcesService } from "@services/import_export_resources.service";
-import { AppImportExportService } from "@services/app_import_export.service";
-import { TooljetDbImportExportService } from "@services/tooljet_db_import_export_service";
-import { DataSourcesService } from "@services/data_sources.service";
-import { AppEnvironmentService } from "@services/app_environments.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ImportExportResourcesController } from '@controllers/import_export_resources.controller';
+import { TooljetDbService } from '@services/tooljet_db.service';
+import { ImportExportResourcesService } from '@services/import_export_resources.service';
+import { AppImportExportService } from '@services/app_import_export.service';
+import { TooljetDbImportExportService } from '@services/tooljet_db_import_export_service';
+import { DataSourcesService } from '@services/data_sources.service';
+import { AppEnvironmentService } from '@services/app_environments.service';
 import { Plugin } from 'src/entities/plugin.entity';
-import { PluginsHelper } from "src/helpers/plugins.helper";
+import { PluginsHelper } from 'src/helpers/plugins.helper';
 import { CredentialsService } from '@services/credentials.service';
 import { DataSource } from 'src/entities/data_source.entity';
 import { tooljetDbOrmconfig } from '../../../ormconfig';
-import { PluginsModule } from "../plugins/plugins.module";
-import { EncryptionService } from "@services/encryption.service";
+import { PluginsModule } from '../plugins/plugins.module';
+import { EncryptionService } from '@services/encryption.service';
 import { Credential } from '../../../src/entities/credential.entity';
+import { CaslModule } from '../casl/casl.module';
 
-// import { CaslModule } from '../casl/casl.module';
-// import { EncryptionService } from '@services/encryption.service';
-
-const imports = [
-  PluginsModule,
-  TypeOrmModule.forFeature([
-    DataSource,
-    Plugin,
-    Credential,
-  ]),
-]
+const imports = [PluginsModule, CaslModule, TypeOrmModule.forFeature([DataSource, Plugin, Credential])];
 
 if (process.env.ENABLE_TOOLJET_DB === 'true') {
   imports.unshift(TypeOrmModule.forRoot(tooljetDbOrmconfig));
