6f850cfb0c04750d7b82df346bf178ddf1a6cfaa
Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
2023-04-14 15:38:13 +0530

    Fixed issue on running app load queries multiple time for the released app (#6040)
    * Fixed issue on multiple app load
    
    * Removed unwanted code and added comments

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -106,6 +106,7 @@ class AppComponent extends React.Component {
             } else if (isApplicationsPath) {
               this.updateCurrentSession({
                 authentication_failed: true,
+                load_app: true,
               });
             }
           });
@@ -138,6 +139,7 @@ class AppComponent extends React.Component {
             ...data,
             current_organization_name,
             organizations: response.organizations,
+            load_app: true,
           });
 
           // if user is trying to load the workspace login page, then redirect to the dashboard
@@ -173,6 +175,7 @@ class AppComponent extends React.Component {
 
                     this.updateCurrentSession({
                       current_organization_name,
+                      load_app: true,
                     });
 
                     if (!this.isThisWorkspaceLoginPage())
