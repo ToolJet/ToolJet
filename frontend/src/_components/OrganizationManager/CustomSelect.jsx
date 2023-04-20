05db8affa4c4d3bf62a568e92cfdfdfa1de38832
Ajith KV <ajith.jaban@gmail.com>
2023-04-20 10:29:19 +0530

    Add data-cy for edit workspace button (#5942)

--- frontend/src/_components/OrganizationManager/CustomSelect.jsx
+++ frontend/src/_components/OrganizationManager/CustomSelect.jsx
@@ -47,6 +47,7 @@ const Menu = (props) => {
             viewBox="0 0 32 32"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            data-cy="edit-workspace-button"
           >
             <rect width="32" height="32" rx="6" fill="#F0F4FF" />
             <path
