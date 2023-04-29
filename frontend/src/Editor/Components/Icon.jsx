a6030676fe6b2d0c48e0f10017e805939ef08382
Shaurya Gupta <shauryag1000@gmail.com>
2023-04-29 20:08:36 +0530

    fixes cursor default to pointer (#5696)
    * fixes cursor default to pointer
    
    * Added cursor to icon widget
    
    ---------
    
    Co-authored-by: Kavin Venkatachalam <kavin.saratha@gmail.com>

--- frontend/src/Editor/Components/Icon.jsx
+++ frontend/src/Editor/Components/Icon.jsx
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from 'react';
 import * as Icons from '@tabler/icons-react';
 import cx from 'classnames';
 
-export const Icon = ({ properties, styles, fireEvent, width, height, registerAction, darkMode, dataCy }) => {
+export const Icon = ({ properties, styles, fireEvent, width, height, registerAction, darkMode, dataCy, component }) => {
   const { icon } = properties;
   const { iconColor, visibility } = styles;
   // eslint-disable-next-line import/namespace
   const IconElement = Icons[icon];
+  const { definition } = component;
+  const { events = [] } = definition;
 
   const color = iconColor === '#000' ? (darkMode ? '#fff' : '#000') : iconColor;
 
@@ -31,7 +33,10 @@ export const Icon = ({ properties, styles, fireEvent, width, height, registerAct
   });
 
   return (
-    <div className={cx('icon-widget', { 'd-none': !showIcon })} data-cy={dataCy}>
+    <div
+      className={cx('icon-widget', { 'd-none': !showIcon }, { 'cursor-pointer': events.length > 0 })}
+      data-cy={dataCy}
+    >
       <IconElement
         color={color}
         style={{ width, height }}
