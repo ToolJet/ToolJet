da29b52e137cd79eab1e4b5ad12ddbcaae34796e
Vik <vkb.info@gmail.com>
2023-05-01 20:05:39 +0530

    allow null value comparision only with is

--- frontend/src/Editor/QueryManager/QueryEditors/TooljetDatabase/util.js
+++ frontend/src/Editor/QueryManager/QueryEditors/TooljetDatabase/util.js
@@ -27,7 +27,7 @@ export const hasEqualWithNull = (queryOptions, operation) => {
     const filterKeys = Object.keys(filters);
     for (let i = 0; i < filterKeys.length; i++) {
       const filter = filters[filterKeys[i]];
-      if ((filter.operator === 'eq' || filter.operator === 'neq') && filter.value === null) {
+      if (filter.operator !== 'is' && filter.value === null) {
         return true;
       }
     }
