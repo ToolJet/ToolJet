291f5b8087416032e796c74fc40f899ce967f619
arpitnath <arpitnath42@gmail.com>
2023-05-02 18:28:44 +0530

    Revert "ignore building grpc plugin for cloud"
    This reverts commit 583ff7fcf8675eb00bcc34700193bbd49d9a8434.

--- plugins/build-packages.js
+++ plugins/build-packages.js
@@ -1,12 +1,7 @@
 const { execSync } = require('child_process');
 
 if (process.env.NODE_ENV === 'production') {
-  // if tooljet host is 'app.tooljet.com', dont build grpc packages
-  if (process.env.TOOLJET_HOST.includes('app.tooljet.com')) {
-    execSync('npx lerna run build --stream --ignore @tooljet/grpc-*');
-  } else {
-    execSync('npx lerna run build --stream');
-  }
+  execSync('npx lerna run build --stream');
 } else {
   execSync('npx lerna run build --stream --no-private');
 }
