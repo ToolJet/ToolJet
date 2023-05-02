583ff7fcf8675eb00bcc34700193bbd49d9a8434
arpitnath <arpitnath42@gmail.com>
2023-05-02 18:22:17 +0530

    ignore building grpc plugin for cloud

--- plugins/build-packages.js
+++ plugins/build-packages.js
@@ -1,7 +1,12 @@
 const { execSync } = require('child_process');
 
 if (process.env.NODE_ENV === 'production') {
-  execSync('npx lerna run build --stream');
+  // if tooljet host is 'app.tooljet.com', dont build grpc packages
+  if (process.env.TOOLJET_HOST.includes('app.tooljet.com')) {
+    execSync('npx lerna run build --stream --ignore @tooljet/grpc-*');
+  } else {
+    execSync('npx lerna run build --stream');
+  }
 } else {
   execSync('npx lerna run build --stream --no-private');
 }
