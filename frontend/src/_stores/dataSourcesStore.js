f35628bbc43672e868c2c5db6989ac94181f461a
Kavin Venkatachalam <kavin.saratha@gmail.com>
2023-05-03 18:38:38 +0530

    Added global data source in store

--- frontend/src/_stores/dataSourcesStore.js
+++ frontend/src/_stores/dataSourcesStore.js
@@ -29,5 +29,6 @@ export const useDataSourcesStore = create(
 );
 
 export const useDataSources = () => useDataSourcesStore((state) => state.dataSources);
+export const useGlobalDataSources = () => useDataSourcesStore((state) => state.globalDataSources);
 export const useLoadingDataSources = () => useDataSourcesStore((state) => state.loadingDataSources);
 export const useDataSourcesActions = () => useDataSourcesStore((state) => state.actions);
