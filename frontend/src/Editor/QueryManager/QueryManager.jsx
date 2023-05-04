8f960a5242bd0b1e90a5d4725067bba4f9838398
stepinfwd <stepinfwd@gmail.com>
2023-05-04 16:59:08 +0530

    typo

--- frontend/src/Editor/QueryManager/QueryManager.jsx
+++ frontend/src/Editor/QueryManager/QueryManager.jsx
@@ -168,7 +168,7 @@ class QueryManagerComponent extends React.Component {
   };
 
   componentDidUpdate(prevState) {
-    if (this.state?.selectedQuery?.id == prevState?.selectedQuery?.name) {
+    if (this.state?.selectedQuery?.id == prevState?.selectedQuery?.id) {
       if (prevState?.selectedQuery?.name !== this.state?.selectedQuery?.name) {
         this.setState({
           queryName: this.state.selectedQuery?.name,
