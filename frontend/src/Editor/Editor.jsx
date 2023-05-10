d24e9391c4e0be3a0575584070fdd35c2453bbb0
Kiran Ashok <stepinfwd@gmail.com>
2023-05-10 11:10:35 +0530

    Fix ::  View remains in same position during page switch in multi page apps (#6036)
    * fix scroll issue during page switch in editor
    
    * fix :: switch page scroll to top on viewer

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -1674,6 +1674,7 @@ class EditorComponent extends React.Component {
   };
 
   switchPage = (pageId, queryParams = []) => {
+    document.getElementById('real-canvas').scrollIntoView();
     if (this.state.currentPageId === pageId) return;
 
     const { name, handle, events } = this.state.appDefinition.pages[pageId];
