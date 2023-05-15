35ba89d309b68b6b4fa219afd22982a8bda0e2e8
Muhsin Shah <muhsinshah21@gmail.com>
2023-05-15 15:15:20 +0530

    refactoring the code

--- server/migrations/1683136077244-AddUniqueConstraintToWorkspaceName.ts
+++ server/migrations/1683136077244-AddUniqueConstraintToWorkspaceName.ts
@@ -15,19 +15,18 @@ export class AddUniqueConstraintToWorkspaceName1683136077244 implements Migratio
   }
 
   public async migrateWorkspaceNames(entityManager: EntityManager) {
-    const workspaces = await entityManager.find(Organization);
+    const workspaces = await entityManager.query(
+      'select sub_query.name from (select count(*) as name_count, name from organizations group by name) sub_query where name_count > 1    '
+    );
     for (const workspace of workspaces) {
-      const { name, id } = workspace;
+      const { name } = workspace;
       const sameNameWorkspaces = await entityManager.find(Organization, { name });
-      if (sameNameWorkspaces.length > 1) {
-        const workspacesToChange = sameNameWorkspaces.filter((workspace) => workspace.id !== id);
-        for (const workspaceToChange of workspacesToChange) {
-          await entityManager.update(
-            Organization,
-            { id: workspaceToChange.id },
-            { name: `${workspaceToChange.name}_${Date.now()}` }
-          );
-        }
+      for (const workspaceToChange of sameNameWorkspaces.slice(1)) {
+        await entityManager.update(
+          Organization,
+          { id: workspaceToChange.id },
+          { name: `${workspaceToChange.name}_${Date.now()}` }
+        );
       }
     }
   }
