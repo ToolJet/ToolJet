bb5162eda641b96ebac47577e859662468554f37
arpitnath <arpitnath42@gmail.com>
2023-05-18 14:10:51 +0530

    clean up

--- server/src/modules/casl/plugins.guard.ts
+++ server/src/modules/casl/plugins.guard.ts
@@ -5,17 +5,13 @@ export class IsPluginApiEnabledGuard implements CanActivate {
   constructor() {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // Check if the plugin API is enabled based on your environment variable
     const enablePluginApi = process.env.ENABLE_MARKETPLACE_FEATURE === 'true';
 
     if (!enablePluginApi) {
       // If the environment variable is not set or is not 'true',
-      // you can choose how to handle the request.
-      // For example, you can return a 404 Not Found response or a custom error message.
       return false;
     }
 
-    // If the environment variable is 'true', allow the request to proceed.
     return true;
   }
 }
