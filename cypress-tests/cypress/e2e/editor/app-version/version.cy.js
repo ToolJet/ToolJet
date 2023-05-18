c3b92403d6915f0d155bdec94947f394173c7f81
Midhun Kumar E <midhun752@gmail.com>
2023-05-18 13:41:41 +0530

    Fix for failing specs on release branch (#6438)
    * Fix multipage failure
    
    * Fix config
    
    * Fix text spec
    
    * Add versions to the suite
    
    * Fix password and versions spec
    
    * Revert config
    
    * Fix password and listview
    
    * Fix versions with temp fix
    
    * fix all failures.
    
    * add retry
    
    * revert button fix
    
    * Add workspace specs to the suite

--- cypress-tests/cypress/e2e/editor/app-version/version.cy.js
+++ cypress-tests/cypress/e2e/editor/app-version/version.cy.js
@@ -99,6 +99,7 @@ describe("App Version Functionality", () => {
     );
 
     cy.reload();
+    cy.wait(7000); //temp fix need to update
     releasedVersionAndVerify((currentVersion = "v3"));
     navigateToCreateNewVersionModal((currentVersion = "v3"));
     createNewVersion((newVersion = ["v6"]), (versionFrom = "v3"));
