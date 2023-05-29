463291176fe6d4788cee4befa8acfc75a37df3b8
Johnson Cherian <johnsonc.dev@gmail.com>
2023-05-29 09:06:51 +0530

    fix: redirect to home page if user hits wrong url in th viewer

--- frontend/src/Editor/Viewer.jsx
+++ frontend/src/Editor/Viewer.jsx
@@ -154,8 +154,8 @@ class ViewerComponent extends React.Component {
           this.state.currentLayout === 'desktop'
             ? '100%'
             : mobileLayoutHasWidgets
-            ? `${this.state.deviceWindowWidth}px`
-            : '1292px',
+              ? `${this.state.deviceWindowWidth}px`
+              : '1292px',
         selectedComponent: null,
         currentState: {
           queries: queryState,
@@ -532,10 +532,23 @@ class ViewerComponent extends React.Component {
           this.handleError(errorDetails, errorAppId, errorVersionId);
         }
 
-        // debugger;
         const pageArray = Object.values(this.state.appDefinition?.pages || {});
+        //checking if page is hidden
         if (
           pageArray.find((page) => page.handle === this.props.params.pageHandle)?.hidden &&
+          this.state.appDefinition?.pages?.[this.state.appDefinition?.homePageId]
+        ) {
+          const homeHandle = this.state.appDefinition?.pages?.[this.state.appDefinition?.homePageId]?.handle;
+          let url = `/applications/${this.state.appId}/versions/${this.state.versionId}/${homeHandle}`;
+          if (this.state.slug) {
+            url = `/applications/${this.state.slug}/${homeHandle}}`;
+          }
+          return <Navigate to={url} replace />;
+        }
+
+        //checking if page exists
+        if (
+          !pageArray.find((page) => page.handle === this.props.params.pageHandle) &&
           this.state.appDefinition?.pages?.[this.state.appDefinition?.homePageId]
         ) {
           const homeHandle = this.state.appDefinition?.pages?.[this.state.appDefinition?.homePageId]?.handle;
