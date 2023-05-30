0f7c7b43304b4731c0ed5cc671ad2d639d20f66f
Kavin Venkatachalam <kavin.saratha@gmail.com>
2023-05-30 18:37:50 +0530

    Fallback - toast bug on running query after renaming it

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -393,8 +393,9 @@ function executeActionWithDebounce(_ref, event, mode, customVariables) {
       }
 
       case 'run-query': {
-        const { queryId } = event;
-        const { name } = useDataQueriesStore.getState().dataQueries.find((query) => query.id === queryId);
+        const { queryId, queryName } = event;
+        const name =
+          useDataQueriesStore.getState().dataQueries.find((query) => query.id === queryId)?.name ?? queryName;
         return runQuery(_ref, queryId, name, undefined, mode);
       }
       case 'logout': {
