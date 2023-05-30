77a632f8fab990a289694062dd3acf1af8a3dbb5
Manish Kushare <kushare.manish9@gmail.com>
2023-05-30 09:18:14 +0530

    fixed vertical alignment issue for cell in table whose column type are action button, multiselect,datepicker (#6356)

--- frontend/src/Editor/Components/Table/Table.jsx
+++ frontend/src/Editor/Components/Table/Table.jsx
@@ -988,7 +988,7 @@ export function Table({
                 return (
                   <tr
                     key={index}
-                    className={`table-row ${
+                    className={`table-row table-editor-component-row ${
                       highlightSelectedRow && row.id === tableDetails.selectedRowId ? 'selected' : ''
                     }`}
                     {...row.getRowProps()}
@@ -1077,7 +1077,9 @@ export function Table({
                           <div
                             className={`td-container ${
                               cell.column.columnType === 'image' && 'jet-table-image-column'
-                            } ${cell.column.columnType !== 'image' && 'w-100 h-100'}`}
+                            } ${
+                              cell.column.columnType !== 'image' && `w-100 ${_.isEmpty(actionButtonsArray) && 'h-100'}`
+                            }`}
                           >
                             <GenerateEachCellValue
                               cellValue={cellValue}
