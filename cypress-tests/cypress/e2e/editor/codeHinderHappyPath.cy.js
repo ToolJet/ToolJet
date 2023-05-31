79836db21e18f3db5b70b0cc437ab60b6a6db4e7
emidhun <midhun752@gmail.com>
2023-05-31 14:53:37 +0530

    Add automation for runjs codehinder popups

---
+++ cypress-tests/cypress/e2e/editor/codeHinderHappyPath.cy.js
@@ -0,0 +1,101 @@
+import {
+  verifyMultipleComponentValuesFromInspector,
+  verifyComponentValueFromInspector,
+} from "Support/utils/commonWidget";
+import { verifyNodeData, openNode, verifyValue } from "Support/utils/inspector";
+import { commonSelectors, commonWidgetSelector } from "Selectors/common";
+import { addNewPage } from "Support/utils/multipage";
+import {
+  selectCSA,
+  selectEvent,
+  addSupportCSAData,
+} from "Support/utils/events";
+import { multipageSelector } from "Selectors/multipage";
+import {
+  selectQuery,
+  deleteQuery,
+  query,
+  changeQueryToggles,
+  renameQueryFromEditor,
+  addInputOnQueryField,
+} from "Support/utils/queries";
+import {
+  verifyCouldnotConnectWithAlert,
+  resizeQueryPanel,
+  verifypreview,
+  addInput,
+} from "Support/utils/dataSource";
+
+describe("Editor- Codehinder", () => {
+  beforeEach(() => {
+    cy.appUILogin();
+    cy.createApp();
+  });
+
+  it.only("should verify query codehinder", () => {
+    cy.viewport(1800, 1800);
+    resizeQueryPanel("50");
+    selectQuery("Run JavaScript code");
+    cy.get(`[data-cy="runjs-input-field"]`)
+      .click()
+      .realHover()
+      .find('[class="svg-icon m-2 popup-btn"]')
+      .click();
+    cy.wait(200);
+
+    addInputOnQueryField(
+      "codehinder-popup",
+      `setTimeout(() => {
+        actions.setVariable('var', 'test');
+      actions.setPageVariable('pageVar', 'pageTest');
+    }, [0]) `
+    );
+    cy.get('[data-cy="codehinder-popup-close-option-icon"]').click();
+    cy.get(`[data-cy="runjs-input-field"]`)
+      .find(".CodeMirror-line")
+      .should(
+        "have.text",
+        `setTimeout(() => {          actions.setVariable(\'var\', \'test\');        actions.setPageVariable(\'pageVar\', \'pageTest\');}, [0])`
+      );
+
+    changeQueryToggles("notification-on-success");
+    cy.get('[data-cy="success-message-input-field"]')
+      .click()
+      .realHover()
+      .find('[class="svg-icon m-2 popup-btn"]')
+      .click();
+
+    addInputOnQueryField("codehinder-popup", `Message from codehinder`);
+    cy.get('[data-cy="codehinder-popup-close-option-icon"]').click();
+    cy.get('[data-cy="success-message-input-field"]')
+      .find(".CodeMirror-line")
+      .should("have.text", "Message from codehinder");
+
+    query("create");
+    cy.verifyToastMessage(commonSelectors.toastMessage, "Query Added");
+    query("run");
+    cy.verifyToastMessage(
+      commonSelectors.toastMessage,
+      "Query (runjs1) completed."
+    );
+    cy.verifyToastMessage(
+      commonSelectors.toastMessage,
+      "Message from codehinder"
+    );
+
+    cy.get(commonWidgetSelector.sidebarinspector).click();
+    cy.get(".tooltip-inner").invoke("hide");
+    verifyNodeData("variables", "Object", "1 entry ");
+    openNode("variables", 0);
+
+    verifyValue("var", "String", `"test"`);
+
+    openNode("page");
+    openNode("variables", 1);
+    verifyValue("pageVar", "String", `"pageTest"`);
+  });
+
+  it("should verify styles codehinder", () => {});
+
+  it("should verify component specific codehinder", () => {});
+});
