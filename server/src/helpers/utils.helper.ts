36f054f22065af7e4551ae25399f44a65ccaa997
Muhsin Shah <muhsinshah21@gmail.com>
2023-06-05 12:53:05 +0530

    fixed lint error

--- server/src/helpers/utils.helper.ts
+++ server/src/helpers/utils.helper.ts
@@ -1,6 +1,6 @@
 import { QueryError } from 'src/modules/data_sources/query.errors';
 import * as sanitizeHtml from 'sanitize-html';
-import { EntityManager, EntityTarget, getManager, Like } from 'typeorm';
+import { EntityManager, getManager } from 'typeorm';
 import { isEmpty } from 'lodash';
 import { ConflictException } from '@nestjs/common';
 import { DataBaseConstraints } from './db_constraints.constants';
