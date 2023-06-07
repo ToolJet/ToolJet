c0372afd84b3ca2774cdaff1d41ae77bad5259e7
Muhsin Shah <muhsinshah21@gmail.com>
2023-06-07 15:24:13 +0530

    resolved conflicts

--- server/src/helpers/utils.helper.ts
+++ server/src/helpers/utils.helper.ts
@@ -5,8 +5,6 @@ import { isEmpty } from 'lodash';
 import { ConflictException } from '@nestjs/common';
 import { DataBaseConstraints } from './db_constraints.constants';
 const protobuf = require('protobufjs');
-import { ConflictException } from '@nestjs/common';
-import { DataBaseConstraints } from './db_constraints.constants';
 
 export function maybeSetSubPath(path) {
   const hasSubPath = process.env.SUB_PATH !== undefined;
