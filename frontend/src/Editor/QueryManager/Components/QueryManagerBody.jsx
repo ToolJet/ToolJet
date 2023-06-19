f60f83335040e577c6ad184c3d39635fbe20e78e
Kavin Venkatachalam <kavin.saratha@gmail.com>
2023-06-19 08:05:36 +0530

    Fixed bug on updating values on the query panel when the query is switched

--- frontend/src/Editor/QueryManager/Components/QueryManagerBody.jsx
+++ frontend/src/Editor/QueryManager/Components/QueryManagerBody.jsx
@@ -57,6 +57,11 @@ export const QueryManagerBody = forwardRef(
     const { changeDataQuery } = useDataQueriesActions();
 
     const [dataSourceMeta, setDataSourceMeta] = useState(null);
+    /* - Added the below line to cause re-rendering when the query is switched
+       - QueryEditors are not updating when the query is switched
+       - TODO: Remove the below line and make query editors update when the query is switched
+    */
+    const [selectedQueryId, setSelectedQueryId] = useState(selectedQuery?.id);
 
     const queryName = selectedQuery?.name ?? '';
     const sourcecomponentName = selectedDataSource?.kind.charAt(0).toUpperCase() + selectedDataSource?.kind.slice(1);
@@ -70,6 +75,7 @@ export const QueryManagerBody = forwardRef(
           ? selectedQuery?.manifestFile?.data?.source
           : DataSourceTypes.find((source) => source.kind === selectedQuery?.kind)
       );
+      setSelectedQueryId(selectedQuery?.id);
       defaultOptions.current = selectedQuery?.options;
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedQuery?.id]);
@@ -244,6 +250,7 @@ export const QueryManagerBody = forwardRef(
     };
 
     const renderQueryElement = () => {
+      if (selectedQueryId !== selectedQuery?.id) return;
       return (
         <div style={{ padding: '0 32px' }}>
           <div>
@@ -277,6 +284,7 @@ export const QueryManagerBody = forwardRef(
     };
 
     const renderEventManager = () => {
+      if (selectedQueryId !== selectedQuery?.id) return;
       const queryComponent = mockDataQueryAsComponent(options?.events || []);
       return (
         <>
@@ -362,6 +370,7 @@ export const QueryManagerBody = forwardRef(
     );
 
     const renderQueryOptions = () => {
+      if (selectedQueryId !== selectedQuery?.id) return;
       return (
         <div
           className={cx(`advanced-options-container font-weight-400 border-top query-manager-border-color`, {
@@ -378,6 +387,7 @@ export const QueryManagerBody = forwardRef(
     };
 
     const renderChangeDataSource = () => {
+      if (selectedQueryId !== selectedQuery?.id) return;
       return (
         <div className="mt-2 pb-4">
           <div
