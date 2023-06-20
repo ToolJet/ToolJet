65528dbe458236552d93b80d158177dd6ec5c161
ajith-k-v <ajith.jaban@gmail.com>
2023-06-20 11:15:57 +0530

    skip public app test case in Share app spec

--- cypress-tests/cypress/e2e/workspace/shareApp.cy.js
+++ cypress-tests/cypress/e2e/workspace/shareApp.cy.js
@@ -96,7 +96,7 @@ describe("App share functionality", () => {
     );
   });
 
-  it("Verify app private and public app visisbility for the same instance user", () => {
+  it.skip("Verify app private and public app visisbility for the same instance user", () => {
     data.firstName = fake.firstName;
     data.email = fake.email.toLowerCase();
 
