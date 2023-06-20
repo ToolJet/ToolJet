ae812ac3283d801219beb290e3b1603e2bc172ed
Manish Kushare <kushare.manish9@gmail.com>
2023-06-20 11:38:48 +0530

    Made switch page action to execute in release 2.7.0 (#6758)
    * bug fixed
    
    * removed confirmed as it was not used
    
    * removed _ref from runPy function as well

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -954,9 +954,9 @@ export function runQuery(_ref, queryId, queryName, confirmed = undefined, mode =
     _self.setState({ currentState: newState }, () => {
       let queryExecutionPromise = null;
       if (query.kind === 'runjs') {
-        queryExecutionPromise = executeMultilineJS(_self, query.options.code, _ref, query?.id, false, confirmed, mode);
+        queryExecutionPromise = executeMultilineJS(_self, query.options.code, query?.id, false, mode);
       } else if (query.kind === 'runpy') {
-        queryExecutionPromise = executeRunPycode(_self, query.options.code, query, _ref, false, mode);
+        queryExecutionPromise = executeRunPycode(_self, query.options.code, query, false, mode);
       } else if (query.kind === 'tooljetdb') {
         const currentSessionValue = authenticationService.currentSessionValue;
         queryExecutionPromise = tooljetDbOperations.perform(
