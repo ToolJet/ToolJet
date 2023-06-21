a41d2b9138e56367ef4311e62e81e8f9eb32a924
arpitnath <arpitnath42@gmail.com>
2023-06-22 03:31:36 +0530

    adds group permission for org constants and required migrations

---
+++ server/data-migrations/1687384710638-BackfillAddOrgEnvironmentConstantsGroupPermissions.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { GroupPermission } from '../src/entities/group_permission.entity';
+
+export class BackfillAddOrgEnvironmentConstantsGroupPermissions1687384710638 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    const GroupPermissionRepository = entityManager.getRepository(GroupPermission);
+
+    await GroupPermissionRepository.update(
+      { group: 'admin' },
+      { orgEnvironmentConstantCreate: true, orgEnvironmentConstantDelete: true }
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    const GroupPermissionRepository = entityManager.getRepository(GroupPermission);
+
+    await GroupPermissionRepository.update(
+      { group: 'admin' },
+      { orgEnvironmentConstantCreate: false, orgEnvironmentConstantDelete: false }
+    );
+  }
+}
