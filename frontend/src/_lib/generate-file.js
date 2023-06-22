6ee9c4df02cd4ffcb3ff00b0c9d06387049dbc3d
vishnu.nemarugommula <vishnunemarugommula@gmail.com>
2023-06-22 19:07:40 +0530

    Modifying generateFile function to make it compatible for pdf file type

--- frontend/src/_lib/generate-file.js
+++ frontend/src/_lib/generate-file.js
@@ -1,6 +1,14 @@
+import jsPDF from 'jspdf';
+
 export default function generateFile(filename, data, fileType) {
+  if (fileType === 'pdf') {
+    generatePDF(filename, data);
+    return;
+  }
+
   const type = fileType === 'csv' ? 'text/csv' : 'text/plain';
   const blob = new Blob([data], { type });
+
   if (window.navigator.msSaveOrOpenBlob) {
     window.navigator.msSaveBlob(blob, filename);
   } else {
@@ -12,4 +20,17 @@ export default function generateFile(filename, data, fileType) {
     document.body.removeChild(elem);
     window.URL.revokeObjectURL(elem.href);
   }
+}
+
+function generatePDF(filename, data) {
+  const doc = new jsPDF();
+  const pageWidth = doc.internal.pageSize.getWidth();
+  const pageHeight = doc.internal.pageSize.getHeight();
+  const margin = 10;
+  const x = margin;
+  const y = margin;
+
+  doc.text(data, x, y, { align: 'left', maxWidth: pageWidth - 2 * margin });
+
+  doc.save(filename);
 }
