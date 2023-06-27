4c6051d12cede6c5a3813d8f69630ad77e5b30a0
manishkushare <kushare.manish9@gmail.com>
2023-06-27 17:15:30 +0530

    fixed the codehinter portal height for go-to-app

--- frontend/src/Editor/Inspector/ActionConfigurationPanels/GotoApp.jsx
+++ frontend/src/Editor/Inspector/ActionConfigurationPanels/GotoApp.jsx
@@ -73,7 +73,7 @@ export function GotoApp({ getAllApps, currentState, event, handlerChanged, event
                 initialValue={event.queryParams[index][0]}
                 onChange={(value) => queryParamChangeHandler(index, 0, value)}
                 mode="javascript"
-                className="form-control codehinter-query-editor-input"
+                className=""
                 height={30}
               />
             </div>
@@ -83,7 +83,7 @@ export function GotoApp({ getAllApps, currentState, event, handlerChanged, event
                 initialValue={event.queryParams[index][1]}
                 onChange={(value) => queryParamChangeHandler(index, 1, value)}
                 mode="javascript"
-                className="form-control codehinter-query-editor-input"
+                // className="form-control codehinter-query-editor-input"
                 height={30}
               />
             </div>
