ad7e8c41897a6a99eb215e5203821e9abb2fefed
Adish M <44204658+adishM98@users.noreply.github.com>
2023-06-28 18:34:17 +0530

    Update setup_machine.sh

--- deploy/ec2/setup_machine.sh
+++ deploy/ec2/setup_machine.sh
@@ -71,7 +71,7 @@ mkdir -p ~/app
 sudo echo -n $SSH_PRIVATE_KEY | base64 -d  >> ~/.ssh/id_rsa
 sudo chmod 400 ~/.ssh/id_rsa
 ssh-keyscan github.com >> .ssh/known_hosts
-git clone -b v2.8.0-ee2.1.4 git@github.com:ToolJet/tj-ee.git ~/app && cd ~/app
+git clone -b develop git@github.com:ToolJet/tj-ee.git ~/app && cd ~/app
 
 # need to revert the branch from v2.8.0-ee2.1.4 to develop
 
