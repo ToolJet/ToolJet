9ba27b40a8cc6f3d58930ea0e9c49648e3e680f4
Adish M <44204658+adishM98@users.noreply.github.com>
2023-06-28 14:31:24 +0530

    Fix:packer set-up file (#236)

--- deploy/ec2/setup_machine.sh
+++ deploy/ec2/setup_machine.sh
@@ -70,9 +70,7 @@ mkdir -p ~/app
 sudo echo -n $SSH_PRIVATE_KEY | base64 -d  >> ~/.ssh/id_rsa
 sudo chmod 400 ~/.ssh/id_rsa
 ssh-keyscan github.com >> .ssh/known_hosts
-git clone -b release/v2.7.0-ee2.1.2 git@github.com:ToolJet/tj-ee.git ~/app && cd ~/app
-
-# need to revert the branch from release/v2.7.0-ee2.1.2 to develop
+git clone -b develop git@github.com:ToolJet/tj-ee.git ~/app && cd ~/app
 
 mv /tmp/.env ~/app/.env
 mv /tmp/setup_app ~/app/setup_app
