145b3554c2b50bc8620462bcae739a9e31215ad7
Anantshree Chandola <anantshreechandola23@gmail.com>
2023-07-03 13:11:12 +0530

    resolve lint errors

--- server/data-migrations/1688114626183-truncateOrganisationFolderAndAppNamesWithLengthGreaterThan100.ts
+++ server/data-migrations/1688114626183-truncateOrganisationFolderAndAppNamesWithLengthGreaterThan100.ts
@@ -4,47 +4,49 @@ import { Folder } from 'src/entities/folder.entity';
 import { Organization } from 'src/entities/organization.entity';
 
 export class TruncateOrganisationFolderAndAppNamesWithLengthGreaterThan1001688114626183 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        const entityManager = queryRunner.manager;
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
 
-        await this.truncateAppNames(entityManager);
-        await this.truncateFolderNames(entityManager);
-        await this.truncateOrganizationNames(entityManager);
+    await this.truncateAppNames(entityManager);
+    await this.truncateFolderNames(entityManager);
+    await this.truncateOrganizationNames(entityManager);
+  }
+
+  public async truncateAppNames(entityManager: EntityManager) {
+    const apps = await entityManager.find(App, {
+      where: 'LENGTH(name) > 100',
+    });
+
+    for (const app of apps) {
+      app.name = app.name.substring(0, 100);
     }
 
-    public async truncateAppNames(entityManager: EntityManager) {
-        const apps = await entityManager.find(App, {
-            where: "LENGTH(name) > 100",
-        });
+    await entityManager.save(apps);
+  }
 
-        for (const app of apps) {
-            app.name = app.name.substring(0, 100);
-            await entityManager.save(app);
-        }
+  public async truncateFolderNames(entityManager: EntityManager) {
+    const folders = await entityManager.find(Folder, {
+      where: 'LENGTH(name) > 100',
+    });
+
+    for (const folder of folders) {
+      folder.name = folder.name.substring(0, 100);
     }
 
-    public async truncateFolderNames(entityManager: EntityManager) {
-        const folders = await entityManager.find(Folder, {
-            where: "LENGTH(name) > 100",
-        });
+    await entityManager.save(folders);
+  }
 
-        for (const folder of folders) {
-            folder.name = folder.name.substring(0, 100);
-            await entityManager.save(folder);
-        }
+  public async truncateOrganizationNames(entityManager: EntityManager) {
+    const organizations = await entityManager.find(Organization, {
+      where: 'LENGTH(name) > 100',
+    });
+
+    for (const organization of organizations) {
+      organization.name = organization.name.substring(0, 100);
     }
 
-    public async truncateOrganizationNames(entityManager: EntityManager) {
-        const organizations = await entityManager.find(Organization, {
-            where: "LENGTH(name) > 100",
-        });
+    await entityManager.save(organizations);
+  }
 
-        for (const organization of organizations) {
-            organization.name = organization.name.substring(0, 100);
-            await entityManager.save(organization);
-        }
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
