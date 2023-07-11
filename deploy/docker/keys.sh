cc1a56dae8ae5f2daabf263d871897913fef23e8
Adish M <adish.madhu@gmail.com>
2023-07-11 14:41:25 +0530

    updating keys.sh and .env.internal.example

--- deploy/docker/keys.sh
+++ deploy/docker/keys.sh
@@ -16,9 +16,9 @@ if [[ -z "$LOCKBOX_MASTER_KEY" ]]; then
     END { if (!found) print "LOCKBOX_MASTER_KEY="key }
   ' .env > temp.env && mv temp.env .env
 
-  echo "Generated LOCKBOX_MASTER_KEY"
+  echo "Generated a secure master key for the lockbox"
 else
-  echo "LOCKBOX_MASTER_KEY already exists"
+  echo "The lockbox master key already exists."
 fi
 
 # Check if SECRET_KEY_BASE is present or empty
@@ -34,9 +34,9 @@ if [[ -z "$SECRET_KEY_BASE" ]]; then
     END { if (!found) print "SECRET_KEY_BASE="key }
   ' .env > temp.env && mv temp.env .env
 
-  echo "Generated SECRET_KEY_BASE"
+  echo "Created a secret key for secure operations."
 else
-  echo "SECRET_KEY_BASE already exists"
+  echo "The secret key base is already in place."
 fi
 
 # Check if PGRST_JWT_SECRET is present or empty
@@ -52,27 +52,50 @@ if [[ -z "$PGRST_JWT_SECRET" ]]; then
     END { if (!found) print "PGRST_JWT_SECRET="key }
   ' .env > temp.env && mv temp.env .env
 
-  echo "Generated PGRST_JWT_SECRET"
+  echo "Generated a unique secret for PGRST authentication."
 else
-  echo "PGRST_JWT_SECRET already exists"
+  echo "The PGRST JWT secret is already generated and in place."
 fi
+
+# Function to generate a random password
+generate_password() {
+  openssl rand -base64 12 | tr -d '/+' | cut -c1-16
+}
 
 # Check if PG_PASS and TOOLJET_DB_PASS are present or empty
 if [[ -z "$PG_PASS" ]] && [[ -z "$TOOLJET_DB_PASS" ]]; then
-  # Prompt for password value
-  read -p "Enter the password value for postgres database: " PASSWORD_VALUE
+  # Generate random passwords
+  PASSWORD=$(generate_password)
 
   # Update .env file
-  awk -v pass="$PASSWORD_VALUE" '
+  awk -v pass="$PASSWORD" '
     BEGIN { FS=OFS="=" }
     /^(PG_PASS|TOOLJET_DB_PASS)=/ { $2=pass; found=1 }
     1
     END { if (!found) print "PG_PASS="pass ORS "TOOLJET_DB_PASS="pass }
   ' .env > temp.env && mv temp.env .env
 
-  echo "Password udapted"
+  echo "Successfully generated a secure password for the PostgreSQL database."
 else
-  echo "Password already exist"
+  echo "Postgres password already exist"
+fi
+
+# Check if PGRST_DB_URI is present or empty
+if [[ -z "$PGRST_DB_URI" ]]; then
+  # Construct PGRST_DB_URI with PG_PASS
+  PGRST_DB_URI="postgres://postgres:$PASSWORD@postgresql/tooljet_db"
+
+  # Update .env file for PGRST_DB_URI
+  awk -v uri="$PGRST_DB_URI" '
+    BEGIN { FS=OFS="=" }
+    /^PGRST_DB_URI=/ { $2=uri; found=1 }
+    1
+    END { if (!found) print "PGRST_DB_URI="uri }
+  ' .env > temp.env && mv temp.env .env
+
+  echo "Successfully updated PGRST database URI"
+else
+  echo "The PGRST DB URI is already configured and in use."
 fi
 
 exec "$@"
