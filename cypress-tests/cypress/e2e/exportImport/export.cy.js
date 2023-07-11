a8d3c81b942766971b8806a554d8e8972d01470c
emidhun <midhun752@gmail.com>
2023-07-11 15:27:44 +0530

    App Builder cypress fix

--- cypress-tests/cypress/e2e/exportImport/export.cy.js
+++ cypress-tests/cypress/e2e/exportImport/export.cy.js
@@ -91,7 +91,7 @@ describe("App Export Functionality", () => {
     cy.get(appVersionSelectors.appVersionMenuField)
       .should("be.visible")
       .click();
-    createNewVersion(otherVersions = ["v2"], currentVersion = "v1");
+    createNewVersion((otherVersions = ["v2"]), (currentVersion = "v1"));
     cy.wait(500);
     cy.dragAndDropWidget("Toggle Switch", 50, 50);
     cy.waitForAutoSave();
