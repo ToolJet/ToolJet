3dcdc8843e0c4607c9174e518698072cc0e2504b
gsmithun4 <gsmithun4@gmail.com>
2023-07-12 15:22:36 +0530

    resolved merge conflicts

--- frontend/src/_services/app_environment.service.js
+++ frontend/src/_services/app_environment.service.js
@@ -33,21 +33,3 @@ function getVersionsByEnvironment(appId, environmentId /* not needed for CE */) 
     requestOptions
   ).then(handleResponse);
 }
-
-/* 
-MERGE NOTES (EE/Cloud) !!!:
- * the endpoint will be changed to /:environmentId/versions
- * function will have environment as new params id
- (So don't miss the changes while resolving the conflicts. and don't remove this comment in future) 
-
- for CE don't need to pass the environmentId
-*/
-function getVersionsByEnvironment(appId, environmentId /* not needed for CE */) {
-  const requestOptions = { method: 'GET', headers: authHeader(), credentials: 'include' };
-  const query = queryString.stringify({ app_id: appId });
-
-  return fetch(
-    `${config.apiUrl}/app-environments${environmentId ? `/${environmentId}` : ''}/versions${query ? `?${query}` : ''}`,
-    requestOptions
-  ).then(handleResponse);
-}
