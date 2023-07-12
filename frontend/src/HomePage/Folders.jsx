b158c9e1d7dc7d510bffee18f174bc4b7bad2521
Kushal Mitra <68588123+web-dev-kushal@users.noreply.github.com>
2023-07-12 22:56:18 +0530

    Updated Folders.jsx

--- frontend/src/HomePage/Folders.jsx
+++ frontend/src/HomePage/Folders.jsx
@@ -160,6 +160,17 @@ export const Folders = function Folders({
     }
   }
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      if(showUpdateForm){
+        executeEditFolder();
+      }
+      else {
+        saveFolder();
+      }
+    }
+  };
+
   const handleInputChange = (e) => {
     setErrorText('');
     const error = validateName(e.target.value, 'Folder name', false, false);
@@ -319,6 +330,7 @@ export const Folders = function Folders({
               value={newFolderName}
               maxLength={50}
               data-cy="folder-name-input"
+              onKeyPress={handleKeyPress}
               autoFocus
             />
             <label className="tj-input-error">{errorText || ''}</label>
