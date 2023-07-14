bef1543da62cff0e7dbacda2a99d740eca356874
LENOVO <rahulwaghmare552@gmail.com>
2023-07-14 22:17:07 +0530

    Bug fixed Password input box dark mode issue

--- frontend/src/Editor/Components/PasswordInput.jsx
+++ frontend/src/Editor/Components/PasswordInput.jsx
@@ -18,6 +18,16 @@ export const PasswordInput = ({
   const [passwordValue, setPasswordValue] = React.useState('');
   const { isValid, validationError } = validate(passwordValue);
 
+  const computedStyles = {
+    height,
+    display: visibility ? '' : 'none',
+    borderRadius: `${borderRadius}px`,
+    color: darkMode && '#fff',
+    borderColor: darkMode && '#DADCDE',
+    backgroundColor: darkMode && '#232e3c',
+    boxShadow: boxShadow,
+  };
+
   React.useEffect(() => {
     setExposedVariable('isValid', isValid);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -37,13 +47,7 @@ export const PasswordInput = ({
         }`}
         placeholder={placeholder}
         value={passwordValue}
-        style={{
-          height,
-          display: visibility ? '' : 'none',
-          borderRadius: `${borderRadius}px`,
-          backgroundColor,
-          boxShadow,
-        }}
+        style={computedStyles}
         data-cy={dataCy}
       />
       <div className="invalid-feedback" data-cy={`${String(component.name).toLowerCase()}-invalid-feedback`}>
