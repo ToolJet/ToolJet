9b68c7540fe007706bb3612cd99c55ea81b740f4
stepinfwd <stepinfwd@gmail.com>
2023-07-18 16:04:54 +0530

    goto params not working

--- frontend/src/Editor/Viewer.jsx
+++ frontend/src/Editor/Viewer.jsx
@@ -557,20 +557,6 @@ class ViewerComponent extends React.Component {
           this.handleError(errorDetails, errorAppId, errorVersionId);
         }
 
-        const pageArray = Object.values(this.state.appDefinition?.pages || {});
-        //checking if page exists
-        if (
-          !pageArray.find((page) => page.handle === this.props.params.pageHandle) &&
-          this.state.appDefinition?.pages?.[this.state.appDefinition?.homePageId]
-        ) {
-          const homeHandle = this.state.appDefinition?.pages?.[this.state.appDefinition?.homePageId]?.handle;
-          let url = `/applications/${this.state.appId}/versions/${this.state.versionId}/${homeHandle}`;
-          if (this.state.slug) {
-            url = `/applications/${this.state.slug}/${homeHandle}`;
-          }
-          return <Navigate to={`${url}${this.props.params.pageHandle ? '' : window.location.search}`} replace />;
-        }
-
         return (
           <div className="viewer wrapper">
             <Confirm
