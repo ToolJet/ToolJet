0715ae7d5020a39f87845189df3399632de1349a
Nakul Nagargade <nakul@tooljet.com>
2023-07-19 16:12:15 +0530

    Update message in All log tab when there are no logs

--- frontend/src/Editor/LeftSidebar/SidebarDebugger/SidebarDebuggerTabs.jsx
+++ frontend/src/Editor/LeftSidebar/SidebarDebugger/SidebarDebuggerTabs.jsx
@@ -4,12 +4,16 @@ import Tabs from 'react-bootstrap/Tabs';
 import Logs from './Logs';
 import { useTranslation } from 'react-i18next';
 
-const DebuggerTabContent = ({ logs, darkMode }) => {
+const DebuggerTabContent = ({ logs, darkMode, tabName }) => {
   const { t } = useTranslation();
   return (
     <div className="card-body mb-5">
       {logs.length === 0 && (
-        <center className="p-2 text-muted">{t(`leftSidebar.Debugger.noErrors`, 'No errors found.')}</center>
+        <center className="p-2 text-muted">
+          {tabName === 'errors'
+            ? t(`leftSidebar.Debugger.noErrors`, 'No errors found.')
+            : t(`leftSidebar.Debugger.noLogs`, 'No Logs found.')}
+        </center>
       )}
 
       <div className="tab-content">
@@ -25,10 +29,10 @@ const SidebarDebuggerTabs = ({ darkMode, errors, allLog }) => {
   return (
     <Tabs defaultActiveKey="allLog" id="sidebar-debugger" className="mb-3 sidebar-debugger" justify>
       <Tab eventKey="allLog" title="All Log">
-        <DebuggerTabContent logs={allLog} darkMode={darkMode} />
+        <DebuggerTabContent logs={allLog} darkMode={darkMode} tabName={'allLogs'} />
       </Tab>
       <Tab eventKey="errors" title="Errors">
-        <DebuggerTabContent logs={errors} darkMode={darkMode} />
+        <DebuggerTabContent logs={errors} darkMode={darkMode} tabName={'errors'} />
       </Tab>
     </Tabs>
   );
