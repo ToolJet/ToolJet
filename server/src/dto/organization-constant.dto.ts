076865f9a52d93408dc0db6c2fd0e0cb73c7399c
arpitnath <arpitnath42@gmail.com>
2023-07-19 19:54:21 +0530

    fixes: error message typo

--- server/src/dto/organization-constant.dto.ts
+++ server/src/dto/organization-constant.dto.ts
@@ -9,8 +9,8 @@ export class CreateOrganizationConstantDto {
   @Matches(/^[a-zA-Z_][a-zA-Z0-9_]*$/, {
     message: 'Constant name must start with a letter or underscore and contain only letters, numbers, and underscores',
   })
-  @MaxLength(32, { message: 'Column name must be less than 32 characters' })
-  @MinLength(1, { message: 'Column name must be at least 1 character' })
+  @MaxLength(32, { message: 'Constant name must be less than 32 characters' })
+  @MinLength(1, { message: 'Constant name must be at least 1 character' })
   constant_name: string;
 
   @IsString({ each: true })
@@ -19,16 +19,16 @@ export class CreateOrganizationConstantDto {
 
   @IsString()
   @IsNotEmpty()
-  @MinLength(1, { message: 'Column name must be at least 1 character' })
-  @MaxLength(10000, { message: 'Column name must be less than 10000 characters' })
+  @MinLength(1, { message: 'Constant value must be at least 1 character' })
+  @MaxLength(10000, { message: 'Constant value must be less than 10000 characters' })
   value: string;
 }
 
 export class UpdateOrganizationConstantDto {
   @IsString()
   @IsNotEmpty()
-  @MinLength(1, { message: 'Column name must be at least 1 character' })
-  @MaxLength(10000, { message: 'Column name must be less than 10000 characters' })
+  @MinLength(1, { message: 'Constant value must be at least 1 character' })
+  @MaxLength(10000, { message: 'Constant value must be less than 10000 characters' })
   value?: string;
 
   @IsString()
@@ -38,7 +38,7 @@ export class UpdateOrganizationConstantDto {
   @IsString()
   @IsNotEmpty()
   @IsOptional()
-  @MaxLength(20, { message: 'Column name must be less than 32 characters' })
-  @MinLength(1, { message: 'Column name must be at least 1 character' })
+  @MaxLength(32, { message: 'Constant name must be less than 32 characters' })
+  @MinLength(1, { message: 'Constant name must be at least 1 character' })
   constant_name?: string;
 }
