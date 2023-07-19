3abd9649fed0e6f246e7edfa361df316b1433ae6
stepinfwd <stepinfwd@gmail.com>
2023-07-19 11:41:17 +0530

    fix :: restricted table to be added as child widget to kanban card

--- frontend/src/Editor/WidgetManager/restrictedWidgetsConfig.js
+++ frontend/src/Editor/WidgetManager/restrictedWidgetsConfig.js
@@ -1,6 +1,6 @@
 export const restrictedWidgetsObj = {
   Form: ['Calendar', 'Kanban', 'Form', 'Tabs', 'Modal', 'Listview', 'Container'],
-  Kanban_card: ['Calendar', 'Kanban', 'Form', 'Tabs', 'Modal', 'Listview', 'Container'],
+  Kanban_card: ['Calendar', 'Kanban', 'Form', 'Tabs', 'Modal', 'Listview', 'Container', 'Table'],
   Calendar: ['Calendar', 'Kanban'],
   Container: ['Calendar', 'Kanban'],
   Modal: ['Calendar', 'Kanban'],
