98e0d06245c2c87d883c242e06f803eebf6c700d
Mike <89523154+mikebarr24@users.noreply.github.com>
2023-07-21 07:06:00 +0100

    fix: datepicker time text colour (#6667)
    Co-authored-by: Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>

--- frontend/src/Editor/Components/Datepicker.jsx
+++ frontend/src/Editor/Components/Datepicker.jsx
@@ -104,7 +104,8 @@ export const Datepicker = function Datepicker({
         showYearDropdown
         dropdownMode="select"
         excludeDates={excludedDates}
-        customInput={<input style={{ borderRadius: `${borderRadius}px`, boxShadow, height }} />}
+        customInput={<input style={{ borderRadius: `${borderRadius}px`, height }} />}
+        timeInputLabel={<div className={`${darkMode && 'theme-dark'}`}>Time</div>}
       />
 
       <div data-cy="date-picker-invalid-feedback" className={`invalid-feedback ${isValid ? '' : 'd-flex'}`}>
