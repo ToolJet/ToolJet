68f6b016294cd8e452fab3de5aa881d5cdd23056
Midhun Kumar E <midhun752@gmail.com>
2023-07-25 13:51:49 +0530

    Fixed cypress App Builder for v2.10.3 (#7118)
    * Fix cypress for v2.10.3
    
    * Remove only
    
    * modify the expossed values count
    
    * fix common methods
    
    * Version modified

--- cypress-tests/cypress/support/commands.js
+++ cypress-tests/cypress/support/commands.js
@@ -49,24 +49,16 @@ Cypress.Commands.add("waitForAutoSave", () => {
 });
 
 Cypress.Commands.add("createApp", (appName) => {
-  cy.get("body").then(($title) => {
-    if ($title.text().includes(commonText.introductionMessage)) {
-      cy.get(commonSelectors.emptyAppCreateButton).eq(0).click();
-    } else {
-      cy.get(commonSelectors.appCreateButton).click();
-    }
-    if (Cypress.env("environment") === "Community") {
-      cy.intercept("GET", "/api/v2/data_sources").as("appDs");
-      cy.wait("@appDs", { timeout: 15000 });
-      cy.skipEditorPopover();
-    }
-    else {
-      cy.intercept("GET", "/api/app-environments/**").as("appDs");
-      cy.wait("@appDs", { timeout: 15000 });
-      cy.skipEditorPopover();
-    }
+  const getAppButtonSelector = ($title) =>
+    $title.text().includes(commonText.introductionMessage)
+      ? commonSelectors.emptyAppCreateButton
+      : commonSelectors.appCreateButton;
 
+  cy.get("body").then(($title) => {
+    cy.get(getAppButtonSelector($title)).click();
   });
+  cy.waitForAppLoad();
+  cy.skipEditorPopover();
 });
 
 Cypress.Commands.add(
@@ -115,9 +107,9 @@ Cypress.Commands.add(
       .invoke("text")
       .then((text) => {
         cy.wrap(subject).type(createBackspaceText(text)),
-        {
-          delay: 0,
-        };
+          {
+            delay: 0,
+          };
       });
     if (!Array.isArray(value)) {
       cy.wrap(subject).type(value, {
@@ -192,9 +184,9 @@ Cypress.Commands.add(
       .invoke("text")
       .then((text) => {
         cy.wrap(subject).type(createBackspaceText(text)),
-        {
-          delay: 0,
-        };
+          {
+            delay: 0,
+          };
       });
   }
 );
@@ -251,16 +243,30 @@ Cypress.Commands.add("reloadAppForTheElement", (elementText) => {
 });
 
 Cypress.Commands.add("skipEditorPopover", () => {
-  cy.wait(1000);
+  cy.wait(2000);
   cy.get("body").then(($el) => {
     if ($el.text().includes("Skip", { timeout: 2000 })) {
       cy.get(commonSelectors.skipButton).realClick();
-    } else {
-      cy.log("instructions modal is skipped ");
     }
+  });
+  const log = Cypress.log({
+    name: "Skip Popover",
+    displayName: "Skip Popover",
+    message: " Popover skipped",
   });
 });
 
+Cypress.Commands.add("waitForAppLoad", () => {
+  const API_ENDPOINT =
+    Cypress.env("environment") === "Community"
+      ? "/api/v2/data_sources"
+      : "/api/app-environments/**";
+
+  const TIMEOUT = 15000;
+
+  cy.intercept("GET", API_ENDPOINT).as("appDs");
+  cy.wait("@appDs", { timeout: TIMEOUT });
+});
 
 Cypress.Commands.add("visitTheWorkspace", (workspaceName) => {
   cy.task("updateId", {
@@ -269,6 +275,6 @@ Cypress.Commands.add("visitTheWorkspace", (workspaceName) => {
   }).then((resp) => {
     let workspaceId = resp.rows[0].id;
     cy.visit(workspaceId);
-  })
+  });
   cy.wait(2000);
-})
+});
