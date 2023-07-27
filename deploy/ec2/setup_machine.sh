f23b5d3195ecd4d20bac7fecd3d75095450f2165
Adish M <adish.madhu@gmail.com>
2023-07-27 17:32:09 +0530

    correction in ec2 setup.sh file

--- deploy/ec2/setup_machine.sh
+++ deploy/ec2/setup_machine.sh
@@ -71,7 +71,7 @@ mkdir -p ~/app
 sudo echo -n $SSH_PRIVATE_KEY | base64 -d  >> ~/.ssh/id_rsa
 sudo chmod 400 ~/.ssh/id_rsa
 ssh-keyscan github.com >> .ssh/known_hosts
-git clone -b release/v2.11.0-ee2.4.0 git@github.com:ToolJet/tj-ee.git ~/app && cd ~/app 
+git clone -b develop git@github.com:ToolJet/tj-ee.git ~/app && cd ~/app 
 
 mv /tmp/.env ~/app/.env
 mv /tmp/setup_app ~/app/setup_app
