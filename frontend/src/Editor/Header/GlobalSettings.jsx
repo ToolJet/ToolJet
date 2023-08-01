9ddb05bbf49cc91a6a02173a43465c22716b3323
Muhsin Shah <muhsinshah21@gmail.com>
2023-08-01 16:46:28 +0530

    working on app slug

--- frontend/src/Editor/Header/GlobalSettings.jsx
+++ frontend/src/Editor/Header/GlobalSettings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 import { SketchPicker } from 'react-color';
 import { Confirm } from '../Viewer/Confirm';
@@ -6,7 +6,7 @@ import { HeaderSection } from '@/_ui/LeftSidebar';
 import { LeftSidebarItem } from '../LeftSidebar/SidebarItem';
 import FxButton from '../CodeBuilder/Elements/FxButton';
 import { CodeHinter } from '../CodeBuilder/CodeHinter';
-import { resolveReferences } from '@/_helpers/utils';
+import { resolveReferences, validateName } from '@/_helpers/utils';
 import { useTranslation } from 'react-i18next';
 import _ from 'lodash';
 import Popover from '@/_ui/Popover';
@@ -22,12 +22,16 @@ export const GlobalSettings = ({
   const { t } = useTranslation();
   const { hideHeader, canvasMaxWidth, canvasMaxWidthType, canvasMaxHeight, canvasBackgroundColor, backgroundFxQuery } =
     globalSettings;
-  const [showPicker, setShowPicker] = React.useState(false);
+  const [showPicker, setShowPicker] = useState(false);
   const currentState = useCurrentState();
-  const [forceCodeBox, setForceCodeBox] = React.useState(true);
-  const [realState, setRealState] = React.useState(currentState);
-  const [showConfirmation, setConfirmationShow] = React.useState(false);
-  const [show, setShow] = React.useState('');
+  const [forceCodeBox, setForceCodeBox] = useState(true);
+  const [realState, setRealState] = useState(currentState);
+  const [showConfirmation, setConfirmationShow] = useState(false);
+  const [show, setShow] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [fields, setFields] = useState({ name: { value: '', error: '' }, slug: { value: null, error: '' } });
+  const [slugProgress, setSlugProgress] = useState(false);
+  const [isDisabled, setDisabled] = useState(true);
 
   const coverStyles = {
     position: 'fixed',
@@ -48,6 +52,41 @@ export const GlobalSettings = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [JSON.stringify(resolveReferences(backgroundFxQuery, realState))]);
 
+  const handleInputChange = (value, field) => {
+    setFields({
+      ...fields,
+      [field]: {
+        ...fields[field],
+        error: null,
+      },
+    });
+    const error = validateName(
+      value,
+      `Workspace ${field}`,
+      false,
+      !(field === 'slug'),
+      !(field === 'slug'),
+      field === 'slug'
+    );
+    setFields({
+      ...fields,
+      [field]: {
+        value,
+        error: error?.errorMsg,
+      },
+    });
+
+    const otherInputErrors = Object.keys(fields).find(
+      (key) => (key !== field && !_.isEmpty(fields[key].error)) || (key !== field && _.isEmpty(fields[key].value))
+    );
+    setDisabled(!error?.status || otherInputErrors);
+  };
+
+  const delayedSlugChange = _.debounce((value, field) => {
+    handleInputChange(value, field);
+    setSlugProgress(false);
+  }, 500);
+
   const popoverContent = (
     <div id="global-settings-popover" className={cx({ 'theme-dark': darkMode })}>
       <div bsPrefix="global-settings-popover">
@@ -55,6 +94,64 @@ export const GlobalSettings = ({
           <HeaderSection.PanelHeader title="Global settings" />
         </HeaderSection>
         <div className="card-body">
+          <div className="row app-slug">
+            <div className="col tj-app-input input-with-icon">
+              <label>Unique workspace slug</label>
+              <input
+                type="text"
+                className={`form-control is-valid`}
+                placeholder={t('header.organization.workspaceSlug', 'unique workspace slug')}
+                disabled={isCreating}
+                maxLength={50}
+                onChange={(e) => {
+                  setSlugProgress(true);
+                  e.persist();
+                  delayedSlugChange(e.target.value, 'slug');
+                }}
+                data-cy="workspace-slug-input-field"
+                autoFocusfields
+              />
+              {fields['slug'].value !== null && !fields['slug'].error && (
+                <div className="icon-container">
+                  <svg width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M14.256 0.244078C14.5814 0.569515 14.5814 1.09715 14.256 1.42259L5.92263 9.75592C5.59719 10.0814 5.06956 10.0814 4.74412 9.75592L0.577452 5.58926C0.252015 5.26382 0.252015 4.73618 0.577452 4.41074C0.902889 4.08531 1.43053 4.08531 1.75596 4.41074L5.33337 7.98816L13.0775 0.244078C13.4029 -0.0813592 13.9305 -0.0813592 14.256 0.244078Z"
+                      fill="#46A758"
+                    />
+                  </svg>
+                </div>
+              )}
+              {fields['slug']?.error ? (
+                <label className="label tj-input-error">{fields['slug']?.error || ''}</label>
+              ) : fields['slug'].value ? (
+                <label className="label label-success">{`Slug accepted!`}</label>
+              ) : (
+                <label className="label label-info">{`URL-friendly 'slug' consists of lowercase letters, numbers, and hyphens`}</label>
+              )}
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col modal-main tj-app-input">
+              <label>Workspace link</label>
+              <div className={`tj-text-input break-all ${darkMode ? 'dark' : ''}`}>
+                {!slugProgress ? (
+                  `${window.public_config?.TOOLJET_HOST}/${fields['slug']?.value || ''}`
+                ) : (
+                  <div className="d-flex gap-2">
+                    <div class="spinner-border text-secondary workspace-spinner" role="status">
+                      <span class="visually-hidden">Loading...</span>
+                    </div>
+                    {`Updating link`}
+                  </div>
+                )}
+              </div>
+              <label className="label label-success label-updated">
+                {fields['slug'].value && !fields['slug'].error ? `Link updated successfully!` : ''}
+              </label>
+            </div>
+          </div>
           <div>
             <div className="d-flex mb-3">
               <span data-cy={`label-hide-header-for-launched-apps`}>
