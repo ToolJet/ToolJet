2ce9f30e4674ada84f18549d7ac20b9f4169f1e5
NanaReyes <dianac.reyesmantilla@gmail.com>
2023-08-01 15:16:23 -0500

    Se modifica componente Link con material ui

--- frontend/src/Editor/Components/Link.jsx
+++ frontend/src/Editor/Components/Link.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import cx from 'classnames';
+import MUILink from '@mui/material/Link';
+import config from 'config';
 
 export const Link = ({ height, properties, styles, fireEvent, registerAction, dataCy }) => {
   const { linkTarget, linkText, targetType } = properties;
@@ -20,26 +22,53 @@ export const Link = ({ height, properties, styles, fireEvent, registerAction, da
     []
   );
   return (
-    <div
-      className={cx('link-widget', { 'd-none': !visibility }, `${underline}`)}
-      style={computedStyles}
-      data-cy={dataCy}
-    >
-      <a
-        {...(linkTarget != '' ? { href: linkTarget } : {})}
-        target={targetType === 'new' && '_blank'}
-        onClick={(event) => {
-          event.stopPropagation();
-          fireEvent('onClick');
-        }}
-        onMouseOver={() => {
-          fireEvent('onHover');
-        }}
-        style={{ color: textColor }}
-        ref={clickRef}
-      >
-        {linkText}
-      </a>
-    </div>
+    <>
+      {config.UI_LIB === 'tooljet' && (
+        <div
+          className={cx('link-widget', { 'd-none': !visibility }, `${underline}`)}
+          style={computedStyles}
+          data-cy={dataCy}
+        >
+          <a
+            {...(linkTarget != '' ? { href: linkTarget } : {})}
+            target={targetType === 'new' && '_blank'}
+            onClick={(event) => {
+              event.stopPropagation();
+              fireEvent('onClick');
+            }}
+            onMouseOver={() => {
+              fireEvent('onHover');
+            }}
+            style={{ color: textColor }}
+            ref={clickRef}
+          >
+            {linkText}
+          </a>
+        </div>
+      )}
+      {config.UI_LIB === 'mui' && (
+        <div
+          className={cx('link-widget', { 'd-none': !visibility }, `${underline}`)}
+          style={computedStyles}
+          data-cy={dataCy}
+        >
+          <MUILink
+            {...(linkTarget != '' ? { href: linkTarget } : {})}
+            target={targetType === 'new' && '_blank'}
+            onClick={(event) => {
+              event.stopPropagation();
+              fireEvent('onClick');
+            }}
+            onMouseOver={() => {
+              fireEvent('onHover');
+            }}
+            style={{ color: textColor }}
+            ref={clickRef}
+          >
+            {linkText}
+          </MUILink>
+        </div>
+      )}
+    </>
   );
 };
