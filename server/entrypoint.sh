eb0ed32a479ba4fe4e6e7b93477e0436a1dd41e3
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2023-08-02 19:42:47 +0530

    fix entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -15,11 +15,11 @@ fi
 
 if [ -z "$DATABASE_URL" ]
 then
-     ./server/scripts/wait-for-it.sh $PG_HOST:${PG_PORT:-5432} --strict --timeout=300 -- $SETUP_CMD
-else 
-     PG_HOST=$(echo "$DATABASE_URL" | awk -F[@/:] '{print $4}')
-     PG_PORT=$(echo "$DATABASE_URL" | awk -F'[@/]' '{split($5,a,":"); print a[2] ? a[2] : "5432"}')
-     ./server/scripts/wait-for-it.sh "$PG_HOST:$PG_PORT" --strict --timeout=300 -- $SETUP_CMD
+  ./server/scripts/wait-for-it.sh $PG_HOST:${PG_PORT:-5432} --strict --timeout=300 -- bash -c "$SETUP_CMD"
+else
+  PG_HOST=$(echo "$DATABASE_URL" | awk -F[@/:] '{print $4}')
+  PG_PORT=$(echo "$DATABASE_URL" | awk -F'[@/]' '{split($5,a,":"); print a[2] ? a[2] : "5432"}')
+  ./server/scripts/wait-for-it.sh "$PG_HOST:$PG_PORT" --strict --timeout=300 -- bash -c "$SETUP_CMD"
 fi
 
 exec "$@"
