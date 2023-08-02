3be01b12bc2ca32c49abf19365a69a412fb714a9
JoseCabrera99 <josecabrera9914@gmail.com>
2023-08-02 14:18:53 -0500

    Instalacion de JOY para uso del CircularProgress

--- frontend/src/Editor/Components/Spinner.jsx
+++ frontend/src/Editor/Components/Spinner.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import config from 'config';
+import { Box } from '@mui/material';
+import { CircularProgress } from '@mui/joy';
 
 export const Spinner = ({ styles, height, dataCy }) => {
   const { colour, size, visibility, boxShadow } = styles;
@@ -8,10 +11,29 @@ export const Spinner = ({ styles, height, dataCy }) => {
     display: visibility ? '' : 'none',
     boxShadow,
   };
+  console.log('colour', colour);
 
   return (
-    <div className="spinner-container" style={baseStyle} data-cy={dataCy}>
-      <div className={`spinner-border spinner-border-${size}`} role="status" style={{ color: colour }}></div>
-    </div>
+    <>
+      {config.UI_LIB === 'tooljet' && (
+        <div className="spinner-container" style={baseStyle} data-cy={dataCy}>
+          <div className={`spinner-border spinner-border-${size}`} role="status" style={{ color: colour }}></div>
+        </div>
+      )}
+      {config.UI_LIB === 'mui' && (
+        <Box className="spinner-container" style={baseStyle}>
+          <CircularProgress
+            size={size}
+            value={70}
+            sx={{
+              '& .MuiCircularProgress-progress': {
+                stroke: colour,
+              },
+            }}
+            role="status"
+          />
+        </Box>
+      )}
+    </>
   );
 };
