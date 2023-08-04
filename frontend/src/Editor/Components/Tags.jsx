b9fa853afea91e0e3ff8608429fdfc8e82812782
NanaReyes <dianac.reyesmantilla@gmail.com>
2023-08-03 23:52:25 -0500

    Se incluye mui en componente tags

--- frontend/src/Editor/Components/Tags.jsx
+++ frontend/src/Editor/Components/Tags.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import config from 'config';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 export const Tags = function Tags({ width, height, properties, styles, dataCy }) {
   const { data } = properties;
@@ -27,11 +30,30 @@ export const Tags = function Tags({ width, height, properties, styles, dataCy })
   }
 
   return (
-    <div style={computedStyles} data-cy={dataCy}>
-      {data &&
-        data.map((item, index) => {
-          return renderTag(item, index);
-        })}
-    </div>
+    <>
+      {config.UI_LIB === 'tooljet' && (
+        <div style={computedStyles} data-cy={dataCy}>
+          {data &&
+            data.map((item, index) => {
+              return renderTag(item, index);
+            })}
+        </div>
+      )}
+      {config.UI_LIB === 'mui' && (
+        <Stack data-cy={dataCy} style={computedStyles} direction="row" spacing={1}>
+          {data &&
+            data.map((item, index) => {
+              return (
+                <Chip
+                  variant="outlined"
+                  key={index}
+                  label={item.title}
+                  style={{ backgroundColor: item.color, color: item.textColor, textTransform: 'none' }}
+                />
+              );
+            })}
+        </Stack>
+      )}
+    </>
   );
 };
