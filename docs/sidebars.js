949e0d622ae9f6da2e948070e8b0effdb9956b72
Shubhendra Singh Chauhan <withshubh@gmail.com>
2023-08-09 12:17:43 +0530

    [docs] Marketplace 1.2 (#6969)
    * [docs]harperdb nosql
    
    * [docs]new operations in dynamodb
    
    * [docs] minor changes in dynamodb
    
    * [docs]redis revamp with encode-decode
    
    * [docs]azure blob gds
    
    * [docs]graphql authentication type udpate
    
    * [docs]added v2.12.0
    
    * minor fix in how to use inspector
    
    * minor fix in pdf&filepicker

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -56,6 +56,7 @@ const sidebars = {
             'data-sources/amazonses',
             'data-sources/appwrite',
             'data-sources/athena',
+            'data-sources/azureblob',
             'data-sources/baserow',
             'data-sources/bigquery',
             'data-sources/firestore',
