e65c33d41ec630c5ae111bbbbd35f97aab07bd80
manishkushare <kushare.manish9@gmail.com>
2023-08-11 12:35:42 +0530

    changed background color of edited cell

--- frontend/src/Editor/Components/Table/Table.jsx
+++ frontend/src/Editor/Components/Table/Table.jsx
@@ -1240,8 +1240,8 @@ export function Table({
                             _.get(tableDetails.changeSet[cell.row.index], currentColumn?.accessor, undefined) !==
                             undefined
                           ) {
-                            cellProps.style.backgroundColor = darkMode ? '#1c252f' : '#ffffde';
-                            cellProps.style['--tblr-table-accent-bg'] = darkMode ? '#1c252f' : '#ffffde';
+                            cellProps.style.backgroundColor = darkMode ? '#391A03' : '#FFF1E7';
+                            cellProps.style['--tblr-table-accent-bg'] = darkMode ? '#391A03' : '#FFF1E7';
                           }
                         }
                       }
