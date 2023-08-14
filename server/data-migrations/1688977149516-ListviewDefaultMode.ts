fb1e45c0fa61c9c72b117702505b26e11352754d
stepinfwd <stepinfwd@gmail.com>
2023-08-14 13:20:06 +0530

    removed unwanted migrations checks in ce

--- server/data-migrations/1688977149516-ListviewDefaultMode.ts
+++ server/data-migrations/1688977149516-ListviewDefaultMode.ts
@@ -1,58 +1,48 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { AppVersion } from '../src/entities/app_version.entity';
-import { MigrationProgress } from 'src/helpers/utils.helper';
+
 export class ListviewDefaultMode1688977149516 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const entityManager = queryRunner.manager;
-    const appVersionsCount = await entityManager.count(AppVersion);
-    const perQuery = 1000;
-    const loopsCount = appVersionsCount / perQuery;
-    const migrationProgress = new MigrationProgress('ListviewDefaultMode1688977149516', appVersionsCount);
-    // Changes for fixing timeout error
-    for (let i = 1; i <= loopsCount; i++) {
-      const skip = perQuery * i - perQuery;
-      const appVersions = await entityManager.find(AppVersion, { take: perQuery, skip });
-      for (const version of appVersions) {
-        const definition = version['definition'];
+    const appVersions = await entityManager.find(AppVersion);
+    for (const version of appVersions) {
+      const definition = version['definition'];
 
-        if (definition) {
-          const pages = definition['pages'];
-          if (pages) {
-            for (const pageId of Object.keys(pages)) {
-              const components = definition['pages'][pageId]['components'];
+      if (definition) {
+        const pages = definition['pages'];
+        if (pages) {
+          for (const pageId of Object.keys(pages)) {
+            const components = definition['pages'][pageId]['components'];
+            if (components) {
+              for (const componentId of Object.keys(components)) {
+                const component = components[componentId];
 
-              if (components) {
-                for (const componentId of Object.keys(components)) {
-                  const component = components[componentId];
+                if (component?.component?.component === 'Listview') {
+                  component['component']['definition']['properties']['mode'] = {
+                    value: 'list',
+                  };
 
-                  if (component?.component?.component === 'Listview') {
-                    component['component']['definition']['properties']['mode'] = {
-                      value: 'list',
-                    };
-
-                    components[componentId] = {
-                      ...component,
-                      component: {
-                        ...component.component,
-                        definition: {
-                          ...component.component.definition,
-                        },
+                  components[componentId] = {
+                    ...component,
+                    component: {
+                      ...component.component,
+                      definition: {
+                        ...component.component.definition,
                       },
-                    };
-                  }
+                    },
+                  };
                 }
               }
-
-              definition['components'] = components;
-              version.definition = definition;
             }
-          }
 
-          await entityManager.update(AppVersion, { id: version.id }, { definition });
+            definition['components'] = components;
+            version.definition = definition;
+          }
         }
-        migrationProgress.show();
+        await entityManager.update(AppVersion, { id: version.id }, { definition });
       }
     }
   }
+
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
