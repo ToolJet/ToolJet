c231f51e6214a7e48b7b85238b56a51514c2f6ff
DiegoMeV <diegom.solinfo@gmail.com>
2023-08-15 10:04:49 -0500

    Tabla con materialUI

--- frontend/src/Editor/Components/Table/GlobalFilter.jsx
+++ frontend/src/Editor/Components/Table/GlobalFilter.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import config from 'config';
+import { Box, TextField } from '@mui/material';
+import { SearchOffOutlined } from '@mui/icons-material';
+
 // Table Search
 export const GlobalFilter = ({
   globalFilter,
@@ -10,6 +14,7 @@ export const GlobalFilter = ({
   darkMode,
 }) => {
   const [value, setValue] = React.useState(globalFilter);
+
   const onChange = useAsyncDebounce((filterValue) => {
     setValue(filterValue);
     setGlobalFilter(filterValue || undefined);
@@ -19,26 +24,51 @@ export const GlobalFilter = ({
   }, 500);
 
   return (
-    <div
-      className="ms-2 d-flex border px-2 mx-1 btn-light align-items-center"
-      style={{ padding: '0.25rem 0' }}
-    >
-      <img
-        src="assets/images/icons/search.svg"
-        alt="search icon"
-        style={{ width: '15px', height: '15px', marginRight: '0.25rem' }}
-      />
-      <input
-        type="text"
-        className={`global-search-field btn-light align-self-center ${darkMode && 'dark-theme-placeholder'}`}
-        defaultValue={value || ''}
-        onChange={(e) => onChange(e.target.value)}
-        placeholder="Search"
-        data-cy="search-input-field"
-        style={{
-          border: '0',
-        }}
-      />
-    </div>
+    <>
+      {config.UI_LIB === 'tooljet' && (
+        <div
+          className="ms-2 d-flex border px-2 mx-1 btn-light align-items-center"
+          style={{ padding: '0.25rem 0' }}
+        >
+          <img
+            src="assets/images/icons/search.svg"
+            alt="search icon"
+            style={{ width: '15px', height: '15px', marginRight: '0.25rem' }}
+          />
+          <input
+            type="text"
+            className={`global-search-field btn-light align-self-center ${darkMode && 'dark-theme-placeholder'}`}
+            defaultValue={value || ''}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder="Search"
+            data-cy="search-input-field"
+            style={{
+              border: '0',
+            }}
+          />
+        </div>
+      )}
+      {config.UI_LIB === 'mui' && (
+        <Box
+          sx={{
+            p: 0.5,
+            pb: 0,
+          }}
+        >
+          <TextField
+            sx={{ width: '100%' }}
+            variant="outlined"
+            size="small"
+            defaultValue={value || ''}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder="Search…"
+            autoFocus
+            InputProps={{
+              startAdornment: <SearchOffOutlined />,
+            }}
+          />
+        </Box>
+      )}
+    </>
   );
 };
