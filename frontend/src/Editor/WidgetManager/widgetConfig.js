bc2c8cfa169c876e93d8e21ad0c213749fb2a11e
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2023-08-19 12:15:31 +0530

    Updated the text on the File picker component

--- frontend/src/Editor/WidgetManager/widgetConfig.js
+++ frontend/src/Editor/WidgetManager/widgetConfig.js
@@ -3215,7 +3215,7 @@ export const widgets = [
         showOnMobile: { value: '{{false}}' },
       },
       properties: {
-        instructionText: { value: 'Drag and Drop some files here, or click to select files' },
+        instructionText: { value: 'Drag and drop files here or click to select files' },
         enableDropzone: { value: '{{true}}' },
         enablePicker: { value: '{{true}}' },
         maxFileCount: { value: '{{2}}' },
