44505279b2b5b38aa5fa166f3cd62e56290f09ee
Johnson Cherian <johnsonc.dev@gmail.com>
2023-08-23 20:04:25 +0530

    fix: removed unused imports

--- frontend/src/Editor/QueryPanel/QueryCard.jsx
+++ frontend/src/Editor/QueryPanel/QueryCard.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import { Tooltip } from 'react-tooltip';
 import { checkExistingQueryName } from '@/_helpers/appUtils';
 import { Confirm } from '../Viewer/Confirm';
