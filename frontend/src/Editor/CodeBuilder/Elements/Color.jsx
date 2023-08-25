97af59e8d02b7a0e923de8c2009451a20d785fc8
stepinfwd <stepinfwd@gmail.com>
2023-08-25 13:42:56 +0530

    feat :: styling color picker styles panel

--- frontend/src/Editor/CodeBuilder/Elements/Color.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Color.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { SketchPicker } from 'react-color';
-import FxButton from './FxButton';
 
 export const Color = ({ value, onChange, forceCodeBox, hideFx = false, pickerStyle = {}, cyLabel }) => {
   const [showPicker, setShowPicker] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
 
   const coverStyles = {
     position: 'fixed',
@@ -11,6 +11,33 @@ export const Color = ({ value, onChange, forceCodeBox, hideFx = false, pickerSty
     right: '0px',
     bottom: '0px',
     left: '0px',
+  };
+  const popoverStyle = {
+    position: 'absolute',
+    right: '0px',
+    top: '0px',
+    zIndex: 10000,
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
+  const outerStyles = {
+    width: '142px',
+    height: '32px',
+    border: !isHovering ? `1px solid var(--slate7)` : `1px solid var(--slate8)`,
+    borderRadius: ' 6px',
+    display: 'flex',
+    paddingLeft: '4px',
+    alignItems: 'center',
+    gap: '4px',
+    background: showPicker ? 'var(--indigo2)' : !isHovering ? 'var(--slate1)' : 'var(--slate4)',
+    outline: showPicker && '1px solid var(--indigo9)',
+    boxShadow: showPicker && '0px 0px 0px 1px #C6D4F9',
   };
 
   const decimalToHex = (alpha) => {
@@ -41,22 +68,27 @@ export const Color = ({ value, onChange, forceCodeBox, hideFx = false, pickerSty
           )}
 
           <div
-            className="row mx-0 form-control color-picker-input"
+            className="row mx-0 color-picker-input d-flex"
             onClick={() => setShowPicker(true)}
             data-cy={`${String(cyLabel)}-picker`}
+            style={outerStyles}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
             <div
               className="col-auto"
               style={{
                 float: 'right',
-                width: '20px',
-                height: '20px',
+                width: '24px',
+                height: '24px',
+                borderRadius: ' 6px',
+                border: `1px solid var(--slate7, #D7DBDF)`,
+                boxShadow: `0px 1px 2px 0px rgba(16, 24, 40, 0.05)`,
                 backgroundColor: value,
-                border: `0.25px solid ${['#ffffff', '#fff', '#1f2936'].includes(value) && '#c5c8c9'}`,
               }}
               data-cy={`${String(cyLabel)}-picker-icon`}
             ></div>
-            <div className="col" data-cy={`${String(cyLabel)}-value`}>
+            <div className="col tj-text-xsm p-0" data-cy={`${String(cyLabel)}-value`}>
               {value}
             </div>
           </div>
