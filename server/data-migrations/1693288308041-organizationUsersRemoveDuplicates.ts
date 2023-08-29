fa1e37362a0b022aec82dcea36f21729b86274fb
gsmithun4 <gsmithun4@gmail.com>
2023-08-29 18:27:01 +0530

    added logs and comments

--- server/data-migrations/1693288308041-organizationUsersRemoveDuplicates.ts
+++ server/data-migrations/1693288308041-organizationUsersRemoveDuplicates.ts
@@ -1,3 +1,5 @@
+import { WORKSPACE_USER_STATUS } from 'src/helpers/user_lifecycle';
+import { MigrationProgress } from 'src/helpers/utils.helper';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class organizationUsersRemoveDuplicates1693288308041 implements MigrationInterface {
@@ -7,6 +9,12 @@ export class organizationUsersRemoveDuplicates1693288308041 implements Migration
       'select sub_query.organization_id, sub_query.user_id from (select count(*) as org_user_count, organization_id, user_id from organization_users group by organization_id, user_id) sub_query where org_user_count > 1'
     );
     if (duplicates && duplicates.length) {
+      const migrationProgress = new MigrationProgress(
+        'organizationUsersRemoveDuplicates1693288308041',
+        duplicates.length
+      );
+
+      console.log(`found ${duplicates.length} duplicates in organization_users table`);
       for (const duplicate of duplicates) {
         let idToKeep;
         const duplicatesWithStatus = await entityManager.query(
@@ -15,18 +23,21 @@ export class organizationUsersRemoveDuplicates1693288308041 implements Migration
         );
 
         if (duplicatesWithStatus && duplicatesWithStatus.length > 0) {
-          idToKeep = duplicatesWithStatus.find((e) => e.status === 'active')?.id;
+          // Keeping record with status active
+          idToKeep = duplicatesWithStatus.find((e) => e.status === WORKSPACE_USER_STATUS.ACTIVE)?.id;
 
           if (!idToKeep) {
+            // Keeping random record
             idToKeep = duplicatesWithStatus[0].id;
           }
         }
         if (idToKeep) {
           await entityManager.query(
-            'delete from organization_users where id!=$1 and organization_id=$1 and user_id=$2',
+            'delete from organization_users where id!=$1 and organization_id=$2 and user_id=$3',
             [idToKeep, duplicate.organization_id, duplicate.user_id]
           );
         }
+        migrationProgress.show();
       }
     }
   }
