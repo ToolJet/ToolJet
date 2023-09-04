2cc5ae3fccbd4cb84094000369a9463d9f853bfe
Mekhla Asopa <dadhichmekhla@gmail.com>
2023-09-04 19:06:20 +0530

    Updated text

--- cypress-tests/cypress/constants/texts/postgreSql.js
+++ cypress-tests/cypress/constants/texts/postgreSql.js
@@ -2,8 +2,8 @@ export const postgreSqlText = {
   labelDataSources: "Datasources",
   labelAddDataSource: "+ add data source",
 
-  allDataSources: "All data sources (41)",
-  allDatabase: "Databases (17)",
+  allDataSources: "All data sources (43)",
+  allDatabase: "Databases (19)",
   allApis: "APIs (20)",
   allCloudStorage: "Cloud Storage (4)",
 
