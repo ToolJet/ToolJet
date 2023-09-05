2c25b5587bbc6a618f34666ac65a1a762e573b57
stepinfwd <stepinfwd@gmail.com>
2023-09-05 15:50:37 +0530

    fix :: global settings broken ui

--- frontend/src/Editor/Header/GlobalSettings.jsx
+++ frontend/src/Editor/Header/GlobalSettings.jsx
@@ -12,6 +12,7 @@ import ExportAppModal from '../../HomePage/ExportAppModal';
 import { useCurrentState } from '@/_stores/currentStateStore';
 import { useAppVersionStore } from '@/_stores/appVersionStore';
 import { shallow } from 'zustand/shallow';
+import { ButtonSolid } from '@/_ui/AppButton/AppButton';
 
 export const GlobalSettings = ({
   globalSettings,
@@ -67,6 +68,7 @@ export const GlobalSettings = ({
     outline: showPicker && '1px solid var(--indigo9)',
     boxShadow: showPicker && '0px 0px 0px 1px #C6D4F9',
   };
+  console.log('darkmode---', darkMode);
 
   return (
     <>
@@ -93,7 +95,7 @@ export const GlobalSettings = ({
           darkMode={darkMode}
         />
       )}
-      <div id="global-settings-popover" className={cx({ 'dark-theme': darkMode, disabled: isVersionReleased })}>
+      <div id="" className={cx({ 'dark-theme': darkMode, disabled: isVersionReleased })}>
         <div bsPrefix="global-settings-popover">
           <HeaderSection darkMode={darkMode}>
             <HeaderSection.PanelHeader title="Global settings" />
@@ -135,9 +137,10 @@ export const GlobalSettings = ({
                 <div className="position-relative">
                   <div className="global-settings-width-input-container">
                     <input
+                      style={{ width: '103px' }}
                       data-cy="maximum-canvas-width-input-field"
                       type="text"
-                      className={`form-control form-control-sm`}
+                      className={`form-control`}
                       placeholder={'0'}
                       onChange={(e) => {
                         const width = e.target.value;
@@ -189,8 +192,45 @@ export const GlobalSettings = ({
                 </div>
               </div>
             </div> */}
-              <div className="d-flex align-items-center">
-                <span className="w-full" data-cy={`label-bg-canvas`}>
+
+              <div className="d-flex align-items-center  global-popover-div-wrap mb-3">
+                <p className="tj-text-xsm color-slate12 w-full m-auto">Export app</p>
+                <div>
+                  <ButtonSolid
+                    variant="primary"
+                    style={{ width: '158px' }}
+                    size="md"
+                    onClick={() => {
+                      setIsExportingApp(true);
+                      document.getElementById('maintenance-app-modal').click();
+                    }}
+                    leftIcon="archive"
+                    iconWidth="12"
+                    data-cy="button-user-status-change"
+                  >
+                    Export this app
+                  </ButtonSolid>
+                </div>
+                {/* <button
+                className="export-app-btn"
+                onClick={() => {
+                  setIsExportingApp(true);
+                  document.getElementById('maintenance-app-modal').click();
+                }}
+              >
+                <svg width="16" height="16" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M2.5 5.78906V17.2891V18.0391C2.5 19.834 3.98027 21.2891 5.77519 21.2891C7.5425 21.2891 9 19.8564 9 18.0891C9 17.6472 9.35817 17.2891 9.8 17.2891H18.5V5.78906C18.5 4.13221 17.1569 2.78906 15.5 2.78906H5.5C3.84315 2.78906 2.5 4.13221 2.5 5.78906ZM9.75 9.61723C9.70334 9.65231 9.65858 9.69108 9.61612 9.73355L8.03033 11.3193C7.73744 11.6122 7.26256 11.6122 6.96967 11.3193C6.67678 11.0264 6.67678 10.5516 6.96967 10.2587L8.55546 8.67289C9.6294 7.59895 11.3706 7.59895 12.4445 8.67289L14.0303 10.2587C14.3232 10.5516 14.3232 11.0264 14.0303 11.3193C13.7374 11.6122 13.2626 11.6122 12.9697 11.3193L11.3839 9.73355C11.3414 9.69108 11.2967 9.65231 11.25 9.61723V13.789C11.25 14.2032 10.9142 14.539 10.5 14.539C10.0858 14.539 9.75 14.2032 9.75 13.789V9.61723ZM22.3766 19.7789C21.9361 21.5093 20.3675 22.7891 18.5 22.7891H6.5C8.36748 22.7891 9.93606 21.5093 10.3766 19.7789C10.5128 19.2437 10.9477 18.7891 11.5 18.7891H21.5C22.0523 18.7891 22.5128 19.2437 22.3766 19.7789Z"
+                    fill="#3E63DD"
+                  />
+                </svg>
+                <span style={{ paddingLeft: '6px' }}>Export this app</span>
+              </button> */}
+              </div>
+              <div className="d-flex align-items-center justify-content-between mb-3">
+                <span className="" data-cy={`label-bg-canvas`}>
                   {t('leftSidebar.Settings.backgroundColorOfCanvas', 'Canvas bavkground')}
                 </span>
                 <div className="canvas-codehinter-container">
@@ -264,26 +304,6 @@ export const GlobalSettings = ({
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="d-flex align-items-center  global-popover-div-wrap">
-              <p className="global-popover-text">Export app</p>
-              <button
-                className="export-app-btn"
-                onClick={() => {
-                  setIsExportingApp(true);
-                  document.getElementById('maintenance-app-modal').click();
-                }}
-              >
-                <svg width="16" height="16" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M2.5 5.78906V17.2891V18.0391C2.5 19.834 3.98027 21.2891 5.77519 21.2891C7.5425 21.2891 9 19.8564 9 18.0891C9 17.6472 9.35817 17.2891 9.8 17.2891H18.5V5.78906C18.5 4.13221 17.1569 2.78906 15.5 2.78906H5.5C3.84315 2.78906 2.5 4.13221 2.5 5.78906ZM9.75 9.61723C9.70334 9.65231 9.65858 9.69108 9.61612 9.73355L8.03033 11.3193C7.73744 11.6122 7.26256 11.6122 6.96967 11.3193C6.67678 11.0264 6.67678 10.5516 6.96967 10.2587L8.55546 8.67289C9.6294 7.59895 11.3706 7.59895 12.4445 8.67289L14.0303 10.2587C14.3232 10.5516 14.3232 11.0264 14.0303 11.3193C13.7374 11.6122 13.2626 11.6122 12.9697 11.3193L11.3839 9.73355C11.3414 9.69108 11.2967 9.65231 11.25 9.61723V13.789C11.25 14.2032 10.9142 14.539 10.5 14.539C10.0858 14.539 9.75 14.2032 9.75 13.789V9.61723ZM22.3766 19.7789C21.9361 21.5093 20.3675 22.7891 18.5 22.7891H6.5C8.36748 22.7891 9.93606 21.5093 10.3766 19.7789C10.5128 19.2437 10.9477 18.7891 11.5 18.7891H21.5C22.0523 18.7891 22.5128 19.2437 22.3766 19.7789Z"
-                    fill="#3E63DD"
-                  />
-                </svg>
-                <span style={{ paddingLeft: '6px' }}>Export this app</span>
-              </button>
             </div>
           </div>
         </div>
