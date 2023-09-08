dede3c5afbc0a16bde3920cdf80ecc6acf13dc62
Abd-Rahman-1999 <s.rahmanabd1999@gmail.com>
2023-09-08 16:26:34 +0530

    Fixed suggested changes

--- frontend/src/Editor/LeftSidebar/SidebarPageSelector/RenameInput.jsx
+++ frontend/src/Editor/LeftSidebar/SidebarPageSelector/RenameInput.jsx
@@ -4,11 +4,8 @@ export const RenameInput = ({ page, updaterCallback, updatePageEditMode }) => {
   const handleAddingNewPage = (pageName) => {
     if (pageName.length > 0 && pageName !== page.name) {
       updaterCallback(page.id, pageName);
-      updatePageEditMode(false);
-    } else {
-      //updaterCallback(page.id, pageName);
-      updatePageEditMode(false);
     }
+    updatePageEditMode(false);
 
   };
 
