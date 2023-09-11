9b3a397570eae162380c418717202dc596e73572
stepinfwd <stepinfwd@gmail.com>
2023-09-11 15:11:18 +0530

    fix :: updated canvas bg to old one

--- frontend/src/Editor/Viewer.jsx
+++ frontend/src/Editor/Viewer.jsx
@@ -485,10 +485,10 @@ class ViewerComponent extends React.Component {
     const bgColor =
       (this.state.appDefinition.globalSettings?.backgroundFxQuery ||
         this.state.appDefinition.globalSettings?.canvasBackgroundColor) ??
-      '#181818';
+      '#2f3c4c';
     const resolvedBackgroundColor = resolveReferences(bgColor, this.props.currentState);
-    if (['#181818', '#F2F2F5', '#2f3c4c', '#edeff5'].includes(resolvedBackgroundColor)) {
-      return this.props.darkMode ? '#181818' : '#F2F2F5';
+    if (['#2f3c4c', '#F2F2F5', '#edeff5'].includes(resolvedBackgroundColor)) {
+      return this.props.darkMode ? '#2f3c4c' : '#F2F2F5';
     }
     return resolvedBackgroundColor;
   };
