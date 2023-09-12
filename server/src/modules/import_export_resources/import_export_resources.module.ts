35cd41b61e984e7a835b02b9bf1bbc9185c7b2a4
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2023-09-12 23:25:27 +0530

    remove unrelated changes

--- server/src/modules/import_export_resources/import_export_resources.module.ts
+++ server/src/modules/import_export_resources/import_export_resources.module.ts
@@ -20,7 +20,6 @@ import { AppsService } from '@services/apps.service';
 import { App } from 'src/entities/app.entity';
 import { AppVersion } from 'src/entities/app_version.entity';
 import { AppUser } from 'src/entities/app_user.entity';
-import { PostgrestProxyService } from '@services/postgrest_proxy.service';
 
 const imports = [
   PluginsModule,
@@ -46,7 +45,6 @@ if (process.env.ENABLE_TOOLJET_DB === 'true') {
     PluginsHelper,
     AppsService,
     CredentialsService,
-    PostgrestProxyService,
   ],
 })
 export class ImportExportResourcesModule {}
