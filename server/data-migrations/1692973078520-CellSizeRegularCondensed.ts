b12db2d79c52d45e749110d1fa37ab5b7a699275
Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
2023-09-20 16:27:00 +0530

    Migration fix on app builder (#106)
    * Migration fix on app builder
    
    * Decreased the query count on migration

--- server/data-migrations/1692973078520-CellSizeRegularCondensed.ts
+++ server/data-migrations/1692973078520-CellSizeRegularCondensed.ts
@@ -1,56 +1,69 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { AppVersion } from '../src/entities/app_version.entity';
+import { MigrationProgress } from 'src/helpers/utils.helper';
 
 export class CellSizeRegularCondensed1692973078520 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const entityManager = queryRunner.manager;
-    const queryBuilder = queryRunner.connection.createQueryBuilder();
-    const appVersionRepository = entityManager.getRepository(AppVersion);
+    const appVersionsCount = await entityManager.count(AppVersion);
+    const perQuery = 100;
+    const loopsCount = appVersionsCount / perQuery;
 
-    const appVersions = await appVersionRepository.find();
+    const migrationProgress = new MigrationProgress('CellSizeRegularCondensed1692973078520', appVersionsCount);
 
-    for (const version of appVersions) {
-      const definition = version?.['definition'];
-      if (definition) {
-        const pages = definition?.['pages'];
-        if (pages) {
-          for (const pageId of Object.keys(pages)) {
-            const components = pages?.[pageId]?.['components'];
-            if (components) {
-              for (const componentId of Object.keys(components)) {
-                const component = components[componentId];
-                if (component?.component?.component === 'Table') {
-                  component.component.definition.styles.cellSize = {
-                    value: 'regular',
-                  };
-                  component.component.styles.cellSize = {
-                    ...component.component.styles.cellSize,
-                    options: [
-                      { name: 'Condensed', value: 'condensed' },
-                      { name: 'Regular', value: 'regular' },
-                    ],
-                  };
-                  components[componentId] = {
-                    ...component,
-                    component: {
-                      ...component.component,
-                      definition: {
-                        ...component.component.definition,
+    // Changes for fixing timeout error
+    for (let i = 1; i <= loopsCount; i++) {
+      const skip = perQuery * i - perQuery;
+
+      const appVersions = await entityManager.find(AppVersion, { take: perQuery, skip });
+      for (const version of appVersions) {
+        const definition = version?.['definition'];
+
+        if (definition) {
+          const pages = definition?.['pages'];
+          if (pages) {
+            for (const pageId of Object.keys(pages)) {
+              const components = pages?.[pageId]?.['components'];
+
+              if (components) {
+                for (const componentId of Object.keys(components)) {
+                  const component = components[componentId];
+
+                  if (
+                    component?.component?.component === 'Table' &&
+                    component?.component?.definition?.styles &&
+                    component?.component?.styles
+                  ) {
+                    component.component.definition.styles.cellSize = {
+                      value: 'regular',
+                    };
+                    component.component.styles.cellSize = {
+                      ...component.component.styles.cellSize,
+                      options: [
+                        { name: 'Condensed', value: 'condensed' },
+                        { name: 'Regular', value: 'regular' },
+                      ],
+                    };
+                    components[componentId] = {
+                      ...component,
+                      component: {
+                        ...component.component,
+                        definition: {
+                          ...component.component.definition,
+                        },
                       },
-                    },
-                  };
+                    };
+                  }
                 }
+                pages[pageId]['components'] = components;
               }
-              pages[pageId]['components'] = components;
             }
+            definition['pages'] = pages;
+            version.definition = definition;
           }
+
+          await entityManager.update(AppVersion, { id: version.id }, { definition });
         }
-
-        definition['pages'] = pages;
-
-        version.definition = definition;
-
-        await queryBuilder.update(AppVersion).set({ definition }).where('id = :id', { id: version.id }).execute();
       }
     }
   }
--- server/data-migrations/1692974311591-TableRowCellStyle.ts
+++ server/data-migrations/1692974311591-TableRowCellStyle.ts
@@ -1,58 +1,68 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { AppVersion } from '../src/entities/app_version.entity';
+import { MigrationProgress } from 'src/helpers/utils.helper';
 
 export class TableRowCellStyle1692974311591 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const entityManager = queryRunner.manager;
-    const queryBuilder = queryRunner.connection.createQueryBuilder();
-    const appVersionRepository = entityManager.getRepository(AppVersion);
+    const appVersionsCount = await entityManager.count(AppVersion);
+    const perQuery = 100;
+    const loopsCount = appVersionsCount / perQuery;
 
-    const appVersions = await appVersionRepository.find();
+    const migrationProgress = new MigrationProgress('TableRowCellStyle1692974311591', appVersionsCount);
 
-    for (const version of appVersions) {
-      const definition = version['definition'];
+    // Changes for fixing timeout error
+    for (let i = 1; i <= loopsCount; i++) {
+      const skip = perQuery * i - perQuery;
 
-      if (definition) {
-        const pages = definition['pages'];
-        if (pages) {
-          for (const pageId of Object.keys(pages)) {
-            const components = pages[pageId]['components'];
-            if (components) {
-              for (const componentId of Object.keys(components)) {
-                const component = components[componentId];
-                if (component?.component?.component === 'Table') {
-                  component.component.definition.styles.tableType = {
-                    value: 'table-classic',
-                  };
-                  component.component.styles.tableType = {
-                    ...component.component.styles.tableType,
-                    options: [
-                      { name: 'Bordered', value: 'table-bordered' },
-                      { name: 'Regular', value: 'table-classic' },
-                      { name: 'Striped', value: 'table-striped' },
-                    ],
-                  };
-                  components[componentId] = {
-                    ...component,
-                    component: {
-                      ...component.component,
-                      definition: {
-                        ...component.component.definition,
+      const appVersions = await entityManager.find(AppVersion, { take: perQuery, skip });
+      for (const version of appVersions) {
+        const definition = version['definition'];
+
+        if (definition) {
+          const pages = definition['pages'];
+          if (pages) {
+            for (const pageId of Object.keys(pages)) {
+              const components = pages[pageId]['components'];
+              if (components) {
+                for (const componentId of Object.keys(components)) {
+                  const component = components[componentId];
+                  if (
+                    component?.component?.component === 'Table' &&
+                    component?.component?.definition?.styles &&
+                    component?.component?.styles
+                  ) {
+                    component.component.definition.styles.tableType = {
+                      value: 'table-classic',
+                    };
+                    component.component.styles.tableType = {
+                      ...component.component.styles.tableType,
+                      options: [
+                        { name: 'Bordered', value: 'table-bordered' },
+                        { name: 'Regular', value: 'table-classic' },
+                        { name: 'Striped', value: 'table-striped' },
+                      ],
+                    };
+                    components[componentId] = {
+                      ...component,
+                      component: {
+                        ...component.component,
+                        definition: {
+                          ...component.component.definition,
+                        },
                       },
-                    },
-                  };
+                    };
+                  }
                 }
+                pages[pageId]['components'] = components;
               }
             }
-            pages[pageId]['components'] = components;
+            definition['pages'] = pages;
+            version.definition = definition;
           }
+
+          await entityManager.update(AppVersion, { id: version.id }, { definition });
         }
-
-        definition['pages'] = pages;
-
-        version.definition = definition;
-
-        await queryBuilder.update(AppVersion).set({ definition }).where('id = :id', { id: version.id }).execute();
       }
     }
   }
