db8bc53ef991c178988f1d2909e589b272f2dfe5
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2023-10-09 22:36:34 +0530

    fix createdb script

--- server/scripts/create-database.ts
+++ server/scripts/create-database.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
-import { exec } from 'child_process';
+import { ExecFileSyncOptions, execFileSync } from 'child_process';
 import { buildAndValidateDatabaseConfig } from './database-config-utils';
 import { isEmpty } from 'lodash';
 
-function createDatabaseFromFile(envPath: string): void {
+async function createDatabaseFromFile(envPath: string): Promise<void> {
   const result = dotenv.config({ path: envPath });
 
   if (process.env.PG_DB_OWNER === 'false') {
@@ -17,10 +17,10 @@ function createDatabaseFromFile(envPath: string): void {
     throw result.error;
   }
 
-  createDatabase();
+  await createDatabase();
 }
 
-function createDatabase(): void {
+async function createDatabase(): Promise<void> {
   const { value: envVars, error } = buildAndValidateDatabaseConfig();
 
   if (error) {
@@ -32,88 +32,97 @@ function createDatabase(): void {
     return;
   }
 
-  const connectivityCheck = exec('command -v createdb');
-
-  connectivityCheck.on('exit', function (signal) {
-    if (signal === 1) {
-      console.error('Unable to connect to database');
-      process.exit(1);
-    }
-  });
-
-  // Allow creating db based on cmd line arg
   const dbNameFromArg = process.argv[2];
-  if (dbNameFromArg) return createDb(envVars, dbNameFromArg);
 
-  createDb(envVars, envVars.PG_DB);
-  if (process.env.ENABLE_TOOLJET_DB == 'true') {
-    createTooljetDb(envVars, envVars.TOOLJET_DB);
+  if (dbNameFromArg) {
+    validateDbName(dbNameFromArg);
+    await createDb(envVars, dbNameFromArg);
+  } else {
+    validateDbName(envVars.PG_DB);
+    await createDb(envVars, envVars.PG_DB);
+    if (process.env.ENABLE_TOOLJET_DB === 'true') {
+      validateDbName(envVars.TOOLJET_DB);
+      await createTooljetDb(envVars, envVars.TOOLJET_DB);
+    }
   }
 }
 
-function createDb(envVars, dbName) {
-  if (isEmpty(dbName)) throw 'Database name cannot be empty';
+function checkCommandAvailable(command: string) {
+  try {
+    execFileSync('command', ['-v', command]);
+  } catch (error) {
+    throw `Error: ${command} not found. Make sure it's installed and available in the system's PATH.`;
+  }
+}
 
-  const createdb =
-    `PGPASSWORD="${envVars.PG_PASS}" createdb ` +
-    `-h ${envVars.PG_HOST} ` +
-    `-p ${envVars.PG_PORT} ` +
-    `-U ${envVars.PG_USER} ` +
-    dbName;
+function executeCreateDb(host: string, port: string, user: string, password: string, dbName: string) {
+  const env = Object.assign({}, process.env, { PGPASSWORD: password });
+  const createDbArgs = ['-h', host, '-p', port, '-U', user, dbName];
+  const options = { env, stdio: 'pipe' } as ExecFileSyncOptions;
 
-  exec(createdb, (err, _stdout, _stderr) => {
-    if (!err) {
-      console.log(`Created database ${dbName}\n`);
-      return;
-    }
+  execFileSync('createdb', createDbArgs, options);
+}
 
-    const errorMessage = `database "${dbName}" already exists\n`;
+function validateDbName(dbName: string): void {
+  const dbNameRegex = /^[a-zA-Z0-9_]+$/;
+  if (!dbNameRegex.test(dbName)) {
+    throw new Error('Invalid database name');
+  }
+}
 
-    if (err.message.includes(errorMessage)) {
+async function createDb(envVars, dbName): Promise<void> {
+  if (isEmpty(dbName)) {
+    throw new Error('Database name cannot be empty');
+  }
+
+  try {
+    executeCreateDb(envVars.PG_HOST, envVars.PG_PORT, envVars.PG_USER, envVars.PG_PASS, dbName);
+    console.log(`Created database ${dbName}\n`);
+  } catch (error) {
+    if (error.message.includes(`database "${dbName}" already exists`)) {
       console.log(`Using Application database\nPG_DB: ${dbName}\nPG_HOST: ${envVars.PG_HOST}\n`);
     } else {
-      console.error(err);
-      process.exit(1);
+      throw error;
     }
-  });
+  }
 }
 
-function createTooljetDb(envVars, dbName) {
-  if (isEmpty(dbName)) throw 'Database name cannot be empty';
+async function createTooljetDb(envVars, dbName): Promise<void> {
+  if (isEmpty(dbName)) {
+    throw new Error('Database name cannot be empty');
+  }
 
-  const createdb =
-    `PGPASSWORD="${envVars.TOOLJET_DB_PASS}" createdb ` +
-    `-h ${envVars.TOOLJET_DB_HOST} ` +
-    `-p ${envVars.TOOLJET_DB_PORT} ` +
-    `-U ${envVars.TOOLJET_DB_USER} ` +
-    dbName;
-
-  exec(createdb, (err, _stdout, _stderr) => {
-    if (!err) {
-      console.log(`Created database ${dbName}\n`);
-      return;
-    }
-
-    const errorMessage = `database "${dbName}" already exists\n`;
-
-    if (err.message.includes(errorMessage)) {
+  try {
+    executeCreateDb(
+      envVars.TOOLJET_DB_HOST,
+      envVars.TOOLJET_DB_PORT,
+      envVars.TOOLJET_DB_USER,
+      envVars.TOOLJET_DB_PASS,
+      dbName
+    );
+  } catch (error) {
+    if (error.message.includes(`database "${dbName}" already exists`)) {
       console.log(`Using Tooljet database\nTOOLJET_DB: ${dbName}\nTOOLJET_DB_HOST: ${envVars.TOOLJET_DB_HOST}\n`);
     } else {
-      console.error(_stderr);
-      process.exit(1);
+      throw error;
     }
-  });
+  }
 }
 
-const nodeEnvPath = path.resolve(process.cwd(), process.env.NODE_ENV === 'test' ? '../.env.test' : '../.env');
+try {
+  checkCommandAvailable('dropdb');
+  const nodeEnvPath = path.resolve(process.cwd(), process.env.NODE_ENV === 'test' ? '../.env.test' : '../.env');
+  const fallbackPath = path.resolve(process.cwd(), '../.env');
 
-const fallbackPath = path.resolve(process.cwd(), '../.env');
-
-if (fs.existsSync(nodeEnvPath)) {
-  createDatabaseFromFile(nodeEnvPath);
-} else if (fs.existsSync(fallbackPath)) {
-  createDatabaseFromFile(fallbackPath);
-} else {
-  console.log('Picking up config from the environment');
-  createDatabase();
+  if (fs.existsSync(nodeEnvPath)) {
+    createDatabaseFromFile(nodeEnvPath);
+  } else if (fs.existsSync(fallbackPath)) {
+    createDatabaseFromFile(fallbackPath);
+  } else {
+    console.log('Picking up config from the environment');
+    createDatabase();
+  }
+} catch (error) {
+  console.error(error);
+  process.exit(1);
 }
