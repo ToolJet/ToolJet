dbe7b40c27313e2821421eeed40b08d072698459
arpitnath <arpitnath42@gmail.com>
2023-10-10 20:13:11 +0530

    fixes: multi-components cloning or copy/paste have same name

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -1482,7 +1482,10 @@ export const addComponents = (
 
   pastedComponents.forEach((component) => {
     const newComponentId = uuidv4();
-    const componentName = computeComponentName(component.component.component, appDefinition.pages[pageId].components);
+    const componentName = computeComponentName(component.component.component, {
+      ...appDefinition.pages[pageId].components,
+      ...finalComponents,
+    });
 
     const isParentAlsoCopied = component.component.parent && componentMap[component.component.parent];
 
