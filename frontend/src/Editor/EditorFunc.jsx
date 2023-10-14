bcf8e168950748f5388df99142743d3056363ff9
arpitnath <arpitnath42@gmail.com>
2023-10-14 16:51:14 +0530

    removes extra whitespace

--- frontend/src/Editor/EditorFunc.jsx
+++ frontend/src/Editor/EditorFunc.jsx
@@ -1270,7 +1270,6 @@ const EditorComponent = (props) => {
       switchPage: true,
       pageId: newPageId,
     });
-
     props?.navigate(`/${getWorkspaceId()}/apps/${appId}/${newHandle}`);
   };
 
