5f1b22cafb2a3d8f5564a8dee2ad90e735424f3a
nikcode9 <129772745+nikcode9@users.noreply.github.com>
2023-10-15 00:26:16 +0530

    Update index.js
    updated with some important info

--- docs/src/plugins/devServer/index.js
+++ docs/src/plugins/devServer/index.js
@@ -1,12 +1,18 @@
 module.exports = function (context, options) {
-    return {
-      name: 'dev-server-plugin',
-      configureWebpack(config, isServer, utils) {
+  return {
+    name: 'dev-server-plugin',
+    configureWebpack(config, isServer, utils) {
+      // Check if it's the development server (isServer) or the production build
+      if (isServer) {
+        // Configuration for the development server
         return {
           devServer: {
+            // Open a specific page when the server starts (e.g., '/docs')
             open: '/docs',
           },
         };
-      },
-    };
+      }
+      // For the production build, you can configure other options here if needed
+    },
   };
+};
