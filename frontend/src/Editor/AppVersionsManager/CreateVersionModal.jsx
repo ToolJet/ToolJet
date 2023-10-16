a000a7fdd9eea63c65f9a5436bf5b8ef737bc33a
Arpit <arpitnath42@gmail.com>
2023-10-16 13:13:13 +0530

    [appdef] api endpoint fixes (#7888)
    * fixes: moved fetching app version to v2 api
    
    * fixes: app slug api

--- frontend/src/Editor/AppVersionsManager/CreateVersionModal.jsx
+++ frontend/src/Editor/AppVersionsManager/CreateVersionModal.jsx
@@ -58,7 +58,7 @@ export const CreateVersion = ({
         });
 
         appVersionService
-          .getOne(appId, data.id)
+          .getAppVersionData(appId, data.id)
           .then((data) => {
             setAppDefinitionFromVersion(data);
           })
