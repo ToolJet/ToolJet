2b4d1190ab829908e66bafba58177443ce6d3d86
manishkushare <kushare.manish9@gmail.com>
2023-10-17 14:07:46 +0530

    initial commit

--- frontend/src/Editor/Inspector/Components/Table/Table.jsx
+++ frontend/src/Editor/Inspector/Components/Table/Table.jsx
@@ -17,6 +17,7 @@ import List from '@/ToolJetUI/List/List';
 import { capitalize, has } from 'lodash';
 import NoListItem from './NoListItem';
 import { ProgramaticallyHandleProperties } from './ProgramaticallyHandleProperties';
+import { ColumnPopoverContent } from './ColumnPopover';
 class TableComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -133,41 +134,58 @@ class TableComponent extends React.Component {
   }
 
   columnPopover = (column, index) => {
-    const timeZoneOptions = [
-      { name: 'UTC', value: 'Etc/UTC' },
-      { name: '-12:00', value: 'Etc/GMT+12' },
-      { name: '-11:00', value: 'Etc/GMT+11' },
-      { name: '-10:00', value: 'Pacific/Honolulu' },
-      { name: '-09:00', value: 'America/Anchorage' },
-      { name: '-08:00', value: 'America/Santa_Isabel' },
-      { name: '-07:00', value: 'America/Chihuahua' },
-      { name: '-06:00', value: 'America/Guatemala' },
-      { name: '-05:00', value: 'America/Bogota' },
-      { name: '-04:00', value: 'America/Halifax' },
-      { name: '-03:30', value: 'America/St_Johns' },
-      { name: '-03:00', value: 'America/Sao_Paulo' },
-      { name: '-02:00', value: 'Etc/GMT+2' },
-      { name: '-01:00', value: 'Atlantic/Cape_Verde' },
-      { name: '+00:00', value: 'UTC' },
-      { name: '+01:00', value: 'Europe/Berlin' },
-      { name: '+02:00', value: 'Africa/Gaborone' },
-      { name: '+03:00', value: 'Asia/Baghdad' },
-      { name: '+04:00', value: 'Asia/Muscat' },
-      { name: '+04:30', value: 'Asia/Kabul' },
-      { name: '+05:00', value: 'Asia/Tashkent' },
-      { name: '+05:30', value: 'Asia/Colombo' },
-      { name: '+05:45', value: 'Asia/Kathmandu' },
-      { name: '+06:00', value: 'Asia/Almaty' },
-      { name: '+06:30', value: 'Asia/Yangon' },
-      { name: '+07:00', value: 'Asia/Bangkok' },
-      { name: '+08:00', value: 'Asia/Makassar' },
-      { name: '+09:00', value: 'Asia/Seoul' },
-      { name: '+09:30', value: 'Australia/Darwin' },
-      { name: '+10:00', value: 'Pacific/Chuuk' },
-      { name: '+11:00', value: 'Pacific/Pohnpei' },
-      { name: '+12:00', value: 'Etc/GMT-12' },
-      { name: '+13:00', value: 'Pacific/Auckland' },
-    ];
+    // return (
+    //   <span>
+    //     {''}
+    //     <ColumnPopover
+    //       column={column}
+    //       index={index}
+    //       darkMode={this.props.darkMode}
+    //       currentState={this.state.currentState}
+    //       onColumnItemChange={this.onColumnItemChange}
+    //       getPopoverFieldSource={this.getPopoverFieldSource}
+    //       setColumnPopoverRootCloseBlocker={this.setColumnPopoverRootCloseBlocker}
+    //       component={this.state.component}
+    //       props={this.props}
+    //       columnEventChanged={this.columnEventChanged}
+    //     />
+    //   </span>
+    // )
+    // const timeZoneOptions = [
+    //   { name: 'UTC', value: 'Etc/UTC' },
+    //   { name: '-12:00', value: 'Etc/GMT+12' },
+    //   { name: '-11:00', value: 'Etc/GMT+11' },
+    //   { name: '-10:00', value: 'Pacific/Honolulu' },
+    //   { name: '-09:00', value: 'America/Anchorage' },
+    //   { name: '-08:00', value: 'America/Santa_Isabel' },
+    //   { name: '-07:00', value: 'America/Chihuahua' },
+    //   { name: '-06:00', value: 'America/Guatemala' },
+    //   { name: '-05:00', value: 'America/Bogota' },
+    //   { name: '-04:00', value: 'America/Halifax' },
+    //   { name: '-03:30', value: 'America/St_Johns' },
+    //   { name: '-03:00', value: 'America/Sao_Paulo' },
+    //   { name: '-02:00', value: 'Etc/GMT+2' },
+    //   { name: '-01:00', value: 'Atlantic/Cape_Verde' },
+    //   { name: '+00:00', value: 'UTC' },
+    //   { name: '+01:00', value: 'Europe/Berlin' },
+    //   { name: '+02:00', value: 'Africa/Gaborone' },
+    //   { name: '+03:00', value: 'Asia/Baghdad' },
+    //   { name: '+04:00', value: 'Asia/Muscat' },
+    //   { name: '+04:30', value: 'Asia/Kabul' },
+    //   { name: '+05:00', value: 'Asia/Tashkent' },
+    //   { name: '+05:30', value: 'Asia/Colombo' },
+    //   { name: '+05:45', value: 'Asia/Kathmandu' },
+    //   { name: '+06:00', value: 'Asia/Almaty' },
+    //   { name: '+06:30', value: 'Asia/Yangon' },
+    //   { name: '+07:00', value: 'Asia/Bangkok' },
+    //   { name: '+08:00', value: 'Asia/Makassar' },
+    //   { name: '+09:00', value: 'Asia/Seoul' },
+    //   { name: '+09:30', value: 'Australia/Darwin' },
+    //   { name: '+10:00', value: 'Pacific/Chuuk' },
+    //   { name: '+11:00', value: 'Pacific/Pohnpei' },
+    //   { name: '+12:00', value: 'Etc/GMT-12' },
+    //   { name: '+13:00', value: 'Pacific/Auckland' },
+    // ];
     return (
       <Popover
         id="popover-basic-2"
@@ -177,7 +195,19 @@ class TableComponent extends React.Component {
           overflowY: 'auto',
         }}
       >
-        <Popover.Body>
+        <ColumnPopoverContent
+          column={column}
+          index={index}
+          darkMode={this.props.darkMode}
+          currentState={this.state.currentState}
+          onColumnItemChange={this.onColumnItemChange}
+          getPopoverFieldSource={this.getPopoverFieldSource}
+          setColumnPopoverRootCloseBlocker={this.setColumnPopoverRootCloseBlocker}
+          component={this.state.component}
+          props={this.props}
+          columnEventChanged={this.columnEventChanged}
+        />
+        {/* <Popover.Body>
           <div className="field mb-2" data-cy={`dropdown-column-type`}>
             <label data-cy={`label-column-type`} className="form-label">
               {this.props.t('widget.Table.columnType', 'Column type')}
@@ -746,7 +776,7 @@ class TableComponent extends React.Component {
             paramMeta={{ type: 'toggle', displayName: 'Column visibility' }}
             paramType="properties"
           />
-        </Popover.Body>
+        </Popover.Body> */}
       </Popover>
     );
   };
