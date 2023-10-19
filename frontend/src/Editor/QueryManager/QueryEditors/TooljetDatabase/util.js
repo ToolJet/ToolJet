245b3ab61a5d530da7cfbcd308c78b2016649db2
Abd-Rahman-1999 <s.rahmanabd1999@gmail.com>
2023-10-19 17:41:18 +0530

    Added validation for empty string

--- frontend/src/Editor/QueryManager/QueryEditors/TooljetDatabase/util.js
+++ frontend/src/Editor/QueryManager/QueryEditors/TooljetDatabase/util.js
@@ -28,7 +28,7 @@ export const hasEmptyStringOrNullValue = (queryOptions, operation) => {
     const filterKeys = Object.keys(filters);
     for (let i = 0; i < filterKeys.length; i++) {
       const filter = filters[filterKeys[i]];
-      if (filter.operator !== 'is' && filter.value === null) {
+      if (filter.operator !== 'is' && (filter.value === null || filter.value === '')) {
         return true;
       }
     }
