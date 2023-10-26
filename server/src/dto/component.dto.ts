c48fa4e1c96b7e830d402a903a9cc331df40d32a
arpitnath <arpitnath42@gmail.com>
2023-10-26 19:58:51 +0530

    updates layout dto

--- server/src/dto/component.dto.ts
+++ server/src/dto/component.dto.ts
@@ -1,5 +1,18 @@
 import { Type } from 'class-transformer';
-import { IsArray, IsBoolean, IsNotEmpty, IsNumber, IsObject, IsOptional, IsString, IsUUID } from 'class-validator';
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+  IsUUID,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  Validate,
+} from 'class-validator';
 
 export class ComponentLayoutDto {
   @IsNumber()
@@ -27,6 +40,37 @@ export class LayoutData {
   @IsObject()
   @IsOptional()
   mobile?: ComponentLayoutDto;
+}
+
+@ValidatorConstraint({ name: 'LayoutDataValidator', async: false })
+class LayoutDataValidator implements ValidatorConstraintInterface {
+  validate(value: any) {
+    if (value) {
+      for (const key in value) {
+        if (!value[key] || typeof value[key] !== 'object' || !value[key].layouts) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Each key in "diff" must have the structure { layouts: LayoutData }`;
+  }
+}
+
+export class LayoutUpdateDto {
+  @IsBoolean()
+  is_user_switched_version: boolean;
+
+  @IsUUID()
+  pageId: string;
+
+  @IsObject()
+  @IsNotEmpty()
+  @Validate(LayoutDataValidator, { each: true })
+  diff: Record<string, { layouts: LayoutData }>;
 }
 
 class ComponentDto {
@@ -91,16 +135,4 @@ export class DeleteComponentDto {
   @IsBoolean()
   @IsOptional()
   is_component_cut: boolean;
-}
-
-export class LayoutUpdateDto {
-  @IsBoolean()
-  is_user_switched_version: boolean;
-
-  @IsUUID()
-  pageId: string;
-
-  @IsObject()
-  @IsNotEmpty()
-  diff: Record<string, { layouts: LayoutData }>;
 }
