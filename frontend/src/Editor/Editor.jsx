2d6fba65390dd334041be8233c488c57a42806e7
arpitnath <arpitnath42@gmail.com>
2023-10-30 17:52:39 +0530

    Revert "fixes: trial-5: fixing vanishing of components"
    This reverts commit a22aec12c789d5bd1c9c1e9cea7d9fb1eef87bfb.

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -202,8 +202,9 @@ const EditorComponent = (props) => {
       }
     });
 
-    setCurrentSessionId(() => uuid());
     $componentDidMount();
+
+    setCurrentSessionId(() => uuid());
 
     // 6. Unsubscribe from the observable when the component is unmounted
     return () => {
@@ -224,12 +225,6 @@ const EditorComponent = (props) => {
 
     if (didAppDefinitionChanged) {
       prevAppDefinition.current = appDefinition;
-    }
-
-    if (appDiffOptions?.skipYmapUpdate) {
-      return updateState({
-        isUpdatingEditorStateInProcess: false,
-      });
     }
 
     if (mounted && didAppDefinitionChanged && currentPageId) {
@@ -365,12 +360,6 @@ const EditorComponent = (props) => {
 
       // Check if others are on the same version and page
       if (!ymapUpdates.areOthersOnSameVersionAndPage) return;
-
-      const currentSession = authenticationService.currentSessionValue;
-      const currentUserId = currentSession?.current_user?.id;
-
-      // if (currentUserId === ymapUpdates.currentUserId) return;
-      // console.log('----arpit:::::', { currentUserId });
 
       // Check if the new application definition is different from the current one
       if (isEqual(appDefinition, ymapUpdates.newDefinition)) return;
@@ -879,7 +868,6 @@ const EditorComponent = (props) => {
         editingVersionId: editingVersion?.id,
         currentSessionId,
         areOthersOnSameVersionAndPage,
-        currentUserId: currentUser?.id,
       });
     }
   };
