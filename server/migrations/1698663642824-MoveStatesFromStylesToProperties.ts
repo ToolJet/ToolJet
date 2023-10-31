1064d44f3e7c54a25f85fb29ae9408c1c64cf31a
Nakul Nagargade <nakul@tooljet.com>
2023-10-31 11:47:32 +0530

    migration to move visibility and disabled state from properties to styles in appdefinition

---
+++ server/migrations/1698663642824-MoveStatesFromStylesToProperties.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { AppVersion } from '../src/entities/app_version.entity';
+
+export class MoveStatesFromStylesToProperties1698663642824 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    const appVersions = await entityManager.find(AppVersion, {
+      select: ['id', 'definition'],
+    });
+
+    for (const version of appVersions) {
+      const definition = version['definition'];
+      if (definition) {
+        for (const pageId of Object.keys(definition.pages)) {
+          let components = { ...definition?.pages[pageId]?.components };
+
+          for (const componentId of Object.keys(components)) {
+            const _component = components[componentId];
+            if (_component.component.component === 'Text' || _component.component.component === 'TextInput') {
+              if (_component?.component?.definition?.styles?.visibility) {
+                _component.component.definition.properties.visibility =
+                  _component?.component?.definition.styles.visibility;
+              }
+              if (_component?.component?.definition?.styles?.disabledState) {
+                _component.component.definition.properties.disabledState =
+                  _component.component?.definition.styles.disabledState;
+              }
+              delete _component.component.definition.styles.visibility;
+              delete _component.component.definition.styles.disabledState;
+              components = {
+                ...components,
+                [componentId]: { ..._component },
+              };
+            }
+          }
+          definition.pages[pageId].components = { ...components };
+          version.definition = definition;
+          await entityManager.update(AppVersion, { id: version.id }, { definition });
+        }
+      }
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
