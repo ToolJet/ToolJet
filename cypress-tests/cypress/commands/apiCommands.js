c6d522d56d5345450b1ccb4aa49a41280ddf0330
ajith-k-v <ajith.jaban@gmail.com>
2023-11-09 15:55:27 +0530

    Revert api command changes

--- cypress-tests/cypress/commands/apiCommands.js
+++ cypress-tests/cypress/commands/apiCommands.js
@@ -115,7 +115,7 @@ Cypress.Commands.add(
   "openApp",
   (
     appId = Cypress.env("appId"),
-    componentSelector = '[data-cy="app-name-input"]'
+    componentSelector = "[data-cy='empty-editor-text']"
   ) => {
     cy.window({ log: false }).then((win) => {
       win.localStorage.setItem("walkthroughCompleted", "true");
