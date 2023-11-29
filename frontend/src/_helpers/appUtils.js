8de9883d5ba0a81049d04109d4ce0b44ef948c7c
stepinfwd <stepinfwd@gmail.com>
2023-11-29 17:34:56 +0530

    update as uuid

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -388,7 +388,7 @@ function debounce(func) {
 
   return (...args) => {
     const event = args[1] || {};
-    const eventId = JSON.stringify(event) + '_' + Date.now(); // Append timestamp
+    const eventId = uuidv4();
 
     if (event.debounce === undefined) {
       return func.apply(this, args);
@@ -1207,7 +1207,7 @@ export function computeComponentState(components = {}) {
       if (!components[key]) return;
 
       const { component } = components[key];
-      const componentMeta = _.cloneDeep(componentTypes.find((comp) => component.component === comp.component));
+      const componentMeta = componentTypes.find((comp) => component.component === comp.component);
 
       const existingComponentName = Object.keys(currentComponents).find((comp) => currentComponents[comp].id === key);
       const existingValues = currentComponents[existingComponentName];
@@ -1824,22 +1824,36 @@ export const buildComponentMetaDefinition = (components = {}) => {
   for (const componentId in components) {
     const currentComponentData = components[componentId];
 
-    const componentMeta = _.cloneDeep(
-      componentTypes.find((comp) => currentComponentData.component.component === comp.component)
-    );
+    const componentMeta = componentTypes.find((comp) => currentComponentData.component.component === comp.component);
 
     const mergedDefinition = {
       ...componentMeta.definition,
 
-      properties: _.merge(componentMeta.definition.properties, currentComponentData?.component.definition.properties),
-      styles: _.merge(componentMeta.definition.styles, currentComponentData?.component.definition.styles),
-      generalStyles: _.merge(
-        componentMeta.definition.generalStyles,
-        currentComponentData?.component.definition.generalStyles
-      ),
-      validation: _.merge(componentMeta.definition.validation, currentComponentData?.component.definition.validation),
-      others: _.merge(componentMeta.definition.others, currentComponentData?.component.definition.others),
-      general: _.merge(componentMeta.definition.general, currentComponentData?.component.definition.general),
+      properties: {
+        ...componentMeta.definition.properties,
+        ...currentComponentData?.component.definition.properties,
+      },
+
+      styles: {
+        ...componentMeta.definition.styles,
+        ...currentComponentData?.component.definition.styles,
+      },
+      generalStyles: {
+        ...componentMeta.definition.generalStyles,
+        ...currentComponentData?.component.definition.generalStyles,
+      },
+      validation: {
+        ...componentMeta.definition.validation,
+        ...currentComponentData?.component.definition.validation,
+      },
+      others: {
+        ...componentMeta.definition.others,
+        ...currentComponentData?.component.definition.others,
+      },
+      general: {
+        ...componentMeta.definition.general,
+        ...currentComponentData?.component.definition.general,
+      },
     };
 
     const mergedComponent = {
