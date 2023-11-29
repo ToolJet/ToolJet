b724374650b5d7aef3c3e4d71dd4f5c3165aa1a0
ajith-k-v <ajith.jaban@gmail.com>
2023-11-29 16:18:54 +0530

    Modify test cases

--- cypress-tests/cypress/constants/texts/workspaceConstants.js
+++ cypress-tests/cypress/constants/texts/workspaceConstants.js
@@ -3,7 +3,7 @@ export const workspaceConstantsText = {
         "To resolve a Workspace constant use {{constants.access_token}}",
     emptyStateHeader: "No Workspace constants yet",
     emptyStateText:
-        "Use workspace constants seamlessly in both the app builder and data source connections across ToolJet.",
+        "Use workspace constants seamlessly within both the app builder and data source connections across the platform.",
     addNewConstantButton: "Create new constant",
     addConstatntText: "Add new constant in production ",
     constantCreatedToast: "Constant has been created",
