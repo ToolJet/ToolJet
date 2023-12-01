52ca91f5bbe9f68b99eaaa8cf88636dee79f2f2b
emidhun <midhun752@gmail.com>
2023-12-01 17:24:28 +0530

    Cypress: Add plugin to common  cypress suite

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -19,9 +19,9 @@ module.exports = defineConfig({
   trashAssetsBeforeRuns: true,
 
   e2e: {
-    setupNodeEvents (on, config) {
+    setupNodeEvents(on, config) {
       on("task", {
-        readPdf (pathToPdf) {
+        readPdf(pathToPdf) {
           return new Promise((resolve) => {
             const pdfPath = path.resolve(pathToPdf);
             let dataBuffer = fs.readFileSync(pdfPath);
@@ -33,7 +33,7 @@ module.exports = defineConfig({
       });
 
       on("task", {
-        readXlsx (filePath) {
+        readXlsx(filePath) {
           return new Promise((resolve, reject) => {
             try {
               let dataBuffer = fs.readFileSync(filePath);
@@ -48,7 +48,7 @@ module.exports = defineConfig({
       });
 
       on("task", {
-        deleteFolder (folderName) {
+        deleteFolder(folderName) {
           return new Promise((resolve, reject) => {
             if (fs.existsSync(folderName)) {
               rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
@@ -66,12 +66,17 @@ module.exports = defineConfig({
       });
 
       on("task", {
-        updateId ({ dbconfig, sql }) {
+        updateId({ dbconfig, sql }) {
           const client = new pg.Pool(dbconfig);
           return client.query(sql);
         },
       });
-      return require("./cypress/plugins/index.js")(on, config);
+
+      require("@cypress/code-coverage/task")(on, config);
+      // return config;
+
+      require("./cypress/plugins/index.js")(on, config);
+      return config;
     },
     experimentalRunAllSpecs: true,
     experimentalModfyObstructiveThirdPartyCode: true,
@@ -84,5 +89,7 @@ module.exports = defineConfig({
     experimentalRunAllSpecs: true,
     trashAssetsBeforeRuns: true,
     experimentalMemoryManagement: true,
+    coverage: true,
+    codeCoverageTasksRegistered: true,
   },
 });
