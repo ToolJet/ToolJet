7ef0a5b6a3e08ae003080a30772ab0260759047c
emidhun <midhun752@gmail.com>
2023-12-11 17:36:19 +0530

    Add assertions for visual testing

--- cypress-tests/cypress/e2e/visualTesting/visualTesting.cy.js
+++ cypress-tests/cypress/e2e/visualTesting/visualTesting.cy.js
@@ -4,6 +4,7 @@ import { fake } from "Fixtures/fake";
 import { commonWidgetText } from "Texts/common";
 
 import { verifyControlComponentAction } from "Support/utils/button";
+import { resizeQueryPanel } from "Support/utils/dataSource";
 
 import {
   openAccordion,
@@ -34,18 +35,63 @@ import {
 describe("Editor- Test Button widget", () => {
   beforeEach(() => {
     cy.apiLogin();
-    cy.apiCreateApp(`visualEditor-App1`);
+    cy.apiCreateApp(`visualEditor-App12`);
     cy.openApp();
+    cy.wait(500);
     // cy.dragAndDropWidget(buttonText.defaultWidgetText, 500, 500);
   });
 
   it("should verify position of component after dragging", () => {
     const data = {};
     data.widgetName = buttonText.defaultWidgetName;
-    cy.percySnapshot("Editor");
+    cy.percySnapshot("Editor lite mode");
+    cy.get(
+      '[class="unstyled-button dark-theme-toggle-btn  sidebar-svg-icon  left-sidebar-item "]'
+    ).click();
+    cy.percySnapshot("Editor dark mode");
+
     cy.dragAndDropWidget("table", 300, 100);
     cy.wait(500);
-    cy.percySnapshot("Table");
+    cy.percySnapshot("Table dark mode");
+    cy.get(
+      '[class="unstyled-button dark-theme-toggle-btn  sidebar-svg-icon  left-sidebar-item "]'
+    ).click();
+    cy.percySnapshot("Table lite mode");
+
+    cy.get('[data-cy="left-sidebar-page-button"]').click();
+    cy.wait(500);
+    cy.percySnapshot("Page menu lite mode");
+
+    cy.get(
+      '[class="unstyled-button dark-theme-toggle-btn  sidebar-svg-icon  left-sidebar-item "]'
+    ).click();
+    cy.get('[data-cy="left-sidebar-page-button"]').click();
+    cy.wait(500);
+    cy.percySnapshot("Page menu dark mode");
+
+    // cy.get(
+    //   '[class="unstyled-button dark-theme-toggle-btn  sidebar-svg-icon  left-sidebar-item "]'
+    // ).click();
+    cy.get('[data-cy="left-sidebar-settings-button"]').click();
+    cy.wait(500);
+    cy.percySnapshot("Global settings dark mode");
+
+    cy.get('[data-cy="show-ds-popover-button"]').click();
+    cy.get('[data-cy="ds-rest api"]').click();
+    resizeQueryPanel(100);
+    cy.percySnapshot("RestApi dark mode");
+
+    cy.get('[data-cy="show-ds-popover-button"]').click();
+    cy.get('[data-cy="ds-run javascript code"]').click();
+    cy.percySnapshot("RunJs dark mode");
+
+    cy.get('[data-cy="show-ds-popover-button"]').click();
+    cy.get('[data-cy="ds-run python code"]').click();
+    cy.percySnapshot("RunPy dark mode");
+
+    cy.get('[data-cy="show-ds-popover-button"]').click();
+    cy.get('[data-cy="ds-tooljet database"]').click();
+    cy.percySnapshot("TJDB dark mode");
 
     cy.apiDeleteApp(data.appName);
   });
