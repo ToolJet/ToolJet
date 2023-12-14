911d1521766b5a304cbb09ea0c8ac9c2c8ed26c4
Arpit <arpitnath42@gmail.com>
2023-12-14 15:50:07 +0530

    [Hotfix] Table component column generations issues (#8309)
    * fixes: on column deletion, the array is replaced with prev data instead of latest update from client
    
    * fixes: table crash on viewer editor as there are duplicte columns while create component definition

--- frontend/src/_helpers/appUtils.js
+++ frontend/src/_helpers/appUtils.js
@@ -1862,8 +1862,15 @@ export const buildComponentMetaDefinition = (components = {}) => {
 
     const mergedDefinition = {
       ...componentMeta.definition,
-
-      properties: _.merge(componentMeta.definition.properties, currentComponentData?.component.definition.properties),
+      properties: _.mergeWith(
+        componentMeta.definition.properties,
+        currentComponentData?.component?.definition?.properties,
+        (objValue, srcValue) => {
+          if (currentComponentData?.component?.component === 'Table' && _.isArray(objValue)) {
+            return srcValue;
+          }
+        }
+      ),
       styles: _.merge(componentMeta.definition.styles, currentComponentData?.component.definition.styles),
       generalStyles: _.merge(
         componentMeta.definition.generalStyles,
