f04eb4bf8410edb9c730e95c59557babb9c7dc46
Adish M <adish.madhu@gmail.com>
2023-12-27 15:43:17 +0530

    testing the build command

--- docs/version-remove.sh
+++ docs/version-remove.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Use head to extract the first version
-jq -r '.[0]' versions.json > tmp_versions.json
+jq '[.[0]]' versions.json > tmp_versions.json
 mv tmp_versions.json versions.json
 
 npm i && npm run build
