e795368eafa81c44170ff4f014e0fbc0c3c0987d
Abd-Rahman-1999 <s.rahmanabd1999@gmail.com>
2023-12-27 15:10:06 +0530

    new datatype dropdown design implemented

--- frontend/src/TooljetDatabase/constants.js
+++ frontend/src/TooljetDatabase/constants.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import BigInt from './Icons/Biginteger.svg';
+import Float from './Icons/Float.svg';
+import Integer from './Icons/Integer.svg';
+import CharacterVar from './Icons/Text.svg';
+import Boolean from './Icons/Toggle.svg';
+
 export const dataTypes = [
-  { value: 'character varying', label: 'varchar' },
-  { value: 'integer', label: 'int' },
-  { value: 'bigint', label: 'bigint' },
-  { value: 'double precision', label: 'float' },
-  { value: 'boolean', label: 'boolean' },
+  {
+    name: 'Varying character strings',
+    label: 'varchar',
+    icon: <CharacterVar width="16" height="16" />,
+    value: 'character varying',
+  },
+  { name: 'Integers up to 4 bytes', label: 'int', icon: <Integer width="16" height="16" />, value: 'integer' },
+  { name: 'Integers up to 8 bytes', label: 'bigint', icon: <BigInt width="16" height="16" />, value: 'bigint' },
+  { name: 'Decimal numbers', label: 'float', icon: <Float width="16" height="16" />, value: 'double precision' },
+  { name: 'Boolean True/False', label: 'boolean', icon: <Boolean width="16" height="16" />, value: 'boolean' },
 ];
 
 export const primaryKeydataTypes = [{ value: 'serial', label: 'serial' }];
@@ -22,3 +34,10 @@ export const operators = [
   { value: 'in', label: 'in' },
   { value: 'is', label: 'is' },
 ];
+
+export const formatOptionLabel = ({ label, icon }) => (
+  <div>
+    <span style={{ marginRight: '8px' }}>{icon}</span>
+    {label}
+  </div>
+);
