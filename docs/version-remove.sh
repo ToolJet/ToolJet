0efc3a5bc3078e76d0eeffe41311300d3417c9c6
Adish M <adish.madhu@gmail.com>
2023-12-27 13:51:51 +0530

    adding script file

---
+++ docs/version-remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Display the un-modified file content
+cat docs/versions.json
+
+# Use head to extract the first version
+head -n 1 docs/versions.json > tmp_versions.json
+mv tmp_versions.json docs/versions.json
+
+exec "$@"
