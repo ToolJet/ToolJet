74175050b905f79c9f39a936b8a3a3db2cb3af84
manishkushare <kushare.manish9@gmail.com>
2023-12-28 18:18:03 +0530

    created boolean column type consisting input checkbox working as toggle switch

---
+++ frontend/src/Editor/Components/Table/Boolean.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Boolean = () => {
+  return (
+    // <div className="">
+    <label class="boolean-switch">
+      <input type="checkbox" />
+      <span class="boolean-slider round"></span>
+    </label>
+    // </div>
+  );
+};
