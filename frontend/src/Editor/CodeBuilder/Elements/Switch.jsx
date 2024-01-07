107fed34c9d0649019af5d9d482f73e98e85fa69
stepinfwd <stepinfwd@gmail.com>
2024-01-08 01:54:52 +0530

    switch fixes

--- frontend/src/Editor/CodeBuilder/Elements/Switch.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Switch.jsx
@@ -4,12 +4,23 @@ import React from 'react';
 
 const Switch = ({ value, onChange, cyLabel, meta, paramName, isIcon }) => {
   const options = meta?.options;
-  const defaultValue = value;
+
+  const defaultValue = typeof value == 'boolean' ? (value ? '{{true}}' : '{{false}}') : value; //for booelan values ex: in checkbox and toggle
+
+  function handleOptionChanged(_value) {
+    onChange(_value);
+  }
+
   return (
     <div>
-      <ToggleGroup onValueChange={onChange} defaultValue={defaultValue}>
+      <ToggleGroup onValueChange={handleOptionChanged} defaultValue={defaultValue}>
         {options.map((option) => (
-          <ToggleGroupItem key={option.value} value={option.value} isIcon={isIcon}>
+          <ToggleGroupItem
+            key={`${option.value}-${meta.type}`}
+            value={option.value}
+            isIcon={isIcon}
+            defaultValue={defaultValue}
+          >
             {option.displayName}
           </ToggleGroupItem>
         ))}
