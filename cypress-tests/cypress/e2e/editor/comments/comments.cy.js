ea6871ff966e71ee991f221f537e67e6660a5217
nandinisaha13 <nandinisaha13@gmail.com>
2024-01-09 16:54:30 +0530

    fix for deletion,resolving of comments along with user mentioning

--- cypress-tests/cypress/e2e/editor/comments/comments.cy.js
+++ cypress-tests/cypress/e2e/editor/comments/comments.cy.js
@@ -1,5 +1,13 @@
 import { fake } from "Fixtures/fake";
+import { commonWidgetSelector } from "Selectors/common";
+import {
+    navigateToCreateNewVersionModal
+} from "Support/utils/version";
+import { createNewVersion } from "Support/utils/exportImport";
 describe("Comments", () => {
+    let currentVersion = "";
+    let newVersion = [];
+    let versionFrom = "";
     beforeEach(() => {
         cy.apiLogin();
         cy.apiCreateApp(`${fake.companyName}-App`);
@@ -31,4 +39,61 @@ describe("Comments", () => {
 
     }
     );
+    it("Should verify the resolved comments", () => {
+        cy.get('[data-cy="left-sidebar-comments-button"]').click();
+        cy.get('[data-cy=real-canvas]').click();
+        cy.get('.comment').click();
+        cy.get('textarea').type("name");
+        cy.get('.col-2 > .btn').click();
+        cy.get('.comment-popover > .card-header > .ms-auto>:eq(0)').click();
+        cy.verifyToastMessage(
+            `[class=go3958317564]`,
+            "Thread resolved"
+        );
+        cy.get('[style="padding: 16px 8px; border-radius: 6px;"] > .d-flex > [aria-selected="false"]').click();
+        cy.get('.fw-400').should("be.have", "text", "Total 1 comment");
+        cy.get('.comment-notification-message').should("be.have", "text", "name");
+    })
+    it("Should verify the comment is deleted", () => {
+        cy.get('[data-cy="left-sidebar-comments-button"]').click();
+        cy.get('[data-cy=real-canvas]').click();
+        cy.get('.comment').click();
+        cy.get('textarea').type("name");
+        cy.get('.col-2 > .btn').click();
+        cy.get('.comment-popover > .card-header > .ms-auto>:eq(1)').click();
+        cy.verifyToastMessage(
+            `[class=go3958317564]`,
+            "Thread deleted"
+        );
+    })
+    it("Should verify user can close comments", () => {
+        cy.get('[data-cy="left-sidebar-comments-button"]').click();
+        cy.get('[data-cy=real-canvas]').click();
+        cy.get('.comment-notification-sidebar > :nth-child(1)>:eq(1)').click();
+        cy.notVisible('.comment');
+
+    })
+    it("Should verify comments do not open on new version", () => {
+        cy.get('[data-cy="left-sidebar-comments-button"]').click();
+        cy.get('[data-cy=real-canvas]').click();
+        navigateToCreateNewVersionModal((currentVersion = "v1"));
+        createNewVersion((newVersion = ["v2"]), (versionFrom = "v1"));
+        cy.notVisible('.comment');
+
+    })
+    it("Should verify the mention of the user", () => {
+        cy.get('[data-cy="left-sidebar-comments-button"]').click();
+        cy.get('[data-cy=real-canvas]').click();
+        cy.get('.comment').click();
+        cy.get('textarea').type("@dev");
+        cy.wait(1000);
+        cy.realPress("Enter")
+        cy.get('.col-2 > .btn').click();
+        cy.get('.navbar-brand').click();
+        cy.get('.notification-center-nav-item').click();
+        cy.get('.text-muted > .row > .col').click();
+        cy.openInCurrentTab('.d-block');
+
+    })
+
 });
