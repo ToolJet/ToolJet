36d6e02dcaaa29d998b9f8a35d4ccaafe472357d
Anantshree Chandola <anantshreechandola23@gmail.com>
2024-01-12 12:58:15 +0530

    correct import

--- frontend/src/_hoc/withRouter.jsx
+++ frontend/src/_hoc/withRouter.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import { setWindowTitle } from '@/_helpers/utils';
 
