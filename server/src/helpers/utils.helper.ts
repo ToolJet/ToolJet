82ad26ce0970939b10fed428783e2f3290613b51
Johnson Cherian <johnsonc.dev@gmail.com>
2024-01-16 16:49:26 +0530

    fix: update configured version for layout calc in app import

--- server/src/helpers/utils.helper.ts
+++ server/src/helpers/utils.helper.ts
@@ -239,7 +239,6 @@ export function isTooljetVersionWithNormalizedAppDefinitionSchem(version) {
 /**
  * Checks if a given Tooljet version is compatible with the grid compatibility fix.
  */
-
 export function shouldApplyGridCompatibilityFix(version) {
-  return semver.satisfies(semver.coerce(version), '>= 2.24.0 < 2.27.0');
+  return semver.satisfies(semver.coerce(version), '>= 2.24.0 < 2.27.7');
 }
