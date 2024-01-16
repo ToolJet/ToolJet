7f8c94117afa7554ff6263c6287d52d54907cc7b
Johnson Cherian <johnsonc.dev@gmail.com>
2024-01-16 16:49:26 +0530

    fix: update configured version for layout calc in app import

--- server/src/services/app_import_export.service.ts
+++ server/src/services/app_import_export.service.ts
@@ -60,7 +60,8 @@ const DefaultDataSourceNames: DefaultDataSourceName[] = [
 const DefaultDataSourceKinds: DefaultDataSourceKind[] = ['restapi', 'runjs', 'runpy', 'tooljetdb', 'workflows'];
 
 function resolveGridPositionForComponent(dimension: number, type: string) {
-  const numberOfGrids = type === 'desktop' ? 43 : 12;
+  // const numberOfGrids = type === 'desktop' ? 43 : 12;
+  const numberOfGrids = 43;
   return Math.round((dimension * numberOfGrids) / 100);
 }
 
