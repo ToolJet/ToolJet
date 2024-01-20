9aa61e7f627675c00f6e81c4bf98417bebd9cf4d
Karan Rathod <karan.altcampus@gmail.com>
2024-01-20 13:47:56 +0530

    keep all items in sidebars collapsed

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -323,7 +323,7 @@ const sidebars = {
                       'azureAD',
                     ],
                   },
-                  'collapsed': false,
+                  'collapsed': true,
                   'items': [
                     'user-authentication/sso/openid/setup',
                     'user-authentication/sso/openid/azuread',
