a9a329fc717a1b918d78b9b398fe33ea193ceb42
stepinfwd <stepinfwd@gmail.com>
2024-01-25 14:39:59 +0530

    test slider

--- frontend/src/Editor/CodeBuilder/Elements/Slider.jsx
+++ frontend/src/Editor/CodeBuilder/Elements/Slider.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomInput from '@/_ui/CustomInput';
-import debounce from 'lodash/debounce';
+import throttle from 'lodash/throttle';
 
 function Slider1({ value, onChange, component }) {
   const [sliderValue, setSliderValue] = useState(value ? value : 33); // Initial value of the slider
@@ -10,13 +10,20 @@ function Slider1({ value, onChange, component }) {
     onChange(`{{${event.target.value}}}`);
   };
 
-  // Debounce function to handle input changes
-  const onInputChange = debounce((e) => {
+  // Throttle function to handle input changes
+  const onInputChange = throttle((e) => {
     let inputValue = parseInt(e.target.value, 10) || 0;
     inputValue = Math.min(inputValue, 100);
     setSliderValue(inputValue);
     onChange(`{{${inputValue}}}`);
   }, 300);
+
+  useEffect(() => {
+    return () => {
+      // Clear the throttle timeout when the component unmounts
+      onInputChange.cancel();
+    };
+  }, [onInputChange]);
 
   return (
     <div className="d-flex flex-column" style={{ width: '142px' }}>
