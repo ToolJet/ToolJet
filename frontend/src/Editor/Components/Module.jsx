f606dc049837e9be1ebd3a5bc0937dcf747fc28d
Sherfin Shamsudeen <sherfin94@gmail.com>
2024-01-31 18:23:49 +0530

    Create module in dashboard

--- frontend/src/Editor/Components/Module.jsx
+++ frontend/src/Editor/Components/Module.jsx
@@ -1,21 +1,24 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useMemo } from 'react';
 import { Viewer } from '../Viewer';
 import { ModuleContext } from '../../_contexts/ModuleContext';
 import { useSuperStore } from '../../_stores/superStore';
+import { v4 as uuidv4 } from 'uuid';
 
-export const Module = function Module({ component, width, id, removeComponent, containerProps }) {
+export const Module = function Module({ component, width, id, parentId, removeComponent, containerProps }) {
   const parentRef = useRef(null);
 
   const [created, setCreated] = useState(false);
   const [createModule, destroyModule] = useSuperStore((state) => [state.createModule, state.destroyModule]);
 
+  const moduleId = useMemo(() => `Module${uuidv4()}`, []);
+
   useEffect(() => {
-    const status = createModule(`Module${id}`);
+    const status = createModule(moduleId);
     console.log({ status });
     setCreated(status);
 
     return () => {
-      destroyModule(`Module${id}`);
+      destroyModule(moduleId);
       setCreated(false);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -24,12 +27,12 @@ export const Module = function Module({ component, width, id, removeComponent, c
   return (
     <div ref={parentRef}>
       {created && (
-        <ModuleContext.Provider value={`Module${id}`}>
+        <ModuleContext.Provider value={moduleId}>
           <Viewer
             moduleMode={true}
-            id={'aa0d725c-5ab3-4d7a-b277-397111ad0f0b'}
-            slug={'aa0d725c-5ab3-4d7a-b277-397111ad0f0b'}
-            versionId={'0063c7b6-a72f-490e-a147-ee5c84235f8b'}
+            id={'e87652b4-478f-4622-a893-733bfd433799'}
+            slug={'e87652b4-478f-4622-a893-733bfd433799'}
+            versionId={'98a4044a-6667-43d3-bdb3-1cdbac6e1998'}
           />
         </ModuleContext.Provider>
       )}
