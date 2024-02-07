36b1e1832fb1c85d7f93b743f3f4953f16d86623
Nakul Nagargade <nakul@tooljet.com>
2024-02-07 17:35:20 +0530

    Remove unnecassary comments

--- server/data-migrations/1706080528978-MoveVisibilityDisabledStatesToProperties.ts
+++ server/data-migrations/1706080528978-MoveVisibilityDisabledStatesToProperties.ts
@@ -5,10 +5,8 @@ import { EntityManager, In, MigrationInterface, QueryRunner } from 'typeorm';
 export class MoveVisibilityDisabledStatesToProperties1706080528992 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Array of component types to be enhanced
     const componentTypes = ['TextInput', 'NumberInput', 'PasswordInput', 'Text', 'DropDown'];
-    const batchSize = 100; // Number of apps to migrate at a time
-    // Obtaining the TypeORM EntityManager from the QueryRunner
+    const batchSize = 100;
     const entityManager = queryRunner.manager;
 
     for (const componentType of componentTypes) {
@@ -16,8 +14,8 @@ export class MoveVisibilityDisabledStatesToProperties1706080528992 implements Mi
         entityManager,
         async (entityManager: EntityManager) => {
           return await entityManager.find(Component, {
-            where: { type: componentType }, // Filtering by component types
-            order: { createdAt: 'ASC' }, // Ordering components by creation date in ascending order
+            where: { type: componentType },
+            order: { createdAt: 'ASC' },
           });
         },
         async (entityManager: EntityManager, components: Component[]) => {
@@ -30,30 +28,25 @@ export class MoveVisibilityDisabledStatesToProperties1706080528992 implements Mi
 
   private async processUpdates(entityManager, components) {
     for (const component of components) {
-      // Extracting properties and styles from the component
       const properties = component.properties;
       const styles = component.styles;
       const general = component.general;
 
-      // Moving 'visibility' from styles to properties
       if (styles.visibility) {
         properties.visibility = styles.visibility;
-        delete styles.visibility; // Removing 'visibility' from styles
+        delete styles.visibility;
       }
 
-      // Moving 'disabledState' from styles to properties
       if (styles.disabledState) {
         properties.disabledState = styles.disabledState;
-        delete styles.disabledState; // Removing 'disabledState' from styles
+        delete styles.disabledState;
       }
 
-      // Moving 'tooltip' from general to properties
       if (general?.tooltip) {
         properties.tooltip = general?.tooltip;
-        delete general?.tooltip; // Removing 'tooltip' from general
+        delete general?.tooltip;
       }
 
-      // Updating the component in the database with the modified properties and styles
       await entityManager.update(Component, component.id, { properties, styles, general });
     }
 
