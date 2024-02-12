d69a217f41b11745b4e6d619a4c59ee5723c30dd
Anantshree Chandola <anantshreechandola23@gmail.com>
2024-02-12 16:37:14 +0530

    small fix

--- frontend/src/_components/OrganizationLogin/GoogleSsoModal.jsx
+++ frontend/src/_components/OrganizationLogin/GoogleSsoModal.jsx
@@ -17,7 +17,6 @@ export function GoogleSSOModal({ settings, onClose, changeStatus }) {
   const [hasChanges, setHasChanges] = useState(false);
   const { t } = useTranslation();
 
-
   useEffect(() => {
     setSettings(settings);
     setEnabled(settings?.enabled || false);
@@ -37,12 +36,12 @@ export function GoogleSSOModal({ settings, onClose, changeStatus }) {
     const newEnabledStatus = !enabled;
     setEnabled(newEnabledStatus);
     changeStatus('google');
+  };
 
   const reset = () => {
     setClientId(settings?.configs?.client_id || '');
     setHasChanges(false);
   };
-};
 
   const copyFunction = (input) => {
     let text = document.getElementById(input).innerHTML;
@@ -70,46 +69,45 @@ export function GoogleSSOModal({ settings, onClose, changeStatus }) {
   };
 
   // GoogleHeader Component
-function GoogleHeader() {
+  function GoogleHeader() {
     const { t } = useTranslation();
     return (
-          <div className="d-flex justify-content-between title-with-toggle" style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '100%',
-            marginBottom: '0px',
-            height: '42px'
-          }}>
-            <div>
-            <label className="switch">
-              <input 
-                type="checkbox" 
-                checked={enabled}
-                onChange={onToggleChange}
-              />
-              <span className="slider round"></span>
-            </label>
-                <span className="sso-type-header" data-cy="card-title" style={{ marginBottom: '0px', fontWeight: '500' }}>
-                  {t('header.organization.menus.manageSSO.google.title', 'Google')}
-                </span>
-            </div>
-            <div className="card-title" style={{marginBottom: '0px'}}>
-              <span className={`tj-text-xsm ${enabled ? 'enabled-tag' : 'disabled-tag'}`} data-cy="status-label">
-                {enabled ? t('header.organization.menus.manageSSO.google.enabled', 'Enabled') : t('header.organization.menus.manageSSO.google.disabled', 'Disabled')}
-              </span>
-            </div>
-          </div>
+      <div
+        className="d-flex justify-content-between title-with-toggle"
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+          marginBottom: '0px',
+          height: '42px',
+        }}
+      >
+        <div>
+          <label className="switch">
+            <input type="checkbox" checked={enabled} onChange={onToggleChange} />
+            <span className="slider round"></span>
+          </label>
+          <span className="sso-type-header" data-cy="card-title" style={{ marginBottom: '0px', fontWeight: '500' }}>
+            {t('header.organization.menus.manageSSO.google.title', 'Google')}
+          </span>
+        </div>
+        <div className="card-title" style={{ marginBottom: '0px' }}>
+          <span className={`tj-text-xsm ${enabled ? 'enabled-tag' : 'disabled-tag'}`} data-cy="status-label">
+            {enabled
+              ? t('header.organization.menus.manageSSO.google.enabled', 'Enabled')
+              : t('header.organization.menus.manageSSO.google.disabled', 'Disabled')}
+          </span>
+        </div>
+      </div>
     );
   }
 
-// GoogleFooter Component
-function GoogleFooter() {
+  // GoogleFooter Component
+  function GoogleFooter() {
     const { t } = useTranslation();
     return (
-      <div className="form-footer sso-card-footer" style={{display: 'flex',
-        gap: '10px',
-        marginTop: '0.5rem'}}>
+      <div className="form-footer sso-card-footer" style={{ display: 'flex', gap: '10px', marginTop: '0.5rem' }}>
         <ButtonSolid onClick={reset} data-cy="cancel-button" variant="tertiary" className="sso-footer-cancel-btn">
           {t('globals.cancel', 'Cancel')}
         </ButtonSolid>
@@ -131,11 +129,11 @@ function GoogleFooter() {
   }
 
   const renderModalTitle = () => {
-        return <GoogleHeader />;
-    }
+    return <GoogleHeader />;
+  };
 
   const renderFooterContent = () => {
-      return <GoogleFooter />;
+    return <GoogleFooter />;
   };
 
   return (
@@ -147,47 +145,47 @@ function GoogleFooter() {
           title={renderModalTitle()}
           footerContent={renderFooterContent()}
           customClassName="modal-custom-height"
-          size='lg'
+          size="lg"
           closeButton={false}
         >
           {
-      <div className="sso-card-wrapper">
-        <div className="card-body">
-          <form noValidate className="sso-form-wrap">
-            <div className="form-group mb-3">
-              <label className="form-label" data-cy="client-id-label">
-                {t('header.organization.menus.manageSSO.google.clientId', 'Client Id')}
-              </label>
-              <div className="tj-app-input">
-                <input
-                  type="text "
-                  className="form-control"
-                  placeholder={t('header.organization.menus.manageSSO.google.enterClientId', 'Enter Client Id')}
-                  value={clientId}
-                  onChange={(e) => handleClientIdChange(e.target.value)}
-                  data-cy="client-id-input"
-                />
+            <div className="sso-card-wrapper">
+              <div className="card-body">
+                <form noValidate className="sso-form-wrap">
+                  <div className="form-group mb-3">
+                    <label className="form-label" data-cy="client-id-label">
+                      {t('header.organization.menus.manageSSO.google.clientId', 'Client Id')}
+                    </label>
+                    <div className="tj-app-input">
+                      <input
+                        type="text "
+                        className="form-control"
+                        placeholder={t('header.organization.menus.manageSSO.google.enterClientId', 'Enter Client Id')}
+                        value={clientId}
+                        onChange={(e) => handleClientIdChange(e.target.value)}
+                        data-cy="client-id-input"
+                      />
+                    </div>
+                  </div>
+                  {configId && (
+                    <div className="form-group mb-3">
+                      <label className="form-label" data-cy="redirect-url-label">
+                        {t('header.organization.menus.manageSSO.google.redirectUrl', 'Redirect URL')}
+                      </label>
+                      <div className="d-flex justify-content-between form-control align-items-center">
+                        <p data-cy="redirect-url" id="redirect-url">{`${window.public_config?.TOOLJET_HOST}${
+                          window.public_config?.SUB_PATH ? window.public_config?.SUB_PATH : '/'
+                        }sso/google/${configId}`}</p>
+                        <SolidIcon name="copy" width="16" onClick={() => copyFunction('redirect-url')} />
+                      </div>
+                    </div>
+                  )}
+                </form>
               </div>
             </div>
-            {configId && (
-              <div className="form-group mb-3">
-                <label className="form-label" data-cy="redirect-url-label">
-                  {t('header.organization.menus.manageSSO.google.redirectUrl', 'Redirect URL')}
-                </label>
-                <div className="d-flex justify-content-between form-control align-items-center">
-                  <p data-cy="redirect-url" id="redirect-url">{`${window.public_config?.TOOLJET_HOST}${
-                    window.public_config?.SUB_PATH ? window.public_config?.SUB_PATH : '/'
-                  }sso/google/${configId}`}</p>
-                  <SolidIcon name="copy" width="16" onClick={() => copyFunction('redirect-url')} />
-                </div>
-              </div>
-            )}
-          </form>
-        </div>
-      </div>
-    }
+          }
         </Modal>
       )}
     </div>
   );
-};
+}
