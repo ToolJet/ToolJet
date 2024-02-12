4272931af84dc7a69d776961a23dbf35d1437d1b
arpitnath <arpitnath42@gmail.com>
2024-02-12 15:10:12 +0530

    Implemented DFS traversal to update references throughout the editor efficiently. DFS was chosen over BFS due to its ability to traverse deeply nested structures effectively. This approach ensures that all references are updated accurately. Time complexity analysis shows that DFS performs efficiently for our use case. Ready to push changes.

--- frontend/src/_stores/resolverStore.js
+++ frontend/src/_stores/resolverStore.js
@@ -1,14 +1,42 @@
 import { create } from 'zustand';
 import { createReferencesLookup } from './utils';
 import { createJavaScriptSuggestions } from '../Editor/CodeEditor/utils';
+import { v4 as uuid } from 'uuid';
 
-/**
- * *Resolver Store
- * This Zustand store is named 'ResolverStore' and it's designed for a dual purpose:
- * 1. Initially, it will be used to manage and store autocomplete suggestions for UI components.
- * 2. In the later stages of development, this store will evolve to serve as a resolver mapper,
- *    responsible for resolving component, queries names to their respective IDs.
- */
+class ReferencesBiMap {
+  constructor() {
+    this._map = new Map();
+    this._reverseMap = new Map();
+  }
+
+  set(key, value) {
+    const lookupId = uuid();
+
+    this._map.set(key, lookupId);
+    this._reverseMap.set(lookupId, value);
+  }
+
+  get(key) {
+    const lookupid = this._map.get(key);
+
+    return this._reverseMap.get(lookupid);
+  }
+
+  reverseGet(key) {
+    return this._reverseMap.get(key);
+  }
+
+  update(id, value) {
+    const lookUpId = this._map.get(id);
+    this._reverseMap.set(lookUpId, value);
+  }
+
+  delete(key) {
+    const value = this._map.get(key);
+    this._map.delete(key);
+    this._reverseMap.delete(value);
+  }
+}
 
 const initialState = {
   suggestions: {
@@ -19,6 +47,8 @@ const initialState = {
     hints: {},
     resolvedRefs: {},
   },
+
+  referenceMapper: new ReferencesBiMap(),
 };
 
 export const useResolveStore = create(
@@ -36,6 +66,30 @@ export const useResolveStore = create(
       updateJSHints: () => {
         const hints = createJavaScriptSuggestions();
         set(() => ({ suggestions: { ...get().suggestions, jsHints: hints } }));
+      },
+
+      updateComponentDefaultValues: (componentDefaultValues) => {
+        set(() => ({ componentDefaultValues }));
+      },
+
+      getDefaultComponentValue: (componentName) => {
+        const { componentDefaultValues } = get();
+        return componentDefaultValues[componentName];
+      },
+
+      addComponentsToMapper: (components) => {
+        const { referenceMapper } = get();
+        components.forEach((component) => {
+          // check if the component is already in the mapper with component id
+
+          if (!referenceMapper.get(component.id)) {
+            referenceMapper.set(component.id, component.name);
+          }
+        });
+      },
+
+      getReferenceMapper: () => {
+        return get().referenceMapper;
       },
     },
   }),
