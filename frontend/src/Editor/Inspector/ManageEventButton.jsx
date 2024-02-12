a0c9011e6b430f930254f15796a9087a27a0d264
Nakul Nagargade <133095394+nakulnagargade@users.noreply.github.com>
2024-02-12 10:04:54 +0530

    Add loader when deleting a event in inspector (#8741)

--- frontend/src/Editor/Inspector/ManageEventButton.jsx
+++ frontend/src/Editor/Inspector/ManageEventButton.jsx
@@ -13,9 +13,9 @@ const ManageEventButton = ({
   removeHandler,
   actionsUpdatedLoader,
   eventsUpdatedLoader,
+  eventsDeletedLoader,
 }) => {
   const [isHovered, setIsHovered] = useState(false);
-
   return (
     <div style={{ marginBottom: '8px' }}>
       <div
@@ -23,60 +23,67 @@ const ManageEventButton = ({
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div data-cy="event-handler-card" className="d-flex">
-          <span className="d-flex align-items-center px-2">
-            <SortableList.DragHandle show />
-          </span>
-          <div
-            className="d-flex justify-content-between"
-            role="button"
-            style={{ padding: '6px 12px 6px 8px', width: '100%' }}
-          >
-            <div className="text-truncate event-handler-text" data-cy="event-handler">
-              {!eventsUpdatedLoader ? (
-                eventDisplayName
+        {eventsDeletedLoader ? (
+          <div className="d-flex justify-content-center p-2">
+            {' '}
+            <Spinner style={{ width: '16px', height: '16px', color: 'var(--indigo9)' }} />
+          </div>
+        ) : (
+          <div data-cy="event-handler-card" className="d-flex">
+            <span className="d-flex align-items-center px-2">
+              <SortableList.DragHandle show />
+            </span>
+            <div
+              className="d-flex justify-content-between"
+              role="button"
+              style={{ padding: '6px 12px 6px 8px', width: '100%' }}
+            >
+              <div className="text-truncate event-handler-text" data-cy="event-handler">
+                {!eventsUpdatedLoader ? (
+                  eventDisplayName
+                ) : (
+                  <Spinner style={{ width: '16px', height: '16px', color: 'var(--indigo9)' }} />
+                )}
+              </div>
+              {!actionsUpdatedLoader ? (
+                <div className="text-truncate event-name-text" data-cy="event-name">
+                  <small className="event-action font-weight-light text-truncate">
+                    {actionName ? actionName : 'Select action'}
+                  </small>
+                  {!actionName && <AddRectangle width={13.33} />}
+                  <span>
+                    <span>
+                      {isHovered && (
+                        <ButtonSolid
+                          variant="tertiary"
+                          size="xs"
+                          className={'list-menu-option-btn'}
+                          onClick={(event) => {
+                            event.stopPropagation();
+                          }}
+                        >
+                          <div
+                            className="list-item-popover-option"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              removeHandler(index);
+                            }}
+                          >
+                            <div className="d-flex align-center">
+                              <Trash fill={'#E54D2E'} width={12} />
+                            </div>
+                          </div>
+                        </ButtonSolid>
+                      )}
+                    </span>
+                  </span>
+                </div>
               ) : (
                 <Spinner style={{ width: '16px', height: '16px', color: 'var(--indigo9)' }} />
               )}
             </div>
-            {!actionsUpdatedLoader ? (
-              <div className="text-truncate event-name-text" data-cy="event-name">
-                <small className="event-action font-weight-light text-truncate">
-                  {actionName ? actionName : 'Select action'}
-                </small>
-                {!actionName && <AddRectangle width={13.33} />}
-                <span>
-                  <span>
-                    {isHovered && (
-                      <ButtonSolid
-                        variant="tertiary"
-                        size="xs"
-                        className={'list-menu-option-btn'}
-                        onClick={(event) => {
-                          event.stopPropagation();
-                        }}
-                      >
-                        <div
-                          className="list-item-popover-option"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            removeHandler(index);
-                          }}
-                        >
-                          <div className="d-flex align-center">
-                            <Trash fill={'#E54D2E'} width={12} />
-                          </div>
-                        </div>
-                      </ButtonSolid>
-                    )}
-                  </span>
-                </span>
-              </div>
-            ) : (
-              <Spinner style={{ width: '16px', height: '16px', color: 'var(--indigo9)' }} />
-            )}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
