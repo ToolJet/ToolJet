beba3f1cb190ee5af9ec8dbab1b224a5e7bab34a
Midhun G S <gsmithun4@gmail.com>
2024-02-20 18:56:06 +0530

    lint fix (#1040)

--- frontend/src/WorkflowEditor/BottomToolBar/ReactFlow/CustomButtons.jsx
+++ frontend/src/WorkflowEditor/BottomToolBar/ReactFlow/CustomButtons.jsx
@@ -1,49 +1,46 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import { useReactFlow, Panel } from 'reactflow';
-import ZoomIn from '../../BottomToolBar/icons/zoomin.svg'
-import ZoomOut from '../../BottomToolBar/icons/zoomout.svg'
-import FitView from '../../BottomToolBar/icons/fitview.svg'
+import ZoomIn from '../../BottomToolBar/icons/zoomin.svg';
+import ZoomOut from '../../BottomToolBar/icons/zoomout.svg';
+import FitView from '../../BottomToolBar/icons/fitview.svg';
 import LockIcon from './LockIcon';
 import UnlockIcon from './UnlockIcon';
 import { useToggleInteractivity } from '../../utils';
 
-
 function CustomButtons() {
+  const { zoomIn, zoomOut, fitView } = useReactFlow();
+  const [isInteractive, setIsInteractive] = useState(false);
+  const onToggleInteractivity = useToggleInteractivity();
 
-    const { zoomIn, zoomOut, fitView } = useReactFlow();
-    const [isInteractive, setIsInteractive] = useState(false);
-    const onToggleInteractivity = useToggleInteractivity();
+  const handleInteractivity = () => {
+    onToggleInteractivity();
+    setIsInteractive(!isInteractive);
+  };
 
-    const handleInteractivity = () => {
-      onToggleInteractivity()
-        setIsInteractive(!isInteractive)
-    }
-
-    const panelStyle = {
-        display : 'flex',
-        alignItems:'center',
-        justifyContent:'center'
-      };
-
+  const panelStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
 
   return (
     <Panel position="top-left">
-      <div className='custom-react-flow-btns' style={panelStyle}>
-        <button onClick={zoomIn} className='controlBtnRf'>
-          <ZoomIn/>
+      <div className="custom-react-flow-btns" style={panelStyle}>
+        <button onClick={zoomIn} className="controlBtnRf">
+          <ZoomIn />
         </button>
-        <button onClick={zoomOut} className='controlBtnRf'>
-          <ZoomOut/>
+        <button onClick={zoomOut} className="controlBtnRf">
+          <ZoomOut />
         </button>
-        <button onClick={fitView} className='controlBtnRf'>
-          <FitView/>
+        <button onClick={fitView} className="controlBtnRf">
+          <FitView />
         </button>
-        <button onClick={handleInteractivity} className='controlBtnRf'>
-        {isInteractive ? <LockIcon /> : <UnlockIcon />}
+        <button onClick={handleInteractivity} className="controlBtnRf">
+          {isInteractive ? <LockIcon /> : <UnlockIcon />}
         </button>
       </div>
     </Panel>
-  )
+  );
 }
 
-export default CustomButtons
+export default CustomButtons;
