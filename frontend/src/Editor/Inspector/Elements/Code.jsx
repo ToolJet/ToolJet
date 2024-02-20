01879b7a127bbc5954a79e984816ab029a5694b5
arpitnath <arpitnath42@gmail.com>
2024-02-20 17:57:54 +0530

    refactor

--- frontend/src/Editor/Inspector/Elements/Code.jsx
+++ frontend/src/Editor/Inspector/Elements/Code.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
-import { resolveReferences } from '@/_helpers/utils';
 import { useCurrentState } from '@/_stores/currentStateStore';
 import CodeEditor from '@/Editor/CodeEditor';
 import { useResolverStoreActions } from '@/_stores/resolverStore';
+import { getDefinitionInitialValue } from './utils';
 
 const CLIENT_SERVER_TOGGLE_FIELDS = ['serverSidePagination', 'serverSideSort', 'serverSideFilter'];
 
@@ -23,64 +23,12 @@ export const Code = ({
 }) => {
   const currentState = useCurrentState();
 
-  const getDefinitionForNewProps = (param) => {
-    if (param === 'enablePagination') {
-      const clientSidePagination = component?.component?.definition?.properties?.clientSidePagination?.value ?? false;
-      const serverSidePagination = component?.component?.definition?.properties?.serverSidePagination?.value ?? false;
-      const isPaginationEnabled =
-        resolveReferences(clientSidePagination, currentState) || resolveReferences(serverSidePagination, currentState);
+  let initialValue = !_.isEmpty(definition)
+    ? definition.value
+    : getDefinitionInitialValue(paramType, param.name, component, currentState, definition.value);
 
-      if (isPaginationEnabled) return '{{true}}';
-      return '{{false}}';
-    }
-    // Following condition is needed to support older Text component not having textFormat switch
-    if (component?.component?.component === 'Text' && param === 'textFormat') {
-      const doTextFormatAlreadyExist = component?.component?.definition?.properties?.textFormat;
-      if (!doTextFormatAlreadyExist) {
-        return 'html';
-      }
-    } else if (['showAddNewRowButton', 'allowSelection', 'defaultSelectedRow'].includes(param)) {
-      if (param === 'allowSelection') {
-        const highlightSelectedRow = component?.component?.definition?.properties?.highlightSelectedRow?.value ?? false;
-        const showBulkSelector = component?.component?.definition?.properties?.showBulkSelector?.value ?? false;
-        const allowSelection =
-          resolveReferences(highlightSelectedRow, currentState) || resolveReferences(showBulkSelector, currentState);
-
-        return '{{' + `${allowSelection}` + '}}';
-      } else if (param === 'defaultSelectedRow') {
-        return `{{{id:1}}}`;
-      } else {
-        return '{{true}}';
-      }
-    } else if (param === 'selectRowOnCellEdit') {
-      const selectRowOnCellEdit =
-        component?.component?.definition?.properties?.selectRowOnCellEdit?.value ?? '{{true}}';
-      return selectRowOnCellEdit;
-    }
-    return '';
-  };
-
-  let initialValue = !_.isEmpty(definition) ? definition.value : getDefinitionForNewProps(param.name);
   const paramMeta = accordian ? componentMeta[paramType]?.[param.name] : componentMeta[paramType][param.name];
   const displayName = paramMeta.displayName || param.name;
-
-  /*
-    following block is written for cellSize Prop to support backward compatibility, 
-    because from older app we also get cellSize value as compact or spacious, 
-    so accordigly we update the initial value with the new values respectively
-  */
-  if (paramType === 'styles' && param.name === 'cellSize') {
-    switch (initialValue) {
-      case 'compact':
-        initialValue = 'condensed';
-        break;
-      case 'spacious':
-        initialValue = 'regular';
-        break;
-      default:
-        break;
-    }
-  }
 
   function handleCodeChanged(value) {
     onChange(param, 'value', value, paramType);
