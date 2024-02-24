becec4af7f293c0a3d999c20de50ffb83f48c770
Muhsin Shah <muhsinshah21@gmail.com>
2024-02-24 20:30:51 +0530

    admin route state timing issue

--- frontend/src/Routes/AdminRoute.jsx
+++ frontend/src/Routes/AdminRoute.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouteLoader } from './RouteLoader';
 import { useSessionManagement } from '@/_hooks/useSessionManagement';
 
 export const AdminRoute = ({ children, navigate }) => {
-  const { isLoading, session } = useSessionManagement();
+  const { isLoading, isValidSession, session, setLoading } = useSessionManagement({
+    disableValidSessionCallback: true,
+  });
   const { admin } = session;
 
-  if (admin === false) {
-    return navigate(
-      {
-        pathname: '/',
-        state: { from: location },
-      },
-      { replace: true }
-    );
-  }
+  useEffect(() => {
+    if (isValidSession && admin !== null) {
+      if (!admin) {
+        return navigate(
+          {
+            pathname: '/',
+            state: { from: location },
+          },
+          { replace: true }
+        );
+      }
+      setLoading(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isValidSession, admin]);
 
   return <RouteLoader isLoading={isLoading}>{children}</RouteLoader>;
 };
