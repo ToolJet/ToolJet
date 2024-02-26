8225884ba89e36a10dfa5760d93a8550e6816b31
stepinfwd <stepinfwd@gmail.com>
2024-02-26 12:10:44 +0530

    fix

--- frontend/src/Editor/Components/NumberInput.jsx
+++ frontend/src/Editor/Components/NumberInput.jsx
@@ -163,7 +163,7 @@ export const NumberInput = function NumberInput({
   }, [isValid]);
 
   const computedStyles = {
-    height: height == 36 ? (padding == 'default' ? '36px' : '38px') : padding == 'default' ? height : height + 2,
+    height: height == 36 ? (padding == 'default' ? '36px' : '40px') : padding == 'default' ? height : height + 4,
     borderRadius: `${borderRadius}px`,
     color: darkMode && textColor === '#11181C' ? '#ECEDEE' : textColor,
     borderColor: isFocused
