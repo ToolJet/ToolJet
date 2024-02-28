90f0dbd741ba5c80940a783a917cc6ba1b3910f0
Nakul Nagargade <nakul@tooljet.com>
2024-02-28 11:56:44 +0530

    Add dark mode hover color

--- frontend/src/Editor/Components/Table/NullRenderer/NullRenderer.jsx
+++ frontend/src/Editor/Components/Table/NullRenderer/NullRenderer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './nullRenderer.scss';
+import classNames from 'classnames';
 
 const NullRenderer = ({ darkMode }) => {
   return (
     <div className="d-flex align-items-center h-100">
       <span
-        className="null-renderer-text"
+        className={classNames('null-renderer-text', { 'dark-theme': darkMode })}
         style={{
           color: darkMode ? '#CFD3D8' : '#1B1F24',
           backgroundColor: darkMode ? '#3C434B' : '#E4E7EB',
