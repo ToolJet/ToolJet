e35e72254e94a22c0d9789958ec0c109056257ac
stepinfwd <stepinfwd@gmail.com>
2024-03-07 14:55:39 +0530

    fixing tab opening condition

--- frontend/src/Editor/Components/Table/Link.jsx
+++ frontend/src/Editor/Components/Table/Link.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export const Link = ({ cellValue, linkTarget, underline, underlineColor, linkColor, displayText }) => {
+  console.log('linkTarget---', linkTarget);
   return (
     <div className="w-100 table-link-column">
       <a
         className={underline === 'hover' ? 'table-link-hover' : 'table-link'}
         href={cellValue}
-        target={linkTarget !== '-self' ? '_blank' : '_self'}
+        target={linkTarget == '_self' || linkTarget == false ? '_self' : '_blank'}
         onClick={(e) => {
           e.stopPropagation();
         }}
