e3ed5c1365df9260f44f12d4734a467a8ab23081
Johnson Cherian <johnsonc.dev@gmail.com>
2024-03-12 10:15:03 +0530

    refactor: removed unused code

--- frontend/src/Editor/DraggableBox.jsx
+++ frontend/src/Editor/DraggableBox.jsx
@@ -282,26 +282,21 @@ export const DraggableBox = React.memo(
             style={getStyles(isDragging, isSelectedComponent)}
           >
             <div ref={preview} role="DraggableBox" style={isResizing ? { opacity: 0.5 } : { opacity: 1 }}>
-              {mode === 'edit' &&
-                !readOnly &&
-                // (configWidgetHandlerForModalComponent || mouseOver || isSelectedComponent) &&
-                !isResizing && (
-                  <ConfigHandle
-                    id={id}
-                    removeComponent={removeComponent}
-                    component={component}
-                    position={layoutData.top < 15 ? 'bottom' : 'top'}
-                    widgetTop={layoutData.top}
-                    widgetHeight={layoutData.height}
-                    isMultipleComponentsSelected={isMultipleComponentsSelected}
-                    configWidgetHandlerForModalComponent={configWidgetHandlerForModalComponent}
-                    mouseOver={mouseOver}
-                    isSelectedComponent={isSelectedComponent}
-                    showHandle={
-                      (configWidgetHandlerForModalComponent || mouseOver || isSelectedComponent) && !isResizing
-                    }
-                  />
-                )}
+              {mode === 'edit' && !readOnly && !isResizing && (
+                <ConfigHandle
+                  id={id}
+                  removeComponent={removeComponent}
+                  component={component}
+                  position={layoutData.top < 15 ? 'bottom' : 'top'}
+                  widgetTop={layoutData.top}
+                  widgetHeight={layoutData.height}
+                  isMultipleComponentsSelected={isMultipleComponentsSelected}
+                  configWidgetHandlerForModalComponent={configWidgetHandlerForModalComponent}
+                  mouseOver={mouseOver}
+                  isSelectedComponent={isSelectedComponent}
+                  showHandle={(configWidgetHandlerForModalComponent || mouseOver || isSelectedComponent) && !isResizing}
+                />
+              )}
               <Sentry.ErrorBoundary
                 fallback={<h2>Something went wrong.</h2>}
                 beforeCapture={(scope) => {
