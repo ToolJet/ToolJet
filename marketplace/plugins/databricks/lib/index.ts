463cdd5a67b6c9d0311f31abd27fb1e588d18b60
Syed Mohammad Akhtar Rizvi <85864291+ShazanRizvi@users.noreply.github.com>
2024-03-24 10:44:52 +0530

    Databricks integration

--- marketplace/plugins/databricks/lib/index.ts
+++ marketplace/plugins/databricks/lib/index.ts
@@ -1,11 +1,44 @@
 import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-marketplace/common';
 import { SourceOptions, QueryOptions } from './types';
+import { DBSQLClient } from '@databricks/sql';
 
 export default class Databricks implements QueryService {
-  async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
+  async getConnection(sourceOptions: SourceOptions): Promise<DBSQLClient> {
+    const credentials = {
+      host: sourceOptions.host,
+      path: sourceOptions.http_path,
+      token: sourceOptions.personal_access_token,
+    };
+    const client = new DBSQLClient();
+    client.connect(credentials);
+    return client;
+  }
+  async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
+    const result = { message: 'test connection successful' };
     return {
       status: 'ok',
-      data: {},
+      data: result,
     };
+  }
+  async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
+    try {
+      const result = await this.getConnection(sourceOptions).then(async (client) => {
+        const session = await client.openSession();
+        const queryOperation = await session.executeStatement(queryOptions.sql_query, {
+          runAsync: true,
+          maxRows: 1000,
+        });
+        const result = await queryOperation.fetchAll();
+        await session.close();
+        await client.close();
+        return result;
+      });
+      return {
+        status: 'ok',
+        data: result,
+      };
+    } catch (error) {
+      throw new QueryError('Query could not be completed', error.message, {});
+    }
   }
 }
--- marketplace/plugins/databricks/lib/types.ts
+++ marketplace/plugins/databricks/lib/types.ts
@@ -1,4 +1,13 @@
-export type SourceOptions = {};
+export type SourceOptions = {
+  host: string;
+  port: string;
+  http_path: string;
+  default_catalog: string;
+  default_schema: string;
+  personal_access_token: string;
+};
 export type QueryOptions = {
   operation: string;
+  sql_query: string;
+  mode: string;
 };
