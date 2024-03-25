fc21b850811fd81a9c5f16311637107ac98b50e0
stepinfwd <stepinfwd@gmail.com>
2024-03-25 19:39:35 +0530

    adding prefix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: ['class'],
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './app/**/*.{js,jsx}', './src/**/*.{js,jsx}'],
-  prefix: '',
+  prefix: 'tw-',
   corePlugins: {
     preflight: false,
   },
