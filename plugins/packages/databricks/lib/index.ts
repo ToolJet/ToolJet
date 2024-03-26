c281ec1697d8f93d81437ab8f61e08c1cbf63793
Syed Mohammad Akhtar Rizvi <85864291+ShazanRizvi@users.noreply.github.com>
2024-03-26 14:47:34 +0530

    icon, error handling

--- plugins/packages/databricks/lib/index.ts
+++ plugins/packages/databricks/lib/index.ts
@@ -6,32 +6,26 @@ import IOperation from '@databricks/sql/dist/contracts/IOperation';
 
 export default class Databricks implements QueryService {
   async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
-    const credentials = {
-      host: sourceOptions.host,
-      path: sourceOptions.http_path,
-      token: sourceOptions.personal_access_token,
-    };
+    const client = await this.getConnection(sourceOptions);
+    const session: IDBSQLSession = await client.openSession();
+    const queryOperation: IOperation = await session.executeStatement('SELECT 1', {
+      runAsync: true,
+      maxRows: 10000,
+    });
+    let result;
     try {
-      const client = await this.getConnection(sourceOptions);
-      const result = client.connect(credentials).then(async (client) => {
-        const session: IDBSQLSession = await client.openSession();
-
-        const queryOperation: IOperation = await session.executeStatement('SELECT 1', {
-          runAsync: true,
-          maxRows: 10000, // This option enables the direct results feature.
-        });
-        const result = await queryOperation.fetchAll();
-        await session.close();
-        await client.close();
-        return result;
-      });
-      return {
-        status: 'ok',
-        data: result,
-      };
+      result = await queryOperation.fetchAll();
     } catch (error) {
-      throw new QueryError('Error connecting to databricks', error.message, {});
+      throw new Error('Error in connection: ' + error.message);
+    } finally {
+      await session.close();
+      await client.close();
     }
+
+    return {
+      status: 'ok',
+      data: result,
+    };
   }
   async getConnection(sourceOptions: SourceOptions): Promise<DBSQLClient> {
     const credentials = {
@@ -39,29 +33,38 @@ export default class Databricks implements QueryService {
       path: sourceOptions.http_path,
       token: sourceOptions.personal_access_token,
     };
-    const client = new DBSQLClient();
-    client.connect(credentials);
-    return client;
+    if (credentials.host === '' || credentials.path === '' || credentials.token === '') {
+      throw new Error(
+        'Cannot find Server Hostname, HTTP Path, or personal access token. ' +
+          'Check the environment variables DATABRICKS_SERVER_HOSTNAME, ' +
+          'DATABRICKS_HTTP_PATH, and DATABRICKS_TOKEN.'
+      );
+    } else {
+      const client = new DBSQLClient();
+      client.connect(credentials);
+      return client;
+    }
   }
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     const client = await this.getConnection(sourceOptions);
-
     const session: IDBSQLSession = await client.openSession();
     const queryOperation: IOperation = await session.executeStatement(queryOptions.sql_query, {
       runAsync: true,
       maxRows: 10000,
     });
-    const result = await queryOperation.fetchAll();
-
-    await session.close();
-    await client.close();
+    let result;
+    try {
+      result = await queryOperation.fetchAll();
+    } catch (error) {
+      throw new QueryError('Error fetching query result', error.message, {});
+    } finally {
+      await session.close();
+      await client.close();
+    }
 
     return {
       status: 'ok',
       data: result,
     };
-  }
-  catch(error) {
-    throw new QueryError('Query could not be completed', error.message, {});
   }
 }
