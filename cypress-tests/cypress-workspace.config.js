cb155aa744f27e9a054eaf01890e758fefaa3d8b
Nakul Nagargade <133095394+nakulnagargade@users.noreply.github.com>
2024-03-27 13:01:37 +0530

    hottfix: Fix infinite render in dropdown when value changed to array (#9198)
    * Fix infinite render in dropdown when value changed to array
    
    * Update versions
    
    * Alphabetically sort the queries when sorted by query type
    
    * Sort alphabettically first when within same kind of query
    
    * Modify platfrom cypress test cases (#9208)
    
    ---------
    
    Co-authored-by: Ajith KV <ajith.jaban@gmail.com>

--- cypress-tests/cypress-workspace.config.js
+++ cypress-tests/cypress-workspace.config.js
@@ -76,7 +76,7 @@ module.exports = defineConfig({
     experimentalRunAllSpecs: true,
     baseUrl: "http://localhost:8082",
     specPattern: [
-      "cypress/e2e/happyPath/platform/ceTestcases/appSlug.cy.js",
+      "cypress/e2e/happyPath/platform/ceTestcases/editor/appSlug.cy.js",
       "cypress/e2e/happyPath/platform/commonTestcases/**/*.cy.js",
       "cypress/e2e/happyPath/platform/ceTestcases/**/!(*appSlug).cy.js"
     ],
