d8632b164149477c7ef7d9d85da82972a607919b
Muhsin Shah <muhsinshah21@gmail.com>
2024-04-15 15:38:26 +0530

    moved environment manager directory

--- frontend/src/Editor/Header/index.js
+++ frontend/src/Editor/Header/index.js
@@ -16,7 +16,7 @@ import queryString from 'query-string';
 import { isEmpty } from 'lodash';
 import LogoNavDropdown from '@/_components/LogoNavDropdown';
 import RightTopHeaderButtons from './RightTopHeaderButtons';
-import EnvironmentManager from './RightTopHeaderButtons/EnvironmentManager';
+import EnvironmentManager from './EnvironmentManager';
 
 export default function EditorHeader({
   M,
