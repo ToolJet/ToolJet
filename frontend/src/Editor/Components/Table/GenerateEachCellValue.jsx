e0f4fbb339b5fc17ce0a6268bc1d8508d9822c1d
Nakul Nagargade <133095394+nakulnagargade@users.noreply.github.com>
2024-04-19 16:19:43 +0530

    feat: Null and empty string handling in table component (#8916)
    * Sgtash
    
    * feat: Null and empty string handling in table component
    
    * Code refactor
    
    * Commit null renderer
    
    * Fix on click focus and onBlur delay
    
    * Add dark mode hover color
    
    * Code refactor
    
    * Remove renderToString
    
    * Fixes

--- frontend/src/Editor/Components/Table/GenerateEachCellValue.jsx
+++ frontend/src/Editor/Components/Table/GenerateEachCellValue.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import _ from 'lodash';
 import { validateWidget } from '@/_helpers/utils';
 import { useMounted } from '@/_hooks/use-mount';
+import NullRenderer from './NullRenderer/NullRenderer';
 
 export default function GenerateEachCellValue({
   cellValue,
@@ -15,13 +16,49 @@ export default function GenerateEachCellValue({
   cellTextColor,
   cell,
   currentState,
+  isCellValueChanged,
+  setIsCellValueChanged,
+  darkMode,
 }) {
   const mounted = useMounted();
   const updateCellValue = useRef();
   const isTabKeyPressed = useRef(false);
+  const cellRef = useRef(null);
+
   const [showHighlightedCells, setHighlighterCells] = React.useState(globalFilter ? true : false);
+  const [isNullCellClicked, setIsNullCellClicked] = React.useState(false);
   const columnTypeAllowToRenderMarkElement = ['text', 'string', 'default', 'number', undefined];
+
+  const handleCellClick = () => {
+    setIsNullCellClicked(true);
+    if (isEditable && columnTypeAllowToRenderMarkElement.includes(columnType)) {
+      setHighlighterCells(false);
+    }
+  };
+
+  const handleCellBlur = (e) => {
+    e.stopPropagation();
+    if (isTabKeyPressed.current) {
+      isTabKeyPressed.current = false;
+      return;
+    } else {
+      updateCellValue.current = e.target.value;
+      if (!showHighlightedCells && updateCellValue.current === cellValue) {
+        updateCellValue.current = null;
+        setHighlighterCells(true);
+      }
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'Tab') {
+      isTabKeyPressed.current = true;
+      setHighlighterCells(false);
+    }
+  };
+
   let validationData = {};
+
   if (cell.column.isEditable && showHighlightedCells) {
     if (cell.column.columnType === 'number') {
       validationData = {
@@ -68,7 +105,6 @@ export default function GenerateEachCellValue({
     if (mounted && _.isEmpty(rowChangeSet)) {
       setHighlighterCells(true);
     }
-    //In the dependency array to ingnore linting warning, added mounted but it's not working out, any way to avoid ingnoring dependency array
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rowData, rowChangeSet]);
 
@@ -86,37 +122,13 @@ export default function GenerateEachCellValue({
         .replace(new RegExp(`(${normReq.join('|')})`, 'gi'), (match) => `<mark>${match}</mark>`);
     }
   }
-  return (
-    <div
-      onClick={() => {
-        if (isEditable && columnTypeAllowToRenderMarkElement.includes(columnType)) {
-          setHighlighterCells(false);
-        }
-      }}
-      onBlur={(e) => {
-        e.stopPropagation();
-        if (isTabKeyPressed.current) {
-          isTabKeyPressed.current = false;
-          return;
-        } else {
-          updateCellValue.current = e.target.value;
-          //removing _.isEmpty(rowChangeSet) flag from if statement at the end
-          if (!showHighlightedCells && updateCellValue.current === cellValue) {
-            updateCellValue.current = null;
-            setHighlighterCells(true);
-          }
-        }
-      }}
-      onKeyUp={(e) => {
-        if (e.key === 'Tab') {
-          isTabKeyPressed.current = true;
-          setHighlighterCells(false);
-        }
-      }}
-      className={`w-100 h-100 ${columnType === 'selector' && 'd-flex align-items-center justify-content-center'}`}
-    >
-      {!isColumnTypeAction && columnTypeAllowToRenderMarkElement.includes(columnType) && showHighlightedCells ? (
-        <div className="d-flex justify-content-center flex-column w-100 h-100 generate-cell-value-component-div-wrapper">
+
+  const _renderCellWhenHighlighted = () => {
+    return (
+      <div className="d-flex justify-content-center flex-column w-100 h-100 generate-cell-value-component-div-wrapper">
+        {cellValue === null ? (
+          <NullRenderer darkMode={darkMode} />
+        ) : (
           <div
             style={{
               color: cellTextColor,
@@ -127,21 +139,71 @@ export default function GenerateEachCellValue({
             tabIndex={0}
             className={`form-control-plaintext form-control-plaintext-sm ${columnType === 'text' && 'h-100 my-1'}`}
           ></div>
-          <div
-            style={{
-              display: cell.column.isEditable && validationData.validationError ? 'block' : 'none',
-              width: '100%',
-              marginTop: ' 0.25rem',
-              fontSize: ' 85.7142857%',
-              color: '#d63939',
-            }}
-          >
-            {validationData.validationError}
-          </div>
+        )}
+        <div
+          style={{
+            display: cell.column.isEditable && validationData.validationError ? 'block' : 'none',
+            width: '100%',
+            marginTop: ' 0.25rem',
+            fontSize: ' 85.7142857%',
+            color: '#d63939',
+          }}
+        >
+          {validationData.validationError}
         </div>
-      ) : (
-        cellRender
-      )}
+      </div>
+    );
+  };
+
+  const _renderNullCell = () => {
+    if (isEditable) {
+      if (!isNullCellClicked && !updateCellValue.current) {
+        return <NullRenderer darkMode={darkMode} />;
+      } else return cellRender;
+    } else {
+      return <NullRenderer darkMode={darkMode} />;
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (cellRef.current && !cellRef.current.contains(event.target) && !isCellValueChanged) {
+        // Adding setTimeout to avoid this event to be executed before input's blur event
+        setTimeout(() => {
+          setIsNullCellClicked(false);
+          setIsCellValueChanged(false);
+        }, 100);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isCellValueChanged, setIsCellValueChanged]);
+
+  useEffect(() => {
+    if (isNullCellClicked) {
+      const inputElement = document.getElementById(`table-input-${cell.column.id}`);
+      if (inputElement) {
+        inputElement.focus();
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isNullCellClicked]);
+
+  return (
+    <div
+      onClick={handleCellClick}
+      onBlur={handleCellBlur}
+      onKeyUp={handleKeyUp}
+      className={`w-100 h-100 ${columnType === 'selector' && 'd-flex align-items-center justify-content-center'}`}
+      ref={cellRef}
+    >
+      {!isColumnTypeAction && columnTypeAllowToRenderMarkElement.includes(columnType) && showHighlightedCells
+        ? _renderCellWhenHighlighted()
+        : cellValue === null
+        ? _renderNullCell()
+        : cellRender}
     </div>
   );
 }
--- frontend/src/Editor/Components/Table/Table.jsx
+++ frontend/src/Editor/Components/Table/Table.jsx
@@ -172,11 +172,12 @@ export function Table({
   const [tableDetails, dispatch] = useReducer(reducer, initialState());
   const [hoverAdded, setHoverAdded] = useState(false);
   const [generatedColumn, setGeneratedColumn] = useState([]);
+  const [isCellValueChanged, setIsCellValueChanged] = useState(false);
+
   const mergeToTableDetails = (payload) => dispatch(reducerActions.mergeToTableDetails(payload));
   const mergeToFilterDetails = (payload) => dispatch(reducerActions.mergeToFilterDetails(payload));
   const mergeToAddNewRowsDetails = (payload) => dispatch(reducerActions.mergeToAddNewRowsDetails(payload));
   const mounted = useMounted();
-
   const [resizingColumnId, setResizingColumnId] = useState(null);
 
   const prevDataFromProps = useRef();
@@ -232,6 +233,8 @@ export function Table({
 
   function handleExistingRowCellValueChange(index, key, value, rowData) {
     const changeSet = tableDetails.changeSet;
+    setIsCellValueChanged(true);
+
     const dataUpdates = tableDetails.dataUpdates || [];
     const clonedTableData = _.cloneDeep(tableData);
 
@@ -269,6 +272,7 @@ export function Table({
   }, [JSON.stringify(tableDetails)]);
 
   function handleNewRowCellValueChange(index, key, value, rowData) {
+    setIsCellValueChanged(true);
     const changeSet = copyOfTableDetails.current.addNewRowsDetails.newRowsChangeSet || {};
     const dataUpdates = copyOfTableDetails.current.addNewRowsDetails.newRowsDataUpdates || {};
     let obj = changeSet ? changeSet[index] || {} : {};
@@ -1540,6 +1544,9 @@ export function Table({
                               cellTextColor={cellTextColor}
                               cell={cell}
                               currentState={currentState}
+                              isCellValueChanged={isCellValueChanged}
+                              setIsCellValueChanged={setIsCellValueChanged}
+                              darkMode={darkMode}
                             />
                           </div>
                         </td>
@@ -1617,6 +1624,7 @@ export function Table({
                       variant="primary"
                       className={`tj-text-xsm`}
                       onClick={() => {
+                        setIsCellValueChanged(false);
                         onEvent('onBulkUpdate', tableEvents, { component }).then(() => {
                           handleChangesSaved();
                         });
@@ -1635,6 +1643,7 @@ export function Table({
                       variant="tertiary"
                       className={`tj-text-xsm`}
                       onClick={() => {
+                        setIsCellValueChanged(false);
                         handleChangesDiscarded();
                       }}
                       data-cy={`table-button-discard-changes`}
