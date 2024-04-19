b5974b2b8d4569110dba7e08ce606fc6cde4b512
Nakul Nagargade <133095394+nakulnagargade@users.noreply.github.com>
2024-04-19 12:55:16 +0530

    feat: Add light/dark/auto mode theme setting at application level (#9091)
    * Add app mode in global settings
    
    * Fix width of app mode toggle
    
    * Resolve code comments
    
    * Fix design comments
    
    * Fix when canvas bg is empty
    
    * Product feedback
    
    * Fix dark theme issue in table
    
    * Code refactor
    
    * Always enable dark mode toggle in editor
    
    * Fix canvas container color
    
    * fix: update currentstate theme on appMode change
    
    * fix: check theme as string from localstorage
    
    * Update frontend/src/Editor/Header/AppModeToggle.jsx
    
    Co-authored-by: Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
    
    * Update frontend/src/Editor/Viewer/DesktopHeader.jsx
    
    Co-authored-by: Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
    
    * fix: check app theme when switching global theme
    
    * fix: check if appmode is empty and show toggle button in viewer
    
    * fix: set appmode on editor load from appdef
    
    ---------
    
    Co-authored-by: Johnson Cherian <johnsonc.dev@gmail.com>
    Co-authored-by: Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>

--- frontend/src/Editor/Box.jsx
+++ frontend/src/Editor/Box.jsx
@@ -159,6 +159,7 @@ export const Box = memo(
     isResizing,
     adjustHeightBasedOnAlignment,
     currentLayout,
+    darkMode,
   }) => {
     const { t } = useTranslation();
     const backgroundColor = yellow ? 'yellow' : '';
@@ -206,7 +207,6 @@ export const Box = memo(
         ? validateProperties(resolvedGeneralStyles, componentMeta.generalStyles)
         : [resolvedGeneralStyles, []];
 
-    const darkMode = localStorage.getItem('darkMode') === 'true';
     const { variablesExposedForPreview, exposeToCodeHinter } = useContext(EditorContext) || {};
 
     let styles = {
