930c72744f525c656ba73fc068218c843fb71f23
Akshay <akshaysasidharan93@gmail.com>
2024-04-24 01:48:21 +0530

    Add ability to run TJDB queries from public apps (#9134)
    * add ability to run TJDB queries from public apps
    
    * make TJDB query run similar to other datasources
    
    * add result key for backward compatibility
    
    * throw query error on postgrest request failures

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -138,6 +138,7 @@ const imports = [
   ImportExportResourcesModule,
   CopilotModule,
   OrganizationConstantModule,
+  TooljetDbModule,
 ];
 
 if (process.env.SERVE_CLIENT !== 'false' && process.env.NODE_ENV === 'production') {
