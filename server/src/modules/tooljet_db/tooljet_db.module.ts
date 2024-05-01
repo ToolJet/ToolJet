5ebc6ceed4d84d2d5a33641b65175ae5804a4a29
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2024-05-01 22:44:13 +0530

    support async loading of db connection

--- server/src/modules/tooljet_db/tooljet_db.module.ts
+++ server/src/modules/tooljet_db/tooljet_db.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Credential } from '../../../src/entities/credential.entity';
 import { TooljetDbController } from '@controllers/tooljet_db.controller';
@@ -8,17 +8,34 @@ import { PostgrestProxyService } from '@services/postgrest_proxy.service';
 import { TooljetDbBulkUploadService } from '@services/tooljet_db_bulk_upload.service';
 import { TooljetDbOperationsService } from '@services/tooljet_db_operations.service';
 import { tooljetDbOrmconfig } from 'ormconfig';
-
-const imports = [TypeOrmModule.forFeature([Credential]), CaslModule];
-
-if (process.env.ENABLE_TOOLJET_DB === 'true') {
-  imports.unshift(TypeOrmModule.forRoot(tooljetDbOrmconfig));
-}
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports,
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([Credential]),
+    CaslModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      name: 'tooljetDb',
+      useFactory: (config: ConfigService) => {
+        if (config.get('ENABLE_TOOLJET_DB') === 'true') {
+          return tooljetDbOrmconfig;
+        }
+        return {};
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [TooljetDbController],
   providers: [TooljetDbService, TooljetDbBulkUploadService, TooljetDbOperationsService, PostgrestProxyService],
   exports: [TooljetDbOperationsService],
 })
-export class TooljetDbModule {}
+export class TooljetDbModule implements OnModuleInit {
+  constructor(private configService: ConfigService) {}
+
+  onModuleInit() {
+    const enableTooljetDb = this.configService.get('ENABLE_TOOLJET_DB') === 'true';
+    console.log(`ToolJet Database enabled: ${enableTooljetDb}`);
+  }
+}
