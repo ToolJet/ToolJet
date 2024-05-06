d6ca4639386c895f9f10645065b3fadcee9e8eb0
arpitnath <arpitnath42@gmail.com>
2024-05-07 03:55:08 +0530

    fixes: container components' children vanishes on layout change tabs, listview children vanishes from editor (UI) on switching to mobile layout and back to desktop layout

--- frontend/src/Editor/Container.jsx
+++ frontend/src/Editor/Container.jsx
@@ -373,7 +373,7 @@ export const Container = ({
           monitor,
           boxes,
           canvasBoundingRect,
-          item.currentLayout,
+          currentLayout,
           snapToGrid,
           zoomLevel
         );
