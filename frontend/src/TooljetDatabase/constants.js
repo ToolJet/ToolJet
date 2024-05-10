41ea5b2c36b3dd8bfe7fa95829b8a4f2284f09d4
Akshay <akshaysasidharan93@gmail.com>
2024-05-10 10:35:26 +0530

    Fix: Minor UI fixes (#9670)
    * add description for serial data type
    
    * make default placeholder text all caps
    
    * revise placeholder text

--- frontend/src/TooljetDatabase/constants.js
+++ frontend/src/TooljetDatabase/constants.js
@@ -19,11 +19,21 @@ export const dataTypes = [
   { name: 'Integers up to 8 bytes', label: 'bigint', icon: <BigInt width="16" height="16" />, value: 'bigint' },
   { name: 'Decimal numbers', label: 'float', icon: <Float width="16" height="16" />, value: 'double precision' },
   { name: 'Boolean True/False', label: 'boolean', icon: <Boolean width="16" height="16" />, value: 'boolean' },
-  { name: 'serial', label: 'serial', icon: <Serial width="16" height="16" />, value: 'serial' },
+  {
+    name: 'Auto-incrementing integers',
+    label: 'serial',
+    icon: <Serial width="16" height="16" />,
+    value: 'serial',
+  },
 ];
 
 export const serialDataType = [
-  { name: 'serial', label: 'serial', icon: <Serial width="16" height="16" />, value: 'serial' },
+  {
+    name: 'Auto-incrementing integers',
+    label: 'serial',
+    icon: <Serial width="16" height="16" />,
+    value: 'serial',
+  },
 ];
 
 export const operators = [
