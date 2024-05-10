b7d46c37810e71ac1d902e8a5de854839011b44d
Nakul Nagargade <nakul@tooljet.com>
2024-05-10 15:32:34 +0530

    Add down rollbackUpdates

--- server/data-migrations/1715248567720-MoveCheckboxToggleDisabledToProperties.ts
+++ server/data-migrations/1715248567720-MoveCheckboxToggleDisabledToProperties.ts
@@ -2,12 +2,12 @@ import { Component } from 'src/entities/component.entity';
 import { processDataInBatches } from 'src/helpers/utils.helper';
 import { EntityManager, MigrationInterface, QueryRunner } from 'typeorm';
 
-export class MoveCheckboxToggleDisabledToProperties1715248128046 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    const componentTypes = ['Checkbox', 'Button'];
-    const batchSize = 100;
-    const entityManager = queryRunner.manager;
+const componentTypes = ['Checkbox', 'Button'];
+const batchSize = 100;
 
+export class MoveCheckboxButtonDisabledToProperties1715248128046 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
     for (const componentType of componentTypes) {
       await processDataInBatches(
         entityManager,
@@ -61,5 +61,58 @@ export class MoveCheckboxToggleDisabledToProperties1715248128046 implements Migr
     }
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  private async rollbackUpdates(entityManager, components) {
+    for (const component of components) {
+      const properties = component.properties;
+      const styles = component.styles;
+      const general = component.general;
+      const generalStyles = component.generalStyles;
+
+      if (properties.visibility !== undefined) {
+        styles.visibility = properties.visibility;
+        delete properties.visibility;
+      }
+
+      if (properties.disabledState !== undefined) {
+        styles.disabledState = properties.disabledState;
+        delete properties.disabledState;
+      }
+
+      if (properties.tooltip !== undefined) {
+        general.tooltip = properties.tooltip;
+        delete properties.tooltip;
+      }
+
+      if (styles.boxShadow !== undefined) {
+        generalStyles.boxShadow = styles.boxShadow;
+        delete styles.boxShadow;
+      }
+
+      await entityManager.update(Component, component.id, {
+        properties,
+        styles,
+        general,
+        generalStyles,
+      });
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    for (const componentType of componentTypes) {
+      await processDataInBatches(
+        entityManager,
+        async (entityManager: EntityManager) => {
+          return await entityManager.find(Component, {
+            where: { type: componentType },
+            order: { createdAt: 'ASC' },
+          });
+        },
+        async (entityManager: EntityManager, components: Component[]) => {
+          await this.rollbackUpdates(entityManager, components);
+        },
+        batchSize
+      );
+    }
+  }
 }
