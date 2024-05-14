9ae26e485ebc2e8fe81aa326423748e03a07b508
parthy007 <parthadhikari1812@gmail.com>
2024-05-14 17:48:20 +0530

    Remove unrequired code

--- frontend/src/_hooks/useMountTransition.js
+++ frontend/src/_hooks/useMountTransition.js
@@ -7,7 +7,6 @@ const useMountTransition = (isMounted, unmountDelay) => {
     let timeoutId;
 
     if (isMounted && !isTransitioning) {
-      setIsTransitioning(true);
       timeoutId = setTimeout(() => setIsTransitioning(true), unmountDelay);
     } else if (!isMounted && isTransitioning) {
       timeoutId = setTimeout(() => setIsTransitioning(false), unmountDelay);
