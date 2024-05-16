f29be81a43b48cd5ba3d0a58f9cb54ef016b8a29
parthy007 <parthadhikari1812@gmail.com>
2024-05-16 12:35:54 +0530

    Update the text on deleting FKR

--- frontend/src/TooljetDatabase/Forms/ColumnForm.jsx
+++ frontend/src/TooljetDatabase/Forms/ColumnForm.jsx
@@ -474,7 +474,7 @@ const ColumnForm = ({
         showToolTipForFkOnReadDocsSection={true}
       />
       <ConfirmDialog
-        title={'Delete foreign key'}
+        title={'Delete foreign key relation'}
         show={onDeletePopup}
         message={'Deleting the foreign key relation cannot be reversed. Are you sure you want to continue?'}
         onConfirm={() => {
