d91490e5992dc8b636bf58a647a1eb8975fcda0a
Anantshree Chandola <anantshreechandola23@gmail.com>
2024-05-16 15:49:58 +0530

    Styling fixes for UI comps (#9752)

--- frontend/ee/components/UsersPage/UsersTable.jsx
+++ frontend/ee/components/UsersPage/UsersTable.jsx
@@ -7,8 +7,7 @@ import { Pagination } from '@/_components';
 import SolidIcon from '@/_ui/Icon/SolidIcons';
 import { Tooltip } from 'react-tooltip';
 import UsersActionMenu from './UsersActionMenu';
-import { humanizeifDefaultGroupName } from '@/_helpers/utils';
-import { decodeEntities } from '@/_helpers/utils';
+import { humanizeifDefaultGroupName, decodeEntities } from '@/_helpers/utils';
 
 const UsersTable = ({
   isLoading,
--- frontend/src/_ui/HttpHeaders/SourceEditor.jsx
+++ frontend/src/_ui/HttpHeaders/SourceEditor.jsx
@@ -12,22 +12,7 @@ export default ({ options, addNewKeyValuePair, removeKeyValuePair, keyValuePairV
   return (
     <div className="table-content-wrapper">
       {options.length === 0 && (
-        <div
-          style={{
-            border: '2px dashed #ccc',
-            borderRadius: '6px',
-            padding: '10px',
-            textAlign: 'center',
-            color: '#ccc',
-            width: '654px',
-            height: '32px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            fontSize: '14px',
-            marginBottom: '10px',
-          }}
-        >
+        <div className="empty-key-value">
           <InfoIcon style={{ width: '16px', marginRight: '5px' }} />
           <span>There are no key value pairs added</span>
         </div>
