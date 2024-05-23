27e170fb8d9424fec93ccba6ef8cb5871252c869
Muhsin Shah C P <muhsinshah21@gmail.com>
2024-05-23 19:46:06 +0530

    Fixed constants empty array while versions switch (#1457)

--- frontend/src/_stores/currentStateStore.js
+++ frontend/src/_stores/currentStateStore.js
@@ -61,7 +61,14 @@ export const useCurrentStateStore = create(
         },
         setEditorReady: (isEditorReady) => set({ isEditorReady }),
         initializeCurrentStateOnVersionSwitch: () => {
-          set({ ...initialState }, false, { type: 'INITIALIZE_CURRENT_STATE_ON_VERSION_SWITCH', initialState });
+          const newInitialState = {
+            ...initialState,
+            constants: get().constants,
+          };
+          set({ ...newInitialState }, false, {
+            type: 'INITIALIZE_CURRENT_STATE_ON_VERSION_SWITCH',
+            newInitialState,
+          });
         },
       },
     }),
