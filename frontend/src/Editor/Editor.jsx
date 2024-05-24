b98dca611d661358638b5d00f313d25ea486f25b
Johnson Cherian <johnsonc.dev@gmail.com>
2024-05-24 17:15:46 +0530

    fix: delete all children and nested children on component delete (#9182)
    * fix: delete all children and nested children on component delete
    
    * refactor: removed duplicate logics
    
    * refactor: reuse functions and remove duplicate functions
    
    * fix: update parentid of kanban modal child on version creation
    
    * fix: add kanban modal children to delete list

--- frontend/src/Editor/Editor.jsx
+++ frontend/src/Editor/Editor.jsx
@@ -26,6 +26,7 @@ import {
   removeSelectedComponent,
   buildAppDefinition,
   buildComponentMetaDefinition,
+  getAllChildComponents,
   runQueries,
 } from '@/_helpers/appUtils';
 import { Confirm } from './Viewer/Confirm';
@@ -1286,17 +1287,9 @@ const EditorComponent = (props) => {
 
       let childComponents = [];
 
-      if (newDefinition.pages[currentPageId].components?.[componentId].component.component === 'Tabs') {
-        childComponents = Object.keys(newDefinition.pages[currentPageId].components).filter((key) =>
-          newDefinition.pages[currentPageId].components[key].component.parent?.startsWith(componentId)
-        );
-      } else {
-        childComponents = Object.keys(newDefinition.pages[currentPageId].components).filter(
-          (key) => newDefinition.pages[currentPageId].components[key].component.parent === componentId
-        );
-      }
+      childComponents = getAllChildComponents(newDefinition.pages[currentPageId].components, componentId);
 
-      childComponents.forEach((componentId) => {
+      childComponents.forEach(({ componentId }) => {
         delete newDefinition.pages[currentPageId].components[componentId];
       });
 
