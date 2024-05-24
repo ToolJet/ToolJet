7febec533dd6a046d2a1b2112ec2b8d144fcdc80
parthy007 <parthadhikari1812@gmail.com>
2024-05-24 12:09:56 +0530

    Disable boolean datatype from dropdown

--- frontend/src/TooljetDatabase/Forms/TableKeyRelations.jsx
+++ frontend/src/TooljetDatabase/Forms/TableKeyRelations.jsx
@@ -69,21 +69,19 @@ function SourceKeyRelation({
             icon: columns?.dataTypeDetails?.icon ?? columns?.dataTypeDetails[0]?.icon,
             value: columns?.column_name,
             dataType: columns?.data_type,
-            isDisabled: columns?.data_type === 'serial' ? true : false,
+            isDisabled: columns?.data_type === 'serial' || columns?.data_type === 'boolean' ? true : false,
           },
         ]
-      : Object.values(columns)
-          .filter((item) => item.data_type !== 'boolean') // Exclude boolean columns
-          .map((item) => {
-            return {
-              name: item?.column_name,
-              label: item?.column_name,
-              icon: item?.dataTypeDetails?.icon ?? item?.dataTypeDetails?.[0]?.icon,
-              value: item?.column_name,
-              dataType: item?.data_type,
-              isDisabled: item?.data_type === 'serial' ? true : false,
-            };
-          });
+      : Object.values(columns).map((item) => {
+          return {
+            name: item?.column_name,
+            label: item?.column_name,
+            icon: item?.dataTypeDetails?.icon ?? item?.dataTypeDetails?.[0]?.icon,
+            value: item?.column_name,
+            dataType: item?.data_type,
+            isDisabled: item?.data_type === 'serial' || item?.data_type === 'boolean' ? true : false,
+          };
+        });
 
   const tableList = tables
     .filter((item) => item?.table_name !== tableName)
