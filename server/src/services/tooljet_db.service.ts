59db111bb0690da0f9063c8b505339d6e03ed179
Abd-Rahman-1999 <s.rahmanabd1999@gmail.com>
2024-05-27 13:21:03 +0530

    fix : while deleting a column if the column name starts with uppercase, it throws error

--- server/src/services/tooljet_db.service.ts
+++ server/src/services/tooljet_db.service.ts
@@ -610,7 +610,7 @@ export class TooljetDbService {
 
     if (!internalTable) throw new NotFoundException('Internal table not found: ' + tableName);
 
-    const query = `ALTER TABLE "${internalTable.id}" DROP COLUMN ${column['column_name']}`;
+    const query = `ALTER TABLE "${internalTable.id}" DROP COLUMN "${column['column_name']}"`;
     try {
       const result = await this.tooljetDbManager.query(query);
       await this.tooljetDbManager.query("NOTIFY pgrst, 'reload schema'");
