854ca72acca42dd19083d902f2c1721902265a73
Nakul Nagargade <nakul@tooljet.com>
2024-05-28 20:30:16 +0530

    fix

--- frontend/src/Editor/Components/TextInput.jsx
+++ frontend/src/Editor/Components/TextInput.jsx
@@ -12,6 +12,7 @@ export const TextInput = function TextInput({
   properties,
   styles,
   setExposedVariable,
+  setExposedVariables,
   fireEvent,
   component,
   darkMode,
@@ -157,32 +158,38 @@ export const TextInput = function TextInput({
   }, [properties.value]);
 
   useEffect(() => {
-    setExposedVariable('setBlur', async function () {
-      textInputRef.current.blur();
-    });
-    setExposedVariable('setFocus', async function () {
-      textInputRef.current.focus();
-    });
-    setExposedVariable('disable', async function (value) {
-      setDisable(value);
-    });
-    setExposedVariable('visibility', async function (value) {
-      setVisibility(value);
-    });
+    const exposedVariables = {
+      setFocus: async function () {
+        textInputRef.current.focus();
+      },
+      setBlur: async function () {
+        textInputRef.current.blur();
+      },
+      disable: async function (value) {
+        setDisable(value);
+      },
+      visibility: async function (value) {
+        setVisibility(value);
+      },
+    };
+    setExposedVariables(exposedVariables);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    setExposedVariable('setText', async function (text) {
-      setValue(text);
-      setExposedVariable('value', text);
-      fireEvent('onChange');
-    });
-    setExposedVariable('clear', async function () {
-      setValue('');
-      setExposedVariable('value', '');
-      fireEvent('onChange');
-    });
+    const exposedVariables = {
+      setText: async function (text) {
+        setValue(text);
+        setExposedVariable('value', text);
+        fireEvent('onChange');
+      },
+      clear: async function () {
+        setValue('');
+        setExposedVariable('value', '');
+        fireEvent('onChange');
+      },
+    };
+    setExposedVariables(exposedVariables);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setValue]);
   const iconName = styles.icon; // Replace with the name of the icon you want
