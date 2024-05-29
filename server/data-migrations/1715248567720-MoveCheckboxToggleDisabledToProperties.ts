76feb150d0cb0b46c31104dec5402e4bca98b4e8
Nakul Nagargade <133095394+nakulnagargade@users.noreply.github.com>
2024-05-29 11:41:55 +0530

    Move disabled, visibility, tooltip and box shadow to properties in Checkbox and Button (#9665)
    * init textinput revamp
    
    * updated styles panel
    
    * bugfix
    
    * updates
    
    * fix :: accordion
    
    * fix :: styling
    
    * add box shadow , additional property,tooltip
    
    * fix conditional render for styles
    
    * feat :: fixed order of each property and styles
    
    * feat :: styling input
    
    * bugfix
    
    * feat :: add option to add icon
    
    * add option to add icon
    
    * adding option to toggle visibility
    
    * updated password input with new design
    
    * chnaging component location
    
    * bugfix
    
    * style fixes
    
    * fix :: added loader
    
    * updated :: few detailing
    
    * few bugfixes
    
    * fix :: for form widget label
    
    * fixes
    
    * added option to add icon color
    
    * including label field for password input
    
    * fix for label
    
    * fix
    
    * test fix backward compatibility for height
    
    * updates
    
    * revert
    
    * adding key for distinguishing older and newer widgets
    
    * testing
    
    * test
    
    * test
    
    * update
    
    * update
    
    * migration testing
    
    * limit vertical resizing in textinput
    
    * testing
    
    * throw test
    
    * test
    
    * adding check for label length
    
    * fixing edge cases
    
    * removing resize
    
    * backward compatibility height
    
    * backward compatibility
    
    * number input review fixes
    
    * added exposed items
    
    * fixing csa
    
    * ui fixes
    
    * fix height compatibility
    
    * feat :: csa for all inputs and exposed variables
    
    * backward compatibility fixes and validation fixes
    
    * fixes :: textinput positioning of loader and icon
    
    * fix :: password input
    
    * cleanup and fixes
    
    * fixes
    
    * cleanup
    
    * fixes
    
    * review fixes
    
    * review fixes
    
    * typo fix
    
    * fix padding
    
    * review fixes styles component panel
    
    * fix naming
    
    * fix padding
    
    * feat :: toggle switch revamp
    
    * init checkbox
    
    * fixes
    
    * fixes
    
    * switch fixes
    
    * validation fix
    
    * fixes
    
    * cleanup
    
    * height fix
    
    * fix height toggle
    
    * updates
    
    * fix :: icons position
    
    * updates
    
    * cleanup
    
    * updates events , csa
    
    * cleanup
    
    * backward compatibility
    
    * clean
    
    * backward compatibility fix
    
    * label fixed to one line
    
    * feat :: change validation from properties
    
    * ui fixes
    
    * icon name
    
    * removed 'px' text from tooltip
    
    * added onchange event for checkbox
    
    * fixes placeholder
    
    * few updates :: removing label in form
    
    * ui in form
    
    * fire onchange
    
    * update :: number input validation behaviour
    
    * testing fixes
    
    * added side handlers
    
    * removing unwanted fx
    
    * disabling fx for padding field
    
    * ordering change
    
    * fix
    
    * label issue + restricted side handler
    
    * fix :: box shadow bug
    
    * fix
    
    * on change event doesnt propagate exposed vars correctly
    
    * adding debounce for slider value change
    
    * fix :: for modal ooen bug during onfocus event
    
    * test slider
    
    * fix :: bugs regarding state update in checbox , slider , slider bug
    
    * update slider with radix slider
    
    * bugfix
    
    * update tooltip
    
    * fix toggle switch
    
    * fixes : inspector
    
    * fix : checkbox label
    
    * Move disabled, visibility, tooltip and box shadow to properties
    
    * Remove toggleswitch and button
    
    * Add down rollbackUpdates
    
    * fix : filter order of toggle (#9879)
    
    * fix merge issue
    
    * fix
    
    ---------
    
    Co-authored-by: stepinfwd <stepinfwd@gmail.com>

---
+++ server/data-migrations/1715248567720-MoveCheckboxToggleDisabledToProperties.ts
@@ -0,0 +1,118 @@
+import { Component } from 'src/entities/component.entity';
+import { processDataInBatches } from 'src/helpers/utils.helper';
+import { EntityManager, MigrationInterface, QueryRunner } from 'typeorm';
+
+const componentTypes = ['Checkbox', 'Button'];
+const batchSize = 100;
+
+export class MoveCheckboxButtonDisabledToProperties1715248128046 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    for (const componentType of componentTypes) {
+      await processDataInBatches(
+        entityManager,
+        async (entityManager: EntityManager) => {
+          return await entityManager.find(Component, {
+            where: { type: componentType },
+            order: { createdAt: 'ASC' },
+          });
+        },
+        async (entityManager: EntityManager, components: Component[]) => {
+          await this.processUpdates(entityManager, components);
+        },
+        batchSize
+      );
+    }
+  }
+
+  private async processUpdates(entityManager, components) {
+    for (const component of components) {
+      const properties = component.properties;
+      const styles = component.styles;
+      const general = component.general;
+      const generalStyles = component.generalStyles;
+
+      if (styles.visibility) {
+        properties.visibility = styles.visibility;
+        delete styles.visibility;
+      }
+
+      if (styles.disabledState) {
+        properties.disabledState = styles.disabledState;
+        delete styles.disabledState;
+      }
+
+      if (general?.tooltip) {
+        properties.tooltip = general?.tooltip;
+        delete general?.tooltip;
+      }
+
+      if (generalStyles?.boxShadow) {
+        styles.boxShadow = generalStyles?.boxShadow;
+        delete generalStyles?.boxShadow;
+      }
+
+      await entityManager.update(Component, component.id, {
+        properties,
+        styles,
+        general,
+        generalStyles,
+      });
+    }
+  }
+
+  private async rollbackUpdates(entityManager, components) {
+    for (const component of components) {
+      const properties = component.properties;
+      const styles = component.styles;
+      const general = component.general;
+      const generalStyles = component.generalStyles;
+
+      if (properties.visibility !== undefined) {
+        styles.visibility = properties.visibility;
+        delete properties.visibility;
+      }
+
+      if (properties.disabledState !== undefined) {
+        styles.disabledState = properties.disabledState;
+        delete properties.disabledState;
+      }
+
+      if (properties.tooltip !== undefined) {
+        general.tooltip = properties.tooltip;
+        delete properties.tooltip;
+      }
+
+      if (styles.boxShadow !== undefined) {
+        generalStyles.boxShadow = styles.boxShadow;
+        delete styles.boxShadow;
+      }
+
+      await entityManager.update(Component, component.id, {
+        properties,
+        styles,
+        general,
+        generalStyles,
+      });
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const entityManager = queryRunner.manager;
+    for (const componentType of componentTypes) {
+      await processDataInBatches(
+        entityManager,
+        async (entityManager: EntityManager) => {
+          return await entityManager.find(Component, {
+            where: { type: componentType },
+            order: { createdAt: 'ASC' },
+          });
+        },
+        async (entityManager: EntityManager, components: Component[]) => {
+          await this.rollbackUpdates(entityManager, components);
+        },
+        batchSize
+      );
+    }
+  }
+}
