c3fdb359817415ff8b599c6059a43b575ebf1e74
Kavin Venkatachalam <kavin.saratha@gmail.com>
2024-06-07 17:12:18 +0530

    Reverting commit 583460c

--- frontend/src/Editor/Box.jsx
+++ frontend/src/Editor/Box.jsx
@@ -4,13 +4,9 @@ import BoxUI from './BoxUI';
 import _ from 'lodash';
 
 import { shouldUpdate } from './ControlledComponentToRender';
-import { useEditorStore } from '@/_stores/editorStore';
 
 export const Box = React.memo((props) => {
   const { id, component, mode, customResolvables } = props;
-
-  useEditorStore((state) => state.componentsNeedsUpdateOnNextRender);
-  //!Force a re-render of the component
 
   return (
     <HydrateWithResolveReferences id={id} mode={mode} component={component} customResolvables={customResolvables}>
