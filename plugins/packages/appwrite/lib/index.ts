242f630e0d2346f0a2155f3264efd808d99d99fc
blank0537 <111295371+blank0537@users.noreply.github.com>
2024-06-10 13:06:55 +0530

    Appwrite plugin issue fixes and added features (#9285)
    * Appwrite plugin issue fixes and added features
    
    * update test connection, removed duplicate operations and updated types

--- plugins/packages/appwrite/lib/index.ts
+++ plugins/packages/appwrite/lib/index.ts
@@ -1,5 +1,5 @@
 import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-plugins/common';
-import { SourceOptions, QueryOptions } from './types';
+import { SourceOptions, QueryOptions, ReturnObject } from './types';
 import sdk from 'node-appwrite';
 import { bulkUpdate, createDocument, deleteDocument, getDocument, queryCollection, updateDocument } from './operations';
 const JSON5 = require('json5');
@@ -7,15 +7,21 @@ const JSON5 = require('json5');
 export default class Appwrite implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     const database = await this.getConnection(sourceOptions);
+    const { database_id } = sourceOptions;
     const operation = queryOptions.operation;
     const body = this.returnObject(queryOptions.body);
+    const isBodyEmpty = !Object.keys(body).length;
     let result = {};
 
     try {
+      if (!queryOptions.collectionId) {
+        throw new Error('Collection id is required.');
+      }
       switch (operation) {
         case 'list_docs':
           result = await queryCollection(
             database,
+            database_id,
             queryOptions.collectionId,
             queryOptions.limit,
             queryOptions.order_fields,
@@ -26,22 +32,36 @@ export default class Appwrite implements QueryService {
           );
           break;
         case 'get_document':
-          result = await getDocument(database, queryOptions.collectionId, queryOptions.documentId);
+          if (!queryOptions.documentId) throw new Error('Document id is required');
+          result = await getDocument(database, database_id, queryOptions.collectionId, queryOptions.documentId);
           break;
         case 'add_document':
-          result = await createDocument(database, queryOptions.collectionId, body);
+          if (isBodyEmpty) throw new Error('Body is required');
+          result = await createDocument(database, database_id, queryOptions.collectionId, body as object);
           break;
         case 'update_document':
-          result = await updateDocument(database, queryOptions.collectionId, queryOptions.documentId, body);
+          if (!queryOptions.documentId) throw new Error('Document id is required');
+          if (isBodyEmpty) throw new Error('Body is required');
+          result = await updateDocument(
+            database,
+            database_id,
+            queryOptions.collectionId,
+            queryOptions.documentId,
+            body as object
+          );
           break;
         case 'delete_document':
-          result = await deleteDocument(database, queryOptions.collectionId, queryOptions.documentId);
+          if (!queryOptions.documentId) throw new Error('Document id is required');
+          result = await deleteDocument(database, database_id, queryOptions.collectionId, queryOptions.documentId);
           break;
         case 'bulk_update':
+          if (!queryOptions.records) throw new Error('Records field is required');
+          if (!queryOptions['document_id_key']) throw new Error('Key for document id field is required');
           result = await bulkUpdate(
             database,
+            database_id,
             queryOptions.collectionId,
-            this.returnObject(queryOptions.records),
+            this.returnObject(queryOptions.records) as object[],
             queryOptions['document_id_key']
           );
           break;
@@ -56,14 +76,14 @@ export default class Appwrite implements QueryService {
     };
   }
 
-  private returnObject(data: any) {
+  returnObject(data: ReturnObject | string): ReturnObject {
     if (!data) {
       return {};
     }
     return typeof data === 'string' ? JSON5.parse(data) : data;
   }
 
-  async getConnection(sourceOptions: SourceOptions, _options?: object): Promise<sdk.Database> {
+  async getConnection(sourceOptions: SourceOptions, _options?: object): Promise<sdk.Databases> {
     const { host, secret_key, project_id } = sourceOptions;
     const client = new sdk.Client();
 
@@ -72,20 +92,20 @@ export default class Appwrite implements QueryService {
       .setProject(project_id) // Your project ID
       .setKey(secret_key); // Your secret API key;
 
-    return new sdk.Database(client);
+    return new sdk.Databases(client);
   }
 
   async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
     const databaseClient = await this.getConnection(sourceOptions);
 
-    if (!databaseClient) {
-      throw new Error('Invalid credentials');
+    try {
+      await databaseClient.listCollections(sourceOptions.database_id);
+
+      return {
+        status: 'ok',
+      };
+    } catch (err) {
+      throw new Error(`Connection test failed - ${err.message}`);
     }
-
-    await databaseClient.listCollections();
-
-    return {
-      status: 'ok',
-    };
   }
 }
