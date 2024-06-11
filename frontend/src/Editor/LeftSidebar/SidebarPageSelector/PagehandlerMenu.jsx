c9156ecd9626ffb0159cec6434cb2ce303d5a58f
Vijaykant Yadav <vjy239@gmail.com>
2024-06-11 12:05:48 +0530

    fix: collapsable property

--- frontend/src/Editor/LeftSidebar/SidebarPageSelector/PagehandlerMenu.jsx
+++ frontend/src/Editor/LeftSidebar/SidebarPageSelector/PagehandlerMenu.jsx
@@ -118,7 +118,14 @@ export const PagehandlerMenu = ({
             setShowMenu(true);
           }}
         >
-          <SolidIcon viewBox="0 0 20 24" width="20" view dataCy={`page-menu`} name="morevertical" />
+          <SolidIcon
+            viewBox="0 0 20 24"
+            fill="var(--slate9)"
+            width="20"
+            view
+            dataCy={`page-menu`}
+            name="morevertical"
+          />
         </Button.UnstyledButton>
       </span>
     </OverlayTrigger>
