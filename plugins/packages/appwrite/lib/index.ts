3a4d46bb587b962a26a1330c87a840b39406b345
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2024-06-12 17:51:02 +0530

    remove bulk upload operation

--- plugins/packages/appwrite/lib/index.ts
+++ plugins/packages/appwrite/lib/index.ts
@@ -1,7 +1,7 @@
 import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions, ReturnObject } from './types';
 import sdk from 'node-appwrite';
-import { bulkUpdate, createDocument, deleteDocument, getDocument, queryCollection, updateDocument } from './operations';
+import { createDocument, deleteDocument, getDocument, queryCollection, updateDocument } from './operations';
 const JSON5 = require('json5');
 
 export default class Appwrite implements QueryService {
@@ -53,17 +53,6 @@ export default class Appwrite implements QueryService {
         case 'delete_document':
           if (!queryOptions.documentId) throw new Error('Document id is required');
           result = await deleteDocument(database, database_id, queryOptions.collectionId, queryOptions.documentId);
-          break;
-        case 'bulk_update':
-          if (!queryOptions.records) throw new Error('Records field is required');
-          if (!queryOptions['document_id_key']) throw new Error('Key for document id field is required');
-          result = await bulkUpdate(
-            database,
-            database_id,
-            queryOptions.collectionId,
-            this.returnObject(queryOptions.records) as object[],
-            queryOptions['document_id_key']
-          );
           break;
       }
     } catch (error) {
