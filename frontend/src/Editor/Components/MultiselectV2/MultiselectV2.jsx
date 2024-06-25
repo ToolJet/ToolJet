9714fa2f0473a101a534bb6ad5c5ad97e20d32d4
Vijaykant Yadav <vjy239@gmail.com>
2024-06-25 18:40:46 +0530

    fix: dynamic values for options in dropdown/multiselect

--- frontend/src/Editor/Components/MultiselectV2/MultiselectV2.jsx
+++ frontend/src/Editor/Components/MultiselectV2/MultiselectV2.jsx
@@ -87,10 +87,10 @@ export const MultiselectV2 = ({
     const _options = advanced ? schema : options;
     let _selectOptions = Array.isArray(_options)
       ? _options
-          .filter((data) => data.visible)
+          .filter((data) => data.visible.value)
           .map((value) => ({
             ...value,
-            isDisabled: value.disable,
+            isDisabled: value.disable.value,
           }))
       : [];
     return _selectOptions;
@@ -441,6 +441,7 @@ export const MultiselectV2 = ({
             optionsLoadingState={optionsLoadingState}
             darkMode={darkMode}
             fireEvent={() => fireEvent('onSelect')}
+            menuPlacement="auto"
           />
         </div>
       </div>
