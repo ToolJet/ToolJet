e6337be1d86baf21c9f26053b0d0f103255e6494
Vijaykant Yadav <vjy239@gmail.com>
2024-07-08 14:08:19 +0530

    bug fixes

--- frontend/src/Editor/Inspector/EventManager.jsx
+++ frontend/src/Editor/Inspector/EventManager.jsx
@@ -13,7 +13,7 @@ import defaultStyles from '@/_ui/Select/styles';
 import { useTranslation } from 'react-i18next';
 import { useDataQueriesStore } from '@/_stores/dataQueriesStore';
 import RunjsParameters from './ActionConfigurationPanels/RunjsParamters';
-import { useAppDataActions, useAppDataStore, useIsSaving } from '@/_stores/appDataStore';
+import { useAppDataActions, useAppDataStore, useIsActionSaving } from '@/_stores/appDataStore';
 import { isQueryRunnable } from '@/_helpers/utils';
 import { shallow } from 'zustand/shallow';
 import AddNewButton from '@/ToolJetUI/Buttons/AddNewButton/AddNewButton';
@@ -69,10 +69,15 @@ export const EventManager = ({
 
   const { handleYmapEventUpdates } = useContext(EditorContext) || {};
 
-  const { updateAppVersionEventHandlers, createAppVersionEventHandlers, deleteAppVersionEventHandler, updateState } =
-    useAppDataActions();
+  const {
+    updateAppVersionEventHandlers,
+    createAppVersionEventHandlers,
+    deleteAppVersionEventHandler,
+    updateState,
+    setIsActionSaving,
+  } = useAppDataActions();
 
-  const isSaving = useIsSaving();
+  const isActionSaving = useIsActionSaving();
 
   const currentEvents = allAppEvents?.filter((event) => {
     if (customEventRefs) {
@@ -303,6 +308,7 @@ export const EventManager = ({
   }
 
   function handlerChanged(index, param, value) {
+    setIsActionSaving(true);
     let newEvents = _.cloneDeep(events);
 
     let updatedEvent = newEvents[index];
@@ -444,8 +450,8 @@ export const EventManager = ({
                 styles={styles}
                 useMenuPortal={false}
                 useCustomStyles={true}
-                isDisabled={isSaving}
-                isLoading={isSaving}
+                isDisabled={isActionSaving}
+                isLoading={isActionSaving}
               />
             </div>
           </div>
