7a60208358c22fc74c57bc2c347a9f6c92fb7a38
Kavin Venkatachalam <kavin.saratha@gmail.com>
2024-07-09 18:42:23 +0530

    chore: reverted the listView fix on viewer as it is slowing down/forzen the page for large date set

--- frontend/src/_stores/currentStateStore.js
+++ frontend/src/_stores/currentStateStore.js
@@ -103,7 +103,6 @@ useCurrentStateStore.subscribe((state) => {
   updateCanvasBackground(useEditorStore.getState().canvasBackground);
 
   const isStoreIntialized = useResolveStore.getState().storeReady;
-  const isViewMode = state.globals.mode?.value === 'view';
   if (!isStoreIntialized) {
     const isPageSwitched = useResolveStore.getState().isPageSwitched;
 
@@ -125,12 +124,6 @@ useCurrentStateStore.subscribe((state) => {
     );
 
     return useResolveStore.getState().actions.updateStoreState({ storeReady: true });
-  } else if (isViewMode) {
-    //With viewer mode, we need to add app suggestions for current state updated
-    useResolveStore.getState().actions.addAppSuggestions({
-      components: state.components,
-      queries: state.queries,
-    });
   }
 }, shallow);
 
