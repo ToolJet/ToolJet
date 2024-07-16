2a8c54cf0fdc1c949e6edd693aae2a0e9a74d094
Johnson Cherian <johnson@tooljet.com>
2024-07-16 17:24:01 +0530

    fix: statistics widget and page switch crash issue (#504)

--- frontend/src/Editor/Container.jsx
+++ frontend/src/Editor/Container.jsx
@@ -646,7 +646,7 @@ export const Container = ({
       }
       if (Object.keys(value)?.length > 0) {
         setBoxes((boxes) => {
-          // Ensure boxes[id] exists
+          // Ensure boxes[id] exists. This can happen is page is already switched and the component attributes change gets triggered after that
           if (!boxes[id]) {
             console.error(`Box with id ${id} does not exist`);
             return boxes;
@@ -668,7 +668,6 @@ export const Container = ({
             },
           });
         });
-
         if (!_.isEmpty(opts)) {
           paramUpdatesOptsRef.current = opts;
         }
