88b4846d8fbead250886a888c877cd07b0f08b9e
rohanlahori <rohanlahori99@gmail.com>
2024-07-23 12:56:20 +0530

    fixed alignment issue in plugins

--- frontend/src/GlobalDatasources/GlobalDataSourcesPage/index.jsx
+++ frontend/src/GlobalDatasources/GlobalDataSourcesPage/index.jsx
@@ -332,8 +332,8 @@ export const GlobalDataSourcesPage = ({ darkMode = false, updateSelectedDatasour
               title={item.title}
               src={item?.src}
               usePluginIcon={isEmpty(item?.iconFile?.data)}
-              height="35px"
-              width="35px"
+              height={type == 'Plugins' ? '25px' : '35px'}
+              width={type == 'Plugins' ? '25px' : '35px'}
               actionButton={addDataSourceBtn(item)}
               className="datasource-card"
               titleClassName={'datasource-card-title'}
