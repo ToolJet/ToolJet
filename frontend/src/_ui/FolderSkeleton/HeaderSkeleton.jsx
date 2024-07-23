145896854811ecc724f4623374c991a6d0a06205
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-07-23 16:43:54 +0530

    add border (#10427)

--- frontend/src/_ui/FolderSkeleton/HeaderSkeleton.jsx
+++ frontend/src/_ui/FolderSkeleton/HeaderSkeleton.jsx
@@ -3,7 +3,10 @@ import Skeleton from 'react-loading-skeleton';
 
 export default function HeaderSkeleton() {
   return (
-    <div className="d-flex justify-content-between w-100" style={{ marginTop: '2rem' }}>
+    <div
+      className="d-flex justify-content-between w-100"
+      style={{ marginTop: '2rem', borderBottom: '1px solid var(--slate5)' }}
+    >
       <div className="justify-content-start">
         <Skeleton count={1} height={20} width={60} className="mb-3" />
       </div>
