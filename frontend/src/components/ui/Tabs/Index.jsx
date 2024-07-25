612fd4a2f9636b5fc9ac1e3703cc0e89d993f9f5
Devanshu Rastogi <devanshu.rastogi05@gmail.com>
2024-07-25 09:50:02 +0530

    Made changes in Tabs Component Storybook

--- frontend/src/components/ui/Tabs/Index.jsx
+++ frontend/src/components/ui/Tabs/Index.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Tabs, TabsList, TabsTrigger } from './Tabs';
 import SolidIcon from '@/_ui/Icon/SolidIcons';
 
-const TabsComponent = (props) => {
+const TabsComponent = ({ tabs = {}, onChange, ...props }) => {
   return (
-    <Tabs defaultValue={props.defaultValue} className="w-[400px]" onValueChange={(value) => console.log(value)}>
+    <Tabs onValueChange={onChange} {...props}>
       <TabsList>
-        <TabsTrigger value="acc">
-          <SolidIcon name={props.leadingIcon} className="tw-h-[18px] tw-w-[18px]" />
-        </TabsTrigger>
-        <TabsTrigger value="password">
-          <SolidIcon name={props.leadingIcon} className="tw-h-[18px] tw-w-[18px]" />
-        </TabsTrigger>
+        {props.type === 'text' &&
+          Object.keys(tabs).map((key) => (
+            <TabsTrigger key={key} value={tabs[key]}>
+              {key}
+            </TabsTrigger>
+          ))}
+        {props.type === 'icon' &&
+          Object.keys(tabs).map((key) => (
+            <TabsTrigger key={key} value={tabs[key]}>
+              <SolidIcon name={key} className="tw-h-[18px] tw-w-[18px]" />
+            </TabsTrigger>
+          ))}
       </TabsList>
     </Tabs>
   );
 };
 
 export default TabsComponent;
+
+TabsComponent.propTypes = {
+  type: PropTypes.oneOf(['text', 'icon']),
+  tabs: PropTypes.object,
+  defaultValue: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  className: PropTypes.string,
+};
+
+TabsComponent.defaultProps = {
+  type: 'text',
+  tabs: {
+    option1: 'value 1',
+    option2: 'value 2',
+    option3: 'value 3',
+  },
+  defaultValue: '',
+  onChange: (value) => {},
+  className: '',
+};
--- frontend/src/components/ui/Tabs/Tabs.stories.jsx
+++ frontend/src/components/ui/Tabs/Tabs.stories.jsx
@@ -10,36 +10,52 @@ export default {
     layout: 'centered',
   },
   argTypes: {
-    checked: {
-      control: 'boolean',
+    type: {
+      options: ['text', 'icon'],
+      control: 'select',
     },
-    disabled: {
-      control: 'boolean',
+    tabs: {
+      control: 'object',
     },
-    label: {
+    defaultValue: {
       control: 'text',
     },
-    helper: {
+    value: {
       control: 'text',
     },
-    size: {
-      options: ['default', 'large'],
-      control: 'radio',
-    },
-    align: {
-      options: ['left', 'right'],
-      control: 'radio',
-    },
-    required: {
-      control: 'boolean',
+    onChange: {
+      control: 'function',
     },
     className: {
-      control: 'text',
-    },
-    leadingIcon: {
       control: 'text',
     },
   },
 };
 
-export const RocketTabs = {};
+const Template = (args) => <TabsComponent {...args} />;
+
+export const RocketTabs = Template.bind({});
+RocketTabs.args = {
+  type: 'text',
+  tabs: {
+    option1: 'value 1',
+    option2: 'value 2',
+    option3: 'value 3',
+  },
+  defaultValue: '',
+  onChange: (value) => {},
+  className: '',
+};
+
+export const RocketTabsWithIcons = (args) => {
+  return (
+    <TabsComponent
+      {...args}
+      type="icon"
+      tabs={{
+        smilerectangle: 'value 1',
+        search01: 'value 2',
+      }}
+    />
+  );
+};
