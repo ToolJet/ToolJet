ab9904797e4783ee6db0e0e70eda9e5fe7804d8c
Aman Regu <amanregu@gmail.com>
2024-08-26 13:03:01 +0530

    test changes

---
+++ docs/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on where your components are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
