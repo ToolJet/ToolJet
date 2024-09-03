2ff42eb6d7d608b42c8850d36d8d85faa47d2c42
Unnikrishnan KP <unnitallman@users.noreply.github.com>
2024-09-03 21:35:50 +0530

    Update heroku-postbuild.sh

--- heroku-postbuild.sh
+++ heroku-postbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ $HEROKU_APP_NAME ]
+if [ $NEETODEPLOY_APP_DEFAULT_URL ]
 then
-    TOOLJET_HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
-    TOOLJET_SERVER_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
+    TOOLJET_HOST=$NEETODEPLOY_APP_DEFAULT_URL
+    TOOLJET_SERVER_URL=$NEETODEPLOY_APP_DEFAULT_URL
 fi
 
 npm run build && npm run deploy
