8316fd70a50bbab5af562bb9f05460ce56db0a05
rohanlahori <rohanlahori99@gmail.com>
2024-09-20 01:23:57 +0530

    removed white labelling changes from the background wrapper

--- frontend/src/modules/onboarding/components/OnboardingFormWrapper/OnboardingFormWrapper.jsx
+++ frontend/src/modules/onboarding/components/OnboardingFormWrapper/OnboardingFormWrapper.jsx
@@ -1,37 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Logo from '@/modules/common/resources/images/Logo';
 import './resources/styles/onboarding-form-wrapper.styles.scss';
-import {
-  retrieveWhiteLabelLogo,
-  fetchWhiteLabelDetails,
-  defaultWhiteLabellingSettings,
-} from '@white-label/whiteLabelling';
-
+import { getSubpath } from '@/_helpers/routes';
 const OnboardingFormWrapper = ({ children: components }) => {
-  const [whiteLabelLogo, setWhiteLableLogo] = useState('');
-  const [imageWidth, setImageWidth] = useState(null);
-  useEffect(() => {
-    fetchWhiteLabelDetails();
-    setWhiteLableLogo(retrieveWhiteLabelLogo());
-  }, []);
-  const handleImageWidth = (event) => {
-    const { naturalWidth } = event.target;
-    setImageWidth(naturalWidth < 130 ? naturalWidth : 130);
-  };
-
   const redirectToLoginPage = () => {
-    window.location.href = '/';
+    window.location.href = getSubpath() ? `${getSubpath()}` : `'/'`;
   };
   return (
     <div className="onboarding-form-wrapper">
       <div className="tooljet-header cursor-pointer" onClick={redirectToLoginPage}>
-        {whiteLabelLogo != '' &&
-        window.location.pathname != '/setup' &&
-        whiteLabelLogo != defaultWhiteLabellingSettings.WHITE_LABEL_LOGO ? (
-          <img onLoad={handleImageWidth} width={imageWidth} height="26px" src={whiteLabelLogo} />
-        ) : (
-          <Logo />
-        )}
+        <Logo />
       </div>
       {components}
     </div>
