ac3906af8b329d9650e128748381f2e0bab725de
Aman Regu <amanregu@gmail.com>
2024-09-20 03:53:20 +0530

    contain styles

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on where your components are located
+    "./versioned_docs/version-2.50.0-LTS/doc-home-page.mdx",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false, // This prevents Tailwind from resetting global styles
+  },
 }
