0be3417fc56ec7c153094ed1b2087c3627a8e141
Aman Regu <amanregu@gmail.com>
2024-09-23 16:37:29 +0530

    fix build errors

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
-}
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
