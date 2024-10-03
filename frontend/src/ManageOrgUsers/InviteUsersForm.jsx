3ad723fe530d99795e92fc88688351e2661634d6
Ayyan-Shaikh <ashaikh@chuckads.com>
2024-10-03 11:55:44 +0530

    Refactor InviteUsersForm component to support dark mode

--- frontend/src/ManageOrgUsers/InviteUsersForm.jsx
+++ frontend/src/ManageOrgUsers/InviteUsersForm.jsx
@@ -32,6 +32,8 @@ function InviteUsersForm({
 
   const hiddenFileInput = useRef(null);
 
+  const darkmode = localStorage.getItem('darkMode') === 'true';
+
   useEffect(() => {
     if (currentEditingUser && groups.length) {
       const { first_name, last_name, email, groups: addedToGroups } = currentEditingUser;
@@ -131,7 +133,11 @@ function InviteUsersForm({
                     onClick={() => setActiveTab(1)}
                     data-cy="button-invite-with-email"
                   >
-                    <SolidIcon name="mail" width="14" fill={activeTab == 1 ? '#11181C' : '#687076'} />
+                    <SolidIcon
+                      name="mail"
+                      width="14"
+                      fill={activeTab == 1 ? (darkmode ? '#FFFFFF' : '#11181C') : darkmode ? '#AAAAAA' : '#687076'}
+                    />
                     <span> Invite with email</span>
                   </button>
                   <button
@@ -139,7 +145,11 @@ function InviteUsersForm({
                     onClick={() => setActiveTab(2)}
                     data-cy="button-upload-csv-file"
                   >
-                    <SolidIcon name="fileupload" width="14" fill={activeTab == 2 ? '#11181C' : '#687076'} />
+                    <SolidIcon
+                      name="fileupload"
+                      width="14"
+                      fill={activeTab == 2 ? (darkmode ? '#FFFFFF' : '#11181C') : darkmode ? '#AAAAAA' : '#687076'}
+                    />
                     <span>Upload CSV file</span>
                   </button>
                 </div>
