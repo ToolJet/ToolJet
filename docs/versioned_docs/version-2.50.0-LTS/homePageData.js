c74f52ddf6b5da457b3568ebf40f6db62ce81de0
Aman Regu <amanregu@gmail.com>
2024-10-07 17:20:56 +0530

    fix links

--- docs/versioned_docs/version-2.50.0-LTS/homePageData.js
+++ docs/versioned_docs/version-2.50.0-LTS/homePageData.js
@@ -18,7 +18,7 @@ export const featureCards = [
       title: "ToolJet Database", 
       color: "text-blue-500", 
       content: "Powered by PostgreSQL, offering a user-friendly UI editor. ToolJet Database allows you to manage, edit, and interact with your data directly within the platform.",
-      href: "/docs/tooljet-database/overview"
+      href: "/docs/tooljet-db/tooljet-database"
   },
   { 
       icon: Workflow, 
@@ -49,7 +49,7 @@ export const setupCards = [
       title: "Choose Your ToolJet", 
       color: "text-blue-500", 
       content: "Discover the ideal ToolJet version for your development needs. Choose between our Long-Term Support (LTS) versions for stability and reliability, or explore Pre-Release versions to access the latest features. Select the option that best aligns with your project requirements ",
-      href: "/docs/setup/choose-tooljet"
+      href: "/docs/setup/choose-your-tooljet/"
   },
   { 
       icon: Scale, 
@@ -63,14 +63,18 @@ export const setupCards = [
 export const deployOptions = [
   { icon: Cloud, title: "DigitalOcean", href: "/docs/setup/digitalocean" },
   { icon: Container, title: "Docker", href: "/docs/setup/docker" },
-  { icon: Server, title: "AWS EC2", href: "/docs/setup/aws-ec2" },
-  { icon: Server, title: "AWS ECS", href: "/docs/setup/aws-ecs" },
+  { icon: Server, title: "AWS EC2", href: "/docs/setup/ec2" },
+  { icon: Server, title: "AWS ECS", href: "/docs/setup/ecs" },
   { icon: Server, title: "Openshift", href: "/docs/setup/openshift" },
   { icon: Telescope, title: "Helm", href: "/docs/setup/helm" },
   { icon: Boxes, title: "Kubernetes", href: "/docs/setup/kubernetes" },
   { icon: Globe, title: "Kubernetes (GKE)", href: "/docs/setup/kubernetes-gke" },
   { icon: Globe, title: "Kubernetes (AKS)", href: "/docs/setup/kubernetes-aks" },
   { icon: Globe, title: "Kubernetes (EKS)", href: "/docs/setup/kubernetes-eks" },
+  { icon: Globe, title: "Azure Container apps", href: "/docs/setup/azure-container" },
+  { icon: Globe, title: "Google Cloud Run", href: "/docs/setup/google-cloud-run" },
+
+
 ];
 
 export const dataCards = [
@@ -79,41 +83,41 @@ export const dataCards = [
       title: "Overview", 
       color: "text-blue-500", 
       content: "Gain a broad understanding of ToolJet's features and capabilities. Learn how it simplifies app development with powerful tools and an intuitive interface.",
-      href: "/docs/data/overview"
+      href: "/docs/data-sources/overview"
   },
   { 
       icon: FileSpreadsheet, 
       title: "Sample Data Source", 
       color: "text-blue-500", 
       content: "Explore sample data sources to quickly integrate with ToolJet. Test features and workflows using predefined datasets.",
-      href: "/docs/data/sample-datasource"
+      href: "/docs/data-sources/sample-data-sources"
   },
   { 
       icon: Folder, 
       title: "Datasource Library", 
       color: "text-blue-500", 
       content: "Browse ToolJet's datasource library to connect with databases, APIs, and external services seamlessly.",
-      href: "/docs/data/datasource-library"
+      href: "/docs/tooljet-concepts/what-are-datasources/"
   },
   { 
       icon: Wand2, 
       title: "Transformation", 
       color: "text-blue-500", 
       content: "Leverage ToolJet's transformation capabilities to manipulate and format data from various sources with ease.",
-      href: "/docs/data/transformation"
+      href: "/docs/tutorial/transformations/"
   }
 ];
 
 export const organizationCards = [
-  { icon: LayoutDashboard, title: "Dashboard", href: "/docs/org/dashboard" },
-  { icon: Users, title: "Workspaces", href: "/docs/org/workspaces" },
-  { icon: UserCheck, title: "User authentication", href: "/docs/org/user-authentication" },
-  { icon: Lock, title: "Permissions", href: "/docs/org/permissions" },
-  { icon: UserPlus, title: "Users and groups", href: "/docs/org/users-and-groups" },
-  { icon: ScrollText, title: "Audit logs", href: "/docs/org/audit-logs" },
-  { icon: Megaphone, title: "White label", href: "/docs/org/white-label" },
-  { icon: Gem, title: "Super admin", href: "/docs/org/super-admin" },
-  { icon: Mail, title: "Licensing", href: "/docs/org/licensing" }
+  { icon: LayoutDashboard, title: "Dashboard", href: "/docs/dashboard" },
+  { icon: Users, title: "Workspaces", href: "/docs/org-management/workspaces/workspace_overview" },
+  { icon: UserCheck, title: "User authentication", href: "/docs/user-authentication/workspace-login" },
+  { icon: Lock, title: "Permissions", href: "/docs/org-management/permissions" },
+  { icon: UserPlus, title: "Users and groups", href: "/docs/tutorial/manage-users-groups" },
+  { icon: ScrollText, title: "Audit logs", href: "/docs/Enterprise/audit_logss" },
+  { icon: Megaphone, title: "White label", href: "/docs/Enterprise/white-label" },
+  { icon: Gem, title: "Super admin", href: "/docs/Enterprise/superadmin" },
+  { icon: Mail, title: "Licensing", href: "/docs/org-management/licensing/self-hosted" }
 ];
 
 export const releaseCards = [
@@ -122,21 +126,21 @@ export const releaseCards = [
       title: "Git Sync", 
       color: "text-blue-500", 
       content: "Sync your ToolJet projects with Git repositories, enabling version control and collaboration across teams.",
-      href: "/docs/releases/git-sync"
+      href: "/docs/git-sync"
   },
   { 
       icon: Box, 
       title: "Multi-Environment", 
       color: "text-blue-500", 
       content: "Easily manage and deploy applications across multiple environments, ensuring smooth transitions between development, staging, and production.",
-      href: "/docs/releases/multi-environment"
+      href: "/docs/release-management/multi-environment"
   },
   { 
       icon: GitMerge, 
       title: "Versioning and Release", 
       color: "text-blue-500", 
       content: "Implement version control and release management to track changes, roll back updates, and maintain stable app deployments.",
-      href: "/docs/releases/versioning"
+      href: "/docs/tutorial/versioning-and-release"
   }
 ];
 
@@ -146,28 +150,28 @@ export const resourceCards = [
       title: "Marketplace", 
       color: "text-blue-500", 
       content: "Discover a variety of plugins, extensions and integrations in ToolJet's marketplace to enhance your app-building experience.",
-      href: "/docs/resources/marketplace"
+      href: "/docs/marketplace/marketplace-overview"
   },
   { 
       icon: Wand, 
       title: "Copilot", 
       color: "text-blue-500", 
       content: "Boost productivity with ToolJet Copilot. Get AI-powered suggestions and assistance while building your applications.",
-      href: "/docs/resources/copilot"
+      href: "/docs/tooljet-copilot"
   },
   { 
       icon: Flag, 
       title: "Tracking", 
       color: "text-blue-5000", 
       content: "ToolJet ensures user privacy by acting as a proxy, never storing data from sources. It offers anonymous usage tracking with options to disable features for enhanced control.",
-      href: "/docs/resources/tracking"
+      href: "/docs/tracking"
   },
   { 
       icon: ShieldCheck, 
       title: "Security", 
       color: "text-blue-500", 
       content: "ToolJet guarantees your data security with SOC 2 compliance, robust encryption, and secure credential handling. We never store your data, ensuring peace of mind with every connection",
-      href: "/docs/resources/security"
+      href: "/docs/security"
   }
 ];
 
@@ -211,6 +215,6 @@ export const sectionCards = {
   gettingStarted : {
     title: "Getting Started",
     description: "Discover how to create and publish apps within minutes",
-    link: "/docs/getting-started",
+    link: "/docs/getting-started/quickstart-guide",
 }
 };
