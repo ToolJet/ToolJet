264cfe51621b4737985f30c253bae3cedcce5ba1
Rohan Lahori <64496391+rohanlahori@users.noreply.github.com>
2024-11-12 20:12:42 +0530

    ce ui lag changes (#11294)
    * ce ui lag changes
    
    * removed commented part
    
    * removed unused change + ui wrapper changes
    
    * invitation flow fixes

--- frontend/src/modules/onboarding/pages/SetupAdminPage/SetupAdminPageCE.jsx
+++ frontend/src/modules/onboarding/pages/SetupAdminPage/SetupAdminPageCE.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { OnboardingBackgroundWrapper, OnboardingQuestions } from '@/modules/onboarding/components';
 import { SetupAdminForm } from './components';
 import { GeneralFeatureImage } from '@/modules/common/components';
 import useOnboardingStore from '@/modules/onboarding/stores/onboardingStore';
 import { shallow } from 'zustand/shallow';
+import { TJLoader } from '@/_ui/TJLoader/TJLoader';
+const PostOnboardingComponent = () => <TJLoader />;
 
 const SetupAdminPageCE = () => {
-  const { currentStep } = useOnboardingStore(
+  const { currentStep, isOnboardingStepsCompleted } = useOnboardingStore(
     (state) => ({
       currentStep: state.currentStep,
+      isOnboardingStepsCompleted: state.isOnboardingStepsCompleted,
     }),
     shallow
   );
-
-  if (currentStep > 0) {
+  if (isOnboardingStepsCompleted && PostOnboardingComponent) {
+    return <PostOnboardingComponent />;
+  } else if (currentStep > 0) {
     return <OnboardingQuestions />;
   }
 
