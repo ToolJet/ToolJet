9a998b1830dc081256924da4276d5abb5aec8354
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-11-15 12:02:25 +0530

    Removes unwanted imports

--- frontend/src/AppBuilder/_hooks/useExposeVariables.js
+++ frontend/src/AppBuilder/_hooks/useExposeVariables.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useExposeState = (loadingState, visibleState, disabledState, setExposedVariables, setExposedVariable) => {
   const [isDisabled, setDisable] = useState(disabledState || false);
