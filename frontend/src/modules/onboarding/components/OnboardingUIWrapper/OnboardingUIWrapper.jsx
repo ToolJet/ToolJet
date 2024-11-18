b692e5c8d01d464f6f82124a9d4c15a76879011c
rohanlahori <rohanlahori99@gmail.com>
2024-11-18 13:51:41 +0530

    ee-cherrypicked

--- frontend/src/modules/onboarding/components/OnboardingUIWrapper/OnboardingUIWrapper.jsx
+++ frontend/src/modules/onboarding/components/OnboardingUIWrapper/OnboardingUIWrapper.jsx
@@ -3,8 +3,12 @@ import OnboardingFormWrapper from '../OnboardingFormWrapper/OnboardingFormWrappe
 import './resources/styles/onboarding-ui-wrapper.styles.scss';
 
 const OnboardingUIWrapper = ({ children: components }) => {
-  const pageLocation = window.location.pathname;
-  if (pageLocation == '/setup') {
+  const isEmptyPath = window.location.pathname == '/';
+  const isSetupRoute = window.location.pathname.split('/').pop().toLowerCase() === 'setup';
+  const pathEndSegments = window.location.pathname.split('/').filter(Boolean).slice(-2);
+  const isInvitationRoute =
+    pathEndSegments.length === 2 && pathEndSegments[0] === 'invitations' && pathEndSegments[1]?.length > 0;
+  if (isSetupRoute || isInvitationRoute || isEmptyPath) {
     return (
       <div className="onboarding-setup-wrapper">
         <OnboardingFormWrapper>{components}</OnboardingFormWrapper>
