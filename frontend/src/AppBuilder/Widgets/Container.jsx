aa4cd1fc2e2279f9fd6e3c0a35b5366334faa705
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-12-04 00:26:38 -0800

    chore: Exposes CSAs for Container widget (#11229)
    * chore: Exposes CSAs for Container widget
    
    * Introduces hook to expose variables
    
    * Updates hook to expose variable after setting the state
    
    * Update frontend/src/AppBuilder/_hooks/useExposeVariables.js
    
    Co-authored-by: Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>
    
    * Removes unwanted imports
    
    * Exposing CSA to control component actions
    
    * Moves CSA's to additional actions section
    
    * Fixes wrong handle param
    
    ---------
    
    Co-authored-by: Kavin Venkatachalam <50441969+kavinvenkatachalam@users.noreply.github.com>

--- frontend/src/AppBuilder/Widgets/Container.jsx
+++ frontend/src/AppBuilder/Widgets/Container.jsx
@@ -1,9 +1,28 @@
 import React, { useMemo } from 'react';
 import { Container as ContainerComponent } from '@/AppBuilder/AppCanvas/Container';
 import Spinner from '@/_ui/Spinner';
+import { useExposeState } from '@/AppBuilder/_hooks/useExposeVariables';
 
-export const Container = ({ id, properties, styles, darkMode, height, width }) => {
-  const { visibility, disabledState, borderRadius, borderColor, boxShadow } = styles;
+export const Container = ({
+  id,
+  properties,
+  styles,
+  darkMode,
+  height,
+  width,
+  setExposedVariables,
+  setExposedVariable,
+}) => {
+  const { borderRadius, borderColor, boxShadow } = styles;
+
+  const { isDisabled, isVisible, isLoading } = useExposeState(
+    properties.loadingState,
+    properties.visibility,
+    properties.disabledState,
+    setExposedVariables,
+    setExposedVariable
+  );
+
   const bgColor = useMemo(() => {
     return {
       backgroundColor:
@@ -16,19 +35,19 @@ export const Container = ({ id, properties, styles, darkMode, height, width }) =
     borderRadius: borderRadius ? parseFloat(borderRadius) : 0,
     border: `1px solid ${borderColor}`,
     height,
-    display: visibility ? 'flex' : 'none',
+    display: isVisible ? 'flex' : 'none',
     overflow: 'hidden auto',
     position: 'relative',
     boxShadow,
   };
   return (
     <div
-      className={`jet-container ${properties.loadingState && 'jet-container-loading'}`}
+      className={`jet-container ${isLoading && 'jet-container-loading'}`}
       id={id}
-      data-disabled={disabledState}
+      data-disabled={isDisabled}
       style={computedStyles}
     >
-      {properties.loadingState ? (
+      {isLoading ? (
         <Spinner />
       ) : (
         <ContainerComponent id={id} styles={bgColor} canvasHeight={height} canvasWidth={width} darkMode={darkMode} />
