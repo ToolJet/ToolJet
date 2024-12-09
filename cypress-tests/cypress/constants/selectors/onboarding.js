dd59b497237c8dfba132e16a4a38444979394d56
Srimanitejas123 <mani@tooljet.com>
2024-12-09 13:10:09 +0530

    Added cypress test cases for user onboarding flow (#11499)
    * Add data-cy for newly added components
    
    * Add data-cy for onboarding page elements
    
    * Modify failed test cases
    
    * Adding more cases
    
    * Modify onboarding test cases
    
    * Modify user invite flow
    
    * chnages on onboarding test scripts
    
    * revert the changes
    
    * revert the changes
    
    * removed .only form profile file
    
    * resolved review changes
    
    ---------
    
    Co-authored-by: ajith-k-v <ajith.jaban@gmail.com>
    Co-authored-by: Sri mani Teja s <mani@Sris-MacBook-Pro-4.local>

--- cypress-tests/cypress/constants/selectors/onboarding.js
+++ cypress-tests/cypress/constants/selectors/onboarding.js
@@ -4,11 +4,17 @@ export const cyParamName = (paramName = "") => {
 
 export const onboardingSelectors = {
   emailLabel: "[data-cy='email-label']",
-  emailInput: "[data-cy='email-input']",
+  emailInput: "[data-cy='email-input-input']",
+  signupEmailInput: "[data-cy='email-input']",
+  loginEmailInput: "[data-cy='email-input']",
   passwordLabel: "[data-cy='password-label']",
-  passwordInput: "[data-cy='password-input']",
+  passwordInput: "[data-cy='password-input-input']",
+  loginPasswordInput: "[data-cy='password-input']",
   signInButton: "[data-cy='sign-in-button']",
   passwordError: "[data-cy='password-error']",
   nameInput: "[data-cy='name-input']",
-  nameLabel: '[data-cy="name-label"]'
-}
+  nameLabel: '[data-cy="name-input-label"]',
+  signupNameLabel: '[data-cy="name-label"]',
+  forgotEmailInput: '[data-cy="email-input-field-input"]',
+  createAnAccountLink: '[data-cy="create-an-account-link"]',
+} 
