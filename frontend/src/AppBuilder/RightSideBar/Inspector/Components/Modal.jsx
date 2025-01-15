1cdce24724efd53bda5d6d4d0497110919fef70a
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2025-01-15 01:55:21 -0800

    Adds additional actions and Data Modal.jsx

--- frontend/src/AppBuilder/RightSideBar/Inspector/Components/Modal.jsx
+++ frontend/src/AppBuilder/RightSideBar/Inspector/Components/Modal.jsx
@@ -3,6 +3,7 @@ import Accordion from '@/_ui/Accordion';
 import { renderElement } from '../Utils';
 import { baseComponentProperties } from './DefaultComponent';
 import { resolveReferences } from '@/_helpers/utils';
+import { deepClone } from '@/_helpers/utilities/utils.helpers';
 
 export const Modal = ({ componentMeta, darkMode, ...restProps }) => {
   const {
@@ -16,6 +17,23 @@ export const Modal = ({ componentMeta, darkMode, ...restProps }) => {
     allComponents,
   } = restProps;
 
+  let properties = [];
+  let additionalActions = [];
+  let dataProperties = [];
+  let optionsProperties = [];
+  const events = Object.keys(componentMeta.events);
+  const validations = Object.keys(componentMeta.validation || {});
+
+  for (const [key] of Object.entries(componentMeta?.properties)) {
+    if (componentMeta?.properties[key]?.section === 'additionalActions') {
+      additionalActions.push(key);
+    } else if (componentMeta?.properties[key]?.accordian === 'Data') {
+      dataProperties.push(key);
+    } else {
+      properties.push(key);
+    }
+  }
+
   const renderCustomElement = (param, paramType = 'properties') => {
     return renderElement(component, componentMeta, paramUpdated, dataQueries, param, paramType, currentState);
   };
@@ -24,9 +42,8 @@ export const Modal = ({ componentMeta, darkMode, ...restProps }) => {
       component.component.definition.properties.useDefaultButton?.value ?? false
     );
     const accordionItems = [];
-    const options = ['useDefaultButton'];
-
     let renderOptions = [];
+    const options = ['useDefaultButton'];
 
     options.map((option) => renderOptions.push(renderCustomElement(option)));
 
@@ -37,33 +54,41 @@ export const Modal = ({ componentMeta, darkMode, ...restProps }) => {
     });
 
     accordionItems.push({
+      title: 'Data',
+      isOpen: true,
+      children: dataProperties?.map((property) => renderCustomElement(property)),
+    });
+
+    accordionItems.push({
+      title: 'Additional actions',
+      isOpen: true,
+      children: additionalActions?.map((property) => renderCustomElement(property)),
+    });
+
+    accordionItems.push({
       title: 'Options',
       children: renderOptions,
     });
+
     return accordionItems;
   };
 
-  let additionalActions = [];
-  let properties = [];
-  const events = Object.keys(componentMeta.events);
-  const validations = Object.keys(componentMeta.validation || {});
-
-  for (const [key] of Object.entries(componentMeta?.properties)) {
-    if (componentMeta?.properties[key]?.section === 'additionalActions') {
-      additionalActions.push(key);
-    } else {
-      properties.push(key);
-    }
-  }
-
   const filteredProperties = properties.filter(
-    (property) => property !== 'useDefaultButton' && property !== 'triggerButtonLabel'
+    (property) =>
+      property !== 'useDefaultButton' && property !== 'triggerButtonLabel' && !dataProperties.includes(property)
   );
+  let updatedComponent = deepClone(component);
+  if (component.component.definition.properties.size.value === 'fullscreen') {
+    updatedComponent.component.properties.modalHeight = {
+      ...updatedComponent.component.properties.modalHeight,
+      isDisabled: true,
+    };
+  }
 
   const accordionItems = baseComponentProperties(
     filteredProperties,
     events,
-    component,
+    updatedComponent,
     componentMeta,
     layoutPropertyChanged,
     paramUpdated,
@@ -76,26 +101,7 @@ export const Modal = ({ componentMeta, darkMode, ...restProps }) => {
     darkMode
   );
 
-  accordionItems.splice(1, 0, ...conditionalAccordionItems(component));
-
-  accordionItems.push({
-    title: `Additional Actions`,
-    isOpen: true,
-    children: additionalActions.map((property) => {
-      return renderElement(
-        component,
-        componentMeta,
-        paramUpdated,
-        dataQueries,
-        property,
-        'properties',
-        currentState,
-        allComponents,
-        darkMode,
-        componentMeta.properties?.[property]?.placeholder
-      );
-    }),
-  });
+  accordionItems.splice(1, 0, ...conditionalAccordionItems(updatedComponent));
 
   return <Accordion items={accordionItems} />;
 };
