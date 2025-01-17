cd8e330208dd4396ac0cd88bee448740e7ea190a
devanshu052000 <devanshu.rastogi05@gmail.com>
2025-01-17 15:06:47 +0530

    Fix: Not able to copy value in debugger

--- frontend/src/_ui/JSONTreeViewer/JSONNode.jsx
+++ frontend/src/_ui/JSONTreeViewer/JSONNode.jsx
@@ -269,7 +269,7 @@ export const JSONNode = ({ data, ...restProps }) => {
 
     return (
       <div style={{ fontSize: '9px', marginTop: '0px', right: '10px' }} className="d-flex position-absolute">
-        {enableCopyToClipboard && (
+        {enableCopyToClipboard ? (
           <ToolTip message={'Copy to clipboard'}>
             <span
               onClick={() => {
@@ -281,6 +281,12 @@ export const JSONNode = ({ data, ...restProps }) => {
               <DefaultCopyIcon />
             </span>
           </ToolTip>
+        ) : (
+          // Temporary fix for hover issue for copy value button. Need to remove this once inspector gets revamped.
+          <span
+            style={{ height: '13px', width: '13px', marginBottom: '4px' }}
+            className="mx-1 copy-to-clipboard"
+          ></span>
         )}
         <ToolTip message={'Copy value'}>
           <span
