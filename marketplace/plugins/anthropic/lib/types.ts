ecb3288f7a17406d47203129a814dfe70624cdc6
namanmathur372 <namandotmathur@gmail.com>
2025-01-20 13:47:13 +0530

    Update Type Definitions for Anthropic Datasource
    Updated SourceOptions type to include apiKey.
    Expanded QueryOptions to support additional fields: model, system_prompt, message, temperature, and max_size.
    Added type definitions for vision operation, including MessageParam, Content, and ImageSource.
    Introduced Operation enum with Chat and Vision options for better operation handling.

--- marketplace/plugins/anthropic/lib/types.ts
+++ marketplace/plugins/anthropic/lib/types.ts
@@ -1,4 +1,36 @@
-export type SourceOptions = {};
-export type QueryOptions = {
-  operation: string;
+
+export type SourceOptions = {
+  apiKey: string;
 };
+
+export type QueryOptions = {
+  model?: string; 
+  operation: Operation; 
+  system_prompt?: string; 
+  message?: string; 
+  temperature?: number | string; 
+  max_size?: number | string; 
+};
+
+//for vision operation
+/*export type MessageParam = {
+  role: "user" | "assistant";
+  content: string | Content[];
+};
+
+export type Content = {
+  type: "text" | "image";
+  text?: string; 
+  source?: ImageSource; 
+};
+
+export type ImageSource = {
+  type: "base64";
+  media_type: "image/jpeg";
+  data: string;
+};*/
+
+export enum Operation {
+  Chat = "chat",
+  Vision = "vision"
+}
