729d028459587bc4b6f730c6e79da8faaa74bcc7
Parth Adhikari <parthadhikari@192.168.1.2>
2025-01-20 18:58:13 +0530

    Fix same key value names

--- marketplace/plugins/huggingface/lib/query_operation.ts
+++ marketplace/plugins/huggingface/lib/query_operation.ts
@@ -15,7 +15,11 @@ export async function text_generation_operation(api_url, queryOptions, headers) 
 }
 
 export async function summarisation_operation(api_url, queryOptions, headers) {
-  const { model, input, operation_parameters } = queryOptions;
+  const {
+    model_summarisation: model,
+    input_summarisation: input,
+    operation_parameters_summarisation: operation_parameters,
+  } = queryOptions;
   const response = await fetch(`${api_url}${model}`, {
     method: 'POST',
     headers,
