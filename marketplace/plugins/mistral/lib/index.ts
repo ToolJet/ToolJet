0f510085298d04c7bad6ff4c71f986ad0520a1ff
parthy007 <parthadhikari1812@gmail.com>
2025-01-22 14:04:59 +0530

    Add operation and connection logics

---
+++ marketplace/plugins/mistral/lib/index.ts
@@ -0,0 +1,111 @@
+import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-marketplace/common';
+import { SourceOptions, QueryOptions } from './types';
+import { Mistral as MistralClient } from '@mistralai/mistralai';
+
+export default class MistralService implements QueryService {
+  async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
+    const client = await this.getConnection(sourceOptions);
+    const { operation } = queryOptions;
+    let result = {};
+
+    try {
+      switch (operation) {
+        case 'text_generation': {
+          const {
+            model,
+            messages,
+            max_size,
+            temperature,
+            top_p,
+            stop_tokens,
+            random_seed,
+            response_format,
+            presence_penalty,
+            frequency_penalty,
+            completions,
+            safe_prompt,
+          } = queryOptions;
+
+          const parsedMessages = typeof messages === 'string' ? JSON.parse(messages) : messages;
+
+          const chatRequest = {
+            model: model?.value,
+            messages: parsedMessages,
+            ...(max_size && { max_tokens: parseInt(max_size) }),
+            ...(temperature && { temperature: parseFloat(temperature) }),
+            ...(top_p && { top_p: parseFloat(top_p) }),
+            ...(stop_tokens && { stop: JSON.parse(stop_tokens) }),
+            ...(random_seed && { random_seed: parseInt(random_seed) }),
+            ...(response_format.value && { response_format: response_format?.value }),
+            ...(presence_penalty && { presence_penalty: parseFloat(presence_penalty) }),
+            ...(frequency_penalty && { frequency_penalty: parseFloat(frequency_penalty) }),
+            ...(completions && { n: parseInt(completions) }),
+            ...(safe_prompt.value !== undefined && { safe_prompt: Boolean(safe_prompt.value) }),
+          };
+
+          const response = await client.chat.complete(chatRequest);
+          result = {
+            chat: response.choices[0].message.content,
+          };
+          break;
+        }
+      }
+    } catch (error) {
+      let errorMessage = 'An unknown error occurred';
+      let mainError = null;
+
+      if (error) {
+        if (error.message.includes('Input validation failed')) {
+          if (error.cause?.issues?.[0]?.message) {
+            errorMessage = error.cause.issues[0].message;
+          } else {
+            errorMessage = 'Invalid input parameters';
+          }
+        } else {
+          errorMessage = error.message;
+        }
+        mainError = error;
+      }
+
+      const errorDetails = {
+        errorType: mainError?.name || 'Error',
+        raw: error,
+      };
+      throw new QueryError('Query could not be completed', errorMessage, errorDetails);
+    }
+    return {
+      status: 'ok',
+      data: result,
+    };
+  }
+
+  async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
+    try {
+      const client = await this.getConnection(sourceOptions);
+      const chatResponse = await client.chat.complete({
+        model: 'mistral-large-latest',
+        messages: [{ role: 'user', content: 'What is the best French cheese?' }],
+      });
+      if (chatResponse.choices[0].message.content) {
+        return {
+          status: 'ok',
+          message: 'Connection established successfully',
+        };
+      }
+      return {
+        status: 'failed',
+        message: 'Could not get a valid response from Mistral API',
+      };
+    } catch (error) {
+      return {
+        status: 'failed',
+        message: error.message || 'Could not connect to Mistral API',
+      };
+    }
+  }
+
+  async getConnection(sourceOptions: SourceOptions) {
+    const { api_key } = sourceOptions;
+    return new MistralClient({ apiKey: api_key });
+  }
+}
---
+++ marketplace/plugins/mistral/lib/types.ts
@@ -0,0 +1,22 @@
+export type SourceOptions = {
+  api_key: string;
+};
+export type QueryOptions = {
+  operation: string;
+  model: ValueType;
+  messages: string;
+  max_size: string;
+  temperature: string;
+  top_p: string;
+  stop_tokens: string;
+  random_seed: string;
+  response_format: ValueType;
+  presence_penalty: string;
+  frequency_penalty: string;
+  completions: string;
+  safe_prompt: ValueType;
+};
+
+export type ValueType = {
+  value: string;
+};
