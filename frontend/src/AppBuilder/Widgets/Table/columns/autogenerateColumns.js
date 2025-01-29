eece8d3e07f3387d71bbc6699a292fe053314a15
Parth <108089718+parthy007@users.noreply.github.com>
2025-01-29 19:31:26 +0530

    Integrate: Add new Mistral plugin to the marketplace in LTS (#11854)
    * Fix crashing of table if nested data is provided
    
    * Virtualized dropdownV2 & multiselectV2 options
    
    * Fix sort showing applied even when the column doesn't exist
    
    * Fixed no options text not appearing & increased overscan
    
    * Removed logs
    
    * Fix toggle in dynamicform
    
    * Add mistral base setup
    
    * Add datasource page inputs
    
    * Add querymanager page inputs
    
    * Add operations and connection logic
    
    * Add mistral plugin to integration page
    
    * Add mistral module
    
    * Fix response format logic
    
    * update version
    
    * Hotfix: GraphQL plugin appends an empty URL parameter (?=) to the endpoint. (#11865)
    
    * graphql plugin url appended with empty urlparams
    
    * version bump
    
    * Simplify AI tag search
    
    * Change plugin id
    
    * Remove unwanted function
    
    * Fix: Page Handle not getting updated in inspector.
    
    * Fix: Page handle not getting updated in app when used in components.
    
    * Fix: Page name not getting updated in inspector.
    
    * Change the file name of plugin
    
    * Update package-lock
    
    * Update max_tokens label
    
    * Fix toggle
    
    * Fix toggle value extraction
    
    * Fix value extraction check
    
    ---------
    
    Co-authored-by: Nakul Nagargade <nakul@tooljet.com>
    Co-authored-by: Shaurya Sharma <shaurya064@gmail.com>
    Co-authored-by: Ganesh Kumar <40178541+ganesh8056@users.noreply.github.com>
    Co-authored-by: devanshu052000 <devanshu.rastogi05@gmail.com>
    Co-authored-by: Johnson Cherian <johnsonc.dev@gmail.com>

--- frontend/src/AppBuilder/Widgets/Table/columns/autogenerateColumns.js
+++ frontend/src/AppBuilder/Widgets/Table/columns/autogenerateColumns.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function autogenerateColumns(
   tableData,
-  existingColumns,
+  existingColumns = [],
   columnDeletionHistory,
   useDynamicColumn,
   dynamicColumn = [],
