217b79db2ae3800aa4d80e41833f9209e4e9cb01
parthy007 <parthadhikari1812@gmail.com>
2025-01-30 12:22:25 +0530

    Fix lint issues
    Add weaviate-client

--- marketplace/plugins/weaviate/lib/index.ts
+++ marketplace/plugins/weaviate/lib/index.ts
@@ -1,40 +1,45 @@
 import { QueryService, QueryResult, ConnectionTestResult, QueryError } from '@tooljet-marketplace/common';
 import { SourceOptions, QueryOptions, Operation } from './types';
-import { getSchema, listObjects, createObject, getObjectById, deleteObjectById, getCollection, createCollection, deleteCollection } from './query_operations';
+import {
+  getSchema,
+  listObjects,
+  createObject,
+  getObjectById,
+  deleteObjectById,
+  getCollection,
+  createCollection,
+  deleteCollection,
+} from './query_operations';
 import weaviate, { WeaviateClient } from 'weaviate-client';
 export default class Weaviate implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
-    const client  = await this.getConnection(sourceOptions)
+    const client = await this.getConnection(sourceOptions);
     let result = {};
     try {
-      console.log('tjdb index',{queryOptions});
-      
       switch (queryOptions.operation) {
         case Operation.get_schema:
-          result = await getSchema(client,queryOptions.collectionName);
+          result = await getSchema(client, queryOptions.collectionName);
           break;
         case Operation.list_objects:
-          console.log('tjdb list object switch');
-          
           result = await listObjects(client, queryOptions.collectionName);
           break;
         case Operation.get_collection:
-          result = await getCollection(client,queryOptions.collectionName,queryOptions.consistency);
-          break;  
+          result = await getCollection(client, queryOptions.collectionName, queryOptions.consistency);
+          break;
         case Operation.create_collection:
-          result = await createCollection(client,queryOptions.collectionName);
+          result = await createCollection(client, queryOptions.collectionName);
           break;
         case Operation.delete_collection:
-          result = await deleteCollection(client,queryOptions.collectionName);
+          result = await deleteCollection(client, queryOptions.collectionName);
           break;
         case Operation.create_object:
-          result = await createObject(client,queryOptions.collectionName,queryOptions.properties);
+          result = await createObject(client, queryOptions.collectionName, queryOptions.properties);
           break;
         case Operation.get_object_by_id:
-          result = await getObjectById(client,queryOptions.collectionName,queryOptions.objectId);
+          result = await getObjectById(client, queryOptions.collectionName, queryOptions.objectId);
           break;
         case Operation.delete_object_by_id:
-          result = await deleteObjectById(client,queryOptions.collectionName,queryOptions.objectId);
+          result = await deleteObjectById(client, queryOptions.collectionName, queryOptions.objectId);
           break;
         default:
           throw new QueryError('Invalid operation', 'Operation not supported', {});
@@ -42,32 +47,27 @@ export default class Weaviate implements QueryService {
     } catch (error) {
       throw new QueryError('Query could not be completed', error?.message, {});
     }
-    
+
     return { status: 'ok', data: result };
   }
 
   async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
     try {
-     const client =  await this.getConnection(sourceOptions);
-    await client.collections.listAll();
-    return { status: 'ok' };
-
+      const client = await this.getConnection(sourceOptions);
+      await client.collections.listAll();
+      return { status: 'ok' };
     } catch (error) {
       throw new QueryError('Connection failed', error?.message, {});
     }
   }
 
   async getConnection(sourceOptions: SourceOptions) {
-
-    const weaviateURL = sourceOptions.instanceUrl as string
-    const weaviateKey = sourceOptions.apiKey as string
+    const weaviateURL = sourceOptions.instanceUrl as string;
+    const weaviateKey = sourceOptions.apiKey as string;
     const client: WeaviateClient = await weaviate.connectToWeaviateCloud(weaviateURL, {
-        authCredentials: new weaviate.ApiKey(weaviateKey),
-      }
-    )
+      authCredentials: new weaviate.ApiKey(weaviateKey),
+    });
 
     return client;
-    
   }
 }
-
--- marketplace/plugins/weaviate/lib/query_operations.ts
+++ marketplace/plugins/weaviate/lib/query_operations.ts
@@ -1,9 +1,8 @@
 import weaviate, { WeaviateClient } from 'weaviate-client';
 
-
-export async function getSchema(client: WeaviateClient,collectionName:string ) {
+export async function getSchema(client: WeaviateClient, collectionName: string) {
   try {
-    let collection = client.collections.get(collectionName);
+    const collection = client.collections.get(collectionName);
     const collectionDefinition = await collection.config.get();
     return collectionDefinition.properties;
   } catch (error) {
@@ -12,105 +11,94 @@ export async function getSchema(client: WeaviateClient,collectionName:string ) {
   }
 }
 
-
-export async function listObjects(client: WeaviateClient,collectionName:string) {
+export async function listObjects(client: WeaviateClient, collectionName: string) {
   try {
     const collection = client.collections.get(collectionName);
     const res = await collection.query.fetchObjects();
     return res;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-export async function createObject(client:WeaviateClient, collectionName:string, properties: Record<string,any>) {
+export async function createObject(client: WeaviateClient, collectionName: string, properties: Record<string, any>) {
   try {
     const collection = client.collections.get(collectionName);
-    if(!collection) return "Collection not found";
+    if (!collection) return 'Collection not found';
 
-    const result =  await collection.data.insert({
+    const result = await collection.data.insert({
       properties: properties,
       // vectors: Array(1536).fill(0.12345)
       vectors: {
         title: Array(1536).fill(0.12345),
         review_body: Array(1536).fill(0.31313),
-        title_country: Array(1536).fill(0.05050),
-      }
+        title_country: Array(1536).fill(0.0505),
+      },
     });
-    
-    return result;
 
+    return result;
   } catch (error) {
     console.log(error);
-    
   }
 }
 
-export async function getObjectById(client:WeaviateClient, collectionName:string, objectId:string) {
+export async function getObjectById(client: WeaviateClient, collectionName: string, objectId: string) {
   try {
     const collection = client.collections.get(collectionName);
-    if(!collection) return "Collection not exists";
+    if (!collection) return 'Collection not exists';
 
-   const result= await collection.query.fetchObjectById(objectId);
-   return result;
-   
-    
+    const result = await collection.query.fetchObjectById(objectId);
+    return result;
   } catch (error) {
     console.error('Error fetching object by ID:', error);
     throw error;
   }
 }
 
-export async function deleteObjectById(client: WeaviateClient, collectionName: string, id:string) {
+export async function deleteObjectById(client: WeaviateClient, collectionName: string, id: string) {
   try {
-    const collectionToDelete = client.collections.get(collectionName)
-    if(!collectionToDelete) return "Collection does not exists";
-   
-    return await collectionToDelete.data.deleteById(id)
+    const collectionToDelete = client.collections.get(collectionName);
+    if (!collectionToDelete) return 'Collection does not exists';
+
+    return await collectionToDelete.data.deleteById(id);
   } catch (error) {
     console.error('Error deleting object:', error);
     throw error;
   }
 }
 
-export async function getCollection(client: WeaviateClient,collectionName:string,consistency:boolean) {
+export async function getCollection(client: WeaviateClient, collectionName: string, consistency: boolean) {
   try {
     const collection = client.collections.get(collectionName);
-    if(!collection) return "Collection not found";
-    if(collection && consistency) {
-      collection.withConsistency("QUORUM");
+    if (!collection) return 'Collection not found';
+    if (collection && consistency) {
+      collection.withConsistency('QUORUM');
     }
     return collection;
   } catch (error) {
-    console.log("Error in getting collection", error);
+    console.log('Error in getting collection', error);
   }
 }
 
-export async function createCollection(
-  client: WeaviateClient,
-  collectionName: string,
-) {
+export async function createCollection(client: WeaviateClient, collectionName: string) {
   try {
     const collection = await client.collections.create({
       name: collectionName,
     });
     return collection;
   } catch (error) {
-    console.log("Error in getting collection", error);
+    console.log('Error in getting collection', error);
   }
 }
-export async function deleteCollection(
-  client: WeaviateClient,
-  collectionName: string,
-) {
+export async function deleteCollection(client: WeaviateClient, collectionName: string) {
   try {
     const collection = client.collections.get(collectionName);
     if (!collection) {
       throw Error(`Collection ${collectionName} does not exist`);
     }
     await client.collections.delete(collectionName);
-    return "Collection deleted successfully";
+    return 'Collection deleted successfully';
   } catch (error) {
-    console.log("Error in getting collection", error);
+    console.log('Error in getting collection', error);
   }
 }
