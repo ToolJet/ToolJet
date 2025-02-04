a1868a3e00fd045e580d3f85189bf3956029ad71
manishkushare <kushare.manish9@gmail.com>
2025-02-04 12:21:23 +0530

    Added tootip to chat header

--- frontend/src/AppBuilder/Widgets/Chat/components/ChatHeader.jsx
+++ frontend/src/AppBuilder/Widgets/Chat/components/ChatHeader.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Button } from '@/components/ui/Button/Button';
+import { ToolTip } from '@/_components/ToolTip';
 import SolidIcon from '@/_ui/Icon/SolidIcons';
 
 export const ChatHeader = ({ title, onDownload, onClear }) => (
   <div className="chat-header">
     <span className="chat-title tj-text-xx-large">{title}</span>
     <div className="button-group">
-      <Button variant="ghost" onClick={onDownload} iconOnly title="Download chat history">
-        <SolidIcon name="pagedownload" width="16" fill="var(--icons-strong)" />
-      </Button>
-      <Button variant="ghost" onClick={onClear} iconOnly>
-        <SolidIcon name="clearhistory" width="16" fill="var(--icons-strong)" />
-      </Button>
+      <ToolTip message="Download chat history">
+        <Button variant="ghost" onClick={onDownload} iconOnly title="Download chat history">
+          <SolidIcon name="pagedownload" width="16" fill="var(--icons-strong)" />
+        </Button>
+      </ToolTip>
+
+      <ToolTip message="Clear chat history">
+        <Button variant="ghost" onClick={onClear} iconOnly>
+          <SolidIcon name="clearhistory" width="16" fill="var(--icons-strong)" />
+        </Button>
+      </ToolTip>
     </div>
   </div>
 );
