2d4ca1e322cd271edc3fd90d3499bd8ab261577a
Raman Kumar <k.raman1998@yahoo.in>
2025-02-05 09:36:51 +0530

    feat: modify name of property from id to Tab 7(#11267)

--- server/src/modules/apps/services/widget-config/tabs.js
+++ server/src/modules/apps/services/widget-config/tabs.js
@@ -54,6 +54,10 @@ export const tabsConfig = {
     useDynamicOptions: {
       type: 'toggle',
       displayName: 'Dynamic options',
+      validation: {
+        schema: { type: 'boolean' },
+        defaultValue: true,
+      },
     },
     loadingState: {
       type: 'toggle',
@@ -224,7 +228,7 @@ export const tabsConfig = {
       showOnMobile: { value: '{{false}}' },
     },
     properties: {
-      useDynamicOptions: { value: '{{false}}' },
+      useDynamicOptions: { value: '{{true}}' },
       tabs: {
         value:
           "{{[ \n\t\t{ title: 'Home', id: '0' }, \n\t\t{ title: 'Profile', id: '1' }, \n\t\t{ title: 'Settings', id: '2' } \n ]}}",
@@ -232,20 +236,30 @@ export const tabsConfig = {
       tabItems: {
         value: [
           {
-            id: 1,
+            id: '0',
             title: 'Home',
+            loading: { value: false },
             disable: { value: false },
             visible: { value: true },
           },
           {
-            id: 2,
+            id: '1',
             title: 'Profile',
+            loading: { value: false },
             disable: { value: false },
             visible: { value: true },
           },
           {
-            id: 3,
+            id: '2',
             title: 'Settings',
+            loading: { value: false },
+            disable: { value: false },
+            visible: { value: true },
+          },
+          {
+            id: '3',
+            title: 'Additional Tab',
+            loading: { value: false },
             disable: { value: false },
             visible: { value: true },
           },
