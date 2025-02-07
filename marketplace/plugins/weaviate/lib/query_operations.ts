9545bb53e3edf80a4803e4487c5cb4f5bda9ead1
parthy007 <parthadhikari1812@gmail.com>
2025-02-07 18:57:37 +0530

    Add operations logic

--- marketplace/plugins/weaviate/lib/query_operations.ts
+++ marketplace/plugins/weaviate/lib/query_operations.ts
@@ -1,104 +1,238 @@
-import weaviate, { WeaviateClient } from 'weaviate-client';
+import { CollectionOperation, ObjectsOperation, QueryOptions } from './types';
 
-export async function getSchema(client: WeaviateClient, collectionName: string) {
+export async function getSchema(queryOptions: QueryOptions, BASE_URL, headers) {
+  if (queryOptions.consistency) {
+    headers.consistency = queryOptions.consistency;
+  }
   try {
-    const collection = client.collections.get(collectionName);
-    const collectionDefinition = await collection.config.get();
-    return collectionDefinition.properties;
+    const response = await fetch(`${BASE_URL}/v1/schema`, {
+      method: 'GET',
+      headers: headers,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error in fetching schema! status: ${response.status}`);
+    }
+    return await response.json();
   } catch (error) {
-    console.error('Error fetching schema:', error);
-    throw error;
+    return error;
   }
 }
 
-export async function listObjects(client: WeaviateClient, collectionName: string) {
-  try {
-    const collection = client.collections.get(collectionName);
-    const res = await collection.query.fetchObjects();
-    return res;
-  } catch (error) {
-    console.log(error);
+export async function collectionOperation(queryOptions: QueryOptions, BASE_URL, headers) {
+  const SCHEMA_URL = `${BASE_URL}/v1/schema`;
+  switch (queryOptions.operation_collection) {
+    case CollectionOperation.get_collection: {
+      return await getCollection(queryOptions.collectionName, queryOptions.consistency, SCHEMA_URL, headers);
+    }
+    case CollectionOperation.create_collection: {
+      return await createCollection(queryOptions, SCHEMA_URL, headers);
+    }
+    case CollectionOperation.delete_collection: {
+      return await deleteCollection(queryOptions.collectionName, SCHEMA_URL, headers);
+    }
   }
 }
 
-export async function createObject(client: WeaviateClient, collectionName: string, properties: Record<string, any>) {
+export async function getCollection(collectionName: string, consistency: boolean, SCHEMA_URL, headers) {
   try {
-    const collection = client.collections.get(collectionName);
-    if (!collection) return 'Collection not found';
+    if (consistency) {
+      headers.consistency = consistency;
+    }
+    const response = await fetch(`${SCHEMA_URL}/${collectionName}`, {
+      method: 'GET',
+      headers: headers,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error in getting collection! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    return error;
+  }
+}
 
-    const result = await collection.data.insert({
-      properties: properties,
-      // vectors: Array(1536).fill(0.12345)
-      vectors: {
-        title: Array(1536).fill(0.12345),
-        review_body: Array(1536).fill(0.31313),
-        title_country: Array(1536).fill(0.0505),
+export async function createCollection(queryOptions: QueryOptions, SCHEMA_URL, headers) {
+  const collectionConfig = {
+    class: queryOptions.collectionName,
+    vectorizer: queryOptions.vectorizer,
+    vectorIndexType: queryOptions.vector_index_type,
+    vectorIndexConfig: JSON.parse(queryOptions.vector_index_config),
+    shardingConfig: JSON.parse(queryOptions.sharding_config),
+    replicationConfig: {
+      factor: Number(queryOptions.factor),
+      asyncEnabled: Boolean(queryOptions.async_enabled),
+      deletionStrategy: queryOptions.deletion_strategy,
+    },
+    invertedIndexConfig: {
+      cleanupIntervalSeconds: Number(queryOptions.clean_up_interval_seconds),
+      bm25: JSON.parse(queryOptions.bm_25),
+      stopwords: JSON.parse(queryOptions.stop_words),
+      indexTimestamps: Boolean(queryOptions.index_time_stamps),
+      indexNullState: Boolean(queryOptions.index_null_state),
+      indexPropertyLength: Boolean(queryOptions.index_property_length),
+    },
+    moduleConfig: JSON.parse(queryOptions.module_config),
+    description: queryOptions.description,
+    properties: JSON.parse(queryOptions.properties),
+  };
+
+  try {
+    const response = await fetch(SCHEMA_URL, {
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(collectionConfig),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function deleteCollection(collectionName: string, SCHEMA_URL, headers) {
+  try {
+    const response = await fetch(`${SCHEMA_URL}/${collectionName}`, {
+      method: 'DELETE',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
       },
     });
-
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function getObjectById(client: WeaviateClient, collectionName: string, objectId: string) {
-  try {
-    const collection = client.collections.get(collectionName);
-    if (!collection) return 'Collection not exists';
-
-    const result = await collection.query.fetchObjectById(objectId);
-    return result;
-  } catch (error) {
-    console.error('Error fetching object by ID:', error);
-    throw error;
-  }
-}
-
-export async function deleteObjectById(client: WeaviateClient, collectionName: string, id: string) {
-  try {
-    const collectionToDelete = client.collections.get(collectionName);
-    if (!collectionToDelete) return 'Collection does not exists';
-
-    return await collectionToDelete.data.deleteById(id);
-  } catch (error) {
-    console.error('Error deleting object:', error);
-    throw error;
-  }
-}
-
-export async function getCollection(client: WeaviateClient, collectionName: string, consistency: boolean) {
-  try {
-    const collection = client.collections.get(collectionName);
-    if (!collection) return 'Collection not found';
-    if (collection && consistency) {
-      collection.withConsistency('QUORUM');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return collection;
+
+    return true;
   } catch (error) {
-    console.log('Error in getting collection', error);
+    return error;
   }
 }
 
-export async function createCollection(client: WeaviateClient, collectionName: string) {
+export async function objectsOperation(queryOptions: QueryOptions, BASE_URL, headers) {
+  const OBJECT_URL = `${BASE_URL}/v1/objects`;
+  switch (queryOptions.operation_objects) {
+    case ObjectsOperation.create_object: {
+      return await createObject(queryOptions, OBJECT_URL, headers);
+    }
+    case ObjectsOperation.get_object_by_id: {
+      return await getObjectById(queryOptions, OBJECT_URL, headers);
+    }
+    case ObjectsOperation.list_objects: {
+      return await listObjects(queryOptions, OBJECT_URL, headers);
+    }
+    case ObjectsOperation.delete_object_by_id: {
+      return await deleteObjectById(queryOptions, OBJECT_URL, headers);
+    }
+  }
+}
+
+export async function listObjects(queryOptions: QueryOptions, OBJECT_URL, headers) {
+  if (!queryOptions.collectionName) throw new Error('Collection name is required');
+  const params = new URLSearchParams();
+  params.append('class', queryOptions.collectionName);
+  const paramMapping = {
+    include_vectors: (val: string) => val === 'true' || JSON.parse(val),
+    after: String,
+    offset: Number,
+    limit: Number,
+    include: (val: string) => val.split(','),
+    sort: (val: string) => val.split(','),
+    order: (val: string) => val.split(','),
+    tenant: String,
+  };
   try {
-    const collection = await client.collections.create({
-      name: collectionName,
+    Object.entries(paramMapping).forEach(([key, converter]) => {
+      if (queryOptions[key] && queryOptions[key] !== '{}' && queryOptions[key] !== '') {
+        const value = converter(queryOptions[key]);
+        if (value) params.append(key, String(value));
+      }
     });
-    return collection;
+    const response = await fetch(`${OBJECT_URL}?${params.toString()}`, {
+      method: 'GET',
+      headers: headers,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error in listing objects! status: ${response.status}`);
+    }
+    return await response.json();
   } catch (error) {
-    console.log('Error in getting collection', error);
+    return error;
   }
 }
-export async function deleteCollection(client: WeaviateClient, collectionName: string) {
+
+export async function createObject(queryOptions: QueryOptions, OBJECT_URL, headers) {
   try {
-    const collection = client.collections.get(collectionName);
-    if (!collection) {
-      throw Error(`Collection ${collectionName} does not exist`);
+    const requestBody = {
+      class: queryOptions.collectionName_create_object,
+      properties: JSON.parse(queryOptions.properties_create_object),
+      id: queryOptions.object_uuid,
+    };
+
+    if (queryOptions.vectors_create_object) {
+      if (Array.isArray(queryOptions.vectors_create_object)) {
+        requestBody['vector'] = queryOptions.vectors_create_object;
+      } else {
+        requestBody['vectors'] = queryOptions.vectors_create_object;
+      }
     }
-    await client.collections.delete(collectionName);
-    return 'Collection deleted successfully';
+
+    const response = await fetch(`${OBJECT_URL}`, {
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
   } catch (error) {
-    console.log('Error in getting collection', error);
+    return error;
+  }
+}
+
+export async function getObjectById(queryOptions: QueryOptions, OBJECT_URL, headers) {
+  const { collectionName_get_object: collection, objectId_get_object: uuid } = queryOptions;
+  try {
+    const response = await fetch(`${OBJECT_URL}/${collection}/${uuid}`, {
+      method: 'GET',
+      headers: headers,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function deleteObjectById(queryOptions: QueryOptions, OBJECT_URL, headers) {
+  const { collectionName_delete_object: collection, objectId_delete_object: uuid } = queryOptions;
+  try {
+    const response = await fetch(`${OBJECT_URL}/${collection}/${uuid}`, {
+      method: 'DELETE',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return true;
+  } catch (error) {
+    return error;
   }
 }
--- marketplace/plugins/weaviate/lib/types.ts
+++ marketplace/plugins/weaviate/lib/types.ts
@@ -1,21 +1,62 @@
 export interface SourceOptions {
   instanceUrl: string;
   apiKey: string;
+  connection_type?: string;
+  host?: string;
+  port?: string;
 }
 
 export interface QueryOptions {
-  operation: Operation;
+  data_type: string;
+  operation_schema: SchemaOperation;
+  operation_collection: CollectionOperation;
+  operation_objects: ObjectsOperation;
   collectionName?: string;
   consistency?: boolean;
   objectId?: string;
-  properties?: Record<string, any>;
+  properties?: string;
+  vectorizer?: string;
+  vector_index_type?: string;
+  vector_index_config?: string;
+  sharding_config?: string;
+  factor?: string;
+  async_enabled?: string;
+  clean_up_interval_seconds?: string;
+  bm_25?: string;
+  deletion_strategy?: string;
+  stop_words?: string;
+  index_time_stamps?: string;
+  index_null_state?: string;
+  index_property_length?: string;
+  module_config?: string;
+  description?: string;
+  include_vectors?: string;
+  after?: string;
+  offset?: string;
+  limit?: string;
+  include?: string;
+  sort?: string;
+  tenant?: string;
+  collectionName_create_object?: string;
+  object_uuid?: string;
+  properties_create_object?: string;
+  vectors_create_object?: string;
+  references?: string;
+  collectionName_delete_object?: string;
+  objectId_delete_object?: string;
+  collectionName_get_object?: string;
+  objectId_get_object?: string;
 }
 
-export enum Operation {
+export enum SchemaOperation {
   get_schema = 'get_schema',
+}
+export enum CollectionOperation {
   get_collection = 'get_collection',
   create_collection = 'create_collection',
   delete_collection = 'delete_collection',
+}
+export enum ObjectsOperation {
   list_objects = 'list_objects',
   create_object = 'create_object',
   get_object_by_id = 'get_object_by_id',
