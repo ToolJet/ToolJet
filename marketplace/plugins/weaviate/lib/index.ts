8833c260450c41d7b5f27813a32459daf71b309d
Parth <108089718+parthy007@users.noreply.github.com>
2025-02-09 15:21:46 +0530

    Integrate: Add new Weaviate plugin to the marketplace (#11947)
    * Fix lint issues
    
    Add weaviate-client
    
    * Fix plugin name and description
    
    * Fix toggle
    
    * Add operations logic
    
    * Add datasource form inputs
    
    * Add querymanager inputs
    
    * Add icon
    
    * Remove weaviate client

--- marketplace/plugins/weaviate/lib/index.ts
+++ marketplace/plugins/weaviate/lib/index.ts
@@ -1,73 +1,61 @@
 import { QueryService, QueryResult, ConnectionTestResult, QueryError } from '@tooljet-marketplace/common';
-import { SourceOptions, QueryOptions, Operation } from './types';
-import { getSchema, listObjects, createObject, getObjectById, deleteObjectById, getCollection, createCollection, deleteCollection } from './query_operations';
-import weaviate, { WeaviateClient } from 'weaviate-client';
+import { SourceOptions, QueryOptions } from './types';
+import { getSchema, collectionOperation, objectsOperation } from './query_operations';
 export default class Weaviate implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
-    const client  = await this.getConnection(sourceOptions)
     let result = {};
     try {
-      console.log('tjdb index',{queryOptions});
-      
-      switch (queryOptions.operation) {
-        case Operation.get_schema:
-          result = await getSchema(client,queryOptions.collectionName);
+      const headers = {};
+      let BASE_URL;
+      if (sourceOptions.connection_type === 'local') {
+        BASE_URL = `http://${sourceOptions.host}:${sourceOptions.port}`;
+      } else {
+        BASE_URL = `${sourceOptions.instanceUrl}`;
+        headers['Authorization'] = `Bearer ${sourceOptions.apiKey}`;
+      }
+      switch (queryOptions.data_type) {
+        case 'schema': {
+          result = await getSchema(queryOptions, BASE_URL, headers);
           break;
-        case Operation.list_objects:
-          console.log('tjdb list object switch');
-          
-          result = await listObjects(client, queryOptions.collectionName);
+        }
+        case 'collection': {
+          result = await collectionOperation(queryOptions, BASE_URL, headers);
           break;
-        case Operation.get_collection:
-          result = await getCollection(client,queryOptions.collectionName,queryOptions.consistency);
-          break;  
-        case Operation.create_collection:
-          result = await createCollection(client,queryOptions.collectionName);
+        }
+        case 'objects': {
+          result = await objectsOperation(queryOptions, BASE_URL, headers);
           break;
-        case Operation.delete_collection:
-          result = await deleteCollection(client,queryOptions.collectionName);
-          break;
-        case Operation.create_object:
-          result = await createObject(client,queryOptions.collectionName,queryOptions.properties);
-          break;
-        case Operation.get_object_by_id:
-          result = await getObjectById(client,queryOptions.collectionName,queryOptions.objectId);
-          break;
-        case Operation.delete_object_by_id:
-          result = await deleteObjectById(client,queryOptions.collectionName,queryOptions.objectId);
-          break;
+        }
         default:
           throw new QueryError('Invalid operation', 'Operation not supported', {});
       }
     } catch (error) {
       throw new QueryError('Query could not be completed', error?.message, {});
     }
-    
+
     return { status: 'ok', data: result };
   }
 
   async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
     try {
-     const client =  await this.getConnection(sourceOptions);
-    await client.collections.listAll();
-    return { status: 'ok' };
-
+      let response;
+      if (sourceOptions.connection_type === 'local') {
+        response = await fetch(`http://${sourceOptions.host}:${sourceOptions.port}/v1/schema`, {
+          method: 'GET',
+        });
+      } else {
+        response = await fetch(`${sourceOptions.instanceUrl}/v1/schema`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${sourceOptions.apiKey}`,
+          },
+        });
+      }
+      if (response.ok) {
+        return { status: 'ok' };
+      }
     } catch (error) {
       throw new QueryError('Connection failed', error?.message, {});
     }
   }
-
-  async getConnection(sourceOptions: SourceOptions) {
-
-    const weaviateURL = sourceOptions.instanceUrl as string
-    const weaviateKey = sourceOptions.apiKey as string
-    const client: WeaviateClient = await weaviate.connectToWeaviateCloud(weaviateURL, {
-        authCredentials: new weaviate.ApiKey(weaviateKey),
-      }
-    )
-
-    return client;
-    
-  }
 }
-
