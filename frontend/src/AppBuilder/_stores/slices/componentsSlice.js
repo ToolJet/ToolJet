064953b7501a506127c6cb14642381b37572b94e
Shaurya Sharma <shaurya064@gmail.com>
2025-03-03 23:17:37 +0530

    Fixed issue causing field is empty error to popup on 0

--- frontend/src/AppBuilder/_stores/slices/componentsSlice.js
+++ frontend/src/AppBuilder/_stores/slices/componentsSlice.js
@@ -502,7 +502,7 @@ export const createComponentsSlice = (set, get) => ({
 
     const resolvedMandatory = getResolvedValue(mandatory, customResolveObjects) || false;
 
-    if (resolvedMandatory == true && !widgetValue) {
+    if (resolvedMandatory == true && !widgetValue && widgetValue !== 0) {
       return {
         isValid: false,
         validationError: `Field cannot be empty`,
