a56c277e77e84bb42d32a36a64ec2fa206701921
Shaurya Sharma <shaurya064@gmail.com>
2025-03-05 00:34:14 +0530

    Changed global settings panel view

--- frontend/src/AppBuilder/LeftSidebar/GlobalSettings/index.jsx
+++ frontend/src/AppBuilder/LeftSidebar/GlobalSettings/index.jsx
@@ -8,6 +8,8 @@ import useStore from '@/AppBuilder/_stores/store';
 import { shallow } from 'zustand/shallow';
 import AppModeToggle from './AppModeToggle';
 import ThemeSelect from './ThemeSelect';
+import MaintenanceMode from './MaintenanceMode';
+import HideHeaderToggle from './HideHeaderToggle';
 
 const GlobalSettings = ({ darkMode }) => {
   const shouldFreeze = useStore((state) => state.getShouldFreeze());
@@ -17,10 +19,21 @@ const GlobalSettings = ({ darkMode }) => {
       <div>
         <div bsPrefix="global-settings-popover" className="global-settings-panel">
           <HeaderSection>
-            <HeaderSection.PanelHeader title="Global settings" />
+            <HeaderSection.PanelHeader title="Global settings">
+              <div className="d-flex w-100 justify-content-end">
+                <AppExport darkMode={darkMode} />
+              </div>
+            </HeaderSection.PanelHeader>
           </HeaderSection>
-          <div className="card-body">
+          <div className="card-body" style={{ paddingBottom: '0px' }}>
             <SlugInput />
+          </div>
+          <div style={{ padding: '12px 16px' }} className={cx({ disabled: shouldFreeze })}>
+            <MaintenanceMode darkMode={darkMode} />
+            <HideHeaderToggle darkMode={darkMode} />
+          </div>
+          <div className={cx({ 'dark-theme': darkMode })}>
+            <span className="canvas-styles-header">Canvas Styles</span>
           </div>
           <div style={{ padding: '12px 16px' }} className={cx({ disabled: shouldFreeze })}>
             <div className="tj-text-xsm color-slate12 ">
