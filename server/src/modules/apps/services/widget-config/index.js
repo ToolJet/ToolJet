1f5ce115344b9fde1b9515995be0283129192db5
Shaurya Sharma <shaurya064@gmail.com>
2025-03-05 01:46:02 +0530

    File rename fix

--- server/src/modules/apps/services/widget-config/index.js
+++ server/src/modules/apps/services/widget-config/index.js
@@ -58,7 +58,7 @@ import { datetimePickerV2Config } from './datetimepickerV2';
 import { datePickerV2Config } from './datepickerV2';
 import { timePickerConfig } from './timepicker';
 
-export {
+const widgets = {
   buttonConfig,
   tableConfig,
   chartConfig,
@@ -119,3 +119,48 @@ export {
   iconConfig,
   boundedBoxConfig,
 };
+
+const universalProps = {
+  properties: {},
+  general: {
+    tooltip: { type: 'code', displayName: 'Tooltip', validation: { schema: { type: 'string' } } },
+  },
+  others: {},
+  events: {},
+  styles: {},
+  validate: true,
+  generalStyles: {
+    boxShadow: { type: 'boxShadow', displayName: 'Box Shadow' },
+  },
+  definition: {
+    others: {},
+    events: [],
+    styles: {},
+    generalStyles: {
+      boxShadow: { value: '0px 0px 0px 0px #00000040' },
+    },
+  },
+};
+
+const combineProperties = (widget, universal, isArray = false) => {
+  return {
+    ...universal,
+    ...widget,
+    properties: { ...universal.properties, ...widget.properties },
+    general: { ...universal.general, ...widget.general },
+    others: { ...universal.others, ...widget.others },
+    events: isArray ? [...universal.events, ...widget.events] : { ...universal.events, ...widget.events },
+    styles: { ...universal.styles, ...widget.styles },
+    generalStyles: { ...universal.generalStyles, ...widget.generalStyles },
+    exposedVariables: { ...universal.exposedVariables, ...widget.exposedVariables },
+  };
+};
+
+export const componentTypes = Object.values(widgets).map((widget) => {
+  return {
+    ...combineProperties(widget, universalProps),
+    definition: combineProperties(widget.definition, universalProps.definition, true),
+  };
+});
+
+export default widgets;
