ee96301be1d49bea54babeb3eaef80d06677e00f
Muhsin Shah C P <muhsinshah21@gmail.com>
2025-03-05 20:46:19 +0530

    Removing copilot from the constants & fixing other bugs in the sheets

--- frontend/src/OrganizationSettingsPage/constant.js
+++ frontend/src/OrganizationSettingsPage/constant.js
@@ -3,7 +3,6 @@ export const workspaceSettingsLinks = [
   { id: 'groups', name: 'Groups', route: 'groups', conditions: ['admin'] },
   { id: 'workspacelogin', name: 'Workspace login', route: 'workspace-login', conditions: ['admin', 'wsLoginEnabled'] },
   { id: 'workspace-variables', name: 'Workspace variables', route: 'workspace-variables', conditions: ['admin'] },
-  { id: 'copilot', name: 'Copilot', route: 'copilot', conditions: ['admin'] },
   { id: 'custom-styles', name: 'Custom styles', route: 'custom-styles', conditions: ['admin'] },
   { id: 'configure-git', name: 'Configure Git', route: 'configure-git', conditions: ['admin'] },
 ];
