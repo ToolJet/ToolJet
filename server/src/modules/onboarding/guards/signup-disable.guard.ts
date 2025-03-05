65517f70e98f6bdfc6e4bc33c3665add7323f803
rohanlahori <rohanlahori99@gmail.com>
2025-03-05 17:27:55 +0530

    signup-fixes

--- server/src/modules/onboarding/guards/signup-disable.guard.ts
+++ server/src/modules/onboarding/guards/signup-disable.guard.ts
@@ -1,14 +1,13 @@
-import { INSTANCE_SYSTEM_SETTINGS } from '@modules/instance-settings/constants';
-import { InstanceSettingsUtilService } from '@modules/instance-settings/util.service';
+// import { INSTANCE_SYSTEM_SETTINGS } from '@modules/instance-settings/constants';
+// import { InstanceSettingsUtilService } from '@modules/instance-settings/util.service';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SignupDisableGuard implements CanActivate {
-  constructor(protected readonly instanceSettingsUtilService: InstanceSettingsUtilService) {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const enableSignUp = await this.instanceSettingsUtilService.getSettings(INSTANCE_SYSTEM_SETTINGS.ENABLE_SIGNUP);
-    return request.body.organizationId || enableSignUp;
+  constructor(private configService: ConfigService) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    return this.configService.get<string>('DISABLE_SIGNUPS') !== 'true';
   }
 }
