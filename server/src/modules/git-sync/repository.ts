014aa29648864342ff1f8f17550c709c03e34c10
rohanlahori <rohanlahori99@gmail.com>
2025-03-10 19:24:36 +0530

    fixes

--- server/src/modules/git-sync/repository.ts
+++ server/src/modules/git-sync/repository.ts
@@ -1,7 +1,12 @@
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
 import { OrganizationGitSync } from '@entities/organization_git_sync.entity';
-
+@Injectable()
 export class OrganizationGitSyncRepository extends Repository<OrganizationGitSync> {
+  constructor(private dataSource: DataSource) {
+    super(OrganizationGitSync, dataSource.createEntityManager());
+  }
+  // need to check if it's required or not
   async findActiveByOrganization(organizationId: string): Promise<OrganizationGitSync | null> {
     return this.createQueryBuilder('orgGitSync')
       .where('orgGitSync.organizationId = :organizationId', { organizationId })
@@ -9,23 +14,19 @@ export class OrganizationGitSyncRepository extends Repository<OrganizationGitSyn
       .leftJoinAndSelect('orgGitSync.appGitSync', 'appGitSync')
       .getOne();
   }
-
   async findByGitUrl(gitUrl: string): Promise<OrganizationGitSync | null> {
     return this.findOne({
       where: { gitUrl },
       relations: ['organization', 'appGitSync'],
     });
   }
-
   async createOrganizationGitSync(createParams: Partial<OrganizationGitSync>): Promise<OrganizationGitSync> {
-    const newSync = this.create(createParams);
+    const newSync = this.manager.create(OrganizationGitSync, createParams);
     return this.save(newSync);
   }
-
   async updateGitSyncStatus(organizationId: string, isEnabled: boolean): Promise<void> {
     await this.update({ organizationId }, { isEnabled });
   }
-
   async findWithRelations(id: string): Promise<OrganizationGitSync | null> {
     return this.findOne({
       where: { id },
