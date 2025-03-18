0b57979e91c8e57ef406a806121db93da478fdbf
Srimaniteja <mani@tooljet.com>
2025-03-18 14:32:22 +0530

    Cypress appwrite data source spec (#12275)
    * Added cypress spec for appwrite
    
    * removed unwanted commands

--- cypress-tests/cypress/constants/selectors/Plugins.js
+++ cypress-tests/cypress/constants/selectors/Plugins.js
@@ -1,20 +1,27 @@
 export const pluginSelectors = {
-    regionField: '[data-cy="region-section"] .react-select__control',
-    regionFieldValue: '[data-cy="region-section"] .react-select__single-value',
-    amazonsesAccesKey: '[data-cy="access-key-section"]',
-    operationDropdown: '[data-cy="operation-select-dropdown"]',
-    sendEmailInputField: '[data-cy="send_mail_to-input-field"]',
-    ccEmailInputField: '[data-cy="cc_to-input-field"]',
-    bccEmailInputField: '[data-cy="bcc_to-input-field"]',
-    sendEmailFromInputField: '[data-cy="send_mail_from-input-field"]',
-    emailSubjetInputField: '[data-cy="subject-input-field"]',
-    emailbodyInputField: '[data-cy="body-input-field"]',
-    amazonAthenaDbName: '[data-cy="database-text-field"]'
+  regionField: '[data-cy="region-section"] .react-select__control',
+  regionFieldValue: '[data-cy="region-section"] .react-select__single-value',
+  amazonsesAccesKey: '[data-cy="access-key-section"]',
+  operationDropdown: '[data-cy="operation-select-dropdown"]',
+  sendEmailInputField: '[data-cy="send_mail_to-input-field"]',
+  ccEmailInputField: '[data-cy="cc_to-input-field"]',
+  bccEmailInputField: '[data-cy="bcc_to-input-field"]',
+  sendEmailFromInputField: '[data-cy="send_mail_from-input-field"]',
+  emailSubjetInputField: '[data-cy="subject-input-field"]',
+  emailbodyInputField: '[data-cy="body-input-field"]',
+  amazonAthenaDbName: '[data-cy="database-text-field"]',
 };
 
 export const baserowSelectors = {
-    hostField: '[data-cy="host-select-dropdown"]',
-    baserowApiKey: '[data-cy="api-token-text-field"]',
-    table: '[data-cy="table-id-input-field"]',
-    rowIdinputfield: '[data-cy="row-id-input-field"]',
+  hostField: '[data-cy="host-select-dropdown"]',
+  baserowApiKey: '[data-cy="api-token-text-field"]',
+  table: '[data-cy="table-id-input-field"]',
+  rowIdinputfield: '[data-cy="row-id-input-field"]',
+};
+
+export const appWriteSelectors = {
+  projectID: '[data-cy="project-id-text-field"]',
+  collectionId: '[data-cy="collectionid-input-field"]',
+  documentId: '[data-cy="documentid-input-field"]',
+  bodyInput: '[data-cy="body-input-field"]',
 };
---
+++ cypress-tests/cypress/e2e/happyPath/marketplace/commonTestcases/data-source/appWrite.cy.js
@@ -0,0 +1,308 @@
+import { fake } from "Fixtures/fake";
+import { postgreSqlSelector } from "Selectors/postgreSql";
+import { postgreSqlText } from "Texts/postgreSql";
+import { appwriteText } from "Texts/appWrite";
+import { appWriteSelectors } from "Selectors/Plugins";
+import { commonSelectors } from "Selectors/common";
+import { commonText } from "Texts/common";
+
+import {
+  fillDataSourceTextField,
+  selectAndAddDataSource,
+} from "Support/utils/postgreSql";
+
+import {
+  verifyCouldnotConnectWithAlert,
+  deleteDatasource,
+  closeDSModal,
+  addQuery,
+  addDsAndAddQuery,
+} from "Support/utils/dataSource";
+
+import { openQueryEditor } from "Support/utils/dataSource";
+import { dataSourceSelector } from "../../../../../constants/selectors/dataSource";
+
+const data = {};
+data.dsName = fake.lastName.toLowerCase().replaceAll("[^A-Za-z]", "");
+
+describe("Data source AppWrite", () => {
+  beforeEach(() => {
+    cy.apiLogin();
+    cy.defaultWorkspaceLogin();
+    cy.intercept("POST", "/api/data_queries").as("createQuery");
+  });
+
+  it("Should verify elements on appwrite connection form", () => {
+    const Host = Cypress.env("appwrite_host");
+    const ProjectID = Cypress.env("appwrite_projectID");
+    const DatabaseID = Cypress.env("appwrite_databaseID");
+    const SecretKey = Cypress.env("appwrite_secretkey");
+
+    cy.get(commonSelectors.globalDataSourceIcon).click();
+    closeDSModal();
+
+    cy.get(postgreSqlSelector.commonlyUsedLabelAndCount).should(
+      "have.text",
+      postgreSqlText.commonlyUsed
+    );
+
+    cy.get(postgreSqlSelector.apiLabelAndCount).should(
+      "have.text",
+      postgreSqlText.allApis
+    );
+    cy.get(postgreSqlSelector.cloudStorageLabelAndCount).should(
+      "have.text",
+      postgreSqlText.allCloudStorage
+    );
+
+    selectAndAddDataSource("databases", appwriteText.appwrite, data.dsName);
+
+    fillDataSourceTextField(
+      appwriteText.host,
+      appwriteText.hostPlaceholder,
+      Host
+    );
+
+    fillDataSourceTextField(
+      appwriteText.ProjectID,
+      appwriteText.projectIdPlaceholder,
+      ProjectID
+    );
+
+    fillDataSourceTextField(
+      appwriteText.DatabaseID,
+      appwriteText.databaseIdPlaceholder,
+      DatabaseID
+    );
+
+    fillDataSourceTextField(
+      appwriteText.SecretKey,
+      appwriteText.SecretKeyPlaceholder,
+      SecretKey
+    );
+
+    cy.get(postgreSqlSelector.buttonTestConnection).click();
+    cy.get(postgreSqlSelector.textConnectionVerified, {
+      timeout: 10000,
+    }).should("have.text", postgreSqlText.labelConnectionVerified);
+
+    cy.get(postgreSqlSelector.buttonSave)
+      .verifyVisibleElement("have.text", postgreSqlText.buttonTextSave)
+      .click();
+    cy.verifyToastMessage(
+      commonSelectors.toastMessage,
+      postgreSqlText.toastDSSaved
+    );
+
+    deleteDatasource(`cypress-${data.dsName}-Appwrite`);
+  });
+
+  it("Should verify the functionality of appwrite connection form.", () => {
+    const Host = Cypress.env("appwrite_host");
+    const ProjectID = Cypress.env("appwrite_projectID");
+    const DatabaseID = Cypress.env("appwrite_databaseID");
+    const SecretKey = Cypress.env("appwrite_secretkey");
+
+    cy.get(commonSelectors.globalDataSourceIcon).click();
+    closeDSModal();
+
+    selectAndAddDataSource("databases", appwriteText.appwrite, data.dsName);
+
+    fillDataSourceTextField(
+      appwriteText.host,
+      appwriteText.hostPlaceholder,
+      Host
+    );
+
+    fillDataSourceTextField(
+      appwriteText.ProjectID,
+      appwriteText.projectIdPlaceholder,
+      ProjectID
+    );
+
+    fillDataSourceTextField(
+      appwriteText.DatabaseID,
+      appwriteText.databaseIdPlaceholder,
+      DatabaseID
+    );
+
+    fillDataSourceTextField(
+      appwriteText.SecretKey,
+      appwriteText.SecretKeyPlaceholder,
+      SecretKey
+    );
+
+    cy.get(postgreSqlSelector.buttonTestConnection).click();
+    cy.get(postgreSqlSelector.textConnectionVerified, {
+      timeout: 10000,
+    }).should("have.text", postgreSqlText.labelConnectionVerified);
+
+    cy.get(postgreSqlSelector.buttonSave)
+      .verifyVisibleElement("have.text", postgreSqlText.buttonTextSave)
+      .click();
+    cy.verifyToastMessage(
+      commonSelectors.toastMessage,
+      postgreSqlText.toastDSSaved
+    );
+    deleteDatasource(`cypress-${data.dsName}-Appwrite`);
+  });
+
+  it("Should be able to run the query with a valid connection", () => {
+    const Host = Cypress.env("appwrite_host");
+    const ProjectID = Cypress.env("appwrite_projectID");
+    const DatabaseID = Cypress.env("appwrite_databaseID");
+    const SecretKey = Cypress.env("appwrite_secretkey");
+    const CollectionID = Cypress.env("appwrite_collectionID");
+
+    cy.get(commonSelectors.globalDataSourceIcon).click();
+    closeDSModal();
+
+    selectAndAddDataSource("databases", appwriteText.appwrite, data.dsName);
+
+    fillDataSourceTextField(
+      appwriteText.host,
+      appwriteText.hostPlaceholder,
+      Host
+    );
+    fillDataSourceTextField(
+      appwriteText.ProjectID,
+      appwriteText.projectIdPlaceholder,
+      ProjectID
+    );
+    fillDataSourceTextField(
+      appwriteText.DatabaseID,
+      appwriteText.databaseIdPlaceholder,
+      DatabaseID
+    );
+    fillDataSourceTextField(
+      appwriteText.SecretKey,
+      appwriteText.SecretKeyPlaceholder,
+      SecretKey
+    );
+
+    cy.get(postgreSqlSelector.buttonTestConnection).click();
+
+    cy.get(postgreSqlSelector.textConnectionVerified, {
+      timeout: 10000,
+    }).should("have.text", postgreSqlText.labelConnectionVerified);
+    cy.get(postgreSqlSelector.buttonSave)
+      .verifyVisibleElement("have.text", postgreSqlText.buttonTextSave)
+      .click();
+
+    cy.verifyToastMessage(
+      commonSelectors.toastMessage,
+      postgreSqlText.toastDSSaved
+    );
+
+    cy.get(commonSelectors.globalDataSourceIcon).click();
+    cy.get(
+      `[data-cy="cypress-${data.dsName}-appwrite-button"]`
+    ).verifyVisibleElement("have.text", `cypress-${data.dsName}-appwrite`);
+    cy.wait(1000);
+
+    cy.get(commonSelectors.dashboardIcon).click();
+    cy.get(commonSelectors.appCreateButton).click();
+    cy.get(commonSelectors.appNameInput).click().type(data.dsName);
+    cy.get(commonSelectors.createAppButton).click();
+    cy.skipWalkthrough();
+
+    cy.get('[data-cy="show-ds-popover-button"]').click();
+    cy.get(".css-4e90k9").type(`${data.dsName}`);
+    cy.contains(`[id*="react-select-"]`, data.dsName).click();
+    cy.get('[data-cy="query-rename-input"]').clear().type(data.dsName);
+
+    // Create API document for delete operation
+
+    cy.request({
+      method: "POST",
+      url: `https://cloud.appwrite.io/v1/databases/${DatabaseID}/collections/${CollectionID}/documents`,
+      headers: {
+        "X-Appwrite-Project": ProjectID,
+        "X-Appwrite-Key": SecretKey,
+        "Content-Type": "application/json",
+      },
+      body: {
+        documentId: "unique()",
+        data: {
+          User_name: "test",
+          User_ID: 30,
+        },
+        permissions: ['read("any")'],
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      cy.wrap(response.body.$id).as("documentId");
+    });
+
+    // Verify all operations
+    const operations = [
+      "List documents",
+      "Get document",
+      "Add Document to Collection",
+      "Update document",
+      "Delete document",
+    ];
+
+    cy.get("@documentId").then((documentId) => {
+      operations.forEach((operation) => {
+        cy.get(".react-select__input")
+          .eq(1)
+          .type(`${operation}{enter}`, { force: true });
+
+        if (operation === "Get document") {
+          cy.get(appWriteSelectors.collectionId).clearAndTypeOnCodeMirror(
+            CollectionID
+          );
+
+          cy.get(appWriteSelectors.documentId).clearAndTypeOnCodeMirror(
+            Cypress.env("appwrite_documentID")
+          );
+        }
+
+        if (operation === "Add Document to Collection") {
+          cy.get(appWriteSelectors.collectionId).clearAndTypeOnCodeMirror(
+            CollectionID
+          );
+          cy.get(appWriteSelectors.bodyInput).clearAndTypeOnCodeMirror(
+            '{"User_name": "John Updated", "User_ID": 35}'
+          );
+        }
+
+        if (operation === "Update document") {
+          cy.get(appWriteSelectors.collectionId).clearAndTypeOnCodeMirror(
+            CollectionID
+          );
+
+          cy.get(appWriteSelectors.documentId).clearAndTypeOnCodeMirror(
+            Cypress.env("appwrite_documentID")
+          );
+          cy.get(appWriteSelectors.bodyInput).clearAndTypeOnCodeMirror(
+            '{"User_name": "John Updated", "User_ID": 35}'
+          );
+        }
+
+        if (operation === "List documents") {
+          cy.get(appWriteSelectors.collectionId).clearAndTypeOnCodeMirror(
+            CollectionID
+          );
+        }
+
+        if (operation === "Delete document") {
+          cy.get(appWriteSelectors.collectionId).clearAndTypeOnCodeMirror(
+            CollectionID
+          );
+          cy.get(appWriteSelectors.documentId).clearAndTypeOnCodeMirror(
+            documentId
+          );
+        }
+        cy.get(dataSourceSelector.queryPreviewButton).click();
+
+        // Verify toast message
+        cy.verifyToastMessage(
+          commonSelectors.toastMessage,
+          `Query (${data.dsName}) completed.`
+        );
+      });
+    });
+  });
+});
