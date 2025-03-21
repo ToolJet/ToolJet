ce130241f0884acf3c78d52a5effc0b6f68a003e
Nakul Nagargade <nakul@tooljet.com>
2025-03-21 12:48:33 +0530

    fix

--- frontend/src/Editor/Components/verticalDivider.jsx
+++ frontend/src/Editor/Components/verticalDivider.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 export const VerticalDivider = function Divider({ styles, height, width, dataCy, darkMode, properties }) {
   const { dividerColor, boxShadow, dividerStyle } = styles;
-  const { visibility } = properties;
   const color =
     dividerColor === '' || ['#000', '#000000'].includes(dividerColor) ? (darkMode ? '#fff' : '#000') : dividerColor;
 
   return (
     <div
       className="row"
-      style={{ display: visibility ? 'flex' : 'none', padding: '0 8px', width, height }}
+      style={{ display: properties?.visibility ? 'flex' : 'none', padding: '0 8px', width, height }}
       data-cy={dataCy}
     >
       <div className="col-6"></div>
