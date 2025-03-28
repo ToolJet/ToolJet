cb83efb355842f5eec61ab2eb97d6c3d44ac7e77
Muhsin Shah C P <muhsinshah21@gmail.com>
2025-03-28 14:42:35 +0530

    Added nestjs init

--- server/data-migrations/1740401100000-SetDefaultWorkspace.ts
+++ server/data-migrations/1740401100000-SetDefaultWorkspace.ts
@@ -1,10 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { TOOLJET_EDITIONS } from '@modules/app/constants';
 import { getTooljetEdition } from '@helpers/utils.helper';
+import { AppModule } from '@modules/app/module';
+import { NestFactory } from '@nestjs/core';
 
 export class SetDefaultWorkspace1740401100000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    if (getTooljetEdition() === TOOLJET_EDITIONS.CE) {
+    // This is to load the env. Need to find a better way than initializing nest
+    await NestFactory.createApplicationContext(await AppModule.register({ IS_GET_CONTEXT: true }));
+    if (process.env.TOOLJET_EDITION !== TOOLJET_EDITIONS.EE) {
+      console.log('Skipping migration as it is not EE edition');
       return;
     }
 
