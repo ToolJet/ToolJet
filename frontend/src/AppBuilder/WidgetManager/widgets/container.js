86355f6f9bb3a921f35b6470c8356fd309a41849
Johnson Cherian <johnsonc.dev@gmail.com>
2025-03-28 16:45:54 +0530

    chore: Adds new design for form, container default children (#12239)
    Co-authored-by: Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>

--- frontend/src/AppBuilder/WidgetManager/widgets/container.js
+++ frontend/src/AppBuilder/WidgetManager/widgets/container.js
@@ -3,7 +3,7 @@ export const containerConfig = {
   displayName: 'Container',
   description: 'Group components',
   defaultSize: {
-    width: 5,
+    width: 10,
     height: 200,
   },
   component: 'Container',
@@ -47,10 +47,16 @@ export const containerConfig = {
         defaultValue: true,
       },
     },
+    headerHeight: {
+      type: 'numberInput',
+      displayName: 'Header height',
+      validation: { schema: { type: 'union', schemas: [{ type: 'string' }, { type: 'number' }] }, defaultValue: 80 },
+    },
   },
   defaultChildren: [
     {
       componentName: 'Text',
+      slotName: 'header',
       layout: {
         top: 20,
         left: 1,
@@ -96,15 +102,6 @@ export const containerConfig = {
         defaultValue: '#fff',
       },
       accordian: 'container',
-    },
-    headerHeight: {
-      type: 'numberInput',
-      displayName: 'Height',
-      validation: {
-        schema: { type: 'number' },
-        defaultValue: 80,
-      },
-      accordian: 'header',
     },
     borderRadius: {
       type: 'numberInput',
@@ -157,6 +154,7 @@ export const containerConfig = {
       loadingState: { value: `{{false}}` },
       visibility: { value: '{{true}}' },
       disabledState: { value: '{{false}}' },
+      headerHeight: { value: `{{80}}` },
     },
     events: [],
     styles: {
