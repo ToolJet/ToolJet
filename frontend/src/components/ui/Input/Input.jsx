3f341d05a676728afdad6f1a63e42fc19abe7c06
parthy007 <parthadhikari1812@gmail.com>
2025-03-29 21:24:10 +0530

    Correctly switch between the password eye svg

--- frontend/src/components/ui/Input/Input.jsx
+++ frontend/src/components/ui/Input/Input.jsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { inputVariants } from './InputUtils/Variants';
-import SolidIcon from '@/_ui/Icon/SolidIcons';
+import SolidIcon from '../../../_ui/Icon/SolidIcons';
 
 const Input = React.forwardRef(({ className, size, type, ...props }, ref) => {
   const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
   const isPasswordField = type === 'password';
 
   const togglePasswordVisibility = () => {
-    setIsPasswordVisible((prev) => !prev);
+    if (!props.disabled) {
+      setIsPasswordVisible((prev) => !prev);
+    }
   };
 
   return (
@@ -17,18 +19,19 @@ const Input = React.forwardRef(({ className, size, type, ...props }, ref) => {
         type={isPasswordField && isPasswordVisible ? 'text' : type}
         className={cn(
           inputVariants({ size }),
-          `tw-relative tw-peer tw-flex tw-text-[12px]/[18px] tw-w-full tw-rounded-[8px] tw-border-[1px] tw-border-solid tw-bg-background-surface-layer-01 tw-py-[7px] tw-text-text-default focus-visible:tw-ring-[1px] focus-visible:tw-ring-offset-[1px] focus-visible:tw-ring-border-accent-strong focus-visible:tw-ring-offset-border-accent-strong focus-visible:tw-border-transparent disabled:tw-cursor-not-allowed disabled:tw-border-transparent ${props.styles}`,
+          `tw-relative tw-peer tw-flex tw-text-[12px]/[18px] tw-w-full tw-rounded-[8px] tw-border-[1px] tw-border-solid tw-bg-background-surface-layer-01 tw-py-[7px] tw-text-text-default focus-visible:tw-ring-[1px] focus-visible:tw-ring-offset-[1px] focus-visible:tw-ring-border-accent-strong focus-visible:tw-ring-offset-border-accent-strong focus-visible:tw-border-transparent disabled:tw-cursor-not-allowed ${props.styles}`,
           className
         )}
         ref={ref}
         {...props}
       />
       {isPasswordField && (
-        <div
-          className="tw-absolute tw-right-3 tw-top-1/2 tw-transform -tw-translate-y-1/2 tw-cursor-pointer tw-z-50 tw-w-[25px] tw-h-full tw-flex tw-justify-center tw-items-center"
-          onClick={togglePasswordVisibility}
-        >
-          <SolidIcon name="eye1" width="16" height="16" />
+        <div onClick={togglePasswordVisibility}>
+          {isPasswordVisible ? (
+            <SolidIcon className="eye-icon" name="eye" />
+          ) : (
+            <SolidIcon className="eye-icon" name="eyedisable" />
+          )}
         </div>
       )}
     </>
