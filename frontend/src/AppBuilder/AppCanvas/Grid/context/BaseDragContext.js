ed42d0bb00b60b4ac2970feaa455555a1fa1cfde
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2025-04-02 20:41:08 +0530

    Review fixes

--- frontend/src/AppBuilder/AppCanvas/Grid/context/BaseDragContext.js
+++ frontend/src/AppBuilder/AppCanvas/Grid/context/BaseDragContext.js
@@ -4,10 +4,12 @@ import { getWidgetById } from './contextUtils';
 /**
  * Shared base class between single-widget and group-widget drag context.
  */
+
+const WIDGET_ID_LENGTH = 36;
 export class BaseDragContext {
   constructor({ sourceSlotId, targetSlotId, widgets }) {
-    const sourceWidgetId = sourceSlotId?.slice(0, 36);
-    const targetWidgetId = targetSlotId?.slice(0, 36);
+    const sourceWidgetId = sourceSlotId?.slice(0, WIDGET_ID_LENGTH);
+    const targetWidgetId = targetSlotId?.slice(0, WIDGET_ID_LENGTH);
 
     const sourceWidget = getWidgetById(widgets, sourceWidgetId);
     const targetWidget = getWidgetById(widgets, targetWidgetId);
@@ -18,7 +20,7 @@ export class BaseDragContext {
   }
 
   updateTarget(targetSlotId) {
-    const targetWidgetId = targetSlotId?.slice(0, 36);
+    const targetWidgetId = targetSlotId?.slice(0, WIDGET_ID_LENGTH);
     const targetWidget = getWidgetById(this.widgets, targetWidgetId);
     this.target = new DropAreaEntity(targetWidget, targetSlotId);
   }
