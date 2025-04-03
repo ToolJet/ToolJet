58c3ff535719c15912b3886207efd73195f846ce
devanshu052000 <devanshu.rastogi05@gmail.com>
2025-04-03 14:50:28 +0530

    Fix error on query duplication

--- frontend/src/AppBuilder/_stores/slices/dataQuerySlice.js
+++ frontend/src/AppBuilder/_stores/slices/dataQuerySlice.js
@@ -260,7 +260,7 @@ export const createDataQuerySlice = (set, get) => ({
               {
                 ...data,
                 data_source_id: queryToClone.data_source_id,
-                plugin: { iconFile: queryToClone.plugin.iconFile, icon_file: queryToClone.plugin.icon_file },
+                plugin: { iconFile: queryToClone.plugin?.iconFile, icon_file: queryToClone.plugin?.icon_file },
               },
               ...state.dataQuery.queries.modules[moduleId],
             ];
