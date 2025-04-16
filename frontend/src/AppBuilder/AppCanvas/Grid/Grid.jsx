de9db2ead95f579ce199048e23bb3bb93b29c416
NishidhJain11 <nishidh@tooljet.com>
2025-04-16 15:48:56 +0530

    Fix Issue #12589: Hide the actual component when user actually changes it size i.e on resize event & not on resize start event

--- frontend/src/AppBuilder/AppCanvas/Grid/Grid.jsx
+++ frontend/src/AppBuilder/AppCanvas/Grid/Grid.jsx
@@ -579,6 +579,11 @@ export default function Grid({ gridWidth, currentLayout }) {
         keepRatio={false}
         individualGroupableProps={individualGroupableProps}
         onResize={(e) => {
+          if(resizingComponentId !== e.target.id) {
+            useGridStore.getState().actions.setResizingComponentId(e.target.id);
+            showGridLines();
+          }
+          
           const currentWidget = boxList.find(({ id }) => id === e.target.id);
           let _gridWidth = useGridStore.getState().subContainerWidths[currentWidget.component?.parent] || gridWidth;
           if (currentWidget.component?.parent) {
@@ -639,9 +644,7 @@ export default function Grid({ gridWidth, currentLayout }) {
             return false;
           }
           handleActivateNonDraggingComponents();
-          useGridStore.getState().actions.setResizingComponentId(e.target.id);
           e.setMin([gridWidth, GRID_HEIGHT]);
-          showGridLines();
         }}
         onResizeEnd={(e) => {
           try {
