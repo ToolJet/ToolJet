dec1c1814c418c20317f0e909a224bb9431f09ed
devanshu052000 <devanshu.rastogi05@gmail.com>
2025-04-17 16:56:07 +0530

    Updated the module file with required repositories and entities

--- server/src/modules/app-permissions/module.ts
+++ server/src/modules/app-permissions/module.ts
@@ -5,6 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupPermissions } from '@entities/group_permissions.entity';
 import { User } from '@entities/user.entity';
 import { RolesRepository } from '@modules/roles/repository';
+import { PageUsersRepository } from './repositories/page-users.repository';
+import { PagePermissionsRepository } from './repositories/page-permissions.repository';
+import { PageUser } from '@entities/page_users.entity';
+import { PagePermission } from '@entities/page_permissions.entity';
 
 export class AppPermissionsModule {
   static async register(configs: { IS_GET_CONTEXT: boolean }): Promise<DynamicModule> {
@@ -15,9 +19,16 @@ export class AppPermissionsModule {
 
     return {
       module: AppPermissionsModule,
-      imports: [TypeOrmModule.forFeature([GroupPermissions, User])],
+      imports: [TypeOrmModule.forFeature([GroupPermissions, User, PageUser, PagePermission])],
       controllers: [AppPermissionsController],
-      providers: [AppPermissionsService, AppPermissionsUtilService, RolesRepository, FeatureAbilityFactory],
+      providers: [
+        AppPermissionsService,
+        AppPermissionsUtilService,
+        RolesRepository,
+        PageUsersRepository,
+        PagePermissionsRepository,
+        FeatureAbilityFactory,
+      ],
       exports: [AppPermissionsUtilService],
     };
   }
