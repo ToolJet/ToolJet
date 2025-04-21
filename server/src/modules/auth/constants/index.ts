c5ca4ad4a1f95c17ede3386c0a369e415d3c9ca3
Anantshree Chandola <anantshreechandola23@gmail.com>
2025-04-21 11:20:25 +0530

    update

--- server/src/modules/auth/constants/index.ts
+++ server/src/modules/auth/constants/index.ts
@@ -10,27 +10,10 @@ export enum FEATURE_KEY {
 
   // Setup and Signup
   SETUP_ADMIN = 'setupAdmin', // POST 'setup-admin'
-  SIGNUP = 'signup', // POST 'signup'
-  ACCEPT_INVITE = 'acceptInvite', // POST 'accept-invite'
-  RESEND_INVITE = 'resendInvite', // POST 'resend-invite'
-  VERIFY_INVITE_TOKEN = 'verifyInviteToken', // GET 'verify-invite-token'
-  VERIFY_ORGANIZATION_TOKEN = 'verifyOrganizationToken', // GET 'verify-organization-token'
-  SETUP_ACCOUNT_FROM_TOKEN = 'setupAccountFromToken', // POST 'setup-account-from-token'
-
-  // Trial and Onboarding
-  REQUEST_TRIAL = 'requestTrial', // GET 'request-trial'
-  ACTIVATE_TRIAL = 'activateTrial', // POST 'activate-trial'
-  GET_ONBOARDING_SESSION = 'getOnboardingSession', // GET 'onboarding-session'
-  GET_SIGNUP_ONBOARDING_SESSION = 'getSignupOnboardingSession', // GET 'signup-onboarding-session'
-  FINISH_ONBOARDING = 'finishOnboarding', // POST 'finish-onboarding'
-  TRIAL_DECLINED = 'trialDeclined', // GET 'trial-declined'
 
   // Password Management
   FORGOT_PASSWORD = 'forgotPassword', // POST 'forgot-password'
   RESET_PASSWORD = 'resetPassword', // POST 'reset-password'
-
-  // Invitee Details
-  GET_INVITEE_DETAILS = 'getInviteeDetails', // GET 'invitee-details'
 
   // Oauth
   OAUTH_SIGN_IN = '/oauth/sign-in/:configId',
