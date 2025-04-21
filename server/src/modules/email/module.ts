1565fa14a35016d9fdf4d151a1473156cc94d3b1
devanshu <devanshuguptaknp@gmail.com>
2025-04-21 13:59:37 +0530

    fix: added smtpmodule import

--- server/src/modules/email/module.ts
+++ server/src/modules/email/module.ts
@@ -3,6 +3,7 @@ import { getImportPath } from '@modules/app/constants';
 import { WhiteLabellingModule } from '@modules/white-labelling/module';
 import { InstanceSettingsModule } from '@modules/instance-settings/module';
 import { DataSourcesModule } from '@modules/data-sources/module';
+import { SMTPModule } from '@modules/smtp/module';
 
 export class EmailModule {
   static async register(configs?: { IS_GET_CONTEXT: boolean }): Promise<DynamicModule> {
@@ -10,15 +11,15 @@ export class EmailModule {
     const { EmailService } = await import(`${importPath}/email/service`);
     const { EmailUtilService } = await import(`${importPath}/email/util.service`);
     const { EmailListener } = await import(`${importPath}/email/listener`);
-    const { SMTPUtilService } = await import(`${importPath}/smtp/util.service`);
     return {
       module: EmailModule,
       imports: [
         await WhiteLabellingModule.register(configs),
         await InstanceSettingsModule.register(configs),
         await DataSourcesModule.register(configs),
+        await SMTPModule.register(configs),
       ],
-      providers: [EmailService, EmailListener, EmailUtilService, SMTPUtilService],
+      providers: [EmailService, EmailListener, EmailUtilService],
     };
   }
 }
