bcf1cec8ad1ef2ae4ca3c461de43b0aebc5b8784
Rudra deep Biswas <rudra21ultra@gmail.com>
2025-04-21 17:31:44 +0530

    remove ds

--- server/src/modules/licensing/guards/app.guard.ts
+++ server/src/modules/licensing/guards/app.guard.ts
@@ -1,31 +1,42 @@
 import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
 import { LICENSE_FIELD, LICENSE_LIMIT } from '@modules/licensing/constants';
 import { LicenseTermsService } from '../interfaces/IService';
-import { DataSource } from 'typeorm';
 import { EntityManager } from 'typeorm';
+import { dbTransactionWrap } from '@helpers/database.helper';
+import { App } from '@entities/app.entity';
 
 @Injectable()
 export class AppCountGuard implements CanActivate {
-  constructor(protected licenseTermsService: LicenseTermsService, protected readonly _dataSource: DataSource) {}
+  constructor(protected licenseTermsService: LicenseTermsService) {}
 
-  async appsCount(manager: EntityManager): Promise<number> {
-    return manager
-      .createQueryBuilder('apps', 'a')
-      .innerJoin('organizations', 'o', 'a.organization_id = o.id')
-      .where('a.type = :type', { type: 'front-end' })
-      .andWhere('o.status = :status', { status: 'active' })
-      .getCount();
-  } //Dependancy error using licenseCountsService
+  //Dependancy error using licenseCountsService need to check
+  async fetchTotalAppCount(manager: EntityManager): Promise<number> {
+    const apps = await manager.find(App, {
+      where: {
+        type: 'front-end',
+        organization: {
+          status: 'active',
+        },
+      },
+      relations: ['organization'],
+    });
+
+    return apps.length;
+  }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const appCount = await this.licenseTermsService.getLicenseTerms(LICENSE_FIELD.APP_COUNT);
-    if (appCount === LICENSE_LIMIT.UNLIMITED) {
-      return true;
-    }
+    return await dbTransactionWrap(async (manager: EntityManager) => {
+      const appCount = await this.licenseTermsService.getLicenseTerms(LICENSE_FIELD.APP_COUNT);
 
-    if ((await this.appsCount(this._dataSource.manager)) >= appCount) {
-      throw new HttpException('You have reached your maximum limit for apps.', 451);
-    }
-    return true;
+      if (appCount === LICENSE_LIMIT.UNLIMITED) {
+        return true;
+      }
+
+      if ((await this.fetchTotalAppCount(manager)) >= appCount) {
+        throw new HttpException('You have reached your maximum limit for apps.', 451);
+      }
+
+      return true;
+    });
   }
 }
