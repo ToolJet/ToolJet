828bf9fe5379e05be6823b91191cc09a2353b96a
Nakul Nagargade <nakul@tooljet.com>
2025-04-29 10:24:40 +0530

    Fix s11 integrations bugs

--- frontend/src/AppBuilder/AppCanvas/appCanvasUtils.js
+++ frontend/src/AppBuilder/AppCanvas/appCanvasUtils.js
@@ -573,23 +573,7 @@ export function pasteComponents(targetParentId, copiedComponentObj) {
     componentData.definition.others.showOnDesktop.value = currentLayout === 'desktop' ? `{{true}}` : `{{false}}`;
     componentData.definition.others.showOnMobile.value = currentLayout === 'mobile' ? `{{true}}` : `{{false}}`;
 
-    // Adjust width if parent changed
     let width = component.layouts[currentLayout].width;
-
-    if (targetParentId !== component.component?.parent) {
-      const containerWidth = useGridStore.getState().subContainerWidths[targetParentId || 'canvas'];
-      const oldContainerWidth = useGridStore.getState().subContainerWidths[component?.component?.parent || 'canvas'];
-      width = Math.round((width * oldContainerWidth) / containerWidth);
-
-      // Ensure minimum width
-      width = Math.max(width, 1);
-
-      // Adjust position and width if exceeding grid bounds
-      if (width + component.layouts[currentLayout].left > NO_OF_GRIDS) {
-        component.layouts[currentLayout].left = Math.max(0, NO_OF_GRIDS - width);
-        width = Math.min(width, NO_OF_GRIDS);
-      }
-    }
 
     component.layouts[currentLayout] = {
       ...component.layouts[currentLayout],
