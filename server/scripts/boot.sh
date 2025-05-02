5d9ab6aecff49fd9f8e6dc22363bd99b3e45aefc
Adish M <adish.madhu@gmail.com>
2025-05-02 21:07:11 +0530

    Adding volume for render PR app

--- server/scripts/boot.sh
+++ server/scripts/boot.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
 
-service postgresql start
+DATA_DIR="/var/data/postgresql"
+if [ ! -s "$DATA_DIR/PG_VERSION" ]; then
+  echo "Initializing PostgreSQL data directory..."
+  pg_ctl -D "$DATA_DIR" init
+fi
+
+echo "Starting PostgreSQL using custom data directory..."
+pg_ctl -D "$DATA_DIR" -o "-c listen_addresses='localhost'" -w start
+
+# Set up the user and DB only if it doesn't exist
+psql -U postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = 'tooljet'" | grep -q 1 || \
+  psql -U postgres -c "CREATE ROLE tooljet WITH LOGIN SUPERUSER PASSWORD 'postgres';"
 
 echo "
    _____           _   ___      _
