59cac14f4dea487a9ea8285a9247cf6addbc387d
devanshu052000 <devanshu.rastogi05@gmail.com>
2025-05-22 17:54:04 +0530

    Added filter logic for released apps

--- server/src/modules/app-permissions/repositories/query-users.repository.ts
+++ server/src/modules/app-permissions/repositories/query-users.repository.ts
@@ -2,6 +2,7 @@ import { QueryUser } from '@entities/query_users.entity';
 import { Injectable } from '@nestjs/common';
 import { DataSource, EntityManager, Repository } from 'typeorm';
 import { dbTransactionWrap } from '@helpers/database.helper';
+import { QueryPermission } from '@entities/query_permissions.entity';
 
 @Injectable()
 export class QueryUsersRepository extends Repository<QueryUser> {
@@ -40,6 +41,43 @@ export class QueryUsersRepository extends Repository<QueryUser> {
         });
       });
       return manager.save(queryUsers);
+    }, manager || this.manager);
+  }
+
+  async checkQueryUserWithGroup(
+    queryPermission: QueryPermission,
+    userId: string,
+    manager?: EntityManager
+  ): Promise<boolean> {
+    return dbTransactionWrap(async (manager: EntityManager) => {
+      const result = await manager
+        .createQueryBuilder(QueryUser, 'query_users')
+        .innerJoin('query_users.permissionGroup', 'group')
+        .innerJoin('group.groupUsers', 'groupUser')
+        .where('query_users.queryPermission = :permissionId', {
+          permissionId: queryPermission.id,
+        })
+        .andWhere('groupUser.userId = :userId', { userId })
+        .getOne();
+
+      return !!result;
+    }, manager || this.manager);
+  }
+
+  async checkQueryUserWithSingle(
+    queryPermission: QueryPermission,
+    userId: string,
+    manager?: EntityManager
+  ): Promise<boolean> {
+    return dbTransactionWrap(async (manager: EntityManager) => {
+      const queryUser = await manager.findOne(QueryUser, {
+        where: {
+          queryPermission: { id: queryPermission.id },
+          userId,
+        },
+      });
+
+      return !!queryUser;
     }, manager || this.manager);
   }
 }
