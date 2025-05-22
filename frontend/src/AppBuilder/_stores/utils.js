ff22d593717b0790cf15cf6d598a4a1e8e8b5dfb
Shaurya Sharma <shaurya064@gmail.com>
2025-05-23 01:11:19 +0530

    Base theme added

--- frontend/src/AppBuilder/_stores/utils.js
+++ frontend/src/AppBuilder/_stores/utils.js
@@ -715,9 +715,92 @@ export const baseTheme = {
   definition: {
     brand: {
       colors: {
-        primary: { light: '#4368E3', dark: '#4A6DD9' },
-        secondary: { light: '#6A727C', dark: '#CFD3D8' },
-        tertiary: { light: '#1E823B', dark: '#318344' },
+        primary: {
+          light: '#4368E3',
+          dark: '#4A6DD9',
+        },
+        secondary: {
+          light: '#6A727C',
+          dark: '#CFD3D8',
+        },
+        tertiary: {
+          light: '#1E823B',
+          dark: '#318344',
+        },
+      },
+    },
+    text: {
+      font: 'IBM Plex Sans',
+      colors: {
+        primary: {
+          light: '#1B1F24',
+          dark: '#CFD3D8',
+        },
+        placeholder: {
+          light: '#6A727C',
+          dark: '#858C94',
+        },
+        disabled: {
+          light: '#ACB2B9',
+          dark: '#545B64',
+        },
+      },
+    },
+    border: {
+      radius: {
+        default: 6,
+        small: 0,
+        large: 0,
+      },
+      colors: {
+        default: {
+          light: '#CCD1D5',
+          dark: '#3C434B',
+        },
+        weak: {
+          light: '#E4E7EB',
+          dark: '#EEF0F1',
+        },
+        disabled: {
+          light: '#E4E7EB',
+          dark: '#F6F8FA',
+        },
+      },
+    },
+    systemStatus: {
+      colors: {
+        success: {
+          light: '#1E823B',
+          dark: '#318344',
+        },
+        error: {
+          light: '#D72D39',
+          dark: '#D03F43',
+        },
+        warning: {
+          light: '#BF4F03',
+          dark: '#BA5722',
+        },
+      },
+    },
+    surface: {
+      colors: {
+        appBackground: {
+          light: '#F6F6F6',
+          dark: '#121518',
+        },
+        surface1: {
+          light: '#FFFFFF',
+          dark: '#1E2226',
+        },
+        surface2: {
+          light: '#F6F8FA',
+          dark: '#2B3036',
+        },
+        surface3: {
+          light: '#E4E7EB',
+          dark: '#3C434B',
+        },
       },
     },
   },
