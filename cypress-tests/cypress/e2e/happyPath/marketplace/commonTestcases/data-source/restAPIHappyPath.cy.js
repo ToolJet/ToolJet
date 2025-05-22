f3c19d6f777fe745d95182493689319afb27c810
Mekhla Asopa <dadhichmekhla@gmail.com>
2025-05-22 19:43:43 +0530

    add content-type scenario

--- cypress-tests/cypress/e2e/happyPath/marketplace/commonTestcases/data-source/restAPIHappyPath.cy.js
+++ cypress-tests/cypress/e2e/happyPath/marketplace/commonTestcases/data-source/restAPIHappyPath.cy.js
@@ -331,7 +331,7 @@ describe("Data source Rest API", () => {
     cy.verifyToastMessage(commonSelectors.toastMessage, "Data Source Saved");
     cy.apiDeleteGDS(`cypress-${data.dataSourceName}-restapi`);
   });
-  it.only("Should verify connection response for all methods", () => {
+  it("Should verify connection response for all methods", () => {
     cy.apiCreateGDS(
       `${Cypress.env("server_host")}/api/data-sources`,
       `cypress-${data.dataSourceName}-restapi`,
@@ -367,89 +367,66 @@ describe("Data source Rest API", () => {
     );
     cy.reload();
 
-    cy.apiCreateApp(`${fake.companyName}-restAPI-App`);
+    cy.apiCreateApp(`${fake.companyName}-restAPI-CURD-App`);
     cy.openApp();
-    createAndRunRestAPIQuery(
-      "get_beeceptor_data",
-      `cypress-${data.dataSourceName}-restapi`,
-      "GET",
-      "",
-      [],
-      [],
-      null,
-      true,
-      "/api/users",
-      true
-    );
-    createAndRunRestAPIQuery(
-      "post_restapi",
-      `cypress-${data.dataSourceName}-restapi`,
-      "POST",
-      "",
-      [["Content-Type", "application/json"]],
-      [],
-      {
-        price: 200,
-        name: "Violin",
-      },
-      true,
-      "/api/users",
-      true
-    );
+    createAndRunRestAPIQuery({
+      queryName: "get_beeceptor_data",
+      dsName: `cypress-${data.dataSourceName}-restapi`,
+      method: "GET",
+      urlSuffix: "/api/users",
+      run: true,
+    });
+    createAndRunRestAPIQuery({
+      queryName: "post_restapi",
+      dsName: `cypress-${data.dataSourceName}-restapi`,
+      method: "POST",
+      headersList: [["Content-Type", "application/json"]],
+      rawBody: '{"price": 200,"name": "Violin"}',
+      urlSuffix: "/api/users",
+      expectedResponseShape: { price: 200, name: "Violin", id: true },
+    });
     cy.readFile("cypress/fixtures/restAPI/storedId.json").then(
       (postResponseID) => {
         const id1 = postResponseID.id;
-        createAndRunRestAPIQuery(
-          "put_restapi_id",
-          `cypress-${data.dataSourceName}-restapi`,
-          "PUT",
-          "",
-          [["Content-Type", "application/json"]],
-          [],
-          {
-            price: 500,
-            name: "Guitar",
-          },
-          true,
-          `/api/users/${id1}`,
-          true
-        );
-        createAndRunRestAPIQuery(
-          "patch_restapi_id",
-          `cypress-${data.dataSourceName}-restapi`,
-          "PATCH",
-          "",
-          [["Content-Type", "application/json"]],
-          [],
-          { price: 999 },
-          true,
-          `/api/users/${id1}`,
-          true
-        );
-        createAndRunRestAPIQuery(
-          "get_restapi_id",
-          `cypress-${data.dataSourceName}-restapi`,
-          "GET",
-          "",
-          [],
-          [],
-          true,
-          `/api/users/${id1}`,
-          true
-        );
-        createAndRunRestAPIQuery(
-          "delete_restapi_id",
-          `cypress-${data.dataSourceName}-restapi`,
-          "DELETE",
-          "",
-          [],
-          [],
-          true,
-          `/api/users/${id1}`
-        );
+
+        createAndRunRestAPIQuery({
+          queryName: "put_restapi_id",
+          dsName: `cypress-${data.dataSourceName}-restapi`,
+          method: "PUT",
+          headersList: [["Content-Type", "application/json"]],
+          rawBody: '{"price": 500,"name": "Guitar"}',
+          urlSuffix: `/api/users/${id1}`,
+          expectedResponseShape: { price: 500, name: "Guitar", id: id1 },
+        });
+        createAndRunRestAPIQuery({
+          queryName: "patch_restapi_id",
+          dsName: `cypress-${data.dataSourceName}-restapi`,
+          method: "PATCH",
+          headersList: [["Content-Type", "application/json"]],
+          rawBody: '{"price": 999 }',
+          urlSuffix: `/api/users/${id1}`,
+          run: true,
+          expectedResponseShape: { price: 999, id: id1 },
+        });
+        createAndRunRestAPIQuery({
+          queryName: "get_restapi_id",
+          dsName: `cypress-${data.dataSourceName}-restapi`,
+          method: "GET",
+          urlSuffix: `/api/users/${id1}`,
+          run: true,
+          expectedResponseShape: { price: 999, name: "Guitar", id: id1 },
+        });
+        createAndRunRestAPIQuery({
+          queryName: "delete_restapi_id",
+          dsName: `cypress-${data.dataSourceName}-restapi`,
+          method: "DELETE",
+          urlSuffix: `/api/users/${id1}`,
+          run: true,
+          expectedResponseShape: { success: true },
+        });
       }
     );
-    // cy.apiDeleteGDS(`cypress-${data.dataSourceName}-restapi`);
+    cy.apiDeleteGDS(`cypress-${data.dataSourceName}-restapi`);
   });
   it("Should verify response for basic authentication type connection", () => {
     cy.apiCreateGDS(
@@ -497,33 +474,20 @@ describe("Data source Rest API", () => {
     );
     cy.reload();
     cy.intercept("GET", "/api/library_apps").as("appLibrary");
-    cy.apiCreateApp(`${fake.companyName}-restAPI-App`);
-    cy.openApp();
-    createAndRunRestAPIQuery(
-      "get_basic_auth_valid",
-      `cypress-${data.dataSourceName}-restapi`,
-      "GET",
-      "",
-      [],
-      [],
-      null,
-      true,
-      "/basic-auth/user/pass",
-      true,
-      { authenticated: true, user: "user" }
-    );
-    createAndRunRestAPIQuery(
-      "get_basic_auth_invalid",
-      `cypress-${data.dataSourceName}-restapi`,
-      "GET",
-      "",
-      [],
-      [],
-      null,
-      true,
-      "/basic-auth/invaliduser/invalidpass",
-      false
-    );
+    cy.apiCreateApp(`${fake.companyName}-restAPI-Basic-App`);
+    createAndRunRestAPIQuery({
+      queryName: "get_basic_auth_valid",
+      dsName: `cypress-${data.dataSourceName}-restapi`,
+      method: "GET",
+      urlSuffix: "/basic-auth/user/pass",
+      expectedResponseShape: { authenticated: true, user: "user" },
+    });
+    createAndRunRestAPIQuery({
+      queryName: "get_basic_auth_invalid",
+      dsName: `cypress-${data.dataSourceName}-restapi`,
+      method: "GET",
+      urlSuffix: "/basic-auth/invaliduser/invalidpass",
+    });
     cy.apiDeleteGDS(`cypress-${data.dataSourceName}-restapi`);
   });
   it("Should verify response for bearer authentication type connection", () => {
@@ -572,21 +536,15 @@ describe("Data source Rest API", () => {
     );
     cy.reload();
     cy.intercept("GET", "/api/library_apps").as("appLibrary");
-    cy.apiCreateApp(`${fake.companyName}-restAPI-App`);
-    // cy.openApp();
-    createAndRunRestAPIQuery(
-      "get_bearer_auth_valid",
-      `cypress-${data.dataSourceName}-restapi`,
-      "GET",
-      "",
-      [],
-      [],
-      null,
-      true,
-      "/bearer",
-      true,
-      { authenticated: true, token: "my-token-123" }
-    );
+    cy.apiCreateApp(`${fake.companyName}-restAPI-Bearer-App`);
+    cy.openApp();
+    createAndRunRestAPIQuery({
+      queryName: "get_bearer_auth_valid",
+      dsName: `cypress-${data.dataSourceName}-restapi`,
+      method: "GET",
+      urlSuffix: "/bearer",
+      expectedResponseShape: { authenticated: true, token: "my-token-123" },
+    });
     cy.intercept("GET", "api/data_sources?**").as("datasource");
     cy.apiCreateGDS(
       `${Cypress.env("server_host")}/api/data-sources`,
@@ -631,23 +589,17 @@ describe("Data source Rest API", () => {
         { key: "tokenData", encrypted: false },
       ]
     );
-    cy.apiCreateApp(`${fake.companyName}-restAPI-App-invalid`);
-    //cy.openApp();
-    createAndRunRestAPIQuery(
-      "get_bearer_auth_invalid",
-      `cypress-${data.dataSourceName}-restapi-invalid`,
-      "GET",
-      "",
-      [],
-      [],
-      null,
-      true,
-      "/bearer",
-      false
-    );
+    cy.apiCreateApp(`${fake.companyName}-restAPI-Bearer-invalid`);
+    cy.openApp();
+    createAndRunRestAPIQuery({
+      queryName: "get_bearer_auth_invalid",
+      dsName: `cypress-${data.dataSourceName}-restapi-invalid`,
+      method: "GET",
+      urlSuffix: "/bearer",
+    });
     cy.apiDeleteGDS(`cypress-${data.dataSourceName}-restapi`);
   });
-  it("Should verify response for authentication code grant type connection", () => {
+  it.skip("Should verify response for authentication code grant type connection", () => {
     cy.apiCreateGDS(
       `${Cypress.env("server_host")}/api/data-sources`,
       `cypress-${data.dataSourceName}-restapi`,
@@ -704,6 +656,95 @@ describe("Data source Rest API", () => {
     );
     cy.reload();
     cy.intercept("GET", "/api/library_apps").as("appLibrary");
-    cy.apiCreateApp(`${fake.companyName}-restAPI-App`);
+    cy.apiCreateApp(`${fake.companyName}-client-Grant-RestAPI`);
+  });
+  it("Should verify response for content-type", () => {
+    cy.apiCreateApp(`${fake.companyName}-restAPI-Content-App`);
+    createAndRunRestAPIQuery({
+      queryName: "post_json",
+      dsName: "restapidefault",
+      method: "POST",
+      url: "https://jsonplaceholder.typicode.com/posts",
+      headersList: [["Content-Type", "application/json"]],
+      rawBody: '{"title": "foo","body": "bar","userId": 1}',
+      run: true,
+      urlSuffix: "",
+      expectedResponseShape: { id: true, title: "foo", body: "bar", userId: 1 },
+    });
+    createAndRunRestAPIQuery({
+      queryName: "post_raw_text",
+      dsName: "restapidefault",
+      method: "POST",
+      url: "https://httpbin.org/post",
+      headersList: [["Content-Type", "text/plain"]],
+      rawBody: "This is plain text content",
+      jsonBody: null,
+      run: true,
+      expectedResponseShape: { data: "This is plain text content" },
+    });
+    createAndRunRestAPIQuery({
+      queryName: "post_form_urlencoded",
+      dsName: "restapidefault",
+      method: "POST",
+      url: "https://httpbin.org/post",
+      headersList: [["Content-Type", "application/x-www-form-urlencoded"]],
+      bodyList: [
+        ["name", "Jane"],
+        ["age", "30"],
+      ],
+      expectedResponseShape: {
+        "form.name": "Jane",
+        "form.age": "30",
+      },
+    });
+    createAndRunRestAPIQuery({
+      queryName: "post_xml_soap",
+      dsName: "restapidefault",
+      method: "POST",
+      url: "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL",
+      headersList: [["Content-Type", "text/xml; charset=utf-8"]],
+      rawBody: `<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <ListOfContinentsByName xmlns="http://www.oorsprong.org/websamples.countryinfo">
+        </ListOfContinentsByName>
+      </soap:Body>
+    </soap:Envelope>`,
+      jsonBody: null,
+      bodyList: [],
+      cookiesList: [["session", "abc123"]],
+      paramsList: [["lang", "en"]],
+      run: true,
+      shouldSucceed: true,
+      expectedResponseShape: {},
+    });
+    // createAndRunRestAPIQuery({
+    //   queryName: "post_text_csv",
+    //   dsName: "restapidefault",
+    //   method: "POST",
+    //   url: `https://tejasvi.free.beeceptor.com/csv-upload`,
+    //   headersList: [["Content-Type", "text/csv"]],
+    //   rawBody:
+    //     "id,name,email\n1,Alice,alice@example.com\n2,Bob,bob@example.com",
+    //   expectedResponseShape: {
+    //     data: '{\n       "status": "ok",\n        "message": "File uploaded successfully",\n        "body": id,name,email\n1,Alice,alice@example.com\n2,Bob,bob@example.com\n}',
+    //   },
+    // });
+    // const filename = "tooljet.png";
+
+    // createAndRunRestAPIQuery({
+    //   queryName: "upload_image",
+    //   dsName: "restapidefault",
+    //   method: "POST",
+    //   url: `https://tejasvi.free.beeceptor.commultipart-upload`,
+    //   headersList: [["Content-Type", "multipart/form-data"]],
+    //   bodyList: [
+    //     ["Image_File", "fixture:Image/tooljet.png"],
+    //     ["filename", filename],
+    //   ],
+    //   expectedResponseShape: {
+    //     filename: filename,
+    //   },
+    // });
   });
 });
