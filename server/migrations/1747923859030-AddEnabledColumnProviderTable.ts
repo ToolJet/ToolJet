58b5d0a03eb43e4df0ed74494dbfdd6824d7eb71
rohanlahori <rohanlahori99@gmail.com>
2025-05-23 14:22:51 +0530

    migration

---
+++ server/migrations/1747923859030-AddEnabledColumnProviderTable.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddEnabledColumnProviderTable1747923859030 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "organization_git_ssh" ADD COLUMN IF NOT EXISTS "is_enabled" boolean DEFAULT FALSE;
+    `);
+
+    await queryRunner.query(`
+      UPDATE "organization_git_ssh" git_ssh
+      SET "is_enabled" = TRUE 
+      FROM "organization_git_sync" git_sync 
+      WHERE git_ssh."config_id" = git_sync."id" 
+        AND git_sync."is_enabled" = TRUE 
+        AND git_sync."git_type" = 'github_ssh';
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "organization_git_https" ADD COLUMN IF NOT EXISTS "is_enabled" boolean DEFAULT FALSE;
+    `);
+
+    await queryRunner.query(`
+      UPDATE "organization_git_https" git_ssh
+      SET "is_enabled" = TRUE 
+      FROM "organization_git_sync" git_sync 
+      WHERE git_ssh."config_id" = git_sync."id" 
+        AND git_sync."is_enabled" = TRUE 
+        AND git_sync."git_type" = 'github_https';
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "organization_gitlab" ADD COLUMN IF NOT EXISTS "is_enabled" boolean DEFAULT FALSE;
+    `);
+
+    await queryRunner.query(`
+      UPDATE "organization_gitlab" git_ssh
+      SET "is_enabled" = TRUE 
+      FROM "organization_git_sync" git_sync 
+      WHERE git_ssh."config_id" = git_sync."id" 
+        AND git_sync."is_enabled" = TRUE 
+        AND git_sync."git_type" = 'gitlab';
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
