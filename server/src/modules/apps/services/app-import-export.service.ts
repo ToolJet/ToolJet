8bc68b7920b8a0b0b9fc431e6c2e4a0d8b6eec7c
rohanlahori <rohanlahori99@gmail.com>
2025-05-26 14:59:11 +0530

    Revert "revert appimport export changes"
    This reverts commit b139db811e44157eef04ed919c93c315d5837d98.

--- server/src/modules/apps/services/app-import-export.service.ts
+++ server/src/modules/apps/services/app-import-export.service.ts
@@ -33,7 +33,6 @@ import { DataSourcesUtilService } from '@modules/data-sources/util.service';
 import { DataSourcesRepository } from '@modules/data-sources/repository';
 import { AppEnvironmentUtilService } from '@modules/app-environments/util.service';
 import { ComponentsService } from './component.service';
-import { UsersUtilService } from '@modules/users/util.service';
 interface AppResourceMappings {
   defaultDataSourceIdMapping: Record<string, string>;
   dataQueryMapping: Record<string, string>;
@@ -91,7 +90,6 @@ export class AppImportExportService {
     protected dataSourcesUtilService: DataSourcesUtilService,
     protected dataSourcesRepository: DataSourcesRepository,
     protected appEnvironmentUtilService: AppEnvironmentUtilService,
-    protected usersUtilService: UsersUtilService,
     protected readonly entityManager: EntityManager,
     protected componentsService: ComponentsService
   ) {}
@@ -106,7 +104,7 @@ export class AppImportExportService {
         .createQueryBuilder(App, 'apps')
         .where('apps.id = :id AND apps.organization_id = :organizationId', {
           id,
-          organizationId: user?.organizationId,
+          organizationId: user.organizationId,
         });
       const appToExport = await queryForAppToExport.getOne();
 
@@ -135,7 +133,7 @@ export class AppImportExportService {
       const appEnvironments = await manager
         .createQueryBuilder(AppEnvironment, 'app_environments')
         .where('app_environments.organizationId = :organizationId', {
-          organizationId: user?.organizationId,
+          organizationId: user.organizationId,
         })
         .orderBy('app_environments.createdAt', 'ASC')
         .getMany();
@@ -352,8 +350,8 @@ export class AppImportExportService {
     return await catchDbException(async () => {
       const importedApp = manager.create(App, {
         name: appParams.name,
-        organizationId: user?.organizationId,
-        userId: user.id, //fetch super admin user id for EE
+        organizationId: user.organizationId,
+        userId: user.id,
         slug: null,
         icon: appParams.icon,
         creationMode: `${isGitApp ? 'GIT' : 'DEFAULT'}`,
@@ -774,7 +772,7 @@ export class AppImportExportService {
 
         const { dataQueryMapping } = await this.createDataQueriesForAppVersion(
           manager,
-          user?.organizationId,
+          user.organizationId,
           importingDataQueriesForAppVersion,
           importingDataSource,
           dataSourceForAppVersion,
@@ -1165,7 +1163,7 @@ export class AppImportExportService {
     appResourceMappings: AppResourceMappings
   ) {
     const defaultDataSourceIds = await this.createDefaultDataSourceForVersion(
-      user?.organizationId,
+      user.organizationId,
       appResourceMappings.appVersionMapping[appVersion.id],
       DefaultDataSourceKinds,
       manager
@@ -1204,7 +1202,7 @@ export class AppImportExportService {
           kind: dataSource.kind,
           type: DataSourceTypes.DEFAULT,
           scope: 'global',
-          organizationId: user?.organizationId,
+          organizationId: user.organizationId,
         },
       });
     };
@@ -1215,7 +1213,7 @@ export class AppImportExportService {
           kind: dataSource.kind,
           type: In([DataSourceTypes.DEFAULT, DataSourceTypes.SAMPLE]),
           scope: 'global',
-          organizationId: user?.organizationId,
+          organizationId: user.organizationId,
         },
       });
     };
@@ -1233,7 +1231,7 @@ export class AppImportExportService {
 
       if (plugin) {
         const newDataSource = manager.create(DataSource, {
-          organizationId: user?.organizationId,
+          organizationId: user.organizationId,
           name: dataSource.name,
           kind: dataSource.kind,
           type: DataSourceTypes.DEFAULT,
@@ -1248,7 +1246,7 @@ export class AppImportExportService {
 
     const createNewGlobalDs = async (ds: DataSource): Promise<DataSource> => {
       const newDataSource = manager.create(DataSource, {
-        organizationId: user?.organizationId,
+        organizationId: user.organizationId,
         name: dataSource.name,
         kind: dataSource.kind,
         type: DataSourceTypes.DEFAULT,
@@ -1276,7 +1274,7 @@ export class AppImportExportService {
   ) {
     appResourceMappings = { ...appResourceMappings };
     const currentOrgEnvironments = await this.appEnvironmentUtilService.getAll(
-      user?.organizationId,
+      user.organizationId,
       appVersion.appId,
       manager
     );
@@ -1338,7 +1336,7 @@ export class AppImportExportService {
     appResourceMappings = { ...appResourceMappings };
     const { appVersionMapping, appDefaultEnvironmentMapping } = appResourceMappings;
     const organization: Organization = await manager.findOne(Organization, {
-      where: { id: user?.organizationId },
+      where: { id: user.organizationId },
       relations: ['appEnvironments'],
     });
     let currentEnvironmentId: string;
@@ -1557,7 +1555,7 @@ export class AppImportExportService {
 
     // Create default data sources
     const defaultDataSourceIds = await this.createDefaultDataSourceForVersion(
-      user?.organizationId,
+      user.organizationId,
       version.id,
       DefaultDataSourceKinds,
       manager
@@ -1565,7 +1563,7 @@ export class AppImportExportService {
     let envIdArray: string[] = [];
 
     const organization: Organization = await manager.findOne(Organization, {
-      where: { id: user?.organizationId },
+      where: { id: user.organizationId },
       relations: ['appEnvironments'],
     });
     envIdArray = [...organization.appEnvironments.map((env) => env.id)];
@@ -1574,7 +1572,7 @@ export class AppImportExportService {
       await Promise.all(
         defaultAppEnvironments.map(async (en) => {
           const env = manager.create(AppEnvironment, {
-            organizationId: user?.organizationId,
+            organizationId: user.organizationId,
             name: en.name,
             isDefault: en.isDefault,
             priority: en.priority,
@@ -1641,7 +1639,7 @@ export class AppImportExportService {
             ? this.replaceTooljetDbTableIds(
                 query.options,
                 externalResourceMappings['tooljet_database'],
-                user?.organizationId
+                user.organizationId
               )
             : query.options,
       });
