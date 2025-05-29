aa9705d5788911da18e249ed1df52fef3a565696
thesynthax <kushagra1403@gmail.com>
2025-05-29 20:45:39 +0530

    typesense error standardization
    Signed-off-by: thesynthax <kushagra1403@gmail.com>

--- plugins/packages/typesense/lib/operations.ts
+++ plugins/packages/typesense/lib/operations.ts
@@ -1,53 +1,33 @@
+const parse = (obj) => {
+  return obj !== '' ? JSON.parse(obj) : {};
+}
+
 export async function createCollection(client, schema: string): Promise<object> {
-  try {
-    const result = await client.collections().create(JSON.parse(schema));
-    return result;
-  } catch (exception) {
-    return { error: exception.message, status: 'failed' };
-  }
+  const result = await client.collections().create(parse(schema));
+  return result;
 }
 
 export async function search(client, collection: string, searchParams: string): Promise<object> {
-  try {
-    const result = await client.collections(collection).documents().search(JSON.parse(searchParams));
-    return result;
-  } catch (exception) {
-    return { error: exception.message, status: 'failed' };
-  }
+  const result = await client.collections(collection).documents().search(parse(searchParams));
+  return result;
 }
 
 export async function indexDocument(client, collection: string, document: string): Promise<object> {
-  try {
-    const result = await client.collections(collection).documents().create(JSON.parse(document));
-    return result;
-  } catch (exception) {
-    return { error: exception.message, status: 'failed' };
-  }
+  const result = await client.collections(collection).documents().create(parse(document));
+  return result;
 }
 
 export async function getDocument(client, collection: string, id: string): Promise<object> {
-  try {
-    const result = await client.collections(collection).documents(id).retrieve();
-    return result;
-  } catch (exception) {
-    return { error: exception.message, status: 'failed' };
-  }
+  const result = await client.collections(collection).documents(id).retrieve();
+  return result;
 }
 
 export async function updateDocument(client, collection: string, id: string, document: string): Promise<object> {
-  try {
-    const result = await client.collections(collection).documents(id).update(JSON.parse(document));
-    return result;
-  } catch (exception) {
-    return { error: exception.message, status: 'failed' };
-  }
+  const result = await client.collections(collection).documents(id).update(parse(document));
+  return result;
 }
 
 export async function deleteDocument(client, collection: string, id: string): Promise<object> {
-  try {
-    const result = await client.collections(collection).documents(id).delete();
-    return result;
-  } catch (exception) {
-    return { error: exception.message, status: 'failed' };
-  }
+  const result = await client.collections(collection).documents(id).delete();
+  return result;
 }
