0d358e441dc4ed4c569cc28480517379d0e44c2c
Kavin Venkatachalam <kavin.saratha@gmail.com>
2025-06-05 09:06:36 +0530

    chore: enabled console logs

--- frontend/src/_ui/ErrorBoundary/index.jsx
+++ frontend/src/_ui/ErrorBoundary/index.jsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component {
 
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
-    console.log(error, errorInfo);
+    console.log('error--- 1---  ', error, errorInfo);
   }
 
   render() {
