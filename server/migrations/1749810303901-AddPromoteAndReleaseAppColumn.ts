5ed48794662d029cd653c4fc8af248c37b457f3c
rohanlahori <rohanlahori99@gmail.com>
2025-06-13 17:50:11 +0530

    removed extra migration

--- server/migrations/1749810303901-AddPromoteAndReleaseAppColumn.ts
+++ server/migrations/1749810303901-AddPromoteAndReleaseAppColumn.ts
@@ -1,7 +1,9 @@
+import { GroupPermissions } from '@entities/group_permissions.entity';
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class AddPromoteAndReleaseAppColumn1749810303901 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    const manager = queryRunner?.manager;
     await queryRunner.addColumns('permission_groups', [
       new TableColumn({
         name: 'promote_app',
@@ -16,6 +18,39 @@ export class AddPromoteAndReleaseAppColumn1749810303901 implements MigrationInte
         isNullable: false,
       }),
     ]);
+
+    const permissionGroups = await manager.find(GroupPermissions);
+
+    for (const group of permissionGroups) {
+      const { name, type, appCreate, appDelete } = group;
+      const hasAppPermissions = appCreate === true || appDelete;
+
+      // For custom groups and builders :
+      // If the group has  -> app create || app delete permissions  : assign promote and release permissions to the group otherwise false
+      if (type === 'default') {
+        switch (name) {
+          case 'admin':
+            group.promoteApp = true;
+            group.releaseApp = true;
+            break;
+          case 'end-user':
+            group.promoteApp = false;
+            group.releaseApp = false;
+            break;
+          case 'builder':
+            group.promoteApp = hasAppPermissions;
+            group.releaseApp = hasAppPermissions;
+            break;
+          default:
+            break;
+        }
+      } else if (type === 'custom') {
+        group.promoteApp = hasAppPermissions;
+        group.releaseApp = hasAppPermissions;
+      }
+
+      await manager.save(group);
+    }
   }
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
