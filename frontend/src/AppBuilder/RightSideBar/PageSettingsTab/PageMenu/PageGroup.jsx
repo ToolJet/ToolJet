3bc70ad514f0f0be611ce65556c3695a9e1d04c3
Vijaykant Yadav <vjy239@gmail.com>
2025-07-01 12:29:51 +0530

    bug fixes

--- frontend/src/AppBuilder/RightSideBar/PageSettingsTab/PageMenu/PageGroup.jsx
+++ frontend/src/AppBuilder/RightSideBar/PageSettingsTab/PageMenu/PageGroup.jsx
@@ -14,7 +14,7 @@ import { ToolTip } from '@/_components';
 import { useModuleContext } from '@/AppBuilder/_contexts/ModuleContext';
 import { resolveReferences } from '@/_helpers/utils';
 
-const RenderPage = ({
+export const RenderPage = ({
   page,
   currentPageId,
   switchPageWrapper,
@@ -43,7 +43,7 @@ const RenderPage = ({
 
     return <Icon {...props} />;
   };
-  return (resolveReferences(page?.hidden?.value) || page.disabled) && page?.restricted ? null : (
+  return resolveReferences(page?.hidden?.value) || page.disabled || page?.restricted ? null : (
     <div
       key={page.name}
       data-id={page.id}
