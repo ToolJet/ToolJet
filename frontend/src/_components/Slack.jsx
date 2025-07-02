bd574ae2260fd96fdc0a3206cd84b9edbfdef987
parthy007 <parthadhikari1812@gmail.com>
2025-07-03 00:11:37 +0530

    Update slack frontend

--- frontend/src/_components/Slack.jsx
+++ frontend/src/_components/Slack.jsx
@@ -4,6 +4,10 @@ import { useTranslation } from 'react-i18next';
 import { toast } from 'react-hot-toast';
 import Button from '@/_ui/Button';
 import { retrieveWhiteLabelText } from '@white-label/whiteLabelling';
+import Input from '@/_ui/Input';
+import Select from '@/_ui/Select';
+import { canDeleteDataSource, canUpdateDataSource } from '@/_helpers';
+import SolidIcon from '@/_ui/Icon/SolidIcons';
 
 const Slack = ({
   optionchanged,
@@ -13,11 +17,28 @@ const Slack = ({
   selectedDataSource,
   currentAppEnvironmentId,
   isDisabled,
+  workspaceConstants,
 }) => {
   const [authStatus, setAuthStatus] = useState(null);
   const [whiteLabelText, setWhiteLabelText] = useState('');
-  const plugin_id = selectedDataSource?.plugin?.id;
+  const hostUrl = window.public_config?.TOOLJET_HOST;
+  const subPathUrl = window.public_config?.SUB_PATH;
+  const fullUrl = `${hostUrl}${subPathUrl ? subPathUrl : '/'}oauth2/authorize`;
+  const redirectUri = fullUrl;
   const { t } = useTranslation();
+
+  const defaultScopes = [
+    'users:read',
+    'channels:read',
+    'groups:read',
+    'im:read',
+    'mpim:read',
+    'channels:history',
+    'groups:history',
+    'im:history',
+    'mpim:history',
+  ];
+
   useEffect(() => {
     async function fetchLabel() {
       const text = await retrieveWhiteLabelText();
@@ -37,7 +58,7 @@ const Slack = ({
     }
 
     datasourceService
-      .fetchOauth2BaseUrl(provider, plugin_id, {})
+      .fetchOauth2BaseUrl(provider, null, options)
       .then((data) => {
         const authUrl = `${data.url}&scope=${scope}&access_type=offline&prompt=select_account`;
 
@@ -62,6 +83,12 @@ const Slack = ({
     });
   }
 
+  function toggleAccessType() {
+    options?.access_type?.value === 'chat:write'
+      ? optionchanged('access_type', '')
+      : optionchanged('access_type', 'chat:write');
+  }
+
   return (
     <div>
       <div className="row">
@@ -75,12 +102,34 @@ const Slack = ({
                 { whiteLabelText }
               )}
             </p>
+            <div className="row w-100">
+              <div className="card-body datasource-footer-info" style={{ height: '100px' }}>
+                <div className="row">
+                  <div className="col-1">
+                    <SolidIcon name="information" fill="#3E63DD" />
+                  </div>
+
+                  <div className="col" style={{ maxWidth: '480px' }}>
+                    <p data-cy="white-list-ip-text" className="tj-text" style={{ fontSize: '12px' }}>
+                      By default this data source requires the following permissions.
+                    </p>
+                    <p data-cy="white-list-ip-text" className="tj-text">
+                      {defaultScopes.map((scope) => (
+                        <span key={scope} class="badge badge-light mb-1 mt-1" style={{ marginRight: '.25rem' }}>
+                          {scope}
+                        </span>
+                      ))}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div>
               <label className="form-check mt-3">
                 <input
                   className="form-check-input"
                   type="radio"
-                  onClick={() => optionchanged('access_type', 'chat:write')}
+                  onClick={() => toggleAccessType()}
                   checked={options?.access_type?.value === 'chat:write'}
                   disabled={authStatus === 'waiting_for_token' || isDisabled}
                 />
@@ -96,6 +145,59 @@ const Slack = ({
                 </span>
               </label>
             </div>
+          </div>
+          <div>
+            <label className="form-label mt-3">Slack app</label>
+            <Select
+              options={[
+                { value: 'from_env', name: 'Use environment variables' },
+                { value: 'from_datasource_configuration', name: 'Custom slack app' },
+              ]}
+              value={options?.credential_source?.value}
+              onChange={(value) => {
+                optionchanged('credential_source', value);
+              }}
+              width="100%"
+              isDisabled={!canUpdateDataSource(selectedDataSource?.id) && !canDeleteDataSource()}
+              encrypted={false}
+            />
+          </div>
+          {options?.credential_source?.value === 'from_datasource_configuration' && (
+            <>
+              <div>
+                <label className="form-label mt-3">Client ID</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => optionchanged('client_id', e.target.value)}
+                  value={options?.client_id?.value ?? ''}
+                  placeholder="Client ID"
+                  workspaceConstants={workspaceConstants}
+                />
+              </div>
+              <div>
+                <label className="form-label mt-3">Client Secret</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => optionchanged('client_secret', e.target.value)}
+                  value={options?.client_secret?.value ?? ''}
+                  placeholder="Client Secret"
+                  workspaceConstants={workspaceConstants}
+                  encrypted={true}
+                />
+              </div>
+            </>
+          )}
+          <div>
+            <label className="form-label mt-3">Redirect URI</label>
+            <Input
+              value={redirectUri}
+              helpText="In Slack, use the URL above when prompted to enter an OAuth callback or redirect URL"
+              type="copyToClipboard"
+              disabled="true"
+              className="form-control"
+            />
           </div>
         </div>
       </div>
