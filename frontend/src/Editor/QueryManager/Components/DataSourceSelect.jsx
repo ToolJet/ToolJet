df6949876d4ae97b8f49429955bfb9d315693e0d
manishkushare <kushare.manish9@gmail.com>
2025-07-08 17:54:41 +0530

    Added sample data source field in the query switcher and while adding new node

--- frontend/src/Editor/QueryManager/Components/DataSourceSelect.jsx
+++ frontend/src/Editor/QueryManager/Components/DataSourceSelect.jsx
@@ -16,7 +16,15 @@ import './../queryManager.theme.scss';
 import { DATA_SOURCE_TYPE } from '@/_helpers/constants';
 import { workflowDefaultSources } from '../constants';
 
-function DataSourceSelect({ isDisabled, selectRef, closePopup, workflowDataSources, onNewNode, staticDataSources }) {
+function DataSourceSelect({
+  isDisabled,
+  selectRef,
+  closePopup,
+  workflowDataSources,
+  onNewNode,
+  staticDataSources,
+  sampleDataSources = [],
+}) {
   const dataSources = useDataSources();
   const globalDataSources = useGlobalDataSources();
   const sampleDataSource = useSampleDataSource();
@@ -137,6 +145,37 @@ function DataSourceSelect({ isDisabled, selectRef, closePopup, workflowDataSourc
     ...userDefinedSourcesOpts,
   ];
 
+  // Group sample data sources by kind
+  const groupedSampleDataSources =
+    sampleDataSources && sampleDataSources.length > 0
+      ? Object.entries(groupBy(sampleDataSources, 'kind')).map(([kind, sources]) => ({
+          label: (
+            <div>
+              <DataSourceIcon source={sources[0]} height={16} />
+              <span className="ms-1 small">{dataSourcesKinds.find((dsk) => dsk.kind === kind)?.name || kind}</span>
+            </div>
+          ),
+          options: sources.map((source) => ({
+            label: (
+              <div
+                key={source.id}
+                className="py-2 px-2 rounded option-nested-datasource-selector small text-truncate"
+                style={{ fontSize: '13px' }}
+                data-tooltip-id="tooltip-for-add-query-dd-option"
+                data-tooltip-content={decodeEntities(source.name)}
+                data-cy={`ds-${source.name.toLowerCase()}`}
+              >
+                {decodeEntities(source.name)}
+                <Tooltip id="tooltip-for-add-query-dd-option" className="tooltip query-manager-ds-select-tooltip" />
+              </div>
+            ),
+            value: source.id,
+            isNested: true,
+            source,
+          })),
+        }))
+      : [];
+
   const dataSourcesAvailable = [
     {
       label: (
@@ -159,6 +198,22 @@ function DataSourceSelect({ isDisabled, selectRef, closePopup, workflowDataSourc
       })),
     },
     ...userDefinedSourcesOpts,
+    // Sample data sources group header
+    ...(groupedSampleDataSources.length > 0
+      ? [
+          {
+            label: (
+              <div>
+                <span className="color-slate9" style={{ fontWeight: 500 }}>
+                  Sample data sources
+                </span>
+              </div>
+            ),
+            isDisabled: true,
+          },
+          ...groupedSampleDataSources,
+        ]
+      : []),
   ];
 
   const dataSourceList = workflowDataSources && workflowDataSources ? dataSourcesAvailable : DataSourceOptions;
