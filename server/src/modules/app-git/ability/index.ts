2cc63bb11356a7d3932af77b8c7bba73c45f68a7
rohanlahori <rohanlahori99@gmail.com>
2025-07-08 12:50:36 +0530

    git sync fixes

--- server/src/modules/app-git/ability/index.ts
+++ server/src/modules/app-git/ability/index.ts
@@ -3,15 +3,15 @@ import { Ability, AbilityBuilder, InferSubjects } from '@casl/ability';
 import { AbilityFactory } from '@modules/app/ability-factory';
 import { UserAllPermissions } from '@modules/app/types';
 import { FEATURE_KEY } from '../constants';
-import { App } from '@entities/app.entity';
+import { AppGitSync } from '@entities/app_git_sync.entity';
 
-type Subjects = InferSubjects<typeof App> | 'all';
+type Subjects = InferSubjects<typeof AppGitSync> | 'all';
 export type AppGitAbility = Ability<[FEATURE_KEY, Subjects]>;
 
 @Injectable()
 export class FeatureAbilityFactory extends AbilityFactory<FEATURE_KEY, Subjects> {
   protected getSubjectType() {
-    return App;
+    return AppGitSync;
   }
 
   protected defineAbilityFor(
@@ -30,15 +30,15 @@ export class FeatureAbilityFactory extends AbilityFactory<FEATURE_KEY, Subjects>
     // Grant feature-level access based on resource actions
     if (isAdmin || superAdmin || isBuilder) {
       // Admin or Super Admin gets full access to all features
-      can(FEATURE_KEY.GIT_CREATE_APP, App);
-      can(FEATURE_KEY.GIT_UPDATE_APP, App);
-      can(FEATURE_KEY.GIT_GET_APPS, App);
-      can(FEATURE_KEY.GIT_GET_APP, App);
-      can(FEATURE_KEY.GIT_GET_APP_CONFIG, App);
-      can(FEATURE_KEY.GIT_SYNC_APP, App);
-      can(FEATURE_KEY.GIT_APP_VERSION_RENAME, App);
-      can(FEATURE_KEY.GIT_APP_CONFIGS_UPDATE, App);
-      can(FEATURE_KEY.GIT_FETCH_APP_CONFIGS, App);
+      can(FEATURE_KEY.GIT_CREATE_APP, AppGitSync);
+      can(FEATURE_KEY.GIT_UPDATE_APP, AppGitSync);
+      can(FEATURE_KEY.GIT_GET_APPS, AppGitSync);
+      can(FEATURE_KEY.GIT_GET_APP, AppGitSync);
+      can(FEATURE_KEY.GIT_GET_APP_CONFIG, AppGitSync);
+      can(FEATURE_KEY.GIT_SYNC_APP, AppGitSync);
+      can(FEATURE_KEY.GIT_APP_VERSION_RENAME, AppGitSync);
+      can(FEATURE_KEY.GIT_APP_CONFIGS_UPDATE, AppGitSync);
+      can(FEATURE_KEY.GIT_FETCH_APP_CONFIGS, AppGitSync);
       return;
     }
 
@@ -47,13 +47,13 @@ export class FeatureAbilityFactory extends AbilityFactory<FEATURE_KEY, Subjects>
       isAllAppsViewable ||
       (userAppGitPermissions?.viewableAppsId?.length && appId && userAppGitPermissions?.viewableAppsId?.includes(appId))
     ) {
-      can(FEATURE_KEY.GIT_GET_APPS, App);
-      can(FEATURE_KEY.GIT_GET_APP, App);
+      can(FEATURE_KEY.GIT_GET_APPS, AppGitSync);
+      can(FEATURE_KEY.GIT_GET_APP, AppGitSync);
     }
 
     // CREATE-based features
     if (isAllAppsCreatable) {
-      can(FEATURE_KEY.GIT_CREATE_APP, App);
+      can(FEATURE_KEY.GIT_CREATE_APP, AppGitSync);
     }
 
     // UPDATE-based features
@@ -61,8 +61,8 @@ export class FeatureAbilityFactory extends AbilityFactory<FEATURE_KEY, Subjects>
       isAllAppsEditable ||
       (userAppGitPermissions?.editableAppsId?.length && appId && userAppGitPermissions.editableAppsId.includes(appId))
     ) {
-      can(FEATURE_KEY.GIT_UPDATE_APP, App);
-      can(FEATURE_KEY.GIT_SYNC_APP, App);
+      can(FEATURE_KEY.GIT_UPDATE_APP, AppGitSync);
+      can(FEATURE_KEY.GIT_SYNC_APP, AppGitSync);
     }
 
     // Additional checks based on specific actions
@@ -71,7 +71,7 @@ export class FeatureAbilityFactory extends AbilityFactory<FEATURE_KEY, Subjects>
       appId &&
       userAppGitPermissions.editableAppsId.includes(appId)
     ) {
-      can(FEATURE_KEY.GIT_GET_APP_CONFIG, App);
+      can(FEATURE_KEY.GIT_GET_APP_CONFIG, AppGitSync);
     }
   }
 }
