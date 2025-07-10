86cfb328ba765f88af0f004b1f5dd94a66418f54
satyam <satyamg065@gmail.com>
2025-07-10 18:45:48 +0530

    add aws bedrock plugin

---
+++ marketplace/plugins/aws-bedrock/lib/types.ts
@@ -0,0 +1,23 @@
+import {
+  ModelCustomization,
+  InferenceType,
+  ModelModality,
+} from "@aws-sdk/client-bedrock";
+
+export interface SourceOptions {
+  access_key: string;
+  secret_key: string;
+  region: string;
+  session_token?: string;
+}
+
+export interface QueryOptions {
+  operation?: "generate_content" | "list_foundation_models";
+  model_id?: string;
+  request_body?: any;
+  content_type?: string;
+  by_customization_type?: ModelCustomization;
+  by_inference_type?: InferenceType;
+  by_output_modality?: ModelModality;
+  by_provider?: string;
+}
