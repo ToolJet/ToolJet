23638faccb73cb14e3ea551fb9086930c31b5d67
manishkushare <kushare.manish9@gmail.com>
2025-07-15 16:28:46 +0530

    Bug fixed:toggle theme in audit log hard refreshes component

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -328,7 +328,7 @@ class AppComponent extends React.Component {
                   }
                 />
 
-                {getAuditLogsRoutes(this.props)}
+                {getAuditLogsRoutes(mergedProps)}
                 <Route
                   exact
                   path="/:workspaceId/profile-settings"
