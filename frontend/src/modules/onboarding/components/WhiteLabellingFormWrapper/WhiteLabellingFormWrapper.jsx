133289a74236fa1e806c2ab342b0bca1a838e21e
Rohan Lahori <64496391+rohanlahori@users.noreply.github.com>
2025-07-18 22:17:34 +0530

    Bugfixes/platform p0 branch6 (#13485)
    * fixes
    
    * passed edition in missing templates
    
    * fix for white labelling
    
    * white-labelling fixes
    
    * fixes
    
    * fix for undefined location
    
    * css fixes for workspace settings page
    
    * ui fixes
    
    * design fix for user action menu
    
    * dropdown style fixes

--- frontend/src/modules/onboarding/components/WhiteLabellingFormWrapper/WhiteLabellingFormWrapper.jsx
+++ frontend/src/modules/onboarding/components/WhiteLabellingFormWrapper/WhiteLabellingFormWrapper.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import Logo from '@/modules/common/resources/images/Logo';
 import './resources/styles/whitelabelling-form-wrapper.styles.scss';
 import { getSubpath } from '@/_helpers/routes';
-import { retrieveWhiteLabelLogo, checkWhiteLabelsDefaultState } from '@white-label/whiteLabelling';
-const WhiteLabellingFormWrapper = ({ children: components }) => {
+const WhiteLabellingFormWrapper = ({ children: components, whiteLabelFavIcon, isWhiteLabelApplied }) => {
   const IMAGE_WIDTH = 36;
   const IMAGE_HEIGHT = 36;
-  const whiteLabelLogo = retrieveWhiteLabelLogo();
-  const isDefaultWhiteLabel = checkWhiteLabelsDefaultState();
   const redirectToLoginPage = () => {
     window.location.href = getSubpath() ? `${getSubpath()}` : '/';
   };
-  if (whiteLabelLogo == null) {
+  if (whiteLabelFavIcon == null) {
     return <div></div>;
   }
   return (
     <div className="white-labelling-form-wrapper">
       <div className="tooljet-header cursor-pointer" onClick={redirectToLoginPage}>
-        {window.location.pathname != '/setup' && !isDefaultWhiteLabel ? (
-          <img width={IMAGE_WIDTH} height={IMAGE_HEIGHT} src={whiteLabelLogo} />
+        {window.location.pathname != '/setup' && isWhiteLabelApplied ? (
+          <img width={IMAGE_WIDTH} height={IMAGE_HEIGHT} src={whiteLabelFavIcon} />
         ) : (
           <Logo />
         )}
