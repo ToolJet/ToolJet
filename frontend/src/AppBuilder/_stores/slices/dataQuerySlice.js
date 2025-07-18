3f5cb6c22d580f699e8f7b2eda8f9a2c2478bedc
devanshu052000 <devanshu.rastogi05@gmail.com>
2025-07-18 10:26:44 +0530

    Fix: Query creation not working

--- frontend/src/AppBuilder/_stores/slices/dataQuerySlice.js
+++ frontend/src/AppBuilder/_stores/slices/dataQuerySlice.js
@@ -81,7 +81,12 @@ export const createDataQuerySlice = (set, get) => ({
       const dataQueries = get().dataQuery.queries.modules[moduleId];
       const currDataQueries = [...dataQueries];
       const runOnCreate = options.runOnCreate;
-      const { permissions, ...cleanSelectedQuery } = selectedQuery; //Remove permissions before using the query data
+
+      let cleanSelectedQuery = { ...selectedQuery };
+      if(selectedQuery?.permissions) {
+        delete cleanSelectedQuery?.permissions; //Remove the permissions array from the selectedQuery before using it if exists
+      }
+
       set((state) => {
         state.dataQuery.queries.modules[moduleId] = [
           {
