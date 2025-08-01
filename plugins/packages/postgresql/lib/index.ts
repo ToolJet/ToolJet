5a124b1431a9050638b3f7e504486a56312bd326
Ganesh Kumar <ganesh8056234@gmail.com>
2025-08-01 11:51:09 +0530

    statement timeout is been added to connection string method

--- plugins/packages/postgresql/lib/index.ts
+++ plugins/packages/postgresql/lib/index.ts
@@ -155,7 +155,6 @@ export default class PostgresqlQueryService implements QueryService {
     } else if (sourceOptions.connection_type === 'string' && sourceOptions.connection_string) {
       connectionConfig = {
         connectionString: sourceOptions.connection_string,
-        ssl: this.getSslConfig(sourceOptions),
         statement_timeout: this.STATEMENT_TIMEOUT,
       };
     }
