1d6f3ac538d3a74859f658a47081a98d7be7d8fc
devanshu052000 <devanshu.rastogi05@gmail.com>
2025-08-19 18:03:38 +0530

    Convert horizontal alignment styling property to fx field in text input component

--- server/src/modules/apps/services/widget-config/textinput.js
+++ server/src/modules/apps/services/widget-config/textinput.js
@@ -112,7 +112,6 @@ export const textinputConfig = {
         { displayName: 'alignrightinspector', value: 'right', iconName: 'alignrightinspector' },
       ],
       accordian: 'label',
-      isFxNotRequired: true,
     },
     width: {
       type: 'slider',
