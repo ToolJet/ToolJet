c36e51974e0bdd0c92658efb891e95cb1e459c23
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2025-08-25 18:44:02 +0530

    Fixes issues with avatar

--- frontend/src/_ui/AvatarGroup/index.jsx
+++ frontend/src/_ui/AvatarGroup/index.jsx
@@ -23,10 +23,10 @@ const AvatarGroup = ({
         {avatars.slice(maxDisplay, avatars.length).map((avatar, index) => (
           <div
             key={`hidden-${avatar.id || `avatar-${index}`}`}
-            className="tw-flex tw-items-center tw-p-1 hover:tw-bg-interactive-hover tw-cursor-pointer tw-rounded-md"
+            className="tw-flex tw-items-center tw-px-1 tw-h-10 hover:tw-bg-interactive-hover tw-cursor-pointer tw-rounded-md"
           >
             {renderAvatar(avatar, index + maxDisplay)}
-            <div className="tw-ml-2 tw-text-truncate tw-mt-1">
+            <div className="tw-ml-2 tw-text-truncate">
               <div className="tw-text-sm tw-font-medium">{avatar.title}</div>
             </div>
           </div>
@@ -47,10 +47,9 @@ const AvatarGroup = ({
       realtime={avatar.realtime || false}
       className={cx(
         avatar.className,
-        'tw-w-5 tw-h-5',
+        'tw-w-5 tw-h-5 tw-inline-flex',
         variant === 'multiplayer' &&
-          'tw-border-2 tw-border-white tw-transition-all tw-duration-200 tw-ease-in-out tw-rounded-full tw--mt-1.5 hover:tw-transform hover:tw--translate-y-0.5 hover:tw-shadow-elevation-200',
-        darkMode && variant === 'multiplayer' && 'tw-border-[#2d343b]'
+          'tw-border-2 tw-border-solid tw-border-background-surface-layer-01 tw-transition-all tw-duration-200 tw-ease-in-out !tw-rounded-full hover:tw-transform hover:tw--translate-y-0.5 hover:tw-shadow-elevation-200'
       )}
       onClick={onAvatarClick ? () => onAvatarClick(avatar, index) : undefined}
     />
@@ -63,34 +62,36 @@ const AvatarGroup = ({
   );
 
   const overflowClasses = cx(
-    'tw-border-2 tw-border-solid tw-bg-background-surface-layer-02 tw-border-border-default tw-rounded-full tw-px-2 tw-py-0.5 tw-text-[11px] tw-font-medium tw-text-text-medium tw-cursor-pointer -tw-mt-1.5 tw-z-10',
-    variant === 'multiplayer' &&
-      "tw-bg-[#c3c7df] tw-text-[#2d343b] tw-font-['IBM_Plex_Sans'] tw-text-sm tw-font-medium tw-leading-4 tw-border-2 tw-border-white hover:tw-bg-[#8890992e]",
-    darkMode &&
-      variant === 'multiplayer' &&
-      'tw-bg-[#8890992e] tw-text-white tw-border-[#2d343b] hover:tw-bg-[#c3c7df] hover:tw-text-[#2d343b]'
+    'tw-border-2 tw-border-solid tw-bg-background-surface-layer-02 tw-border-background-surface-layer-01 tw-rounded-full tw-px-2 tw-py-0.5 tw-text-[11px] tw-font-medium tw-text-text-medium tw-cursor-pointer -tw-mt-1.5 tw-z-10'
   );
 
   return (
-    <div className={containerClasses}>
-      {visibleAvatars.map((avatar, index) => renderAvatar(avatar, index))}
-      {hasOverflow && (
-        <OverlayTrigger
-          trigger="click"
-          placement="top"
-          rootClose
-          overlay={
-            <Popover id="avatar-group-popover" className={`shadow tw-custom-popover ${darkMode ? 'dark-theme' : ''}`}>
-              <Popover.Body className={`tw-p-1 tw-bg-background-surface-layer-01 tw-min-w-[200px]`}>
-                {popoverContent || defaultPopoverContent()}
-              </Popover.Body>
-            </Popover>
-          }
-        >
-          <div className={overflowClasses}>+{hiddenCount}</div>
-        </OverlayTrigger>
-      )}
-    </div>
+    <>
+      <style jsx>{`
+        .avatar-list-stacked > .avatar {
+          margin-top: -0.375rem;
+        }
+      `}</style>
+      <div className={containerClasses}>
+        {visibleAvatars.map((avatar, index) => renderAvatar(avatar, index))}
+        {hasOverflow && (
+          <OverlayTrigger
+            trigger="click"
+            placement="top"
+            rootClose
+            overlay={
+              <Popover id="avatar-group-popover" className={`shadow tw-custom-popover ${darkMode ? 'dark-theme' : ''}`}>
+                <Popover.Body className={`tw-p-1 tw-bg-background-surface-layer-01 tw-min-w-[200px]`}>
+                  {popoverContent || defaultPopoverContent()}
+                </Popover.Body>
+              </Popover>
+            }
+          >
+            <div className={overflowClasses}>+{hiddenCount}</div>
+          </OverlayTrigger>
+        )}
+      </div>
+    </>
   );
 };
 
