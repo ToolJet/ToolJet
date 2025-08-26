5d9adf76a46a4459181a26eb5849acc3b619fd17
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2025-08-26 13:08:27 +0530

    Squash: Apply appbuilder navigation changes from design/appbuilder-nav branch
    - Add ToggleLayoutButtons component with tooltips
    - Add AvatarGroup component with hover effects
    - Update header components (EditAppName, EditorHeader)
    - Update Avatar component
    - Update pages sidebar styles
    - Add header styles

--- frontend/src/AppBuilder/Header/EditAppName.jsx
+++ frontend/src/AppBuilder/Header/EditAppName.jsx
@@ -4,11 +4,12 @@ import React, { useRef, useEffect, useState } from 'react';
 import { ToolTip } from '@/_components';
 import { appsService } from '@/_services';
 import { handleHttpErrorMessages, validateName } from '@/_helpers/utils';
-import { InfoOrErrorBox } from './InfoOrErrorBox';
 import { toast } from 'react-hot-toast';
 import useStore from '@/AppBuilder/_stores/store';
 import { shallow } from 'zustand/shallow';
 import { useModuleContext } from '@/AppBuilder/_contexts/ModuleContext';
+import { AppModal } from '@/_components/AppModal';
+import { PenLine } from 'lucide-react';
 
 function EditAppName() {
   const { moduleId } = useModuleContext();
@@ -25,12 +26,10 @@ function EditAppName() {
   const darkMode = localStorage.getItem('darkMode') === 'true';
   const [name, setName] = useState(appName);
   const [isValid, setIsValid] = useState(true);
-  const [isEditing, setIsEditing] = useState(false);
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [warningText, setWarningText] = useState('');
-
-  const inputRef = useRef(null);
+  const [showRenameModal, setShowRenameModal] = useState(false);
 
   useEffect(() => {
     setName(appName);
@@ -46,18 +45,16 @@ function EditAppName() {
     setErrorMessage(message);
   };
 
-  const saveAppName = async (newName) => {
+  const handleSave = async (newName) => {
     const trimmedName = newName.trim();
     if (validateName(trimmedName, 'App', false, true)?.errorMsg) {
       setName(appName);
       clearError();
-      setIsEditing(false);
       return;
     }
 
     if (trimmedName === appName) {
       setIsValid(true);
-      setIsEditing(false);
       setName(appName);
       return;
     }
@@ -65,9 +62,7 @@ function EditAppName() {
     try {
       await appsService.saveApp(appId, { name: trimmedName });
       setAppName(trimmedName);
-
       setIsValid(true);
-      setIsEditing(false);
       toast.success('App name successfully updated!');
     } catch (error) {
       if (error.statusCode === 409) {
@@ -75,97 +70,76 @@ function EditAppName() {
       } else {
         clearError();
         setName(appName);
-        setIsEditing(false);
         handleHttpErrorMessages(error, 'app');
       }
     }
   };
 
-  const handleKeyDown = (e) => {
-    if ((e.key === 'z' || e.key === 'Z') && (e.ctrlKey || e.metaKey)) {
-      e.preventDefault();
+  const handleRenameApp = async (newAppName, appId) => {
+    try {
+      await appsService.saveApp(appId, { name: newAppName });
+      setAppName(newAppName);
+      toast.success('App name has been updated!');
+      return true;
+    } catch (errorResponse) {
+      if (errorResponse.statusCode === 409) {
+        return false;
+      }
+      if (errorResponse.statusCode !== 451) {
+        throw errorResponse;
+      }
     }
   };
 
-  React.useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown); // Clean up the event listener
-    };
-  }, []);
-
-  const handleBlur = () => {
-    saveAppName(name);
+  const handleCancel = () => {
+    setName(appName);
+    clearError();
   };
 
-  const handleFocus = () => {
-    setIsValid(true);
-    setIsEditing(true);
-  };
-
-  const handleInput = (e) => {
-    const newValue = e.target.value;
-    setName(newValue);
-    if (newValue.length >= 50) {
+  const validateAppName = (value) => {
+    if (value.length >= 50) {
       setWarningText('Maximum length has been reached');
-    } else {
-      setWarningText('');
-      clearError();
+      return 'Maximum length has been reached';
     }
+    setWarningText('');
+    clearError();
+    return '';
   };
-
-  const borderColor = isError
-    ? 'var(--light-tomato-10, #DB4324)' // Apply error border color
-    : darkMode
-    ? 'var(--dark-border-color, #2D3748)' // Change this to the appropriate dark border color
-    : 'var(--light-border-color, #FFF0EE)';
 
   // Define the message based on the pageType prop
   const messageType = 'App';
 
   return (
-    <div className={`app-name input-icon ${darkMode ? 'dark' : ''}`}>
-      <ToolTip message={name} placement="bottom" isVisible={!isEditing && appCreationMode !== 'GIT'}>
-        <input
-          ref={inputRef}
-          type="text"
-          onChange={() => {
-            //this was quick fix. replace this with actual tooltip props and state later
-            if (document.getElementsByClassName('tooltip').length) {
-              document.getElementsByClassName('tooltip')[0].style.display = 'none';
-            }
-          }}
-          onInput={handleInput}
-          onBlur={handleBlur}
-          onFocus={handleFocus}
-          onClick={() => {
-            inputRef.current.select();
-            setIsEditing(true);
-          }}
-          className={`form-control-plaintext form-control-plaintext-sm ${
-            (!isError && !isEditing) || isValid ? '' : 'is-invalid'
-          } ${isError ? 'error' : ''}`} // Add the 'error' class when there's an error
-          style={{ border: `1px solid ${borderColor}` }}
-          value={name}
-          maxLength={50}
-          data-cy="app-name-input"
+    <>
+      <div className="tw-h-full tw-flex tw-items-center">
+        <ToolTip message={name} placement="bottom" isVisible={appCreationMode !== 'GIT'}>
+          <button
+            className="edit-app-name-button tw-h-8 tw-rounded-lg tw-pr-1 tw-w-auto tw-font-medium tw-cursor-pointer tw-outline-none tw-bg-transparent tw-border tw-border-transparent hover:tw-border-border-strong tw-shadow-none tw-group tw-transition-all tw-duration-300 tw-flex tw-items-center tw-relative"
+            type="button"
+            onClick={() => setShowRenameModal(true)}
+          >
+            <span className=" tw-truncate tw-w-full tw-block group-hover:tw-w-[calc(100%-24px)]">{name}</span>
+            <span className="tw-absolute tw-right-0.5 tw-top-1 tw-text-icon-default tw-hidden group-hover:tw-block tw-w-7 tw-h-7 tw-items-center tw-justify-center">
+              <PenLine width="16" height="16" name="pencil" />
+            </span>
+          </button>
+        </ToolTip>
+      </div>
+
+      {showRenameModal && (
+        <AppModal
+          show={showRenameModal}
+          closeModal={() => setShowRenameModal(false)}
+          processApp={handleRenameApp}
+          selectedAppId={appId}
+          selectedAppName={appName}
+          title="Rename app"
+          actionButton="Rename app"
+          actionLoadingButton={'Renaming'}
+          appType="app"
         />
-      </ToolTip>
-      <InfoOrErrorBox
-        active={isError || isEditing}
-        message={
-          errorMessage ||
-          warningText ||
-          (name.length >= 50
-            ? 'Maximum length has been reached'
-            : `${messageType} name should be unique and max 50 characters`)
-        }
-        isWarning={warningText || name.length >= 50}
-        isError={isError}
-        darkMode={darkMode}
-        additionalClassName={isError ? 'error' : ''}
-      />
-    </div>
+      )}
+    </>
   );
 }
 
---
+++ frontend/src/AppBuilder/Header/ToggleLayoutButtons.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import cx from 'classnames';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Button } from '@/components/ui/Button/Button';
+import { Monitor, Smartphone } from 'lucide-react';
+
+export function ToggleLayoutButtons({
+  currentLayout,
+  toggleCurrentLayout,
+  clearSelectionBorder,
+  showFullWidth,
+  darkMode,
+}) {
+  return (
+    <div className={cx({ '!tw-w-100': showFullWidth })} data-cy="layout-toggle-container">
+      <div
+        className="d-flex align-items-center p-1 current-layout tw-gap-0.5"
+        role="tablist"
+        aria-orientation="horizontal"
+        data-cy="layout-toggle-buttons"
+      >
+        <OverlayTrigger placement="bottom" overlay={<Tooltip id="desktop-layout-tooltip">Desktop Layout</Tooltip>}>
+          <Button
+            variant="ghost"
+            className={cx({
+              'tw-pressed tw-bg-button-outline-pressed': currentLayout === 'desktop',
+            })}
+            iconOnly
+            aria-label="Switch to desktop layout"
+            aria-selected={currentLayout === 'desktop'}
+            tabIndex={0}
+            type="button"
+            onClick={() => {
+              toggleCurrentLayout('desktop');
+              clearSelectionBorder();
+            }}
+            data-cy="button-change-layout-to-desktop"
+          >
+            <Monitor width="16" height="16" className="tw-text-icon-strong" />
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger placement="bottom" overlay={<Tooltip id="mobile-layout-tooltip">Mobile Layout</Tooltip>}>
+          <Button
+            variant="ghost"
+            className={cx({
+              'tw-pressed tw-bg-button-outline-pressed': currentLayout === 'mobile',
+            })}
+            iconOnly
+            aria-label="Switch to mobile layout"
+            aria-selected={currentLayout === 'mobile'}
+            tabIndex={-1}
+            type="button"
+            onClick={() => {
+              toggleCurrentLayout('mobile');
+              clearSelectionBorder();
+            }}
+            data-cy="button-change-layout-to-mobile"
+          >
+            <Smartphone width="16" height="16" className="tw-text-icon-strong" />
+          </Button>
+        </OverlayTrigger>
+      </div>
+    </div>
+  );
+}
