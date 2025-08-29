45358bcdd543a13f156d009de4f1f23e1849b31c
Kartik Gupta <kartikgupta@Kartiks-MacBook-Pro.local>
2025-08-29 17:17:10 +0530

    sse changes

--- server/src/modules/ai/module.ts
+++ server/src/modules/ai/module.ts
@@ -1,32 +1,42 @@
-import { DynamicModule } from '@nestjs/common';
-import { getImportPath } from '@modules/app/constants';
-import { AiConversationRepository } from './repositories/ai-conversation.repository';
-import { AiConversationMessageRepository } from './repositories/ai-conversation-message.repository';
-import { AiResponseVoteRepository } from './repositories/ai-response-vote.repository';
-import { AppsRepository } from '@modules/apps/repository';
-import { FeatureAbilityFactory } from './ability';
-import { TooljetDbModule } from '@modules/tooljet-db/module';
-import { DataQueriesModule } from '@modules/data-queries/module';
-import { LicenseModule } from '@modules/licensing/module';
-import { AppPermissionsModule } from '@modules/app-permissions/module';
-import { ImportExportResourcesModule } from '@modules/import-export-resources/module';
-import { ArtifactRepository } from './repositories/artifact.repository';
-import { SubModule } from '@modules/app/sub-module';
-import { DataQueryRepository } from '@modules/data-queries/repository';
-import { DataSourcesModule } from '@modules/data-sources/module';
-import { AppEnvironmentsModule } from '@modules/app-environments/module';
-
+import { DynamicModule } from "@nestjs/common";
+import { getImportPath } from "@modules/app/constants";
+import { AiConversationRepository } from "./repositories/ai-conversation.repository";
+import { AiConversationMessageRepository } from "./repositories/ai-conversation-message.repository";
+import { AiResponseVoteRepository } from "./repositories/ai-response-vote.repository";
+import { AppsRepository } from "@modules/apps/repository";
+import { FeatureAbilityFactory } from "./ability";
+import { TooljetDbModule } from "@modules/tooljet-db/module";
+import { DataQueriesModule } from "@modules/data-queries/module";
+import { LicenseModule } from "@modules/licensing/module";
+import { AppPermissionsModule } from "@modules/app-permissions/module";
+import { ImportExportResourcesModule } from "@modules/import-export-resources/module";
+import { ArtifactRepository } from "./repositories/artifact.repository";
+import { SubModule } from "@modules/app/sub-module";
+import { DataQueryRepository } from "@modules/data-queries/repository";
+import { DataSourcesModule } from "@modules/data-sources/module";
+import { AppEnvironmentsModule } from "@modules/app-environments/module";
+import { VersionRepository } from "@modules/versions/repository";
 
 export class AiModule extends SubModule {
-  static async register(configs: { IS_GET_CONTEXT: boolean }): Promise<DynamicModule> {
+  static async register(configs: {
+    IS_GET_CONTEXT: boolean;
+  }): Promise<DynamicModule> {
     const importPath = await getImportPath(configs?.IS_GET_CONTEXT);
     const { AiController } = await import(`${importPath}/ai/controller`);
     const { AiService } = await import(`${importPath}/ai/service`);
     const { AiUtilService } = await import(`${importPath}/ai/util.service`);
-    const { AgentsService } = await import(`${importPath}/ai/services/agents.service`);
-    const { ComponentsService } = await import(`${importPath}/apps/services/component.service`);
-    const { GraphService } = await import(`${importPath}/ai/services/graph.service`);
-    const { EventsService } = await import(`${importPath}/apps/services/event.service`);
+    const { AgentsService } = await import(
+      `${importPath}/ai/services/agents.service`
+    );
+    const { ComponentsService } = await import(
+      `${importPath}/apps/services/component.service`
+    );
+    const { GraphService } = await import(
+      `${importPath}/ai/services/graph.service`
+    );
+    const { EventsService } = await import(
+      `${importPath}/apps/services/event.service`
+    );
 
     return {
       module: AiModule,
@@ -48,6 +58,7 @@ export class AiModule extends SubModule {
         ComponentsService,
         // ImportExportResourcesService,
         AiConversationRepository,
+        VersionRepository,
         AiConversationMessageRepository,
         AppsRepository,
         AiResponseVoteRepository,
