c6c5270f735bb7e70dc9a627979981270bba9176
rudrapratik30 <pratik104agrawal@gmail.com>
2025-09-09 13:06:30 +0530

    [docs]: Setup Layout

--- docs/src/components/DocsCard/DocsCardList.jsx
+++ docs/src/components/DocsCard/DocsCardList.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { DocsCard } from './';
-import styles from './DocsCard.css'
+import React from 'react' 
+import { DocsCard } from './'; 
+import styles from './DocsCard.css' 
 
-export const DocsCardList = ({ list }) => {
-    return (
-        <div className='card-container-setup'>
-            {list.map(item => <DocsCard key={item.docId} label={item.label} imgSrc={item.docId.split('/').pop()} link={item.href} />)}
-        </div>
-    )
+export const DocsCardList = ({ list }) => { 
+    return ( 
+        <div className='card-container-setup'> 
+            {list.map(item => {
+                const docId = item?.docId || "";
+                const parts = docId.split("/");
+                const imgSrc = item.customProps?.icon || parts[parts.length - 1];
+                
+                return (
+                    <DocsCard
+                        key={docId || item.label}
+                        label={item.label}
+                        imgSrc={imgSrc}
+                        link={item.href}
+                    />
+                );
+            })}
+        </div> 
+    ) 
 }
-
--- docs/versioned_docs/version-3.16.0-LTS/setup/index.md
+++ docs/versioned_docs/version-3.16.0-LTS/setup/index.md
@@ -3,14 +3,8 @@
 Check out the different methods you can use to deploy ToolJet on your machine.
 
 ```mdx-code-block
-import DocCardList from '@theme/DocCardList';
+import {DocsCardList} from '../../../src/components/DocsCard';
 import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
 
-<DocCardList items={useCurrentSidebarCategory().items} />
+<DocsCardList list={useCurrentSidebarCategory().items} />
 ```
-
-
-<!-- import {DocsCardList} from '../../../src/components/DocsCard';
-import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
-
-<DocsCardList list={useCurrentSidebarCategory().items} /> -->
