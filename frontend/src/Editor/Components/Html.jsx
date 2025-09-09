2c77ce6ea644382b98efc6b0d2ba477e1fef34f2
Vijaykant Yadav <vjy239@gmail.com>
2025-09-09 21:31:35 +0530

    Enhances HTML widget with loading, disabled, and visibility states

--- frontend/src/Editor/Components/Html.jsx
+++ frontend/src/Editor/Components/Html.jsx
@@ -1,37 +1,142 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DOMPurify from 'dompurify';
+import Spinner from '@/_ui/Spinner';
+import { useBatchedUpdateEffectArray } from '@/_hooks/useBatchedUpdateEffectArray';
 
-export const Html = function ({ height, properties, styles, darkMode, dataCy }) {
-  const { rawHtml: stringifyHTML } = properties;
+export const Html = function ({
+  height,
+  properties,
+  styles,
+  darkMode,
+  dataCy,
+  setExposedVariable,
+  setExposedVariables,
+}) {
+  const { rawHtml: stringifyHTML, loadingState, disabledState, visibility } = properties || {};
   const baseStyle = {
     backgroundColor: darkMode ? '#47505D' : '#ffffff',
     color: darkMode ? 'white' : 'black',
   };
-  const { visibility, boxShadow } = styles;
+  const { boxShadow } = styles || {};
+
+  const isInitialRender = useRef(true);
 
   const [rawHtml, setRawHtml] = useState('');
-  useEffect(() => {
-    setRawHtml(stringifyHTML);
-  }, [stringifyHTML]);
-  DOMPurify.addHook('afterSanitizeAttributes', function (node) {
-    // set all elements owning target to target=_blank
-    if ('target' in node) {
-      node.setAttribute('target', '_blank');
-      node.setAttribute('rel', 'noopener');
-    }
+  const [exposedVariablesTemporaryState, setExposedVariablesTemporaryState] = useState({
+    isVisible: visibility,
+    isLoading: loadingState,
+    isDisabled: disabledState,
+    rawHTML: stringifyHTML || '',
   });
+
+  const updateExposedVariablesState = (key, value) => {
+    setExposedVariablesTemporaryState((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  useEffect(() => {
+    DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+      // set all elements owning target to target=_blank
+      if ('target' in node) {
+        node.setAttribute('target', '_blank');
+        node.setAttribute('rel', 'noopener');
+      }
+    });
+  }, []);
+
+  useBatchedUpdateEffectArray([
+    {
+      dep: visibility,
+      sideEffect: () => {
+        setExposedVariable('isVisible', visibility);
+        updateExposedVariablesState('isVisible', visibility);
+      },
+    },
+    {
+      dep: loadingState,
+      sideEffect: () => {
+        setExposedVariable('isLoading', loadingState);
+        updateExposedVariablesState('isLoading', loadingState);
+      },
+    },
+    {
+      dep: disabledState,
+      sideEffect: () => {
+        setExposedVariable('isDisabled', disabledState);
+        updateExposedVariablesState('isDisabled', disabledState);
+      },
+    },
+    {
+      dep: stringifyHTML || '',
+      sideEffect: () => {
+        const rawHtmlValue = stringifyHTML || '';
+        setRawHtml(rawHtmlValue);
+        setExposedVariable('rawHTML', rawHtmlValue);
+        updateExposedVariablesState('rawHTML', rawHtmlValue);
+      },
+    },
+  ]);
+
+  useEffect(() => {
+    const exposedVariables = {
+      rawHTML: stringifyHTML || '',
+      setRawHTML: async function (value) {
+        const rawHtmlValue = value || '';
+        setRawHtml(rawHtmlValue);
+        setExposedVariable('rawHTML', rawHtmlValue);
+        updateExposedVariablesState('rawHTML', rawHtmlValue);
+      },
+      setVisibility: async function (value) {
+        setExposedVariable('isVisible', !!value);
+        updateExposedVariablesState('isVisible', !!value);
+      },
+      setLoading: async function (value) {
+        setExposedVariable('isLoading', !!value);
+        updateExposedVariablesState('isLoading', !!value);
+      },
+      setDisable: async function (value) {
+        setExposedVariable('isDisabled', !!value);
+        updateExposedVariablesState('isDisabled', !!value);
+      },
+      isVisible: visibility,
+      isLoading: loadingState,
+      isDisabled: disabledState,
+    };
+    setExposedVariables(exposedVariables);
+    isInitialRender.current = false;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div
-      style={{ display: visibility ? '' : 'none', width: '100%', height, overflowY: 'auto', boxShadow }}
+      className={`jet-container ${exposedVariablesTemporaryState.isLoading && 'jet-container-loading'}`}
+      data-disabled={exposedVariablesTemporaryState.isDisabled}
+      style={{
+        background: exposedVariablesTemporaryState.isLoading && 'var(--cc-surface1-surface)',
+        display: exposedVariablesTemporaryState.isVisible ? 'flex' : 'none',
+        border: exposedVariablesTemporaryState.isLoading && '1px solid var(--cc-default-border)',
+        borderRadius: exposedVariablesTemporaryState.isLoading && '6px',
+        width: '100%',
+        height,
+        overflowY: 'auto',
+        boxShadow,
+        position: 'relative',
+        opacity: exposedVariablesTemporaryState.isDisabled ? 0.5 : 1,
+        pointerEvents: exposedVariablesTemporaryState.isDisabled ? 'none' : 'auto',
+      }}
       data-cy={dataCy}
+      aria-busy={exposedVariablesTemporaryState.isLoading}
     >
-      {
+      {exposedVariablesTemporaryState.isLoading ? (
+        <Spinner />
+      ) : (
         <div
           style={baseStyle}
           dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHtml, { FORCE_BODY: true }) }}
         />
-      }
+      )}
     </div>
   );
 };
--- server/src/modules/apps/services/widget-config/html.js
+++ server/src/modules/apps/services/widget-config/html.js
@@ -16,23 +16,57 @@ export const htmlConfig = {
         defaultValue: `<body><div><h1>Hello World</h1></div></body>`,
       },
     },
+    loadingState: {
+      type: 'toggle',
+      displayName: 'Loading state',
+      validation: { schema: { type: 'boolean' }, defaultValue: true },
+      section: 'additionalActions',
+    },
+    visibility: {
+      type: 'toggle',
+      displayName: 'Visibility',
+      validation: { schema: { type: 'boolean' }, defaultValue: true },
+
+      section: 'additionalActions',
+    },
+    disabledState: {
+      type: 'toggle',
+      displayName: 'Disable',
+      validation: { schema: { type: 'boolean' }, defaultValue: true },
+      section: 'additionalActions',
+    },
   },
   others: {
     showOnDesktop: { type: 'toggle', displayName: 'Show on desktop' },
     showOnMobile: { type: 'toggle', displayName: 'Show on mobile' },
   },
   events: {},
-  styles: {
-    visibility: {
-      type: 'toggle',
-      displayName: 'Visibility',
-      validation: {
-        schema: { type: 'boolean' },
-        defaultValue: true,
-      },
-    },
-  },
+  styles: {},
   exposedVariables: {},
+  actions: [
+    {
+      handle: 'setRawHTML',
+      displayName: 'Set raw html',
+      params: [
+        { handle: 'setRawHTML', displayName: 'Value', defaultValue: '<body><div><h1>Hello World</h1></div></body>' },
+      ],
+    },
+    {
+      handle: 'setVisibility',
+      displayName: 'Set visibility',
+      params: [{ handle: 'setVisibility', displayName: 'Value', defaultValue: '{{false}}', type: 'toggle' }],
+    },
+    {
+      handle: 'setDisable',
+      displayName: 'Set disable',
+      params: [{ handle: 'setDisable', displayName: 'Value', defaultValue: '{{false}}', type: 'toggle' }],
+    },
+    {
+      handle: 'setLoading',
+      displayName: 'Set loading',
+      params: [{ handle: 'setLoading', displayName: 'Value', defaultValue: '{{false}}', type: 'toggle' }],
+    },
+  ],
   definition: {
     others: {
       showOnDesktop: { value: '{{true}}' },
@@ -43,6 +77,9 @@ export const htmlConfig = {
         value: `<body><main><section class="hero" style="height:306px;display: flex;
             justify-content: center;padding:0 1px;align-items: center;text-align:center">You can build your custom HTML-CSS template here</section></main></body>`,
       },
+      visibility: { value: '{{true}}' },
+      disabledState: { value: '{{false}}' },
+      loadingState: { value: '{{false}}' },
     },
     events: [],
     styles: {
