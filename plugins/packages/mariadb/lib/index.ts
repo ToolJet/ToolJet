2986dfa17702f8c6da08b79506033043b647ff73
Ganesh Kumar <ganesh8056234@gmail.com>
2025-10-08 00:00:23 +0530

    Mariadb graceful connection handling

--- plugins/packages/mariadb/lib/index.ts
+++ plugins/packages/mariadb/lib/index.ts
@@ -1,26 +1,53 @@
-import { QueryError, QueryResult, QueryService, ConnectionTestResult } from '@tooljet-plugins/common';
+import {
+  QueryError,
+  QueryResult,
+  QueryService,
+  ConnectionTestResult,
+  getCachedConnection,
+  cacheConnectionWithConfiguration,
+  generateSourceOptionsHash,
+} from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions } from './types';
 const mariadb = require('mariadb');
+
 export default class Mariadb implements QueryService {
-  private defaultConnectionLimit = '5';
-  async run(sourceOptions: SourceOptions, queryOptions: QueryOptions): Promise<QueryResult> {
-    let result = {};
-    const mariadbClient = await this.getConnection(sourceOptions);
+  private defaultConnectionLimit = '10';
+
+  async run(
+    sourceOptions: SourceOptions,
+    queryOptions: QueryOptions,
+    dataSourceId: string,
+    dataSourceUpdatedAt: string
+  ): Promise<QueryResult> {
+    const checkCache = true;
+    let conn;
+    const mariadbConnectionPool = await this.getConnection(
+      sourceOptions,
+      {},
+      checkCache,
+      dataSourceId,
+      dataSourceUpdatedAt
+    );
     try {
-      const rows = await mariadbClient.query(queryOptions.query);
-      result = this.toJson(rows);
+      conn = await mariadbConnectionPool.getConnection();
+      const rows = await conn.query(queryOptions.query);
+      const result = this.toJson(rows);
+      return {
+        status: 'ok',
+        data: result,
+      };
     } catch (error) {
       throw new QueryError('Query could not be completed', error.message, {});
+    } finally {
+      if (conn) conn.release(); // Release the connection back to the pool
+      if (!checkCache) await mariadbConnectionPool.end();
     }
-    return {
-      status: 'ok',
-      data: result,
-    };
   }
 
   async testConnection(sourceOptions: SourceOptions): Promise<ConnectionTestResult> {
+    let conn;
     try {
-      const conn = await this.getConnection(sourceOptions);
+      conn = await this.buildTestConnection(sourceOptions);
       const rows = await conn.query('SELECT 1 as val');
 
       if (!rows) {
@@ -31,21 +58,28 @@ export default class Mariadb implements QueryService {
       };
     } catch (error) {
       throw new QueryError(`Connection test failed: ${error.sqlMessage}`, error.message, {});
+    } finally {
+      if (conn) conn.end();
     }
   }
 
-  async getConnection(sourceOptions: SourceOptions): Promise<any> {
+  private buildConnectionPool(sourceOptions: SourceOptions): Promise<any> {
     const connectionLimit =
       sourceOptions.connectionLimit && sourceOptions.connectionLimit !== ''
         ? sourceOptions.connectionLimit
         : this.defaultConnectionLimit;
+
+    // Timeout to get a new connection from pool in ms. - acquireTimeout : Default is 10000ms
     const poolConfig = {
       host: sourceOptions.host,
       user: sourceOptions.user,
       password: sourceOptions.password,
-      connectionLimit: connectionLimit,
       port: sourceOptions.port,
       database: sourceOptions.database,
+      multipleStatements: true,
+      connectionLimit: connectionLimit, // Maximum number of connections in the pool - Updated to 10 from 5
+      connectTimeout: 60000, // 60 seconds - Sets the connection timeout in milliseconds.
+      minConnections: 0, // Minimum idle connections in the pool
     };
 
     const sslObject = { rejectUnauthorized: (sourceOptions.ssl_certificate ?? 'none') != 'none' };
@@ -61,16 +95,67 @@ export default class Mariadb implements QueryService {
     if (sourceOptions.ssl_enabled) poolConfig['ssl'] = sslObject;
 
     try {
-      const mariadbPool = await mariadb.createPool(poolConfig);
+      const mariadbPool = mariadb.createPool(poolConfig);
       mariadbPool.on('error', (error) => {
         console.error(error);
       });
 
-      return mariadbPool.getConnection();
+      return mariadbPool;
+    } catch (error) {
+      console.error('Error while creating database connection pool:', error.message);
+      throw new QueryError('Database connection failed', error.message, {});
+    }
+  }
+
+  private async buildTestConnection(sourceOptions: SourceOptions): Promise<any> {
+    const connectionConfig = {
+      host: sourceOptions.host,
+      user: sourceOptions.user,
+      password: sourceOptions.password,
+      port: sourceOptions.port,
+      database: sourceOptions.database,
+      connectTimeout: 60000, // 60 seconds - Sets the connection timeout in milliseconds.
+    };
+
+    const sslObject = { rejectUnauthorized: (sourceOptions.ssl_certificate ?? 'none') != 'none' };
+    if (sourceOptions.ssl_certificate === 'ca_certificate') {
+      sslObject['ca'] = sourceOptions.ca;
+    }
+    if (sourceOptions.ssl_certificate === 'self_signed') {
+      sslObject['ca'] = sourceOptions.ca;
+      sslObject['cert'] = sourceOptions.cert;
+      sslObject['key'] = sourceOptions.key;
+    }
+
+    if (sourceOptions.ssl_enabled) connectionConfig['ssl'] = sslObject;
+
+    try {
+      const conn = await mariadb.createConnection(connectionConfig);
+      return conn;
     } catch (error) {
       console.error('Error while establishing database connection:', error.message);
       throw new QueryError('Database connection failed', error.message, {});
     }
+  }
+
+  async getConnection(
+    sourceOptions: SourceOptions,
+    options: any,
+    checkCache: boolean,
+    dataSourceId: string,
+    dataSourceUpdatedAt?: string
+  ): Promise<any> {
+    if (checkCache) {
+      const optionsHash = generateSourceOptionsHash(sourceOptions);
+      const enhancedCacheKey = `${dataSourceId}_${optionsHash}`;
+      const cachedConnectionPool = await getCachedConnection(enhancedCacheKey, dataSourceUpdatedAt);
+      if (cachedConnectionPool) return cachedConnectionPool;
+
+      const connectionPool = await this.buildConnectionPool(sourceOptions);
+      cacheConnectionWithConfiguration(dataSourceId, enhancedCacheKey, connectionPool);
+      return connectionPool;
+    }
+    return await this.buildConnectionPool(sourceOptions);
   }
 
   private toJson(data) {
