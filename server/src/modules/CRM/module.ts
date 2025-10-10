684f17631ecffa02d86b0ac4cde933e075a60832
rohanlahori <rohanlahori99@gmail.com>
2025-10-10 19:33:47 +0530

    fixed module files

--- server/src/modules/CRM/module.ts
+++ server/src/modules/CRM/module.ts
@@ -1,5 +1,4 @@
 import { Module, DynamicModule } from '@nestjs/common';
-import { EmailModule } from '@modules/email/module';
 import { SubModule } from '@modules/app/sub-module';
 
 @Module({})
@@ -12,10 +11,9 @@ export class CrmModule extends SubModule {
     ]);
     return {
       module: CrmModule,
-      imports: [await EmailModule.register(configs)],
       controllers: isMainImport ? [CrmController] : [],
       providers: isMainImport ? [CRMService, CrmListenerService] : [], // Register CRM Listener service
-      exports: [CrmListenerService], // Export the CRM Listener if needed elsewhere
+      exports: [], // Export the CRM Listener if needed elsewhere
     };
   }
 }
