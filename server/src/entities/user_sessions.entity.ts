ed409c6243fd98f56242f357a9728ff0e48f6e45
parthy007 <parthadhikari1812@gmail.com>
2025-10-17 11:34:40 +0530

    feat: update UserSessions table

--- server/src/entities/user_sessions.entity.ts
+++ server/src/entities/user_sessions.entity.ts
@@ -30,6 +30,18 @@ export class UserSessions extends BaseEntity {
   @Column({ name: 'session_type', type: 'enum', enum: SessionType, default: 'user' })
   sessionType: SessionType.USER | SessionType.PAT;
 
+  @Column({ name: 'access_token', type: 'text', nullable: true })
+  accessToken: string | null;
+
+  @Column({ name: 'refresh_token', type: 'text', nullable: true })
+  refreshToken: string | null;
+
+  @Column({ name: 'token_expires_at', type: 'timestamp', nullable: true })
+  tokenExpiresAt: Date | null;
+
+  @Column({ name: 'sso_config_id', type: 'varchar', nullable: true })
+  ssoConfigId: string | null;
+
   @ManyToOne(() => UserPersonalAccessToken, { nullable: true, onDelete: 'SET NULL' })
   @JoinColumn({ name: 'pat_id' })
   pat: UserPersonalAccessToken | null;
