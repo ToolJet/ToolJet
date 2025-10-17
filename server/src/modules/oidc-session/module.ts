70fcaa975f1009c15f38a1f4b9b20335aac57caa
rohanlahori <rohanlahori99@gmail.com>
2025-10-17 15:07:52 +0530

    fixed dependency issues

--- server/src/modules/oidc-session/module.ts
+++ server/src/modules/oidc-session/module.ts
@@ -1,26 +1,18 @@
 import { DynamicModule } from '@nestjs/common';
 import { SubModule } from '@modules/app/sub-module';
 import { LoginConfigsModule } from '@modules/login-configs/module';
+import { InstanceSettingsModule } from '@modules/instance-settings/module';
+import { SSOConfigsRepository } from '@modules/login-configs/repository';
 
 export class OidcSessionModule extends SubModule {
   static async register(config: { IS_GET_CONTEXT: boolean }, isMainImport?: boolean): Promise<DynamicModule> {
     const { OidcSessionUtilService } = await this.getProviders(config, 'oidc-session', ['oidc-session-util.service']);
 
-    const providerImports = [
-      //   RolesRepository,
-      //   UserRepository,
-      //   AppsRepository,
-      //   OrganizationRepository,
-      //   OrganizationUsersRepository,
-      //   GroupPermissionsRepository,
-      //   FeatureAbilityFactory,
-      //   UserSessionRepository,
-      OidcSessionUtilService,
-    ];
+    const providerImports = [OidcSessionUtilService, SSOConfigsRepository];
 
     return {
       module: OidcSessionModule,
-      imports: [await LoginConfigsModule.register(config)],
+      imports: [await LoginConfigsModule.register(config), await InstanceSettingsModule.register(config)],
       providers: providerImports,
       exports: [OidcSessionUtilService],
     };
--- server/src/modules/oidc-session/oidc-session-util.service.ts
+++ server/src/modules/oidc-session/oidc-session-util.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TransactionLogger } from '@modules/logging/service';
 import { LoginConfigsUtilService } from '@modules/login-configs/util.service';
-import { AuthUtilService } from '@modules/auth/util.service';
 
 @Injectable()
 export class OidcSessionUtilService {
   constructor(
     protected readonly configService: ConfigService,
     protected readonly loginConfigsUtilService: LoginConfigsUtilService,
-    // protected readonly authUtilService: AuthUtilService,
     protected readonly transactionLogger: TransactionLogger
   ) {}
 
