46944be754525c0ce233968d965f081953a32f25
rohanlahori <rohanlahori99@gmail.com>
2025-10-31 17:04:34 +0530

    git sync minor changes

--- server/src/modules/apps/services/app-import-export.service.ts
+++ server/src/modules/apps/services/app-import-export.service.ts
@@ -329,6 +329,7 @@ export class AppImportExportService {
 
         return {
           ...component,
+          // updatedAt: query?.updatedAt,
           permissions: groupPermission
             ? {
                 permissionGroup: groupPermission.users
