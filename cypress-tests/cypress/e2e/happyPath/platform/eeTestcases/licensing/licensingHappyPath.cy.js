a28bf92a87a1a58a87fab76e30abc4739fc8dfc2
Mekhla Asopa <dadhichmekhla@gmail.com>
2025-11-04 19:01:36 +0530

    added test case for paid

--- cypress-tests/cypress/e2e/happyPath/platform/eeTestcases/licensing/licensingHappyPath.cy.js
+++ cypress-tests/cypress/e2e/happyPath/platform/eeTestcases/licensing/licensingHappyPath.cy.js
@@ -1,4 +1,6 @@
 import { commonSelectors } from "Selectors/common";
+import { groupsSelector } from "Selectors/manageGroups";
+import { workspaceConstantsSelectors } from "Selectors/workspaceConstants";
 import { commonText, settingsText, workspaceSettingsText } from "Texts/common";
 import { fake } from "Fixtures/fake";
 import { licenseText } from "Texts/license";
@@ -6,12 +8,14 @@ import { licenseSelectors } from "Selectors/license";
 import { usersSelector } from "Selectors/manageUsers";
 import {
   switchTabs,
-  verifylicenseTab,
-  verifySubTabs,
+  verifyLicenseTab,
+  verifySubTabsAndStoreCurrentLimits,
   verifyAccessTab,
   verifyDomainTab,
   verifyResourceLimit,
   verifyTooltip,
+  verifyTotalLimitsWithPlan,
+  getLicenseExpiryDate,
 } from "Support/utils/license";
 import * as common from "Support/utils/common";
 import { dashboardSelector } from "../../../../../constants/selectors/dashboard";
@@ -20,7 +24,6 @@ import {
   instanceSettingsSelector,
   whiteLabellingSelectors,
 } from "Selectors/eeCommon";
-import { workflowSelector } from "Selectors/workflows";
 import { commonEeSelectors } from "Selectors/eeCommon";
 
 describe("License Page", () => {
@@ -31,7 +34,7 @@ describe("License Page", () => {
     cy.visit("/");
   });
 
-  it("Should verify license page elements with the basic plan ", () => {
+  it("Should verify license page elements with the basic plan", () => {
     common.navigateToSettingPage();
     cy.get(licenseSelectors.listOfItems(licenseText.license)).click();
 
@@ -45,47 +48,23 @@ describe("License Page", () => {
       .and("contain.text", licenseText.comparePlansText);
 
     switchTabs(licenseText.licenseKeyLabel);
-    verifylicenseTab();
+    verifyLicenseTab();
 
     switchTabs(licenseText.limitsTabTitle);
-    verifySubTabs(
-      licenseText.limitsTab.aiCreditsSubTab,
-      licenseText.aiCreditsSubTab,
-      {
-        "Monthly recurring": "0/0",
-        "Add on credits": "0/0",
-      }
-    );
-    verifySubTabs(licenseText.limitsTab.appsSubTab, licenseText.appsSubTab, {
-      "Number of Apps": "1/2",
+    const limitSubTabs = [
+      "aiCredits",
+      "apps",
+      "workspaces",
+      "users",
+      "workflows",
+      "tables",
+    ];
+    limitSubTabs.forEach((subTab) => {
+      verifySubTabsAndStoreCurrentLimits(
+        licenseText.limitsTab[`${subTab}SubTab`],
+        licenseText[`${subTab}SubTab`]
+      );
     });
-    verifySubTabs(
-      licenseText.limitsTab.workspacesSubTab,
-      licenseText.workspacesSubTab,
-      {
-        "Number of Workspaces": "1/1",
-      }
-    );
-    verifySubTabs(licenseText.limitsTab.usersSubTab, licenseText.usersSubTab, {
-      "Number of Total Users": "1/52",
-      "Number of Builders": "1/2",
-      "Number of End Users": "0/50",
-      "Number of Super Admins": "1/1",
-    });
-    verifySubTabs(
-      licenseText.limitsTab.workflowsSubTab,
-      licenseText.workflowsSubTab,
-      {
-        "Number of Workflows": "0/2",
-      }
-    );
-    verifySubTabs(
-      licenseText.limitsTab.tablesSubTab,
-      licenseText.tablesSubTab,
-      {
-        "Number of Tables": "Unlimited",
-      }
-    );
 
     switchTabs(licenseText.accessTabTitle);
     verifyAccessTab();
@@ -94,35 +73,36 @@ describe("License Page", () => {
     verifyDomainTab();
   });
 
-  it("Should verify banners and tooltips with the basic plan ", () => {
+  it("Should verify banners and tooltips with the basic plan", () => {
+    const planName = "basic";
     cy.get(commonSelectors.workspaceName).click();
 
     cy.get('[data-cy="workspace-count"]').should("be.visible");
-    verifyResourceLimit("workspaces", "basic");
+    verifyResourceLimit("workspaces", planName);
 
     cy.get(dashboardSelector.homePageContent).click();
-    verifyResourceLimit("apps", "basic");
+    verifyResourceLimit("apps", planName);
 
     // cy.get(workflowSelector.globalWorkFlowsIcon).click();
-    // verifyResourceLimit("workflow", "basic");
+    // verifyResourceLimit("workflow", planName);
 
     common.navigateToManageUsers();
     cy.get(usersSelector.buttonAddUsers).click();
-    verifyResourceLimit("builders", "basic", "usage");
+    verifyResourceLimit("builders", planName, "usage");
 
     cy.reload();
 
     common.navigateToManageGroups();
-    cy.get('[data-cy="create-new-group-button"]').should("be.disabled");
+    cy.get(groupsSelector.createNewGroupButton).should("be.disabled");
     verifyTooltip(
-      '[data-cy="create-new-group-button"]',
-      "Custom groups are available only in paid plans",
+      groupsSelector.createNewGroupButton,
+      "Custom groups are not available in your plan",
       true
     );
 
     verifyResourceLimit(
       "custom-groups",
-      "basic",
+      planName,
       "custom-groups",
       "Custom groups & permissions are paid features"
     );
@@ -145,7 +125,7 @@ describe("License Page", () => {
     ).click();
     verifyResourceLimit(
       "custom-themes",
-      "basic",
+      planName,
       "custom-themes",
       "Custom themes is our paid feature. Upgrade to a paid plan to add and customize themes."
     );
@@ -154,10 +134,13 @@ describe("License Page", () => {
     cy.get(licenseSelectors.listOfItems(settingsText.allUsersListItem)).click({
       force: true,
     });
+
+    verifyTotalLimitsWithPlan(["builders", "end-users", "user"], planName);
+
     navigateToEditUser(commonText.email);
     verifyResourceLimit(
       "edit-user",
-      "basic",
+      planName,
       "edit-user",
       "Edit user details with our paid plans. For more,"
     );
@@ -193,6 +176,160 @@ describe("License Page", () => {
       "Audit logs are available only in paid plans",
       true
     );
+
+    // cy.apiCreateApp(`${fake.companyName}-license-App`);
+    // cy.openApp();
+
+    // cy.get('[data-cy="list-current-env-name"]').click();
+    // cy.get('[data-cy="env-name-list"]')
+    //   .eq(1)
+    //   .within(() => {
+    //     verifyTooltip(
+    //       '[data-cy="env-name-dropdown"]',
+    //       "Multi-environments are available only in paid plans"
+    //     );
+    //   });
+
+    // cy.get('[data-cy="env-name-list"]')
+    //   .eq(2)
+    //   .within(() => {
+    //     verifyTooltip(
+    //       '[data-cy="env-name-dropdown"]',
+    //       "Multi-environments are available only in paid plans"
+    //     );
+    //   });
+  });
+
+  it("Should verify license page elements with the paid plan", () => {
+    cy.writeFile("cypress/fixtures/license/currentLimits.json", {});
+
+    cy.apiUpdateLicense("validWithLimits");
+
+    getLicenseExpiryDate().then((expectedText) => {
+      common.navigateToSettingPage();
+      cy.get(licenseSelectors.listOfItems(licenseText.license)).click();
+      cy.get(licenseSelectors.expiryStatus).should("have.text", expectedText);
+    });
+
+    cy.contains(licenseText.licenseOverviewTitle).should("be.visible");
+    cy.get(commonSelectors.breadcrumbPageTitle).should(
+      "have.text",
+      licenseText.license
+    );
+    cy.get(licenseSelectors.comparePlansText)
+      .should("be.visible")
+      .and("contain.text", licenseText.comparePlansText);
+
+    switchTabs(licenseText.licenseKeyLabel);
+    verifyLicenseTab();
+
+    switchTabs(licenseText.limitsTabTitle);
+    const limitSubTabs = [
+      "aiCredits",
+      "apps",
+      "workspaces",
+      "users",
+      "workflows",
+      "tables",
+    ];
+    limitSubTabs.forEach((subTab) => {
+      verifySubTabsAndStoreCurrentLimits(
+        licenseText.limitsTab[`${subTab}SubTab`],
+        licenseText[`${subTab}SubTab`]
+      );
+    });
+
+    switchTabs(licenseText.accessTabTitle);
+    verifyAccessTab(true);
+
+    switchTabs(licenseText.domainTabTitle);
+    verifyDomainTab();
+  });
+
+  it("Should verify banners and tooltips with the paid plan", () => {
+    const planName = "Enterprise";
+    cy.get(commonSelectors.workspaceName).click();
+
+    cy.get(licenseSelectors.workspaceCount).should("be.visible");
+    cy.get(commonSelectors.addWorkspaceButton).should("be.visible");
+    verifyResourceLimit("workspaces", planName);
+
+    cy.get(dashboardSelector.homePageContent).click();
+    verifyResourceLimit("apps", planName);
+
+    // cy.get(workflowSelector.globalWorkFlowsIcon).click();
+    // verifyResourceLimit("workflow", planName);
+
+    common.navigateToManageUsers();
+    cy.get(usersSelector.buttonAddUsers).click();
+    verifyResourceLimit("builders", planName, "usage");
+
+    cy.reload();
+
+    common.navigateToManageGroups();
+    cy.get(groupsSelector.createNewGroupButton).should("be.enabled");
+
+    cy.get(
+      licenseSelectors.listOfItems(workspaceSettingsText.customStylesListItem)
+    ).click();
+    cy.get(licenseSelectors.paidFeatureButton).should("not.exist");
+    cy.get(commonSelectors.saveButton).should("be.enabled");
+
+    cy.get(
+      licenseSelectors.listOfItems(
+        workspaceSettingsText.configureGitSyncListItem
+      )
+    ).click();
+    cy.get(licenseSelectors.paidFeatureButton).should("not.exist");
+
+    cy.get(
+      licenseSelectors.listOfItems(workspaceSettingsText.themesListItem)
+    ).click();
+    cy.get('[data-cy="create-new-theme-button"]').should("be.enabled");
+
+    common.navigateToSettingPage();
+    cy.get(licenseSelectors.listOfItems(settingsText.allUsersListItem)).click({
+      force: true,
+    });
+
+    verifyTotalLimitsWithPlan(["builders", "end-users", "user"], planName);
+
+    cy.get(
+      licenseSelectors.listOfItems(settingsText.manageInstanceSettingsListItem)
+    ).click();
+    cy.get(licenseSelectors.paidFeatureButton).should("not.exist");
+    cy.get(instanceSettingsSelector.allowWorkspaceToggle)
+      .eq(0)
+      .should("not.be.checked");
+
+    cy.get(
+      licenseSelectors.listOfItems(settingsText.whiteLabellingListItem)
+    ).click();
+    cy.get(licenseSelectors.paidFeatureButton).should("not.exist");
+    cy.get(whiteLabellingSelectors.appLogoInput).should("be.enabled");
+
+    cy.get(commonSelectors.workspaceConstantsIcon).click();
+
+    cy.get(licenseSelectors.listOfItems("staging"))
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    cy.get(workspaceConstantsSelectors.addNewConstantButton).should(
+      "be.enabled"
+    );
+
+    cy.get(licenseSelectors.listOfItems("production"))
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    cy.get(workspaceConstantsSelectors.addNewConstantButton).should(
+      "be.enabled"
+    );
+
+    cy.get(commonSelectors.dashboardIcon).click();
+    cy.get(commonSelectors.settingsIcon).click();
+    cy.get(commonEeSelectors.auditLogIcon).should("be.visible").click();
+    cy.get(commonSelectors.pageSectionHeader).should("have.text", "Audit logs");
 
     // cy.apiCreateApp(`${fake.companyName}-license-App`);
     // cy.openApp();
