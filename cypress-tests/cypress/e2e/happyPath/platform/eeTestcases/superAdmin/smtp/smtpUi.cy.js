3f9e772e6b4427ac5cd677faefdf4c3ea8ffef92
ajith-k-v <ajith.jaban@gmail.com>
2025-11-12 19:58:47 +0530

    Add super admin cases

---
+++ cypress-tests/cypress/e2e/happyPath/platform/eeTestcases/superAdmin/smtp/smtpUi.cy.js
@@ -0,0 +1,13 @@
+import { openSMTPSettings, verifySmtpSettingsUI } from "Support/utils/superAdminSMTP";
+
+describe("Instance Settings - SMTP Settings UI", () => {
+    beforeEach(() => {
+        cy.apiLogin();
+        cy.visit("/");
+    });
+
+    it("verifies SMTP settings UI", () => {
+        openSMTPSettings();
+        verifySmtpSettingsUI();
+    });
+});
