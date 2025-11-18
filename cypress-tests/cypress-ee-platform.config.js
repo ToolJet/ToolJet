9624359c99818bb7f7ccab337b4a65b9d76bf388
ajith-k-v <ajith.jaban@gmail.com>
2025-11-18 19:21:09 +0530

    Browser test commit

--- cypress-tests/cypress-ee-platform.config.js
+++ cypress-tests/cypress-ee-platform.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
   projectId: "sk3oji",
 
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       config.baseUrl = environment.baseUrl;
 
       require("./cypress/config/tasks")(on);
@@ -51,11 +51,11 @@ module.exports = defineConfig({
       "cypress/e2e/happyPath/platform/eeTestcases/licensing/paidPlanTestcases/**/*.cy.js",
       "cypress/e2e/happyPath/platform/eeTestcases/licensing/updateLicense.cy.js",
       "cypress/e2e/happyPath/platform/eeTestcases/sso/**/*.cy.js",
-      "cypress/e2e/happyPath/platform/eeTestcases/multi-env/**/*.cy.js",
-      "cypress/e2e/happyPath/platform/eeTestcases/externalApi/**/*.cy.js",
-      "cypress/e2e/happyPath/platform/eeTestcases/userMetadata/**/*.cy.js",
-      "cypress/e2e/happyPath/platform/ceTestcases/**/*.cy.js",
-      "cypress/e2e/happyPath/platform/commonTestcases/**/*.cy.js",
+      // "cypress/e2e/happyPath/platform/eeTestcases/multi-env/**/*.cy.js",
+      // "cypress/e2e/happyPath/platform/eeTestcases/externalApi/**/*.cy.js",
+      // "cypress/e2e/happyPath/platform/eeTestcases/userMetadata/**/*.cy.js",
+      // "cypress/e2e/happyPath/platform/ceTestcases/**/*.cy.js",
+      // "cypress/e2e/happyPath/platform/commonTestcases/**/*.cy.js",
     ],
 
     testIsolation: true,
