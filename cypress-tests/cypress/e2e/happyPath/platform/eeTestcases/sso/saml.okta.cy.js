afbc92d579d8ef5839e16aed0ac9ebb6387766a2
ajith-k-v <ajith.jaban@gmail.com>
2025-11-21 00:03:37 +0530

    update failed test cases

--- cypress-tests/cypress/e2e/happyPath/platform/eeTestcases/sso/saml.okta.cy.js
+++ cypress-tests/cypress/e2e/happyPath/platform/eeTestcases/sso/saml.okta.cy.js
@@ -78,6 +78,7 @@ describe("SAML SSO", () => {
         cleanAllUsers();
         cy.apiDeleteAllApps();
     });
+
     afterEach("", () => {
         cy.apiLogin();
         deleteOrganisationSSO(data.workspaceName, ["saml"]);
