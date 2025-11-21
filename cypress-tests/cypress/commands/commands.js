4aa5d9717a2bc763368cf43e7abc85c385f4c557
Mekhla Asopa <dadhichmekhla@gmail.com>
2025-11-21 18:50:49 +0530

    fix user invite flow spec

--- cypress-tests/cypress/commands/commands.js
+++ cypress-tests/cypress/commands/commands.js
@@ -488,7 +488,7 @@ Cypress.Commands.add("defaultWorkspaceLogin", () => {
     cy.visit("/my-workspace");
     // cy.wait("@getLicenseAccess");
     cy.wait(2000);
-    cy.get('[data-cy="home-page-logo"]', { timeout: 50000 }).should(
+    cy.get(commonWidgetSelector.homePageLogo, { timeout: 50000 }).should(
       "be.visible",
       { timeout: 20000 }
     );
