7c401b4de0c5540b497ca12499355155349ff3a9
rohanlahori <rohanlahori99@gmail.com>
2025-11-28 16:29:41 +0530

    fixed layout co_relation id issue

--- server/src/modules/apps/services/app-import-export.service.ts
+++ server/src/modules/apps/services/app-import-export.service.ts
@@ -1460,6 +1460,7 @@ export class AppImportExportService {
                 newLayout.width = layout.width;
                 newLayout.height = layout.height;
                 newLayout.component = savedComponent;
+                newLayout.co_relation_id = layout.id;
 
                 await manager.save(newLayout);
               })
