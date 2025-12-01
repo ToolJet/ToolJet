4aa533bc006b840af3f1ea36571470196c5a9df5
rohanlahori <rohanlahori99@gmail.com>
2025-12-01 11:58:52 +0530

    migration changes

--- server/data-migrations/1758793442013-UpdateAppVersionStatusAndFields.ts
+++ server/data-migrations/1758793442013-UpdateAppVersionStatusAndFields.ts
@@ -11,21 +11,31 @@ export class UpdateAppVersionStatusAndFields1758793442013 implements MigrationIn
             INNER JOIN app_environments ae ON av.current_environment_id = ae.id
             WHERE ae.name = 'development'
         `);
-    const developmentVersionIds = allDevelopmentVersionIds.map((row) => row.id);
-    const nonDevelopmentVersionIDs = allVersions
-      .filter((version) => !developmentVersionIds.includes(version.id))
-      .map((row) => row.id);
-    if (nonDevelopmentVersionIDs && nonDevelopmentVersionIDs.length) {
-      await queryRunner.query(
-        `UPDATE app_versions SET status = '${AppVersionStatus.PUBLISHED}' WHERE id = ANY($1::uuid[])`,
-        [nonDevelopmentVersionIDs]
-      );
+    const allReleasedVersionIds = await queryRunner.query(`
+      SELECT current_version_id FROM apps
+  `);
+    const releasedVersionIds = allReleasedVersionIds.map((row) => row.current_version_id);
+    const draftVersionStatusArray = allDevelopmentVersionIds
+      .map((row) => row.id)
+      .filter((id) => !releasedVersionIds.has(id));
+
+    const draftVersionIds = new Set(draftVersionStatusArray);
+
+    // All versions that are not drafts are ---> Published
+    const publishedVersionStatusArray = allVersions.map((row) => row.id).filter((id) => !draftVersionIds.has(id));
+
+    if (publishedVersionStatusArray.length) {
+      await queryRunner.query(`UPDATE app_versions SET status = $1 WHERE id = ANY($2::uuid[])`, [
+        AppVersionStatus.PUBLISHED,
+        publishedVersionStatusArray,
+      ]);
     }
-    if (developmentVersionIds && developmentVersionIds.length) {
-      await queryRunner.query(
-        `UPDATE app_versions SET status = '${AppVersionStatus.DRAFT}' WHERE id = ANY($1::uuid[])`,
-        [developmentVersionIds]
-      );
+
+    if (draftVersionStatusArray.length) {
+      await queryRunner.query(`UPDATE app_versions SET status = $1 WHERE id = ANY($2::uuid[])`, [
+        AppVersionStatus.DRAFT,
+        draftVersionStatusArray,
+      ]);
     }
   }
 
