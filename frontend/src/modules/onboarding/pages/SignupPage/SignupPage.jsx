41c2e361471ab6754054aeca3575d49051f5c369
Siddharth Pundir <145639697+Siddharthpl@users.noreply.github.com>
2025-12-11 18:44:35 +0530

    Added the passwrod complexity if its env is present (#14718)
    * Added the passwrod complexity if its env is present
    
    * Updating the frontend password input
    
    * bugs
    
    * validatepassword
    
    * update cypress test cases for new password flow
    
    * Fix failing cypress test cases
    
    * toast
    
    * error msg
    
    * sus
    
    * Update login flow test cases
    
    * valid password
    
    ---------
    
    Co-authored-by: gsmithun4 <gsmithun4@gmail.com>
    Co-authored-by: Rudhra Deep Biswas <rudra21ultra@gmail.com>
    Co-authored-by: ajith-k-v <ajith.jaban@gmail.com>

--- frontend/src/modules/onboarding/pages/SignupPage/SignupPage.jsx
+++ frontend/src/modules/onboarding/pages/SignupPage/SignupPage.jsx
@@ -48,16 +48,22 @@ const SignupPage = ({ configs, organizationId }) => {
         .then((response) => onInvitedUserSignUpSuccess(response, navigate))
         .catch((errorObj) => {
           let errorMessage;
-          const isThereAnyErrorsArray = errorObj?.error?.length && typeof errorObj?.error?.[0] === 'string';
-          if (errorObj?.error?.includes('reached your limit')) {
-            // Note : The fix is made to handle the case when errorObj?.error is a string and not an object
-            errorMessage = errorObj?.error;
-          } else if (isThereAnyErrorsArray) {
-            errorMessage = errorObj?.error?.[0];
-          } else if (typeof errorObj?.error?.error === 'string') {
-            errorMessage = errorObj?.error?.error;
+          if (typeof errorObj?.error === 'string') {
+            errorMessage = errorObj.error; 
+          }
+          if (!errorMessage) {
+            const isThereAnyErrorsArray = errorObj?.error?.length && typeof errorObj?.error?.[0] === 'string';
+            if (errorObj?.error?.includes('reached your limit')) {
+              // Note : The fix is made to handle the case when errorObj?.error is a string and not an object
+              errorMessage = errorObj?.error;
+            } else if (isThereAnyErrorsArray) {
+              errorMessage = errorObj?.error?.[0];
+            } else if (typeof errorObj?.error?.error === 'string') {
+              errorMessage = errorObj?.error?.error;
+            }
           }
           errorMessage && toast.error(errorMessage);
+          onFaluire(errorObj);
         });
     } else {
       authenticationService
