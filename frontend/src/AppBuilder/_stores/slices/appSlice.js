c72f12f7e7c28630bc20d17e1386d5c19e2db2b5
Shaurya Sharma <79473274+shaurya-sharma064@users.noreply.github.com>
2025-12-12 16:47:15 +0530

    Revert "fix: handle visibility in view mode for WidgetWrapper and appSlice (#…" (#14782)
    This reverts commit 9b8ce77827d27b744668e350cc26bf36fd14966e.

--- frontend/src/AppBuilder/_stores/slices/appSlice.js
+++ frontend/src/AppBuilder/_stores/slices/appSlice.js
@@ -152,19 +152,14 @@ export const createAppSlice = (set, get) => ({
 
     const maxPermanentHeight = currentMainCanvasComponents.reduce((max, component) => {
       const layout = component?.layouts?.[currentLayout];
-      if (!layout) {
-        return max;
-      }
       const visibility =
         getResolvedValue(component?.component?.definition?.properties?.visibility?.value) ||
         getResolvedValue(component?.component?.definition?.styles?.visibility?.value);
 
-      // In view mode, skip components with visibility false or undefined
-      if (currentMode === 'view' && !visibility) {
+      const height = visibility ? layout.height : 10;
+      if (!layout) {
         return max;
       }
-
-      const height = visibility ? layout.height : 10;
       const sum = layout.top + height;
       return Math.max(max, sum);
     }, 0);
