d5f1037275cde23ac1dae82f9b2255f1e91140fb
Siddharthpl <siddharthpundir73@gmail.com>
2025-12-30 10:12:11 +0530

    Revert "Removed email verification for ee and ce"
    This reverts commit 20eedfb4a4019ac41deed6ab1ac808537a67281d.

--- frontend/src/modules/onboarding/pages/SignupPage/SignupPage.jsx
+++ frontend/src/modules/onboarding/pages/SignupPage/SignupPage.jsx
@@ -72,16 +72,7 @@ const SignupPage = ({ configs, organizationId }) => {
           const { organizationInviteUrl } = response;
           if (organizationInviteUrl) onInvitedUserSignUpSuccess(response, navigate);
           setSigningUserInfo({ email, name });
-
-          // Show email verification flow only for cloud edition.
-          if (edition === 'cloud') {
-            setSignupSuccess(true);
-          } else {
-            // For non-cloud editions, skip email verification screen and redirect to login.
-            const loginRedirect = redirectTo ? `?redirectTo=${redirectTo}` : '';
-            navigate(`/login${loginRedirect}`, { replace: true });
-          }
-
+          setSignupSuccess(true);
           onSuccess();
         })
         .catch((e) => {
--- server/src/modules/onboarding/util.service.ts
+++ server/src/modules/onboarding/util.service.ts
@@ -9,7 +9,7 @@ import { LicenseCountsService } from '../licensing/services/count.service';
 import { LICENSE_TRIAL_API, ORGANIZATION_INSTANCE_KEY } from '../licensing/constants';
 import got from 'got/dist/source';
 import { HttpException } from '@nestjs/common';
-import { fullName, generateNextNameAndSlug, generateOrgInviteURL, getTooljetEdition } from 'src/helpers/utils.helper';
+import { fullName, generateNextNameAndSlug, generateOrgInviteURL } from 'src/helpers/utils.helper';
 import { NotAcceptableException } from '@nestjs/common';
 import { Organization } from '../../entities/organization.entity';
 import { EntityManager } from 'typeorm';
@@ -17,7 +17,6 @@ import {
   getUserStatusAndSource,
   lifecycleEvents,
   SOURCE,
-  USER_STATUS,
   WORKSPACE_USER_STATUS,
   WORKSPACE_USER_SOURCE,
 } from '@modules/users/constants/lifecycle';
@@ -443,23 +442,6 @@ export class OnboardingUtilService implements IOnboardingUtilService {
         lastName: user.lastName,
         role: user.role,
       });
-      
-      // Auto-activate users for non-cloud editions (skip email verification)
-      const edition = getTooljetEdition();
-      const isCloudEdition = edition === 'cloud';
-      if (!isCloudEdition && user.status === USER_STATUS.INVITED) {
-        await this.userRepository.updateOne(
-          user.id,
-          {
-            status: USER_STATUS.ACTIVE,
-            invitationToken: null,
-          },
-          manager
-        );
-        user.status = USER_STATUS.ACTIVE;
-        user.invitationToken = null;
-      }
-      
       if (signingUpOrganization) {
         /* Attach the user and user groups to the organization */
         const organizationUser = await this.organizationUserRepository.createOne(
@@ -469,30 +451,20 @@ export class OnboardingUtilService implements IOnboardingUtilService {
           manager,
           WORKSPACE_USER_SOURCE.SIGNUP
         );
-        
-        // Auto-activate organization user for non-cloud editions
-        if (!isCloudEdition && organizationUser.status === WORKSPACE_USER_STATUS.INVITED) {
-          await this.organizationUsersUtilService.activateOrganization(organizationUser, manager);
-        }
-        
         await this.licenseUserService.validateUser(manager, organizationId);
-        
-        // Only send verification email for cloud edition
-        if (isCloudEdition) {
-          this.eventEmitter.emit('emailEvent', {
-            type: EMAIL_EVENTS.SEND_WELCOME_EMAIL,
-            payload: {
-              to: user.email,
-              name: user.firstName,
-              invitationtoken: user.invitationToken,
-              organizationInvitationToken: organizationUser.invitationToken,
-              organizationId: signingUpOrganization.id,
-              organizationName: signingUpOrganization.name,
-              sender: null,
-              redirectTo: redirectTo,
-            },
-          });
-        }
+        this.eventEmitter.emit('emailEvent', {
+          type: EMAIL_EVENTS.SEND_WELCOME_EMAIL,
+          payload: {
+            to: user.email,
+            name: user.firstName,
+            invitationtoken: user.invitationToken,
+            organizationInvitationToken: organizationUser.invitationToken,
+            organizationId: signingUpOrganization.id,
+            organizationName: signingUpOrganization.name,
+            sender: null,
+            redirectTo: redirectTo,
+          },
+        });
         // this.eventEmitter.emit(
         //   'auditLogEntry',
         //   {
@@ -507,39 +479,15 @@ export class OnboardingUtilService implements IOnboardingUtilService {
         // );
         return {};
       } else {
-        // Auto-activate users for non-cloud editions (skip email verification)
-        const edition = getTooljetEdition();
-        const isCloudEdition = edition === 'cloud';
-        if (!isCloudEdition && user.status === USER_STATUS.INVITED) {
-          await this.userRepository.updateOne(
-            user.id,
-            {
-              status: USER_STATUS.ACTIVE,
-              invitationToken: null,
-            },
-            manager
-          );
-          user.status = USER_STATUS.ACTIVE;
-          user.invitationToken = null;
-        }
-        
-        // Use user's default organization ID if signingUpOrganization is null
-        const orgIdForValidation = user.defaultOrganizationId || organizationId;
-        if (orgIdForValidation) {
-          await this.licenseUserService.validateUser(manager, orgIdForValidation);
-        }
-        
-        // Only send verification email for cloud edition
-        if (isCloudEdition) {
-          this.eventEmitter.emit('emailEvent', {
-            type: EMAIL_EVENTS.SEND_WELCOME_EMAIL,
-            payload: {
-              to: user.email,
-              name: user.firstName,
-              invitationtoken: user.invitationToken,
-            },
-          });
-        }
+        await this.licenseUserService.validateUser(manager, organizationId);
+        this.eventEmitter.emit('emailEvent', {
+          type: EMAIL_EVENTS.SEND_WELCOME_EMAIL,
+          payload: {
+            to: user.email,
+            name: user.firstName,
+            invitationtoken: user.invitationToken,
+          },
+        });
 
         // this.eventEmitter.emit(
         //   'auditLogEntry',
@@ -692,7 +640,7 @@ export class OnboardingUtilService implements IOnboardingUtilService {
       );
 
       // Create organization user entry
-      const organizationUser = await this.organizationUserRepository.createOne(
+      await this.organizationUserRepository.createOne(
         user,
         defaultWorkspace,
         true,
@@ -700,41 +648,18 @@ export class OnboardingUtilService implements IOnboardingUtilService {
         WORKSPACE_USER_SOURCE.SIGNUP
       );
 
-      // Auto-activate users for non-cloud editions (skip email verification)
-      const edition = getTooljetEdition();
-      const isCloudEdition = edition === 'cloud';
-      if (!isCloudEdition && user.status === USER_STATUS.INVITED) {
-        await this.userRepository.updateOne(
-          user.id,
-          {
-            status: USER_STATUS.ACTIVE,
-            invitationToken: null,
-          },
-          manager
-        );
-        user.status = USER_STATUS.ACTIVE;
-        user.invitationToken = null;
-        
-        // Also activate the organization user for non-cloud editions
-        if (organizationUser.status === WORKSPACE_USER_STATUS.INVITED) {
-          await this.organizationUsersUtilService.activateOrganization(organizationUser, manager);
-        }
-      }
-
       // Validate license
       await this.licenseUserService.validateUser(manager, user?.defaultOrganizationId);
 
-      // Only send verification email for cloud edition
-      if (isCloudEdition) {
-        this.eventEmitter.emit('emailEvent', {
-          type: EMAIL_EVENTS.SEND_WELCOME_EMAIL,
-          payload: {
-            to: user.email,
-            name: user.firstName,
-            invitationtoken: user.invitationToken,
-          },
-        });
-      }
+      // Send welcome email
+      this.eventEmitter.emit('emailEvent', {
+        type: EMAIL_EVENTS.SEND_WELCOME_EMAIL,
+        payload: {
+          to: user.email,
+          name: user.firstName,
+          invitationtoken: user.invitationToken,
+        },
+      });
 
       return {};
     }, manager);
