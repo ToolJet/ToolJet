19117e5dd7450b40edffbadec36c3cca42e86ed0
Mekhla Asopa <dadhichmekhla@gmail.com>
2026-01-05 18:56:17 +0530

    update config

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
     },
 
     baseUrl: "http://localhost:8082",
-    specPattern: "cypress/e2e/happyPath/marketplace/commonTestcases/datasources/postgresHappyPath.cy.js",
+    specPattern: "cypress/e2e/happyPath/**/*.cy.js",
 
     testIsolation: true,
     retries: {
