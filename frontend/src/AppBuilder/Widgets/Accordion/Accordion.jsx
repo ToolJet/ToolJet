7b9bd3cdb4664dbb885889818fe39f0b592b4bd1
devanshu052000 <devanshu.rastogi05@gmail.com>
2026-02-04 03:18:59 +0530

    Create the base for the new Accordion component including CSAs

---
+++ frontend/src/AppBuilder/Widgets/Accordion/Accordion.jsx
@@ -0,0 +1,209 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { Container as ContainerComponent } from '@/AppBuilder/AppCanvas/Container';
+import Spinner from '@/_ui/Spinner';
+import { useDynamicHeight } from '@/_hooks/useDynamicHeight';
+import {
+  CONTAINER_FORM_CANVAS_PADDING,
+  SUBCONTAINER_CANVAS_BORDER_WIDTH,
+} from '@/AppBuilder/AppCanvas/appCanvasConstants';
+import '../Container/container.scss';
+import './accordion.scss';
+import Header from './components/Header';
+import { useBatchedUpdateEffectArray } from '@/_hooks/useBatchedUpdateEffectArray';
+
+export const Accordion = ({
+  id,
+  properties,
+  styles,
+  darkMode,
+  height,
+  width,
+  setExposedVariables,
+  setExposedVariable,
+  adjustComponentPositions,
+  currentLayout,
+  componentCount = 0,
+  currentMode,
+  subContainerIndex,
+  fireEvent,
+}) => {
+  const { headerHeight = 80, showHeader, dynamicHeight, loadingState, visibility, disabledState } = properties;
+  const { borderRadius, borderColor, boxShadow, headerDividerColor, chevronIconColor } = styles;
+
+  const [exposedVariablesTemporaryState, setExposedVariablesTemporaryState] = useState({
+    isVisible: visibility,
+    isDisabled: disabledState,
+    isLoading: loadingState,
+    isExpanded: true, // State for expanding/collapsing accordion component
+  });
+
+  // Helpers
+  const updateExposedVariablesState = (key, value) => {
+    setExposedVariablesTemporaryState((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  // Dynamic height
+  const isDynamicHeightEnabled = dynamicHeight && currentMode === 'view';
+  useDynamicHeight({
+    isDynamicHeightEnabled,
+    id,
+    height,
+    adjustComponentPositions,
+    currentLayout,
+    isContainer: true,
+    componentCount,
+    value: JSON.stringify({ headerHeight, showHeader }),
+    visibility: exposedVariablesTemporaryState.isVisible,
+    subContainerIndex,
+  });
+
+  // Effects
+  useEffect(() => {
+    setExposedVariables({
+      ...exposedVariablesTemporaryState,
+      expand: async function () {
+        setExposedVariable('isExpanded', true);
+        updateExposedVariablesState('isExpanded', true);
+        fireEvent('onExpand');
+      },
+      collapse: async function () {
+        setExposedVariable('isExpanded', false);
+        updateExposedVariablesState('isExpanded', false);
+        fireEvent('onCollapse');
+      },
+      setVisibility: async function (value) {
+        setExposedVariable('isVisible', value);
+        updateExposedVariablesState('isVisible', value);
+      },
+      setDisable: async function (value) {
+        setExposedVariable('isDisabled', value);
+        updateExposedVariablesState('isDisabled', value);
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useBatchedUpdateEffectArray([
+    {
+      dep: exposedVariablesTemporaryState.isExpanded,
+      sideEffect: () => {
+        setExposedVariable('isExpanded', exposedVariablesTemporaryState.isExpanded);
+      },
+    },
+    {
+      dep: loadingState,
+      sideEffect: () => {
+        updateExposedVariablesState('isLoading', loadingState);
+        setExposedVariable('isLoading', loadingState);
+      },
+    },
+    {
+      dep: visibility,
+      sideEffect: () => {
+        updateExposedVariablesState('isVisible', visibility);
+        setExposedVariable('isVisible', visibility);
+      },
+    },
+    {
+      dep: disabledState,
+      sideEffect: () => {
+        updateExposedVariablesState('isDisabled', disabledState);
+        setExposedVariable('isDisabled', disabledState);
+      },
+    },
+  ]);
+
+  // Styles
+  const contentBgColor = useMemo(() => {
+    return {
+      backgroundColor:
+        ['#fff', '#ffffffff'].includes(styles.backgroundColor) && darkMode ? '#232E3C' : styles.backgroundColor,
+    };
+  }, [styles.backgroundColor, darkMode]);
+
+  const computedStyles = {
+    backgroundColor: contentBgColor.backgroundColor,
+    borderRadius: borderRadius ? parseFloat(borderRadius) : 0,
+    border: `${SUBCONTAINER_CANVAS_BORDER_WIDTH}px solid ${borderColor}`,
+    ...(isDynamicHeightEnabled && exposedVariablesTemporaryState.isExpanded && { minHeight: `${height}px` }),
+    height: !exposedVariablesTemporaryState.isExpanded
+      ? headerHeight + CONTAINER_FORM_CANVAS_PADDING + 3 + 2 // 3 for bottom padding and 2 for top-bottom border
+      : isDynamicHeightEnabled
+      ? '100%'
+      : height,
+    display: exposedVariablesTemporaryState.isVisible ? 'flex' : 'none',
+    flexDirection: 'column',
+    position: 'relative',
+    boxShadow,
+    '--cc-container-header-divider-color': headerDividerColor,
+  };
+
+  const containerContentStyles = useMemo(() => {
+    return {
+      display: 'flex',
+      height: '100%',
+      padding: `${CONTAINER_FORM_CANVAS_PADDING}px`,
+      ...(exposedVariablesTemporaryState.isDisabled && {
+        opacity: 0.5,
+        pointerEvents: 'none',
+      }),
+    };
+  }, [exposedVariablesTemporaryState.isDisabled]);
+
+  return (
+    <div
+      className={`jet-container ${exposedVariablesTemporaryState.isLoading ? 'jet-container-loading' : ''} ${
+        exposedVariablesTemporaryState.isExpanded ? 'jet-container-expanded' : 'jet-container-collapsed'
+      }`}
+      id={id}
+      data-disabled={exposedVariablesTemporaryState.isDisabled}
+      style={computedStyles}
+    >
+      {exposedVariablesTemporaryState.isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {showHeader && (
+            <Header
+              id={id}
+              height={height}
+              width={width}
+              isDynamicHeightEnabled={isDynamicHeightEnabled}
+              headerBackgroundColor={styles.headerBackgroundColor}
+              darkMode={darkMode}
+              borderRadius={borderRadius}
+              headerHeight={headerHeight}
+              isExpanded={exposedVariablesTemporaryState.isExpanded}
+              setExpanded={(expand) => updateExposedVariablesState('isExpanded', expand)}
+              fireEvent={fireEvent}
+              isDisabled={exposedVariablesTemporaryState.isDisabled}
+              chevronIconColor={chevronIconColor}
+            />
+          )}
+          {exposedVariablesTemporaryState.isExpanded && (
+            <div
+              style={containerContentStyles}
+              className={`${dynamicHeight && `dynamic-${id}`} widget-type-container`}
+              data-disabled={exposedVariablesTemporaryState.isDisabled}
+            >
+              <ContainerComponent
+                id={id}
+                styles={{
+                  ...contentBgColor,
+                  borderRadius: `${borderRadius}px`,
+                }}
+                canvasHeight={isDynamicHeightEnabled ? '100%' : height}
+                canvasWidth={width}
+                darkMode={darkMode}
+                componentType="Container"
+              />
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
---
+++ frontend/src/AppBuilder/Widgets/Accordion/components/Header.jsx
@@ -0,0 +1,95 @@
+import React, { useMemo } from 'react';
+import { useActiveSlot } from '@/AppBuilder/_hooks/useActiveSlot';
+import { HorizontalSlot } from '@/AppBuilder/Widgets/Form/Components/HorizontalSlot';
+import useStore from '@/AppBuilder/_stores/store';
+import { shallow } from 'zustand/shallow';
+import { CONTAINER_FORM_CANVAS_PADDING } from '@/AppBuilder/AppCanvas/appCanvasConstants';
+import TablerIcon from '@/_ui/Icon/TablerIcon';
+
+const Header = (props) => {
+  const {
+    id,
+    height,
+    width,
+    isDynamicHeightEnabled,
+    headerBackgroundColor,
+    darkMode,
+    borderRadius,
+    headerHeight,
+    isExpanded,
+    setExpanded,
+    fireEvent,
+    isDisabled,
+    chevronIconColor,
+  } = props;
+
+  const setComponentProperty = useStore((state) => state.setComponentProperty, shallow);
+  const activeSlot = useActiveSlot(id); // Track the active slot for this widget
+  const headerMaxHeight = isDynamicHeightEnabled ? 10000 : parseInt(height, 10) - 100 - 10;
+
+  const headerBgColor = useMemo(() => {
+    return {
+      backgroundColor:
+        ['#fff', '#ffffffff'].includes(headerBackgroundColor) && darkMode ? '#232E3C' : headerBackgroundColor,
+    };
+  }, [headerBackgroundColor, darkMode]);
+
+  const containerHeaderStyles = useMemo(() => {
+    return {
+      flexShrink: 0,
+      padding: `${CONTAINER_FORM_CANVAS_PADDING}px ${CONTAINER_FORM_CANVAS_PADDING}px 3px ${CONTAINER_FORM_CANVAS_PADDING}px`,
+      maxHeight: `${headerMaxHeight}px`,
+      borderTopLeftRadius: `${borderRadius}px`,
+      borderTopRightRadius: `${borderRadius}px`,
+      ...(!isExpanded && { borderBottomLeftRadius: `${borderRadius}px` }),
+      ...headerBgColor,
+    };
+  }, [headerMaxHeight, borderRadius, isExpanded, headerBgColor]);
+
+  const updateHeaderSizeInStore = ({ newHeight }) => {
+    const _height = parseInt(newHeight, 10);
+    setComponentProperty(id, `headerHeight`, _height, 'properties', 'value', false);
+  };
+
+  return (
+    <div className="tj-accordion-header">
+      <div className="tw-h-full tw-w-full">
+        <HorizontalSlot
+          slotName={'header'}
+          slotStyle={containerHeaderStyles}
+          id={`${id}-header`}
+          height={headerHeight}
+          width={width}
+          darkMode={darkMode}
+          isDisabled={isDisabled}
+          isActive={activeSlot === `${id}-header`}
+          onResize={updateHeaderSizeInStore}
+          componentType="Container"
+        />
+      </div>
+      <div className="tj-accordion-close-btn">
+        <span
+          data-cy={`accordion-close-button`}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const expand = !isExpanded;
+
+            setExpanded(expand);
+            fireEvent(expand ? 'onExpand' : 'onCollapse');
+          }}
+        >
+          <TablerIcon
+            iconName="IconChevronUp"
+            size={24}
+            color={chevronIconColor}
+            className={`tw-transition-transform tw-duration-200 ${isExpanded ? '' : 'tw-rotate-180'}`}
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
