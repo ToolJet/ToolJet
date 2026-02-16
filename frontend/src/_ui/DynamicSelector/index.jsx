9b4f0c2b37c0ce1e2ccd33f600964f700e4c910b
abhijeet760 <abhijeet@tooljet.com>
2026-02-16 07:19:16 +0000

    autofetch

--- frontend/src/_ui/DynamicSelector/index.jsx
+++ frontend/src/_ui/DynamicSelector/index.jsx
@@ -268,6 +268,29 @@ const DynamicSelector = ({
     }
   }, [selectedDataSource]);
 
+  useEffect(() => {
+    if (autoFetch && !isDependentField && selectedDataSource?.id && invokeMethod && !isFxMode) {
+      const cacheKey = `${propertyKey}_cache`;
+      const existingCache = get(options, cacheKey) || {};
+      
+      const isMultiAuth = selectedDataSource?.options?.multiple_auth_enabled;
+      const userId = currentUser?.id;
+      
+      let cachedData = null;
+      if (isMultiAuth) {
+        if (userId && existingCache[userId]) {
+          cachedData = existingCache[userId]['nonDependentCache'];
+        }
+      } else {
+        cachedData = existingCache['nonDependentCache'];
+      }
+      
+      if (!cachedData) {
+        handleFetch();
+      }
+    }
+  }, [autoFetch, selectedDataSource?.id, invokeMethod, isFxMode]);
+
   // Watch for changes in dependency state
   useEffect(() => {
     if (isDependentField && compositeDependencyKey && depsReady) {
