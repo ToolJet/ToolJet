7645b51b5eacad828187094ccb75485238b49c50
Shantanu Mane <maneshantanu.20@gmail.com>
2026-02-24 17:28:09 +0530

    feat: Enhance external API group creation with security guard and feature initialization

--- server/src/modules/external-apis/controllers/groups.controller.ts
+++ server/src/modules/external-apis/controllers/groups.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Post, Param, Body, NotFoundException } from '@nestjs/common';
+import { Controller, Param, Body, NotFoundException, UseGuards, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { CreateGroupExternalDto } from '../dto/groups.dto';
+import { InitFeature } from '@modules/app/decorators/init-feature.decorator';
+import { FEATURE_KEY } from '@modules/external-apis/constants';
+import { ExternalApiSecurityGuard } from '@ee/auth/guards/external-api-security.guard';
 
 @Controller('ext')
 export class ExternalApisGroupsController {
-    constructor() { }
+  constructor() {}
 
-    async createGroup(
-        @Param('workspaceId') workspaceId: string,
-        @Body() createGroupDto: CreateGroupExternalDto
-    ): Promise<void> {
-        throw new NotFoundException();
-    }
+  @InitFeature(FEATURE_KEY.CREATE_GROUP)
+  @UseGuards(ExternalApiSecurityGuard)
+  @Post('workspace/:workspaceId/groups')
+  @HttpCode(HttpStatus.CREATED)
+  async createGroup(
+    @Param('workspaceId') workspaceId: string,
+    @Body() createGroupDto: CreateGroupExternalDto
+  ): Promise<void> {
+    throw new NotFoundException();
+  }
 }
