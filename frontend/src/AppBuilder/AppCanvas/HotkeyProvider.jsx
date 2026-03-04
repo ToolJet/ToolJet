d583baca8a514782bf1c70aa616ef8e8baef667b
parthy007 <parthadhikari1812@gmail.com>
2026-03-04 13:45:17 +0530

    fix: unfreeze module

--- frontend/src/AppBuilder/AppCanvas/HotkeyProvider.jsx
+++ frontend/src/AppBuilder/AppCanvas/HotkeyProvider.jsx
@@ -14,7 +14,7 @@ export const HotkeyProvider = ({ children, mode, currentLayout, canvasMaxWidth, 
   const handleRedo = useStore((state) => state.handleRedo);
   const setWidgetDeleteConfirmation = useStore((state) => state.setWidgetDeleteConfirmation);
   const moveComponentPosition = useStore((state) => state.moveComponentPosition, shallow);
-  const shouldFreeze = useStore((state) => state.getShouldFreeze());
+  const shouldFreeze = useStore((state) => state.getShouldFreeze(false, isModuleEditor));
   const enableReleasedVersionPopupState = useStore((state) => state.enableReleasedVersionPopupState, shallow);
   const clearSelectedComponents = useStore((state) => state.clearSelectedComponents, shallow);
   const getSelectedComponents = useStore((state) => state.getSelectedComponents, shallow);
