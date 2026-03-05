f316be4be7e9208cd7aff8419e780781a2648c93
ajith-k-v <ajith.jaban@gmail.com>
2026-03-05 18:03:53 +0530

    add zoom and rotate functionality validation

--- cypress-tests/cypress/e2e/happyPath/appbuilder/component-new/image.cy.js
+++ cypress-tests/cypress/e2e/happyPath/appbuilder/component-new/image.cy.js
@@ -1,11 +1,14 @@
 import {
     genralProperties,
-    verifyExistance
+    verifyExistance,
+    verifyImageFeature
 } from "Support/utils/appBuilder/featuresValidation";
 
 describe("Image Component - Feature Validation", { baseUrl: null }, () => {
     const componentSelector = '[data-cy="draggable-widget-image1"]';
     const desktopToggle = '[data-cy="toggleswitch1"] .d-flex';
+    const zoomToggle = '[data-cy="zoombutton"] input[type="checkbox"]';
+    const rotateToggle = '[data-cy="rotatebutton"] input[type="checkbox"]';
     const visibilityToggle = '[data-cy="toggleswitch3"] .d-flex';
     const disableToggle = '[data-cy="toggleswitch4"] .d-flex';
     const loadingState = '[data-cy="toggleswitch5"] .d-flex';
@@ -21,9 +24,30 @@ describe("Image Component - Feature Validation", { baseUrl: null }, () => {
         { controllerSelector: disableToggle, state: "exist" },
     ];
 
+    const imageFeatures = [
+        {
+            toggleSelector: zoomToggle,
+            action: { target: '.react-transform-component', dblclick: true },
+            assertion: {
+                target: '.react-transform-component',
+                style: 'scale',
+            },
+            resetAfter: true,
+        },
+        {
+            toggleSelector: rotateToggle,
+            action: { preClick: true, hover: true, target: '.img-control-btn' },
+            assertion: {
+                target: 'img',
+                style: 'rotate',
+            },
+            resetAfter: true,
+        },
+    ];
+
     beforeEach(() => {
         cy.visit(appUrl);
-        cy.viewport(1600, 1200);
+        cy.viewport(1800, 1400);
     });
 
     it("should verify image component properties", () => {
@@ -31,6 +55,14 @@ describe("Image Component - Feature Validation", { baseUrl: null }, () => {
         properties.forEach(({ controllerSelector, state, className }) => {
             genralProperties(componentSelector, controllerSelector, state, className);
         });
+
+        genralProperties(componentSelector, loadingState, "exist");
+        genralProperties(componentSelector, disableToggle, "exist");
+
+        imageFeatures.forEach((feature) => {
+            verifyImageFeature(componentSelector, feature);
+        });
     });
+
 
 });
--- cypress-tests/cypress/support/utils/appBuilder/featuresValidation.js
+++ cypress-tests/cypress/support/utils/appBuilder/featuresValidation.js
@@ -8,3 +8,53 @@ export const genralProperties = (componentSelector, controllerSelector, state = 
     if (state) cy.get(componentSelector).should(state);
     if (className) cy.get(componentSelector).find(`.${className}`).should("exist");
 }
+
+export const verifyImageFeature = (componentSelector, featureConfig) => {
+    const { toggleSelector, action, assertion, resetAfter } = featureConfig;
+
+    if (toggleSelector) {
+        cy.get(toggleSelector).click();
+    }
+
+    if (action) {
+        if (action.preClick) {
+            cy.get(componentSelector).first().click();
+        }
+        if (action.hover) {
+            cy.get(componentSelector).first().trigger('mouseover');
+        }
+        const clickMethod = action.dblclick ? 'dblclick' : 'click';
+        if (action.coordinates) {
+            cy.get(componentSelector).first()[clickMethod](action.coordinates.x, action.coordinates.y);
+        } else {
+            const actionTarget = action.target
+                ? cy.get(componentSelector).first().find(action.target)
+                : cy.get(componentSelector).first();
+            actionTarget[clickMethod](action.options || {});
+        }
+    }
+
+    if (assertion) {
+        const assertTarget = assertion.target
+            ? cy.get(componentSelector).first().find(assertion.target)
+            : cy.get(componentSelector).first();
+
+        if (assertion.style) {
+            assertTarget
+                .should('have.attr', 'style')
+                .and('include', assertion.style);
+        }
+
+        if (assertion.state) {
+            assertTarget.should(assertion.state);
+        }
+
+        if (assertion.ariaLabel) {
+            assertTarget.should('have.attr', 'aria-label', assertion.ariaLabel);
+        }
+    }
+
+    if (resetAfter && toggleSelector) {
+        cy.get(toggleSelector).click();
+    }
+}
