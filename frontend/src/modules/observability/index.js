02e12d3270990dc793273f46d872fdc2df2a3cd0
Adish M <adish.madhu@gmail.com>
2026-03-10 11:13:02 +0530

    feat(observability): remove observability service and related components

--- frontend/src/modules/observability/index.js
+++ frontend/src/modules/observability/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import ObservabilityPage from './pages/ObservabilityPage';
+import * as eeModules from '@ee/modules';
 import { AdminRoute } from '@/Routes';
 import DesktopOnlyRoute from '@/Routes/DesktopOnlyRoute';
 
-/* NOTE:
-    This file should be the entry point to a module.
-    Anything inside the module shouldn't be accessible outside module folder
-*/
-const getObservabilityRoutes = (props) => [
-  <Route
-    exact
-    key="observability"
-    path="/observability"
-    element={
-      <DesktopOnlyRoute darkMode={props.darkMode}>
-        <AdminRoute {...props}>
-          <ObservabilityPage darkMode={props.darkMode} />
-        </AdminRoute>
-      </DesktopOnlyRoute>
-    }
-  />,
-];
+const EEObservabilityPage = eeModules?.Observability?.components?.ObservabilityPage?.default;
+
+const getObservabilityRoutes = (props) => {
+  if (!EEObservabilityPage) return [];
+  return [
+    <Route
+      exact
+      key="observability"
+      path="/observability"
+      element={
+        <DesktopOnlyRoute darkMode={props.darkMode}>
+          <AdminRoute {...props}>
+            <EEObservabilityPage darkMode={props.darkMode} />
+          </AdminRoute>
+        </DesktopOnlyRoute>
+      }
+    />,
+  ];
+};
 
 export default getObservabilityRoutes;
