b5e01f7a39757c5c257fb33397419bbfabe66cb1
Rudhra Deep Biswas <rudra21ultra@gmail.com>
2026-03-12 17:06:53 +0530

    migrations improvement

--- server/data-migrations/1773127399691-MoveOauthTokensToDB.ts
+++ server/data-migrations/1773127399691-MoveOauthTokensToDB.ts
@@ -40,7 +40,8 @@ export class MoveOauthTokensToDB1773127399691 implements MigrationInterface {
                    OR
 
                    -- multi-auth OFF: has credential_id on access_token
-                   options->'access_token'->>'credential_id' IS NOT NULL
+                   (options->'multiple_auth_enabled'->>'value')::boolean = false
+                   AND options->'access_token'->>'credential_id' IS NOT NULL
                  )
           ORDER  BY id
           LIMIT  $2
