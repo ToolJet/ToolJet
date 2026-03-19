be585dc5473e56f9e0985b2432015643c5b37adf
Nishidh Jain <nishidhjain909@gmail.com>
2026-03-19 15:15:25 +0530

    feat: Add search bar for apps & workflows

--- frontend/src/pages/layouts/WorkspaceLayout/Header.jsx
+++ frontend/src/pages/layouts/WorkspaceLayout/Header.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { useSearch } from '@/pages/shared/SearchBar';
+import { useAppsStore } from '@/pages/shared/store';
+
+import SearchBar from './SearchBar';
 import WorkspaceSelector from './WorkspaceSelector';
 
 export default function Header() {
+  const setAppSearchQuery = useAppsStore((state) => state.setAppSearchQuery);
+
+  const { searchTerm, setSearchTerm, debouncedSearchTerm } = useSearch({ debounceDelay: 500 });
+
+  const handleSearchTermChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    setAppSearchQuery(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(
+    () => () => {
+      setAppSearchQuery('');
+    },
+    []
+  );
+
   return (
-    <header className="tw-flex tw-items-center tw-gap-2 tw-h-12 tw-border-b tw-border-border-weak tw-pl-3 tw-pr-8">
+    <header className="tw-grid tw-grid-cols-3 tw-items-center tw-h-12 tw-border-b tw-border-border-weak tw-pl-3 tw-pr-8">
       <WorkspaceSelector />
+
+      <SearchBar
+        searchTerm={searchTerm}
+        onSearchTermChange={handleSearchTermChange}
+        classes={{ searchBarContainer: 'tw-mx-auto' }}
+      />
     </header>
   );
 }
