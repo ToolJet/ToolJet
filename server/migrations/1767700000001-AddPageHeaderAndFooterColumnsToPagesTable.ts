381c987972cb712742739fc0310ec8bbd42095dd
Nakul Nagargade <nakul@tooljet.com>
2026-03-25 16:02:57 +0530

    Combine migrations

--- server/migrations/1767700000001-AddPageHeaderAndFooterColumnsToPagesTable.ts
+++ server/migrations/1767700000001-AddPageHeaderAndFooterColumnsToPagesTable.ts
@@ -1,18 +1,23 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export class AddPageHeaderColumnToPagesTable1767700000000 implements MigrationInterface {
+export class AddPageHeaderAndFooterColumnsToPagesTable1767700000001 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn(
-            'pages',
+        await queryRunner.addColumns('pages', [
             new TableColumn({
                 name: 'page_header',
                 type: 'jsonb',
                 isNullable: true,
-            })
-        );
+            }),
+            new TableColumn({
+                name: 'page_footer',
+                type: 'jsonb',
+                isNullable: true,
+            }),
+        ]);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('pages', 'page_footer');
         await queryRunner.dropColumn('pages', 'page_header');
     }
 }
