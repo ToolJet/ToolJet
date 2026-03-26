fdeae4511925f4d3ce7e02cfd11c4fe67481e744
emidhun <midhun752@gmail.com>
2026-03-26 11:22:36 +1100

    test: add Azure Blob Storage marketplace datasource Cypress spec

---
+++ cypress-tests/cypress/e2e/happyPath/marketplace/commonTestcases/datasources/azureblobstorageHappyPath.cy.js
@@ -0,0 +1,75 @@
+import { fake } from "Fixtures/fake";
+import { dsCommonSelector } from "Selectors/marketplace/common";
+import { verifyConnectionFormUI } from "Support/utils/marketplace/dataSource/dataSourceFormUIHelpers";
+import { fillDSConnectionForm, verifyDSConnection } from "Support/utils/marketplace/dataSource/dataSourceFormFillHelpers";
+import { azureBlobStorageUIConfig, azureBlobStorageFormConfig } from "Constants/constants/marketplace/datasources/azureblobstorage";
+
+const data = {};
+
+describe("Azure Blob Storage", () => {
+    data.dataSourceName = fake.lastName
+        .toLowerCase()
+        .replaceAll("[^A-Za-z]", "");
+    const azureBlobStorageDataSourceName = `cypress-${data.dataSourceName}-azureblobstorage`;
+    beforeEach(() => {
+        cy.on("uncaught:exception", () => false);
+        cy.apiLogin();
+        cy.viewport(1400, 1600);
+    });
+
+    afterEach(() => {
+        cy.apiDeleteDataSource(azureBlobStorageDataSourceName);
+    });
+
+    it("1. Azure Blob Storage - Verify connection form UI elements - ALL FIELDS", () => {
+        cy.apiCreateDataSource(
+            `${Cypress.env("server_host")}/api/data-sources`,
+            `${azureBlobStorageDataSourceName}`,
+            "azureblobstorage",
+            [
+                { key: "connection_string", value: "", encrypted: false }
+            ]
+        );
+        cy.visit('/my-workspace/data-sources');
+        cy.waitForElement(dsCommonSelector.dataSourceNameButton(azureBlobStorageDataSourceName));
+        cy.get(dsCommonSelector.dataSourceNameButton(azureBlobStorageDataSourceName)).click();
+        verifyConnectionFormUI(azureBlobStorageUIConfig.defaultFields);
+    });
+
+    it("2. Azure Blob Storage - Verify data source connection with valid credentials", () => {
+        cy.apiCreateDataSource(
+            `${Cypress.env("server_host")}/api/data-sources`,
+            `${azureBlobStorageDataSourceName}`,
+            "azureblobstorage",
+            [
+                { key: "connection_string", value: "", encrypted: false }
+            ]
+        );
+        cy.visit('/my-workspace/data-sources');
+        cy.waitForElement(dsCommonSelector.dataSourceNameButton(azureBlobStorageDataSourceName));
+        cy.get(dsCommonSelector.dataSourceNameButton(azureBlobStorageDataSourceName)).click();
+
+        fillDSConnectionForm(azureBlobStorageFormConfig, []);
+
+        verifyDSConnection();
+    });
+
+    it("3. Azure Blob Storage - Verify UI and connection together", () => {
+        cy.apiCreateDataSource(
+            `${Cypress.env("server_host")}/api/data-sources`,
+            `${azureBlobStorageDataSourceName}`,
+            "azureblobstorage",
+            [
+                { key: "connection_string", value: "", encrypted: false }
+            ]
+        );
+        cy.visit('/my-workspace/data-sources');
+        cy.waitForElement(dsCommonSelector.dataSourceNameButton(azureBlobStorageDataSourceName));
+        cy.get(dsCommonSelector.dataSourceNameButton(azureBlobStorageDataSourceName)).click();
+
+        verifyConnectionFormUI(azureBlobStorageUIConfig.defaultFields);
+
+        fillDSConnectionForm(azureBlobStorageFormConfig, azureBlobStorageFormConfig.invalidConnectionString);
+        verifyDSConnection("failed", "Invalid URL");
+    });
+});
