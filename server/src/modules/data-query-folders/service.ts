a2c8d0fd9cf85bcd466aabc644b4e908b2ef98ca
Shaurya Sharma <shaurya064@gmail.com>
2026-03-30 00:21:59 +0530

    Added CE Fallback

---
+++ server/src/modules/data-query-folders/service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { IDataQueryFoldersService } from './interfaces/IService';
+import { CreateFolderDto, DeleteFolderDto, RenameFolderDto, ReorderDto } from './dto';
+
+@Injectable()
+export class DataQueryFoldersService implements IDataQueryFoldersService {
+  async createFolder(_dto: CreateFolderDto): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  async renameFolder(_id: string, _dto: RenameFolderDto): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+
+  async deleteFolder(_id: string, _dto: DeleteFolderDto): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  async reorder(_dto: ReorderDto): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  async getFolders(_appVersionId: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
