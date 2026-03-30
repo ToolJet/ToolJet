a71956b903d9103e4c71872ef4fd2b6c7e54b229
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2026-03-31 01:09:12 +0530

    Revert "fix: remove console.error for expected null-return in getDefaultWorkspaceOfInstance"
    This reverts commit 0930b9d84cba161c09404cc828dd6c7d6124e436.

--- server/src/modules/organizations/repository.ts
+++ server/src/modules/organizations/repository.ts
@@ -218,6 +218,7 @@ export class OrganizationRepository extends Repository<Organization> {
           where: { isDefault: true },
         });
       } catch {
+        console.error('No default workspace in this instance');
         return null;
       }
     });
