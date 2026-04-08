a59b7401452e7a5e47a177a1ddd672be440ab523
Nishidh Jain <nishidhjain909@gmail.com>
2026-04-08 22:47:44 +0530

    fix: Allow actions on page where branching is not enabled but have isWorkspaceBranchLocked like modules & workflows (bug resolved: folder create, edit & delete action)

--- frontend/src/_hooks/useIsWorkspaceBranchLocked.js
+++ frontend/src/_hooks/useIsWorkspaceBranchLocked.js
@@ -1,11 +1,17 @@
+import { useMatch } from 'react-router-dom';
+
 import { useWorkspaceBranchesStore } from '@/_stores/workspaceBranchesStore';
 
 export function useIsWorkspaceBranchLocked() {
+  const isAppsDashboardPage = useMatch('/:workspaceId');
+
   const orgGitConfig = useWorkspaceBranchesStore((state) => state.orgGitConfig);
   const isInitialized = useWorkspaceBranchesStore((state) => state.isInitialized);
   const currentBranch = useWorkspaceBranchesStore((state) => state.currentBranch);
 
-  if (!isInitialized || !orgGitConfig) return false;
+  const branchingEnabledPages = Boolean(isAppsDashboardPage);
+
+  if (!isInitialized || !orgGitConfig || !branchingEnabledPages) return false;
 
   const isBranchingEnabled = orgGitConfig?.is_branching_enabled || orgGitConfig?.isBranchingEnabled;
   const isDefault = currentBranch?.is_default || currentBranch?.isDefault;
