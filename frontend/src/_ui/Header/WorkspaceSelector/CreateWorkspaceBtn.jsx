162db92f2f8ba01c1df62c057a6866978c4d40c8
Nishidh Jain <nishidhjain909@gmail.com>
2026-04-13 21:06:49 +0530

    fix: Add new workspace button not visible to end user

--- frontend/src/_ui/Header/WorkspaceSelector/CreateWorkspaceBtn.jsx
+++ frontend/src/_ui/Header/WorkspaceSelector/CreateWorkspaceBtn.jsx
@@ -2,38 +2,26 @@ import React from 'react';
 
 import { withEditionSpecificComponent } from '@/modules/common/helpers/withEditionSpecificComponent';
 import { Button } from '@/components/ui/Button/Button';
-import LicenseBanner from '@/modules/common/components/LicenseBanner';
 
 function CreateWorkspaceBtn({ onAddWorkspace }) {
-  return <BaseCreateWorkspaceBtn showAddNewWorkspaceBtn onAddWorkspace={onAddWorkspace} />;
+  return <BaseCreateWorkspaceBtn onAddWorkspace={onAddWorkspace} />;
 }
 
 export default withEditionSpecificComponent(CreateWorkspaceBtn, 'common');
 
-export function BaseCreateWorkspaceBtn({ onAddWorkspace, showAddNewWorkspaceBtn = false, workspacesLimit = null }) {
+export function BaseCreateWorkspaceBtn({ onAddWorkspace, hideAddNewWorkspaceBtn = false }) {
   return (
-    <>
-      {showAddNewWorkspaceBtn && (
-        <Button
-          isLucid
-          variant="ghost"
-          leadingIcon="plus"
-          className="tw-w-full"
-          data-cy="add-new-workspace-button"
-          onClick={onAddWorkspace}
-        >
-          Add new workspace
-        </Button>
-      )}
-
-      <LicenseBanner
-        type="workspaces"
-        size="small"
-        bannerVariant="inline-info"
-        classes="[button+&]:tw-mt-2"
-        showNewBanner
-        limits={workspacesLimit ?? {}}
-      />
-    </>
+    !hideAddNewWorkspaceBtn && (
+      <Button
+        isLucid
+        variant="ghost"
+        leadingIcon="plus"
+        className="tw-w-full"
+        data-cy="add-new-workspace-button"
+        onClick={onAddWorkspace}
+      >
+        Add new workspace
+      </Button>
+    )
   );
 }
