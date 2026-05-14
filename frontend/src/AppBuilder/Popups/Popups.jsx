f54c41b3704740506d962aa1e18fd95504c81428
Shaurya Sharma <shaurya064@gmail.com>
2026-05-14 17:12:28 +0530

    Design update

--- frontend/src/AppBuilder/Popups/Popups.jsx
+++ frontend/src/AppBuilder/Popups/Popups.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Confirm } from '@/AppBuilder/Viewer/Confirm';
+import { QueryConfirmPopup } from './QueryConfirmPopup';
 import useStore from '@/AppBuilder/_stores/store';
 import { shallow } from 'zustand/shallow';
 
@@ -7,19 +7,17 @@ export const Popups = ({ darkMode }) => {
   const queryConfirmationList = useStore((state) => state.dataQuery.queryConfirmationList, shallow);
   const onQueryConfirmOrCancel = useStore((state) => state.queryPanel.onQueryConfirmOrCancel);
 
+  const first = queryConfirmationList?.[0];
+
   return (
     <div>
-      <Confirm
+      <QueryConfirmPopup
         show={queryConfirmationList?.length > 0}
-        message={
-          queryConfirmationList?.[0]?.confirmationMessage ||
-          `Do you want to run this query - ${queryConfirmationList?.[0]?.queryName}?`
-        }
-        onConfirm={(queryConfirmationData) => onQueryConfirmOrCancel(queryConfirmationData, true)}
-        onCancel={() => onQueryConfirmOrCancel(queryConfirmationList?.[0])}
-        queryConfirmationData={queryConfirmationList?.[0]}
+        message={first?.confirmationMessage || `Do you want to run this query - ${first?.queryName}?`}
+        onConfirm={() => onQueryConfirmOrCancel(first, true)}
+        onCancel={() => onQueryConfirmOrCancel(first)}
         darkMode={darkMode}
-        key={queryConfirmationList?.[0]?.queryName}
+        dialogKey={first?.queryName}
       />
     </div>
   );
