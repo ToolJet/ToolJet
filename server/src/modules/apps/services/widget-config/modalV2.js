aa297877273f3e0f39d606832690cddb6cd9f93b
parthy007 <parthadhikari1812@gmail.com>
2026-06-04 14:31:20 +0530

    fix: add server side config

--- server/src/modules/apps/services/widget-config/modalV2.js
+++ server/src/modules/apps/services/widget-config/modalV2.js
@@ -426,7 +426,7 @@ export const modalV2Config = {
     styles: {
       icon: { value: 'IconAlignBoxBottomLeft' },
       iconVisibility: { value: false },
-      iconColor: { value: 'var(--cc-default-icon)' },
+      iconColor: { value: 'var(--cc-surface1-surface)' },
       direction: { value: 'left' },
       headerBackgroundColor: { value: 'var(--cc-surface1-surface)' },
       footerBackgroundColor: { value: 'var(--cc-surface1-surface)' },
@@ -434,7 +434,7 @@ export const modalV2Config = {
       triggerButtonBackgroundColor: { value: 'var(--cc-primary-brand)' },
       triggerButtonHoverBackgroundMode: { value: 'auto' },
       triggerButtonHoverBackgroundColor: { value: 'var(--cc-primary-brand)' },
-      triggerButtonTextColor: { value: '#ffffffff' },
+      triggerButtonTextColor: { value: 'var(--cc-surface1-surface)' },
       triggerButtonTextSize: { value: '{{14}}' },
       triggerButtonFontWeight: { value: 'normal' },
       triggerButtonContentAlignment: { value: 'center' },
