ToolJet/ToolJet

commit b6af792c9977a126ce60bd23bb93451efb42b342

Feat: Add tooltip messages for locked branches and enhance input components with tooltips

frontend/src/AppBuilder/Header/RightTopHeaderButtons/ManageAppUsers.jsx
@@ -248,8 +248,7 @@ class ManageAppUsersComponent extends React.Component {
               <div class="shareable-link-container">
                 <div className="make-public mb-3">
                   <div className="form-check form-switch d-flex align-items-center">
-                    {this.props.isVersionReleased ||
-                    (() => {
+                    {(() => {
                       const { currentBranch } = useWorkspaceBranchesStore.getState();
                       return currentBranch && !currentBranch.is_default && !currentBranch.isDefault;
                     })() ? (
@@ -269,8 +268,14 @@ class ManageAppUsersComponent extends React.Component {
                     ) : (
                       <div style={{ display: 'flex', alignItems: 'left', gap: '8px' }}>
                         <ToolTip
-                          message={TOOLTIP_MESSAGES.RELEASE_VERSION_URL_UNAVAILABLE}
+                          message={(() => {
+                            const { currentBranch } = useWorkspaceBranchesStore.getState();
+                            return currentBranch?.is_default || currentBranch?.isDefault
+                              ? TOOLTIP_MESSAGES.DEFAULT_BRANCH_LOCKED
+                              : TOOLTIP_MESSAGES.RELEASE_VERSION_URL_UNAVAILABLE;
+                          })()}
                           placement={'top'}
+                          width="210px"
                           show={isHovered}
                         >
                           <div
@@ -285,6 +290,7 @@ class ManageAppUsersComponent extends React.Component {
                             <input
                               className="form-check-input"
                               type="checkbox"
+                              checked={this?.props?.isPublic}
                               disabled
                               style={{
                                 opacity: 0.3,
@@ -322,54 +328,43 @@ class ManageAppUsersComponent extends React.Component {
                       <span className="input-group-text applink-text flex-grow-1 slug-ellipsis" data-cy="app-link">
                         {appLink}
                       </span>
-                      <div className="input-with-icon">
-                        <input
-                          type="text"
-                          className={`form-control form-control-sm ${slugButtonClass}`}
-                          placeholder={this.props.slug}
-                          maxLength={50}
-                          onChange={(e) => {
-                            e.persist();
-                            this.delayedSlugChange(e);
-                          }}
-                          style={{ maxWidth: '150px' }}
-                          defaultValue={this.props.slug}
-                          data-cy="app-name-slug-input"
-                          disabled={
-                            !this.props.isVersionReleased &&
-                            (() => {
+                      <ToolTip
+                        message={TOOLTIP_MESSAGES.DEFAULT_BRANCH_LOCKED}
+                        placement="top"
+                        width="210px"
+                        show={(() => {
+                          const { currentBranch } = useWorkspaceBranchesStore.getState();
+                          return currentBranch?.is_default || currentBranch?.isDefault;
+                        })()}
+                      >
+                        <div className="input-with-icon">
+                          <input
+                            type="text"
+                            className={`form-control form-control-sm ${slugButtonClass}`}
+                            placeholder={this.props.slug}
+                            maxLength={50}
+                            onChange={(e) => {
+                              e.persist();
+                              this.delayedSlugChange(e);
+                            }}
+                            style={{ maxWidth: '150px' }}
+                            defaultValue={this.props.slug}
+                            data-cy="app-name-slug-input"
+                            disabled={(() => {
                               const { currentBranch } = useWorkspaceBranchesStore.getState();
                               return currentBranch?.is_default || currentBranch?.isDefault;
-                            })()
-                          }
-                        />
-                        {isSlugVerificationInProgress && (
-                          <div className="icon-container">
-                            <div class="spinner-border text-secondary " role="status">
-                              <span class="visually-hidden">Loading...</span>
+                            })()}
+                          />
+                          {isSlugVerificationInProgress && (
+                            <div className="icon-container">
+                              <div class="spinner-border text-secondary " role="status">
+                                <span class="visually-hidden">Loading...</span>
+                              </div>
                             </div>
-                          </div>
-                        )}
+                          )}
 
-                        <div className="icon-container">
-                          {newSlug?.error ? (
-                            <svg
-                              width="21"
-                              height="20"
-                              viewBox="0 0 21 20"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                clip-rule="evenodd"
-                                d="M3.94252 3.61195C4.31445 3.24003 4.91746 3.24003 5.28939 3.61195L10.3302 8.6528L15.3711 3.61195C15.743 3.24003 16.346 3.24003 16.718 3.61195C17.0899 3.98388 17.0899 4.5869 16.718 4.95882L11.6771 9.99967L16.718 15.0405C17.0899 15.4125 17.0899 16.0155 16.718 16.3874C16.346 16.7593 15.743 16.7593 15.3711 16.3874L10.3302 11.3465L5.28939 16.3874C4.91746 16.7593 4.31445 16.7593 3.94252 16.3874C3.57059 16.0155 3.57059 15.4125 3.94252 15.0405L8.98337 9.99967L3.94252 4.95882C3.57059 4.5869 3.57059 3.98388 3.94252 3.61195Z"
-                                fill="#E54D2E"
-                              />
-                            </svg>
-                          ) : (
-                            isSlugUpdated &&
-                            !isSlugVerificationInProgress && (
+                          <div className="icon-container">
+                            {newSlug?.error ? (
                               <svg
                                 width="21"
                                 height="20"
@@ -380,14 +375,32 @@ class ManageAppUsersComponent extends React.Component {
                                 <path
                                   fill-rule="evenodd"
                                   clip-rule="evenodd"
-                                  d="M17.5859 5.24408C17.9114 5.56951 17.9114 6.09715 17.5859 6.42259L9.25259 14.7559C8.92715 15.0814 8.39951 15.0814 8.07407 14.7559L3.90741 10.5893C3.58197 10.2638 3.58197 9.73618 3.90741 9.41074C4.23284 9.08531 4.76048 9.08531 5.08592 9.41074L8.66333 12.9882L16.4074 5.24408C16.7328 4.91864 17.2605 4.91864 17.5859 5.24408Z"
-                                  fill="#46A758"
+                                  d="M3.94252 3.61195C4.31445 3.24003 4.91746 3.24003 5.28939 3.61195L10.3302 8.6528L15.3711 3.61195C15.743 3.24003 16.346 3.24003 16.718 3.61195C17.0899 3.98388 17.0899 4.5869 16.718 4.95882L11.6771 9.99967L16.718 15.0405C17.0899 15.4125 17.0899 16.0155 16.718 16.3874C16.346 16.7593 15.743 16.7593 15.3711 16.3874L10.3302 11.3465L5.28939 16.3874C4.91746 16.7593 4.31445 16.7593 3.94252 16.3874C3.57059 16.0155 3.57059 15.4125 3.94252 15.0405L8.98337 9.99967L3.94252 4.95882C3.57059 4.5869 3.57059 3.98388 3.94252 3.61195Z"
+                                  fill="#E54D2E"
                                 />
                               </svg>
-                            )
-                          )}
+                            ) : (
+                              isSlugUpdated &&
+                              !isSlugVerificationInProgress && (
+                                <svg
+                                  width="21"
+                                  height="20"
+                                  viewBox="0 0 21 20"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M17.5859 5.24408C17.9114 5.56951 17.9114 6.09715 17.5859 6.42259L9.25259 14.7559C8.92715 15.0814 8.39951 15.0814 8.07407 14.7559L3.90741 10.5893C3.58197 10.2638 3.58197 9.73618 3.90741 9.41074C4.23284 9.08531 4.76048 9.08531 5.08592 9.41074L8.66333 12.9882L16.4074 5.24408C16.7328 4.91864 17.2605 4.91864 17.5859 5.24408Z"
+                                    fill="#46A758"
+                                  />
+                                </svg>
+                              )
+                            )}
+                          </div>
                         </div>
-                      </div>
+                      </ToolTip>
                       <span className="input-group-text">
                         <CopyToClipboard text={shareableLink} onCopy={() => toast.success('Link copied to clipboard')}>
                           <svg

frontend/src/AppBuilder/Header/EditAppName.jsx
@@ -82,6 +82,7 @@ function EditAppName() {
         <ToolTip
           message={getDisabledTooltipMessage()}
           placement="bottom"
+          width="210px"
           isVisible={appCreationMode !== 'GIT' || isRenameDisabled}
         >
           <button

frontend/src/AppBuilder/LeftSidebar/GlobalSettings/SlugInput.jsx
@@ -12,6 +12,7 @@ import { useModuleContext } from '@/AppBuilder/_contexts/ModuleContext';
 import { useWorkspaceBranchesStore } from '@/_stores/workspaceBranchesStore';
 import { ToolTip } from '@/_components/ToolTip';
 import { TriangleAlert } from 'lucide-react';
+import { TOOLTIP_MESSAGES } from '@/_helpers/constants';
 // import { useStore } from '@/store';
 
 const SlugInput = () => {
@@ -90,8 +91,10 @@ const SlugInput = () => {
 
   const delayedSlugChange = _.debounce((value, field) => handleInputChange(value, field), 500);
 
+  const { currentBranch } = useWorkspaceBranchesStore();
+  const isDefaultBranch = currentBranch?.is_default || currentBranch?.isDefault;
+
   const SlugLabel = () => {
-    const { currentBranch } = useWorkspaceBranchesStore();
     const isOnFeatureBranch = currentBranch && !currentBranch.is_default && !currentBranch.isDefault;
 
     return (
@@ -117,21 +120,31 @@ const SlugInput = () => {
       <div className="row">
         <div className="col">
           <SlugLabel />
-          <InputComponent
-            helperText={
-              !slug?.error && !isSlugUpdated
-                ? "URL-friendly 'slug' consists of lowercase letters, numbers, and hyphens"
-                : undefined
-            }
-            placeholder={t('editor.appSlug', 'Unique app slug')}
-            maxLength={50}
-            onChange={(e, validateObj) => {
-              e.persist();
-              delayedSlugChange(e.target.value, 'slug');
-            }}
-            data-cy="app-slug-input-field"
-            defaultValue={slug?.value || oldSlug || ''}
-          />
+          <ToolTip
+            message={TOOLTIP_MESSAGES.DEFAULT_BRANCH_LOCKED}
+            placement="top"
+            width="210px"
+            show={isDefaultBranch}
+          >
+            <div>
+              <InputComponent
+                helperText={
+                  !slug?.error && !isSlugUpdated
+                    ? "URL-friendly 'slug' consists of lowercase letters, numbers, and hyphens"
+                    : undefined
+                }
+                placeholder={t('editor.appSlug', 'Unique app slug')}
+                maxLength={50}
+                onChange={(e, validateObj) => {
+                  e.persist();
+                  delayedSlugChange(e.target.value, 'slug');
+                }}
+                data-cy="app-slug-input-field"
+                defaultValue={slug?.value || oldSlug || ''}
+                disabled={isDefaultBranch}
+              />
+            </div>
+          </ToolTip>
           {slug?.error ? (
             <label className="label tj-input-error" data-cy="app-slug-error-label">
               {slug?.error || ''}

frontend/src/HomePage/HomePage.jsx
@@ -1129,6 +1129,13 @@ class HomePageComponent extends React.Component {
         this.setState({ appOperations: { ...appOperations, selectedApp: app }, showAddToFolderModal: true });
         break;
       case 'change-icon':
+        if (this.isWorkspaceBranchLocked()) {
+          this.setState({
+            appOperations: { ...appOperations, selectedApp: app, selectedIcon: app?.icon },
+            showSwitchBranchForChangeIcon: true,
+          });
+          break;
+        }
         this.setState({
           appOperations: { ...appOperations, selectedApp: app, selectedIcon: app?.icon },
           showChangeIconModal: true,
@@ -1749,6 +1756,14 @@ class HomePageComponent extends React.Component {
             onBranchSwitch={() => {
               this.fetchApps(1, this.state.currentFolder.id);
               this.setState({ showSwitchBranchForClone: false, showCloneAppModal: true });
+            }}
+          />
+          <WorkspaceSwitchBranchModal
+            show={this.state.showSwitchBranchForChangeIcon}
+            onClose={() => this.setState({ showSwitchBranchForChangeIcon: false })}
+            onBranchSwitch={() => {
+              this.fetchApps(1, this.state.currentFolder.id);
+              this.setState({ showSwitchBranchForChangeIcon: false, showChangeIconModal: true });
             }}
           />
           <AppActionModal

frontend/src/_helpers/constants.js
@@ -154,6 +154,7 @@ export const DEFAULT_ERROR_MESSAGE = {
 export const TOOLTIP_MESSAGES = {
   SHARE_URL_UNAVAILABLE: 'Share URL is unavailable until current version is released',
   RELEASE_VERSION_URL_UNAVAILABLE: 'Release the version to make it public',
+  DEFAULT_BRANCH_LOCKED: 'Master branch is locked. Switch branch to update app slug.',
 };
 
 export const DATA_SOURCE_TYPE = {

server/src/modules/apps/services/app-import-export.service.ts
@@ -561,7 +561,7 @@ export class AppImportExportService {
             }
           }
 
-          moduleApps.push(await this.export(user, resolvedId, { version_id: versionDbId }));
+          moduleApps.push(await this.export(user, resolvedId, { version_id: versionDbId }, parentBranchId));
         })
       );
 
@@ -822,6 +822,12 @@ export class AppImportExportService {
           }
         } else {
           // Module doesn't exist — import it fresh.
+          // For git-sync imports, override the module's slug with a fresh UUID to
+          // avoid collisions with the app_versions_default_branch_slug_unique constraint.
+          // The source slug (typically the source App.id) is meaningless in the target workspace.
+          if (isGitApp && importedModule?.appV2) {
+            importedModule.appV2.slug = uuid();
+          }
 
           const { newApp, resourceMapping } = await this.import(
             user,
@@ -834,6 +840,16 @@ export class AppImportExportService {
             manager,
             branchId
           );
+
+          // For git-sync imports, preserve the source module's co_relation_id so
+          // ModuleViewer components (which store moduleAppId.value = co_relation_id)
+          // resolve correctly without rewriting. createImportedAppForUser sets
+          // co_relation_id = source.id by default, but ModuleViewer references
+          // the source's co_relation_id field — overwrite to match.
+          if (isGitApp && importedModule?.appV2?.co_relation_id && newApp.co_relation_id !== importedModule.appV2.co_relation_id) {
+            await manager.update(App, { id: newApp.id }, { co_relation_id: importedModule.appV2.co_relation_id });
+            newApp.co_relation_id = importedModule.appV2.co_relation_id;
+          }
 
           // createImportedAppForUser sets new.co_relation_id = source.id. Use
           // the target's co_relation_id as the stable key so consumer

commit 0b8aff28226a762d343fcb7f5033dbc24ecf71a5

fix(share-modal): re-enable make-public toggle for non-git-sync workspaces
Commit b6af792c accidentally dropped the isVersionReleased OR guard
when adding branch-lock tooltips. Without it, the toggle condition
evaluated to false for git-sync OFF users (no currentBranch) and the
checkbox rendered permanently disabled. Restored the correct intent:
enable when no branch context exists (git-sync OFF) or on a non-default
branch; disable only on the default branch.

frontend/src/AppBuilder/Header/RightTopHeaderButtons/ManageAppUsers.jsx
@@ -250,7 +250,9 @@ class ManageAppUsersComponent extends React.Component {
                   <div className="form-check form-switch d-flex align-items-center">
                     {(() => {
                       const { currentBranch } = useWorkspaceBranchesStore.getState();
-                      return currentBranch && !currentBranch.is_default && !currentBranch.isDefault;
+                      // Enabled: git-sync OFF (no branch) or on a non-default branch.
+                      // Disabled only when on the default branch (edits must flow via merge).
+                      return !currentBranch || (!currentBranch.is_default && !currentBranch.isDefault);
                     })() ? (
                       <div>
                         <input
@@ -268,12 +270,7 @@ class ManageAppUsersComponent extends React.Component {
                     ) : (
                       <div style={{ display: 'flex', alignItems: 'left', gap: '8px' }}>
                         <ToolTip
-                          message={(() => {
-                            const { currentBranch } = useWorkspaceBranchesStore.getState();
-                            return currentBranch?.is_default || currentBranch?.isDefault
-                              ? 'Master branch is locked. Switch branch to make the application public.'
-                              : TOOLTIP_MESSAGES.RELEASE_VERSION_URL_UNAVAILABLE;
-                          })()}
+                          message="Master branch is locked. Switch branch to make the application public."
                           placement={'top'}
                           width="210px"
                           show={isHovered}