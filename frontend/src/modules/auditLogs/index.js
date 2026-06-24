4baa2548bb376f63a8c4c53dfa486fd86cff9599
Nishidh Jain <nishidhjain909@gmail.com>
2026-06-24 08:56:59 +0530

    perf: Lazy load Audit logs page, reduces initial chunk size by ~68.8 KB

--- frontend/src/modules/auditLogs/index.js
+++ frontend/src/modules/auditLogs/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import AuditLogsPage from './pages/AuditLogsPage';
-import { AdminRoute } from '@/Routes';
+import React, { lazy } from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+import { TJLoader } from '@/_ui/TJLoader';
+import { AdminRoute } from '@/Routes/AdminRoute';
+import { pickEditionSpecificComponent } from '@/modules/common/helpers/pickEditionSpecificComponent';
 import DesktopOnlyRoute from '@/Routes/DesktopOnlyRoute';
+
+const eeAuditLogsPage = lazy(() => import('@ee/modules/AuditLogs'));
+
+const AuditLogsPage = pickEditionSpecificComponent({
+  ce: () => <Navigate to="/" replace />,
+  ee: eeAuditLogsPage,
+  cloudSameAsEE: true,
+  fallback: <TJLoader />,
+});
+
 /* NOTE:
     This file should be the entry point to a module. 
     Anything inside the module shouldn't be accessible outside module folder 
