7532da90f693c1a61ddf941544a37a82390aa0c1
Vamshi <vamshi@Vamshis-MacBook-Pro.local>
2026-07-15 19:32:40 +0530

    fix: notifications tooltip

--- cypress-tests/cypress/e2e/happyPath/platform/commonTestcases/workspace/workspaceUiTestcases/dashboard.cy.js
+++ cypress-tests/cypress/e2e/happyPath/platform/commonTestcases/workspace/workspaceUiTestcases/dashboard.cy.js
@@ -173,7 +173,7 @@ describe("dashboard", () => {
       commonSelectors.workspaceConstantsIcon,
       "Workspace constants"
     );
-    verifyTooltip(commonSelectors.notificationsIcon, "Comment notifications");
+    verifyTooltip(commonSelectors.notificationsIcon, "Notifications");
     verifyTooltip(dashboardSelector.modeToggle, "Mode");
   });
 
