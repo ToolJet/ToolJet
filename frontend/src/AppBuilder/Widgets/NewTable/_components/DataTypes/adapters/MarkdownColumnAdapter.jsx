e5b9e356a3aebe20287334d864824fa80f35ea3a
devanshu052000 <devanshu.rastogi05@gmail.com>
2026-08-08 01:25:42 +0530

    feat: add editing state management to MarkdownColumn component
    - Introduced isEditing and setIsEditing props to the MarkdownColumn component, enabling better control over the editing state.
    - This enhancement supports improved user interaction and editing capabilities within the table cells.

--- frontend/src/AppBuilder/Widgets/NewTable/_components/DataTypes/adapters/MarkdownColumnAdapter.jsx
+++ frontend/src/AppBuilder/Widgets/NewTable/_components/DataTypes/adapters/MarkdownColumnAdapter.jsx
@@ -44,6 +44,8 @@ export const MarkdownColumn = ({
       containerWidth={containerWidth}
       darkMode={darkMode}
       maxHeight={getMaxHeight(isMaxRowHeightAuto, maxRowHeightValue, cellSize)}
+      isEditing={isEditing}
+      setIsEditing={setIsEditing}
     />
   );
 };
