df17123485e5609c7c219c647da5ca885506752b
gsmithun4 <gsmithun4@gmail.com>
2026-08-12 11:10:56 +0530

    style: format code for improved readability across multiple components

--- frontend/src/AppBuilder/_hooks/useAppData.js
+++ frontend/src/AppBuilder/_hooks/useAppData.js
@@ -461,8 +461,8 @@ const useAppData = (
               'is_maintenance_on' in result
                 ? result.is_maintenance_on
                 : 'isMaintenanceOn' in result
-                ? result.isMaintenanceOn
-                : false,
+                  ? result.isMaintenanceOn
+                  : false,
             organizationId: appData.organizationId || appData.organization_id,
             homePageId: homePageId,
             isPublic: appData.is_public,
@@ -932,8 +932,8 @@ const useAppData = (
             'is_maintenance_on' in appData
               ? appData.is_maintenance_on
               : 'isMaintenanceOn' in appData
-              ? appData.isMaintenanceOn
-              : false,
+                ? appData.isMaintenanceOn
+                : false,
           organizationId: appData.organizationId || appData.organization_id,
           homePageId: appData.editing_version.homePageId,
           isPublic: appData.isPublic,
