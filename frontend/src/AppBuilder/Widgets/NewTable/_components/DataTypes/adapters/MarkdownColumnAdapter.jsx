37f0f8b1d4ad34cde8f4bba3ccf064901017115c
Madhav Jha <jhamadhav28@gmail.com>
2026-09-07 11:59:27 +0530

    Table: markdown column onFocus console issue test added

--- frontend/src/AppBuilder/Widgets/NewTable/_components/DataTypes/adapters/MarkdownColumnAdapter.jsx
+++ frontend/src/AppBuilder/Widgets/NewTable/_components/DataTypes/adapters/MarkdownColumnAdapter.jsx
@@ -44,6 +44,9 @@ export const MarkdownColumn = ({
       containerWidth={containerWidth}
       darkMode={darkMode}
       maxHeight={getMaxHeight(isMaxRowHeightAuto, maxRowHeightValue, cellSize)}
+      isEditing={isEditing}
+      setIsEditing={setIsEditing}
+      id={id}
     />
   );
 };
