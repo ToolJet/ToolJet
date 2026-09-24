7e9175a7d26f755679052da9810aa7a5cba361a3
Madhav Jha <jhamadhav28@gmail.com>
2026-09-24 10:39:05 +0530

    Table: JSON export issue fix added

--- frontend/src/AppBuilder/Widgets/NewTable/_utils/exportData.js
+++ frontend/src/AppBuilder/Widgets/NewTable/_utils/exportData.js
@@ -29,12 +29,14 @@ const getData = (table, forExcel = false) => {
     accessorKeys.forEach((accessorKey) => {
       const cellValue = row.original[accessorKey];
       const isNumber = typeof cellValue === 'number';
+      const isObject = cellValue !== null && typeof cellValue === 'object';
+      const exportValue = isObject ? JSON.stringify(cellValue) : cellValue;
 
       rowData.push(
         !forExcel // Get formatted data for 'export to Excel' as expected by zipcelx's config
-          ? cellValue
+          ? exportValue
           : {
-              value: cellValue,
+              value: exportValue,
               type: isNumber ? 'number' : 'string',
             }
       );
